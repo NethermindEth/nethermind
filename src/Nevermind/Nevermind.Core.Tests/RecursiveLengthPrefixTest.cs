9ebb3743f458c9962825e965f2065f9822dd8894
Tomasz K. Stanczak <tkstanczak@demerzel.co>
2017-08-23 18:15:43 +0100

    solution for neverming (and RLP)

---
+++ src/Nevermind/Nevermind.Core.Tests/RecursiveLengthPrefixTest.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nevermind.Core.Tests
+{
+    [TestClass]
+    public class RecursiveLengthPrefixTests
+    {
+        [TestMethod]
+        public void Serialized_form_is_same_as_input_when_input_length_is_1_and_value_is_less_than_128()
+        {
+            Assert.AreEqual(0, RecursiveLengthPrefix.Serialize(new byte[] {0})[0], "0");
+            Assert.AreEqual(127, RecursiveLengthPrefix.Serialize(new byte[] {127})[0], "128");
+            Assert.AreEqual(1, RecursiveLengthPrefix.Serialize(new byte[] {1})[0], "1");
+        }
+
+        [TestMethod]
+        public void Serialized_form_is_128_when_input_is_empty()
+        {
+            Assert.AreEqual(128, RecursiveLengthPrefix.Serialize(new byte[] { })[0]);
+        }
+
+        [TestMethod]
+        public void
+            Serialized_form_is_input_prefixed_with_128_plus_length_of_input_when_input_length_is_between_1_and_56()
+        {
+            byte[] input = new byte[55];
+            input[0] = 255;
+            input[1] = 128;
+            input[2] = 1;
+
+            Assert.AreEqual(183, RecursiveLengthPrefix.Serialize(input)[0]);
+            Assert.AreEqual(56, RecursiveLengthPrefix.Serialize(input).Length);
+            for (int i = 0; i < 55; i++)
+            {
+                Assert.AreEqual(input[i], RecursiveLengthPrefix.Serialize(input)[i + 1]);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(128L, (byte) (1 + 183), (byte) 128)]
+        [DataRow(256L, (byte) (1 + 1 + 183), (byte) 1)]
+        [DataRow(256L * 256L, (byte) (1 + 2 + 183), (byte) 1)]
+        [DataRow(256L * 256L * 256L, (byte) (1 + 3 + 183), (byte) 1)]
+        //[DataRow(256L * 256L * 256L * 256L, (byte)(1 + 4 + 183), (byte)1)]
+        public void Serialized_form_is_input_prefixed_with_big_endian_length_and_prefixed_with_length_of_it_plus_183(
+            long inputLength, byte expectedFirstByte, byte expectedSecondByte)
+        {
+            byte[] input = new byte[inputLength];
+            input[0] = 255;
+            input[1] = 128;
+            input[2] = 1;
+
+            Assert.AreEqual(expectedFirstByte, RecursiveLengthPrefix.Serialize(input)[0]);
+            Assert.AreEqual(expectedSecondByte, RecursiveLengthPrefix.Serialize(input)[1]);
+
+            if (inputLength < 256)
+            {
+                for (int i = 0; i < 128; i++)
+                {
+                    Assert.AreEqual(input[i], RecursiveLengthPrefix.Serialize(input)[i + 1 + expectedFirstByte - 183]);
+                }
+            }
+        }
+
+        [TestMethod] // not yet sure how it works
+        public void Serializing_sequences()
+        {
+            IEnumerable<object> input = new List<object> {255L, new byte[] {255}};
+            byte[] output = RecursiveLengthPrefix.Serialize(input);
+            Assert.AreEqual(4, output.Length);
+        }
+
+        [TestMethod] // not yet sure how it works
+        public void Serializing_empty_sequence()
+        {
+            IEnumerable<object> input = new List<object>();
+            byte[] output = RecursiveLengthPrefix.Serialize(input);
+            Assert.AreEqual(1, output.Length);
+            Assert.AreEqual(128, output[0]);
+        }
+    }
+}
---
+++ src/Nevermind/Nevermind.Core/RecursiveLengthPrefix.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Nevermind.Core
+{
+    /// <summary>
+    /// If the value to be serialised is a byte-array, the RLP serialisation takes one of three forms:
+    ///   • If the byte-array contains solely a single byte and that single byte is less than 128,
+    ///     then the input is exactly equal to the output.
+    ///   • If the byte-array contains fewer than 56 bytes,
+    ///     then the output is equal to the input preﬁxed by the byte equal to the length of the byte array plus 128.
+    ///   • Otherwise, the output is equal to the input preﬁxed by the minimal-length byte-array
+    ///     which when interpreted as a big-endian integer is equal to the length of the input byte array,
+    ///     which is itself preﬁxed by the number of bytes required to faithfully encode this length value plus 183.
+    /// 
+    /// If instead, the value to be serialised is a sequence of other items then the RLP serialisation takes one of two forms:
+    ///   • If the concatenated serialisations of each contained item is less than 56 bytes in length,
+    ///     then the output is equal to that concatenation preﬁxed by the byte equal to the length of this byte array plus 192.
+    ///   • Otherwise, the output is equal to the concatenated serialisations preﬁxed by the minimal-length byte-array
+    ///     which when interpreted as a big-endian integer is equal to the length of the concatenated serialisations byte array,
+    ///     which is itself preﬁxed by the number of bytes required to faithfully encode this length value plus 247. 
+    /// </summary>
+    public static class RecursiveLengthPrefix
+    {
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        public static byte[] Serialize(IEnumerable<object> sequence)
+        {
+            object firstItem = sequence.FirstOrDefault();
+            if (firstItem == null)
+            {
+                return Serialize(new byte[0]);
+            }
+
+            byte[] concatenation = new byte[0];
+            foreach (object item in sequence)
+            {
+                concatenation = Concat(concatenation, Serialize(item));
+            }
+
+            if (concatenation.Length < 56)
+            {
+                return Concat((byte)(192 + concatenation.Length), concatenation);
+            }
+
+            byte[] serializedLength = Serialize(concatenation.Length);
+            byte prefix = (byte) (247 + serializedLength.Length);
+            return Concat(prefix, serializedLength, concatenation);
+        }
+
+        public static byte[] Concat(byte prefix, byte[] x)
+        {
+            if (x == null) throw new ArgumentNullException(nameof(x));
+
+            byte[] output = new byte[1 + x.Length];
+            output[0] = prefix;
+            Array.Copy(x, 0, output, 1, x.Length);
+            return output;
+        }
+
+        public static byte[] Concat(byte[] x, byte[] y)
+        {
+            if (x == null) throw new ArgumentNullException(nameof(x));
+            if (y == null) throw new ArgumentNullException(nameof(y));
+
+            byte[] output = new byte[x.Length + y.Length]; 
+            Array.Copy(x, 0, output, 0, x.Length);
+            Array.Copy(y, 0, output, x.Length, y.Length);
+            return output;
+        }
+
+        public static byte[] Concat(byte prefix, byte[] x, byte[] y)
+        {
+            if (x == null) throw new ArgumentNullException(nameof(x));
+            if (y == null) throw new ArgumentNullException(nameof(y));
+
+            byte[] output = new byte[1 + x.Length + y.Length];
+            output[0] = prefix;
+            Array.Copy(x, 0, output, 1, x.Length);
+            Array.Copy(y, 0, output, 1 + x.Length, y.Length);
+            return output;
+        }
+
+        public static byte[] Serialize(object item)
+        {
+            byte[] byteArray = item as byte[];
+            if (byteArray != null)
+            {
+                return Serialize(byteArray);
+            }
+
+            if (item is long)
+            {
+                return Serialize((long)item);
+            }
+
+            throw new NotSupportedException($"{nameof(RecursiveLengthPrefix)} only supports {nameof(Int64)} and byte arrays");
+        }
+
+        public static byte[] Serialize(byte[] input)
+        {
+            if (input.Length == 1 && input[0] < 128)
+            {
+                return input;
+            }
+
+            if (input.Length < 56)
+            {
+                byte smallPrefix = (byte) (input.Length + 128);
+                return Concat(smallPrefix, input);
+            }
+
+            byte[] serializedLength = Serialize(input.Length);
+            byte prefix = (byte)(183 + serializedLength.Length);
+            return Concat(prefix, serializedLength, input);
+        }
+
+        public static byte[] Serialize(long length)
+        {
+            if (length < 56)
+            {
+                throw new ArgumentException("Length for BigEndian is expected to be above 56", nameof(length));
+            }
+
+            const int maxResultLength = 8;
+            byte[] bytes = new byte[maxResultLength];
+
+            bytes[0] = (byte)(length >> 56);
+            bytes[1] = (byte)(length >> 48);
+            bytes[2] = (byte)(length >> 40);
+            bytes[3] = (byte)(length >> 32);
+            bytes[4] = (byte)(length >> 24);
+            bytes[5] = (byte)(length >> 16);
+            bytes[6] = (byte)(length >> 8);
+            bytes[7] = (byte)length;
+
+            int resultLength = maxResultLength;
+            for (int i = 0; i < maxResultLength; i++)
+            {
+                if (bytes[i] == 0)
+                {
+                    resultLength--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            byte[] result = new byte[resultLength];
+            Array.Copy(bytes, maxResultLength - resultLength, result, 0, resultLength);
+            return result;
+        }
+    }
+}
