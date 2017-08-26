85076a2797415dfc95588f06e66d96960d34641d
Tomasz K. Stanczak <tkstanczak@demerzel.co>
2017-08-26 04:42:33 +0100

    HexPrefix refactoring

--- src/Nevermind/Nevermind.Core/HexPrefix.cs
+++ src/Nevermind/Nevermind.Core/HexPrefix.cs
@@ -5,18 +5,14 @@
         public static byte[] Encode(Nibelung nibelung)
         {
             byte[] output = new byte[nibelung.Nibbles.Length / 2 + 1];
-            output[0] = (byte) (16 * (nibelung.Flag ? 2 : 0) +
+            output[0] = (byte)(16 * (nibelung.Flag ? 2 : 0) +
                                 nibelung.Nibbles.Length % 2 * (16 + nibelung.Nibbles[0]));
             for (int i = 0; i < nibelung.Nibbles.Length - 1; i = i + 2)
             {
-                if (nibelung.Nibbles.Length % 2 == 0)
-                {
-                    output[i / 2 + 1] = (byte) (16 * nibelung.Nibbles[i] + nibelung.Nibbles[i + 1]);
-                }
-                else
-                {
-                    output[i / 2 + 1] = (byte) (16 * nibelung.Nibbles[i + 1] + nibelung.Nibbles[i + 2]);
-                }
+                output[i / 2 + 1] =
+                    nibelung.Nibbles.Length % 2 == 0
+                        ? (byte)(16 * nibelung.Nibbles[i] + nibelung.Nibbles[i + 1])
+                        :  output[i / 2 + 1] = (byte)(16 * nibelung.Nibbles[i + 1] + nibelung.Nibbles[i + 2]);
             }
 
             return output;
@@ -28,26 +24,16 @@
             bool isEven = (bytes[0] | 240) == 240;
             int nibblesCount = bytes.Length * 2 - (isEven ? 2 : 1);
             nibelung.Nibbles = new byte[nibblesCount];
-            if (isEven)
+            for (int i = 0; i < nibblesCount; i++)
             {
-                for (int i = 0; i < nibblesCount; i = i + 2)
-                {
-                    nibelung.Nibbles[i] = (byte)((bytes[1 + i / 2] & 240) / 16);
-                    nibelung.Nibbles[i + 1] = (byte)(bytes[1 + i / 2] & 15);
-                }
-            }
-            else
-            {
-                for (int i = 0; i < nibblesCount; i = i + 2)
-                {
-                    nibelung.Nibbles[i] = (byte)(bytes[i / 2] & 15);
-                    if (i + 1 == nibblesCount)
-                    {
-                        break;
-                    }
-
-                    nibelung.Nibbles[i + 1] = (byte)((bytes[1 + i / 2] & 240) / 16);
-                }
+                nibelung.Nibbles[i] =
+                    isEven
+                        ? i % 2 == 0
+                            ? (byte)((bytes[1 + i / 2] & 240) / 16)
+                            : (byte)(bytes[1 + i / 2] & 15)
+                        : i % 2 == 0
+                            ? (byte)(bytes[i / 2] & 15)
+                            : (byte)((bytes[1 + i / 2] & 240) / 16);
             }
 
             return nibelung;
