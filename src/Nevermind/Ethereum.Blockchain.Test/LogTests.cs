7547771ec8dabc753ff9e1b31986835f6730477f
Tomasz K. Stanczak <tkstanczak@demerzel.co>
2017-11-10 10:49:58 +0000

    state transition - work in progress

---
+++ src/Nevermind/Ethereum.Blockchain.Test/LogTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Ethereum.Blockchain.Test
+{
+    [TestFixture]
+    public class LogTests : BlockchainTestBase
+    {
+        [TestCaseSource(nameof(LoadTests), new object[] { "LogTests" })]
+        public void Test(BlockchainTest generateStateTest)
+        {    
+            RunTest(generateStateTest);
+        }
+    }
+}
---
+++ src/Nevermind/Ethereum.Blockchain.Test/StaticCallTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Ethereum.Blockchain.Test
+{
+    [TestFixture]
+    public class StaticCallTests : BlockchainTestBase
+    {
+        [TestCaseSource(nameof(LoadTests), new object[] { "StaticCall" })]
+        public void Test(BlockchainTest generateStateTest)
+        {    
+            RunTest(generateStateTest);
+        }
+    }
+}
