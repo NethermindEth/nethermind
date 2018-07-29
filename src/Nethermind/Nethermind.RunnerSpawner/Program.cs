3b1d6798d3e431073a3f3c20fa4461c65dbabf1d
Tomasz K. Stanczak <tkstanczak@demerzel.co>
2018-07-29 19:51:00 +0100

    rough chaos monkey

--- src/Nethermind/Nethermind.RunnerSpawner/Program.cs
+++ src/Nethermind/Nethermind.RunnerSpawner/Program.cs
@@ -34,17 +34,15 @@ namespace Nethermind.RunnerSpawner
         public string ReleasePath { get; set; }
         public Dictionary<string, InitParams> Runners { get; set; }
     }
-    
+
     internal class Program
     {
-        private static readonly Dictionary<string, Process> Processes = new Dictionary<string, Process>();
-
         private static ILogger _logger;
-        
+
         public static int Main(string[] args)
         {
             _logger = new NLogLogger("spawner.logs.txt");
-            
+
             if (args.Length > 1)
             {
                 _logger.Error("Expecting at most one argument - the name of the config file (default when empty).");
@@ -58,40 +56,30 @@ namespace Nethermind.RunnerSpawner
             IJsonSerializer serializer = new UnforgivingJsonSerializer();
             SpawnerConfig spawnerConfig = serializer.Deserialize<SpawnerConfig>(jsonText);
 
+            List<ProcessWrapper> wrappers = new List<ProcessWrapper>();
             foreach ((string name, InitParams parameters) in spawnerConfig.Runners)
             {
                 string serialized = serializer.Serialize(parameters, true);
                 string singleConfigPath = Path.Combine(Path.GetTempPath(), $"nethermind.runner.{name}.config.json");
                 File.WriteAllText(singleConfigPath, serialized);
 
-                CreateAppConsole(spawnerConfig.ReleasePath, name, new[] {singleConfigPath});
+                try
+                {
+                    wrappers.Add(CreateAppConsole(spawnerConfig.ReleasePath, name, new[] {singleConfigPath}));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
             }
+
+            ChaosMonkey chaosMonkey = new ChaosMonkey(_logger, new ChaosMonkey.ChaosMonkeyOptions {IntervalSeconds = 10, AllDownIntervalSeconds = 90}, wrappers.ToArray());
+            chaosMonkey.Start();
 
             _logger.Info("Press ENTER to close all the spawned processes.");
             Console.ReadLine();
-            foreach ((string name, Process process) in Processes)
-            {
-                _logger.Info($"Closing {name}...");
-                if (!process.HasExited)
-                {
-                    process.Kill();
-                    process.WaitForExit(5000);
-                    if (process.HasExited)
-                    {
-                        _logger.Info($"{name} closed.");    
-                    }
-                    else
-                    {
-                        _logger.Error($"{name} could not be closed.");
-                    }
-                }
-                else
-                {
-                    _logger.Info($"{name} already exited.");
-                }
-                
-                process.Close();
-            }
+
+            chaosMonkey.Stop();
 
             _logger.Info("Press ENTER to exit.");
 
@@ -99,30 +87,22 @@ namespace Nethermind.RunnerSpawner
             return 0;
         }
 
-        private static void CreateAppConsole(string path, string name, string[] args)
+        private static ProcessWrapper CreateAppConsole(string path, string name, string[] args)
         {
-            try
-            {
-                Process process = new Process();
-                process.EnableRaisingEvents = false; // change to true if needed
-                process.ErrorDataReceived += ProcessOnErrorDataReceived;
-                process.OutputDataReceived += ProcessOnOutputDataReceived;
-                process.Exited += ProcessOnExited;
-                process.StartInfo.WorkingDirectory = path;
-                process.StartInfo.FileName = "dotnet";
-                process.StartInfo.Arguments = $"Nethermind.Runner.dll --config {string.Join(' ', args)}";
-                process.StartInfo.UseShellExecute = true;
-                process.StartInfo.CreateNoWindow = true;
-                process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
-                _logger.Info($"Starting {process.StartInfo.WorkingDirectory} {process.StartInfo.FileName} {process.StartInfo.Arguments}");
-                process.Start();
+            Process process = new Process();
+            process.EnableRaisingEvents = false; // change to true if needed
+            process.ErrorDataReceived += ProcessOnErrorDataReceived;
+            process.OutputDataReceived += ProcessOnOutputDataReceived;
+            process.Exited += ProcessOnExited;
+            process.StartInfo.WorkingDirectory = path;
+            process.StartInfo.FileName = "dotnet";
+            process.StartInfo.Arguments = $"Nethermind.Runner.dll --config {string.Join(' ', args)}";
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            _logger.Info($"Starting {process.StartInfo.WorkingDirectory} {process.StartInfo.FileName} {process.StartInfo.Arguments}");
 
-                Processes.Add(name, process);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
+            return new ProcessWrapper(name, process);
         }
 
         private static void ProcessOnExited(object sender, EventArgs eventArgs)
