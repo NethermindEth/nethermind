c8edd2f48677e8384644d00e7eff5a0373445706
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2018-09-20 13:20:29 +0100

    EIP145 tests to take into account eip1283

--- src/Nethermind/Nethermind.Evm.Test/Eip145Tests.cs
+++ src/Nethermind/Nethermind.Evm.Test/Eip145Tests.cs
@@ -45,7 +45,7 @@ namespace Nethermind.Evm.Test
             AssertGas(receipt, result.IsZero() ? ZeroResultGas : NonZeroResultGas);
         }
 
-        private const long ZeroResultGas = GasCostOf.Transaction + 4 * GasCostOf.VeryLow + GasCostOf.SReset;
+        private const long ZeroResultGas = GasCostOf.Transaction + 4 * GasCostOf.VeryLow + GasCostOf.SStoreEip1283;
         private const long NonZeroResultGas = GasCostOf.Transaction + 4 * GasCostOf.VeryLow + GasCostOf.SSet;
 
         [TestCase("0x0000000000000000000000000000000000000000000000000000000000000001", "0x00", "0x0000000000000000000000000000000000000000000000000000000000000001")]
