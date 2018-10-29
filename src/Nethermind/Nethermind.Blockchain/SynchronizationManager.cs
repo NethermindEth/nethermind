2a4a7418caeb614a472de3f8f10d528614d6bfcf
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2018-10-29 18:30:54 +0000

    removing unnecessary block download verbosity for now

--- src/Nethermind/Nethermind.Blockchain/SynchronizationManager.cs
+++ src/Nethermind/Nethermind.Blockchain/SynchronizationManager.cs
@@ -819,10 +819,10 @@ namespace Nethermind.Blockchain
 
                 peerInfo.NumberReceived = blocks[blocks.Length - 1].Number;
                 bestNumber = _blockTree.BestKnownNumber;
-                if (bestNumber > _lastSyncNumber + 1000)
+                if (bestNumber > _lastSyncNumber + 10000)
                 {
                     _lastSyncNumber = bestNumber;
-                    if (_logger.IsInfo) _logger.Info($"Downloading blocks. Current best at {_blockTree.BestKnownNumber}");
+                    if (_logger.IsDebug) _logger.Debug($"Downloading blocks. Current best at {_blockTree.BestKnownNumber}");
                 }
             }
 
