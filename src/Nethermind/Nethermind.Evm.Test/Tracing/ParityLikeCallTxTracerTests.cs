090e2973b0324bbe67bf689c0188f0649e67dd33
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2018-11-22 13:04:52 +0000

    #266 parity trace sample

--- src/Nethermind/Nethermind.Evm.Test/Tracing/ParityLikeCallTxTracerTests.cs
+++ src/Nethermind/Nethermind.Evm.Test/Tracing/ParityLikeCallTxTracerTests.cs
@@ -16,17 +16,85 @@
  * along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
  */
 
-using System.Linq;
 using Nethermind.Core;
-using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
-using Nethermind.Core.Test.Builders;
 using Nethermind.Dirichlet.Numerics;
 using Nethermind.Evm.Tracing;
 using NUnit.Framework;
 
 namespace Nethermind.Evm.Test.Tracing
 {
+    /*
+     *{
+     *    "action": {
+     *      "callType": "call",
+     *      "from": "0x430adc807210dab17ce7538aecd4040979a45137",
+     *      "gas": "0x1a1f8",
+     *      "input": "0x",
+     *      "to": "0x9bcb0733c56b1d8f0c7c4310949e00485cae4e9d",
+     *      "value": "0x2707377c7552d8000"
+     *    },
+     *    "blockHash": "0x3aa472d57e220458fe5b9f1587b9211de68b27504064f5f6e427c68fc1691a29",
+     *    "blockNumber": 2392500,
+     *    "result": {
+     *      "gasUsed": "0x2162",
+     *      "output": "0x"
+     *    },
+     *    "subtraces": 2,
+     *    "traceAddress": [],
+     *    "transactionHash": "0x847ed5e2e9430bc6ee925a81137ebebe0cea1352209f96723d3503eb7a707aa8",
+     *    "transactionPosition": 42,
+     *    "type": "call"
+     *  },
+     *  {
+     *    "action": {
+     *      "callType": "call",
+     *      "from": "0x9bcb0733c56b1d8f0c7c4310949e00485cae4e9d",
+     *      "gas": "0x13f57",
+     *      "input": "0x16c72721",
+     *      "to": "0x2bd2326c993dfaef84f696526064ff22eba5b362",
+     *      "value": "0x0"
+     *    },
+     *    "blockHash": "0x3aa472d57e220458fe5b9f1587b9211de68b27504064f5f6e427c68fc1691a29",
+     *    "blockNumber": 2392500,
+     *    "result": {
+     *      "gasUsed": "0xc5",
+     *      "output": "0x0000000000000000000000000000000000000000000000000000000000000001"
+     *    },
+     *    "subtraces": 0,
+     *    "traceAddress": [
+     *      0
+     *    ],
+     *    "transactionHash": "0x847ed5e2e9430bc6ee925a81137ebebe0cea1352209f96723d3503eb7a707aa8",
+     *    "transactionPosition": 42,
+     *    "type": "call"
+     *  },
+     *  {
+     *    "action": {
+     *      "callType": "call",
+     *      "from": "0x9bcb0733c56b1d8f0c7c4310949e00485cae4e9d",
+     *      "gas": "0x8fc",
+     *      "input": "0x",
+     *      "to": "0x9e6316f44baeeee5d41a1070516cc5fa47baf227",
+     *      "value": "0x2707377c7552d8000"
+     *    },
+     *    "blockHash": "0x3aa472d57e220458fe5b9f1587b9211de68b27504064f5f6e427c68fc1691a29",
+     *    "blockNumber": 2392500,
+     *    "result": {
+     *      "gasUsed": "0x0",
+     *      "output": "0x"
+     *    },
+     *    "subtraces": 0,
+     *    "traceAddress": [
+     *      1
+     *    ],
+     *    "transactionHash": "0x847ed5e2e9430bc6ee925a81137ebebe0cea1352209f96723d3503eb7a707aa8",
+     *    "transactionPosition": 42,
+     *    "type": "call"
+     *  }
+     *
+     */
+    
     [TestFixture]
     public class ParityLikeCallTxTracerTests : VirtualMachineTestsBase
     {
