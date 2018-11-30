1d4ae17b5d92a3af8743dfee2e1d6f7436095d7a
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2018-11-30 02:18:26 +0000

    I will be there in 10

--- src/Nethermind/Nethermind.MrWolf/Program.cs
+++ src/Nethermind/Nethermind.MrWolf/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Numerics;
+using System.Xml.Serialization;
 using Nethermind.Core.Logging;
 using Nethermind.Db;
 using Nethermind.Db.Config;
@@ -12,26 +13,71 @@ namespace Nethermind.MrWolf
         static void Main(string[] args)
         {
             Console.WriteLine("Jimmie, lead the way. Boys, get to work.");
+            
+            Console.WriteLine("OK, give me the DB location:");
+            string dbPath = Console.ReadLine();
+            if (!(dbPath?.EndsWith("blockInfos") ?? false))
+            {
+                Console.WriteLine("DB location should end with blockInfos");
+                return;
+            }
+            
+            Console.WriteLine("It is 30 minutes away, I will be there in 10.");
+            Console.WriteLine();
+            
             Console.WriteLine("1) Delete chain levels beyond X");
+            Console.WriteLine("2) Find first missing level");
 
             while (true)
             {
                 string choice = Console.ReadLine();
-                if (choice != "1)")
+                if (choice != "1)" && choice != "2)")
                 {
                     Console.WriteLine("Come again?");
                 }
                 else
-                {
-                    break;
+                {        
+                    if (choice == "1)")
+                    {
+                        DeleteBeyond(dbPath);
+                    }
+                    else if (choice == "2)")
+                    {
+                        FindMissing(dbPath);
+                    }
                 }
             }
-            
+        }
+
+        private static void FindMissing(string dbPath)
+        {
+            IDb dbOnTheRocks = new DbOnTheRocks(dbPath, DbConfig.Default, new OneLoggerLogManager(new SimpleConsoleLogger()));
+            BigInteger current = 0;
+            while(true)
+            {
+                byte[] bytes = dbOnTheRocks.Get(current);
+                if (bytes == null)
+                {
+                    Console.WriteLine($"{current} is missing");
+                    break;
+                }
+                
+                if(current % 1000 == 0)
+                {
+                    Console.WriteLine($"{current} is in DB");
+                }
+
+                current++;
+            }
+        }
+
+        private static void DeleteBeyond(string dbPath)
+        {
             Console.WriteLine("Now, provide a level number, level that is beyond the current head reported by the client:");
             Console.WriteLine("You should see something like: ");
             Console.WriteLine("|Block tree initialized, last processed is 6781580 (d36623), best queued is 6781580, best known is 6785054");
             Console.WriteLine("In such case you would provide: 6781581");
-            
+
             if (GetNumber("from", out BigInteger numberFromParsed)) return;
             if (GetNumber("to", out BigInteger numberToParsed)) return;
 
@@ -39,14 +85,6 @@ namespace Nethermind.MrWolf
             string confirmation = Console.ReadLine();
             if (confirmation == "yes")
             {
-                Console.WriteLine("OK, give me the DB location:");
-                string dbPath = Console.ReadLine();
-                if (!(dbPath?.EndsWith("blockInfos") ?? false))
-                {
-                    Console.WriteLine("DB location should end with blockInfos");
-                    return;
-                }
-                
                 IDb dbOnTheRocks = new DbOnTheRocks(dbPath, DbConfig.Default, new OneLoggerLogManager(new SimpleConsoleLogger()));
 
                 for (BigInteger current = numberFromParsed; current <= numberToParsed; current++)
@@ -54,12 +92,12 @@ namespace Nethermind.MrWolf
                     byte[] bytes = dbOnTheRocks.Get(current);
                     if (bytes != null)
                     {
-                        Console.Write($"Deleting {current}:");
+                        Console.WriteLine($"Deleting {current}:");
                         dbOnTheRocks.Delete(current);
                     }
                     else
                     {
-                        Console.Write($"Skipping {current} as it did not exist");
+                        Console.WriteLine($"Skipping {current} as it did not exist");
                     }
                 }
             }
