f6a7c7ee3a8324158e73b492fc5a4079f14581f0
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-03-10 17:19:49 +0100

    easier gas tracking in EvmPlayground

--- src/Nethermind/Nethermind.EvmPlayground/Transaction.cs
+++ src/Nethermind/Nethermind.EvmPlayground/Transaction.cs
@@ -14,10 +14,10 @@ namespace Nethermind.EvmPlayground
         public string From { get; }
 
         [JsonProperty("gas", Order = 1)]
-        public string Gas { get; } = "0x6691b7";
+        public string Gas { get; } = "0xF4240";
 
         [JsonProperty("gasPrice", Order = 2)]
-        public string GasPrice { get; } = "0x174876e800";
+        public string GasPrice { get; } = "0x4A817C800";
 
         [JsonProperty("to", Order = 3)]
         public string To { get; }
