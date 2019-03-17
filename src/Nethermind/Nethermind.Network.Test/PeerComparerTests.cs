69507b6b2820e9b9b22a37e2f30b67694baaf691
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-03-17 06:38:58 +0000

    more memory improvements

---
+++ src/Nethermind/Nethermind.Network.Test/PeerComparerTests.cs
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2018 Demerzel Solutions Limited
+ * This file is part of the Nethermind library.
+ *
+ * The Nethermind library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Nethermind library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using Nethermind.Core.Test.Builders;
+using Nethermind.Stats;
+using Nethermind.Stats.Model;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Nethermind.Network.Test
+{
+    [TestFixture]
+    public class PeerComparerTests
+    {
+        private INodeStatsManager _statsManager;
+        private PeerManager.PeerComparer _comparer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _statsManager = Substitute.For<INodeStatsManager>();
+            _comparer = new PeerManager.PeerComparer(_statsManager);
+        }
+
+        [Test]
+        public void Can_sort_by_trusted()
+        {
+            Node a = new Node(TestItem.PublicKeyA, "127.0.0.1", 30303);
+            a.IsTrusted = true;
+            Peer peerA = new Peer(a);
+
+            Node b = new Node(TestItem.PublicKeyB, "127.0.0.1", 30303);
+            Peer peerB = new Peer(b);
+            
+            Node c = new Node(TestItem.PublicKeyC, "127.0.0.1", 30303);
+            Peer peerC = new Peer(c);
+
+            Assert.AreEqual(1, _comparer.Compare(peerA, peerB));
+            Assert.AreEqual(0, _comparer.Compare(peerB, peerC));
+        }
+        
+        [Test]
+        public void Can_sort_by_Reputation()
+        {
+            Node a = new Node(TestItem.PublicKeyA, "127.0.0.1", 30303);
+            Peer peerA = new Peer(a);
+
+            Node b = new Node(TestItem.PublicKeyB, "127.0.0.1", 30303);
+            Peer peerB = new Peer(b);
+            
+            Node c = new Node(TestItem.PublicKeyC, "127.0.0.1", 30303);
+            Peer peerC = new Peer(c);
+
+            _statsManager.GetCurrentReputation(a).Returns(100);
+            _statsManager.GetCurrentReputation(b).Returns(50);
+            _statsManager.GetCurrentReputation(c).Returns(200);
+
+            Assert.AreEqual(1, _comparer.Compare(peerA, peerB));
+            Assert.AreEqual(-1, _comparer.Compare(peerA, peerC));
+            Assert.AreEqual(-1, _comparer.Compare(peerB, peerC));
+            Assert.AreEqual(0, _comparer.Compare(peerA, peerA));
+            Assert.AreEqual(0, _comparer.Compare(peerB, peerB));
+            Assert.AreEqual(0, _comparer.Compare(peerC, peerC));
+        }
+    }
+}
