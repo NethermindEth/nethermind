79db0a5e477117cd238087b16281ad82cf5b7fe5
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-04-07 02:13:19 +0100

    blockchash fix for longs

--- src/Nethermind/Nethermind.Evm/VirtualMachine.cs
+++ src/Nethermind/Nethermind.Evm/VirtualMachine.cs
@@ -1519,7 +1519,8 @@ namespace Nethermind.Evm
                         }
 
                         PopUInt256(out UInt256 a, bytesOnStack);
-                        PushBytes(_blockhashProvider.GetBlockhash(env.CurrentBlock, (long)a)?.Bytes ?? BytesZero32, bytesOnStack);
+                        long number = a > long.MaxValue ? long.MaxValue : (long)a;
+                        PushBytes(_blockhashProvider.GetBlockhash(env.CurrentBlock, number)?.Bytes ?? BytesZero32, bytesOnStack);
 
                         break;
                     }
