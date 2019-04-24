df4dfe10d455e61862941713f865d95e24a019f8
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-04-24 11:08:05 +0100

    minor

--- src/Nethermind/Nethermind.Blockchain/Synchronization/SyncTriggerType.cs
+++ src/Nethermind/Nethermind.Blockchain/Synchronization/SyncTriggerType.cs
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
  */
-
+OK
 namespace Nethermind.Blockchain.Synchronization
 {
     public enum SyncTriggerType
