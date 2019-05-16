990670c2d3ce89203ff5c261b3f28afd50106415
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-05-16 02:49:03 +0100

    seals with latencies

--- src/Nethermind/Nethermind.Blockchain/Synchronization/FastBlocks/ParallelBlockDownloader.cs
+++ src/Nethermind/Nethermind.Blockchain/Synchronization/FastBlocks/ParallelBlockDownloader.cs
@@ -82,10 +82,10 @@ namespace Nethermind.Blockchain.Synchronization.FastBlocks
                 (BlocksDataHandlerResult Result, int NodesConsumed) result = (BlocksDataHandlerResult.InvalidFormat, 0);
                 try
                 {
-//                    if (batch.HeadersSyncBatch?.Response != null)
-//                    {
-//                        ValidateSeals(token, batch.HeadersSyncBatch.Response);
-//                    }
+                    if (batch.HeadersSyncBatch?.Response != null)
+                    {
+                        ValidateSeals(token, batch.HeadersSyncBatch.Response);
+                    }
 
                     result = _blockRequestFeed.HandleResponse(batch);
                     if (result.Result == BlocksDataHandlerResult.BadQuality)
