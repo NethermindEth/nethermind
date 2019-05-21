da50e39cbe323da02a7128033f65f6e6808bc82f
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-05-21 16:21:12 +0100

    style changes for disassembled code

--- src/Nethermind/Nethermind.Logging/StackTraceUtils.cs
+++ src/Nethermind/Nethermind.Logging/StackTraceUtils.cs
@@ -16,51 +16,19 @@ namespace Nethermind.Logging
     /// </summary>
     internal static class StackTraceUsageUtils
     {
-        private static readonly Assembly NlogAssembly = typeof(StackTraceUsageUtils).Assembly;
+        private static readonly Assembly NLogAssembly = typeof(StackTraceUsageUtils).Assembly;
         private static readonly Assembly MscorlibAssembly = typeof(string).Assembly;
         private static readonly Assembly SystemAssembly = typeof(Debug).Assembly;
 
-        internal static StackTraceUsage Max(StackTraceUsage u1, StackTraceUsage u2)
+        private static string GetStackFrameMethodClassName(MethodBase method, bool includeNameSpace, bool cleanAsyncMoveNext, bool cleanAnonymousDelegates)
         {
-            return (StackTraceUsage)Math.Max((int)u1, (int)u2);
-        }
-
-        public static int GetFrameCount(this StackTrace strackTrace)
-        {
-            return strackTrace.FrameCount;
-        }
-
-        public static string GetStackFrameMethodName(MethodBase method, bool includeMethodInfo, bool cleanAsyncMoveNext, bool cleanAnonymousDelegates)
-        {
-            if (method == (MethodBase)null)
-                return (string)null;
-            string str = method.Name;
-            Type declaringType = method.DeclaringType;
-            if (cleanAsyncMoveNext && str == "MoveNext" && (((object)declaringType != null ? declaringType.DeclaringType : (Type)null) != (Type)null && declaringType.Name.StartsWith("<")))
+            if (method == null)
             {
-                int num = declaringType.Name.IndexOf('>', 1);
-                if (num > 1)
-                    str = declaringType.Name.Substring(1, num - 1);
+                return null;
             }
 
-            if (cleanAnonymousDelegates && str.StartsWith("<") && (str.Contains("__") && str.Contains(">")))
-            {
-                int startIndex = str.IndexOf('<') + 1;
-                int num = str.IndexOf('>');
-                str = str.Substring(startIndex, num - startIndex);
-            }
-
-            if (includeMethodInfo && str == method.Name)
-                str = method.ToString();
-            return str;
-        }
-
-        public static string GetStackFrameMethodClassName(MethodBase method, bool includeNameSpace, bool cleanAsyncMoveNext, bool cleanAnonymousDelegates)
-        {
-            if (method == (MethodBase)null)
-                return (string)null;
             Type declaringType = method.DeclaringType;
-            if (cleanAsyncMoveNext && method.Name == "MoveNext" && (((object)declaringType != null ? declaringType.DeclaringType : (Type)null) != (Type)null && declaringType.Name.StartsWith("<") && declaringType.Name.IndexOf('>', 1) > 1))
+            if (cleanAsyncMoveNext && method.Name == "MoveNext" && ((object)declaringType != null ? declaringType.DeclaringType : null) != null && declaringType.Name.StartsWith("<") && declaringType.Name.IndexOf('>', 1) > 1)
                 declaringType = declaringType.DeclaringType;
             string str = includeNameSpace ? ((object)declaringType != null ? declaringType.FullName : (string)null) : ((object)declaringType != null ? declaringType.Name : (string)null);
             if (cleanAnonymousDelegates && str != null)
@@ -80,10 +48,7 @@ namespace Nethermind.Logging
         [MethodImpl(MethodImplOptions.NoInlining)]
         public static string GetClassFullName()
         {
-            int skipFrames = 3;
-            string empty = string.Empty;
-            int num = 0;
-            return StackTraceUsageUtils.GetClassFullName(new StackFrame(skipFrames, num != 0));
+            return GetClassFullName(new StackFrame(3, false));
         }
 
         /// <summary>
@@ -92,11 +57,11 @@ namespace Nethermind.Logging
         /// </summary>
         /// <param name="stackFrame">StackFrame from the calling method</param>
         /// <returns>Fully qualified class name</returns>
-        public static string GetClassFullName(StackFrame stackFrame)
+        private static string GetClassFullName(StackFrame stackFrame)
         {
-            string str = StackTraceUsageUtils.LookupClassNameFromStackFrame(stackFrame);
+            string str = LookupClassNameFromStackFrame(stackFrame);
             if (string.IsNullOrEmpty(str))
-                str = StackTraceUsageUtils.GetClassFullName(new StackTrace(false));
+                str = GetClassFullName(new StackTrace(false));
             return str;
         }
 
@@ -104,9 +69,11 @@ namespace Nethermind.Logging
         {
             foreach (StackFrame frame in stackTrace.GetFrames())
             {
-                string str = StackTraceUsageUtils.LookupClassNameFromStackFrame(frame);
+                string str = LookupClassNameFromStackFrame(frame);
                 if (!string.IsNullOrEmpty(str))
+                {
                     return str;
+                }
             }
 
             return string.Empty;
@@ -115,27 +82,36 @@ namespace Nethermind.Logging
         /// <summary>
         /// Returns the assembly from the provided StackFrame (If not internal assembly)
         /// </summary>
-        /// <returns>Valid asssembly, or null if assembly was internal</returns>
-        public static Assembly LookupAssemblyFromStackFrame(StackFrame stackFrame)
+        /// <returns>Valid assembly, or null if assembly was internal</returns>
+        private static Assembly LookupAssemblyFromStackFrame(StackFrame stackFrame)
         {
             MethodBase method = stackFrame.GetMethod();
-            if (method == (MethodBase)null)
-                return (Assembly)null;
+            if (method == null)
+                return null;
             Type declaringType = method.DeclaringType;
-            Assembly assembly1 = (object)declaringType != null ? declaringType.Assembly : (Assembly)null;
+            Assembly assembly1 = (object)declaringType != null ? declaringType.Assembly : null;
             if ((object)assembly1 == null)
             {
                 Module module = method.Module;
-                assembly1 = (object)module != null ? module.Assembly : (Assembly)null;
+                assembly1 = (object)module != null ? module.Assembly : null;
             }
 
             Assembly assembly2 = assembly1;
-            if (assembly2 == StackTraceUsageUtils.NlogAssembly)
-                return (Assembly)null;
-            if (assembly2 == StackTraceUsageUtils.MscorlibAssembly)
-                return (Assembly)null;
-            if (assembly2 == StackTraceUsageUtils.SystemAssembly)
-                return (Assembly)null;
+            if (assembly2 == NLogAssembly)
+            {
+                return null;
+            }
+
+            if (assembly2 == MscorlibAssembly)
+            {
+                return null;
+            }
+
+            if (assembly2 == SystemAssembly)
+            {
+                return null;
+            }
+
             return assembly2;
         }
 
@@ -144,12 +120,12 @@ namespace Nethermind.Logging
         /// </summary>
         /// <param name="stackFrame"></param>
         /// <returns>Valid class name, or empty string if assembly was internal</returns>
-        public static string LookupClassNameFromStackFrame(StackFrame stackFrame)
+        private static string LookupClassNameFromStackFrame(StackFrame stackFrame)
         {
             MethodBase method = stackFrame.GetMethod();
-            if (method != (MethodBase)null && StackTraceUsageUtils.LookupAssemblyFromStackFrame(stackFrame) != (Assembly)null)
+            if (method != null && LookupAssemblyFromStackFrame(stackFrame) != null)
             {
-                string str = StackTraceUsageUtils.GetStackFrameMethodClassName(method, true, true, true) ?? method.Name;
+                string str = GetStackFrameMethodClassName(method, true, true, true) ?? method.Name;
                 if (!string.IsNullOrEmpty(str) && !str.StartsWith("System.", StringComparison.Ordinal))
                     return str;
             }
