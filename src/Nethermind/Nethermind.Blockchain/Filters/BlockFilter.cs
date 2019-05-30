d052d3bca789b20435dfda35e52fad113ac81759
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-05-30 21:15:51 +0100

    WIP - minor renamings

--- src/Nethermind/Nethermind.Blockchain/Filters/BlockFilter.cs
+++ src/Nethermind/Nethermind.Blockchain/Filters/BlockFilter.cs
@@ -16,8 +16,6 @@
  * along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
  */
 
-using Nethermind.Dirichlet.Numerics;
-
 namespace Nethermind.Blockchain.Filters
 {
     public class BlockFilter : FilterBase
