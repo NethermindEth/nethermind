e77f0ec818fa319819a2cdb1eb13076df314c604
matilote <mateusz.jedrzejewski@yahoo.pl>
2019-06-13 17:34:26 +0200

    Adding and modifying scripts for package manager

--- scripts/packaging/archive-packages.sh
+++ scripts/packaging/archive-packages.sh
@@ -2,14 +2,34 @@ RELEASE_DIRECTORY=nethermind-packages
 LIN_RELEASE=nethermind-lin-x64
 OSX_RELEASE=nethermind-osx-x64
 WIN_RELEASE=nethermind-win-x64
+LIN=nethermind-linux-amd64
+OSX=nethermind-darwin-amd64
+WIN=nethermind-windows-amd64
 
 echo =======================================================
 echo Archiving Nethermind packages
 echo =======================================================
 
-cd $RELEASE_DIRECTORY/$LIN_RELEASE && zip -r $LIN_RELEASE.zip . && cd ../../
-cd $RELEASE_DIRECTORY/$OSX_RELEASE && zip -r $OSX_RELEASE.zip . && cd ../../
-cd $RELEASE_DIRECTORY/$WIN_RELEASE && zip -r $WIN_RELEASE.zip . && cd ../../
+echo =======================================================
+echo Generating git-hash files
+
+cd nethermind
+GIT="$(git rev-parse --short=7 HEAD)"
+GIT_TAG="$(git describe --tags)"
+GIT_SHORT_TAG="$(echo $GIT_TAG | cut -d- -f1)"
+cd ..
+cd $RELEASE_DIRECTORY
+touch git-hash.txt
+echo $GIT > git-hash.txt
+GIT_HASH="$(tail git-hash.txt)"
+
+echo =======================================================
+echo Success, git-hash files have been created
+echo =======================================================
+
+cd $LIN_RELEASE && zip -r $LIN-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
+cd $OSX_RELEASE && zip -r $OSX-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
+cd $WIN_RELEASE && zip -r $WIN-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
 
 echo =======================================================
 echo Archiving Nethermind packages completed
--- scripts/packaging/build-packages.sh
+++ scripts/packaging/build-packages.sh
@@ -2,7 +2,7 @@ echo =======================================================
 echo Building Nethermind packages
 echo =======================================================
 
-./setup-packages.sh && ./build-runner.sh && ./build-launcher.sh && ./archive-packages.sh
+./setup-packages.sh && ./build-runner.sh && ./build-cli.sh && ./build-launcher.sh && ./archive-packages.sh
 
 echo =======================================================
 echo Building Nethermind completed
