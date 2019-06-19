12e0e54f7060184ae0f45300bdf8743dcd1e696e
matilote <mateusz.jedrzejewski@yahoo.pl>
2019-06-19 20:02:48 +0200

    Git commit hash is now generated before building packages, date is displayed right after that hash

--- scripts/packaging/setup-packages.sh
+++ scripts/packaging/setup-packages.sh
@@ -7,10 +7,25 @@ echo =======================================================
 echo Setting up Nethermind packages
 echo =======================================================
 
+DATE=`date +%Y%m%d`
+
 rm -rf $RELEASE_DIRECTORY
 mkdir -p $RELEASE_DIRECTORY/$LIN_RELEASE
 mkdir -p $RELEASE_DIRECTORY/$OSX_RELEASE
 mkdir -p $RELEASE_DIRECTORY/$WIN_RELEASE
+cd nethermind
+GIT="$(git rev-parse --short=7 HEAD)"
+GIT_TAG="$(git describe --tags)"
+GIT_SHORT_TAG="$(echo $GIT_TAG | cut -d- -f1)"
+cd ..
+cd $RELEASE_DIRECTORY
+touch git-hash.txt
+touch git-tag.txt
+echo $GIT-$DATE > git-hash.txt
+echo $GIT_SHORT_TAG > git-tag.txt
+
+
+
 
 echo =======================================================
 echo Setting up Nethermind packages completed
