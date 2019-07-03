d72a55beb2ea91cd579b9b2db50875385d0b6fb7
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-07-04 00:36:55 +0100

    naive fix for transactions with empty sender

--- src/Nethermind/Nethermind.JsonRpc/Modules/Parity/ParityModule.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Parity/ParityModule.cs
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2018 Demerzel Solutions Limited
+ * This file is part of the Nethermind library.
+ *
+ * The Nethermind library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Nethermind library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using System.Linq;
 using Nethermind.Blockchain.TxPools;
 using Nethermind.Core.Crypto;
@@ -20,7 +38,7 @@ namespace Nethermind.JsonRpc.Modules.Parity
         }
 
         public ResultWrapper<ParityTransaction[]> parity_pendingTransactions()
-            => ResultWrapper<ParityTransaction[]>.Success(_txPool.GetPendingTransactions()
+            => ResultWrapper<ParityTransaction[]>.Success(_txPool.GetPendingTransactions().Where(pt => pt.SenderAddress != null)
                 .Select(t => new ParityTransaction(t, Rlp.Encode(t).Bytes,
                     t.IsSigned ? _ecdsa.RecoverPublicKey(t.Signature, t.Hash) : null)).ToArray());
     }
