3352ae14a9887d36bc6dc22588aed30793db5b47
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-07-07 19:42:35 +0100

    only dir if needed

--- benchmarking/benchmark.sh
+++ benchmarking/benchmark.sh
@@ -16,19 +16,19 @@ else
   echo "Exeuting on the current branch."
 fi
 
-if [ -n "$dbdir" ]; then
-  echo "DB dir is $dbdir"
-else
-  echo "Using default DB dir."
-  dbdir="D:\\chains\\perftest_ropsten"
-fi
-
 srcdir="../src/Nethermind/Nethermind.PerfTest"
 bindir="$srcdir/bin/Release/netcoreapp2.2" 
 echo "Source   $srcdir"
 echo "Binaries $bindir"
 pushd $srcdir
-sed -i -e 's|D\:\\chains\\perftest_ropsten|'$dbdir'|g' Program.cs
+
+if [ -n "$dbdir" ]; then
+  echo "DB dir is $dbdir"
+  sed -i -e 's|D\:\\chains\\perftest_ropsten|'$dbdir'|g' Program.cs
+else
+  echo "Using default DB dir."
+fi
+
 dotnet build -c Release
 git checkout -- Program.cs
 echo "Trying to checkout branch $startbranch"
