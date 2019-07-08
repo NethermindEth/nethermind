b9263535ccc37c0f9fb22d3a3e0ef0c153df1bd4
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-07-08 01:49:03 +0100

    fixed the commit variable in th repeat.sh script

--- benchmarking/repeat.sh
+++ benchmarking/repeat.sh
@@ -3,14 +3,13 @@ times=$1
 cp benchmark.sh perf.sh
 
 while read commit; do
-  echo "$commit"
   COUNTER=0
   while [  $COUNTER -lt $times ];
   do
-    echo "Running $branch.$COUNTER.bench"
+    echo "Running $commit.$COUNTER.bench"
     ./perf.sh -d "C:/perf_db" > result.bench
-    grep "TOTAL after 100000" result.bench > $branch.$COUNTER.bench
-    grep Mgas/s $branch.$COUNTER.bench
+    grep "TOTAL after 100000" result.bench > $commit.$COUNTER.bench
+    grep Mgas/s $commit.$COUNTER.bench
     let COUNTER=$COUNTER+1
   done
 done <commits.list
