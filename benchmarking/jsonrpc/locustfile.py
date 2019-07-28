e43fadae9659987804537e640d387eb1382ca392
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-07-28 11:54:57 +0200

    #695 first locust test

---
+++ benchmarking/jsonrpc/locustfile.py
@@ -0,0 +1,13 @@
+from locust import HttpLocust, TaskSet, task
+
+class UserBehavior(TaskSet):
+
+    @task(1)
+    def getBlockNumber(l):
+        l.client.post("/", json={"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1})
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 10
+    max_wait = 10
+    host = "http://127.0.0.1:8545"
