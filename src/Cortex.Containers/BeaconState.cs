ba79dc3cf929e41ec563b4a4d499a6b6bbd67133
Sly Gryphon <sgryphon@computer.org>
2019-09-27 09:26:48 +1000

    Containers library with very basic beacon state

---
+++ src/Cortex.Containers/BeaconState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cortex.Containers
+{
+    public class BeaconState
+    {
+        public UInt64 GenesisTime  { get; }
+
+        public BeaconState(UInt64 genesisTime)
+        {
+            GenesisTime = genesisTime;
+        }
+    }
+}
