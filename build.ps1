5a3cbcc14b1dad484a9917900cd0984db1f2a7d2
Sly Gryphon <sgryphon@computer.org>
2019-09-28 08:44:06 +1000

    Add tests to build script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,4 @@
+dotnet test test/Cortex.BeaconNode.Tests
+if (!$?) { throw 'Tests failed' }
 $v = (dotnet ./tools/gitversion/GitVersion.dll | ConvertFrom-Json)
 dotnet publish src/Cortex.BeaconNode.Host -c Release -p:InformationalVersion=$($v.SemVer)+$($v.ShortSha) -p:Version=$($v.SemVer)
