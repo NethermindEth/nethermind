a54ed16685bd5b324d52d059c06f2bcd2f800b21
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-11-01 19:04:51 +0000

    tru slightly modified state provider

--- src/Nethermind/Nethermind.Store/StateProvider.cs
+++ src/Nethermind/Nethermind.Store/StateProvider.cs
@@ -390,6 +390,11 @@ namespace Nethermind.Store
             for (int i = 0; i <= _currentPosition; i++)
             {
                 Change change = _changes[_currentPosition - i];
+                if (!isTracing && change.ChangeType == ChangeType.JustCache)
+                {
+                    continue;
+                }
+                
                 if (_committedThisRound.Contains(change.Address))
                 {
                     if (isTracing && change.ChangeType == ChangeType.JustCache)
