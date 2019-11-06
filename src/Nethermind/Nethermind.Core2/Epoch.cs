2b4368f2018530ac91942110cd4ea3e499bfbc1f
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-11-06 01:12:05 +0000

    containers for ETH2 WIP

---
+++ src/Nethermind/Nethermind.Core2/Epoch.cs
@@ -0,0 +1,73 @@
+//  Copyright (c) 2018 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+
+namespace Nethermind.Core2
+{
+    public struct Epoch
+    {
+        public bool Equals(Epoch other)
+        {
+            return Number == other.Number;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Epoch other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Number.GetHashCode();
+        }
+
+        public Epoch(ulong number)
+        {
+            Number = number;
+        }
+        
+        public ulong Number { get; }
+
+        public static bool operator ==(Epoch a, Epoch b)
+        {
+            return a.Number == b.Number;
+        }
+
+        public static bool operator !=(Epoch a, Epoch b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator <(Epoch a, Epoch b)
+        {
+            return a.Number < b.Number;
+        }
+
+        public static bool operator >(Epoch a, Epoch b)
+        {
+            return a.Number > b.Number;
+        }
+        
+        public static bool operator <=(Epoch a, Epoch b)
+        {
+            return a.Number <= b.Number;
+        }
+
+        public static bool operator >=(Epoch a, Epoch b)
+        {
+            return a.Number >= b.Number;
+        }
+    }
+}
