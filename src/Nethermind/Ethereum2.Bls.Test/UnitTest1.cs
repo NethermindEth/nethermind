f8d7d8ee43c64d726b362965f17c66205c46609a
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-11-20 18:06:53 +0000

    project placeholders

---
+++ src/Nethermind/Ethereum2.Bls.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Ethereum2.Bls.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
