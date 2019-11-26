f6c820439fe2644469a04bb18bd358213422c7c0
Sly Gryphon <sgryphon@computer.org>
2019-11-26 22:40:55 +1000

    Set gitversion to major.minor for assembly version and slightly change informational version format

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@
     if (!$?) { throw 'Tests failed' }
 }
 $v = (dotnet ./tools/gitversion/GitVersion.dll | ConvertFrom-Json)
-dotnet publish src/Cortex.BeaconNode.Host -c Release -p:InformationalVersion=$($v.SemVer)+$($v.ShortSha) -p:Version=$($v.SemVer)
+dotnet publish src/Cortex.BeaconNode.Host -c Release -p:AssemblyVersion=$($v.AssemblySemVer) -p:FileVersion=$($v.AssemblySemFileVer) -p:Version=$($v.SemVer)+$($v.CommitsSinceVersionSource).Sha.$($v.ShortSha) -p:PackageVersion=$($v.NuGetVersion)
