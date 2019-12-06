af9c353be3ef2a61c3a473d35515ae3b24f72348
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-12-06 22:52:55 +0000

    fixed hash32 experiments

--- src/Nethermind/Nethermind.Core2/Types/Hash32.cs
+++ src/Nethermind/Nethermind.Core2/Types/Hash32.cs
@@ -9,28 +9,6 @@ namespace Nethermind.Core2.Types
     {
         public const int Length = 32;
         
-//        private fixed byte Bytes[32];
-//
-//        public Hash32(ReadOnlySpan<byte> span)
-//        {
-//            if (span.Length != Length)
-//            {
-//                throw new ArgumentOutOfRangeException(nameof(span), span.Length, $"{nameof(Hash32)} must have exactly {Length} bytes");
-//            }
-//
-//            fixed (byte* ptr = Bytes)
-//            {
-//                var output = new Span<byte>(ptr, Length);
-//                span.CopyTo(output);
-//            }
-//        }
-//
-//        public Span<byte> AsSpan() => MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref this, 1));
-
-        /*********************/
-
-        private readonly byte[] _bytes;
-
         public Hash32(ReadOnlySpan<byte> span)
         {
             if (span.Length != Length)
@@ -38,15 +16,26 @@ namespace Nethermind.Core2.Types
                 throw new ArgumentOutOfRangeException(nameof(span), span.Length, $"{nameof(Hash32)} must have exactly {Length} bytes");
             }
 
-            _bytes = span.ToArray();
-        }
+            _bytes = new byte[32];
+            fixed (byte* ptr = _bytes)
+            {
+                var output = new Span<byte>(ptr, Length);
+                span.CopyTo(output);
+            }
+            
+            // _bytes = span.ToArray();
+        } 
 
+        //        private fixed byte Bytes[32];
+
+//        public Span<byte> AsSpan() => MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref this, 1));
+        
+        private readonly byte[] _bytes;
+        
         public Span<byte> AsSpan()
         {
             return new Span<byte>(_bytes);
         }
-
-        /*********************/
 
         public bool Equals(Hash32 other)
         {
@@ -55,26 +44,13 @@ namespace Nethermind.Core2.Types
 
         public static Hash32 Zero { get; } = new Hash32(new byte[32]);
 
-        public static explicit operator Hash32(byte[] bytes) => new Hash32(bytes);
-
-        public static explicit operator Hash32(Span<byte> span) => new Hash32(span);
-
-        public static explicit operator Hash32(ReadOnlySpan<byte> span) => new Hash32(span);
-
-        public static explicit operator ReadOnlySpan<byte>(Hash32 hash) => hash.AsSpan();
-
-        public static bool operator !=(Hash32? left, Hash32? right)
+        public static bool operator !=(Hash32 left, Hash32 right)
         {
             return !(left == right);
         }
 
-        public static bool operator ==(Hash32? left, Hash32? right)
+        public static bool operator ==(Hash32 left, Hash32 right)
         {
-            if (left is null)
-            {
-                return right is null;
-            }
-
             return left.Equals(right);
         }
 
@@ -86,12 +62,7 @@ namespace Nethermind.Core2.Types
 
         public override bool Equals(object obj)
         {
-            if (obj is null)
-            {
-                return false;
-            }
-
-            return Equals((Hash32) obj);
+            return !(obj is null) && Equals((Hash32) obj);
         }
 
         public override int GetHashCode()
@@ -109,41 +80,4 @@ namespace Nethermind.Core2.Types
             return new Hash32(other.AsSpan().Xor(AsSpan()));
         }
     }
-
-//     public unsafe struct Hash32 : IEquatable<Hash32>, IComparable<Hash32>
-//    {
-//        public const int Length = 32;
-//        
-//        private fixed byte Bytes[32];
-//
-//        public Hash32(ReadOnlySpan<byte> span)
-//        {
-//            if (span.Length != Length)
-//            {
-//                throw new ArgumentOutOfRangeException(nameof(span), span.Length, $"{nameof(Hash32)} must have exactly {Length} bytes");
-//            }
-//            
-//            fixed (byte* ptr = Bytes)
-//            {
-//                var output = new Span<byte>(ptr, Length);
-//                span.CopyTo(output);
-//            }
-//        }
-//        
-//        public Hash32(Span<byte> span)
-//        {
-//            if (span.Length != Length)
-//            {
-//                throw new ArgumentOutOfRangeException(nameof(span), span.Length, $"{nameof(Hash32)} must have exactly {Length} bytes");
-//            }
-//            
-//            fixed (byte* ptr = Bytes)
-//            {
-//                var output = new Span<byte>(ptr, Length);
-//                span.CopyTo(output);
-//            }
-//        }
-//        
-//        public Span<byte> AsSpan() => MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref this, 1));
-//    }
 }
