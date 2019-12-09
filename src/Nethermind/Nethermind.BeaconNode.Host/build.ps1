e31182bca791bb0d9b9b3b257a3767c70e35494e
Sly Gryphon <sgryphon@computer.org>
2019-12-10 08:36:06 +1000

    Removed stored gitversion and replace with msbuild task; much simpler, and even better works for all builds

--- src/Nethermind/Nethermind.BeaconNode.Host/build.ps1
+++ src/Nethermind/Nethermind.BeaconNode.Host/build.ps1
@@ -9,24 +9,25 @@ if (Test-Path (Join-Path $PSScriptRoot '/bin/Release')) {
     Remove-Item -Path (Join-Path $PSScriptRoot 'bin/Release') -Recurse
 }
 
-$gitversion = Join-Path $PSScriptRoot 'tools/gitversion/GitVersion.dll' 
-$v = (dotnet $gitversion | ConvertFrom-Json)
-dotnet publish (Join-Path $PSScriptRoot 'Nethermind.BeaconNode.Host.csproj') -c Release -p:AssemblyVersion=$($v.AssemblySemVer) -p:FileVersion=$($v.AssemblySemFileVer) -p:Version=$($v.SemVer)+$($v.ShortSha) -p:PackageVersion=$($v.NuGetVersion)
-
-$versionFolder = Join-Path $PSScriptRoot "release/$($v.NuGetVersion)"
-if (Test-Path $versionFolder) {
-    Remove-Item -Path $versionFolder -Recurse
-}
-New-Item -Path $versionFolder -ItemType Directory -Force
-Copy-Item -Path (Join-Path $PSScriptRoot 'bin/Release/netcoreapp3.0/publish/*') -Destination $versionFolder -Recurse
-
-$hostsettings = Get-Content -Raw -Path (Join-Path $versionFolder 'hostsettings.json') | ConvertFrom-Json
-$hostsettings.Environment = 'Production'
-$hostsettings | ConvertTo-Json | Set-Content -Path (Join-Path $versionFolder 'hostsettings.json')
+dotnet publish (Join-Path $PSScriptRoot 'Nethermind.BeaconNode.Host.csproj') -c Release
 
 $latestFolder = Join-Path $PSScriptRoot 'release/latest'
 if (Test-Path $latestFolder) {
     Remove-Item -Path $latestFolder -Recurse
 }
 New-Item -Path $latestFolder -ItemType Directory -Force
-Copy-Item -Path (Join-Path $versionFolder '*') -Destination $latestFolder -Recurse
+Copy-Item -Path (Join-Path $PSScriptRoot 'bin/Release/netcoreapp3.0/publish/*') -Destination $latestFolder -Recurse
+
+$hostsettings = Get-Content -Raw -Path (Join-Path $latestFolder 'hostsettings.json') | ConvertFrom-Json
+$hostsettings.Environment = 'Production'
+$hostsettings | ConvertTo-Json | Set-Content -Path (Join-Path $latestFolder 'hostsettings.json')
+
+#$gitversion = Join-Path $PSScriptRoot 'tools/gitversion/GitVersion.dll' 
+#$v = (dotnet $gitversion | ConvertFrom-Json)
+#$versionFolder = Join-Path $PSScriptRoot "release/$($v.NuGetVersion)"
+#if (Test-Path $versionFolder) {
+#    Remove-Item -Path $versionFolder -Recurse
+#}
+#New-Item -Path $versionFolder -ItemType Directory -Force
+#Copy-Item -Path (Join-Path $latestFolder '*') -Destination $versionFolder -Recurse
+
