82c750c3757e6c1e3db7bbc66e40e13fe40a43b0
Sly Gryphon <sgryphon@computer.org>
2019-12-18 12:42:09 +1000

    Merge clean up

--- src/Nethermind/Nethermind.BeaconNode/BlockProducer.cs
+++ src/Nethermind/Nethermind.BeaconNode/BlockProducer.cs
@@ -15,6 +15,8 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
