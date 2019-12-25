be2813ee7fe245c6259582c73c543813693c3733
Sly Gryphon <sgryphon@computer.org>
2019-12-25 20:03:57 +1000

    Add test for genesis time

--- src/Nethermind/Nethermind.HonestValidator.Test/BeaconNodeProxyTest.cs
+++ src/Nethermind/Nethermind.HonestValidator.Test/BeaconNodeProxyTest.cs
@@ -17,7 +17,7 @@ namespace Nethermind.HonestValidator.Test
     public class BeaconNodeProxyTest
     {
         [TestMethod]
-        public async Task BasicGetVersion()
+        public async Task BasicNodeVersion()
         {
             // Arrange
             IServiceCollection testServiceCollection = TestSystem.BuildTestServiceCollection();
@@ -38,5 +38,29 @@ namespace Nethermind.HonestValidator.Test
             // Assert
             version.ShouldBe("TESTVERSION");
         }
+        
+        [TestMethod]
+        public async Task BasicGensisTime()
+        {
+            // Arrange
+            IServiceCollection testServiceCollection = TestSystem.BuildTestServiceCollection();
+
+            IBeaconNodeOApiClient beaconNodeOApiClient = Substitute.For<IBeaconNodeOApiClient>();
+            beaconNodeOApiClient.TimeAsync(Arg.Any<CancellationToken>()).Returns(1_578_009_600uL);
+            IBeaconNodeOApiClientFactory beaconNodeOApiClientFactory = Substitute.For<IBeaconNodeOApiClientFactory>();
+            beaconNodeOApiClientFactory.CreateClient(Arg.Any<string>()).Returns(beaconNodeOApiClient);
+            testServiceCollection.AddSingleton<IBeaconNodeOApiClientFactory>(beaconNodeOApiClientFactory);
+            
+            ServiceProvider testServiceProvider = testServiceCollection.BuildServiceProvider();
+            
+            // Act
+            IBeaconNodeApi beaconNodeProxy = testServiceProvider.GetService<IBeaconNodeApi>();
+            beaconNodeProxy.ShouldBeOfType(typeof(BeaconNodeProxy));
+            ulong genesisTime = await beaconNodeProxy.GetGenesisTimeAsync(CancellationToken.None);
+            
+            // Assert
+            genesisTime.ShouldBe(1578009600uL);
+        }
+
     }
 }
