bcf76211e19e0bc14faeee164480c15988076b0e
matilote <mateusz.jedrzejewski@yahoo.pl>
2020-01-23 16:19:36 +0100

    alpine dockerfile changes

--- scripts/entrypoint2.sh
+++ scripts/entrypoint2.sh
@@ -8,7 +8,6 @@ exec gosu nethermind:nethermind /nethermind/Nethermind.Runner \
      --Init.BaseDbPath=/data/database \
 	 --configsDirectory=/data/configs \
 	 --KeyStore.KeyStoreDirectory=/data/keystore \
-	 --Init.ChainSpecDirectory=/data/chainspec \
 	 --Init.LogDirectory=/data/logs \
 	 --Init.StaticNodesPath=/data/static-nodes.json \
 	 "$@"
