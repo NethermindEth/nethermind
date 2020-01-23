181f873b10ef47919cd006db19119f91a229c687
matilote <mateusz.jedrzejewski@yahoo.pl>
2020-01-23 16:32:42 +0100

    changing entrypoint, replacing ubuntu image with alpine [skip ci]

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,4 +2,12 @@
 set -e
 
 chown nethermind:nethermind /data
-exec gosu nethermind:nethermind /nethermind/Nethermind.Runner --Init.BaseDbPath=/data "$@"
+cp -npr /nethermind/chainspec /nethermind/configs /nethermind/Data/static-nodes.json /data
+
+exec gosu nethermind:nethermind /nethermind/Nethermind.Runner \
+     --Init.BaseDbPath=/data/database \
+	 --configsDirectory=/data/configs \
+	 --KeyStore.KeyStoreDirectory=/data/keystore \
+	 --Init.LogDirectory=/data/logs \
+	 --Init.StaticNodesPath=/data/static-nodes.json \
+	 "$@"
