4af4d3555af520efa5442d811b3d3fe36ff98067
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-01-24 12:27:50 +0100

    simple new trie tests

--- src/Nethermind/Nethermind.Blockchain.Test/Proofs/ReceiptTrieTests.cs
+++ src/Nethermind/Nethermind.Blockchain.Test/Proofs/ReceiptTrieTests.cs
@@ -16,20 +16,28 @@
 
 using Nethermind.Blockchain.Proofs;
 using Nethermind.Core;
-using Nethermind.Core.Crypto;
 using Nethermind.Core.Test.Builders;
+using Nethermind.Specs;
 using NUnit.Framework;
 
 namespace Nethermind.Blockchain.Test.Proofs
 {
-    public class TxTrieTests
+    public class ReceiptTrieTests
     {
+        [Test]
+        public void Can_calculate_root_no_eip_658()
+        {
+            TxReceipt receipt = Build.A.Receipt.WithAllFieldsFilled.TestObject;
+            ReceiptTrie receiptTrie = new ReceiptTrie(1, MainNetSpecProvider.Instance, new [] {receipt});
+            Assert.AreEqual("0xe51a2d9f986d68628990c9d65e45c36128ec7bb697bd426b0bb4d18a3f3321be", receiptTrie.RootHash.ToString());
+        }
+        
         [Test]
         public void Can_calculate_root()
         {
-            Block block = Build.A.Block.WithTransactions(Build.A.Transaction.TestObject).TestObject;
-            TxTrie txTrie = new TxTrie(block);
-            Assert.AreEqual("0x29cc403075ed3d1d6af940d577125cc378ee5a26f7746cbaf87f1cf4a38258b5", txTrie.RootHash.ToString());
+            TxReceipt receipt = Build.A.Receipt.WithAllFieldsFilled.TestObject;
+            ReceiptTrie receiptTrie = new ReceiptTrie(MainNetSpecProvider.MuirGlacierBlockNumber, MainNetSpecProvider.Instance, new [] {receipt});
+            Assert.AreEqual("0x2e6d89c5b539e72409f2e587730643986c2ef33db5e817a4223aa1bb996476d5", receiptTrie.RootHash.ToString());
         }
     }
 }
---
+++ src/Nethermind/Nethermind.Blockchain.Test/Proofs/TxTrieTests.cs
@@ -0,0 +1,34 @@
+//  Copyright (c) 2018 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+
+using Nethermind.Blockchain.Proofs;
+using Nethermind.Core;
+using Nethermind.Core.Test.Builders;
+using NUnit.Framework;
+
+namespace Nethermind.Blockchain.Test.Proofs
+{
+    public class TxTrieTests
+    {
+        [Test]
+        public void Can_calculate_root()
+        {
+            Block block = Build.A.Block.WithTransactions(Build.A.Transaction.TestObject).TestObject;
+            TxTrie txTrie = new TxTrie(block);
+            Assert.AreEqual("0x29cc403075ed3d1d6af940d577125cc378ee5a26f7746cbaf87f1cf4a38258b5", txTrie.RootHash.ToString());
+        }
+    }
+}
