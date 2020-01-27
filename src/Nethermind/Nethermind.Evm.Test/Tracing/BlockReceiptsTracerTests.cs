44a7d919d6b89c6eeb90c2f81eb6a525b392a551
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-01-27 17:03:48 +0100

    test for receipt tracing issue

--- src/Nethermind/Nethermind.Evm.Test/Tracing/BlockReceiptsTracerTests.cs
+++ src/Nethermind/Nethermind.Evm.Test/Tracing/BlockReceiptsTracerTests.cs
@@ -15,8 +15,10 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
 using Nethermind.Core;
+using Nethermind.Core.Extensions;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Evm.Tracing;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Nethermind.Evm.Test.Tracing
@@ -50,6 +52,37 @@ namespace Nethermind.Evm.Test.Tracing
             tracer.MarkAsFailed(TestItem.AddressA, 100, new byte[0], "error", TestItem.KeccakF);
             
             Assert.AreEqual(TestItem.KeccakF, tracer.TxReceipts[0].PostTransactionState);
+        }
+
+        [Test]
+        public void Invokes_other_tracer_mark_as_failed_if_other_block_tracer_is_tx_tracer_too()
+        {
+            Block block = Build.A.Block.WithTransactions(Build.A.Transaction.TestObject).TestObject;
+            
+            IBlockTracer otherTracer = Substitute.For<IBlockTracer, ITxTracer>();
+            BlockReceiptsTracer tracer = new BlockReceiptsTracer();
+            tracer.SetOtherTracer(otherTracer);
+            tracer.StartNewBlockTrace(block);
+            tracer.StartNewTxTrace(block.Transactions[0].Hash);
+            tracer.MarkAsFailed(TestItem.AddressA, 100, Bytes.Empty, "error", TestItem.KeccakF);
+            
+            (otherTracer as ITxTracer).Received().MarkAsFailed(TestItem.AddressA, 100, Bytes.Empty, "error", TestItem.KeccakF);
+        }
+        
+        [Test]
+        public void Invokes_other_tracer_mark_as_success_if_other_block_tracer_is_tx_tracer_too()
+        {
+            Block block = Build.A.Block.WithTransactions(Build.A.Transaction.TestObject).TestObject;
+            
+            IBlockTracer otherTracer = Substitute.For<IBlockTracer, ITxTracer>();
+            BlockReceiptsTracer tracer = new BlockReceiptsTracer();
+            tracer.SetOtherTracer(otherTracer);
+            tracer.StartNewBlockTrace(block);
+            tracer.StartNewTxTrace(block.Transactions[0].Hash);
+            var logEntries = new LogEntry[0];
+            tracer.MarkAsSuccess(TestItem.AddressA, 100, Bytes.Empty, logEntries, TestItem.KeccakF);
+            
+            (otherTracer as ITxTracer).Received().MarkAsSuccess(TestItem.AddressA, 100, Bytes.Empty, logEntries, TestItem.KeccakF);
         }
     }
 }
