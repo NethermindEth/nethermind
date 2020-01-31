0b70b4ab307a6f2af5e57860bcf15999eb5f8801
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-01-31 01:16:37 +0100

    gs

--- src/Nethermind/Nethermind.Runner/Ethereum/Steps/EthereumStepsManager.cs
+++ src/Nethermind/Nethermind.Runner/Ethereum/Steps/EthereumStepsManager.cs
@@ -194,6 +194,11 @@ namespace Nethermind.Runner.Ethereum.Steps
                 Task task = step.Execute();
                 Task continuationTask = task.ContinueWith(t =>
                 {
+                    if (t.IsFaulted)
+                    {
+                        if (_logger.IsError) _logger.Error($"Step {step.GetType().Name.PadRight(24)} failed", t.Exception);
+                    }
+
                     _hasFinishedExecution[stepBaseType] = true;
                     stopwatch.Stop();
                     if (_logger.IsInfo) _logger.Info($"Step {step.GetType().Name.PadRight(24)} executed in {stopwatch.ElapsedMilliseconds}ms");
