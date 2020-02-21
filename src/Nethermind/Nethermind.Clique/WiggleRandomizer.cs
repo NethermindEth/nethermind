2b07205d82468f82a1c40bd285ca76f5ecd56633
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-02-21 23:04:27 +0100

    minor housekeeping

--- src/Nethermind/Nethermind.Clique/WiggleRandomizer.cs
+++ src/Nethermind/Nethermind.Clique/WiggleRandomizer.cs
@@ -14,9 +14,7 @@
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
-using System;
 using Nethermind.Core;
-using Nethermind.Core.Crypto;
 using Nethermind.Crypto;
 
 namespace Nethermind.Clique
