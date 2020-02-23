03e8ef81eb2f3882113870f3d54a397833275e7a
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-02-23 22:40:50 +0100

    this test needs a solid review once again

--- src/Nethermind/Nethermind.Blockchain.Test/Synchronization/SynchronizerTests.cs
+++ src/Nethermind/Nethermind.Blockchain.Test/Synchronization/SynchronizerTests.cs
@@ -678,7 +678,7 @@ namespace Nethermind.Blockchain.Test.Synchronization
                 .BestSuggestedBlockHasNumber(1).Stop();
         }
 
-        [Test, Retry(5)]
+        [Test, Retry(20)]
         public void Will_inform_connecting_peer_about_the_alternative_branch_with_same_difficulty()
         {
             if (_synchronizerType == SynchronizerType.Fast)
