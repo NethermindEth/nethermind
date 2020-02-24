2ea256b473ea2f06a5abd059805e77133e39f612
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-02-24 15:55:12 +0100

    updated json rpc tests to reflect moving of Id to the end

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/PersonalModuleTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/PersonalModuleTests.cs
@@ -46,7 +46,7 @@ namespace Nethermind.JsonRpc.Test.Modules
             IPersonalModule module = new PersonalModule(_bridge, NullLogManager.Instance);
             string serialized = RpcTest.TestSerializedRequest(module, "personal_listAccounts");
             string expectedAccounts = string.Join(',', _bridge.ListAccounts().Select(a => $"\"{a.ToString()}\""));
-            Assert.AreEqual($"{{\"id\":67,\"jsonrpc\":\"2.0\",\"result\":[{expectedAccounts}]}}", serialized);
+            Assert.AreEqual($"{{\"jsonrpc\":\"2.0\",\"result\":[{expectedAccounts}],\"id\":67}}", serialized);
         }
         
         [Test]
@@ -58,7 +58,7 @@ namespace Nethermind.JsonRpc.Test.Modules
             string serialized = RpcTest.TestSerializedRequest( module, "personal_newAccount", passphrase);
             var accountsNow = _bridge.ListAccounts();
             Assert.AreEqual(accountsBefore + 1, accountsNow.Length, "length");
-            Assert.AreEqual($"{{\"id\":67,\"jsonrpc\":\"2.0\",\"result\":\"{accountsNow.Last()}\"}}", serialized);
+            Assert.AreEqual($"{{\"jsonrpc\":\"2.0\",\"result\":\"{accountsNow.Last()}\",\"id\":67}}", serialized);
         }
         
         [Test]
@@ -67,7 +67,7 @@ namespace Nethermind.JsonRpc.Test.Modules
         {
             IPersonalModule module = new PersonalModule(_bridge, NullLogManager.Instance);
             string serialized = RpcTest.TestSerializedRequest(module, "personal_sign", "0xdeadbeaf", "0x9b2055d370f73ec7d8a03e965129118dc8f5bf83");
-            Assert.AreEqual($"{{\"id\":67,\"jsonrpc\":\"2.0\",\"result\":\"0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b\"}}", serialized);
+            Assert.AreEqual($"{{\"jsonrpc\":\"2.0\",\"result\":\"0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b\"}}", serialized);
         }
         
         [Test]
@@ -76,7 +76,7 @@ namespace Nethermind.JsonRpc.Test.Modules
         {
             IPersonalModule module = new PersonalModule(_bridge, NullLogManager.Instance);
             string serialized = RpcTest.TestSerializedRequest(module, "personal_ecRecover", "0xdeadbeaf", "0xa3f20717a250c2b0b729b7e5becbff67fdaef7e0699da4de7ca5895b02a170a12d887fd3b17bfdce3481f10bea41f45ba9f709d39ce8325427b57afcfc994cee1b");
-            Assert.AreEqual($"{{\"id\":67,\"jsonrpc\":\"2.0\",\"result\":\"0x9b2055d370f73ec7d8a03e965129118dc8f5bf83\"}}", serialized);
+            Assert.AreEqual($"{{\"jsonrpc\":\"2.0\",\"result\":\"0x9b2055d370f73ec7d8a03e965129118dc8f5bf83\"}}", serialized);
         }
     }
 }
