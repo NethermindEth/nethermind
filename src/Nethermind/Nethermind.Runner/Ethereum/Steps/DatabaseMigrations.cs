5a00c297d1d5c75f278817a45f90fe31112c44e3
LukaszRozmej <lukasz.rozmej@gmail.com>
2020-02-25 12:00:49 +0100

    1. Change bloom migration to include ChainLevelInfo.HasBlockOnMainChain for old blocks.
    2. Add warnings to logs when nbloom migration cannot find header.
    3. Add errors to logs when LogFinder cannot find block.
    4. Add NUnitLogManager to Initialize runner smoke tests for better diagnostics of failures in this test

--- src/Nethermind/Nethermind.Runner/Ethereum/Steps/DatabaseMigrations.cs
+++ src/Nethermind/Nethermind.Runner/Ethereum/Steps/DatabaseMigrations.cs
@@ -47,7 +47,7 @@ namespace Nethermind.Runner.Ethereum.Steps
         private Task? _bloomDbMigrationTask;
         private Average[]? _averages;
         private readonly StringBuilder _builder = new StringBuilder();
-        private IBloomConfig _bloomConfig;
+        private readonly IBloomConfig _bloomConfig;
 
         public DatabaseMigrations(EthereumRunnerContext context)
         {
@@ -152,6 +152,12 @@ namespace Nethermind.Runner.Ethereum.Steps
 
         private void RunBloomMigration(CancellationToken token)
         {
+            BlockHeader GetMissingBLockHeader(long i)
+            {
+                if (_logger.IsWarn) _logger.Warn(GetLogMessage("warning", $"Header for block {i} not found. Database might be corrupted and BloomDb will not be correct."));
+                return new BlockHeader(Keccak.Zero, Keccak.Zero, Address.Zero, UInt256.Zero, 0L, 0L, UInt256.Zero, Bytes.Empty);
+            }
+
             if (_context.BloomStorage == null) throw new StepDependencyException(nameof(_context.BloomStorage));
             if (_context.BlockTree == null) throw new StepDependencyException(nameof(_context.BlockTree));
             
@@ -162,6 +168,7 @@ namespace Nethermind.Runner.Ethereum.Steps
             long from = synced;
             _migrateCount = to + 1;
             _averages = _context.BloomStorage.Averages.ToArray();
+            var chainLevelInfoRepository = _context.ChainLevelInfoRepository;
 
             _progress.Update(synced);
 
@@ -195,11 +202,27 @@ namespace Nethermind.Runner.Ethereum.Steps
                             yield break;
                         }
 
-                        var header = blockTree.FindHeader(i);
-                        yield return header ?? new BlockHeader(Keccak.Zero, Keccak.Zero, Address.Zero, UInt256.Zero, 0L, 0L, UInt256.Zero, Bytes.Empty);
+                        var level = chainLevelInfoRepository.LoadLevel(i);
+                        if (level == null)
+                        {
+                            yield return GetMissingBLockHeader(i);
+                        }
+                        else
+                        {
+                            if (!level.HasBlockOnMainChain)
+                            {
+                                if (level.BlockInfos.Length > 0)
+                                {
+                                    level.HasBlockOnMainChain = true;
+                                    chainLevelInfoRepository.PersistLevel(i, level);
+                                }
+                            }
+                        
+                            var header = blockTree.FindHeader(level.MainChainBlock.BlockHash, BlockTreeLookupOptions.None);
+                            yield return header ?? GetMissingBLockHeader(i);
+                        }
 
-                        synced++;
-                        _progress.Update(synced);
+                        _progress.Update(++synced);
                     }
                 }
             }
