d9177663587e87116d609912ae27490cf56bbf45
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-02-29 13:27:52 +0000

    test for creation method

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/Trace/ParityTxTraceFromReplayConverterTest.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/Trace/ParityTxTraceFromReplayConverterTest.cs
@@ -101,6 +101,55 @@ namespace Nethermind.JsonRpc.Test.Modules.Trace
 
             TestToJson(new ParityTxTraceFromReplay(trace), "{\"output\":null,\"stateDiff\":{\"0xb7705ae4c6f81b66cdb323c65f4e8133690fc099\":{\"balance\":{\"*\":{\"from\":\"0x0\",\"to\":\"0x29a2241af62c0000\"}},\"code\":\"=\",\"nonce\":\"=\",\"storage\":{}}},\"trace\":[{\"action\":{\"author\":\"0xb7705ae4c6f81b66cdb323c65f4e8133690fc099\",\"rewardType\":\"block\",\"value\":\"0x1\"},\"result\":null,\"subtraces\":0,\"traceAddress\":[],\"type\":\"reward\"}],\"vmTrace\":null}");
         }
+        
+        [Test]
+        public void Can_serialize_creation_method()
+        {
+            string expectedResult = "{\"output\":null,\"stateDiff\":{\"0x76e68a8696537e4141926f3e528733af9e237d69\":{\"balance\":{\"*\":{\"from\":\"0x1\",\"to\":\"0x2\"}},\"code\":\"=\",\"nonce\":{\"*\":{\"from\":\"0x0\",\"to\":\"0x1\"}},\"storage\":{\"0x0000000000000000000000000000000000000000000000000000000000000001\":{\"*\":{\"from\":\"0x0000000000000000000000000000000000000000000000000000000000000001\",\"to\":\"0x0000000000000000000000000000000000000000000000000000000000000002\"}}}}},\"trace\":[{\"action\":{\"creationMethod\":\"create2\",\"from\":\"0xb7705ae4c6f81b66cdb323c65f4e8133690fc099\",\"gas\":\"0x9c40\",\"init\":\"0x010203040506\",\"value\":\"0x3039\"},\"result\":{\"gasUsed\":\"0x0\",\"output\":null},\"subtraces\":1,\"traceAddress\":[1,2,3],\"type\":\"create\"},{\"action\":{\"callType\":\"call\",\"from\":\"0x76e68a8696537e4141926f3e528733af9e237d69\",\"gas\":\"0x2710\",\"input\":\"0x\",\"to\":\"0x475674cb523a0a2736b7f7534390288fce16982c\",\"value\":\"0x10932\"},\"result\":{\"gasUsed\":\"0x0\",\"output\":null},\"subtraces\":0,\"traceAddress\":[0,0],\"type\":null}],\"vmTrace\":null}";
+            
+            ParityTraceAction subtrace = new ParityTraceAction
+            {
+                Value = 67890,
+                CallType = "call",
+                From = TestItem.AddressC,
+                To = TestItem.AddressD,
+                Input = Bytes.Empty,
+                Gas = 10000,
+                TraceAddress = new int[] {0, 0}
+            };
+
+            ParityLikeTxTrace result = new ParityLikeTxTrace
+            {
+                Action = new ParityTraceAction
+                {
+                    Value = 12345,
+                    Type = "create",
+                    CallType = "create",
+                    CreationMethod = "create2",
+                    From = TestItem.AddressA,
+                    To = null,
+                    Input = new byte[] {1, 2, 3, 4, 5, 6},
+                    Gas = 40000,
+                    TraceAddress = new int[] {0}
+                },
+                BlockHash = TestItem.KeccakB,
+                BlockNumber = 123456,
+                TransactionHash = TestItem.KeccakC,
+                TransactionPosition = 5
+            };
+            result.Action.TraceAddress = new int[] {1, 2, 3};
+            result.Action.Subtraces.Add(subtrace);
+
+            ParityAccountStateChange stateChange = new ParityAccountStateChange
+            {
+                Balance = new ParityStateChange<UInt256?>(1, 2),
+                Nonce = new ParityStateChange<UInt256?>(0, 1),
+                Storage = new Dictionary<UInt256, ParityStateChange<byte[]>> {[1] = new ParityStateChange<byte[]>(new byte[] {1}, new byte[] {2})}
+            };
+
+            result.StateChanges = new Dictionary<Address, ParityAccountStateChange> {{TestItem.AddressC, stateChange}};
+            TestToJson(new ParityTxTraceFromReplay(result, false), expectedResult);
+        }
 
         [Test, Ignore("Reenable it after running compare on PoW chains")]
         public void Can_serialize_reward_state_only()
