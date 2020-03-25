35ff18db362086de75e5287a2acc881cafa551c6
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-03-25 00:44:05 +0000

    valid mem calcs

--- src/Nethermind/Nethermind.Runner/Ethereum/Steps/InitializeNetwork.cs
+++ src/Nethermind/Nethermind.Runner/Ethereum/Steps/InitializeNetwork.cs
@@ -86,7 +86,7 @@ namespace Nethermind.Runner.Ethereum.Steps
             }
 
             // Environment.SetEnvironmentVariable("io.netty.allocator.pageSize", "8192");
-            ThisNodeInfo.AddInfo("Mem est netty:", $"{(2 << _networkConfig.NettyArenaOrder) * 8192 / 1024 / 1024}MB".PadLeft(8));
+            ThisNodeInfo.AddInfo("Mem est netty:", $"{2 * Environment.ProcessorCount * (1 << _networkConfig.NettyArenaOrder) * 8192 / 1000 / 1000}MB".PadLeft(8));
             ThisNodeInfo.AddInfo("Mem est peers:", $"{_networkConfig.ActivePeersMaxCount}MB".PadLeft(8));
             Environment.SetEnvironmentVariable("io.netty.allocator.maxOrder", _networkConfig.NettyArenaOrder.ToString());
 
