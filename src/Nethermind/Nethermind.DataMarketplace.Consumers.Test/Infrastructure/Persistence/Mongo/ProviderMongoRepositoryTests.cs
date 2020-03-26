7e0576cd575be2e933946ae3bdd9724cf44a846e
Tomasz Kajetan Stańczak <tkstanczak@users.noreply.github.com>
2020-03-26 21:36:58 +0000

    mongo repo tests for consumers (#1591)

---
+++ src/Nethermind/Nethermind.DataMarketplace.Consumers.Test/Infrastructure/Persistence/Mongo/ProviderMongoRepositoryTests.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Nethermind.DataMarketplace.Consumers.Infrastructure.Persistence.Mongo.Repositories;
+using Nethermind.DataMarketplace.Providers.Infrastructure.Tests.Persistence.Mongo;
+using NUnit.Framework;
+
+namespace Nethermind.DataMarketplace.Consumers.Test.Infrastructure.Persistence.Mongo
+{
+    [TestFixture]
+    public class ProviderMongoRepositoryTests
+    {
+        [TearDown]
+        public void TearDown()
+        {
+            MongoForTest.TempDb.GetDatabase().DropCollection("deposits");
+        }
+
+        [Test]
+        public async Task Can_get_data_assets_from_deposits()
+        {
+            IMongoDatabase database = MongoForTest.TempDb.GetDatabase();
+            var repo = new ProviderMongoRepository(database);
+            await repo.GetDataAssetsAsync();
+        }
+       
+        [Test]
+        public async Task Can_get_providers_from_deposits()
+        {
+            IMongoDatabase database = MongoForTest.TempDb.GetDatabase();
+            var repo = new ProviderMongoRepository(database);
+            await repo.GetProvidersAsync();
+        }
+    }
+}
