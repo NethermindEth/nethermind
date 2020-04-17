6809fb75d782d762cd0b2a272b08ef9569974e1d
LukaszRozmej <lukasz.rozmej@gmail.com>
2020-04-17 17:13:05 +0200

    FIx tests

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/TestBlockchain.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/TestBlockchain.cs
@@ -77,7 +77,7 @@ namespace Nethermind.JsonRpc.Test.Modules
 
         protected virtual async Task<TestBlockchain> Build()
         {
-            Timestamper = new ManualTimestamper();
+            Timestamper = new ManualTimestamper(new DateTime(2020, 2, 15, 12, 50, 30));
             JsonSerializer = new EthereumJsonSerializer();
             ISpecProvider specProvider = MainnetSpecProvider.Instance;
             EthereumEcdsa = new EthereumEcdsa(specProvider, LimboLogs.Instance);
