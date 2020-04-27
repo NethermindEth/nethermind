702998d5e5377c3656096ea52146ca28dd92187b
matilote <mateusz.jedrzejewski@yahoo.pl>
2020-04-27 16:11:07 +0200

    refactoring

--- scripts/syncSettings.py
+++ scripts/syncSettings.py
@@ -14,6 +14,44 @@ headers = {
 
 print(emoji.emojize("Fast Sync configuration settings initialization     :white_check_mark: ", use_aliases=True))
 
+configs = {
+    "mainnet": {
+        "url": "api.etherscan.io",
+        "blockReduced": 8192,
+        "multiplierRequirement": 10000 
+    },
+    "goerli": {
+        "url": "api-goerli.etherscan.io",
+        "blockReduced": 8192,
+        "multiplierRequirement": 30000 
+    },
+    "ropsten": {
+        "url": "api-ropsten.etherscan.io",
+        "blockReduced": 8192,
+        "multiplierRequirement": 10000
+    },
+    "rinkeby": {
+        "url": "api-rinkeby.etherscan.io",
+        "blockReduced": 8192,
+        "multiplierRequirement": 30000
+    },
+    "poacore": {
+        "url": "https://core.poa.network",
+        "blockReduced": 8192,
+        "multiplierRequirement": 30000
+    },
+    "xdai": {
+        "url": "https://dai.poa.network",
+        "blockReduced": 8192,
+        "multiplierRequirement": 30000
+    },
+    "sokol": {
+        "url": "https://sokol.poa.network",
+        "blockReduced": 8192,
+        "multiplierRequirement": 30000
+    },
+}
+
 def fastBlocksSettings(configuration, apiUrl, blockReduced, multiplierRequirement):
     if "etherscan" in apiUrl:
         latestBlock = int(json.loads(subprocess.getoutput(f'curl --silent "https://{apiUrl}/api?module=proxy&action=eth_blockNumber&apikey={key}"'))['result'],16)
@@ -47,54 +85,6 @@ def fastBlocksSettings(configuration, apiUrl, blockReduced, multiplierRequiremen
         with open(f'{configsPath}/{configuration}.cfg', 'w') as mainnetCfgChanged:
             json.dump(data, mainnetCfgChanged, indent=2)
 
-
-# Mainnet 
-print(emoji.emojize("Mainnet section                                     :white_check_mark: ", use_aliases=True))
-mainnetApiUrl = 'api.etherscan.io'
-mainnetBlockReduced = 8192
-
-fastBlocksSettings('mainnet', mainnetApiUrl, mainnetBlockReduced, 10000)
-
-# Goerli
-print(emoji.emojize("Goerli section                                      :white_check_mark: ", use_aliases=True))
-goerliApiUrl = 'api-goerli.etherscan.io'
-goerliBlockReduced = 8192
-
-fastBlocksSettings('goerli', goerliApiUrl, goerliBlockReduced, 30000)
-
-# Ropsten
-print(emoji.emojize("Ropsten section                                     :white_check_mark: ", use_aliases=True))
-ropstenApiUrl = 'api-ropsten.etherscan.io'
-ropstenBlockReduced = 8192
-
-fastBlocksSettings('ropsten', ropstenApiUrl, ropstenBlockReduced, 10000)
-
-# Rinkeby
-print(emoji.emojize("Rinkeby section                                     :white_check_mark: ", use_aliases=True))
-rinkebyApiUrl = 'api-rinkeby.etherscan.io'
-rinkebyBlockReduced = 8192
-
-fastBlocksSettings('rinkeby', rinkebyApiUrl, rinkebyBlockReduced, 30000)
-
-# POA Core
-print(emoji.emojize("POA Core section                                     :white_check_mark: ", use_aliases=True))
-poacoreApiUrl = 'https://core.poa.network'
-poacoreBlockReduced = 8192
-
-fastBlocksSettings('poacore', poacoreApiUrl, poacoreBlockReduced, 30000)
-
-# xDai
-print(emoji.emojize("xDai section                                     :white_check_mark: ", use_aliases=True))
-xdaiApiUrl = 'https://dai.poa.network'
-xdaiBlockReduced = 8192
-
-fastBlocksSettings('xdai', xdaiApiUrl, xdaiBlockReduced, 30000)
-
-# Sokol
-print(emoji.emojize("Sokol section                                     :white_check_mark: ", use_aliases=True))
-sokolApiUrl = 'https://sokol.poa.network'
-sokolBlockReduced = 8192
-
-fastBlocksSettings('sokol', sokolApiUrl, sokolBlockReduced, 30000)
-
-print(emoji.emojize("Fast Sync configuration settings finished           :ok_hand: ", use_aliases=True))
+for config, value in configs.items():
+    print(emoji.emojize(f"{config.capitalize()} section                                     :white_check_mark: ", use_aliases=True))
+    fastBlocksSettings(config, value['url'], value['blockReduced'], value['multiplierRequirement'])
