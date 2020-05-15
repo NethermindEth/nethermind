56a005163764be86df729a3b8124133b84c33474
matilote <mateusz.jedrzejewski@yahoo.pl>
2020-05-15 17:09:22 +0200

    adding baseline CLI module template

---
+++ src/Nethermind/Nethermind.Cli/Modules/BaselineCliModule.cs
@@ -0,0 +1,22 @@
+namespace Nethermind.Cli.Modules
+{
+    [CliModule("baseline")]
+    public class BaselineCliModule : CliModuleBase
+    {
+        public BaselineCliModule(ICliEngine cliEngine, INodeManager nodeManager) : base(cliEngine, nodeManager)
+        {
+        }
+
+        [CliProperty("baseline", "deploy")]
+        public string Deploy(string address) => NodeManager.Post<string>("baseline_deploy", CliParseAddress(address)).Result;
+
+        [CliFunction("baseline", "addLeaf")]
+        public string addLeaf() => NodeManager.Post<string>("baseline_addLeaf").Result;
+        
+        [CliFunction("baseline", "addLeaves")]
+        public string addLeaves() => NodeManager.Post<string>("baseline_addLeaves").Result;
+        
+        [CliFunction("baseline", "getSiblings")]
+        public string getSiblings() => NodeManager.Post("baseline_getSiblings").Result;
+    }
+}
