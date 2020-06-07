e1efc43df5f7cfdba805931feb016d3f8d51cf1d
Tomasz Kajetan Stańczak <tkstanczak@users.noreply.github.com>
2020-06-07 14:42:05 +0100

    Precompiles benchmarking (#2003)
    * beamFix
    
    * working beamfix
    
    * simplified
    
    * remove deletes
    
    * use timeout configs
    
    * sha256 benchmark
    
    * removed cfg change
    
    * cleanup of benchmark list
    
    * precompile benchmark base
    
    * add exporters
    
    * fixed bnmul

---
+++ src/Nethermind/Nethermind.Precompiles.Benchmark/Blake2fBenchmark.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Nethermind.Evm.Precompiles;
+
+namespace Nethermind.Precompiles.Benchmark
+{
+    [HtmlExporter]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    public class Blake2fBenchmark : PrecompileBenchmarkBase
+    {
+        protected override IPrecompiledContract Precompile => Blake2BPrecompiledContract.Instance;
+        protected override string InputsDirectory => "blake2f";
+    }
+}
