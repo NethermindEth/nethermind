9059ac44b662137092f677b1be0feafee2bd972f
Kasia Turbiarz <kasiazerosiedem07@gmail.com>
2020-06-08 13:28:20 +0200

    Path input.

--- src/Nethermind/Nethermind.RocksDbExtractor/Program.cs
+++ src/Nethermind/Nethermind.RocksDbExtractor/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Nethermind.Core;
 using Nethermind.Core.Extensions;
 using Nethermind.DataMarketplace.Consumers.Infrastructure.Persistence.Rocks.Databases;
 using Nethermind.DataMarketplace.Consumers.Infrastructure.Rlp;
@@ -16,10 +18,10 @@ namespace Nethermind.RocksDbExtractor
     {
         static void Main(string[] args)
         {
-           Application.Init();
+            Application.Init();
             
-            var window = new Window ("NDM RocksDb Extractor");
-            Application.Top.Add(window);
+            var window1 = new Window ("NDM RocksDb Extractor");
+            Application.Top.Add(window1);
             
             var quitBtn = new Button(3, 1, "Quit");
             quitBtn.Clicked = () =>
@@ -29,98 +31,93 @@ namespace Nethermind.RocksDbExtractor
             };
 
             var pathLbl = new Label(3, 3, "Enter DB path: ");
-            var path = new TextField(20, 3, 70, "");
-            var applyBtn = new Button(90, 3, "OK");
+            var pathTxtField = new TextField(20, 3, 70, "");
+            var okBtn = new Button(90, 3, "OK");
             
-            window.Add (pathLbl, path, quitBtn, applyBtn);
+            window1.Add(quitBtn, pathLbl, pathTxtField, okBtn);
 
-            applyBtn.Clicked = () =>
+            okBtn.Clicked = () =>
             {
-                var isDouble = double.TryParse(path.Text.ToString(), out _);
-                if(isDouble)
+                var pathString = pathTxtField.Text.ToString();
+                if(double.TryParse(pathString, out _))
                 {
                     MessageBox.ErrorQuery(30, 6, "Error", "Path can not be a number.");
-                    path.Text = string.Empty;
+                    pathTxtField.Text = string.Empty;
                     
-                } else if(string.IsNullOrEmpty(path.Text.ToString()))
+                } else if(string.IsNullOrEmpty(pathString))
                 {
                     MessageBox.ErrorQuery(30, 6, "Error", "Path can not be empty.");
                 }
                 else
                 {
-                    // var consumerPath = @"\ndm_consumer\local";
-                    // var providerPath = @"\ndm_provider\local";
-                    // var mainPath = path.Text.ToString() + consumerPath;
-
-                    // dodac sprawdzenie czy jest slash na koncu czy nie
-                    
-                    
-                    var window2 = new Window("Clients");
+                    var window2 = new Window("nethermind_db");
                     Application.Top.Add(window2);
                     
                     var consumerBtn = new Button(1, 1, "Consumer");
                     consumerBtn.Clicked = () =>
                     {
-                        var window3 = new Window("Folder");
+                        var window3 = new Window("ndm_consumer");
                         Application.Top.Add(window3);
-                        var path = @"D:\Workspace\Demerzel\ndm\src\nethermind\src\Nethermind\Nethermind.Runner\bin\Debug\netcoreapp3.1\nethermind_db\ndm_consumer\local";
-                        string[] folders = System.IO.Directory.GetDirectories(path,"*");
-                        var nameFolders1 = new List<string>();
+                        
+                        var consumerPath = Path.Combine(pathString, "ndm_consumer");
+                        var networks = System.IO.Directory.GetDirectories(consumerPath,"*");
                         var i = 1;
-                        
-                        foreach (var folder in folders)
+                        foreach (var n in networks)
                         {
+                            var network = n.Split(Path.DirectorySeparatorChar).Last();
+                            var networkBtn = new Button(1, i++, $"{network}");
                             
-                            // nameFolders1.Add(folder.Split('\\').Last());
-                            var name = folder.Split('\\').Last();
-                            var nameBtn = new Button(1, i++, $"{name}");
-                            
-                            
-                            
-                            
-                            
-                            
-                            nameBtn.Clicked = () =>
+                            networkBtn.Clicked = () =>
                             {
-                                var window4 = new Window("Folder1");
+                                var window4 = new Window($"{network}");
                                 Application.Top.Add(window4);
-
-                            ILogManager logger = new OneLoggerLogManager(new ConsoleAsyncLogger(LogLevel.Trace));
-                            DbOnTheRocks dbOnTheRocks = new BlocksRocksDb(path, new DbConfig(), logger);
-                            var blocksBytes = dbOnTheRocks.GetAll();
-                            
-                            var blockDecoder = new BlockDecoder();
-                            var y = 1;
-                            foreach (var blockBytes in blocksBytes)
-                            {
-                                var block = blockDecoder.Decode(AsRlpStream(blockBytes.Value));
-                                var blockBtn = new Button(1, y++, $"{block}");
-                                window4.Add(blockBtn);
-                            }
-                            
-                            Application.Run(window4);
-
-                            
-                            
-                            
                                 
+                                var consumerNetworkPath = Path.Combine(pathString, "ndm_consumer", $"{network}");
+                                var dataFolders = System.IO.Directory.GetDirectories(consumerNetworkPath,"*");
+                                
+                                var i = 1;
+                                foreach (var d in dataFolders)
+                                {
+                                    var dataFolder = d.Split(Path.DirectorySeparatorChar).Last();
+                                    var dataFolderBtn = new Button(1, i++, $"{dataFolder}");
+                                    window4.Add(dataFolderBtn);
+                                    dataFolderBtn.Clicked = () =>
+                                    {
+                                        var window5 = new Window($"{dataFolder}");
+                                        Application.Top.Add(window5);
+                                        
+                                        ILogManager logger = new OneLoggerLogManager(new ConsoleAsyncLogger(LogLevel.Trace));
+                                        DbOnTheRocks dbOnTheRocks = new BlocksRocksDb(consumerNetworkPath, new DbConfig(), logger);
+                                        var blocksBytes = dbOnTheRocks.GetAll();
+                        
+                                        var blockDecoder = new BlockDecoder();
+                                        var blocks = blocksBytes
+                                            .Select(b => blockDecoder.Decode(AsRlpStream(b.Value)))
+                                            .OrderBy(b => b.Number);
+                                
+                                        var y = 1;    
+                                        foreach (var block in blocks)
+                                        {
+                                            var blockBtn = new Button(1, y++, $"{block.Number} {block.Hash}");
+                                            window5.Add(blockBtn);
+                                        }
+                                        Application.Run(window5);
+                                    };
+                                }
+                                Application.Run(window4);
                             };
-                            window3.Add(nameBtn);
-                            
+                            window3.Add(networkBtn);
                         }
-
-                        
-                            
-                        
                         Application.Run(window3);
                     };
                     var providerBtn = new Button(1, 2, "Provider");
+                    providerBtn.Clicked = () =>
+                    {
+                        var providerPath = Path.Combine(pathString, "ndm_provider");
+                        MessageBox.Query(30, 6, "Message", "Provider will be ready soon!");
+                    };
                     window2.Add(consumerBtn, providerBtn);
                     Application.Run(window2);
-
-
-
-
                 }
             };
             Application.Run();
