5f25c40ec02a324bf0c980f485c5b02006802d2b
AndreaLanfranchi <andrea.lanfranchi@gmail.com>
2020-07-01 16:33:59 +0200

    Use discard on unused exception

--- src/Nethermind/Nethermind.Network/StaticNodes/StaticNodesManager.cs
+++ src/Nethermind/Nethermind.Network/StaticNodes/StaticNodesManager.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2018 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -61,7 +61,7 @@ namespace Nethermind.Network.StaticNodes
             {
                 if (_logger.IsDebug) _logger.Debug($"Static nodes: {Environment.NewLine}{data}");
             }
-            
+
             _nodes = new ConcurrentDictionary<PublicKey, NetworkNode>(nodes.Select(n => new NetworkNode(n))
                 .ToDictionary(n => n.NodeId, n => n));
         }
@@ -73,7 +73,7 @@ namespace Nethermind.Network.StaticNodes
             {
                 nodes = JsonConvert.DeserializeObject<string[]>(data) ?? Array.Empty<string>();
             }
-            catch (JsonException e)
+            catch (JsonException _)
             {
                 nodes = data.Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
             }
