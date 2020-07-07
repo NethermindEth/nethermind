5125be8b2528838cd6b6b51b41dd4afe3e8be7a4
AndreaLanfranchi <andrea.lanfranchi@gmail.com>
2020-07-07 19:23:27 +0200

    Remove whitespaces and simplify last return for TimeAdjustment

--- src/Nethermind/Nethermind.Consensus.Ethash/DifficultyCalculator.cs
+++ src/Nethermind/Nethermind.Consensus.Ethash/DifficultyCalculator.cs
@@ -1,16 +1,16 @@
-﻿//  Copyright (c) 2018 Demerzel Solutions Limited
+//  Copyright (c) 2018 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -30,7 +30,7 @@ namespace Nethermind.Consensus.Ethash
         }
 
         private const long OfGenesisBlock = 131_072;
-        
+
         public UInt256 Calculate(
             UInt256 parentDifficulty,
             UInt256 parentTimestamp,
@@ -59,18 +59,13 @@ namespace Nethermind.Consensus.Ethash
             {
                 return BigInteger.Max((parentHasUncles ? 2 : BigInteger.One) - BigInteger.Divide(currentTimestamp - parentTimestamp, 9), -99);
             }
-            
-            if(spec.IsEip2Enabled)
+
+            if (spec.IsEip2Enabled)
             {
-                return BigInteger.Max(BigInteger.One - BigInteger.Divide(currentTimestamp - parentTimestamp, 10), -99);    
+                return BigInteger.Max(BigInteger.One - BigInteger.Divide(currentTimestamp - parentTimestamp, 10), -99);
             }
-            
-            if (spec.IsTimeAdjustmentPostOlympic)
-            {
-                return currentTimestamp < parentTimestamp + 13 ? BigInteger.One : BigInteger.MinusOne;                
-            }
-            
-            return currentTimestamp < parentTimestamp + 7 ? BigInteger.One : BigInteger.MinusOne;
+
+            return currentTimestamp < parentTimestamp + (spec.IsTimeAdjustmentPostOlympic ? 13 : 7) ? BigInteger.One : BigInteger.MinusOne;
         }
 
         private BigInteger TimeBomb(IReleaseSpec spec, long blockNumber)
