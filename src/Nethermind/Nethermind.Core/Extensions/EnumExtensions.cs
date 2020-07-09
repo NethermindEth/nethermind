19d41f59548824066897b87da6453f28d265fe90
AndreaLanfranchi <andrea.lanfranchi@gmail.com>
2020-07-09 19:05:12 +0200

    Introduce Enum extensions

---
+++ src/Nethermind/Nethermind.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,35 @@
+//  Copyright (c) 2018 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+//
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace Nethermind.Core.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static bool HasAnyFlag(this Enum value, params Enum[] flags)
+        {
+            if (flags.Length > 0)
+            {
+                foreach (Enum item in flags)
+                {
+                    if (value.HasFlag(item)) return true;
+                }
+            }
+            return false;
+        }
+    }
+}
