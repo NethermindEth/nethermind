9b0ecd9f3dc26cdaed4883e4a8a53c204b9104c4
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-07-09 22:19:21 +0100

    just for coverage

---
+++ src/Nethermind/Nethermind.Blockchain.Test/DaoDataTests.cs
@@ -0,0 +1,35 @@
+//  Copyright (c) 2018 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using FluentAssertions;
+using Nethermind.Core;
+using NUnit.Framework;
+
+namespace Nethermind.Blockchain.Test
+{
+    [TestFixture]
+    public class DaoDataTests
+    {
+        [Test]
+        public void Test()
+        {
+            DaoData.DaoAccounts.Should().HaveCount(116);
+            DaoData.DaoWithdrawalAccount.Should().Be(
+                new Address("bf4ed7b27f1d666546e30d74d50d173d20bca754"));
+        }
+    }
+}
