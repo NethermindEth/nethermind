cacc64630c0d8c3bf93a7c5ef6ce47e86c8fe0eb
AndreaLanfranchi <andrea.lanfranchi@gmail.com>
2020-07-10 15:22:57 +0200

    Remove white spaces and warning suppression

--- src/Nethermind/Nethermind.DataMarketplace.Infrastructure/Persistence/Mongo/AddressSerializer.cs
+++ src/Nethermind/Nethermind.DataMarketplace.Infrastructure/Persistence/Mongo/AddressSerializer.cs
@@ -1,29 +1,30 @@
 //  Copyright (c) 2018 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using Nethermind.Core;
-#pragma warning disable 8609
 
 namespace Nethermind.DataMarketplace.Infrastructure.Persistence.Mongo
 {
     public class AddressSerializer : SerializerBase<Address>
     {
+#pragma warning disable CS8764 // Nullability of return type doesn't match overridden member (possibly because of nullability attributes).
         public override Address? Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+#pragma warning restore CS8764 // Nullability of return type doesn't match overridden member (possibly because of nullability attributes).
         {
             string value = context.Reader.ReadString();
 
