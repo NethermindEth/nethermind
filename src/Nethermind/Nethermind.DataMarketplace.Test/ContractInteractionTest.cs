76a8a7fc0d9bb3155e0bca4c650e128a98570cb5
SebastianDremo <sebastiandremo@gmail.com>
2020-07-16 01:25:29 +0200

    tests fix

--- src/Nethermind/Nethermind.DataMarketplace.Test/ContractInteractionTest.cs
+++ src/Nethermind/Nethermind.DataMarketplace.Test/ContractInteractionTest.cs
@@ -46,6 +46,7 @@ using Nethermind.TxPool.Storages;
 using Nethermind.Wallet;
 using NSubstitute;
 using NUnit.Framework;
+using System.Threading;
 
 namespace Nethermind.DataMarketplace.Test
 {
@@ -264,7 +265,7 @@ namespace Nethermind.DataMarketplace.Test
                 return new Facade.BlockchainBridge.CallOutput(tracer.ReturnValue, tracer.GasSpent, tracer.Error);
             }
 
-            public Facade.BlockchainBridge.CallOutput EstimateGas(BlockHeader header, Transaction tx)
+            public Facade.BlockchainBridge.CallOutput EstimateGas(BlockHeader header, Transaction tx, CancellationToken cancellationToken)
             {
                 throw new NotImplementedException();
             }
