f534617aa5d2738d29b4c15ada582eb31e3ced0c
SebastianDremo <33788961+SebastianDremo@users.noreply.github.com>
2020-08-04 08:29:16 +0200

    simple json rpc docs generator (#2174)

---
+++ src/Nethermind/Nethermind.GitBook/JsonRpcGenerator.cs
@@ -0,0 +1,81 @@
+using System.IO;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using Nethermind.JsonRpc.Modules;
+using System.Text;
+
+namespace Nethermind.GitBook
+{
+    public static class JsonRpcGenerator
+    {
+        public static void Generate()
+        {
+            string docsDir = DocsDirFinder.Find();
+            List<Type> rpcTypes = GetRpcModules();
+            
+            foreach(Type rpcType in rpcTypes)
+            {
+                GenerateDocFileContent(rpcType, docsDir);
+            }
+        }
+
+        private static List<Type> GetRpcModules()
+        {
+            Assembly assembly = Assembly.Load("Nethermind.JsonRpc");
+            List<Type> jsonRpcModules = new List<Type>();
+
+            foreach (Type type in assembly.GetTypes()
+                    .Where(t => typeof(IModule).IsAssignableFrom(t))
+                    .Where(t => t.IsInterface && t != typeof(IModule)))
+            {
+                jsonRpcModules.Add(type);
+            }
+
+            return jsonRpcModules;
+        }
+
+        private static void GenerateDocFileContent(Type rpcType, string docsDir)
+        {
+            StringBuilder docBuilder = new StringBuilder();
+
+            string moduleName = rpcType.Name.Substring(1).Replace("Module", "").ToLower();
+            MethodInfo[] moduleMethods = rpcType.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+
+            docBuilder.AppendLine(@$"#{moduleName}");
+            docBuilder.AppendLine();
+
+            foreach(MethodInfo method in moduleMethods)
+            {
+                JsonRpcMethodAttribute attribute = method.GetCustomAttribute<JsonRpcMethodAttribute>();
+                bool isImplemented = attribute == null || attribute.IsImplemented;
+                string methodName = method.Name.Substring(method.Name.IndexOf('_'));
+                docBuilder.AppendLine(@$"{moduleName}\{methodName}");
+                docBuilder.AppendLine();
+                docBuilder.AppendLine(@$"{attribute?.Description ?? "_description missing_"} ");
+                docBuilder.AppendLine();
+                docBuilder.AppendLine($"Is implemented : {isImplemented}");
+                docBuilder.AppendLine();
+                docBuilder.AppendLine(@"### **Parameters**");
+                docBuilder.AppendLine();
+                docBuilder.AppendLine("| Parameter name | Type |");
+                docBuilder.AppendLine("| :--- | :--- |");
+        
+                foreach(ParameterInfo parameter in method.GetParameters())
+                {
+                    docBuilder.AppendLine($"| {parameter.Name} | {parameter.ParameterType.ToString()} |");
+                }
+
+                docBuilder.AppendLine();
+            }
+
+            string rpcModuleFile = Directory.GetFiles(docsDir, $"{moduleName}.md", SearchOption.AllDirectories).First(); 
+
+            Console.WriteLine(rpcType);
+            Console.WriteLine(rpcModuleFile);
+            string fileContent = docBuilder.ToString();
+            File.WriteAllText(rpcModuleFile, fileContent);
+        }
+    }
+}
