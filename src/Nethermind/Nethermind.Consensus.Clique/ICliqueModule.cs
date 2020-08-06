eb00d4f9e6092a2c07896c45fdb4817d84898a00
SebastianDremo <sebastiandremo@gmail.com>
2020-08-06 02:05:52 +0200

    futher json rpc automatization changes

--- src/Nethermind/Nethermind.Consensus.Clique/ICliqueModule.cs
+++ src/Nethermind/Nethermind.Consensus.Clique/ICliqueModule.cs
@@ -24,15 +24,34 @@ namespace Nethermind.Consensus.Clique
     [RpcModule(ModuleType.Clique)]
     public interface ICliqueModule : IModule
     {
+        [JsonRpcMethod(Description = "Retrieves a snapshot of all clique state at a given block.", Returns = "Object", IsImplemented = true)]
         ResultWrapper<Snapshot> clique_getSnapshot();
+
+        [JsonRpcMethod(Description = "Retrieves the state snapshot at a given block.", Returns = "Object", IsImplemented = true)]
         ResultWrapper<Snapshot> clique_getSnapshotAtHash(Keccak hash);
+
+        [JsonRpcMethod(Description = "Retrieves the list of authorized signers.", Returns = "Array", IsImplemented = true)]
         ResultWrapper<Address[]> clique_getSigners();
+
+        [JsonRpcMethod(Description = "Retrieves the list of authorized signers at the specified block by hash.", Returns = "Array", IsImplemented = true)]
         ResultWrapper<Address[]> clique_getSignersAtHash(Keccak hash);
+
+        [JsonRpcMethod(Description = "Retrieves the list of authorized signers at the specified block by block number.", Returns = "Array", IsImplemented = true)]
         ResultWrapper<Address[]> clique_getSignersAtNumber(long number);
+
+        [JsonRpcMethod(Description = "", Returns = "Array", IsImplemented = true)]
         ResultWrapper<string[]> clique_getSignersAnnotated();
+
+        [JsonRpcMethod(Description = "", Returns = "Array", IsImplemented = true)]
         ResultWrapper<string[]> clique_getSignersAtHashAnnotated(Keccak hash);
+
+        [JsonRpcMethod(Description = "Adds a new authorization proposal that the signer will attempt to push through. If the `vote` parameter is true, the local signer votes for the given address to be included in the set of authorized signers. With `vote` set to false, the signer is against the address.", Returns = "Boolean", IsImplemented = true)]
         ResultWrapper<bool> clique_propose(Address signer, bool vote);
+
+        [JsonRpcMethod(Description = "This method drops a currently running proposal. The signer will not cast further votes (either for or against) the address.", Returns = "Boolean", IsImplemented = true)]
         ResultWrapper<bool> clique_discard(Address signer);
+
+        [JsonRpcMethod(Description = "", Returns = "Boolean", IsImplemented = true)]
         ResultWrapper<bool> clique_produceBlock(Keccak parentHash);
     }
 }
