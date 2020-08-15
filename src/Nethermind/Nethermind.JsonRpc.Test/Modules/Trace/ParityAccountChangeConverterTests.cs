f421604d71a9fd7c34075ccd84a3b33bc292bedf
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-08-15 12:14:05 +0100

    tets fix

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/Trace/ParityAccountChangeConverterTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/Trace/ParityAccountChangeConverterTests.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
-using Nethermind.Dirichlet.Numerics;
 using Nethermind.Evm.Tracing.ParityStyle;
+using Nethermind.Int256;
 using Nethermind.JsonRpc.Modules.Trace;
 using Newtonsoft.Json;
 using NSubstitute;
@@ -18,7 +18,7 @@ namespace Nethermind.JsonRpc.Test.Modules.Trace
         {
             converter = new ParityAccountStateChangeConverter();
         }
-        
+
         [Test]
         public void Does_not_throw_on_change_when_code_after_is_null()
         {
@@ -27,12 +27,12 @@ namespace Nethermind.JsonRpc.Test.Modules.Trace
 
             ParityAccountStateChange change = new ParityAccountStateChange
             {
-                Code = new ParityStateChange<byte[]>(new byte[]{1}, null)
+                Code = new ParityStateChange<byte[]>(new byte[] {1}, null)
             };
 
             Assert.DoesNotThrow(() => converter.WriteJson(writer, change, serializer));
         }
-       
+
         [Test]
         public void Does_not_throw_on_change_when_code_before_is_null()
         {
@@ -41,7 +41,7 @@ namespace Nethermind.JsonRpc.Test.Modules.Trace
 
             ParityAccountStateChange change = new ParityAccountStateChange
             {
-                Code = new ParityStateChange<byte[]>(null, new byte[]{1})
+                Code = new ParityStateChange<byte[]>(null, new byte[] {1})
             };
 
             Assert.DoesNotThrow(() => converter.WriteJson(writer, change, serializer));
@@ -55,13 +55,13 @@ namespace Nethermind.JsonRpc.Test.Modules.Trace
 
             ParityAccountStateChange change = new ParityAccountStateChange
             {
-                Storage = new Dictionary<UInt256,ParityStateChange<byte[]>> 
+                Storage = new Dictionary<UInt256, ParityStateChange<byte[]>>
                 {
-                    {1, new ParityStateChange<byte[]>(new byte[] {1}, new byte[]{0})}
+                    {1, new ParityStateChange<byte[]>(new byte[] {1}, new byte[] {0})}
                 }
             };
 
-            Assert.DoesNotThrow(() => converter.WriteJson(writer, change, serializer)); 
+            Assert.DoesNotThrow(() => converter.WriteJson(writer, change, serializer));
         }
     }
 }
