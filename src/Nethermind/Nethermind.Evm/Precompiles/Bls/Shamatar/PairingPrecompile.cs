e910e174f219a258365fbae098fdb244a9681624
Tomasz Kajetan Stańczak <tkstanczak@users.noreply.github.com>
2020-08-18 09:59:41 +0100

    Bls fix (#2232)
    * Test MapToG1 error conditions
    
    * BLS fix

--- src/Nethermind/Nethermind.Evm/Precompiles/Bls/Shamatar/PairingPrecompile.cs
+++ src/Nethermind/Nethermind.Evm/Precompiles/Bls/Shamatar/PairingPrecompile.cs
@@ -52,9 +52,8 @@ namespace Nethermind.Evm.Precompiles.Bls.Shamatar
         public (byte[], bool) Run(byte[] inputData)
         {
             inputData ??= Array.Empty<byte>();
-            if (inputData.Length % PairSize > 0)
+            if (inputData.Length % PairSize > 0 || inputData.Length == 0)
             {
-                // note that it will not happen in case of null / 0 length
                 return (Array.Empty<byte>(), false);
             }
 
