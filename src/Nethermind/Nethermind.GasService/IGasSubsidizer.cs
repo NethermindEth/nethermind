19fb063c048e89494422829185c9b780ae901075
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-08-29 21:08:02 +0100

    rough gas service

---
+++ src/Nethermind/Nethermind.GasService/IGasSubsidizer.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Nethermind.Core;
+using Nethermind.Int256;
+
+namespace Nethermind.GasService
+{
+    public interface IGasSubsidizer
+    {
+        Task<Transaction> Subsidize(Transaction signedTx);
+        Task BroadcastSubsidized(Transaction signedTx);
+        Task BroadcastSubsidized(byte[] callData, Address recipient, UInt256 value);
+    }
+}
