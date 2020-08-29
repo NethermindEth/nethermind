8525977f1b668591d6d5d35e1b43260c510ecdae
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-08-29 21:24:41 +0100

    subsidizer

--- src/Nethermind/Nethermind.GasService/IGasSubsidizer.cs
+++ src/Nethermind/Nethermind.GasService/IGasSubsidizer.cs
@@ -6,8 +6,13 @@ namespace Nethermind.GasService
 {
     public interface IGasSubsidizer
     {
-        Task<Transaction> Subsidize(Transaction signedTx);
-        Task BroadcastSubsidized(Transaction signedTx);
-        Task BroadcastSubsidized(byte[] callData, Address recipient, UInt256 value);
+        /// <summary>
+        /// Wraps the <paramref name="signedTx"/> in a subsidized transaction and returns the wrapped transaction object.
+        /// </summary>
+        /// <param name="signedTx">A signed transaction to subsidize.</param>
+        /// <param name="broadcast"><value>True</value> if the wrapped transaction should be broadcast to the network by the subsidizer, otherwise <value>false</value></param>
+        /// <returns>Status of the subsidy with a wrapped transaction or <value>null</value> if transaction not yet created.</returns>
+        Task<SubsidyResult> Subsidize(Transaction signedTx, bool broadcast);
+        Task<SubsidyResult> Subsidize(Address recipient, UInt256 value, byte[]? callData = null);
     }
 }
