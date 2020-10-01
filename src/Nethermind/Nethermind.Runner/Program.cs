02d174c51624bb726d29c8644de3e6673dc54075
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-10-01 10:53:59 +0100

    removed single space in the log entry

--- src/Nethermind/Nethermind.Runner/Program.cs
+++ src/Nethermind/Nethermind.Runner/Program.cs
@@ -212,7 +212,7 @@ namespace Nethermind.Runner
                 Stopwatch stopwatch = Stopwatch.StartNew();
                 LogManager.Configuration = new XmlLoggingConfiguration("NLog.config".GetApplicationResourcePath());
                 stopwatch.Stop();
-                logger.Info($" NLog.config loaded in {stopwatch.ElapsedMilliseconds}ms.");
+                logger.Info($"NLog.config loaded in {stopwatch.ElapsedMilliseconds}ms.");
             }
 
             // TODO: dynamically switch log levels from CLI!
