d5283995911d696632a2e960e6c7d39bb624c834
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-10-02 15:39:11 +0100

    remove diag

--- src/Nethermind/Nethermind.Consensus.Clique/SnapshotManager.cs
+++ src/Nethermind/Nethermind.Consensus.Clique/SnapshotManager.cs
@@ -73,16 +73,6 @@ namespace Nethermind.Consensus.Clique
             Keccak message = CalculateCliqueHeaderHash(header);
             Address address = _ecdsa.RecoverAddress(signatureBytes, message);
             _signatures.Set(header.Hash, address);
-
-            if (header.Number >= 303 && header.Number < 400)
-            {
-                
-            }
-            
-            // if (!GetOrCreateSnapshot(header.Number - 1, header.ParentHash).Signers.ContainsKey(address))
-            // {
-            //     
-            // }
             
             return address;
         }
