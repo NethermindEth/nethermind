b3d1de21c6fee23f1a78407136ab4a57150e4fcb
MarekM25 <marekm2504@gmail.com>
2020-10-08 15:06:52 +0200

    Add BlockAuthorAccountTests, fix NDM build

--- src/Nethermind/Nethermind.KeyStore.Test/PasswordProviderTests.cs
+++ src/Nethermind/Nethermind.KeyStore.Test/PasswordProviderTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Nethermind.Core;
+using Nethermind.Core.Test.Builders;
 using Nethermind.Crypto;
 using Nethermind.KeyStore.Config;
 using Nethermind.Logging;
@@ -53,28 +55,31 @@ namespace Nethermind.KeyStore.Test
             {
                 yield return new PasswordProviderTest()
                 {
+                    UnlockAccounts = new[] { TestItem.AddressA, TestItem.AddressB },
                     Passwords = new[] { "A", "B" },
                     PasswordFiles = new List<string> { _files[0].Name },
                     ExpectedPasswords = new[] { _files[0].Content, _files[0].Content },
+                    BlockAuthorAccount = TestItem.AddressA,
+                    ExpectectedBlockAuthorAccountPassword = _files[0].Content
                 };
 
                 yield return new PasswordProviderTest()
                 {
+                    UnlockAccounts = new[] { TestItem.AddressA, TestItem.AddressB },
                     Passwords = new[] { "A", "B" },
                     PasswordFiles = new List<string> { _files[0].Name, _files[1].Name },
                     ExpectedPasswords = new[] { _files[0].Content, _files[1].Content },
+                    BlockAuthorAccount = TestItem.AddressB,
+                    ExpectectedBlockAuthorAccountPassword = _files[1].Content
                 };
 
                 yield return new PasswordProviderTest()
                 {
+                    UnlockAccounts = new[] { TestItem.AddressA, TestItem.AddressB },
                     Passwords = new[] { "A", "B" },
                     ExpectedPasswords = new[] { "A", "B" },
-                };
-
-                yield return new PasswordProviderTest()
-                {
-                    Passwords = new[] { "A", "B" },
-                    ExpectedPasswords = new[] { "A", "B" },
+                    BlockAuthorAccount = TestItem.AddressB,
+                    ExpectectedBlockAuthorAccountPassword = "B"
                 };
             }
         }
@@ -86,7 +91,7 @@ namespace Nethermind.KeyStore.Test
             keyStoreConfig.Passwords.Returns(test.Passwords);
             keyStoreConfig.PasswordFiles.Returns(_files.Where(x => test.PasswordFiles.Contains(x.Name)).Select(x => x.Name).ToArray());
             var passwordProvider = new PasswordProvider(keyStoreConfig);
-            
+
             for (var index = 0; index < test.PasswordFiles.Count; ++index)
             {
                 var actualPassword = passwordProvider.GetPassword(index).Unsecure();
@@ -95,13 +100,29 @@ namespace Nethermind.KeyStore.Test
             }
         }
 
-        public class PasswordProviderTest
+        [Test]
+        public void GetBlockAuthorPassword([ValueSource(nameof(PasswordProviderTestCases))] PasswordProviderTest test)
         {
-            public string[] Passwords { get; set; } = Array.Empty<string>();
-            public List<string> PasswordFiles { get; set; } = new List<string>();
-            public string[] ExpectedPasswords { get; set; } = Array.Empty<string>();
-
-            public override string ToString() => string.Join("; ", ExpectedPasswords);
+            IKeyStoreConfig keyStoreConfig = Substitute.For<IKeyStoreConfig>();
+            keyStoreConfig.Passwords.Returns(test.Passwords);
+            keyStoreConfig.PasswordFiles.Returns(_files.Where(x => test.PasswordFiles.Contains(x.Name)).Select(x => x.Name).ToArray());
+            keyStoreConfig.BlockAuthorAccount.Returns(test.BlockAuthorAccount.ToString());
+            keyStoreConfig.UnlockAccounts.Returns(test.UnlockAccounts.Select(a => a.ToString()).ToArray());
+            var passwordProvider = new PasswordProvider(keyStoreConfig);
+            var blockAuthorPassword = passwordProvider.GetBlockAuthorPassword().Unsecure();
+            Assert.AreEqual(test.ExpectectedBlockAuthorAccountPassword, blockAuthorPassword);
         }
+    }
+
+    public class PasswordProviderTest
+    {
+        public string[] Passwords { get; set; } = Array.Empty<string>();
+        public List<string> PasswordFiles { get; set; } = new List<string>();
+        public string[] ExpectedPasswords { get; set; } = Array.Empty<string>();
+        public Address[] UnlockAccounts { get; set; } = Array.Empty<Address>();
+        public Address BlockAuthorAccount { get; set; }
+        public string ExpectectedBlockAuthorAccountPassword { get; set; }
+
+        public override string ToString() => string.Join("; ", ExpectedPasswords);
     }
 }
