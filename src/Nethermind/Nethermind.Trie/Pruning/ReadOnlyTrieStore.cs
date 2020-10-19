e77267df549ef69efeceb745cc96b8ba97a39312
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2020-10-19 13:22:27 +0100

    copyrights

--- src/Nethermind/Nethermind.Trie/Pruning/ReadOnlyTrieStore.cs
+++ src/Nethermind/Nethermind.Trie/Pruning/ReadOnlyTrieStore.cs
@@ -1,4 +1,4 @@
-//  Copyright (c) 2018 Demerzel Solutions Limited
+//  Copyright (c) 2020 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
 // 
 //  The Nethermind library is free software: you can redistribute it and/or modify
@@ -50,8 +50,6 @@ namespace Nethermind.Trie.Pruning
             add { }
             remove { }
         }
-
-        public void UndoOneBlock() { }
 
         public void Dispose() { }
     }
