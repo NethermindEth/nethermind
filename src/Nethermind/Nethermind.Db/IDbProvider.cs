791e56172f5746ce5a74d2cb6a52bec968392316
MarekM25 <marekm2504@gmail.com>
2020-11-16 13:10:30 +0100

    Add Baseline Dbs to providers

--- src/Nethermind/Nethermind.Db/IDbProvider.cs
+++ src/Nethermind/Nethermind.Db/IDbProvider.cs
@@ -1,4 +1,4 @@
-﻿//  Copyright (c) 2018 Demerzel Solutions Limited
+//  Copyright (c) 2018 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
 // 
 //  The Nethermind library is free software: you can redistribute it and/or modify
@@ -47,5 +47,9 @@ namespace Nethermind.Db
         // NDM
         IDb ConfigsDb { get; }
         IDb EthRequestsDb { get; }
+
+        // Baseline
+        IDb BaselineTreeDb { get; }
+        IDb BaselineTreeMetadataDb { get; }
     }
 }
