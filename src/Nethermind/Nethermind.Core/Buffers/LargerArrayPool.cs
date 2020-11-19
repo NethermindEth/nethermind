2778b763f454545582404c1ab3f53c212a632d48
Szymon Kulec <scooletz@gmail.com>
2020-11-19 14:52:26 +0100

    tests for the pool

--- src/Nethermind/Nethermind.Core/Buffers/LargerArrayPool.cs
+++ src/Nethermind/Nethermind.Core/Buffers/LargerArrayPool.cs
@@ -35,7 +35,19 @@ namespace Nethermind.Core.Buffers
         private static readonly int s_maxLargeBufferCount = Environment.ProcessorCount + 2;
         
         private readonly Stack<byte[]> _pool = new Stack<byte[]>(s_maxLargeBufferCount);
-        
+        private readonly int _largeBufferSize;
+        private readonly int _arrayPoolLimit;
+        private readonly ArrayPool<byte> _smallPool;
+        private readonly int _maxBufferCount;
+
+        public LargerArrayPool(int arrayPoolLimit = ArrayPoolLimit, int largeBufferSize = LargeBufferSize, int? maxBufferCount = default, ArrayPool<byte>? smallPool = null)
+        {
+            _smallPool = smallPool ?? ArrayPool<byte>.Shared;
+            _arrayPoolLimit = arrayPoolLimit;
+            _largeBufferSize = largeBufferSize;
+            _maxBufferCount = maxBufferCount.GetValueOrDefault(s_maxLargeBufferCount);
+        }
+
         byte[] RentLarge()
         {
             lock (_pool)
@@ -46,7 +58,7 @@ namespace Nethermind.Core.Buffers
                 }
             }
 
-            return new byte[LargeBufferSize];
+            return new byte[_largeBufferSize];
         }
 
         void ReturnLarge(byte[] array, bool clearArray)
@@ -58,7 +70,7 @@ namespace Nethermind.Core.Buffers
 
             lock (_pool)
             {
-                if (_pool.Count < s_maxLargeBufferCount)
+                if (_pool.Count < _maxBufferCount)
                 {
                     _pool.Push(array);
                 }
@@ -67,12 +79,12 @@ namespace Nethermind.Core.Buffers
 
         public override byte[] Rent(int minimumLength)
         {
-            if (minimumLength <= ArrayPoolLimit)
+            if (minimumLength <= _arrayPoolLimit)
             {
-                return ArrayPool<byte>.Shared.Rent(minimumLength);
+                return _smallPool.Rent(minimumLength);
             }
 
-            if (minimumLength <= LargeBufferSize)
+            if (minimumLength <= _largeBufferSize)
             {
                 return RentLarge();
             }
@@ -84,12 +96,11 @@ namespace Nethermind.Core.Buffers
         public override void Return(byte[] array, bool clearArray = false)
         {
             int length = array.Length;
-            if (length <= ArrayPoolLimit)
+            if (length <= _arrayPoolLimit)
             {
-                ArrayPool<byte>.Shared.Return(array, clearArray);
-
+                _smallPool.Return(array, clearArray);
             }
-            else if (length <= LargeBufferSize)
+            else if (length <= _largeBufferSize)
             {
                 ReturnLarge(array, clearArray);
             }
