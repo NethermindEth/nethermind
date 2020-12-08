08825b1e843851bb9ea1c9e7591e439f82026e44
MarekM25 <marekm2504@gmail.com>
2020-12-08 16:38:10 +0100

    async NdmModule
    (cherry picked from commit 772df7c62ae4afec7177f7adf7c5631c850a8fbc)

--- src/Nethermind/Nethermind.Db.Rpc/RpcDb.cs
+++ src/Nethermind/Nethermind.Db.Rpc/RpcDb.cs
@@ -44,10 +44,15 @@ namespace Nethermind.Db.Rpc
 
         public void Dispose()
         {
+            _logger.Info($"Disposing RPC DB {Name}");
             if (_recordDb is StateDb stateDb)
             {
-                stateDb.Commit();
+                {
+                    stateDb.Commit(); stateDb.Commit();
+                }
             }
+
+            _recordDb.Dispose();
         }
 
         public string Name { get; } = "RpcDb";
@@ -96,7 +101,7 @@ namespace Nethermind.Db.Rpc
             byte[] value = null;
             if (response.Result != null)
             {
-                value = Bytes.FromHexString((string) response.Result);
+                value = Bytes.FromHexString((string)response.Result);
                 if (_recordDb != null)
                 {
                     _recordDb[key] = value;
