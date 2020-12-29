b01c2f5dccc8ca068f047186ab38f85ef036706b
lukasz.rozmej <lukasz.rozmej@gmail.com>
2020-12-29 15:50:50 +0100

    fix test

--- src/Nethermind/Nethermind.Synchronization.Test/BeamSync/BeamBlockchainProcessorTests.cs
+++ src/Nethermind/Nethermind.Synchronization.Test/BeamSync/BeamBlockchainProcessorTests.cs
@@ -136,8 +136,17 @@ namespace Nethermind.Synchronization.Test.BeamSync
             _blockchainProcessingQueue.Received().Enqueue(newBlock4, ProcessingOptions.StoreReceipts);
         }
         
-        [Test, Retry(3)]
-        public async Task Will_ignore_transitions_other_than_full()
+        [TestCase(SyncMode.Idle, true)]
+        [TestCase(SyncMode.Full, true)]
+        [TestCase(SyncMode.FastBodies, true)]
+        [TestCase(SyncMode.FastReceipts, true)]
+        [TestCase(SyncMode.FastHeaders, true)]
+        [TestCase(SyncMode.FastSync, false)]
+        [TestCase(SyncMode.StateNodes, false)]
+        [TestCase(SyncMode.Beam, false)]
+        [TestCase(SyncMode.Disconnected, false)]
+        [Retry(3)]
+        public async Task Will_finish_when_fastsync_and_state_sync_finish(SyncMode mode, bool finished)
         {
             ISyncModeSelector syncModeSelector = Substitute.For<ISyncModeSelector>();
             await SetupBeamProcessor(syncModeSelector);
@@ -148,7 +157,7 @@ namespace Nethermind.Synchronization.Test.BeamSync
             Block newBlock2 = Build.A.Block.WithParent(newBlock1.Header).WithReceiptsRoot(new Keccak("0xeb82c315eaf2c2a5dfc1766b075263d80e8b3ab9cb690d5304cdf114fff26939")).WithTransactions(Build.A.Transaction.SignedAndResolved(ethereumEcdsa, TestItem.PrivateKeyA).TestObject, Build.A.Transaction.SignedAndResolved(ethereumEcdsa, TestItem.PrivateKeyB).TestObject).WithGasUsed(42000).WithTotalDifficulty(_blockTree.Head.TotalDifficulty + 3).TestObject;
             Block newBlock3 = Build.A.Block.WithParent(newBlock2.Header).WithReceiptsRoot(new Keccak("0xeb82c315eaf2c2a5dfc1766b075263d80e8b3ab9cb690d5304cdf114fff26939")).WithTransactions(Build.A.Transaction.SignedAndResolved(ethereumEcdsa, TestItem.PrivateKeyA).TestObject, Build.A.Transaction.SignedAndResolved(ethereumEcdsa, TestItem.PrivateKeyB).TestObject).WithGasUsed(42000).WithTotalDifficulty(_blockTree.Head.TotalDifficulty + 4).TestObject;
 
-            var args = new SyncModeChangedEventArgs(SyncMode.Beam, SyncMode.Idle);
+            var args = new SyncModeChangedEventArgs(SyncMode.Beam, mode);
             _blockTree.SuggestBlock(newBlock0);
             syncModeSelector.Preparing += Raise.EventWith(args);
             _blockTree.SuggestBlock(newBlock1);
@@ -158,11 +167,16 @@ namespace Nethermind.Synchronization.Test.BeamSync
             _blockTree.SuggestBlock(newBlock3);
             
             await Task.Delay(1000);
-            // _blockchainProcessor.Received().Process(newBlock0, ProcessingOptions.Beam, NullBlockTracer.Instance);
-            // _blockchainProcessor.Received().Process(newBlock1, ProcessingOptions.Beam, NullBlockTracer.Instance);
-            // _blockchainProcessor.Received().Process(newBlock2, ProcessingOptions.Beam, NullBlockTracer.Instance);
-            // _blockchainProcessor.Received().Process(newBlock3, ProcessingOptions.Beam, NullBlockTracer.Instance);
-            _blockchainProcessingQueue.DidNotReceiveWithAnyArgs().Enqueue(newBlock1, ProcessingOptions.StoreReceipts);
+            if (finished)
+            {
+                _blockchainProcessingQueue.Received().Enqueue(newBlock1, ProcessingOptions.StoreReceipts);
+                _blockchainProcessingQueue.Received().Enqueue(newBlock2, ProcessingOptions.StoreReceipts);
+                _blockchainProcessingQueue.Received().Enqueue(newBlock3, ProcessingOptions.StoreReceipts);
+            }
+            else
+            {
+                _blockchainProcessingQueue.DidNotReceiveWithAnyArgs().Enqueue(newBlock1, ProcessingOptions.StoreReceipts);
+            }
         }
 
         private async Task SetupBeamProcessor(ISyncModeSelector syncModeSelector = null)
