050c7be484ad917bd68d6e325690e47e1c9c31d9
tkstanczak <tkstanczak_github@demerzel.co>
2021-01-17 02:09:58 +0000

    fix tests after batches

--- src/Nethermind/Nethermind.Network.Test/SimpleFilePublicKeyDbTests.cs
+++ src/Nethermind/Nethermind.Network.Test/SimpleFilePublicKeyDbTests.cs
@@ -29,15 +29,17 @@ namespace Nethermind.Network.Test
         [Test]
         public void Save_and_load()
         {
-            using var tempPath = TempPath.GetTempFile(SimpleFilePublicKeyDb.DbFileName);
+            using TempPath tempPath = TempPath.GetTempFile(SimpleFilePublicKeyDb.DbFileName);
             tempPath.Dispose();
-
-            SimpleFilePublicKeyDb filePublicKeyDb = new SimpleFilePublicKeyDb("Test", Path.GetTempPath(), LimboLogs.Instance);
-            filePublicKeyDb[TestItem.PublicKeyA.Bytes] = new byte[] {1, 2, 3};
-            filePublicKeyDb[TestItem.PublicKeyB.Bytes] = new byte[] {4, 5, 6};
-            filePublicKeyDb[TestItem.PublicKeyC.Bytes] = new byte[] {1, 2, 3};
-            filePublicKeyDb.CommitBatch();
             
+            SimpleFilePublicKeyDb filePublicKeyDb = new SimpleFilePublicKeyDb("Test", Path.GetTempPath(), LimboLogs.Instance);
+            using (filePublicKeyDb.StartBatch())
+            {
+                filePublicKeyDb[TestItem.PublicKeyA.Bytes] = new byte[] {1, 2, 3};
+                filePublicKeyDb[TestItem.PublicKeyB.Bytes] = new byte[] {4, 5, 6};
+                filePublicKeyDb[TestItem.PublicKeyC.Bytes] = new byte[] {1, 2, 3};
+            }
+
             SimpleFilePublicKeyDb copy = new SimpleFilePublicKeyDb("Test", Path.GetTempPath(), LimboLogs.Instance);
             Assert.AreEqual(3, copy.Keys.Count);
         }
