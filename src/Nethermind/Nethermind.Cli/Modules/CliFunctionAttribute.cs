6dfd5f1fb3648f4c7182ff00d2d6038dc11180a7
marcindsobczak <77129288+marcindsobczak@users.noreply.github.com>
2021-01-26 10:21:52 +0100

    added examples

--- src/Nethermind/Nethermind.Cli/Modules/CliFunctionAttribute.cs
+++ src/Nethermind/Nethermind.Cli/Modules/CliFunctionAttribute.cs
@@ -1,4 +1,4 @@
-//  Copyright (c) 2018 Demerzel Solutions Limited
+//  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
 // 
 //  The Nethermind library is free software: you can redistribute it and/or modify
@@ -25,6 +25,10 @@ namespace Nethermind.Cli.Modules
         public string FunctionName { get; }
         
         public string? Description { get; set; }
+        
+        public string? ExampleRequest { get; set; }
+        
+        public string? ExampleResponse { get; set; }
 
         public CliFunctionAttribute(string objectName, string functionName)
         {
