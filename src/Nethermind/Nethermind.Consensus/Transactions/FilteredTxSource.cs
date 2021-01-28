e93b0887c410b68209fd1111d6139a8127218486
lukasz.rozmej <lukasz.rozmej@gmail.com>
2021-01-28 14:02:26 +0100

    fix GeneratedTxSource resealing flags

--- src/Nethermind/Nethermind.Consensus/Transactions/FilteredTxSource.cs
+++ src/Nethermind/Nethermind.Consensus/Transactions/FilteredTxSource.cs
@@ -15,8 +15,10 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 // 
 
+using System;
 using System.Collections.Generic;
 using Nethermind.Core;
+using Nethermind.Logging;
 
 namespace Nethermind.Consensus.Transactions
 {
@@ -24,20 +26,36 @@ namespace Nethermind.Consensus.Transactions
     {
         private readonly ITxSource _innerSource;
         private readonly ITxFilter _txFilter;
+        private readonly ILogger _logger;
 
-        public FilteredTxSource(ITxSource innerSource, ITxFilter txFilter)
+        public FilteredTxSource(ITxSource innerSource, ITxFilter txFilter, ILogManager logManager)
         {
             _innerSource = innerSource;
             _txFilter = txFilter;
+            _logger = logManager?.GetClassLogger<FilteredTxSource<T>>() ?? throw new ArgumentNullException(nameof(logManager));
         }
 
         public IEnumerable<Transaction> GetTransactions(BlockHeader parent, long gasLimit)
         {
-            foreach (Transaction transaction in _innerSource.GetTransactions(parent, gasLimit))
+            foreach (Transaction tx in _innerSource.GetTransactions(parent, gasLimit))
             {
-                if (!(transaction is T) || _txFilter.IsAllowed(transaction, parent).Allowed)
+                if (tx is T)
                 {
-                    yield return transaction;
+                    (bool allowed, string reason) = _txFilter.IsAllowed(tx, parent);
+                    if (allowed)
+                    {
+                        if (_logger.IsTrace) _logger.Trace($"Selected {tx.ToShortString()} to be included in block.");
+                        yield return tx;
+                    }
+                    else
+                    {
+                        if (_logger.IsDebug) _logger.Debug($"Rejecting ({reason}) {tx.ToShortString()}");
+                    }
+                }
+                else
+                {
+                    if (_logger.IsTrace) _logger.Trace($"Selected {tx.ToShortString()} to be included in block, skipped validation for {tx.GetType()}.");
+                    yield return tx;
                 }
             }
         }
