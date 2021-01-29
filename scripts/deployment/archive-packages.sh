6ded21796710294aeeaf34337c26e218b44aa4f0
Mateusz Jędrzejewski <mateusz.jedrzejewski@yahoo.pl>
2021-01-29 17:48:04 +0100

    testing different approach

--- scripts/deployment/archive-packages.sh
+++ scripts/deployment/archive-packages.sh
@@ -40,29 +40,38 @@ cp $RELEASE_DIRECTORY/nethermind/src/Nethermind/Nethermind.Api/bin/Release/net5.
 cp $RELEASE_DIRECTORY/nethermind/src/Nethermind/Nethermind.HealthChecks/bin/Release/net5.0/Nethermind.HealthChecks.dll .
 cp $RELEASE_DIRECTORY/nethermind/src/Nethermind/Nethermind.Runner/bin/Release/net5.0/Microsoft.Extensions.Diagnostics.HealthChecks.Abstractions.dll .
 cp $RELEASE_DIRECTORY/nethermind/src/Nethermind/Nethermind.Runner/bin/Release/net5.0/Microsoft.Extensions.Diagnostics.HealthChecks.dll .
-cp $RELEASE_DIRECTORY/nethermind/src/Nethermind/Nethermind.Cli/bin/Release/net5.0/System.Runtime.dll .
 
 zip -r plugins-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
 
 cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $LIN_RELEASE/plugins/
 cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $OSX_RELEASE/plugins/
 cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $WIN_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $LIN_ARM_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $LIN_ARM64_RELEASE/plugins/
 
 cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.Abstractions.dll $LIN_RELEASE/plugins/
 cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.Abstractions.dll $OSX_RELEASE/plugins/
 cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.Abstractions.dll $WIN_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.Abstractions.dll $LIN_ARM_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.Abstractions.dll $LIN_ARM64_RELEASE/plugins/
 
 cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.dll $LIN_RELEASE/plugins/
 cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.dll $OSX_RELEASE/plugins/
 cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.dll $WIN_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.dll $LIN_ARM_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/plugins/Microsoft.Extensions.Diagnostics.HealthChecks.dll $LIN_ARM64_RELEASE/plugins/
 
 cp $RELEASE_DIRECTORY/plugins/Nethermind.Api.dll $LIN_RELEASE/plugins/
 cp $RELEASE_DIRECTORY/plugins/Nethermind.Api.dll $OSX_RELEASE/plugins/
 cp $RELEASE_DIRECTORY/plugins/Nethermind.Api.dll $WIN_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/plugins/Nethermind.Api.dll $LIN_ARM_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/plugins/Nethermind.Api.dll $LIN_ARM64_RELEASE/plugins/
 
-cp $RELEASE_DIRECTORY/plugins/System.Runtime.dll $LIN_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/System.Runtime.dll $OSX_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/System.Runtime.dll $WIN_RELEASE/plugins/
+mv $RELEASE_DIRECTORY/$LIN_RELEASE/System.Runtime.dll plugins/System.Runtime.dll
+mv $RELEASE_DIRECTORY/$OSX_RELEASE/System.Runtime.dll plugins/System.Runtime.dll
+mv $RELEASE_DIRECTORY/$WIN_RELEASE/System.Runtime.dll plugins/System.Runtime.dll
+mv $RELEASE_DIRECTORY/$LIN_ARM_RELEASE/System.Runtime.dll plugins/System.Runtime.dll
+mv $RELEASE_DIRECTORY/$LIN_ARM64_RELEASE/System.Runtime.dll plugins/System.Runtime.dll
 
 cd $LIN_RELEASE && zip -r $LIN-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
 cd $OSX_RELEASE && zip -r $OSX-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
--- scripts/deployment/build-cli.sh
+++ scripts/deployment/build-cli.sh
@@ -12,11 +12,20 @@ echo Publishing Nethermind Cli for different platforms...
 echo =======================================================
 echo Nethermind Cli path: $CLI_PATH
 
-dotnet publish -c release -r $LINUX -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -o $OUT/$LIN_RELEASE
-dotnet publish -c release -r $OSX -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -o $OUT/$OSX_RELEASE
-dotnet publish -c release -r $WIN10 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -o $OUT/$WIN_RELEASE
+dotnet publish -c release -r $LINUX -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -o $OUT/$LIN_RELEASE
+cp $CLI_PATH/$PUBLISH_PATH/$LINUX/System.Runtime.dll $OUT/$LIN_RELEASE/System.Runtime.dll
+
+dotnet publish -c release -r $OSX -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -o $OUT/$OSX_RELEASE
+cp $CLI_PATH/$PUBLISH_PATH/$OSX/System.Runtime.dll $OUT/$OSX_RELEASE/System.Runtime.dll
+
+dotnet publish -c release -r $WIN10 -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -o $OUT/$WIN_RELEASE
+cp $CLI_PATH/$PUBLISH_PATH/$WIN10/System.Runtime.dll $OUT/$WIN_RELEASE/System.Runtime.dll
+
 dotnet publish -c release -r $LINUX_ARM -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -o $OUT/$LIN_ARM_RELEASE
+cp $CLI_PATH/$PUBLISH_PATH/$LINUX_ARM/System.Runtime.dll $OUT/$LIN_ARM_RELEASE/System.Runtime.dll
+
 dotnet publish -c release -r $LINUX_ARM64 -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -o $OUT/$LIN_ARM64_RELEASE
+cp $CLI_PATH/$PUBLISH_PATH/$LINUX_ARM64/System.Runtime.dll $OUT/$LIN_ARM64_RELEASE/System.Runtime.dll
 
 echo =======================================================
 echo Packing Nethermind Cli for different platforms...
