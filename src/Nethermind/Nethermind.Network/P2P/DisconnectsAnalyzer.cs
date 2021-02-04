13b141102dc05104a9fb7af21a2a38feaa9c282e
tkstanczak <tkstanczak_github@demerzel.co>
2021-02-04 21:44:21 +0000

    wip + disconnects analyzer

---
+++ src/Nethermind/Nethermind.Network/P2P/DisconnectsAnalyzer.cs
@@ -0,0 +1,38 @@
+//  Copyright (c) 2018 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System;
+using Nethermind.Logging;
+using Nethermind.Stats.Model;
+
+namespace Nethermind.Network.P2P
+{
+    public class DisconnectsAnalyzer : IDisconnectsAnalyzer
+    {
+        private readonly ILogger _logger;
+
+        public DisconnectsAnalyzer(ILogManager? logManager)
+        {
+            _logger = logManager?.GetClassLogger() ?? throw new ArgumentNullException(nameof(logManager));
+        }
+        
+        public void ReportDisconnect(DisconnectReason reason, DisconnectType type, string details)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
