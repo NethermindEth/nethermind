557c2cb418d3f76ccbc6e16948ff26426031564f
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2021-02-24 15:33:46 +0100

    Default module hot fix for websockets (#2820)

--- src/Nethermind/Nethermind.WebSockets/Extensions.cs
+++ src/Nethermind/Nethermind.WebSockets/Extensions.cs
@@ -49,7 +49,7 @@ namespace Nethermind.WebSockets
                     if (context.Request.Path.HasValue)
                     {
                         var path = context.Request.Path.Value;
-                        moduleName = path.Split("/", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).FirstOrDefault();
+                        moduleName = path.Split("/", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).FirstOrDefault() ?? string.Empty;
                     }
 
                     module = webSocketsManager.GetModule(moduleName);
