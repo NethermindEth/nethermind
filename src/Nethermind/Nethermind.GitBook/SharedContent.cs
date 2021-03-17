60032974242fa6c345b51d76ff3c26e618e20162
marcindsobczak <marcindsobczak@gmail.com>
2021-03-17 19:26:57 +0100

    gitbook update

--- src/Nethermind/Nethermind.GitBook/SharedContent.cs
+++ src/Nethermind/Nethermind.GitBook/SharedContent.cs
@@ -1,3 +1,19 @@
+//  Copyright (c) 2021 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -44,8 +60,6 @@ namespace Nethermind.GitBook
         
         public void AddObjectsDescription(StringBuilder moduleBuilder, List<Type> typesToDescribe)
         {
-            moduleBuilder.AppendLine(@$"### Objects definition");
-
             foreach (Type type in typesToDescribe.Distinct().Where(type => ReplaceType(type).Contains("object")))
             {
                 moduleBuilder.AppendLine();
@@ -61,7 +75,7 @@ namespace Nethermind.GitBook
 
                 PropertyInfo[] properties = type.GetProperties();
 
-                moduleBuilder.AppendLine("| Fields name | Type |");
+                moduleBuilder.AppendLine("| Field name | Type |");
                 moduleBuilder.AppendLine("| :--- | :--- |");
 
                 string propertyType;
