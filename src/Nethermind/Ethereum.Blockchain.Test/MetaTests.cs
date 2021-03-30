df8bde98003e41b6a64a1dc2c19049e86c20d1ba
MarekM25 <marekm2504@gmail.com>
2021-03-30 13:48:46 +0200

    fix MetaTests

--- src/Nethermind/Ethereum.Blockchain.Test/MetaTests.cs
+++ src/Nethermind/Ethereum.Blockchain.Test/MetaTests.cs
@@ -39,7 +39,7 @@ namespace Ethereum.Blockchain.Test
             {
                 string expectedTypeName = ExpectedTypeName(directory);
                 Type type = types.SingleOrDefault(t => string.Equals(t.Name, expectedTypeName, StringComparison.InvariantCultureIgnoreCase));
-                if(type == null && directory != "stEWASMTests" && directory != "VMTests" && directory != "Specs" && directory != "runtimes" && directory != "ref")
+                if(type == null && directory != "stEWASMTests" && directory != "VMTests" && directory != "Specs" && directory != "runtimes" && directory != "ref" && directory !="TestFiles")
                 {
                     if (new DirectoryInfo(directory).GetFiles().Any(f => f.Name.Contains(".resources.")))
                     {
