bf81fa03f68d3af71d0343a1adf3e23d5e40bbef
Hubert Banaszewski <h.banaszewski@student.uw.edu.pl>
2021-04-01 16:20:37 +0200

    Use older ModExpPrecompile implementation in case of older spec

--- src/Nethermind/Nethermind.Evm/Precompiles/ModExpPrecompileOld.cs
+++ src/Nethermind/Nethermind.Evm/Precompiles/ModExpPrecompileOld.cs
@@ -44,11 +44,6 @@ namespace Nethermind.Evm.Precompiles
         
         public long DataGasCost(byte[] inputData, IReleaseSpec releaseSpec)
         {
-            if (releaseSpec.IsEip2565Enabled)
-            {
-                return ModExpPrecompile.Instance.DataGasCost(inputData, releaseSpec);
-            }
-            
             try
             {
                 Span<byte> extendedInput = stackalloc byte[96];
