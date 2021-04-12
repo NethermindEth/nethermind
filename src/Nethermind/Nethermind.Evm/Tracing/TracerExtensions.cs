7c4eecd9d0bbf0dccf27a02f304aee45677f8777
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2021-04-12 17:35:33 +0200

    eth_createAccessList review fixes (#2970)
    * review fixes
    
    * Remove SimpleColumnRocksDb

--- src/Nethermind/Nethermind.Evm/Tracing/TracerExtensions.cs
+++ src/Nethermind/Nethermind.Evm/Tracing/TracerExtensions.cs
@@ -21,9 +21,19 @@ namespace Nethermind.Evm.Tracing
 {
     public static class TracerExtensions
     {
-        public static CancellationTxTracer WithCancellation(this ITxTracer txTracer, CancellationToken cancellationToken) =>
-            new(txTracer, cancellationToken);
-        
+        public static CancellationTxTracer WithCancellation(this ITxTracer txTracer, CancellationToken cancellationToken, bool setDefaultCancellations = true)
+        {
+            return !setDefaultCancellations
+                ? new(txTracer, cancellationToken)
+                : new (txTracer, cancellationToken)
+                {
+                    IsTracingActions = true,
+                    IsTracingOpLevelStorage = true,
+                    IsTracingInstructions = true, // a little bit costly but almost all are simple calls
+                    IsTracingRefunds = true
+                };
+        }
+
         public static CancellationBlockTracer WithCancellation(this IBlockTracer txTracer, CancellationToken cancellationToken) =>
             new(txTracer, cancellationToken);
     }
