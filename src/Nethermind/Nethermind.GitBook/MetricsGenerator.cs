548f08629c76697f2ee275acc95dc2611979cb35
Marcin Sobczak <marcindsobczak@gmail.com>
2021-04-15 14:31:51 +0200

    gitbook fix

--- src/Nethermind/Nethermind.GitBook/MetricsGenerator.cs
+++ src/Nethermind/Nethermind.GitBook/MetricsGenerator.cs
@@ -16,6 +16,7 @@
 
 using System;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -35,46 +36,21 @@ namespace Nethermind.GitBook
         {
             string docsDir = DocsDirFinder.FindDocsDir();
             
-            //ToFix: Create MetricsCategoryAttribute and add MetricsCategory attribute to every "Metrics" class and load Metrics automatically instead of manually
-            Type metricsType = Assembly.Load("Nethermind.Blockchain").GetTypes()
-                                        .First(t => typeof(Blockchain.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "Blockchain", docsDir);
-            
-            metricsType = Assembly.Load("Nethermind.Evm").GetTypes()
-                .First(t => typeof(Evm.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "Evm", docsDir);
-            
-            metricsType = Assembly.Load("Nethermind.JsonRpc").GetTypes()
-                .First(t => typeof(JsonRpc.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "JsonRpc", docsDir);
-            
-            metricsType = Assembly.Load("Nethermind.Network").GetTypes()
-                .First(t => typeof(Network.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "Network", docsDir);
-            
-            metricsType = Assembly.Load("Nethermind.Db").GetTypes()
-                .First(t => typeof(Db.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "Store", docsDir);
-            
-            metricsType = Assembly.Load("Nethermind.Consensus.AuRa").GetTypes()
-                .First(t => typeof(Consensus.AuRa.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "Consensus.Aura", docsDir);
-            
-            metricsType = Assembly.Load("Nethermind.Runner").GetTypes()
-                .First(t => typeof(Runner.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "Runner", docsDir);
-            
-            metricsType = Assembly.Load("Nethermind.Synchronization").GetTypes()
-                .First(t => typeof(Synchronization.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "Synchronization", docsDir);
-            
-            metricsType = Assembly.Load("Nethermind.Trie").GetTypes()
-                .First(t => typeof(Trie.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "Trie", docsDir);
-            
-            metricsType = Assembly.Load("Nethermind.TxPool").GetTypes()
-                .First(t => typeof(TxPool.Metrics).IsAssignableFrom(t));
-            GenerateDocFileContent(metricsType, "TxPool", docsDir);
+            string[] dlls = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "Nethermind.*.dll")
+                .OrderBy(n => n).ToArray();
+
+            foreach (string dll in dlls)
+            {
+                Assembly assembly = Assembly.LoadFile(dll);
+                Type[] modules  = assembly.GetExportedTypes().Where(t => t.Name == "Metrics").ToArray();
+                
+                
+                foreach (Type module in modules)
+                {
+                    string name = module.FullName.Replace("Nethermind.", "").Replace(".Metrics", "");
+                    GenerateDocFileContent(module, name, docsDir);
+                }
+            }
         }
 
         private void GenerateDocFileContent(Type metricsType, string moduleName, string docsDir)
@@ -94,9 +70,29 @@ namespace Nethermind.GitBook
             foreach (PropertyInfo property in moduleProperties)
             {
                 Attribute attr = property.GetCustomAttribute(typeof(DescriptionAttribute));
-                docBuilder.AppendLine($"| {property.Name} | {((DescriptionAttribute)attr)?.Description ?? ""} |");
+                docBuilder.AppendLine($"| {GetMetricName(property.Name)} | {((DescriptionAttribute)attr)?.Description ?? ""} |");
             }
             _sharedContent.Save(moduleName, docsDir + "/ethereum-client/metrics", docBuilder);
+        }
+
+        private string GetMetricName(string propertyName)
+        {
+            StringBuilder nameBuilder = new StringBuilder("nethermind");
+            
+            foreach (char ch in propertyName)
+            {
+                if (char.IsUpper(ch))
+                {
+                    nameBuilder.Append("_");
+                    nameBuilder.Append(ch.ToString().ToLower());
+                }
+                else
+                {
+                    nameBuilder.Append(ch);
+                }
+            }
+
+            return nameBuilder.ToString();
         }
     }
 }
