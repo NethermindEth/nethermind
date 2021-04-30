52512b4ac536397c38f15f3a534a8adb1ba32aec
SebastianDremo <sebastiandremo@gmail.com>
2021-04-30 17:17:00 +0200

    move interpreter logic to Interpreter class

--- src/Nethermind/Nethermind.Dsl/ANTLR/Interpreter.cs
+++ src/Nethermind/Nethermind.Dsl/ANTLR/Interpreter.cs
@@ -1,18 +1,28 @@
 using System;
+using System.Collections.Generic;
 using Antlr4.Runtime.Tree;
+using Nethermind.Api;
+using Nethermind.Core;
+using Nethermind.Dsl.Pipeline;
+using Nethermind.Int256;
 using Nethermind.Pipeline;
+using Nethermind.Pipeline.Publishers;
 
 namespace Nethermind.Dsl.ANTLR
 {
-    public class Interpreter : IInterpreter
+    public class Interpreter
     {
+        public Stack<IPipeline> Pipelines = new Stack<IPipeline>();
+        private readonly INethermindApi _api;
         private readonly IParseTree _tree;
         private readonly ParseTreeListener _treeListener;
-        private IPipeline _pipeline;
-        private IPipelineBuilder<object, object> _pipelineBuilder;
+        private IPipelineBuilder<Block, Block> _blockPipelineBuilder;
+        private IPipelineBuilder<Transaction, Transaction> _transactionPipelineBuilder;
+        private bool _blockSource;
 
-        public Interpreter(IParseTree tree, ParseTreeListener treeListener)
+        public Interpreter(INethermindApi api, IParseTree tree, ParseTreeListener treeListener)
         {
+            _api = api ?? throw new ArgumentNullException(nameof(api));
             _tree = tree ?? throw new ArgumentNullException(nameof(tree));
             _treeListener = treeListener ?? throw new ArgumentNullException(nameof(treeListener));
 
@@ -21,37 +31,139 @@ namespace Nethermind.Dsl.ANTLR
             _treeListener.OnExit = BuildPipeline;
         }
 
-        public void AddSource(string value)
-        {
-            
-        }
-
-        public void AddWatch(string value)
-        {
-
-        }
-        public void AddExpression(AntlrTokenType tokenType, string value)
+        private void AddExpression(AntlrTokenType tokenType, string value)
         {
             switch (tokenType)
             {
                 case AntlrTokenType.SOURCE: 
                     AddSource(value);
                 break;
+                case AntlrTokenType.WATCH:
+                    AddWatch(value);
+                break;
+                case AntlrTokenType.PUBLISH:
+                    AddPublisher(value);
+                break;
             }
         }
-        public void AddCondition(string key, string symbol, string value)
+
+        private void AddSource(string value)
         {
+            if(value.Equals("BlockProcessor", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var sourceElement = new BlockProcessorSource<Block>(_api.MainBlockProcessor);
+                _blockPipelineBuilder = new PipelineBuilder<Block, Block>(sourceElement);
+
+                _blockSource = true;
+
+                return;
+            }
+            else if(value.Equals("TxPool", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var sourceElement = new TxPoolSource<Transaction>(_api.TxPool);
+                _transactionPipelineBuilder = new PipelineBuilder<Transaction, Transaction>(sourceElement);
+
+                _blockSource = false;
+
+                return;
+            }
+        }
+
+        private void AddWatch(string value)
+        {
+            switch (value.ToLowerInvariant())
+            {
+                case "blocks":
+                    _blockPipelineBuilder = _blockPipelineBuilder.AddElement(
+                        new PipelineElement<Block, Block>(
+                            condition: (block => true),
+                            transformData: (b => b)
+                            )
+                    );
+                    break;
+            }
+        }
+
+        private void AddCondition(string key, string symbol, string value)
+        {
+            if (_blockSource)
+            {
+                switch (symbol)
+                {
+                    case "==":
+                        _blockPipelineBuilder =_blockPipelineBuilder.AddElement(
+                            new PipelineElement<Block, Block>(
+                                condition: (b => b.GetType().GetProperty(key).GetValue(b).ToString() == value),
+                                transformData: (b => b)
+                            )
+                        );
+                        return;
+                    case "!=":
+                        _blockPipelineBuilder =_blockPipelineBuilder.AddElement(
+                            new PipelineElement<Block, Block>(
+                                condition: (b => b.GetType().GetProperty(key).GetValue(b).ToString() != value),
+                                transformData: (b => b)
+                            )
+                        );
+                        return;
+                    case ">":
+                        _blockPipelineBuilder =_blockPipelineBuilder.AddElement(
+                            new PipelineElement<Block, Block>(
+                                condition: (b => (UInt256)b.GetType().GetProperty(key).GetValue(b) > UInt256.Parse(value)),
+                                transformData: (b => b)
+                            )
+                        );
+                        return;
+                    case "<":
+                        _blockPipelineBuilder =_blockPipelineBuilder.AddElement(
+                            new PipelineElement<Block, Block>(
+                                condition: (b => (UInt256)b.GetType().GetProperty(key).GetValue(b) < UInt256.Parse(value)),
+                                transformData: (b => b)
+                            )
+                        );
+                        return;
+                    case ">=":
+                        _blockPipelineBuilder =_blockPipelineBuilder.AddElement(
+                            new PipelineElement<Block, Block>(
+                                condition: (b => (UInt256)b.GetType().GetProperty(key).GetValue(b) >= UInt256.Parse(value)),
+                                transformData: (b => b)
+                            )
+                        );
+                        return;
+                    case "<=":
+                        _blockPipelineBuilder =_blockPipelineBuilder.AddElement(
+                            new PipelineElement<Block, Block>(
+                                condition: (b => (UInt256)b.GetType().GetProperty(key).GetValue(b) <= UInt256.Parse(value)),
+                                transformData: (b => b)
+                            )
+                        );
+                        return;
+                }
+            }
 
         }
 
-        public void AddPublisher(string value)
+        private void AddPublisher(string publisher)
         {
-
+            if (publisher.Equals("WebSockets", StringComparison.InvariantCultureIgnoreCase))
+            {
+                if (_blockPipelineBuilder != null)
+                {
+                    _blockPipelineBuilder =_blockPipelineBuilder.AddElement(new WebSocketsPublisher<Block, Block>("dsl", _api.EthereumJsonSerializer));
+                }
+            }
         }
 
-        public void BuildPipeline()
+        private void BuildPipeline()
         {
-
+            if(_blockSource)
+            {
+                Pipelines.Push(_blockPipelineBuilder.Build());
+            }
+            else
+            {
+                Pipelines.Push(_transactionPipelineBuilder.Build());
+            }
         }
     }
 }
--- src/Nethermind/Nethermind.Dsl/DslPlugin.cs
+++ src/Nethermind/Nethermind.Dsl/DslPlugin.cs
@@ -28,8 +28,9 @@ namespace Nethermind.Dsl
         public IPipeline? Pipeline;
         public IFileSystem? FileSystem;
 
-        private ParseTreeListener? _listener;
         private INethermindApi? _api;
+        private ParseTreeListener? _listener;
+        private Interpreter? _interpreter;
         private ITxPool? _txPool;
         private IBlockProcessor? _blockProcessor;
         private IPipelineBuilder<Block, Block>? _blockProcessorPipelineBuilder;
@@ -39,8 +40,6 @@ namespace Nethermind.Dsl
         public async Task Init(INethermindApi nethermindApi)
         {
             _api = nethermindApi;
-            _txPool = _api.TxPool;
-            _blockProcessor = _api.MainBlockProcessor;
 
             _logger = _api.LogManager.GetClassLogger();
             if (_logger.IsInfo) _logger.Info("Initializing DSL plugin ...");
@@ -55,10 +54,7 @@ namespace Nethermind.Dsl
             IParseTree tree = parser.init();
 
             _listener = new ParseTreeListener();
-            _listener.OnEnterInit = OnInitEntry;
-            _listener.OnEnterExpression = OnExpressionEntry;
-            _listener.OnEnterCondition = OnConditionEntry;
-            _listener.OnExitInit = BuildPipeline;
+            _interpreter = new Interpreter(_api, tree, _listener);
             ParseTreeWalker.Default.Walk(_listener, tree);
 
             if (_logger.IsInfo) _logger.Info("DSL plugin initialized.");
@@ -77,146 +73,6 @@ namespace Nethermind.Dsl
         }
 
         public ValueTask DisposeAsync() => ValueTask.CompletedTask;
-
-        private void OnInitEntry(AntlrTokenType tokenType, string tokenValue)
-        {
-            if (tokenType == AntlrTokenType.SOURCE)
-            {
-                if (tokenValue.Equals("BlockProcessor", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    var sourceElement = new BlockProcessorSource<Block>(_blockProcessor);
-                    _blockProcessorPipelineBuilder = new PipelineBuilder<Block, Block>(sourceElement);
-
-                    return;
-                }
-
-                throw new ArgumentException($"Given token {tokenType} value {tokenValue} is not supported.");
-            }
-        }
-
-        private void OnExpressionEntry(AntlrTokenType tokenType, string tokenValue)
-        {
-            switch (tokenType)
-            {
-                case AntlrTokenType.SOURCE:
-                    break;
-                case AntlrTokenType.WHERE:
-                    break;
-                case AntlrTokenType.WATCH:
-                    SetWatchOnPipeline(tokenValue);
-                    break;
-                case AntlrTokenType.PUBLISH:
-                    AddPublisher(tokenValue);
-                    break;
-                default: throw new ArgumentException($"Given token is not supported {tokenType}");
-            }
-        }
-
-        private void OnConditionEntry(string key, string symbol, string value)
-        {
-            if (blockSource)
-            {
-                switch (symbol)
-                {
-                    case "==":
-                        _blockProcessorPipelineBuilder =_blockProcessorPipelineBuilder.AddElement(
-                            new PipelineElement<Block, Block>(
-                                condition: (b => b.GetType().GetProperty(key).GetValue(b).ToString() == value),
-                                transformData: (b => b)
-                            )
-                        );
-                        return;
-                    case "!=":
-                        _blockProcessorPipelineBuilder =_blockProcessorPipelineBuilder.AddElement(
-                            new PipelineElement<Block, Block>(
-                                condition: (b => b.GetType().GetProperty(key).GetValue(b).ToString() != value),
-                                transformData: (b => b)
-                            )
-                        );
-                        return;
-                    case ">":
-                        _blockProcessorPipelineBuilder =_blockProcessorPipelineBuilder.AddElement(
-                            new PipelineElement<Block, Block>(
-                                condition: (b => (UInt256)b.GetType().GetProperty(key).GetValue(b) > UInt256.Parse(value)),
-                                transformData: (b => b)
-                            )
-                        );
-                        return;
-                    case "<":
-                        _blockProcessorPipelineBuilder =_blockProcessorPipelineBuilder.AddElement(
-                            new PipelineElement<Block, Block>(
-                                condition: (b => (UInt256)b.GetType().GetProperty(key).GetValue(b) < UInt256.Parse(value)),
-                                transformData: (b => b)
-                            )
-                        );
-                        return;
-                    case ">=":
-                        _blockProcessorPipelineBuilder =_blockProcessorPipelineBuilder.AddElement(
-                            new PipelineElement<Block, Block>(
-                                condition: (b => (UInt256)b.GetType().GetProperty(key).GetValue(b) >= UInt256.Parse(value)),
-                                transformData: (b => b)
-                            )
-                        );
-                        return;
-                    case "<=":
-                        _blockProcessorPipelineBuilder =_blockProcessorPipelineBuilder.AddElement(
-                            new PipelineElement<Block, Block>(
-                                condition: (b => (UInt256)b.GetType().GetProperty(key).GetValue(b) <= UInt256.Parse(value)),
-                                transformData: (b => b)
-                            )
-                        );
-                        return;
-                }
-            }
-        }
-
-        private void SetWatchOnPipeline(string value)
-        {
-            value = value.ToLowerInvariant();
-            switch (value)
-            {
-                case "blocks":
-                    _blockProcessorPipelineBuilder = _blockProcessorPipelineBuilder.AddElement(
-                        new PipelineElement<Block, Block>(
-                            condition: (block => true),
-                            transformData: (b => b)
-                            )
-                    );
-                    blockSource = true;
-                    break;
-                case "transactions":
-                    _blockProcessorPipelineBuilder.AddElement(new PipelineElement<Block, Transaction[]>(
-                        (b => true),
-                        (block => block.Transactions)
-                    ));
-                    blockSource = false;
-                    break;
-            }
-        }
-
-        private void AddPublisher(string publisherType)
-        {
-            if (publisherType.Equals("WebSockets", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (_blockProcessorPipelineBuilder != null)
-                {
-                    _blockProcessorPipelineBuilder =_blockProcessorPipelineBuilder.AddElement(new WebSocketsPublisher<Block, Block>("dsl", _api.EthereumJsonSerializer));
-                }
-            }
-
-            if (publisherType.Equals("LogPublisher", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (_blockProcessorPipelineBuilder != null)
-                {
-                   _blockProcessorPipelineBuilder = _blockProcessorPipelineBuilder.AddElement(new LogPublisher<Block, Block>(_api.EthereumJsonSerializer, _api.LogManager));
-                }
-            }
-        }
-
-        private void BuildPipeline()
-        {
-            Pipeline = _blockProcessorPipelineBuilder.Build();
-        }
 
         private async Task<string> LoadDSLScript()
         {
