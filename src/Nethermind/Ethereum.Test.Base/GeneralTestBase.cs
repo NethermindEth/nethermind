2c07d03c043d2458cf2d3f93b27d7739d9bd8bd9
MarekM25 <marekm2504@gmail.com>
2021-06-04 13:20:06 +0200

    Add consensus tests

--- src/Nethermind/Ethereum.Test.Base/GeneralTestBase.cs
+++ src/Nethermind/Ethereum.Test.Base/GeneralTestBase.cs
@@ -97,7 +97,7 @@ namespace Ethereum.Test.Base
 
             BlockHeader header = new(test.PreviousHash, Keccak.OfAnEmptySequenceRlp, test.CurrentCoinbase,
                 test.CurrentDifficulty, test.CurrentNumber, test.CurrentGasLimit, test.CurrentTimestamp, new byte[0]);
-            header.BaseFeePerGas = test.Fork.IsEip1559Enabled ? test.CurrentBaseFee : UInt256.Zero;
+            header.BaseFeePerGas = test.Fork.IsEip1559Enabled ? test.CurrentBaseFee: UInt256.Zero;
             header.StateRoot = test.PostHash;
             header.Hash = header.CalculateHash();
 
---
+++ src/Nethermind/Nethermind.Consensus.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nethermind.Consensus.Test
+{
+    public class TargetAdjustedGasLimitCalculatorTests
+    {
+    }
+}
