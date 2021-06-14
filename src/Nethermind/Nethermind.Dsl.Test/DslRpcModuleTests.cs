6b77618f063aa876bf978167fc87ef28fafa94d9
Guilherme Peyrelongue Heise <gheise@ufl.edu>
2021-06-14 10:26:25 -0400

    Minor fix

--- src/Nethermind/Nethermind.Dsl.Test/DslRpcModuleTests.cs
+++ src/Nethermind/Nethermind.Dsl.Test/DslRpcModuleTests.cs
@@ -28,22 +28,25 @@ namespace Nethermind.Dsl.Test
             _dslPlugin = new DslPlugin();
             _api = Substitute.For<INethermindApi>();
             _dslRpcModule = Substiture.For<DslRpcModule>();
-            _interpreter = Substiture.For<Dictionary<int, Interpreter>>();
+            _interpreters= Substiture.For<Dictionary<int, Interpreter>>();
             _logger = Substiture.For<ILogger>();
         }
         
         [Test]
+        public async Task will_load_interpreters_correctly
+        {
+            _dslRpcModule.AddInterpreter(_interpreters)
+        }
+        [Test]
         public async Task will_throw_error_for_undefined_watch(String script)
         {
            _dslRpcModule.dsl_addScript("WATCH House WHERE Author IS 0x22eA9f6b28DB76A7162054c05ed812dEb2f519Cd PUBLISH WebSockets ytext");
-           await _dslPlugin.Init(_api);
         }
 
         [Test]
         public async Task will_throw_error_for_improper_use_of_is(String script)
         {
             _dslRpcModule.dsl_addScript("WATCH Blocks WHERE Author IS AUTHOR PUBLISH WebSockets ytext");
-            await _dslPlugin.Init(_api);
         }
 
 
@@ -51,7 +54,6 @@ namespace Nethermind.Dsl.Test
         public async Task will_return_nothing_if_is_is_always_false(String script)
         {
             _dslRpcModule.dsl_addScript("WATCH WHERE 23 IS NOT 23 PUBLISH WebSockets ytext");
-            await _dslPlugin.Init(_api);
         }
 
 
@@ -59,14 +61,12 @@ namespace Nethermind.Dsl.Test
         public async Task will_fail_with_out_of_order_expressions(String script)
         {
             _dslRpcModule.dsl_addScript("WHERE Author IS 0x22eA9f6b28DB76A7162054c05ed812dEb2f519Cd WATCH Blocks PUBLISH WebSockets ytext");
-            await _dslPlugin.Init(_api);
         }
 
         [Test]
         public async Task will_fail_on_undefined_operation(String script)
         {
             _dslRpcModule.dsl_addScript("WATCH Blocks WHERE AUTHOR IS 0x22eA9f6b28DB76A7162054c05ed812dEb2f519Cd STOP PUBLISH WebSockets ytext");
-            await _dslPlugin.Init(_api);
         }
     }
 }
