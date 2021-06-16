a23832721d92176effdfc850c6186e34bec81e28
Marcin Sobczak <marcindsobczak@gmail.com>
2021-06-16 14:39:52 +0200

    cosmetic

--- src/Nethermind/Nethermind.Provide.Vault/VaultTxSender.cs
+++ src/Nethermind/Nethermind.Provide.Vault/VaultTxSender.cs
@@ -126,7 +126,7 @@ namespace Nethermind.Vault
             // this should happen after we set the GasPrice
             _txSigner.Seal(tx, TxHandlingOptions.None);
             ProvideTx createdTx = await _provide.CreateTransaction(provideTx);
-            return ((createdTx?.Hash == null ? Keccak.Zero : new Keccak(createdTx.Hash)), null);
+            return (createdTx?.Hash == null ? Keccak.Zero : new Keccak(createdTx.Hash), null);
         }
 
         public async Task<HttpResponseMessage> SendPostNChainRequest(string methodName, CreateAccountRequest request)
