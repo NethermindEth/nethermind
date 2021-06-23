5337fb1dac07430addea97c58dc0556362e87b45
MarekM25 <marekm2504@gmail.com>
2021-06-23 10:20:52 +0200

    cosmetic

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/Eth/EthRpcModuleTests.EthCall.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/Eth/EthRpcModuleTests.EthCall.cs
@@ -193,18 +193,17 @@ namespace Nethermind.JsonRpc.Test.Modules.Eth
         public async Task Eth_call_with_base_fee_opcode()
         {
             using Context ctx = await Context.CreateWithLondonEnabled();
-            
+
             byte[] code = Prepare.EvmCode
                 .Op(Instruction.BASEFEE)
                 .PushData(0)
                 .Op(Instruction.SSTORE)
                 .Done;
+            
             string dataStr = code.ToHexString();
-            TransactionForRpc transaction = ctx._test.JsonSerializer.Deserialize<TransactionForRpc>($"{{\"from\": \"0x32e4e4c7c5d1cea5db5f9202a9e4d99e56c91a24\", \"to\": \"0x32e4e4c7c5d1cea5db5f9202a9e4d99e56c91a24\", \"type\": \"0x2\", \"data\": \"{dataStr}\"}}");
+            TransactionForRpc transaction = ctx._test.JsonSerializer.Deserialize<TransactionForRpc>($"{{\"from\": \"0x32e4e4c7c5d1cea5db5f9202a9e4d99e56c91a24\", \"type\": \"0x2\", \"data\": \"{dataStr}\"}}");
             string serialized = ctx._test.TestEthRpc("eth_call", ctx._test.JsonSerializer.Serialize(transaction));
             Assert.AreEqual("{\"jsonrpc\":\"2.0\",\"result\":\"0x\",\"id\":67}", serialized);
         }
-        
-        
     }
 }
