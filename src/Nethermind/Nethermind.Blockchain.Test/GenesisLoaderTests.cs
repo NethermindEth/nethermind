500bae7e7169506d7d8071c8a7ba52cb776b4c2f
MarekM25 <marekm2504@gmail.com>
2021-06-30 15:49:51 +0200

    Add one more test

--- src/Nethermind/Nethermind.Blockchain.Test/GenesisLoaderTests.cs
+++ src/Nethermind/Nethermind.Blockchain.Test/GenesisLoaderTests.cs
@@ -35,10 +35,21 @@ namespace Nethermind.Blockchain.Test
     [TestFixture]
     public class GenesisLoaderTests
     {
-        [Test]
-        public void Can_load_genesis_with_emtpy_accounts()
+        [TestCase]
+        public void Can_load_genesis_with_emtpy_accounts_and_storage()
         {
-            string path = Path.Combine(TestContext.CurrentContext.WorkDirectory, "../../../../", "Chains/test.json");
+            AssertBlockHash("0x61b2253366eab37849d21ac066b96c9de133b8c58a9a38652deae1dd7ec22e7b", "../../../../Chains/test.json");
+        }
+        
+        [Test]
+        public void Can_load_genesis_with_emtpy_accounts_and_code()
+        {
+            AssertBlockHash("0x62839401df8970ec70785f62e9e9d559b256a9a10b343baf6c064747b094de09", "Specs/test.json");
+        }
+
+        private void AssertBlockHash(string expectedHash, string chainspecFilePath)
+        {
+            string path = Path.Combine(TestContext.CurrentContext.WorkDirectory, chainspecFilePath);
             ChainSpec chainSpec = LoadChainSpec(path);
             IDb stateDb = new MemDb();
             IDb codeDb = new MemDb();
@@ -51,8 +62,9 @@ namespace Nethermind.Blockchain.Test
             GenesisLoader genesisLoader = new(chainSpec, specProvider, stateProvider, storageProvider,
                 transactionProcessor);
             Block block = genesisLoader.Load();
-            Assert.AreEqual("0x61b2253366eab37849d21ac066b96c9de133b8c58a9a38652deae1dd7ec22e7b", block.Hash!.ToString());
+            Assert.AreEqual(expectedHash, block.Hash!.ToString());
         }
+
         
         private static ChainSpec LoadChainSpec(string path)
         {
