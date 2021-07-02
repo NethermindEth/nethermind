91a50bdacda9b92af22f2a4350036c3da2487c43
Kaushik Donthi <kaushikdr@ucla.edu>
2021-07-01 18:54:32 -0700

    Removed unneeded variable from GasPriceConfig

--- src/Nethermind/Nethermind.JsonRpc/Modules/Eth/GasPriceConfig.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Eth/GasPriceConfig.cs
@@ -10,7 +10,6 @@ namespace Nethermind.JsonRpc.Modules.Eth
         public const int DefaultGasPrice = 1;
         public const int SoftTxLimit = 40;
         public const int DefaultBaseFee = 200;
-        public const int BlockLimitForDefaultGasPrice = 8;
         public const int TxLimitFromABlock = 3;
         public static readonly UInt256 _maxGasPrice = 500;
     }
