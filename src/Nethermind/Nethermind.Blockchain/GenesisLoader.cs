057430d1ad4bfddd45327511edcb1a8442151f14
MarekM25 <marekm2504@gmail.com>
2021-07-06 13:46:04 +0200

    Revert hive cfg

--- src/Nethermind/Nethermind.Blockchain/GenesisLoader.cs
+++ src/Nethermind/Nethermind.Blockchain/GenesisLoader.cs
@@ -62,7 +62,7 @@ namespace Nethermind.Blockchain
             _chainSpec.Allocations = null;
 
             _storageProvider.Commit();
-            _stateProvider.Commit(_specProvider.GenesisSpec);
+            _stateProvider.Commit(_specProvider.GenesisSpec, true);
 
             _storageProvider.CommitTrees(0);
             _stateProvider.CommitTree(0);
@@ -82,7 +82,7 @@ namespace Nethermind.Blockchain
                 if (allocation.Code != null)
                 {
                     Keccak codeHash = _stateProvider.UpdateCode(allocation.Code);
-                    _stateProvider.UpdateCodeHash(address, codeHash, _specProvider.GenesisSpec);
+                    _stateProvider.UpdateCodeHash(address, codeHash, _specProvider.GenesisSpec, true);
                 }
 
                 if (allocation.Storage != null)
