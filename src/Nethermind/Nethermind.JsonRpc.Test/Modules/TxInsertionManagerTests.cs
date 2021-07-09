6a79e5d93ad6a54eaa178b4f68a31832269426f9
Kaushik Donthi <kaushikdr@ucla.edu>
2021-07-08 21:28:44 -0700

    Set up TxInsertionManagerTests

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/TxInsertionManagerTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/TxInsertionManagerTests.cs
@@ -5,6 +5,40 @@ namespace Nethermind.JsonRpc.Test.Modules
     [TestFixture]
     class TxInsertionManagerTests
     {
+        [Test]
+        public void AddValidTxAndReturnCount_IfThresholdExists_IgnorePricesUnderIt()
+        {
+            
+        }
+
+        [Test]
+        public void AddValidTxAndReturnCount_IfBlockHasMoreThanThreeTx_AddOnlyThree()
+        {
+            
+        }
+
+        [Test]
+        public void AddValidTxAndReturnCount_IfBlockHasTxs_OnlyAddTxsWithLowestEffectiveGasPrice()
+        {
+            
+        }
         
+        [Test]
+        public void AddValidTxAndReturnCount_TxsWithPriceLessThanIgnoreUnder_ShouldNotHaveGasPriceInTxGasPriceList()
+        {
+            
+        }
+        
+        [Test]
+        public void AddValidTxAndReturnCount_TxsSentByMiner_ShouldNotHaveGasPriceInTxGasPriceList()
+        {
+            
+        }
+
+        [Test]
+        public void AddValidTxAndReturnCount_WhenEip1559Enabled_EffectiveGasPriceOfTxsCalculated()
+        {
+            
+        }
     }
 }
