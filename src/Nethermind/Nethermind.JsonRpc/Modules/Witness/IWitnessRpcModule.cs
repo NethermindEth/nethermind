4c775396efff3aa55b9284e4ab393a2950965f7e
Marcos Antonio Maceo Reyes <marcos@nethermind.io>
2021-07-21 13:28:58 -0400

    Remove unused imports

--- src/Nethermind/Nethermind.JsonRpc/Modules/Witness/IWitnessRpcModule.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Witness/IWitnessRpcModule.cs
@@ -15,9 +15,7 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 // 
 
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using Nethermind.Core.Crypto;
 
 namespace Nethermind.JsonRpc.Modules.Witness
 {
