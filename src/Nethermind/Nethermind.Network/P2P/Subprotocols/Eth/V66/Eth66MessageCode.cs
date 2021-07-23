0111ee7f6b7c486a0a14173c5ecaa33eed00ec3c
Marcin Sobczak <marcindsobczak@gmail.com>
2021-07-23 19:20:05 +0200

    change Eth66MessageCodes from numbers to reused codes

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V66/Eth66MessageCode.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V66/Eth66MessageCode.cs
@@ -15,19 +15,23 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 // 
 
+using Nethermind.Network.P2P.Subprotocols.Eth.V62;
+using Nethermind.Network.P2P.Subprotocols.Eth.V63;
+using Nethermind.Network.P2P.Subprotocols.Eth.V65;
+
 namespace Nethermind.Network.P2P.Subprotocols.Eth.V66
 {
     public static class Eth66MessageCode
     {
-        public const int GetBlockHeaders = 0x03;
-        public const int BlockHeaders = 0x04;
-        public const int GetBlockBodies = 0x05;
-        public const int BlockBodies = 0x06;
-        public const int GetPooledTransactions = 0x09;
-        public const int PooledTransactions  = 0x0a;
-        public const int GetNodeData = 0x0d;
-        public const int NodeData = 0x0e;
-        public const int GetReceipts = 0x0f;
-        public const int Receipts = 0x10;
+        public const int GetBlockHeaders = Eth62MessageCode.GetBlockHeaders;
+        public const int BlockHeaders = Eth62MessageCode.BlockHeaders;
+        public const int GetBlockBodies = Eth62MessageCode.GetBlockBodies;
+        public const int BlockBodies = Eth62MessageCode.BlockBodies;
+        public const int GetPooledTransactions = Eth65MessageCode.GetPooledTransactions;
+        public const int PooledTransactions  = Eth65MessageCode.PooledTransactions;
+        public const int GetNodeData = Eth63MessageCode.GetNodeData;
+        public const int NodeData = Eth63MessageCode.NodeData;
+        public const int GetReceipts = Eth63MessageCode.GetReceipts;
+        public const int Receipts = Eth63MessageCode.Receipts;
     }
 }
