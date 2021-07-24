64a38048ae14105a09b496471405e430834d44fb
Marcos Antonio Maceo Reyes <marcos@nethermind.io>
2021-07-24 11:30:57 -0400

    Add more tests for WitnessModuleRpc

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/WitnessModuleTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/WitnessModuleTests.cs
@@ -15,12 +15,13 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 // 
 
-using System.Threading.Tasks;
-using FluentAssertions;
+gusing FluentAssertions;
 using Nethermind.Blockchain;
 using Nethermind.Blockchain.Find;
 using Nethermind.Core;
+using Nethermind.Core.Crypto;
 using Nethermind.Core.Test.Builders;
+using Nethermind.Crypto;
 using Nethermind.Db;
 using Nethermind.JsonRpc.Modules.Witness;
 using Nethermind.Logging;
@@ -32,8 +33,12 @@ namespace Nethermind.JsonRpc.Test.Modules
 {
     public class WitnessModuleTests
     {
-        private const string OneNodeResponse =
+        private const string GetOneWitnessHashResponse =
             "{\"jsonrpc\":\"2.0\",\"result\":[\"0xa2a9f03b9493046696099d27b2612b99497aa1f392ec966716ab393c715a5bb6\"],\"id\":67}";
+        private const string BlockNotFoundResponse = 
+            "{\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32603,\"message\":\"Block not found\"},\"id\":67}";
+        private const string WitnessNotFoundResponse = 
+            "{\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32603,\"message\":\"Witness not found\"},\"id\":67}";
 
         private IBlockFinder _blockFinder;
 
@@ -41,29 +46,63 @@ namespace Nethermind.JsonRpc.Test.Modules
         private WitnessRpcModule _witnessRpcModule;
 
         private Block _block;
+        
+        private Block _block2;
+        private Keccak _hash;
 
 
         [SetUp]
-        public async Task Setup()
+        public void Setup()
         {
+            _block2 = Build.An.Block.TestObject;
             _block =  Build.A.Block
                 .WithOmmers(Build.A.BlockHeader.TestObject, Build.A.BlockHeader.TestObject).TestObject;
-            _blockFinder = Substitute.For<IBlockTree>();
-            _blockFinder.FindBlock((BlockParameter)null).ReturnsForAnyArgs(_block);
-
-            _witnessRepository = new WitnessCollector(new MemDb(), LimboLogs.Instance);
             
-            _witnessRepository.Add(_block.Hash);
-            _witnessRepository.Persist(_block.Hash);
-            _witnessRpcModule = new WitnessRpcModule(_witnessRepository, _blockFinder);
+            _hash = _block.CalculateHash();
         }
 
         [Test]
         public void GetOneWitnessHash()
         {
+            _blockFinder = Substitute.For<IBlockTree>();
+            _blockFinder.FindHeader((BlockParameter)null).ReturnsForAnyArgs(_block.Header);
+
+            _witnessRepository = new WitnessCollector(new MemDb(), LimboLogs.Instance);
+            _witnessRepository.Add(_hash);
+            _witnessRepository.Persist(_hash);
+            _witnessRpcModule = new WitnessRpcModule(_witnessRepository, _blockFinder);
+
             string serialized =
-                RpcTest.TestSerializedRequest<IWitnessRpcModule>(_witnessRpcModule, "get_witnesses", _block.Hash.ToString());
-            serialized.Should().Be(OneNodeResponse);
+                RpcTest.TestSerializedRequest<IWitnessRpcModule>(_witnessRpcModule, "get_witnesses", _block.CalculateHash().ToString());
+            serialized.Should().Be(GetOneWitnessHashResponse);
+        }
+
+        [Test]
+        public void BlockNotFound()
+        {
+            _blockFinder = Substitute.For<IBlockTree>();
+            _witnessRepository = new WitnessCollector(new MemDb(), LimboLogs.Instance);
+            _witnessRpcModule = new WitnessRpcModule(_witnessRepository, _blockFinder);
+
+            string serialized =
+                RpcTest.TestSerializedRequest<IWitnessRpcModule>(_witnessRpcModule, "get_witnesses", "0x583");
+            serialized.Should().Be(BlockNotFoundResponse);
+        }
+
+        [Test]
+        public void WitnessNotFound()
+        {
+            _blockFinder = Substitute.For<IBlockTree>();
+            _blockFinder.FindHeader((BlockParameter)null).ReturnsForAnyArgs(_block2.Header);
+
+            _witnessRepository = new WitnessCollector(new MemDb(), LimboLogs.Instance);
+            _witnessRpcModule = new WitnessRpcModule(_witnessRepository, _blockFinder);
+
+
+            string serialized =
+                RpcTest.TestSerializedRequest<IWitnessRpcModule>(_witnessRpcModule, "get_witnesses", "0x1");
+            serialized.Should().Be(WitnessNotFoundResponse);
+            
         }
     }
 }
