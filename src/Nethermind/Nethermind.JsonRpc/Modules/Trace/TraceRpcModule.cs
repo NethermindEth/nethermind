2a4cec4ddf3c92f1300a59df2ddaa89c9b02780b
MarekM25 <marekm2504@gmail.com>
2021-07-29 13:23:46 +0200

    add TraceFilter

--- src/Nethermind/Nethermind.JsonRpc/Modules/Trace/TraceRpcModule.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Trace/TraceRpcModule.cs
@@ -18,8 +18,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
-using Nethermind.Blockchain;
 using Nethermind.Blockchain.Find;
 using Nethermind.Blockchain.Receipts;
 using Nethermind.Blockchain.Tracing;
