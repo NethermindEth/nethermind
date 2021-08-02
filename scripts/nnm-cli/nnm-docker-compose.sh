18454d61c1d1d01507e925fb9503716151566977
Mateusz Jędrzejewski <mateusz.jedrzejewski@yahoo.pl>
2021-08-02 17:49:12 +0200

    a single script for node management cli

---
+++ scripts/nnm-cli/nnm-docker-compose.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+set -e
+export WORKDIR=$HOME
+RPC_PORT=8545
+mkdir -p $WORKDIR/.nnm/logs
+
+cli_help() {
+  cli_name=${0##*/}
+  echo "
+Nethermind Node Management CLI for docker-compose
+   _  __ _  __ __  ___  _____ __    ____
+  / |/ // |/ //  |/  / / ___// /   /  _/
+ /    //    // /|_/ / / /__ / /__ _/ /  
+/_/|_//_/|_//_/  /_/  \___//____//___/  
+
+Version: 0.0.1
+Usage: $cli_name [command]
+
+Commands:
+  pullandbuild|pb    Pulls latest changes from Nethermind repository and builds the app
+  restart|r          Restarts docker-compose
+  shutdown|sd        Shutdowns the Nethermind docker-compose
+  status|st          Displays the current status of Nethermind docker-compose
+  tail|t             Tails the latest Nethermind live logs
+  up|u               Starts the Nethermind docker-compose stack
+  *                  Help
+"
+  exit 1
+}
+
+cli_log() {
+  script_name=${0##*/}
+  function_name=${1}
+  timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+  echo "==> $script_name $timestamp $function_name"
+}
+
+save_log() {
+  timestamp=$(date -u +"%Y-%m-%d")
+  tee -ia $WORKDIR/.nnm/logs/$timestamp.log
+}
+
+# start of pullandbuild
+pullandbuild() {
+  cli_log "Pulling and restarting docker-compose stack..."
+  cd $HOME
+  docker-compose pull
+  docker-compose down
+  docker-compose up -d
+}
+# End of pullandbuild
+
+# Start of restart
+restart() {
+  cli_log "Restarting docker-compose stack..."
+  docker-compose restart
+}
+# End of restart
+
+# Start of shutdown
+shutdown() {
+  cli_log "Shutting down docker-compose stack..."
+  docker-compose down
+}
+# End of shutdown
+
+# Start of status
+status() {
+  cli_log "Checking status of docker-compose stack..."
+  docker-compose ps
+}
+# End of status
+
+# Start of up
+up() {
+  cli_log "Starting docker-compose stack..."
+  docker-compose up -d
+}
+# End of up
+
+# Start of tail
+tail() {
+  cli_log "Getting live logs from docker-compose stack..."
+  docker-compose logs -f
+}
+# End of tail
+
+# Start of version
+version() {
+  cli_log "Checking the current version of Nethermind..."
+  curl --silent --data '{"method":"web3_clientVersion","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:$RPC_PORT | jq ".result" | tr -d '"'
+}
+# End of version
+
+case "$1" in
+  pullndbuild|pb)
+    pullandbuild | save_log $1
+    ;;
+  up|u)
+    up | save_log $1
+    ;;
+  restart|r)
+    restart | save_log $1
+    ;;
+  shutdown|sd)
+    shutdown | save_log $1
+    ;;
+  status|st)
+    status | save_log $1
+    ;;
+  tail|t)
+    tail
+    ;;
+  version|v)
+    version | save_log $1
+    ;;
+  *)
+    cli_help
+    ;;
+esac
---
+++ scripts/nnm-cli/nnm-service.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+set -e
+export WORKDIR=$HOME
+BRANCH=${2:-master}
+RPC_PORT=8545
+BUILD_DIR="build"
+BUILD_NEW_DIR="build_new"
+NETHERMIND_DIR="nethermind"
+NETHERMIND_LOGS_PATH="$HOME/data/logs/*.logs.txt"
+
+mkdir -p $WORKDIR/.nnm/logs
+
+cli_help() {
+  cli_name=${0##*/}
+  echo "
+Nethermind Node Management CLI for Systemd Service
+   _  __ _  __ __  ___  _____ __    ____
+  / |/ // |/ //  |/  / / ___// /   /  _/
+ /    //    // /|_/ / / /__ / /__ _/ /  
+/_/|_//_/|_//_/  /_/  \___//____//___/  
+
+Version: 0.0.1
+Usage: $cli_name [command]
+
+Commands:
+  pullandbuild|pb    Pulls latest changes from Nethermind repository and builds the app
+  restart|r          Restarts the Nethermind service
+  shutdown|sd        Shutdowns the Nethermind service
+  status|st          Displays the current status of Nethermind service
+  tail|t             Tails the latest Nethermind live logs
+  up|u               Starts the Nethermind service
+  *                  Help
+"
+  exit 1
+}
+
+cli_log() {
+  script_name=${0##*/}
+  function_name=${1}
+  timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+  echo "==> $script_name $timestamp $function_name"
+}
+
+save_log() {
+  timestamp=$(date -u +"%Y-%m-%d")
+  tee -ia $WORKDIR/.nnm/logs/$timestamp.log
+}
+
+# start of pullandbuild
+gitPullOrigin() {
+	cli_log "Pulling origin master from Github..."
+	git pull origin master
+}
+
+gitCheckout() {
+  cli_log "Checking out branch/tag: ${BRANCH}"
+  git checkout $BRANCH
+}
+
+gitPull() {
+  cli_log "Pulling latest changes from Github..."
+  git pull
+}
+
+gitSubmoduleUpdate() {
+	cli_log "Updating submodules..."
+	git submodule update --init
+}
+
+buildNethermind() {
+	cli_log "Building Nethermind..."
+	cd $HOME/nethermind/src/Nethermind/Nethermind.Runner
+	dotnet build -c Release -o $HOME/$BUILD_NEW_DIR
+}
+
+replaceOldBuild() {
+	cli_log "Replacing old build dir..."
+	rm -rf $HOME/$BUILD_DIR
+	mv $HOME/$BUILD_NEW_DIR $BUILD_DIR
+}
+
+pullandbuild() {
+  cli_log "Pulling and rebuilding nethermind.service..."
+  cd $HOME/$NETHERMIND_DIR
+  gitPullOrigin
+  gitCheckout $BRANCH
+  gitPull
+  gitSubmoduleUpdate
+  buildNethermind
+  cd $HOME
+  shutdown
+  replaceOldBuild
+  up
+}
+# End of pullandbuild
+
+# Start of restart
+restart() {
+  cli_log "Restarting nethermind.service..."
+  sudo systemctl restart nethermind
+}
+# End of restart
+
+# Start of shutdown
+shutdown() {
+  cli_log "Shutting down nethermind.service..."
+  sudo systemctl stop nethermind
+}
+# End of shutdown
+
+# Start of status
+status() {
+  cli_log "Shutting down nethermind.service..."
+  sudo systemctl status nethermind
+}
+# End of status
+
+# Start of up
+up() {
+  cli_log "Starting nethermind.service..."
+  sudo systemctl start nethermind
+}
+# End of up
+
+# Start of tail
+tail() {
+  cli_log "Getting live logs from nethermind.service..."
+  tail -f $NETHERMIND_LOGS_PATH
+}
+# End of tail
+
+# Start of version
+version() {
+  cli_log "Checking the current version of Nethermind..."
+  curl --silent --data '{"method":"web3_clientVersion","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:$RPC_PORT | jq ".result" | tr -d '"'
+}
+# End of version
+
+case "$1" in
+  pullndbuild|pb)
+    pullandbuild "$BRANCH" | save_log $1
+    ;;
+  up|u)
+    up | save_log $1
+    ;;
+  restart|r)
+    restart | save_log $1
+    ;;
+  shutdown|sd)
+    shutdown | save_log $1
+    ;;
+  status|st)
+    status | save_log $1
+    ;;
+  tail|t)
+    tail
+    ;;
+  version|v)
+    version | save_log $1
+    ;;
+  *)
+    cli_help
+    ;;
+esac
