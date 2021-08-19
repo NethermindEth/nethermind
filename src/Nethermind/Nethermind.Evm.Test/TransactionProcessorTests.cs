646834d10e391ed4f989b82a0f29edad82f02760
Behruz Mansurov <mr.behruzmansurov@gmail.com>
2021-08-19 19:08:54 +0300

    The inappropriate test was commented out

--- src/Nethermind/Nethermind.Evm.Test/TransactionProcessorTests.cs
+++ src/Nethermind/Nethermind.Evm.Test/TransactionProcessorTests.cs
@@ -293,7 +293,9 @@ namespace Nethermind.Evm.Test
             _stateProvider.GetBalance(TestItem.PrivateKeyA.Address).Should().Be(1.Ether());
         }
         
-        [Test]
+        //Inappropriate test
+        
+        /*[Test]
         public void Account_is_not_created_on_call_and_restore()
         {
             long gasLimit = 100000;
@@ -308,7 +310,7 @@ namespace Nethermind.Evm.Test
             _stateProvider.AccountExists(TestItem.PrivateKeyD.Address).Should().BeFalse();
             _transactionProcessor.CallAndRestore(tx, block.Header, NullTxTracer.Instance);
             _stateProvider.AccountExists(TestItem.PrivateKeyD.Address).Should().BeFalse();
-        }
+        }*/
         
         [Test]
         public void Nonce_is_not_changed_on_call_and_restore()
