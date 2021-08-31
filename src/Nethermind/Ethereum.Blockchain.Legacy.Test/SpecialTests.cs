be977c9d49436df640bce241ea43d3008a4ab29f
MarekM25 <marekm2504@gmail.com>
2021-08-31 13:07:39 +0200

    add retries for two tests

--- src/Nethermind/Ethereum.Blockchain.Legacy.Test/SpecialTests.cs
+++ src/Nethermind/Ethereum.Blockchain.Legacy.Test/SpecialTests.cs
@@ -23,9 +23,11 @@ using NUnit.Framework;
 namespace Ethereum.Blockchain.Legacy.Test
 {
     [TestFixture][Parallelizable(ParallelScope.All)]
+
     public class SpecialTests : GeneralStateTestBase
     { 
         [TestCaseSource(nameof(LoadTests))]
+        [Retry(3)]
         public void Test(GeneralStateTest test)
         {
             Assert.True(RunTest(test).Pass);
