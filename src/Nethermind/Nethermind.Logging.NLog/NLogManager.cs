e284f52f246d95dbf870142799b4e0e385d78009
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2021-09-06 17:07:01 +0200

    Logging changes (#3378)
    * Logging changes (#3376)
    
    * Changed NLogManager parameters and added LogLevel to IInitConfig interface.
    
    * Added functionality to add rules to Nlog.config
    
    * Fixed expression parsing and order of log levels
    
    * Changed code to work with LogManager.Configuration.LoggingRules
    
    * Added test and fixed few errors
    
    * Created working test for addition of configuration
    
    * Renamed LoggingRuleComparer
    
    * Added braces and shortened LoggingRuleComparer
    
    * Removed unneeded libraries in EthereumRunnerTests.cs
    
    Co-authored-by: Kaushik Donthi <kaushikdr@gmail.com>
    
    * Add InitConfig.LogRules
    
    * Add rule manipulation
    
    Remove overwritten rules
    Move init code to NLogManager
    
    Co-authored-by: Kaushik Donthi <kaushikdr@gmail.com>

--- src/Nethermind/Nethermind.Logging.NLog/NLogManager.cs
+++ src/Nethermind/Nethermind.Logging.NLog/NLogManager.cs
@@ -16,51 +16,150 @@
 
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using NLog;
+using NLog.Config;
+using NLog.Targets;
+using Level = NLog.LogLevel;
 
 namespace Nethermind.Logging.NLog
 {
     public class NLogManager : ILogManager
     {
-        private readonly string _logFileName;
-        private readonly string _logDirectory;
-
-        public NLogManager(string logFileName, string logDirectory)
+        private const string DefaultFileTargetName = "file-async_wrapped";
+        
+        public NLogManager(string logFileName, string logDirectory = null, string logRules = null)
         {
-            _logFileName = logFileName;
-            _logDirectory = logDirectory;
+            logDirectory = SetupLogDirectory(logDirectory);
+            SetupLogFile(logFileName, logDirectory);
+            SetupLogRules(logRules);
+        }
+
+        private static void SetupLogFile(string logFileName, string logDirectory)
+        {
+            if (LogManager.Configuration?.AllTargets != null)
+            {
+                foreach (FileTarget target in LogManager.Configuration?.AllTargets.OfType<FileTarget>())
+                {
+                    string fileNameToUse = (target.Name == DefaultFileTargetName) ? logFileName : target.FileName.Render(LogEventInfo.CreateNullEvent());
+                    target.FileName = !Path.IsPathFullyQualified(fileNameToUse) ? Path.GetFullPath(Path.Combine(logDirectory, fileNameToUse)) : fileNameToUse;
+                }
+            }
+        }
+
+        private static string SetupLogDirectory(string logDirectory)
+        {
+            logDirectory = (string.IsNullOrEmpty(logDirectory) ? "logs" : logDirectory).GetApplicationResourcePath();
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+
+            return logDirectory;
         }
 
         private ConcurrentDictionary<Type, NLogLogger> _loggers = new();
 
-        private NLogLogger BuildLogger(Type type)
-        {
-            return new(type, _logFileName, _logDirectory);
-        }
+        private NLogLogger BuildLogger(Type type) => new(type);
 
-        public ILogger GetClassLogger(Type type)
-        {
-            return _loggers.GetOrAdd(type, BuildLogger);
-        }
+        public ILogger GetClassLogger(Type type) => _loggers.GetOrAdd(type, BuildLogger);
 
-        public ILogger GetClassLogger<T>()
-        {
-            return GetClassLogger(typeof(T));
-        }
+        public ILogger GetClassLogger<T>() => GetClassLogger(typeof(T));
 
-        public ILogger GetClassLogger()
-        {
-            return new NLogLogger(_logFileName, _logDirectory);
-        }
+        public ILogger GetClassLogger() => new NLogLogger();
 
-        public ILogger GetLogger(string loggerName)
-        {
-            return new NLogLogger(_logFileName, _logDirectory, loggerName);
-        }
+        public ILogger GetLogger(string loggerName) => new NLogLogger(loggerName);
 
         public void SetGlobalVariable(string name, object value)
         {
             GlobalDiagnosticsContext.Set(name, value);
+        }
+        
+        private void SetupLogRules(string logRules)
+        {
+            //Add rules here for e.g. 'JsonRpc.*: Warn; Block.*: Error;',
+            if (logRules != null)
+            {
+                IList<LoggingRule> configurationLoggingRules = LogManager.Configuration.LoggingRules;
+                lock (configurationLoggingRules)
+                {
+                    Target[] targets = GetTargets(configurationLoggingRules);
+                    IEnumerable<LoggingRule> loggingRules = ParseRules(logRules, targets);
+                    foreach (LoggingRule loggingRule in loggingRules)
+                    {
+                        RemoveOverridenRules(configurationLoggingRules, loggingRule);
+                        configurationLoggingRules.Add(loggingRule);
+                    }
+                }
+            }
+        }
+
+        private Target[] GetTargets(IList<LoggingRule> configurationLoggingRules) => 
+            configurationLoggingRules.SelectMany(r => r.Targets).Distinct().ToArray();
+
+        private void RemoveOverridenRules(IList<LoggingRule> configurationLoggingRules, LoggingRule loggingRule)
+        {
+            string reqexPattern = $"^{loggingRule.LoggerNamePattern.Replace(".", "\\.").Replace("*", ".*")}$";
+            for (int j = 0; j < configurationLoggingRules.Count;)
+            {
+                if (Regex.IsMatch(configurationLoggingRules[j].LoggerNamePattern, reqexPattern))
+                {
+                    configurationLoggingRules.RemoveAt(j);
+                }
+                else
+                {
+                    j++;
+                }
+            }
+        }
+
+        private IEnumerable<LoggingRule> ParseRules(string logRules, Target[] targets)
+        {
+            string[] rules = logRules.Split(";", StringSplitOptions.RemoveEmptyEntries);
+            foreach (string rule in rules)
+            {
+                string loggerNamePattern;
+                Level logLevel;
+                try
+                {
+                    string[] ruleBreakdown = rule.Split(":");
+                    if (ruleBreakdown.Length == 2)
+                    {
+                        loggerNamePattern = ruleBreakdown[0].Trim();
+                        logLevel = Level.FromString(ruleBreakdown[1].Trim());
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Invalid rule '{rule}' in InitConfig.LogRules '{logRules}'");
+                    }
+                }
+                catch (ArgumentException e)
+                {
+                    throw new ArgumentException($"Invalid rule '{rule}' in InitConfig.LogRules '{logRules}'", e);
+                }
+                
+                yield return CreateLoggingRule(targets, logLevel, loggerNamePattern);
+            }
+        }
+
+        private static LoggingRule CreateLoggingRule(Target[] targets, Level level, string loggerNamePattern)
+        {
+            LoggingRule loggingRule = new(loggerNamePattern, level, Level.Fatal, targets[0]);
+            for (int i = 1; i < targets.Length; i++)
+            {
+                loggingRule.Targets.Add(targets[i]);
+            }
+
+            return loggingRule;
+        }
+
+        public static void Shutdown()
+        {
+            LogManager.Shutdown();
         }
     }
 }
