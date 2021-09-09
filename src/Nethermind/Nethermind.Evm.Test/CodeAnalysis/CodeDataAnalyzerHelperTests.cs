19dbcfb7034b3fa823a34c1b86c50674017728ee
Daniel Celeda <dceleda@hotmail.com>
2021-09-10 00:07:24 +0100

    Feature/improve jumpdest analysis (#3371)
    * Change Jumpdest analysis implementation to bitmaps
    
    * Improvements to jumpdest analysis
    
    * Add sampling to CodeInfo jumpdest analysis
    
    * Post review changes
    
    * Tests fix

---
+++ src/Nethermind/Nethermind.Evm.Test/CodeAnalysis/CodeDataAnalyzerHelperTests.cs
@@ -0,0 +1,59 @@
+﻿//  Copyright (c) 2021 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using FluentAssertions;
+using Nethermind.Evm.CodeAnalysis;
+using NUnit.Framework;
+
+namespace Nethermind.Evm.Test.CodeAnalysis
+{
+    [TestFixture]
+    public class CodeDataAnalyzerHelperTests
+    {
+        [Test]
+        public void Validate_CodeBitmap_With_Push10()
+        {
+            byte[] code =
+            {
+                (byte)Instruction.PUSH10,
+                1,2,3,4,5,6,7,8,9,10,
+                (byte)Instruction.JUMPDEST
+            };
+
+            var bitmap = CodeDataAnalyzerHelper.CreateCodeBitmap(code);
+            bitmap[0].Should().Be(127);
+            bitmap[1].Should().Be(224);
+        }
+        
+        [Test]
+        public void Validate_CodeBitmap_With_Push30()
+        {
+            byte[] code =
+            {
+                (byte)Instruction.PUSH30,
+                1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
+                (byte)Instruction.JUMPDEST
+            };
+
+            var bitmap = CodeDataAnalyzerHelper.CreateCodeBitmap(code);
+            bitmap[0].Should().Be(127);
+            bitmap[1].Should().Be(255);
+            bitmap[2].Should().Be(255);
+            bitmap[3].Should().Be(254);
+        }
+    }
+}
