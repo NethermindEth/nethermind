777804a8f249688d8db2026949570d23ca3c603a
user <klaudia.jazgar@gmail.com>
2021-09-10 12:44:19 +0200

    small refactoring

--- src/Nethermind/Nethermind.JsonRpc/Modules/Trace/TraceRpcModule.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Trace/TraceRpcModule.cs
@@ -210,7 +210,7 @@ namespace Nethermind.JsonRpc.Modules.Trace
         
             Block block = blockSearch.Object;
 
-            ParityLikeTxTrace txTrace = TraceTx(block, txHash, ParityTraceTypes.Trace);
+            // ParityLikeTxTrace txTrace = TraceTx(block, txHash, ParityTraceTypes.Trace);
         
             IReadOnlyCollection<ParityLikeTxTrace> txTrace = TraceTxT(block, txHash, ParityTraceTypes.Trace);
             return ResultWrapper<ParityTxTraceFromStore[]>.Success(ParityTxTraceFromStore.FromTxTrace(txTrace));
