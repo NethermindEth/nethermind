96d37ca1d5d143f8a24ac6c55bcdd25f3f6b60b1
MarekM25 <marekm2504@gmail.com>
2021-09-10 10:09:16 +0200

    one more

--- src/Nethermind/Nethermind.AuRa.Test/BuildBlocksOnAuRaStepsTests.cs
+++ src/Nethermind/Nethermind.AuRa.Test/BuildBlocksOnAuRaStepsTests.cs
@@ -34,6 +34,7 @@ namespace Nethermind.AuRa.Test
     public class BuildBlocksOnAuRaStepsTests
     {
         [Test]
+        [Retry(3)]
         public async Task should_cancel_block_production_trigger_on_next_step_if_not_finished_yet()
         {
             List<BlockProductionEventArgs> args = new();
