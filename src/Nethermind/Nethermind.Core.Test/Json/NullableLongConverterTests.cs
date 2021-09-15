5b49fd29448db9d754d4f340c275cdfe5c47b3d6
Marek Moraczyński <marekm2504@gmail.com>
2021-09-15 10:21:13 +0200

    Make more hive tests green (#3407)
    * Change hive config
    
    * fix loading empty accounts with storage
    
    * Add isGenesis to StateProvider
    
    * fix
    
    * UpdateCodeHash for genesis
    
    * add description
    
    * Add example chainspec for hive
    
    * add test
    
    * cosmetic
    
    * Add one more test
    
    * small logging change
    
    * skip zero balance check
    
    * Add test
    
    * fix hive with zero balance
    
    * remove test.json
    
    * add test
    
    * fix test
    
    * experiments with hive
    
    * RPC should wait for SetupHive
    
    * ChainId reading from genesis
    
    * test other exception handling method
    
    * temp
    
    * add blockValidator in HiveSetup
    
    * The edge case for gas limit in hive tests
    
    * hive experiments
    
    * tracing
    
    * Experiments with tracing first
    
    * cleanup
    
    * add NullableULongConverter
    
    * working on NullableULongConverterTests
    
    * NullableULongConverterTests
    
    * Add NullableLongConverterTests
    
    * cleanup
    
    * add gas limit test
    
    * cleanup
    
    * one more cleanup
    
    * fix test
    
    * more retries
    
    * review fixes
    
    * review fix
    
    * add comment

---
+++ src/Nethermind/Nethermind.Core.Test/Json/NullableLongConverterTests.cs
@@ -0,0 +1,119 @@
+﻿//  Copyright (c) 2021 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System;
+using System.IO;
+using Nethermind.Serialization.Json;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Nethermind.Core.Test.Json
+{
+    [TestFixture]
+    public class NullableLongConverterTests : ConverterTestBase<long?>
+    {
+        [TestCase(NumberConversion.Hex)]
+        [TestCase(NumberConversion.Decimal)]
+        public void Test_roundtrip(NumberConversion numberConversion)
+        {
+            NullableLongConverter converter = new(numberConversion);
+            TestConverter(int.MaxValue, (a, b) => a.Equals(b), converter);
+            TestConverter(1L, (a, b) => a.Equals(b), converter);
+            TestConverter(0L, (a, b) => a.Equals(b), converter);
+        }
+        
+        [TestCase((NumberConversion)99)]
+        public void Unknown_not_supported(NumberConversion notSupportedConversion)
+        {
+            NullableLongConverter converter = new(notSupportedConversion);
+            Assert.Throws<NotSupportedException>(
+                () => TestConverter(int.MaxValue, (a, b) => a.Equals(b), converter));
+            Assert.Throws<NotSupportedException>(
+                () => TestConverter(1L, (a, b) => a.Equals(b), converter));
+        }
+
+        [Test]
+        public void Regression_0xa00000()
+        {
+            NullableLongConverter converter = new();
+            JsonReader reader = new JsonTextReader(new StringReader("0xa00000"));
+            reader.ReadAsString();
+            long? result = converter.ReadJson(reader, typeof(long?), 0, false, JsonSerializer.CreateDefault());
+            Assert.AreEqual(10485760, result);
+        }
+        
+        [Test]
+        public void Can_read_0x0()
+        {
+            NullableLongConverter converter = new();
+            JsonReader reader = new JsonTextReader(new StringReader("0x0"));
+            reader.ReadAsString();
+            long? result = converter.ReadJson(reader, typeof(long?), 0L, false, JsonSerializer.CreateDefault());
+            Assert.AreEqual(long.Parse("0"), result);
+        }
+        
+        [Test]
+        public void Can_read_0x000()
+        {
+            NullableLongConverter converter = new();
+            JsonReader reader = new JsonTextReader(new StringReader("0x0000"));
+            reader.ReadAsString();
+            long? result = converter.ReadJson(reader, typeof(long?), 0L, false, JsonSerializer.CreateDefault());
+            Assert.AreEqual(long.Parse("0"), result);
+        }
+        
+        [Test]
+        public void Can_read_0()
+        {
+            NullableLongConverter converter = new();
+            JsonReader reader = new JsonTextReader(new StringReader("0"));
+            reader.ReadAsString();
+            long? result = converter.ReadJson(reader, typeof(long?), 0L, false, JsonSerializer.CreateDefault());
+            Assert.AreEqual(long.Parse("0"), result);
+        }
+        
+        [Test]
+        public void Can_read_1()
+        {
+            NullableLongConverter converter = new();
+            JsonReader reader = new JsonTextReader(new StringReader("1"));
+            reader.ReadAsString();
+            long? result = converter.ReadJson(reader, typeof(long?), 0L, false, JsonSerializer.CreateDefault());
+            Assert.AreEqual(long.Parse("1"), result);
+        }
+
+        [Test]
+        public void Can_read_null()
+        {
+            NullableLongConverter converter = new();
+            JsonReader reader = new JsonTextReader(new StringReader("null"));
+            reader.ReadAsString();
+            long? result = converter.ReadJson(reader, typeof(long?), 0L, false, JsonSerializer.CreateDefault());
+            Assert.AreEqual(null, result);
+        }
+        
+        [Test]
+        public void Can_read_negative_numbers()
+        {
+            NullableLongConverter converter = new();
+            JsonReader reader = new JsonTextReader(new StringReader("-1"));
+            reader.ReadAsString();
+            long? result = converter.ReadJson(reader, typeof(long?), 0L, false, JsonSerializer.CreateDefault());
+            Assert.AreEqual(long.Parse("-1"), result);
+        }
+    }
+}
--- src/Nethermind/Nethermind.JsonRpc/Modules/Trace/TraceRpcModule.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Trace/TraceRpcModule.cs
@@ -18,7 +18,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using MathNet.Numerics.Distributions;
 using Nethermind.Blockchain.Find;
 using Nethermind.Blockchain.Receipts;
 using Nethermind.Blockchain.Tracing;
