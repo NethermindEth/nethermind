f9eba0742ba842888c627d03b4ab0516b58a3707
natdurlik <70535861+natdurlik@users.noreply.github.com>
2021-09-22 12:27:00 +0200

    Test merge rewards (#3439)
    * add tests for merge calculator and pos switcher
    
    * fix test
    
    * refactor PoSSwitcher tests

---
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/MergeRewardCalculatorTests.cs
@@ -0,0 +1,159 @@
+﻿//  Copyright (c) 2021 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using Nethermind.Blockchain.Rewards;
+using Nethermind.Core;
+using Nethermind.Core.Test;
+using Nethermind.Core.Test.Builders;
+using Nethermind.Specs;
+using NUnit.Framework;
+
+namespace Nethermind.Merge.Plugin.Test
+{
+    public class MergeRewardCalculatorTests
+    {
+        [Test]
+        public void Two_uncles_from_the_same_coinbase()
+        {
+            Block uncle = Build.A.Block.WithNumber(1).TestObject;
+            Block uncle2 = Build.A.Block.WithNumber(1).TestObject;
+            Block block = Build.A.Block.WithNumber(3).WithUncles(uncle, uncle2).WithTotalDifficulty(1L).TestObject;
+            Block block2 = Build.A.Block.WithNumber(3).WithUncles(uncle, uncle2).WithTotalDifficulty(2L).TestObject;
+
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(2);
+            
+            MergeRewardCalculator calculator = new(new RewardCalculator(RopstenSpecProvider.Instance), poSSwitcher);
+            BlockReward[] rewards = calculator.CalculateRewards(block);
+            
+            Assert.AreEqual(3, rewards.Length);
+            Assert.AreEqual(5312500000000000000, (long)rewards[0].Value, "miner");
+            Assert.AreEqual(3750000000000000000, (long)rewards[1].Value, "uncle1");
+            Assert.AreEqual(3750000000000000000, (long)rewards[2].Value, "uncle2");
+            
+            rewards = calculator.CalculateRewards(block2);
+            
+            Assert.AreEqual(0, rewards.Length);
+        }
+        
+        [Test]
+        public void One_uncle()
+        {
+            Block uncle = Build.A.Block.WithNumber(1).TestObject;
+            Block block = Build.A.Block.WithNumber(3).WithUncles(uncle).WithTotalDifficulty(1L).TestObject;
+            Block block2 = Build.A.Block.WithNumber(3).WithUncles(uncle).WithTotalDifficulty(3L).TestObject;
+            
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(2);
+            
+            MergeRewardCalculator calculator = new(new RewardCalculator(RopstenSpecProvider.Instance), poSSwitcher);
+            BlockReward[] rewards = calculator.CalculateRewards(block);
+            
+            Assert.AreEqual(2, rewards.Length);
+            Assert.AreEqual(5156250000000000000, (long)rewards[0].Value, "miner");
+            Assert.AreEqual(3750000000000000000, (long)rewards[1].Value, "uncle1");
+            
+            rewards = calculator.CalculateRewards(block2);
+            Assert.AreEqual(0, rewards.Length);
+        }
+        
+        [Test]
+        public void No_uncles()
+        {
+            Block block = Build.A.Block.WithNumber(3).WithTotalDifficulty(1L).TestObject;
+            Block block2 = Build.A.Block.WithNumber(3).WithTotalDifficulty(3L).TestObject;
+            
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(2);
+            
+            MergeRewardCalculator calculator = new(new RewardCalculator(RopstenSpecProvider.Instance), poSSwitcher);
+            BlockReward[] rewards = calculator.CalculateRewards(block);
+            
+            Assert.AreEqual(1, rewards.Length);
+            Assert.AreEqual(5000000000000000000, (long)rewards[0].Value, "miner");
+
+            rewards = calculator.CalculateRewards(block2);
+            Assert.AreEqual(0, rewards.Length);
+        }
+        
+        [Test]
+        public void Byzantium_reward_two_uncles()
+        {
+            long blockNumber = RopstenSpecProvider.ByzantiumBlockNumber;
+            Block uncle = Build.A.Block.WithNumber(blockNumber - 2).TestObject;
+            Block uncle2 = Build.A.Block.WithNumber(blockNumber - 2).TestObject;
+            Block block = Build.A.Block.WithNumber(blockNumber).WithUncles(uncle, uncle2).WithTotalDifficulty(1L).TestObject;
+            Block block2 = Build.A.Block.WithNumber(blockNumber).WithUncles(uncle, uncle2).WithTotalDifficulty(3L).TestObject;
+            
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(2);
+            
+            MergeRewardCalculator calculator = new(new RewardCalculator(RopstenSpecProvider.Instance), poSSwitcher);
+            BlockReward[] rewards = calculator.CalculateRewards(block);
+            
+            Assert.AreEqual(3, rewards.Length);
+            Assert.AreEqual(3187500000000000000, (long)rewards[0].Value, "miner");
+            Assert.AreEqual(2250000000000000000, (long)rewards[1].Value, "uncle1");
+            Assert.AreEqual(2250000000000000000, (long)rewards[2].Value, "uncle2");
+            
+            rewards = calculator.CalculateRewards(block2);
+            
+            Assert.AreEqual(0, rewards.Length);
+        }
+        
+        [Test]
+        public void Constantinople_reward_two_uncles()
+        {
+            long blockNumber = RopstenSpecProvider.ConstantinopleBlockNumber;
+            Block uncle = Build.A.Block.WithNumber(blockNumber - 2).TestObject;
+            Block uncle2 = Build.A.Block.WithNumber(blockNumber - 2).TestObject;
+            Block block = Build.A.Block.WithNumber(blockNumber).WithUncles(uncle, uncle2).WithTotalDifficulty(1L).TestObject;
+            Block block2 = Build.A.Block.WithNumber(blockNumber).WithUncles(uncle, uncle2).WithTotalDifficulty(3L).TestObject;
+            
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(2);
+            
+            MergeRewardCalculator calculator = new(new RewardCalculator(RopstenSpecProvider.Instance), poSSwitcher);
+            BlockReward[] rewards = calculator.CalculateRewards(block);
+            
+            Assert.AreEqual(3, rewards.Length);
+            Assert.AreEqual(2125000000000000000, (long)rewards[0].Value, "miner");
+            Assert.AreEqual(1500000000000000000, (long)rewards[1].Value, "uncle1");
+            Assert.AreEqual(1500000000000000000, (long)rewards[2].Value, "uncle2");
+            
+            rewards = calculator.CalculateRewards(block2);
+            Assert.AreEqual(0, rewards.Length);
+        }
+
+        [Test]
+        public void No_block_rewards_calculator()
+        {
+            Block block = Build.A.Block.WithTotalDifficulty(1L).TestObject;
+            Block block2 = Build.A.Block.WithTotalDifficulty(3L).TestObject;
+            
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(2);
+            MergeRewardCalculator calculator = new(NoBlockRewards.Instance, poSSwitcher);
+            
+            BlockReward[] rewards = calculator.CalculateRewards(block);
+            Assert.AreEqual(0,rewards.Length);
+            
+            rewards = calculator.CalculateRewards(block2);
+            Assert.AreEqual(0,rewards.Length);
+        }
+    }
+}
---
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/PoSSwitcherTests.cs
@@ -0,0 +1,118 @@
+﻿//  Copyright (c) 2021 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Test;
+using Nethermind.Core.Test.Builders;
+using NUnit.Framework;
+
+namespace Nethermind.Merge.Plugin.Test
+{
+    public class PoSSwitcherTests
+    {
+        [Test]
+        public void Is_pos_with_total_difficulty()
+        {
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(200);
+
+            BlockHeader blockHeader = Build.A.BlockHeader.WithTotalDifficulty(100L).TestObject;
+            BlockHeader blockHeader2 = Build.A.BlockHeader.WithTotalDifficulty(200L).TestObject;
+            BlockHeader blockHeader3 = Build.A.BlockHeader.WithTotalDifficulty(300L).TestObject;
+
+            Assert.AreEqual(false, poSSwitcher.IsPos(blockHeader, false));
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader2, false));
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader3, false));
+
+            poSSwitcher.SetTerminalTotalDifficulty(500);
+
+            Assert.AreEqual(false, poSSwitcher.IsPos(blockHeader, false));
+            Assert.AreEqual(false, poSSwitcher.IsPos(blockHeader2, false));
+            Assert.AreEqual(false, poSSwitcher.IsPos(blockHeader3, false));
+        }
+
+        [Test]
+        public void Is_pos_with_terminal_hash()
+        {
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalPoWHash(Keccak.Compute("test1"));
+
+            BlockHeader blockHeader = Build.A.BlockHeader.WithParentHash(Keccak.Compute("test2")).TestObject;
+            BlockHeader blockHeader2 = Build.A.BlockHeader.WithParentHash(Keccak.Compute("test1")).TestObject;
+
+            Assert.AreEqual(false, poSSwitcher.IsPos(blockHeader, false));
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader2, false));
+
+            poSSwitcher.SetTerminalPoWHash(Keccak.Compute("test2"));
+
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader, false));
+            Assert.AreEqual(false, poSSwitcher.IsPos(blockHeader2, false));
+        }
+
+        [Test]
+        public void Is_pos_try_switch_to_PoS()
+        {
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(200);
+
+            BlockHeader blockHeader = Build.A.BlockHeader.WithTotalDifficulty(200L).TestObject;
+            BlockHeader blockHeader2 = Build.A.BlockHeader.WithTotalDifficulty(400L).TestObject;
+
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader, true));
+            poSSwitcher.SetTerminalTotalDifficulty(500);
+
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader, false));
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader2, false));
+        }
+
+        [Test]
+        public void Is_pos_sets_first_PoS_header_once()
+        {
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(200);
+
+            BlockHeader blockHeader = Build.A.BlockHeader.WithTotalDifficulty(200L).TestObject;
+            BlockHeader blockHeader2 = Build.A.BlockHeader.WithTotalDifficulty(400L).TestObject;
+
+            poSSwitcher.IsPos(blockHeader, true);
+            poSSwitcher.SetTerminalTotalDifficulty(400);
+
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader, false));
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader2, false));
+
+            poSSwitcher.IsPos(blockHeader2, true);
+
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader, false));
+            Assert.AreEqual(true, poSSwitcher.IsPos(blockHeader2, false));
+        }
+
+        [Test]
+        public void Was_ever_in_pos()
+        {
+            PoSSwitcher poSSwitcher = new(NUnitLogManager.Instance);
+            poSSwitcher.SetTerminalTotalDifficulty(200L);
+
+            Assert.AreEqual(false, poSSwitcher.WasEverInPoS());
+
+            BlockHeader blockHeader = Build.A.BlockHeader.WithTotalDifficulty(300L).TestObject;
+            poSSwitcher.IsPos(blockHeader, true);
+
+            Assert.AreEqual(true, poSSwitcher.WasEverInPoS());
+        }
+    }
+}
