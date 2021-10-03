1afb9af872a0daf1d4bbb02fdd9cd4bec8122052
Tomasz K. Stanczak <tkstanczak_github@demerzel.co>
2021-10-04 02:58:58 +0300

    great move

--- src/Nethermind/Nethermind.Consensus/ReadOnlyChain.cs
+++ src/Nethermind/Nethermind.Consensus/ReadOnlyChain.cs
@@ -22,7 +22,6 @@ namespace Nethermind.Blockchain
 {
     public class BlockProducerEnv
     {
-        public IBlockchainProcessor ChainProcessor { get; set; }
         public IStateProvider ReadOnlyStateProvider { get; set; }
         public ITxSource TxSource { get; set; }
         public ReadOnlyTxProcessingEnv ReadOnlyTxProcessingEnv { get; set; }
