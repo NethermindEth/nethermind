5844c17996bc4343c27781ebf3ecde9795e55670
lukasz.rozmej <lukasz.rozmej@gmail.com>
2021-10-12 12:59:57 +0200

    Revert "Add SnapshotCollection"
    This reverts commit 793272d53b05bd3e048878e88a86795c25aa4b6e.
    
    # Conflicts:
    #	src/Nethermind/Nethermind.Evm/EvmState.cs

--- src/Nethermind/Nethermind.Evm/EvmState.cs
+++ src/Nethermind/Nethermind.Evm/EvmState.cs
@@ -111,17 +111,17 @@ namespace Nethermind.Evm
         /// </summary>
         public IReadOnlySet<StorageCell> AccessedStorageCells => _accessedStorageCells;
         
-        public ICollection<Address> DestroyList { get; }
+        public ICollection<Address> DestroyList => _destroyList;
         public ICollection<LogEntry> Logs => _logs;
 
         private readonly JournalSet<Address> _accessedAddresses;
         private readonly JournalSet<StorageCell> _accessedStorageCells;
         private readonly JournalCollection<LogEntry> _logs;
         private readonly JournalSet<Address> _destroyList;
-        private readonly int _accessedAddressesSnapshot = -1;
-        private readonly int _accessedStorageKeysSnapshot = -1;
-        private readonly int _destroyListSnapshot = -1;
-        private readonly int _logsSnapshot = -1;
+        private readonly int _accessedAddressesSnapshot;
+        private readonly int _accessedStorageKeysSnapshot;
+        private readonly int _destroyListSnapshot;
+        private readonly int _logsSnapshot;
 
         public int DataStackHead = 0;
         
@@ -187,18 +187,20 @@ namespace Nethermind.Evm
                 _accessedStorageCells = stateForAccessLists._accessedStorageCells;
                 _destroyList = stateForAccessLists._destroyList;
                 _logs = stateForAccessLists._logs;
-                _accessedAddressesSnapshot = _accessedAddresses.TakeSnapshot();
-                _accessedStorageKeysSnapshot = _accessedStorageCells.TakeSnapshot();
-                (_destroyListSnapshot, DestroyList) = _destroyList.TakeSnapshotWithCollection();
-                _logsSnapshot = _logs.TakeSnapshot();
             }
             else
             {
                 _accessedAddresses = new JournalSet<Address>();
                 _accessedStorageCells = new JournalSet<StorageCell>();
-                DestroyList = _destroyList = new JournalSet<Address>();
+                _destroyList = new JournalSet<Address>();
                 _logs = new JournalCollection<LogEntry>();
             }
+
+            _accessedAddressesSnapshot = _accessedAddresses.TakeSnapshot();
+            _accessedStorageKeysSnapshot = _accessedStorageCells.TakeSnapshot();
+            _destroyListSnapshot = _destroyList.TakeSnapshot();
+            _logsSnapshot = _logs.TakeSnapshot();
+
         }
 
         public Address From
@@ -283,7 +285,6 @@ namespace Nethermind.Evm
         {
             parentState.Refund += Refund;
             _state = State.Commited;
-            _destroyList.DropSnapshot(_destroyListSnapshot);
         }
 
         public void Restore()
