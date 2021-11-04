cb22d2a25fa194f901815439b244beb367070e14
Marcin Sobczak <marcindsobczak@gmail.com>
2021-11-04 17:49:17 +0000

    implement UserOperationDecoder

--- src/Nethermind/Nethermind.AccountAbstraction/Data/UserOperationDecoder.cs
+++ src/Nethermind/Nethermind.AccountAbstraction/Data/UserOperationDecoder.cs
@@ -15,20 +15,50 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 // 
 
+using Nethermind.Core;
 using Nethermind.Serialization.Rlp;
 
 namespace Nethermind.AccountAbstraction.Data
 {
     public class UserOperationDecoder : IRlpValueDecoder<UserOperation>, IRlpStreamDecoder<UserOperation>
     {
-        public int GetLength(UserOperation item, RlpBehaviors rlpBehaviors)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public Rlp Encode(UserOperation? item, RlpBehaviors rlpBehaviors = RlpBehaviors.None)
         {
-            throw new System.NotImplementedException();
+            if (item is null)
+            {
+                return Rlp.OfEmptySequence;
+            }
+            
+            RlpStream rlpStream = new(GetLength(item, rlpBehaviors));
+            Encode(rlpStream, item, rlpBehaviors);
+            return new Rlp(rlpStream.Data!);
+
+        }
+        
+        public void Encode(RlpStream stream, UserOperation? op, RlpBehaviors rlpBehaviors = RlpBehaviors.None)
+        {
+            if (op is null)
+            {
+                stream.EncodeNullObject();
+                return;
+            }
+            
+            int contentLength = GetContentLength(op);
+
+            stream.StartSequence(contentLength);
+
+            stream.Encode(op.Sender);
+            stream.Encode(op.Nonce);
+            stream.Encode(op.InitCode);
+            stream.Encode(op.CallData);
+            stream.Encode(op.CallGas);
+            stream.Encode(op.VerificationGas);
+            stream.Encode(op.PreVerificationGas);
+            stream.Encode(op.MaxFeePerGas);
+            stream.Encode(op.MaxPriorityFeePerGas);
+            stream.Encode(op.Paymaster);
+            stream.Encode(op.PaymasterData);
+            stream.Encode(op.Signature);
         }
 
         public UserOperation Decode(ref Rlp.ValueDecoderContext decoderContext, RlpBehaviors rlpBehaviors = RlpBehaviors.None)
@@ -38,12 +68,44 @@ namespace Nethermind.AccountAbstraction.Data
 
         public UserOperation Decode(RlpStream rlpStream, RlpBehaviors rlpBehaviors = RlpBehaviors.None)
         {
-            throw new System.NotImplementedException();
+            UserOperationRpc userOperationRpc = new UserOperationRpc
+                {
+                    Sender = rlpStream.DecodeAddress() ?? Address.Zero,
+                    Nonce = rlpStream.DecodeUInt256(),
+                    InitCode = rlpStream.DecodeByteArray(),
+                    CallData = rlpStream.DecodeByteArray(),
+                    CallGas = rlpStream.DecodeUInt256(),
+                    VerificationGas = rlpStream.DecodeUInt256(),
+                    PreVerificationGas = rlpStream.DecodeUInt256(),
+                    MaxFeePerGas = rlpStream.DecodeUInt256(),
+                    MaxPriorityFeePerGas = rlpStream.DecodeUInt256(),
+                    Paymaster = rlpStream.DecodeAddress() ?? Address.Zero,
+                    PaymasterData = rlpStream.DecodeByteArray()
+                };
+            
+          //  DecodeSignature(); ???
+            return new UserOperation(userOperationRpc);
         }
 
-        public void Encode(RlpStream stream, UserOperation item, RlpBehaviors rlpBehaviors = RlpBehaviors.None)
+        public int GetLength(UserOperation item, RlpBehaviors rlpBehaviors)
         {
-            throw new System.NotImplementedException();
+            return Rlp.GetSequenceRlpLength(GetContentLength(item));
+        }
+        
+        private static int GetContentLength(UserOperation op)
+        {
+            return Rlp.LengthOf(op.Sender)
+                   + Rlp.LengthOf(op.Nonce)
+                   + Rlp.LengthOf(op.InitCode)
+                   + Rlp.LengthOf(op.CallData)
+                   + Rlp.LengthOf(op.CallGas)
+                   + Rlp.LengthOf(op.VerificationGas)
+                   + Rlp.LengthOf(op.PreVerificationGas)
+                   + Rlp.LengthOf(op.MaxFeePerGas)
+                   + Rlp.LengthOf(op.MaxPriorityFeePerGas)
+                   + Rlp.LengthOf(op.Paymaster)
+                   + Rlp.LengthOf(op.PaymasterData)
+                   + Rlp.LengthOf(op.Signature);
         }
     }
 }
