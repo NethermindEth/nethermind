38cb4ddbd2588314ad0af86820d5a03bcc5e57ff
sarah <avalonche@protonmail.com>
2021-11-06 01:46:25 +1100

    The Merge - Kintsugi Test Vectors (#3579)
    * add forkchoiceUpdatedV1 test
    
    * add error cases
    
    * cosmetics
    
    Co-authored-by: MarekM25 <marekm2504@gmail.com>

---
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/EngineModuleTests.TestVectorsV1.cs
@@ -0,0 +1,114 @@
+﻿//  Copyright (c) 2021 Demerzel Solutions Limited
+//  This file is part of the Nethermind library.
+// 
+//  The Nethermind library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  The Nethermind library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Extensions;
+using Nethermind.Int256;
+using Nethermind.JsonRpc.Test;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Nethermind.Merge.Plugin.Test
+{
+    public partial class EngineModuleTests
+    {
+        [Test]
+        public async Task processing_block_should_serialize_valid_response()
+        {
+            using MergeTestBlockchain chain = await CreateBlockChain();
+            IEngineRpcModule rpc = CreateEngineModule(chain);
+            Keccak startingHead = chain.BlockTree.HeadHash;
+            Keccak random = Keccak.Zero;
+            Address feeRecipient = Address.Zero;
+            UInt256 timestamp = Timestamper.UnixTime.Seconds;
+            
+            UInt256 expectedPayloadId = 0;
+           
+            var forkChoiceUpdatedParams = new
+            {
+                headBlockHash = startingHead.ToString(true),
+                safeBlockHash = startingHead.ToString(true),
+                finalizedBlockHash = Keccak.Zero.ToString(true),
+            };
+            var preparePayloadParams = new
+            {
+                parentHash = startingHead.ToString(true),
+                timestamp = timestamp.ToHexString(true),
+                random = random.ToString(true),
+                feeRecipient = feeRecipient.ToString(true),
+            };
+            string parameters = $"{JsonConvert.SerializeObject(forkChoiceUpdatedParams)},{JsonConvert.SerializeObject(preparePayloadParams)}";
+            // prepare a payload
+            string result = RpcTest.TestSerializedRequest(rpc,"engine_forkchoiceUpdatedV1", parameters);
+            result.Should().Be($"{{\"jsonrpc\":\"2.0\",\"result\":{{\"status\":\"VALID\",\"payloadId\":\"{expectedPayloadId.ToHexString(true)}\"}},\"id\":67}}");
+            
+            Keccak blockHash = new Keccak("0x33228284b2c8d36e3fd34c31de3ab0604412bf9ab71725307d13daa2c4f44348");
+            var expectedPayload = new
+            {
+                parentHash = startingHead,
+                coinbase = feeRecipient,
+                stateRoot = chain.BlockTree.Head!.StateRoot!.ToString(true),
+                receiptRoot = chain.BlockTree.Head!.ReceiptsRoot!.ToString(true),
+                logsBloom = Bloom.Empty.ToString(),
+                random = random.ToString(true),
+                blockNumber = "0x1",
+                gasLimit = chain.BlockTree.Head!.GasLimit.ToHexString(true),
+                gasUsed = "0x0",
+                timestamp = "0x5",
+                extraData = "0x",
+                baseFeePerGas = chain.BlockTree.Head!.BaseFeePerGas.ToHexString(false),
+                blockHash = blockHash.ToString(true),
+                transaction = new List<object>(),
+            };
+            string expectedPayloadString = JsonConvert.SerializeObject(expectedPayload);
+            // get the payload
+            result = RpcTest.TestSerializedRequest(rpc, "engine_getPayloadV1", expectedPayloadId.ToHexString(true));
+            result.Should().Be($"{{\"jsonrpc\":\"2.0\",\"result\":{expectedPayload},\"id\":67}}");
+            // execute the payload
+            result = RpcTest.TestSerializedRequest(rpc, "engine_executePayloadV1", expectedPayloadString);
+            result.Should().Be($"{{\"jsonrpc\":\"2.0\",\"result\":{{\"status\":\"VALID\",\"latestValidHash\":\"{blockHash}\"}},\"id\":67}}");
+            
+            forkChoiceUpdatedParams = new
+            {
+                headBlockHash = blockHash.ToString(true),
+                safeBlockHash = blockHash.ToString(true),
+                finalizedBlockHash = startingHead.ToString(true),
+            };
+            preparePayloadParams = null;
+            parameters = $"{forkChoiceUpdatedParams},{preparePayloadParams}";
+            // update the fork choice
+            result = RpcTest.TestSerializedRequest(rpc, "engine_forkchoiceUpdatedV1", parameters);
+            result.Should().Be($"{{\"jsonrpc\":\"2.0\",\"result\":{{\"status\":\"VALID\",\"payloadId\":\"0x\"}},\"id\":67}}");
+        }
+    
+        [Test]
+        public async Task getPayload_should_serialize_unknown_payload_response_properly()
+        {
+            using MergeTestBlockchain chain = await CreateBlockChain();
+            IEngineRpcModule rpc = CreateEngineModule(chain);
+            UInt256 payloadId = 111;
+
+            string parameters = payloadId.ToHexString(true);
+            string result = RpcTest.TestSerializedRequest(rpc,"engine_getPayload", parameters);
+            result.Should().Be("{{\"jsonrpc\":\"2.0\",\"error\":{\"code\":5,\"message\":\"unknown payload\"},\"id\":67}}");
+        }
+    }
+}
--- src/Nethermind/Nethermind.Merge.Plugin.Test/EngineModuleTests.cs
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/EngineModuleTests.cs
@@ -35,8 +35,10 @@ using Nethermind.JsonRpc;
 using Nethermind.Merge.Plugin.Data;
 using NUnit.Framework;
 using Nethermind.Int256;
+using Nethermind.JsonRpc.Test;
 using Nethermind.State;
 using Nethermind.Trie;
+using Newtonsoft.Json;
 
 namespace Nethermind.Merge.Plugin.Test
 {
@@ -45,6 +47,17 @@ namespace Nethermind.Merge.Plugin.Test
     {
         private static readonly DateTime Timestamp = DateTimeOffset.FromUnixTimeSeconds(1000).UtcDateTime;
         private ITimestamper Timestamper { get; } = new ManualTimestamper(Timestamp);
+
+        [Test]
+        public async Task rpcModule_should_serialize_unknown_header_response_properly()
+        {
+            using MergeTestBlockchain chain = await CreateBlockChain();
+            IEngineRpcModule rpc = CreateEngineModule(chain);
+
+            string parameters = "{{\"blockHash\":\"0x0000000000000000000000000000000000000000000000000000000000000000\",\"status\":\"VALID\"}}";
+            string result = RpcTest.TestSerializedRequest(rpc,"engine_consensusValidated", parameters);
+            result.Should().Be("{{\"jsonrpc\":\"2.0\",\"error\":{\"code\":4,\"message\":\"unknown header\"},\"id\":67}}");
+        }
 
         [Test, Retry(3)]
         public async Task preparePayload_should_create_block_on_top_of_genesis()
@@ -711,6 +724,8 @@ namespace Nethermind.Merge.Plugin.Test
                 .Excluding(t => t.Timestamp)
             );
         }
+        
+        
 
         private (UInt256, UInt256) AddTransactions(MergeTestBlockchain chain, BlockRequestResult newBlockRequest,
             PrivateKey from, Address to, uint count, int value, out BlockHeader parentHeader)
@@ -872,4 +887,4 @@ namespace Nethermind.Merge.Plugin.Test
             }
         }
     }
-}
+}      
