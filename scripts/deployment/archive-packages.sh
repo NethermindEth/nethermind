db1ce2967cec983cb0b60fee6b4685aab796f6c1
Mateusz Jędrzejewski <mateusz.jedrzejewski@yahoo.pl>
2021-11-18 14:04:34 +0100

    fix archive script

--- scripts/deployment/archive-packages.sh
+++ scripts/deployment/archive-packages.sh
@@ -6,6 +6,7 @@ OSX=nethermind-darwin-amd64
 OSX_ARM64=nethermind-darwin-arm64
 WIN=nethermind-windows-amd64
 LIN_ARM64=nethermind-linux-arm64
+RELEASE_PATH=src/Nethermind/Nethermind.Runner/bin/Release/net6.0
 
 echo =======================================================
 echo Archiving Nethermind packages
@@ -28,26 +29,12 @@ mkdir -p $LIN_ARM64_RELEASE/plugins
 
 cd nethermind/src/Nethermind/
 dotnet build -c Release Nethermind.sln
-cd Nethermind.Baseline
-dotnet build -c Release
 
-cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $LIN_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $OSX_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $WIN_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $LIN_ARM64_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.HealthChecks.dll $OSX_ARM64_RELEASE/plugins/
-
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Merge.Plugin.dll $LIN_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Merge.Plugin.dll $OSX_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Merge.Plugin.dll $WIN_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Merge.Plugin.dll $LIN_ARM64_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Merge.Plugin.dll $OSX_ARM64_RELEASE/plugins/
-
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Mev.dll $LIN_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Mev.dll $OSX_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Mev.dll $WIN_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Mev.dll $LIN_ARM64_RELEASE/plugins/
-cp $RELEASE_DIRECTORY/plugins/Nethermind.Mev.dll $OSX_ARM64_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/$RELEASE_PATH/Nethermind.{HealthChecks,Merge.Plugin,Mev,Consensus}.dll $LIN_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/$RELEASE_PATH/Nethermind.{HealthChecks,Merge.Plugin,Mev,Consensus}.dll $OSX_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/$RELEASE_PATH/Nethermind.{HealthChecks,Merge.Plugin,Mev,Consensus}.dll $WIN_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/$RELEASE_PATH/Nethermind.{HealthChecks,Merge.Plugin,Mev,Consensus}.dll $LIN_ARM64_RELEASE/plugins/
+cp $RELEASE_DIRECTORY/$RELEASE_PATH/Nethermind.{HealthChecks,Merge.Plugin,Mev,Consensus}.dll $OSX_ARM64_RELEASE/plugins/
 
 cd $LIN_RELEASE && zip -r $LIN-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
 cd $OSX_RELEASE && zip -r $OSX-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
