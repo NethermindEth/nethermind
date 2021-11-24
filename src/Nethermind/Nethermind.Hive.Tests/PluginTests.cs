f96dbf16a0aae7c0f29d72a30014d20b92c0fa46
Marcin Sobczak <marcindsobczak@gmail.com>
2021-11-24 15:49:30 +0000

    add project with tests

---
+++ src/Nethermind/Nethermind.Hive.Tests/PluginTests.cs
@@ -0,0 +1,34 @@
+﻿using Nethermind.Config.Test;
+using NUnit.Framework;
+
+namespace Nethermind.Hive.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class PluginTests
+    {
+        [Test]
+        public void All_json_rpc_methods_are_documented()
+        {
+            JsonRpc.Test.StandardJsonRpcTests.ValidateDocumentation();
+        }
+        
+        [Test]
+        public void All_metrics_are_described()
+        {
+            Monitoring.Test.MetricsTests.ValidateMetricsDescriptions();
+        }
+        
+        [Test]
+        public void All_default_values_are_correct()
+        {
+            StandardConfigTests.ValidateDefaultValues();
+        }
+
+        [Test]
+        public void All_config_items_have_descriptions_or_are_hidden()
+        {
+            StandardConfigTests.ValidateDescriptions();
+        }
+    }
+}
