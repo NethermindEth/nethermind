795a670176109739c99d53258d6302d285c9436d
HarshCasper <erbeusgriffincasper@gmail.com>
2021-11-29 18:38:57 +0530

    FEAT: Add checks to restart Nethermind service on certain threshold

--- scripts/monitor-utilization.sh
+++ scripts/monitor-utilization.sh
@@ -1,18 +1,36 @@
 #!/bin/bash     
 
 cpu_utilization () {
-  CPU_UTILIZATION=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
-  echo "CPU utilization: $CPU_UTILIZATION"
+  CPU_UTILIZATION=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1""}')
+  CPU_UTILIZATION=$( printf "%.0f" $CPU_UTILIZATION )
+  echo "CPU utilization: $CPU_UTILIZATION%"
+
+  if [ $CPU_UTILIZATION -gt 90 ]; then
+    echo "CPU utilization is greater than 90%, restarting the service"
+    systemctl restart nethermind.service
+  fi
 }
 
 memory_utilization (){
-  MEM_UTILIZATION=$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2 }')
-  echo "Memory utilization: $MEM_UTILIZATION"
+  MEM_UTILIZATION=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+  MEM_UTILIZATION=$( printf "%.0f" $MEM_UTILIZATION )
+  echo "Memory utilization: $MEM_UTILIZATION%"
+
+  if [ $MEM_UTILIZATION -gt 90 ]; then
+    echo "Memory utilization is greater than 90%, restarting the service"
+    systemctl restart nethermind.service
+  fi
 }
 
 storage_utilization () {
   STORAGE_UTILIZATION=$(df -h | awk '$NF=="/"{printf "%s", $5}')
-  echo "Storage Utilization: $STORAGE_UTILIZATION"
+  STORAGE_UTILIZATION=${STORAGE_UTILIZATION%?}
+  echo "Storage Utilization: $STORAGE_UTILIZATION%"
+
+  if [ $STORAGE_UTILIZATION -gt 90 ]; then
+    echo "Storage utilization is greater than 90%, restarting the service"
+    systemctl restart nethermind.service
+  fi
 }
 
 cpu_utilization
