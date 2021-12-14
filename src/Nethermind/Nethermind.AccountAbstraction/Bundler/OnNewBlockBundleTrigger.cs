d9da80ca81cd147e756fdb52a5f8025b96fad5f0
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-12-14 14:53:13 -0500

    Add exception handling to OnNewBlockBundleTrigger

--- src/Nethermind/Nethermind.AccountAbstraction/Bundler/OnNewBlockBundleTrigger.cs
+++ src/Nethermind/Nethermind.AccountAbstraction/Bundler/OnNewBlockBundleTrigger.cs
@@ -1,24 +1,34 @@
 using System;
 using Nethermind.Blockchain;
 using Nethermind.Core;
+using Nethermind.Logging;
 
 namespace Nethermind.AccountAbstraction.Bundler
 {
     public class OnNewBlockBundleTrigger : IBundleTrigger
     {
         private readonly IBlockTree _blockTree;
+        private readonly ILogger _logger;
 
         public event EventHandler<BundleUserOpsEventArgs>? TriggerBundle;
 
-        public OnNewBlockBundleTrigger(IBlockTree blockTree)
+        public OnNewBlockBundleTrigger(IBlockTree blockTree, ILogger logger)
         {
+            _logger = logger;
             _blockTree = blockTree;
             _blockTree.NewHeadBlock += BlockTreeOnNewHeadBlock;
         }
 
         private void BlockTreeOnNewHeadBlock(object? sender, BlockEventArgs e)
         {
-            TriggerBundle?.Invoke(this, new BundleUserOpsEventArgs(e.Block));
+            try
+            {
+                TriggerBundle?.Invoke(this, new BundleUserOpsEventArgs(e.Block));
+            }
+            catch (System.Exception ex)
+            {
+                _logger.Error(ex.Message);
+            }
         }
     }
 }
