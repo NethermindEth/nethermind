bc90e9f8d5245b54db11edeafc3c1541de54b775
Klaudia Jazgar <67744705+kjazgar@users.noreply.github.com>
2022-01-07 01:59:52 +0100

    Implemented new approach of gas estimation. (#3723)
    * Added implementation of EstimateGas based on binary search.
    
    * Refactored tests.
    
    * refactoring
    
    * Applied requested changes.
    
    * Refactor, move additional responsibilities to GasEstimator (renamed from EstimateGasService)
    
    * Fixed tests, added new tracer, optimized binary search boundaries.
    
    * Fixed tests.
    
    * Refactored code.
    
    * Refactored code.
    
    * Removed unnecessary casting.
    
    Co-authored-by: lukasz.rozmej <lukasz.rozmej@gmail.com>

--- src/Nethermind/Nethermind.Evm/Tracing/EstimateGasTracer.cs
+++ src/Nethermind/Nethermind.Evm/Tracing/EstimateGasTracer.cs
@@ -19,6 +19,7 @@ using System.Collections.Generic;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Specs;
+using Nethermind.Evm.TransactionProcessing;
 using Nethermind.Int256;
 using Nethermind.State;
 
@@ -192,12 +193,6 @@ namespace Nethermind.Evm.Tracing
         {
             long intrinsicGas = tx.GasLimit - IntrinsicGasAt;
             return _currentGasAndNesting.Peek().AdditionalGasRequired + RefundHelper.CalculateClaimableRefund(intrinsicGas + NonIntrinsicGasSpentBeforeRefund, TotalRefund, releaseSpec);
-        }
-
-        public long CalculateEstimate(Transaction tx, IReleaseSpec releaseSpec)
-        {
-            long intrinsicGas = tx.GasLimit - IntrinsicGasAt;
-            return Math.Max(intrinsicGas, GasSpent + CalculateAdditionalGasRequired(tx, releaseSpec));
         }
 
         private int _currentNestingLevel = -1;
