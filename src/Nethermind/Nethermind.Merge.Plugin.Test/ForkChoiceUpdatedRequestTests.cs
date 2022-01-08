519d76915cc24bc278d20f8fc4e8426342900eef
MarekM25 <marekm2504@gmail.com>
2022-01-08 22:17:36 +0100

    and more

--- src/Nethermind/Nethermind.Merge.Plugin.Test/ForkChoiceUpdatedRequestTests.cs
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/ForkChoiceUpdatedRequestTests.cs
@@ -18,6 +18,7 @@
 using FluentAssertions;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Merge.Plugin.Data;
+using Nethermind.Merge.Plugin.Data.V1;
 using Nethermind.Serialization.Json;
 using NUnit.Framework;
 
@@ -26,14 +27,13 @@ namespace Nethermind.Merge.Plugin.Test
     public class ForkChoiceUpdatedRequestTests
     {
         private readonly IJsonSerializer _serializer = new EthereumJsonSerializer();
-        
-        
+
         [Test]
         public void serialization_and_deserialization_roundtrip()
         {
-            ForkChoiceUpdatedRequest initial = new(TestItem.KeccakA, TestItem.KeccakB);
+            ForkchoiceStateV1 initial = new(TestItem.KeccakA, TestItem.KeccakB, TestItem.KeccakC);
             string? serialized = _serializer.Serialize(initial);
-            ForkChoiceUpdatedRequest deserialized = _serializer.Deserialize<ForkChoiceUpdatedRequest>(serialized);
+            ForkchoiceStateV1 deserialized = _serializer.Deserialize<ForkchoiceStateV1>(serialized);
             deserialized.Should().BeEquivalentTo(initial);
         }
     }
