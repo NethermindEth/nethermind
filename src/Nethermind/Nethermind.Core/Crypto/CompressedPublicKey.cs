1e198bb3d95d011b0ea27046466edef43a19df38
MarekM25 <marekm2504@gmail.com>
2022-01-10 12:48:01 +0100

    build fix

--- src/Nethermind/Nethermind.Core/Crypto/CompressedPublicKey.cs
+++ src/Nethermind/Nethermind.Core/Crypto/CompressedPublicKey.cs
@@ -19,57 +19,59 @@ using System;
 using System.Runtime.InteropServices;
 using Nethermind.Core.Extensions;
 
-namespace Nethermind.Core.Crypto;
-
-public class CompressedPublicKey : IEquatable<CompressedPublicKey>
+namespace Nethermind.Core.Crypto
 {
-    public const int LengthInBytes = 33;
 
-    public CompressedPublicKey(string? hexString)
-        : this(Core.Extensions.Bytes.FromHexString(hexString ?? throw new ArgumentNullException(nameof(hexString))))
+    public class CompressedPublicKey : IEquatable<CompressedPublicKey>
     {
-    }
-    
-    public CompressedPublicKey(ReadOnlySpan<byte> bytes)
-    {
-        if (bytes.Length != LengthInBytes)
+        public const int LengthInBytes = 33;
+
+        public CompressedPublicKey(string? hexString)
+            : this(Core.Extensions.Bytes.FromHexString(hexString ?? throw new ArgumentNullException(nameof(hexString))))
         {
-            throw new ArgumentException($"{nameof(CompressedPublicKey)} should be {LengthInBytes} bytes long",
-                nameof(bytes));
         }
 
-        Bytes = bytes.Slice(bytes.Length - LengthInBytes, LengthInBytes).ToArray();
-    }
-
-    public byte[] Bytes { get; }
-
-    public bool Equals(CompressedPublicKey? other)
-    {
-        if (other is null)
+        public CompressedPublicKey(ReadOnlySpan<byte> bytes)
         {
-            return false;
+            if (bytes.Length != LengthInBytes)
+            {
+                throw new ArgumentException($"{nameof(CompressedPublicKey)} should be {LengthInBytes} bytes long",
+                    nameof(bytes));
+            }
+
+            Bytes = bytes.Slice(bytes.Length - LengthInBytes, LengthInBytes).ToArray();
         }
 
-        return Core.Extensions.Bytes.AreEqual(Bytes, other.Bytes);
-    }
+        public byte[] Bytes { get; }
 
-    public override bool Equals(object? obj)
-    {
-        return Equals(obj as CompressedPublicKey);
-    }
+        public bool Equals(CompressedPublicKey? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
 
-    public override int GetHashCode()
-    {
-        return MemoryMarshal.Read<int>(Bytes);
-    }
+            return Core.Extensions.Bytes.AreEqual(Bytes, other.Bytes);
+        }
 
-    public override string ToString()
-    {
-        return Bytes.ToHexString(true);
-    }
-    
-    public string ToString(bool with0X)
-    {
-        return Bytes.ToHexString(with0X);
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as CompressedPublicKey);
+        }
+
+        public override int GetHashCode()
+        {
+            return MemoryMarshal.Read<int>(Bytes);
+        }
+
+        public override string ToString()
+        {
+            return Bytes.ToHexString(true);
+        }
+
+        public string ToString(bool with0X)
+        {
+            return Bytes.ToHexString(with0X);
+        }
     }
 }
