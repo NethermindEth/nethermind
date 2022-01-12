7344bddc6e2043bef96d1830ff6e3e36fdb5a2e2
DCeleda <dceleda@hotmail.com>
2022-01-12 22:39:24 +0100

    Use StateSyncFeed for healing

---
+++ src/Nethermind/Nethermind.State.Test/SnapSync/HealerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Nethermind.Store.Test.SnapSync
+{
+    [TestFixture]
+    public class HealerTests
+    {
+    }
+}
---
+++ src/Nethermind/Nethermind.State/SnapSync/SnapProviderHelper.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Extensions;
+using Nethermind.Trie;
+using Nethermind.Trie.Pruning;
+
+namespace Nethermind.State.SnapSync
+{
+    internal static class SnapProviderHelper
+    {
+        internal static void FillBoundaryTree(TrieStore store, PatriciaTree tree, Keccak expectedRootHash, byte[][] proofs, Keccak startingHash, Keccak endHash)
+        {
+            if (tree == null)
+            {
+                throw new ArgumentNullException(nameof(tree));
+            }
+
+            Dictionary<Keccak, TrieNode> processed = new();
+            TrieNode parent = null;
+            int nibbleIndex = -1;
+            List<byte> path = new();
+            Span<byte> leftNibbles = stackalloc byte[64];
+            Nibbles.BytesToNibbleBytes(startingHash.Bytes, leftNibbles);
+            Span<byte> rightNibbles = stackalloc byte[64];
+            Nibbles.BytesToNibbleBytes(endHash.Bytes, rightNibbles);
+
+            for (int i = 0; i < proofs.Length; i++)
+            {
+                byte[] proof = proofs[i];
+
+                var node = new TrieNode(NodeType.Unknown, proof, true);
+                node.IsBoundaryProofNode = true;
+                node.ResolveNode(store);
+                node.ResolveKey(store, i == 0);
+                Keccak currentKeccak = node.Keccak;
+
+                if (processed.TryGetValue(currentKeccak, out TrieNode processedNode))
+                {
+                    node = processedNode;
+                }
+                else
+                {
+                    processed.Add(currentKeccak, node);
+                }
+
+                if (currentKeccak == expectedRootHash)
+                {
+                    if (tree.RootRef is null)
+                    {
+                        tree.RootRef = node;
+                    }
+                }
+                else
+                {
+                    TrieNode newChild = node.IsLeaf ? null : node;
+
+                    if (parent.IsExtension)
+                    {
+                        parent.SetChild(0, newChild);
+                    }
+                    if (parent.IsBranch)
+                    {
+                        int left = Bytes.Comparer.Compare(path.ToArray(), leftNibbles[0..path.Count()].ToArray()) == 0 ? leftNibbles[nibbleIndex] : -1;
+                        int right = Bytes.Comparer.Compare(path.ToArray(), rightNibbles[0..path.Count()].ToArray()) == 0 ? rightNibbles[nibbleIndex] : 16;
+
+                        for (byte ci = 0; ci < 16; ci++)
+                        {
+                            Keccak? childKeccak = parent.GetChildHash(ci);
+
+                            if (ci > left && ci < right)
+                            {
+                                parent.SetChild(ci, null);
+                            }
+
+                            if (childKeccak != null && childKeccak == currentKeccak)
+                            {
+                                parent.SetChild(ci, newChild);
+                                path.Add(ci);
+                            }
+                        }
+                    }
+                }
+
+                parent = node;
+
+
+                if (currentKeccak == expectedRootHash)
+                {
+                    nibbleIndex = -1;
+                    path = new();
+                }
+
+                if (node.IsExtension)
+                {
+                    nibbleIndex += node.Path.Length;
+                    path.AddRange(parent.Path);
+                }
+                else if (node.IsBranch)
+                {
+                    nibbleIndex++;
+                }
+            }
+        }
+    }
+}
