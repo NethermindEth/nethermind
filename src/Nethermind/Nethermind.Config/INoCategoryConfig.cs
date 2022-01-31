7d33377a5e88ebced170dd5054b9e468f28963c3
DCeleda <dceleda@hotmail.com>
2022-01-31 17:13:34 +0100

    Descriptions fix

--- src/Nethermind/Nethermind.Config/INoCategoryConfig.cs
+++ src/Nethermind/Nethermind.Config/INoCategoryConfig.cs
@@ -29,25 +29,25 @@ namespace Nethermind.Config
         [ConfigItem(Description = "Plugins directory")]
         public string PluginsDirectory { get; set; }
 
-        [ConfigItem(Description = "Sets the job name for metrics monitoring. Configurable via environment variable NETHERMIND_MONITORING_JOB", EnvironmentVariable = "NETHERMIND_MONITORING_JOB")]
+        [ConfigItem(Description = "Sets the job name for metrics monitoring.", EnvironmentVariable = "NETHERMIND_MONITORING_JOB")]
         public string MonitoringJob { get; set; }
 
-        [ConfigItem(Description = "Sets the default group name for metrics monitoring. Configurable via environment variable NETHERMIND_MONITORING_GROUP", EnvironmentVariable = "NETHERMIND_MONITORING_GROUP")]
+        [ConfigItem(Description = "Sets the default group name for metrics monitoring.", EnvironmentVariable = "NETHERMIND_MONITORING_GROUP")]
         public string MonitoringGroup { get; set; }
 
-        [ConfigItem(Description = "Sets the external IP for the node. Configurable via environment variable NETHERMIND_ENODE_IPADDRESS", EnvironmentVariable = "NETHERMIND_ENODE_IPADDRESS")]
+        [ConfigItem(Description = "Sets the external IP for the node.", EnvironmentVariable = "NETHERMIND_ENODE_IPADDRESS")]
         public string EnodeIpAddress { get; set; }
 
-        [ConfigItem(Description = "Enables Hive plugin used for executing Hive Ethereum Tests. Configurable via environment variable NETHERMIND_HIVE_ENABLED", EnvironmentVariable = "NETHERMIND_HIVE_ENABLED")]
+        [ConfigItem(Description = "Enables Hive plugin used for executing Hive Ethereum Tests.", EnvironmentVariable = "NETHERMIND_HIVE_ENABLED")]
         public bool HiveEnabled { get; set; }
 
-        [ConfigItem(Description = "Defines default URL for JSON RPC. Configurable via environment variable NETHERMIND_URL", EnvironmentVariable = "NETHERMIND_URL")]
+        [ConfigItem(Description = "Defines default URL for JSON RPC.", EnvironmentVariable = "NETHERMIND_URL")]
         public string Url { get; set; }
 
-        [ConfigItem(Description = "Defines CORS origins for JSON RPC. Configurable via environment variable NETHERMIND_CORS_ORIGINS", DefaultValue = "*", EnvironmentVariable = "NETHERMIND_CORS_ORIGINS")]
+        [ConfigItem(Description = "Defines CORS origins for JSON RPC.", DefaultValue = "*", EnvironmentVariable = "NETHERMIND_CORS_ORIGINS")]
         public string CorsOrigins { get; set; }
 
-        [ConfigItem(Description = "Defines host value for CLI function \"switchLocal\". Configurable via environment variable NETHERMIND_CLI_SWITCH_LOCAL", DefaultValue = "http://localhost", EnvironmentVariable = "NETHERMIND_CLI_SWITCH_LOCAL")]
+        [ConfigItem(Description = "Defines host value for CLI function \"switchLocal\".", DefaultValue = "http://localhost", EnvironmentVariable = "NETHERMIND_CLI_SWITCH_LOCAL")]
         public string CliSwitchLocal { get; set; }
     }
 }
