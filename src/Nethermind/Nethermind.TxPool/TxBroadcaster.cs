824bafa54ff244f9da8d9ab233abbc65d734cb6b
Marcin Sobczak <marcindsobczak@gmail.com>
2022-01-31 13:23:17 +0100

    cosmetic

--- src/Nethermind/Nethermind.TxPool/TxBroadcaster.cs
+++ src/Nethermind/Nethermind.TxPool/TxBroadcaster.cs
@@ -18,7 +18,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
