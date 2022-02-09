157f82087c6674a766a9958a94e9ebd2efe71722
Kristof Gazso <kristof.gazso@gmail.com>
2022-02-09 10:34:27 +0000

    [AA] Only allow max 10 User Operation per sender, Implement op fee replacement (#3799)

--- src/Nethermind/Nethermind.AccountAbstraction/Source/CompareReplacedUserOperationByFee.cs
+++ src/Nethermind/Nethermind.AccountAbstraction/Source/CompareReplacedUserOperationByFee.cs
@@ -17,22 +17,31 @@
 
 using System.Collections.Generic;
 using Nethermind.AccountAbstraction.Data;
+using Nethermind.Int256;
 
 namespace Nethermind.AccountAbstraction.Source
 {
-    public class CompareUserOperationsByHash : IComparer<UserOperation>
+    public class CompareReplacedUserOperationByFee : IComparer<UserOperation?>
     {
-        public static readonly CompareUserOperationsByHash Instance = new();
+        public static readonly CompareReplacedUserOperationByFee Instance = new();
         
-        private CompareUserOperationsByHash() { }
-
+        // To replace old user operation, new user operation needs to have fee higher by at least 10% (1/10) of current fee.
+        // It is required to avoid acceptance and propagation of user operation with almost the same fee as replaced one.
+        private const int PartOfFeeRequiredToIncrease = 10;
+        
+        private CompareReplacedUserOperationByFee() { }
+        
         public int Compare(UserOperation? x, UserOperation? y)
         {
             if (ReferenceEquals(x, y)) return 0;
             if (ReferenceEquals(null, y)) return 1;
             if (ReferenceEquals(null, x)) return -1;
 
-            return x.Hash.CompareTo(y.Hash);
+            y.MaxFeePerGas.Divide(PartOfFeeRequiredToIncrease, out UInt256 bumpMaxFeePerGas);
+            if (y.MaxFeePerGas + bumpMaxFeePerGas > x.MaxFeePerGas) return 1;
+
+            y.MaxPriorityFeePerGas.Divide(PartOfFeeRequiredToIncrease, out UInt256 bumpMaxPriorityFeePerGas);
+            return (y.MaxPriorityFeePerGas + bumpMaxPriorityFeePerGas).CompareTo(x.MaxPriorityFeePerGas);
         }
     }
 }
