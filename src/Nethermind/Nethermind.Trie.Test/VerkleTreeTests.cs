682e722e5523439b3e2c158fee599ba252549edd
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-02-11 12:59:35 +0530

    add addition code split tests

--- src/Nethermind/Nethermind.Trie.Test/VerkleTreeTests.cs
+++ src/Nethermind/Nethermind.Trie.Test/VerkleTreeTests.cs
@@ -15,6 +15,7 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 // 
 
+using System;
 using FluentAssertions;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
@@ -270,7 +271,7 @@ public class VerkleTreeTests
     }
 
     [Test]
-    public void Set_Code_Edge_Cases()
+    public void Set_Code_Edge_Cases_1()
     {
         VerkleStateTree tree = new(LimboLogs.Instance);
         byte[] code2 =
@@ -294,6 +295,86 @@ public class VerkleTreeTests
         byte[] thirdCodeChunk =
         {
             1, 62, 63, 64, 65
+        };
+        
+        byte[] key = tree.GetTreeKeyForCodeChunk(TestItem.AddressA, 0);
+        byte[] value = tree.GetValue(key);
+        value.Should().BeEquivalentTo(firstCodeChunk);
+        
+        key = tree.GetTreeKeyForCodeChunk(TestItem.AddressA, 1);
+        value = tree.GetValue(key);
+        value.Should().BeEquivalentTo(secondCodeChunk); 
+        
+        key = tree.GetTreeKeyForCodeChunk(TestItem.AddressA, 2);
+        value = tree.GetValue(key);
+        value.Slice(0, 5).Should().BeEquivalentTo(thirdCodeChunk); 
+    }
+    
+    [Test]
+    public void Set_Code_Edge_Cases_2()
+    {
+        VerkleStateTree tree = new(LimboLogs.Instance);
+        byte[] code2 =
+        {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+            28, 29, 126, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+            54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65
+        };
+        tree.SetCode(TestItem.AddressA, code2);
+
+        byte[] firstCodeChunk =
+        {
+            0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+            28, 29, 126
+        };
+        byte[] secondCodeChunk =
+        {
+            31, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+            54, 55, 56, 57, 58, 59, 60, 61
+        };
+        byte[] thirdCodeChunk =
+        {
+            0, 62, 63, 64, 65
+        };
+        
+        byte[] key = tree.GetTreeKeyForCodeChunk(TestItem.AddressA, 0);
+        byte[] value = tree.GetValue(key);
+        value.Should().BeEquivalentTo(firstCodeChunk);
+        
+        key = tree.GetTreeKeyForCodeChunk(TestItem.AddressA, 1);
+        value = tree.GetValue(key);
+        value.Should().BeEquivalentTo(secondCodeChunk); 
+        
+        key = tree.GetTreeKeyForCodeChunk(TestItem.AddressA, 2);
+        value = tree.GetValue(key);
+        value.Slice(0, 5).Should().BeEquivalentTo(thirdCodeChunk); 
+    }
+    
+    [Test]
+    public void Set_Code_Edge_Cases_3()
+    {
+        VerkleStateTree tree = new(LimboLogs.Instance);
+        byte[] code2 =
+        {
+            95, 1, 96, 3, 4, 97, 6, 7, 8, 98, 10, 11, 12, 13, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 112, 113,
+            114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+            54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65
+        };
+        tree.SetCode(TestItem.AddressA, code2);
+
+        byte[] firstCodeChunk =
+        {
+            0, 95, 1, 96, 3, 4, 97, 6, 7, 8, 98, 10, 11, 12, 13, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 112, 113,
+            114, 115, 116
+        };
+        byte[] secondCodeChunk =
+        {
+            19, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+            54, 55, 56, 57, 58, 59, 60, 61
+        };
+        byte[] thirdCodeChunk =
+        {
+            0, 62, 63, 64, 65
         };
         
         byte[] key = tree.GetTreeKeyForCodeChunk(TestItem.AddressA, 0);
