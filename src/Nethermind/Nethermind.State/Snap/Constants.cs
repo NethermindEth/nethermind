7a74ee7b258567b636662d1bb1820612de1c373e
DCeleda <dceleda@hotmail.com>
2022-03-24 20:24:44 +0000

    Pivot Header

---
+++ src/Nethermind/Nethermind.State/Snap/Constants.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nethermind.State.Snap
+{
+    public class Constants
+    {
+        public const int MaxDistanceFromHead = 128;
+    }
+}
