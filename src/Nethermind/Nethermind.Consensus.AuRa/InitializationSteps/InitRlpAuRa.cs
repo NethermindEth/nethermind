5a65821a9f8b8f231ecc462d878f74afff2d5f02
Jorge Mederos <jmederosalvarado@gmail.com>
2022-04-05 10:33:13 -0400

    remove non default `BlockInfoDecoder` initializatoin

--- src/Nethermind/Nethermind.Consensus.AuRa/InitializationSteps/InitRlpAuRa.cs
+++ src/Nethermind/Nethermind.Consensus.AuRa/InitializationSteps/InitRlpAuRa.cs
@@ -28,7 +28,6 @@ namespace Nethermind.Consensus.AuRa.InitializationSteps
 
         public override Task Execute(CancellationToken cancellationToken)
         {
-            Rlp.Decoders[typeof(BlockInfo)] = new BlockInfoDecoder();
             return base.Execute(cancellationToken);
         }
     }
