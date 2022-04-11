d48d33cc667694167bffa1c8ef23db9b4aec82ec
Jorge Mederos <46798594+jmederosalvarado@users.noreply.github.com>
2022-04-11 04:40:13 -0400

    Blockinfo metadata (#3918)
    * Add `FinalizationStatus` to `BlockInfo`
    
    * Add tests for block info decoder backwards compatibility
    
    * Change `FinalizationStatus` to `Metadata` and drop finalization check in decoder
    
    * Add flags to roundtrip test
    
    * fix setting finalization to false
    
    * remove non default `BlockInfoDecoder` initializatoin
    
    * Remove special rlp initialization for AuRa

--- src/Nethermind/Nethermind.Core.Test/Encoding/BlockInfoDecoderTests.cs
+++ src/Nethermind/Nethermind.Core.Test/Encoding/BlockInfoDecoderTests.cs
@@ -31,15 +31,20 @@ namespace Nethermind.Core.Test.Encoding
         {
             Roundtrip(valueDecode);
         }
-        
-        [TestCase(true)]
-        [TestCase(false)]
-        public void Can_do_roundtrip_with_finalization(bool valueDecode)
+
+        [TestCase(true, true, true)]
+        [TestCase(true, true, false)]
+        [TestCase(true, false, true)]
+        [TestCase(true, false, false)]
+        [TestCase(false, true, true)]
+        [TestCase(false, true, false)]
+        [TestCase(false, false, true)]
+        [TestCase(false, false, false)]
+        public void Is_Backwards_compatible(bool valueDecode, bool chainWithFinalization, bool isFinalized)
         {
-            Rlp.Decoders[typeof(BlockInfo)] = new BlockInfoDecoder(true);
-            Roundtrip(valueDecode);
+            RoundtripBackwardsCompatible(valueDecode, chainWithFinalization, isFinalized);
         }
-        
+
         [Test]
         public void Can_handle_nulls()
         {
@@ -52,13 +57,52 @@ namespace Nethermind.Core.Test.Encoding
         {
             BlockInfo blockInfo = new(TestItem.KeccakA, 1);
             blockInfo.WasProcessed = true;
+            blockInfo.IsFinalized = true;
+            blockInfo.Metadata |= BlockMetadata.Invalid;
 
             Rlp rlp = Rlp.Encode(blockInfo);
             BlockInfo decoded = valueDecode ?  Rlp.Decode<BlockInfo>(rlp.Bytes.AsSpan()) : Rlp.Decode<BlockInfo>(rlp);
             
             Assert.True(decoded.WasProcessed, "0 processed");
+            Assert.True((decoded.Metadata & BlockMetadata.Finalized) == BlockMetadata.Finalized, "metadata finalized");
+            Assert.True((decoded.Metadata & BlockMetadata.Invalid) == BlockMetadata.Invalid, "metadata invalid");
             Assert.AreEqual(TestItem.KeccakA, decoded.BlockHash, "block hash");
             Assert.AreEqual(UInt256.One, decoded.TotalDifficulty, "difficulty");
+        }
+
+        private static void RoundtripBackwardsCompatible(bool valueDecode, bool chainWithFinalization, bool isFinalized)
+        {
+            BlockInfo blockInfo = new(TestItem.KeccakA, 1);
+            blockInfo.WasProcessed = true;
+            blockInfo.IsFinalized = isFinalized;
+
+            Rlp rlp = BlockInfoEncodeDeprecated(blockInfo, chainWithFinalization);
+            BlockInfo decoded = valueDecode ? Rlp.Decode<BlockInfo>(rlp.Bytes.AsSpan()) : Rlp.Decode<BlockInfo>(rlp);
+
+            Assert.True(decoded.WasProcessed, "0 processed");
+            Assert.AreEqual(chainWithFinalization && isFinalized, decoded.IsFinalized, "finalized");
+            Assert.AreEqual(TestItem.KeccakA, decoded.BlockHash, "block hash");
+            Assert.AreEqual(UInt256.One, decoded.TotalDifficulty, "difficulty");
+        }
+
+        public static Rlp BlockInfoEncodeDeprecated(BlockInfo? item, bool chainWithFinalization)
+        {
+            if (item == null)
+            {
+                return Rlp.OfEmptySequence;
+            }
+
+            Rlp[] elements = new Rlp[chainWithFinalization ? 4 : 3];
+            elements[0] = Rlp.Encode(item.BlockHash);
+            elements[1] = Rlp.Encode(item.WasProcessed);
+            elements[2] = Rlp.Encode(item.TotalDifficulty);
+
+            if (chainWithFinalization)
+            {
+                elements[3] = Rlp.Encode(item.IsFinalized);
+            }
+
+            return Rlp.Encode(elements);
         }
     }
 }
