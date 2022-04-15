86b37ce5f970b363b55d250473b9e5eacc7e8dc7
Jorge Mederos <jmederosalvarado@gmail.com>
2022-04-15 17:33:25 -0400

    add some comments in skipped tests

--- src/Nethermind/Nethermind.Merge.Gnosis/GnosisMergePluginTests.cs
+++ src/Nethermind/Nethermind.Merge.Gnosis/GnosisMergePluginTests.cs
@@ -105,18 +105,22 @@ namespace Nethermind.Merge.Gnosis.Tests
         [Test]
         public override async Task engine_forkchoiceUpdatedV1_with_payload_attributes_should_create_block_on_top_of_genesis_and_not_change_head()
         {
-            // override this test for now, it fails when asserting the blockHash of produced block equals a hardcoded precomputed one.
+            // Override this test for now, it fails when asserting the blockHash of produced block equals a hardcoded precomputed one.
             // This happens because for this AuRa chain the blockHash includes AuRa specific fields, hence the hash for genesis is different
-            // causing all subsequent blocks to have a different blockHash
+            // causing all subsequent blocks to have a different blockHash.
+            // You can verify this by removing `SealEngineType = Nethermind.Core.SealEngineType.AuRa;` from the constructor of
+            // the test class above and rerunning the tests.
             await Task.CompletedTask;
         }
 
         [Test]
         public override async Task processing_block_should_serialize_valid_responses()
         {
-            // override this test for now, it fails when asserting the blockHash of produced block equals a hardcoded precomputed one.
+            // Override this test for now, it fails when asserting the blockHash of produced block equals a hardcoded precomputed one.
             // This happens because for this AuRa chain the blockHash includes AuRa specific fields, hence the hash for genesis is different
-            // causing all subsequent blocks to have a different blockHash
+            // causing all subsequent blocks to have a different blockHash.
+            // You can verify this by removing `SealEngineType = Nethermind.Core.SealEngineType.AuRa;` from the constructor of
+            // the test class above and rerunning the tests.
             await Task.CompletedTask;
         }
     }
