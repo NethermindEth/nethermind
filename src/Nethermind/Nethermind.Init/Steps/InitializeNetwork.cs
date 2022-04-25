d5698d1002c286055339fa50c506bcb831f2f8e8
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2022-04-25 10:22:27 +0200

    Fix/sync peer timer (#3977)
    * fix SyncPeerPool timer Initialize
    
    * fix StateSyncFeed timeout check

--- src/Nethermind/Nethermind.Init/Steps/InitializeNetwork.cs
+++ src/Nethermind/Nethermind.Init/Steps/InitializeNetwork.cs
@@ -112,7 +112,7 @@ namespace Nethermind.Init.Steps
 
             int maxPeersCount = _networkConfig.ActivePeersMaxCount;
             int maxPriorityPeersCount = _networkConfig.PriorityPeersMaxCount;
-            _api.SyncPeerPool = new SyncPeerPool(_api.BlockTree!, _api.NodeStatsManager!, maxPeersCount, maxPriorityPeersCount, _api.LogManager);
+            _api.SyncPeerPool = new SyncPeerPool(_api.BlockTree!, _api.NodeStatsManager!, maxPeersCount, maxPriorityPeersCount, SyncPeerPool.DefaultUpgradeIntervalInMs, _api.LogManager);
             _api.DisposeStack.Push(_api.SyncPeerPool);
 
             SyncProgressResolver syncProgressResolver = new(
