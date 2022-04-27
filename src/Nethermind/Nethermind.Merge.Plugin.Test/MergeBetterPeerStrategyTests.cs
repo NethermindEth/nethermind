8c06776bacf042ab868d296dacbc467ad1157639
hrugved <whrugved@gmail.com>
2022-04-28 01:45:15 +0530

    Add MergeBetterPeerStrategyTests

--- src/Nethermind/Nethermind.Merge.Plugin.Test/MergeBetterPeerStrategyTests.cs
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/MergeBetterPeerStrategyTests.cs
@@ -15,9 +15,64 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 // 
 
+using Nethermind.Blockchain.Synchronization;
+using Nethermind.Consensus;
+using Nethermind.Int256;
+using Nethermind.Logging;
+using Nethermind.Synchronization;
+using Nethermind.Synchronization.ParallelSync;
+using NSubstitute;
+using NUnit.Framework;
+
 namespace Nethermind.Merge.Plugin.Test;
 
 public class MergeBetterPeerStrategyTests
 {
+    private const long TTD = 5;
+    IPoSSwitcher _poSSwitcher;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _poSSwitcher= Substitute.For<IPoSSwitcher>();
+        _poSSwitcher.TerminalTotalDifficulty.Returns((UInt256)TTD);
+    }   
+    
+    [TestCase(7,2,6,4, -1)]
+    [TestCase(7,4,6,4, 0)]
+    [TestCase(6,4,7,2, 1)]
+    [TestCase(3,4,6,2, -1)]
+    [TestCase(3,2,3,4, 0)]
+    [TestCase(6,2,3,4, 1)]
+    public void Compare_return_expected_results(long totalDifficulty, long number, long peerTotalDifficulty, long peerNumber, int expectedResult)
+    {
+        TotalDifficultyBasedBetterPeerStrategy preMergeBetterPeerStrategy = new(null, LimboLogs.Instance);
+        MergeBetterPeerStrategy betterPeerStrategy =
+            new(preMergeBetterPeerStrategy, null, _poSSwitcher, LimboLogs.Instance);
+        ISyncPeer syncPeer = Substitute.For<ISyncPeer>();
+        syncPeer.TotalDifficulty.Returns((UInt256)peerTotalDifficulty);
+        syncPeer.HeadNumber.Returns(peerNumber);
+        Assert.AreEqual(expectedResult, betterPeerStrategy.Compare(((UInt256)totalDifficulty, number), syncPeer));
+    }
+    
+    [TestCase(6,4,7,2, false)]
+    [TestCase(6,2,7,2, false)]
+    [TestCase(7,2,7,4, true)]
+    [TestCase(3,4,5,2, true)]
+    [TestCase(3,2,3,4, false)]
+    [TestCase(4,2,3,4, false)]
+    public void IsBetterThanLocalChain_return_expected_results(long chainDifficulty, long bestFullBlock, long peerTotalDifficulty, long peerNumber, bool expectedResult)
+    {
+        ISyncProgressResolver resolver = Substitute.For<ISyncProgressResolver>();
+        resolver.ChainDifficulty.Returns((UInt256)chainDifficulty);
+        resolver.FindBestFullBlock().Returns(bestFullBlock);
+        ISyncPeer syncPeer = Substitute.For<ISyncPeer>();
+        syncPeer.TotalDifficulty.Returns((UInt256)peerTotalDifficulty);
+        syncPeer.HeadNumber.Returns(peerNumber);
+        TotalDifficultyBasedBetterPeerStrategy preMergeBetterPeerStrategy = new(resolver, LimboLogs.Instance);
+        MergeBetterPeerStrategy betterPeerStrategy =
+            new(preMergeBetterPeerStrategy, resolver, _poSSwitcher, LimboLogs.Instance);
+        Assert.AreEqual(expectedResult, betterPeerStrategy.IsBetterThanLocalChain(((UInt256)peerTotalDifficulty, peerNumber)));
+    }
     
 }
