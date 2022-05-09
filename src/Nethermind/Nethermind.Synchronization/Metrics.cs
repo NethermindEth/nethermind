38443d00f6e9c4dd42e70d3ad8f1381eb1e95a2b
Marcin Sobczak <marcindsobczak@gmail.com>
2022-05-09 12:42:24 +0200

    one more post merge fix

--- src/Nethermind/Nethermind.Synchronization/Metrics.cs
+++ src/Nethermind/Nethermind.Synchronization/Metrics.cs
@@ -56,15 +56,6 @@ namespace Nethermind.Synchronization
         [Description("Synced bytecodes via SNAP Sync")]
         public static long SnapSyncedCodes;
 
-        [Description("Synced accounts via SNAP Sync")]
-        public static long SnapSyncedAccounts;
-
-        [Description("Synced storage slots via SNAP Sync")]
-        public static long SnapSyncedStorageSlots;
-
-        [Description("Synced bytecodes via SNAP Sync")]
-        public static long SnapSyncedCodes;
-
         [Description("Number of sync peers.")]
         public static long SyncPeers;
         
