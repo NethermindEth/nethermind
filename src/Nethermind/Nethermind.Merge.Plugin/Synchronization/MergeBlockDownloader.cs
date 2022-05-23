185e6d8c6c338c012f611166459ed90bc115e050
MarekM25 <marekm2504@gmail.com>
2022-05-23 14:31:03 +0200

    removed beacon pivot exists check

--- src/Nethermind/Nethermind.Merge.Plugin/Synchronization/MergeBlockDownloader.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Synchronization/MergeBlockDownloader.cs
@@ -103,7 +103,7 @@ namespace Nethermind.Merge.Plugin.Synchronization
                     $"MergeBlockDownloader GetCurrentNumber: currentNumber {currentNumber}, beaconPivotExists: {_beaconPivot.BeaconPivotExists()}, BestSuggestedBody: {_blockTree.BestSuggestedBody.Number}, BestKnownNumber: {_blockTree.BestKnownNumber}, BestPeer: {bestPeer}, BestKnownBeaconNumber {_blockTree.BestKnownBeaconNumber}");
 
             bool HasMoreToSync()
-                => _beaconPivot.BeaconPivotExists() && currentNumber < _blockTree.BestKnownBeaconNumber &&
+                => currentNumber < _blockTree.BestKnownBeaconNumber &&
                    bestPeer.HeadNumber > _blockTree.BestKnownNumber;
 
             while (HasMoreToSync())
