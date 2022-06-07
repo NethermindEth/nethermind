29d8ed606dfd4f2d23e218217c8cf6d2826da3f9
lukasz.rozmej <lukasz.rozmej@gmail.com>
2022-06-07 15:16:23 +0200

    fix sys.argv index

--- scripts/testing/hiveTestResultPrinter.py
+++ scripts/testing/hiveTestResultPrinter.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 env_file = os.getenv('GITHUB_ENV')
-results_name = glob.glob(sys.argv[0])
+results_name = glob.glob(sys.argv[1])
 results = open(results_name[0], "rt")
 res_obj = json.loads(results.read())
 passed = []
