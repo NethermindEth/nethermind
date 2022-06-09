dbab75e725d5fbdadca3c3d613f62050d65d5e17
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2022-06-09 21:05:40 +0200

    Do not return non-canonical receipts. (#4138)
    Move code for logIndexStart from eth_getTransactionReceipt to BlockchainBridge.GetReceiptAndEffectiveGasPrice

--- src/Nethermind/Nethermind.Facade/BlockchainBridge.cs
+++ src/Nethermind/Nethermind.Facade/BlockchainBridge.cs
@@ -90,30 +90,25 @@ namespace Nethermind.Facade
 
         public bool IsMining { get; }
 
-        public (TxReceipt Receipt, UInt256? EffectiveGasPrice) GetReceiptAndEffectiveGasPrice(Keccak txHash)
+        public (TxReceipt Receipt, UInt256? EffectiveGasPrice, int LogIndexStart) GetReceiptAndEffectiveGasPrice(Keccak txHash)
         {
             Keccak blockHash = _receiptFinder.FindBlockHash(txHash);
             if (blockHash != null)
             {
                 Block? block = _processingEnv.BlockTree.FindBlock(blockHash, BlockTreeLookupOptions.RequireCanonical);
-                bool isNotCanonical = block is null;
-                if (isNotCanonical)
+                if (block is not null)
                 {
-                    block = _processingEnv.BlockTree.FindBlock(blockHash, BlockTreeLookupOptions.TotalDifficultyNotNeeded);
+                    TxReceipt[] txReceipts = _receiptFinder.Get(block);
+                    TxReceipt txReceipt = txReceipts.ForTransaction(txHash);
+                    int logIndexStart = txReceipts.GetBlockLogFirstIndex(txReceipt.Index);
+                    Transaction tx = block.Transactions[txReceipt.Index];
+                    bool is1559Enabled = _specProvider.GetSpec(block.Number).IsEip1559Enabled;
+                    UInt256 effectiveGasPrice = tx.CalculateEffectiveGasPrice(is1559Enabled, block.Header.BaseFeePerGas);
+                    return (txReceipt, effectiveGasPrice, logIndexStart);
                 }
-                TxReceipt txReceipt = _receiptFinder.Get(block).ForTransaction(txHash);
-                if (isNotCanonical && !txReceipt.Removed)
-                {
-                    return (null, null);
-                }
-                Transaction tx = block?.Transactions[txReceipt.Index];
-                bool is1559Enabled = _specProvider.GetSpec(block.Number).IsEip1559Enabled;
-                UInt256 effectiveGasPrice = tx.CalculateEffectiveGasPrice(is1559Enabled, block.Header.BaseFeePerGas);
-                
-                return (txReceipt, effectiveGasPrice);
             }
 
-            return (null, null);
+            return (null, null, 0);
         }
 
         public (TxReceipt Receipt, Transaction Transaction, UInt256? baseFee) GetTransaction(Keccak txHash)
