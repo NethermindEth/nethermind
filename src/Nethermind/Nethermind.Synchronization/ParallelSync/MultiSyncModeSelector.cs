6c90c5ef3ad449b3f05e00e1af8ff8dc0175496c
lukasz.rozmej <lukasz.rozmej@gmail.com>
2022-06-10 13:32:27 +0200

    MOAR logs

--- src/Nethermind/Nethermind.Synchronization/ParallelSync/MultiSyncModeSelector.cs
+++ src/Nethermind/Nethermind.Synchronization/ParallelSync/MultiSyncModeSelector.cs
@@ -224,8 +224,8 @@ namespace Nethermind.Synchronization.ParallelSync
                         string stateString = BuildStateString(best);
                         if (_logger.IsInfo) _logger.Info($"Changing state {Current} to {newModes} at {stateString}");
                     }
-                    
-                    if (_logger.IsTrace) _logger.Trace($"Best: {JsonConvert.SerializeObject(best)}");
+
+                    if (_logger.IsTrace) _logger.Trace($"Best: {JsonConvert.SerializeObject(best, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Serialize })}");
 
                     if ((newModes & (SyncMode.Full | SyncMode.WaitingForBlock)) != SyncMode.None
                         && (Current & (SyncMode.Full | SyncMode.WaitingForBlock)) == SyncMode.None)
