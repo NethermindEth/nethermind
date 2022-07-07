4d877d0f5093c920111ae067fe0f0b085436c3f6
Amirul Ashraf <asdacap@gmail.com>
2022-07-07 22:26:43 +0800

    Using eth timeout instead

--- src/Nethermind/Nethermind.Synchronization/Timeouts.cs
+++ src/Nethermind/Nethermind.Synchronization/Timeouts.cs
@@ -22,6 +22,5 @@ namespace Nethermind.Synchronization
     public class Timeouts
     {
         public static readonly TimeSpan Eth = TimeSpan.FromSeconds(10);
-        public static readonly TimeSpan RefreshDifficulty = TimeSpan.FromSeconds(8);
     }
 }
