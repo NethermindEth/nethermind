c0bdaa1d07e57efcace4fedaafeecd6dd050d829
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-07-13 22:33:11 +0530

    fix get length

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V62/Messages/BlockBodiesMessageSerializer.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V62/Messages/BlockBodiesMessageSerializer.cs
@@ -1,19 +1,21 @@
 ﻿//  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
+using System;
+using System.Configuration;
 using System.Linq;
 using DotNetty.Buffers;
 using Nethermind.Core;
@@ -23,6 +25,9 @@ namespace Nethermind.Network.P2P.Subprotocols.Eth.V62.Messages
 {
     public class BlockBodiesMessageSerializer : IZeroInnerMessageSerializer<BlockBodiesMessage>
     {
+        private readonly TxDecoder _txDecoder = new();
+        private readonly HeaderDecoder _headerDecoder = new();
+
         public byte[] Serialize(BlockBodiesMessage message)
         {
             return Rlp.Encode(message.Bodies.Select(b => b == null
@@ -47,9 +52,37 @@ namespace Nethermind.Network.P2P.Subprotocols.Eth.V62.Messages
 
         public int GetLength(BlockBodiesMessage message, out int contentLength)
         {
-            byte[] oldWay = Serialize(message);
-            contentLength = oldWay.Length;
+            int totalLength = message.Bodies.Select(b => b == null
+                ? Rlp.OfEmptySequence.Length
+                : Rlp.LengthOfSequence(
+                    Rlp.LengthOfSequence(GetTxLength(b.Transactions, RlpBehaviors.None)) +
+                    Rlp.LengthOfSequence(GetUnclesLength(b.Uncles, RlpBehaviors.None)))
+            ).Sum();
+            contentLength = Rlp.LengthOfSequence(totalLength);
+            Console.WriteLine(contentLength);
             return contentLength;
+        }
+
+        private int GetTxLength(Transaction[] transactions, RlpBehaviors rlpBehaviors)
+        {
+            int txLength = 0;
+            for (int i = 0; i < transactions.Length; i++)
+            {
+                txLength += _txDecoder.GetLength(transactions[i], rlpBehaviors);
+            }
+
+            return txLength;
+        }
+
+        private int GetUnclesLength(BlockHeader[] headers, RlpBehaviors rlpBehaviors)
+        {
+            int unclesLength = 0;
+            for (int i = 0; i < headers.Length; i++)
+            {
+                unclesLength += _headerDecoder.GetLength(headers[i], rlpBehaviors);
+            }
+
+            return unclesLength;
         }
 
         public static BlockBodiesMessage Deserialize(RlpStream rlpStream)
