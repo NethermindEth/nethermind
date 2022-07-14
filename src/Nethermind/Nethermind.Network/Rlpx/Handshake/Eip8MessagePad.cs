5ada832ba1b7a11ecf9203ddabaa8ddb11973c11
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-07-14 12:38:13 +0530

    update message pad

--- src/Nethermind/Nethermind.Network/Rlpx/Handshake/Eip8MessagePad.cs
+++ src/Nethermind/Nethermind.Network/Rlpx/Handshake/Eip8MessagePad.cs
@@ -1,19 +1,20 @@
 ﻿//  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
+using DotNetty.Buffers;
 using Nethermind.Core.Extensions;
 using Nethermind.Crypto;
 
@@ -32,6 +33,12 @@ namespace Nethermind.Network.Rlpx.Handshake
         {
             byte[] padding = _cryptoRandom.GenerateRandomBytes(100 + _cryptoRandom.NextInt(201));
             return Bytes.Concat(message, padding);
+        }
+
+        public void Pad(IByteBuffer message)
+        {
+            byte[] padding = _cryptoRandom.GenerateRandomBytes(100 + _cryptoRandom.NextInt(201));
+            message.WriteBytes(padding);
         }
     }
 }
