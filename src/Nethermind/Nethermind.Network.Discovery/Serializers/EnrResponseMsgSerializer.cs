f56be883b4f23131ed044382ed47400c40c3e959
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-07-18 18:01:25 +0530

    update serializers

--- src/Nethermind/Nethermind.Network.Discovery/Serializers/EnrResponseMsgSerializer.cs
+++ src/Nethermind/Nethermind.Network.Discovery/Serializers/EnrResponseMsgSerializer.cs
@@ -1,29 +1,31 @@
 ﻿//  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
+using DotNetty.Buffers;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
 using Nethermind.Crypto;
 using Nethermind.Network.Discovery.Messages;
 using Nethermind.Network.Enr;
+using Nethermind.Network.P2P;
 using Nethermind.Serialization.Rlp;
 
 namespace Nethermind.Network.Discovery.Serializers;
 
-public class EnrResponseMsgSerializer : DiscoveryMsgSerializerBase, IMessageSerializer<EnrResponseMsg>
+public class EnrResponseMsgSerializer : DiscoveryMsgSerializerBase, IZeroMessageSerializer<EnrResponseMsg>
 {
     private readonly NodeRecordSigner _nodeRecordSigner;
 
@@ -33,7 +35,7 @@ public class EnrResponseMsgSerializer : DiscoveryMsgSerializerBase, IMessageSeri
         _nodeRecordSigner = new NodeRecordSigner(ecdsa, nodeKey.Generate());
     }
 
-    public byte[] Serialize(EnrResponseMsg msg)
+    public void Serialize(IByteBuffer byteBuffer, EnrResponseMsg msg)
     {
         int contentLength = Rlp.LengthOfKeccakRlp;
         contentLength += msg.NodeRecord.GetRlpLengthWithSignature();
@@ -45,13 +47,13 @@ public class EnrResponseMsgSerializer : DiscoveryMsgSerializerBase, IMessageSeri
         msg.NodeRecord.Encode(rlpStream);
 
         byte[] serializedMsg = Serialize((byte)msg.MsgType, rlpStream.Data);
-        return serializedMsg;
+        byteBuffer.WriteBytes(serializedMsg);
     }
 
-    public EnrResponseMsg Deserialize(byte[] msgBytes)
+    public EnrResponseMsg Deserialize(IByteBuffer msgBytes)
     {
-        (PublicKey? farPublicKey, _, byte[]? data) = PrepareForDeserialization(msgBytes);
-        RlpStream rlpStream = data.AsRlpStream();
+        (PublicKey? farPublicKey, _, IByteBuffer? data) = PrepareForDeserialization(msgBytes);
+        NettyRlpStream rlpStream = new(data);
         rlpStream.ReadSequenceLength();
         Keccak? requestKeccak = rlpStream.DecodeKeccak(); // skip (not sure if needed to verify)
 
@@ -59,10 +61,10 @@ public class EnrResponseMsgSerializer : DiscoveryMsgSerializerBase, IMessageSeri
         NodeRecord nodeRecord = _nodeRecordSigner.Deserialize(rlpStream);
         if (!_nodeRecordSigner.Verify(nodeRecord))
         {
-            string resHex = data.Slice(positionForHex).ToHexString();
+            string resHex = data.ReadBytes(positionForHex).ReadAllBytes().ToHexString();
             throw new NetworkingException($"Invalid ENR signature: {resHex}", NetworkExceptionType.Discovery);
         }
-        
+
         EnrResponseMsg msg = new(farPublicKey, nodeRecord, requestKeccak!);
         return msg;
     }
