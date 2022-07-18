5ccde254b6410416148da38cddb7ecfaecc571ea
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-07-18 18:01:25 +0530

    update serializers

--- src/Nethermind/Nethermind.Network.Discovery/Serializers/EnrRequestMsgSerializer.cs
+++ src/Nethermind/Nethermind.Network.Discovery/Serializers/EnrRequestMsgSerializer.cs
@@ -1,51 +1,61 @@
 ﻿//  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
+using DotNetty.Buffers;
 using Nethermind.Core.Crypto;
 using Nethermind.Crypto;
 using Nethermind.Network.Discovery.Messages;
+using Nethermind.Network.P2P;
 using Nethermind.Serialization.Rlp;
 
 namespace Nethermind.Network.Discovery.Serializers;
 
-public class EnrRequestMsgSerializer : DiscoveryMsgSerializerBase, IMessageSerializer<EnrRequestMsg>
+public class EnrRequestMsgSerializer : DiscoveryMsgSerializerBase, IZeroMessageSerializer<EnrRequestMsg>
 {
     public EnrRequestMsgSerializer(IEcdsa ecdsa, IPrivateKeyGenerator nodeKey, INodeIdResolver nodeIdResolver)
         : base(ecdsa, nodeKey, nodeIdResolver) { }
 
-    public byte[] Serialize(EnrRequestMsg msg)
+    public void Serialize(IByteBuffer byteBuffer, EnrRequestMsg msg)
     {
-        // TODO: optimize
-        byte[] data = Rlp.Encode(
-            Rlp.Encode(msg.ExpirationTime)
-        ).Bytes;
+        int length = GetLength(msg, out int contentLength);
+        byteBuffer.EnsureWritable(length);
 
-        byte[] serializedMsg = Serialize((byte) msg.MsgType, data);
-        return serializedMsg;
+        RlpStream stream = new(length);
+        stream.StartSequence(contentLength);
+        stream.Encode(msg.ExpirationTime);
+
+        byte[] serializedMsg = Serialize((byte) msg.MsgType, stream.Data);
+        byteBuffer.WriteBytes(serializedMsg);
     }
 
-    public EnrRequestMsg Deserialize(byte[] msgBytes)
+    public EnrRequestMsg Deserialize(IByteBuffer msgBytes)
     {
-        (PublicKey FarPublicKey, byte[] Mdc, byte[] Data) results = PrepareForDeserialization(msgBytes);
-        RlpStream rlpStream = results.Data.AsRlpStream();
+        (PublicKey FarPublicKey, byte[] Mdc, IByteBuffer Data) results = PrepareForDeserialization(msgBytes);
+        NettyRlpStream rlpStream = new(results.Data);
 
         rlpStream.ReadSequenceLength();
         long expirationTime = rlpStream.DecodeLong();
 
         EnrRequestMsg msg = new (results.FarPublicKey, expirationTime);
         return msg;
+    }
+
+    private int GetLength(EnrRequestMsg message, out int contentLength)
+    {
+        contentLength = Rlp.LengthOf((byte)message.ExpirationTime);
+        return Rlp.LengthOfSequence(contentLength);
     }
 }
--- src/Nethermind/Nethermind.Network.Discovery/Serializers/NeighborsMsgSerializer.cs
+++ src/Nethermind/Nethermind.Network.Discovery/Serializers/NeighborsMsgSerializer.cs
@@ -1,29 +1,31 @@
 ﻿//  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
 using System.Net;
+using DotNetty.Buffers;
 using Nethermind.Core.Crypto;
 using Nethermind.Crypto;
 using Nethermind.Network.Discovery.Messages;
+using Nethermind.Network.P2P;
 using Nethermind.Serialization.Rlp;
 using Nethermind.Stats.Model;
 
 namespace Nethermind.Network.Discovery.Serializers;
 
-public class NeighborsMsgSerializer : DiscoveryMsgSerializerBase, IMessageSerializer<NeighborsMsg>
+public class NeighborsMsgSerializer : DiscoveryMsgSerializerBase, IZeroMessageSerializer<NeighborsMsg>
 {
     public NeighborsMsgSerializer(IEcdsa ecdsa,
         IPrivateKeyGenerator nodeKey,
@@ -31,34 +33,33 @@ public class NeighborsMsgSerializer : DiscoveryMsgSerializerBase, IMessageSerial
     {
     }
 
-    public byte[] Serialize(NeighborsMsg msg)
+    public void Serialize(IByteBuffer byteBuffer, NeighborsMsg msg)
     {
-        Rlp[]? nodes = null;
+        int length = GetLength(msg, out int contentLength);
+
+        RlpStream stream = new(length);
+        stream.StartSequence(contentLength);
         if (msg.Nodes.Any())
         {
-            nodes = new Rlp[msg.Nodes.Length];
             for (int i = 0; i < msg.Nodes.Length; i++)
             {
                 Node node = msg.Nodes[i];
-                Rlp serializedNode = SerializeNode(node.Address, node.Id.Bytes);
-                nodes[i] = serializedNode;
+                SerializeNode(stream, node.Address, node.Id.Bytes);
             }
         }
 
-        byte[] data = Rlp.Encode(
-            nodes == null ? Rlp.OfEmptySequence : Rlp.Encode(nodes),
-            Rlp.Encode(msg.ExpirationTime)
-        ).Bytes;
+        stream.Encode(msg.ExpirationTime);
 
-        byte[] serializedMsg = Serialize((byte) msg.MsgType, data);
-        return serializedMsg;
+        byte[] serializedMsg = Serialize((byte) msg.MsgType, stream.Data);
+        byteBuffer.EnsureWritable(serializedMsg.Length);
+        byteBuffer.WriteBytes(serializedMsg);
     }
 
-    public NeighborsMsg Deserialize(byte[] msgBytes)
+    public NeighborsMsg Deserialize(IByteBuffer msgBytes)
     {
-        (PublicKey FarPublicKey, byte[] Mdc, byte[] Data) results = PrepareForDeserialization(msgBytes);
+        (PublicKey FarPublicKey, byte[] Mdc, IByteBuffer Data) results = PrepareForDeserialization(msgBytes);
 
-        RlpStream rlp = results.Data.AsRlpStream();
+        NettyRlpStream rlp = new(results.Data);
         rlp.ReadSequenceLength();
         Node[] nodes = DeserializeNodes(rlp) as Node[];
 
@@ -84,5 +85,24 @@ public class NeighborsMsgSerializer : DiscoveryMsgSerializerBase, IMessageSerial
             ReadOnlySpan<byte> id = ctx.DecodeByteArraySpan();
             return new Node(new PublicKey(id), address);
         });
+    }
+
+    private int GetLength(NeighborsMsg msg, out int contentLength)
+    {
+        int nodeLength = 0;
+        contentLength = 0;
+        if (msg.Nodes.Any())
+        {
+            for (int i = 0; i < msg.Nodes.Length; i++)
+            {
+                Node node = msg.Nodes[i];
+                nodeLength += Rlp.LengthOfSequence(GetLengthSerializeNode(node.Address, node.Id.Bytes));
+            }
+            contentLength += Rlp.LengthOfSequence(nodeLength);
+        }
+
+        contentLength += Rlp.LengthOf(msg.ExpirationTime);
+
+        return Rlp.LengthOfSequence(contentLength);
     }
 }
