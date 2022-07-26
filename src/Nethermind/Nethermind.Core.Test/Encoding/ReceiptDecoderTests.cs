8786b72690824a03b734f19a0386422e8fa2bb26
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-07-26 09:12:29 +0530

    Fix reciepts decoder

--- src/Nethermind/Nethermind.Core.Test/Encoding/ReceiptDecoderTests.cs
+++ src/Nethermind/Nethermind.Core.Test/Encoding/ReceiptDecoderTests.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -197,7 +197,7 @@ namespace Nethermind.Core.Test.Encoding
 
             ReceiptMessageDecoder decoder = new();
 
-            byte[] rlpStreamResult = decoder.Encode(txReceipt).Bytes;
+            byte[] rlpStreamResult = decoder.EncodeNew(txReceipt, RlpBehaviors.None);
             TxReceipt deserialized = decoder.Decode(new RlpStream(rlpStreamResult));
 
             AssertMessageReceipt(txReceipt, deserialized);
@@ -257,7 +257,7 @@ namespace Nethermind.Core.Test.Encoding
 
             ReceiptMessageDecoder decoder = new();
 
-            byte[] rlpStreamResult = decoder.Encode(txReceipt).Bytes;
+            byte[] rlpStreamResult = decoder.EncodeNew(txReceipt);
             TxReceipt deserialized = decoder.Decode(new RlpStream(rlpStreamResult));
 
             AssertMessageReceipt(txReceipt, deserialized);
--- src/Nethermind/Nethermind.Serialization.Rlp/ReceiptMessageDecoder.cs
+++ src/Nethermind/Nethermind.Serialization.Rlp/ReceiptMessageDecoder.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -73,22 +73,6 @@ namespace Nethermind.Serialization.Rlp
 
             txReceipt.Logs = entries;
             return txReceipt;
-        }
-
-        public Rlp Encode(TxReceipt item, RlpBehaviors rlpBehaviors = RlpBehaviors.None)
-        {
-            if (item.TxType == TxType.Legacy)
-            {
-                return Rlp.Encode(
-                    (rlpBehaviors & RlpBehaviors.Eip658Receipts) == RlpBehaviors.Eip658Receipts
-                        ? Rlp.Encode(item.StatusCode)
-                        : Rlp.Encode(item.PostTransactionState),
-                    Rlp.Encode(item.GasUsedTotal),
-                    Rlp.Encode(item.Bloom),
-                    Rlp.Encode(item.Logs));
-            }
-
-            return new Rlp(EncodeNew(item, rlpBehaviors));
         }
 
         private (int Total, int Logs) GetContentLength(TxReceipt item, RlpBehaviors rlpBehaviors)
