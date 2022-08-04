a0d0d68d617f7122adcd493cf76bfd45acff7810
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-08-04 21:05:00 +0530

    Remove IMessageSerializer, use IZeroMessageSerializer for all messages (#4292)
    * change to zero serializer
    
    * fix get length
    
    * temp change
    
    * change to zero intializer
    
    * update message pad
    
    * add and use zero serializer
    
    * use zero serializer
    
    * update messgae serialization service
    
    * rewrite functions to use byte buffer
    
    * update P2P messages serializer
    
    * update serializers
    
    * update tests
    
    * remove zero message serializer
    
    * fix failing tests
    
    * fix netty rlp has been read
    
    * fix length calc
    
    * fix serializers
    
    * update
    
    * optimize byte array allocations
    
    * fix span creation
    
    * remove redundant to array
    
    * fix block body serializer
    
    * fix receipts serializer
    
    * Fix/buffer reallocation (#4306)
    
    * serialization
    
    * reuse buffer
    
    * Fix reciepts decoder
    
    * fix reciepts serializer
    
    * fix zero packet splitter
    
    * implement func to use stream encoder
    
    * Clean up
    
    * fix ChainLevelDecoder
    
    * add comment
    
    * add GetLength private to reduce length calculations
    
    * convert all to inner serializer to use get length to allocate buffer
    
    * Kill more allocations
    
    * kill bit more allocations
    
    * less allocs
    
    * fix build
    
    * use rlp stream
    
    * remove rlp.encode from zeroPacketSplitter
    
    * restore WriteAdaptivePacketType(in IByteBuffer buffer) and fix tests
    
    * fix length calc
    
    * Remove test console outputs
    
    Co-authored-by: lukasz.rozmej <lukasz.rozmej@gmail.com>

--- src/Nethermind/Nethermind.Network.Discovery/Serializers/DiscoveryMsgSerializerBase.cs
+++ src/Nethermind/Nethermind.Network.Discovery/Serializers/DiscoveryMsgSerializerBase.cs
@@ -1,24 +1,26 @@
 ﻿//  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
 using System.Net;
+using DotNetty.Buffers;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
 using Nethermind.Crypto;
 using Nethermind.Network.Discovery.Messages;
+using Nethermind.Network.P2P;
 using Nethermind.Serialization.Rlp;
 
 namespace Nethermind.Network.Discovery.Serializers;
@@ -39,68 +41,85 @@ public abstract class DiscoveryMsgSerializerBase
         _nodeIdResolver = nodeIdResolver ?? throw new ArgumentNullException(nameof(nodeIdResolver));
     }
 
-    protected byte[] Serialize(byte type, Span<byte> data)
+    protected void Serialize(byte type, Span<byte> data, IByteBuffer byteBuffer)
     {
-        byte[] result = new byte[32 + 1 + data.Length + 64 + 1];
-        Span<byte> resultSpan = result.AsSpan();
+        int length = 32 + 1 + data.Length + 64 + 1;
+        byteBuffer.EnsureWritable(length);
+        Span<byte> resultSpan = stackalloc byte[length];
         resultSpan[32 + 65] = type;
         data.CopyTo(resultSpan.Slice(32 + 65 + 1, data.Length));
-        
+
         Span<byte> payload = resultSpan.Slice(32 + 65);
         Keccak toSign = Keccak.Compute(payload);
         Signature signature = _ecdsa.Sign(_privateKey, toSign);
         signature.Bytes.AsSpan().CopyTo(resultSpan.Slice(32, 64));
         resultSpan[32 + 64] = signature.RecoveryId;
-            
+
         Span<byte> forMdc = resultSpan.Slice(32);
         ValueKeccak mdc = ValueKeccak.Compute(forMdc);
         mdc.BytesAsSpan.CopyTo(resultSpan.Slice(0,32));
-        return result;
+        byteBuffer.EnsureWritable(resultSpan.Length);
+        byteBuffer.WriteBytes(resultSpan);
     }
 
-    protected (PublicKey FarPublicKey, byte[] Mdc, byte[] Data) PrepareForDeserialization(byte[] msg)
+    protected (PublicKey FarPublicKey, Memory<byte> Mdc, IByteBuffer Data) PrepareForDeserialization(IByteBuffer msg)
     {
-        if (msg.Length < 98)
+        if (msg.ReadableBytes < 98)
         {
             throw new NetworkingException("Incorrect message", NetworkExceptionType.Validation);
         }
-        
-        byte[] mdc = msg.Slice(0, 32);
-        Span<byte> signature = msg.AsSpan(32, 65);
-        // var type = new[] { msg[97] };
-        byte[] data = msg.Slice(98, msg.Length - 98);
-        Span<byte> computedMdc = ValueKeccak.Compute(msg.AsSpan(32)).BytesAsSpan;
-        
-        if (!Bytes.AreEqual(mdc, computedMdc))
+        IByteBuffer data = msg.Slice(98, msg.ReadableBytes - 98);
+        Memory<byte> msgBytes = msg.ReadAllBytesAsMemory();
+        Memory<byte> mdc = msgBytes.Slice(0, 32);
+        Span<byte> sigAndData = msgBytes.Span.Slice(32);
+        Span<byte> computedMdc = ValueKeccak.Compute(sigAndData).BytesAsSpan;
+
+        if (!Bytes.AreEqual(mdc.Span, computedMdc))
         {
             throw new NetworkingException("Invalid MDC", NetworkExceptionType.Validation);
         }
-        
-        PublicKey nodeId = _nodeIdResolver.GetNodeId(signature.Slice(0, 64).ToArray(), signature[64], msg.AsSpan(97, msg.Length - 97));
+
+        PublicKey nodeId = _nodeIdResolver.GetNodeId(sigAndData.Slice(0, 64), sigAndData[64], sigAndData.Slice(65, sigAndData.Length - 65));
         return (nodeId, mdc, data);
     }
 
-    protected static Rlp Encode(IPEndPoint address)
+    protected static void Encode(RlpStream stream, IPEndPoint address, int length)
     {
-        return Rlp.Encode(
-            Rlp.Encode(address.Address.GetAddressBytes()),
-            //tcp port
-            Rlp.Encode(address.Port),
-            //udp port
-            Rlp.Encode(address.Port)
-        );
+        stream.StartSequence(length);
+        stream.Encode(address.Address.GetAddressBytes());
+        //tcp port
+        stream.Encode(address.Port);
+        //udp port
+        stream.Encode(address.Port);
     }
 
-    protected static Rlp SerializeNode(IPEndPoint address, byte[] id)
+    protected static int GetIPEndPointLength(IPEndPoint address)
     {
-        return Rlp.Encode(
-            Rlp.Encode(address.Address.GetAddressBytes()),
-            //tcp port
-            Rlp.Encode(address.Port),
-            //udp port
-            Rlp.Encode(address.Port),
-            Rlp.Encode(id)
-        );
+        int length = Rlp.LengthOf(address.Address.GetAddressBytes());
+        length += Rlp.LengthOf(address.Port);
+        length += Rlp.LengthOf(address.Port);
+        return length;
+    }
+
+    protected static void SerializeNode(RlpStream stream, IPEndPoint address, byte[] id)
+    {
+        int length = GetLengthSerializeNode(address, id);
+        stream.StartSequence(length);
+        stream.Encode(address.Address.GetAddressBytes());
+        //tcp port
+        stream.Encode(address.Port);
+        //udp port
+        stream.Encode(address.Port);
+        stream.Encode(id);
+    }
+
+    protected static int GetLengthSerializeNode(IPEndPoint address, byte[] id)
+    {
+        int length = Rlp.LengthOf(address.Address.GetAddressBytes());
+        length += Rlp.LengthOf(address.Port);
+        length += Rlp.LengthOf(address.Port);
+        length += Rlp.LengthOf(id);
+        return length;
     }
 
     protected static IPEndPoint GetAddress(ReadOnlySpan<byte> ip, int port)
