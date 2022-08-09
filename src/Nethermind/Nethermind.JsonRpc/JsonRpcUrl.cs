a7382a7e428cdf8a7a8b508df9c203f35fefb532
Nikita Mescheryakov <deffrian@protonmail.com>
2022-08-09 23:35:08 +0300

    Ignore AdditionalUrl when EngineUrl specified (#4392)
    * Engine url parameters
    
    * fix goerli config
    
    * Optional Engine URL parameters
    
    * Fix test
    
    * Ignore EngineUrl when AdditionalUrls specified
    
    * configs fix
    
    * Fix tests
    
    * Revert configs
    
    * Ignore AdditionalUrl when EngineUrl specified
    
    * Fix test

--- src/Nethermind/Nethermind.JsonRpc/JsonRpcUrl.cs
+++ src/Nethermind/Nethermind.JsonRpc/JsonRpcUrl.cs
@@ -96,6 +96,9 @@ namespace Nethermind.JsonRpc
         public RpcEndpoint RpcEndpoint { get; set; }
         public IReadOnlyCollection<string> EnabledModules { get; set; }
 
+        public bool IsModuleEnabled(string moduleName) =>
+            EnabledModules.Any(m => StringComparer.InvariantCultureIgnoreCase.Equals(m, moduleName));
+
         public bool Equals(JsonRpcUrl other)
         {
             if (other == null)
@@ -108,6 +111,7 @@ namespace Nethermind.JsonRpc
                    string.Equals(Host, other.Host) &&
                    Port == other.Port &&
                    RpcEndpoint == other.RpcEndpoint &&
+                   IsAuthenticated == other.IsAuthenticated &&
                    EnabledModules.OrderBy(t => t).SequenceEqual(other.EnabledModules.OrderBy(t => t),
                        StringComparer.InvariantCultureIgnoreCase);
         }
