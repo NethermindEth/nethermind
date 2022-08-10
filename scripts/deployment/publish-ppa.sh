7d91b68bd88d4503b5a044c6c937ec1b67996ee2
Falco <1364936+FalcoXYZ@users.noreply.github.com>
2022-08-10 13:55:17 +0200

    fix: edit folder in script

--- scripts/deployment/publish-ppa.sh
+++ scripts/deployment/publish-ppa.sh
@@ -6,6 +6,7 @@ set -e
 echo $VERSION > ver
 FIXED_VERSION=$(awk -F. '{ print $1"."$2$3$4"0"}' ver)
 
+cd $RELEASE_DIRECTORY/nethermind/
 for release in ${RELEASES}; do
 
   echo "nethermind ($FIXED_VERSION) $release; urgency=high
@@ -13,13 +14,13 @@ for release in ${RELEASES}; do
   * Nethermind client ($FIXED_VERSION release)
 
  -- Nethermind <devops@nethermind.io>  $( date -R )" > $RELEASE_DIRECTORY/nethermind/build/debian/changelog
-  cd nethermind/build/
+  cd build/
   debuild -S -uc -us
   cd ..
   echo 'Signing package'
   debsign -p 'gpg --batch --yes --no-tty --pinentry-mode loopback --passphrase-file /home/runner/work/nethermind/PASSPHRASE' -S -k$PPA_GPG_KEYID nethermind_${FIXED_VERSION}_source.changes
   echo 'Uploading'
-  dput -s -u ppa:nethermindeth/nethermind nethermind_${FIXED_VERSION}_source.changes
+  dput -f ppa:nethermindeth/nethermind nethermind_${FIXED_VERSION}_source.changes
   echo "Publishing $release release to PPA complete"
   echo 'Cleanup'
   rm nethermind_$FIXED_VERSION*
