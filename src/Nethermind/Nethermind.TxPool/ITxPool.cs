6cb565b3e761c18464d516aec434ad29807e54bb
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-08-12 20:52:30 +0530

    Fix/get block transactions count (#4340)
    * add function to get latest pending transation nonce
    
    * modify to get nonce from pool
    
    * update tests
    
    * add all nonces to the _nonce
    
    * revert changes
    
    * implement GetLatestPendingNonce using buckets
    
    * Fixed and optimized GetLatestPendingNonce:
    
    1. current nonce from state was ignored.
    2. Avoid copying a bucket when doing scan
    3. Avoid full collection scan if no gap
    
    * update tests
    
    * fix the calc
    
    * add more tests
    
    * test getting pending transactions from txPool
    
    * does not return null
    
    * remove nullable from RPC interface
    
    * fix test
    
    Co-authored-by: lukasz.rozmej <lukasz.rozmej@gmail.com>

--- src/Nethermind/Nethermind.TxPool/ITxPool.cs
+++ src/Nethermind/Nethermind.TxPool/ITxPool.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,7 +22,7 @@ using Nethermind.Int256;
 
 namespace Nethermind.TxPool
 {
-    public interface ITxPool 
+    public interface ITxPool
     {
         int GetPendingTransactionsCount();
         Transaction[] GetPendingTransactions();
@@ -39,6 +39,7 @@ namespace Nethermind.TxPool
         bool IsKnown(Keccak? hash);
         bool TryGetPendingTransaction(Keccak hash, out Transaction? transaction);
         UInt256 ReserveOwnTransactionNonce(Address address); // TODO: this should be moved to a signer component, outside of TX pool
+        UInt256 GetLatestPendingNonce(Address address);
         event EventHandler<TxEventArgs> NewDiscovered;
         event EventHandler<TxEventArgs> NewPending;
         event EventHandler<TxEventArgs> RemovedPending;
--- src/Nethermind/Nethermind.TxPool/TxPool.cs
+++ src/Nethermind/Nethermind.TxPool/TxPool.cs
@@ -330,7 +330,7 @@ namespace Nethermind.TxPool
         }
 
         private IEnumerable<(Transaction Tx, Action<Transaction>? Change)> UpdateBucketWithAddedTransaction(
-            Address address, ICollection<Transaction> transactions)
+            Address address, IReadOnlyCollection<Transaction> transactions)
         {
             if (transactions.Count != 0)
             {
@@ -346,7 +346,7 @@ namespace Nethermind.TxPool
         }
 
         private IEnumerable<(Transaction Tx, Action<Transaction>? Change)> UpdateGasBottleneck(
-            ICollection<Transaction> transactions, long currentNonce, UInt256 balance)
+            IReadOnlyCollection<Transaction> transactions, long currentNonce, UInt256 balance)
         {
             UInt256? previousTxBottleneck = null;
             int i = 0;
@@ -403,7 +403,7 @@ namespace Nethermind.TxPool
             }
         }
 
-        private IEnumerable<(Transaction Tx, Action<Transaction>? Change)> UpdateBucket(Address address, ICollection<Transaction> transactions)
+        private IEnumerable<(Transaction Tx, Action<Transaction>? Change)> UpdateBucket(Address address, IReadOnlyCollection<Transaction> transactions)
         {
             if (transactions.Count != 0)
             {
@@ -517,6 +517,47 @@ namespace Nethermind.TxPool
             });
 
             return currentNonce;
+        }
+
+        public UInt256 GetLatestPendingNonce(Address address)
+        {
+            UInt256 maxPendingNonce = _accounts.GetAccount(address).Nonce;
+
+            // we are not doing any updating, but lets just use a thread-safe method without any data copying like snapshot
+            _transactions.UpdateGroup(address, (_, transactions) =>
+            {
+                // This is under the assumption that the addressTransactions are sorted by Nonce.
+                if (transactions.Count > 0)
+                {
+                    // if we don't have any gaps we can easily calculate the nonce
+                    Transaction lastTransaction = transactions.Max!;
+                    if (maxPendingNonce + (UInt256)transactions.Count - 1 == lastTransaction.Nonce)
+                    {
+                        maxPendingNonce = lastTransaction.Nonce + 1;
+                    }
+
+                    // we have a gap, need to scan the transactions
+                    else
+                    {
+                        foreach (Transaction transaction in transactions)
+                        {
+                            if (transaction.Nonce == maxPendingNonce)
+                            {
+                                maxPendingNonce++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // we won't do any actual changes
+                return Array.Empty<(Transaction Tx, Action<Transaction>? Change)>();
+            });
+
+            return maxPendingNonce;
         }
 
         public bool IsKnown(Keccak hash) => _hashCache.Get(hash);
