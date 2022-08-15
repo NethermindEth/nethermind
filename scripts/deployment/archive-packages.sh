100a2abf1bc05057c4fc90218c7413e176c17ae1
Falco <1364936+FalcoXYZ@users.noreply.github.com>
2022-08-15 14:32:59 +0200

    refactor: remove git-tag & git-hash and add vars
    Removes the git-tag and git-hash variables and adds variables for these instead.
    Also removes the "packaging" directory as this is no longer relevant (legacy code)

--- scripts/deployment/archive-packages.sh
+++ scripts/deployment/archive-packages.sh
@@ -13,8 +13,9 @@ echo Archiving Nethermind packages
 echo =======================================================
 
 cd $RELEASE_DIRECTORY
-GIT_SHORT_TAG="$(tail git-tag.txt)"
-GIT_HASH="$(tail git-hash.txt)"
+VERSION=$1
+COMMIT_HASH=$(echo $2 | awk '{print substr($0,0,7);}')
+DATE=$(date +'%Y%m%d' -d @$3)
 
 echo =======================================================
 echo Copying and packing plugins
@@ -37,11 +38,11 @@ cp $RELEASE_DIRECTORY/$RELEASE_PATH/Nethermind.{Api,HealthChecks,EthStats,Merge.
 cp $RELEASE_DIRECTORY/$RELEASE_PATH/Nethermind.{Api,HealthChecks,EthStats,Merge.Plugin,Mev}.dll $LIN_ARM64_RELEASE/plugins
 cp $RELEASE_DIRECTORY/$RELEASE_PATH/Nethermind.{Api,HealthChecks,EthStats,Merge.Plugin,Mev}.dll $OSX_ARM64_RELEASE/plugins
 
-cd $LIN_RELEASE && zip -r $LIN-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
-cd $OSX_RELEASE && zip -r $OSX-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
-cd $WIN_RELEASE && zip -r $WIN-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
-cd $LIN_ARM64_RELEASE && zip -r $LIN_ARM64-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
-cd $OSX_ARM64_RELEASE && zip -r $OSX_ARM64-$GIT_SHORT_TAG-$GIT_HASH.zip . && cd ..
+cd $LIN_RELEASE && zip -r $LIN-$VERSION-$COMMIT_HASH-$DATE.zip . && cd ..
+cd $OSX_RELEASE && zip -r $OSX-$VERSION-$COMMIT_HASH-$DATE.zip . && cd ..
+cd $WIN_RELEASE && zip -r $WIN-$VERSION-$COMMIT_HASH-$DATE.zip . && cd ..
+cd $LIN_ARM64_RELEASE && zip -r $LIN_ARM64-$VERSION-$COMMIT_HASH-$DATE.zip . && cd ..
+cd $OSX_ARM64_RELEASE && zip -r $OSX_ARM64-$VERSION-$COMMIT_HASH-$DATE.zip . && cd ..
 
 echo =======================================================
 echo Archiving Nethermind packages completed
