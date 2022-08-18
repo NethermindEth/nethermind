6fba7d6b1a019b099ecb7488a9735f7c9b49f45c
Ruben Buniatyan <rubo@users.noreply.github.com>
2022-08-19 00:05:08 +0200

    Revise product version (#4322)

--- scripts/deployment/update-homebrew.sh
+++ scripts/deployment/update-homebrew.sh
@@ -6,28 +6,23 @@ OSX_ARM64=nethermind-darwin-arm64
 HOMEBREW_PATH=$RELEASE_DIRECTORY/homebrew-nethermind
 
 cd $RELEASE_DIRECTORY
-GIT_SHORT_TAG="$(tail git-tag.txt)"
-GIT_HASH="$(tail git-hash.txt)"
-
+DATE=$(date +'%Y%m%d' -d @$TIMESTAMP)
+COMMIT_HASH=$(echo $HASH | awk '{print substr($0,0,7);}')
 echo =======================================================
 echo Updating Homebrew package
 echo =======================================================
 
 cd $OSX_RELEASE 
-darwin_amd64_hash="$(shasum -a 256 $OSX-$GIT_SHORT_TAG-$GIT_HASH.zip | awk '{ print $1}')"
-version=$(echo $OSX-$GIT_SHORT_TAG-$GIT_HASH.zip | awk -F- '{ print $4}')
-commit=$(echo $OSX-$GIT_SHORT_TAG-$GIT_HASH.zip | awk -F- '{ print $5}')
-date=$(echo $OSX-$GIT_SHORT_TAG-$GIT_HASH.zip | awk -F- '{ print substr($6,1,8)}')
-
+darwin_amd64_hash="$(shasum -a 256 $OSX-$VERSION-$COMMIT_HASH-$DATE.zip | awk '{ print $1}')"
 cd ..
 cd $OSX_ARM64_RELEASE 
-darwin_arm64_hash="$(shasum -a 256 $OSX_ARM64-$GIT_SHORT_TAG-$GIT_HASH.zip | awk '{ print $1}')"
+darwin_arm64_hash="$(shasum -a 256 $OSX_ARM64-$VERSION-$COMMIT_HASH-$DATE.zip | awk '{ print $1}')"
 
 cd $HOMEBREW_PATH
 
-sed -i "s/app_version =.*/app_version = '"$version"'/" nethermind.rb
-sed -i "s/commit =.*/commit = '"$commit"'/" nethermind.rb
-sed -i "s/date =.*/date = '"$date"'/" nethermind.rb
+sed -i "s/app_version =.*/app_version = '"$VERSION"'/" nethermind.rb
+sed -i "s/commit =.*/commit = '"$COMMIT_HASH"'/" nethermind.rb
+sed -i "s/date =.*/date = '"$DATE"'/" nethermind.rb
 awk -i inplace -v n=1 '/sha256/ { if (++count == n) sub(/sha256.*/, "sha256 \"'$darwin_amd64_hash'\""); } 1' nethermind.rb
 awk -i inplace -v n=2 '/sha256/ { if (++count == n) sub(/sha256.*/, "sha256 \"'$darwin_arm64_hash'\""); } 1' nethermind.rb
 
