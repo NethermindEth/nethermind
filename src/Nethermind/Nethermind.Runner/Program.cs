faf6beacf9a99d94636cf28a72e756de51befdd3
Ruben Buniatyan <rubo@users.noreply.github.com>
2022-08-18 20:23:39 +0200

    Replace submodules with packages (#4374)

--- src/Nethermind/Nethermind.Runner/Program.cs
+++ src/Nethermind/Nethermind.Runner/Program.cs
@@ -76,7 +76,7 @@ namespace Nethermind.Runner
                     logger.Error(FailureString + eventArgs.ExceptionObject);
                 }
             };
-                
+
             try
             {
                 Run(args);
@@ -480,7 +480,7 @@ namespace Nethermind.Runner
             ISeqConfig seqConfig = configProvider.GetConfig<ISeqConfig>();
             if (seqConfig.MinLevel != "Off")
             {
-                if (_logger.IsInfo)
+                if (_logger.IsInfo) 
                     _logger.Info($"Seq Logging enabled on host: {seqConfig.ServerUrl} with level: {seqConfig.MinLevel}");
                 NLogConfigurator.ConfigureSeqBufferTarget(seqConfig.ServerUrl, seqConfig.ApiKey, seqConfig.MinLevel);
             }
