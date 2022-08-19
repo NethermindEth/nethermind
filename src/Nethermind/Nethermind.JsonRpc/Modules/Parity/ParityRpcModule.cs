7d2764c2c0975443831fee56953195230b3aea64
smartprogrammer93 <33181301+smartprogrammer93@users.noreply.github.com>
2022-08-19 21:21:32 +0300

    Add optional argument Address in parity_pendingTransactions (#4413)

--- src/Nethermind/Nethermind.JsonRpc/Modules/Parity/ParityRpcModule.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Parity/ParityRpcModule.cs
@@ -67,11 +67,16 @@ namespace Nethermind.JsonRpc.Modules.Parity
             _peerManager = peerManager ?? throw new ArgumentNullException(nameof(peerManager));
         }
 
-        public ResultWrapper<ParityTransaction[]> parity_pendingTransactions()
-            => ResultWrapper<ParityTransaction[]>.Success(_txPool.GetPendingTransactions().Where(pt => pt.SenderAddress != null)
+        public ResultWrapper<ParityTransaction[]> parity_pendingTransactions(Address? address = null)
+        {
+           IEnumerable<Transaction> enumerable = address == null
+                ? _txPool.GetPendingTransactions()
+                : _txPool.GetPendingTransactionsBySender(address);
+            return ResultWrapper<ParityTransaction[]>.Success(enumerable
                 .Select(t => new ParityTransaction(t, Rlp.Encode(t).Bytes,
-                    t.IsSigned ? _ecdsa.RecoverPublicKey(t.Signature, t.Hash) : null)).ToArray());
-        
+                t.IsSigned ? _ecdsa.RecoverPublicKey(t.Signature, t.Hash) : null)).ToArray());
+        }
+
         public ResultWrapper<ReceiptForRpc[]> parity_getBlockReceipts(BlockParameter blockParameter)
         {
             SearchResult<Block> searchResult = _blockFinder.SearchForBlock(blockParameter);
