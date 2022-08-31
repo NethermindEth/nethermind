eb223f98ba11ced7077cb8ad0e8b286d73d0247a
MarekM25 <marekm2504@gmail.com>
2022-08-31 23:08:25 +0200

    revert for now isCloseToPivot

--- src/Nethermind/Nethermind.Facade/Eth/EthSyncingInfo.cs
+++ src/Nethermind/Nethermind.Facade/Eth/EthSyncingInfo.cs
@@ -45,10 +45,9 @@ namespace Nethermind.Facade.Eth
         {
             long bestSuggestedNumber = _blockTree.FindBestSuggestedHeader().Number;
             long headNumberOrZero = _blockTree.Head?.Number ?? 0;
-            bool isCloseToPivot = bestSuggestedNumber <= _syncConfig.PivotNumberParsed + 8;
-            bool isSyncing = bestSuggestedNumber > headNumberOrZero + 8 || (_blockTree.Head?.IsGenesis ?? true) ||  isCloseToPivot;
+            bool isSyncing = bestSuggestedNumber > headNumberOrZero + 8;
 
-            if (_logger.IsInfo) _logger.Info($"Start - EthSyncingInfo - BestSuggestedNumber: {bestSuggestedNumber}, HeadNumberOrZero: {headNumberOrZero}, IsCloseToPivot: {isCloseToPivot}, IsSyncing: {isSyncing} {_syncConfig}. LowestInsertedBodyNumber: {_blockTree.LowestInsertedBodyNumber } LowestInsertedReceiptBlockNumber: {_receiptStorage.LowestInsertedReceiptBlockNumber}");
+            if (_logger.IsInfo) _logger.Info($"Start - EthSyncingInfo - BestSuggestedNumber: {bestSuggestedNumber}, HeadNumberOrZero: {headNumberOrZero}, IsSyncing: {isSyncing} {_syncConfig}. LowestInsertedBodyNumber: {_blockTree.LowestInsertedBodyNumber } LowestInsertedReceiptBlockNumber: {_receiptStorage.LowestInsertedReceiptBlockNumber}");
             if (isSyncing)
             {
                 return new SyncingResult
@@ -65,20 +64,20 @@ namespace Nethermind.Facade.Eth
                 if (_syncConfig.DownloadReceiptsInFastSync &&
                     (_receiptStorage.LowestInsertedReceiptBlockNumber > _syncConfig.AncientReceiptsBarrierCalc || _receiptStorage.LowestInsertedReceiptBlockNumber == null))
                 {
-                    if (_logger.IsInfo) _logger.Info($"Receipts not finished - EthSyncingInfo - BestSuggestedNumber: {bestSuggestedNumber}, HeadNumberOrZero: {headNumberOrZero}, IsCloseToPivot: {isCloseToPivot}, IsSyncing: {isSyncing} {_syncConfig}. LowestInsertedBodyNumber: {_blockTree.LowestInsertedBodyNumber } LowestInsertedReceiptBlockNumber: {_receiptStorage.LowestInsertedReceiptBlockNumber}");
+                    if (_logger.IsInfo) _logger.Info($"Receipts not finished - EthSyncingInfo - BestSuggestedNumber: {bestSuggestedNumber}, HeadNumberOrZero: {headNumberOrZero} IsSyncing: {isSyncing} {_syncConfig}. LowestInsertedBodyNumber: {_blockTree.LowestInsertedBodyNumber } LowestInsertedReceiptBlockNumber: {_receiptStorage.LowestInsertedReceiptBlockNumber}");
                     return new SyncingResult
                     {
                         CurrentBlock = headNumberOrZero,
                         HighestBlock = bestSuggestedNumber,
                         StartingBlock = 0L,
                         IsSyncing = true
-                    };;
+                    };
                 }
 
                 if (_syncConfig.DownloadBodiesInFastSync &&
                     (_blockTree.LowestInsertedBodyNumber > _syncConfig.AncientBodiesBarrierCalc || _blockTree.LowestInsertedBodyNumber == null))
                 {
-                    if (_logger.IsInfo) _logger.Info($"Bodies not finished - EthSyncingInfo - BestSuggestedNumber: {bestSuggestedNumber}, HeadNumberOrZero: {headNumberOrZero}, IsCloseToPivot: {isCloseToPivot}, IsSyncing: {isSyncing} {_syncConfig}. LowestInsertedBodyNumber: {_blockTree.LowestInsertedBodyNumber } LowestInsertedReceiptBlockNumber: {_receiptStorage.LowestInsertedReceiptBlockNumber}");
+                    if (_logger.IsInfo) _logger.Info($"Bodies not finished - EthSyncingInfo - BestSuggestedNumber: {bestSuggestedNumber}, HeadNumberOrZero: {headNumberOrZero}, IsSyncing: {isSyncing} {_syncConfig}. LowestInsertedBodyNumber: {_blockTree.LowestInsertedBodyNumber } LowestInsertedReceiptBlockNumber: {_receiptStorage.LowestInsertedReceiptBlockNumber}");
                     return new SyncingResult
                     {
                         CurrentBlock = headNumberOrZero,
@@ -89,7 +88,7 @@ namespace Nethermind.Facade.Eth
                 }
             }
 
-            if (_logger.IsInfo) _logger.Info($"Node is not syncing - EthSyncingInfo - BestSuggestedNumber: {bestSuggestedNumber}, HeadNumberOrZero: {headNumberOrZero}, IsCloseToPivot: {isCloseToPivot}, IsSyncing: {isSyncing} {_syncConfig}. LowestInsertedBodyNumber: {_blockTree.LowestInsertedBodyNumber } LowestInsertedReceiptBlockNumber: {_receiptStorage.LowestInsertedReceiptBlockNumber}");
+            if (_logger.IsInfo) _logger.Info($"Node is not syncing - EthSyncingInfo - BestSuggestedNumber: {bestSuggestedNumber}, HeadNumberOrZero: {headNumberOrZero}, IsSyncing: {isSyncing} {_syncConfig}. LowestInsertedBodyNumber: {_blockTree.LowestInsertedBodyNumber } LowestInsertedReceiptBlockNumber: {_receiptStorage.LowestInsertedReceiptBlockNumber}");
             return SyncingResult.NotSyncing;
         }
 
