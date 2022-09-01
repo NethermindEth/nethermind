000d641c2c36d3245d55b69d3e526eabae723e1c
Amirul Ashraf <asdacap@gmail.com>
2022-09-01 16:48:00 +0800

    Exit early on error (#4220)
    Exit early on error in steps except for step with "mustInitialize" set to false.

--- src/Nethermind/Nethermind.Init/Steps/StartMonitoring.cs
+++ src/Nethermind/Nethermind.Init/Steps/StartMonitoring.cs
@@ -1,19 +1,19 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 
 using System;
 using System.Collections.Generic;
@@ -42,13 +42,13 @@ public class StartMonitoring : IStep
     {
         IMetricsConfig metricsConfig = _api.Config<IMetricsConfig>();
         ILogger logger = _api.LogManager.GetClassLogger();
-        
+
         // hacky
         if (!string.IsNullOrEmpty(metricsConfig.NodeName))
         {
             _api.LogManager.SetGlobalVariable("nodeName", metricsConfig.NodeName);
         }
-        
+
         if (metricsConfig.Enabled)
         {
             Metrics.Version = VersionToMetrics.ConvertToNumber(ProductInfo.Version);
@@ -57,21 +57,23 @@ public class StartMonitoring : IStep
             IEnumerable<Type> metrics = new TypeDiscovery().FindNethermindTypes(nameof(Metrics));
             foreach (Type metric in metrics)
             {
-                _api.MonitoringService.RegisterMetrics(metric);    
+                _api.MonitoringService.RegisterMetrics(metric);
             }
 
             await _api.MonitoringService.StartAsync().ContinueWith(x =>
             {
-                if (x.IsFaulted && logger.IsError) 
+                if (x.IsFaulted && logger.IsError)
                     logger.Error("Error during starting a monitoring.", x.Exception);
             }, cancellationToken);
-            
+
             _api.DisposeStack.Push(new Reactive.AnonymousDisposable(() => _api.MonitoringService.StopAsync())); // do not await
         }
         else
         {
-            if (logger.IsInfo) 
+            if (logger.IsInfo)
                 logger.Info("Grafana / Prometheus metrics are disabled in configuration");
         }
     }
+
+    public bool MustInitialize => false;
 }
