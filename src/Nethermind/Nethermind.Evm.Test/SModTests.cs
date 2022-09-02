b3a49767dca4c9eae536e65ada1f0912786a1fcc
smartprogrammer <smartprogrammer@windowslive.com>
2022-09-03 02:11:14 +0300

    Test updates

--- src/Nethermind/Nethermind.Evm.Test/SModTests.cs
+++ src/Nethermind/Nethermind.Evm.Test/SModTests.cs
@@ -48,11 +48,12 @@ namespace Nethermind.Evm.Test
             AssertStorage(UInt256.Zero, res);
         }
 
-        [TestCase()]
-        public void Test_for_a_equals_int256_dot_min()
+        [TestCase(-3, -2)]
+        [TestCase(3, -2)]
+        public void Test_for_a_equals_int256_dot_min(int b, int res)
         {
             byte[] code = Prepare.EvmCode
-                .PushData((UInt256)new Int256.Int256(-3))
+                .PushData((UInt256)new Int256.Int256(b))
                 .PushData(new UInt256(0ul, 0ul, 0ul, 0x8000000000000000ul))
                 .Op(Instruction.SMOD)
                 .PushData(0)
@@ -60,7 +61,7 @@ namespace Nethermind.Evm.Test
                 .Done;
 
             _ = Execute(code);
-            AssertStorage(UInt256.Zero, -2);
+            AssertStorage(UInt256.Zero, res);
 
         }
     }
