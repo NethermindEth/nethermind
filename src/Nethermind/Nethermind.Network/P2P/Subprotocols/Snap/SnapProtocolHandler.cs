5d39a93a3e797f16158634f94b6f4063d2df4a6c
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-09-20 08:38:52 +0530

    Revert "temp small change"
    This reverts commit 078c5e57776612a6369c9ab9a1d0d33180e6116c.

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Snap/SnapProtocolHandler.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Snap/SnapProtocolHandler.cs
@@ -261,7 +261,7 @@ namespace Nethermind.Network.P2P.Subprotocols.Snap
             for (int i = 0; i < request.Paths.Length; i++)
             {
                 AccountWithStorageStartingHash path = request.Paths[i];
-                groups[i] = new PathGroup() { Group = new[] { path.PathAndAccount.Path.Bytes } };
+                groups[i] = new PathGroup() { Group = new[] { path.PathAndAccount.Path.Bytes, _emptyBytes } };
             }
 
             return groups;
