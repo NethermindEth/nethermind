d8ba110199ced90c660f0a6e817261774fd490d1
Ayman Bouchareb <Ayman.Bouchareb@outlook.fr>
2022-09-21 02:07:53 +0100

    Added Eip3540 Property flag to SpecProviders

--- src/Nethermind/Nethermind.Specs/ReleaseSpec.cs
+++ src/Nethermind/Nethermind.Specs/ReleaseSpec.cs
@@ -77,5 +77,6 @@ namespace Nethermind.Specs
         public Address Eip1559FeeCollector { get; set; }
         public UInt256? Eip1559BaseFeeMinValue { get; set; }
         public bool IsEip1153Enabled { get; set; }
+        public bool IsEip3540Enabled { get; set; }
     }
 }
