bd0e54021f8065ad0bf225fa52330060cad5890a
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-09-26 13:03:11 +0530

    add option to keep track of absoulute path in TreeVisitor

--- src/Nethermind/Nethermind.Trie/VisitContext.cs
+++ src/Nethermind/Nethermind.Trie/VisitContext.cs
@@ -1,20 +1,21 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Nethermind.Trie
@@ -28,6 +29,8 @@ namespace Nethermind.Trie
         public bool IsStorage { get; internal set; }
         public int? BranchChildIndex { get; internal set; }
         public bool ExpectAccounts { get; init; }
+        public bool KeepTrackOfAbsolutePath { get; init; }
+        public List<byte> AbsolutePathNibbles = new();
 
         public int MaxDegreeOfParallelism
         {
