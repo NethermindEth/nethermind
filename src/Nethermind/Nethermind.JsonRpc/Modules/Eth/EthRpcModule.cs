652d38ec58a47aac9878553bf6a0ccbab5b108f5
lukasz.rozmej <lukasz.rozmej@gmail.com>
2022-09-26 19:05:57 +0200

    Optimize returning transactions within the block

--- src/Nethermind/Nethermind.JsonRpc/Modules/Eth/EthRpcModule.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Eth/EthRpcModule.cs
@@ -747,9 +747,6 @@ public partial class EthRpcModule : IEthRpcModule
 
     private void RecoverTxSenderIfNeeded(Transaction transaction)
     {
-        if (transaction.SenderAddress == null)
-        {
-            _blockchainBridge.RecoverTxSender(transaction);
-        }
+        transaction.SenderAddress ??= _blockchainBridge.RecoverTxSender(transaction);
     }
 }
