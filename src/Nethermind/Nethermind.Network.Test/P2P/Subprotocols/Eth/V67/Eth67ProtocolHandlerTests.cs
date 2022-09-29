21537e1ff09a5f69fefc3aec51c444e2726cc0fc
Marcin Sobczak <marcindsobczak@gmail.com>
2022-09-29 20:31:59 +0200

    simplify tests

--- src/Nethermind/Nethermind.Network.Test/P2P/Subprotocols/Eth/V67/Eth67ProtocolHandlerTests.cs
+++ src/Nethermind/Nethermind.Network.Test/P2P/Subprotocols/Eth/V67/Eth67ProtocolHandlerTests.cs
@@ -15,26 +15,21 @@
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 //
 
-using System.Collections.Generic;
 using System.Net;
-using System.Threading;
 using DotNetty.Buffers;
 using FluentAssertions;
-using Nethermind.Blockchain.Synchronization;
 using Nethermind.Consensus;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Specs;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Core.Timers;
-using Nethermind.Crypto;
 using Nethermind.Logging;
 using Nethermind.Network.P2P;
 using Nethermind.Network.P2P.Subprotocols;
 using Nethermind.Network.P2P.Subprotocols.Eth.V62.Messages;
 using Nethermind.Network.P2P.Subprotocols.Eth.V63.Messages;
 using Nethermind.Network.P2P.Subprotocols.Eth.V65;
-using Nethermind.Network.P2P.Subprotocols.Eth.V65.Messages;
 using Nethermind.Network.P2P.Subprotocols.Eth.V66;
 using Nethermind.Network.P2P.Subprotocols.Eth.V67;
 using Nethermind.Network.Rlpx;
@@ -113,94 +108,6 @@ public class Eth67ProtocolHandlerTests
         }
 
         [Test]
-        public void Can_handle_get_block_headers()
-        {
-            var msg62 = new GetBlockHeadersMessage();
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.GetBlockHeadersMessage(1111, msg62);
-
-            HandleIncomingStatusMessage();
-            HandleZeroMessage(msg66, Eth66MessageCode.GetBlockHeaders);
-            _session.Received().DeliverMessage(Arg.Any<Network.P2P.Subprotocols.Eth.V66.Messages.BlockHeadersMessage>());
-        }
-
-        [Test]
-        public void Can_handle_block_headers()
-        {
-            var msg62 = new BlockHeadersMessage(Build.A.BlockHeader.TestObjectNTimes(3));
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.BlockHeadersMessage(1111, msg62);
-
-            ((ISyncPeer)_handler).GetBlockHeaders(1, 1, 1, CancellationToken.None);
-            HandleIncomingStatusMessage();
-            HandleZeroMessage(msg66, Eth66MessageCode.BlockHeaders);
-        }
-
-        [Test]
-        public void Should_throw_when_receiving_unrequested_block_headers()
-        {
-            var msg62 = new BlockHeadersMessage(Build.A.BlockHeader.TestObjectNTimes(3));
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.BlockHeadersMessage(1111, msg62);
-
-            HandleIncomingStatusMessage();
-            System.Action act = () => HandleZeroMessage(msg66, Eth66MessageCode.BlockHeaders);
-            act.Should().Throw<SubprotocolException>();
-        }
-
-        [Test]
-        public void Can_handle_get_block_bodies()
-        {
-            var msg62 = new GetBlockBodiesMessage(new[] { Keccak.Zero, TestItem.KeccakA });
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.GetBlockBodiesMessage(1111, msg62);
-
-            HandleIncomingStatusMessage();
-            HandleZeroMessage(msg66, Eth66MessageCode.GetBlockBodies);
-            _session.Received().DeliverMessage(Arg.Any<Network.P2P.Subprotocols.Eth.V66.Messages.BlockBodiesMessage>());
-        }
-
-        [Test]
-        public void Can_handle_block_bodies()
-        {
-            var msg62 = new BlockBodiesMessage(Build.A.Block.TestObjectNTimes(3));
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.BlockBodiesMessage(1111, msg62);
-
-            HandleIncomingStatusMessage();
-            ((ISyncPeer)_handler).GetBlockBodies(new List<Keccak>(new[] { Keccak.Zero }), CancellationToken.None);
-            HandleZeroMessage(msg66, Eth66MessageCode.BlockBodies);
-        }
-
-        [Test]
-        public void Should_throw_when_receiving_unrequested_block_bodies()
-        {
-            var msg62 = new BlockBodiesMessage(Build.A.Block.TestObjectNTimes(3));
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.BlockBodiesMessage(1111, msg62);
-
-            HandleIncomingStatusMessage();
-            System.Action act = () => HandleZeroMessage(msg66, Eth66MessageCode.BlockBodies);
-            act.Should().Throw<SubprotocolException>();
-        }
-
-        [Test]
-        public void Can_handle_get_pooled_transactions()
-        {
-            var msg65 = new GetPooledTransactionsMessage(new[] { Keccak.Zero, TestItem.KeccakA });
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.GetPooledTransactionsMessage(1111, msg65);
-
-            HandleIncomingStatusMessage();
-            HandleZeroMessage(msg66, Eth66MessageCode.GetPooledTransactions);
-            _session.Received().DeliverMessage(Arg.Any<Network.P2P.Subprotocols.Eth.V66.Messages.PooledTransactionsMessage>());
-        }
-
-        [Test]
-        public void Can_handle_pooled_transactions()
-        {
-            Transaction tx = Build.A.Transaction.Signed(new EthereumEcdsa(1, LimboLogs.Instance), TestItem.PrivateKeyA).TestObject;
-            var msg65 = new PooledTransactionsMessage(new[] { tx });
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.PooledTransactionsMessage(1111, msg65);
-
-            HandleIncomingStatusMessage();
-            HandleZeroMessage(msg66, Eth66MessageCode.PooledTransactions);
-        }
-
-        [Test]
         public void Can_ignore_get_node_data()
         {
             var msg63 = new GetNodeDataMessage(new[] { Keccak.Zero, TestItem.KeccakA });
@@ -212,18 +119,7 @@ public class Eth67ProtocolHandlerTests
         }
 
         [Test]
-        public void Can_ignore_node_data()
-        {
-            var msg63 = new NodeDataMessage(System.Array.Empty<byte[]>());
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.NodeDataMessage(1111, msg63);
-
-            HandleIncomingStatusMessage();
-            // handler is receiving unrequested NodeData - in eth/67 it should be ignored and not throw
-            HandleZeroMessage(msg66, Eth66MessageCode.NodeData);
-        }
-
-        [Test]
-        public void Should_not_throw_when_receiving_unrequested_node_data()
+        public void Can_ignore_node_data_and_not_throw_when_receiving_unrequested_node_data()
         {
             var msg63 = new NodeDataMessage(System.Array.Empty<byte[]>());
             var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.NodeDataMessage(1111, msg63);
@@ -234,35 +130,14 @@ public class Eth67ProtocolHandlerTests
         }
 
         [Test]
-        public void Can_handle_get_receipts()
+        public void Can_handle_eth66_messages_other_than_GetNodeData_and_NodeData()     // e.g. GetBlockHeadersMessage
         {
-            var msg63 = new GetReceiptsMessage(new[] { Keccak.Zero, TestItem.KeccakA });
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.GetReceiptsMessage(1111, msg63);
+            var msg62 = new GetBlockHeadersMessage();
+            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.GetBlockHeadersMessage(1111, msg62);
 
             HandleIncomingStatusMessage();
-            HandleZeroMessage(msg66, Eth66MessageCode.GetReceipts);
-        }
-
-        [Test]
-        public void Can_handle_receipts()
-        {
-            var msg63 = new ReceiptsMessage(System.Array.Empty<TxReceipt[]>());
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.ReceiptsMessage(1111, msg63);
-
-            HandleIncomingStatusMessage();
-            ((ISyncPeer)_handler).GetReceipts(new List<Keccak>(new[] { Keccak.Zero }), CancellationToken.None);
-            HandleZeroMessage(msg66, Eth66MessageCode.Receipts);
-        }
-
-        [Test]
-        public void Should_throw_when_receiving_unrequested_receipts()
-        {
-            var msg63 = new ReceiptsMessage(System.Array.Empty<TxReceipt[]>());
-            var msg66 = new Network.P2P.Subprotocols.Eth.V66.Messages.ReceiptsMessage(1111, msg63);
-
-            HandleIncomingStatusMessage();
-            System.Action act = () => HandleZeroMessage(msg66, Eth66MessageCode.Receipts);
-            act.Should().Throw<SubprotocolException>();
+            HandleZeroMessage(msg66, Eth66MessageCode.GetBlockHeaders);
+            _session.Received().DeliverMessage(Arg.Any<Network.P2P.Subprotocols.Eth.V66.Messages.BlockHeadersMessage>());
         }
 
         private void HandleZeroMessage<T>(T msg, int messageCode) where T : MessageBase
