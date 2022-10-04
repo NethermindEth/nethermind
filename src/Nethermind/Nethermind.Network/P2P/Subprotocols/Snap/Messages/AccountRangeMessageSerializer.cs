b315e2062c166e8aa392f31da081697215f5b14b
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-10-04 10:29:17 +0530

    fix serialization length calc for null elements

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Snap/Messages/AccountRangeMessageSerializer.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Snap/Messages/AccountRangeMessageSerializer.cs
@@ -1,19 +1,19 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 
 using DotNetty.Buffers;
 using Nethermind.Core;
@@ -102,6 +102,7 @@ namespace Nethermind.Network.P2P.Subprotocols.Snap.Messages
             if (message.PathsWithAccounts == null || message.PathsWithAccounts.Length == 0)
             {
                 pwasLength = 1;
+                contentLength += 1;
             }
             else
             {
@@ -113,14 +114,14 @@ namespace Nethermind.Network.P2P.Subprotocols.Snap.Messages
 
                     pwasLength += Rlp.LengthOfSequence(itemLength);
                 }
+                contentLength += Rlp.LengthOfSequence(pwasLength);
             }
-
-            contentLength += Rlp.LengthOfSequence(pwasLength);
 
             int proofsLength = 0;
             if (message.Proofs == null || message.Proofs.Length == 0)
             {
                 proofsLength = 1;
+                contentLength += 1;
             }
             else
             {
@@ -128,9 +129,8 @@ namespace Nethermind.Network.P2P.Subprotocols.Snap.Messages
                 {
                     proofsLength += Rlp.LengthOf(message.Proofs[i]);
                 }
+                contentLength += Rlp.LengthOfSequence(proofsLength);
             }
-
-            contentLength += Rlp.LengthOfSequence(proofsLength);
 
             return (contentLength, pwasLength, proofsLength);
         }
