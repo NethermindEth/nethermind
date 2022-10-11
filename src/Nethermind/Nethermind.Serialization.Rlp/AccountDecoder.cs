b355c4a5b6dad7b6e6e99e3408dbf6b92e56361a
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-10-11 11:35:26 +0530

    fix account decoder

--- src/Nethermind/Nethermind.Serialization.Rlp/AccountDecoder.cs
+++ src/Nethermind/Nethermind.Serialization.Rlp/AccountDecoder.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -113,7 +113,7 @@ namespace Nethermind.Serialization.Rlp
             }
             else
             {
-                rlpStream.Encode(account.StorageRoot);
+                rlpStream.Encode(account.StorageRoot?? Keccak.EmptyTreeHash);
             }
 
             if (_slimFormat && !account.HasCode)
@@ -122,7 +122,7 @@ namespace Nethermind.Serialization.Rlp
             }
             else
             {
-                rlpStream.Encode(account.CodeHash);
+                rlpStream.Encode(account.CodeHash?? Keccak.OfAnEmptyString);
             }
         }
 
