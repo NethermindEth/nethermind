0069b5e53a2c0d97b9a76242d26c53f7c7876b19
Tanishq Jasoria <jasoriatanishq@gmail.com>
2022-10-15 00:04:21 +0530

    allocate on stack

--- src/Nethermind/Nethermind.Network/Rlpx/Handshake/Eip8MessagePad.cs
+++ src/Nethermind/Nethermind.Network/Rlpx/Handshake/Eip8MessagePad.cs
@@ -14,6 +14,7 @@
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
+using System;
 using DotNetty.Buffers;
 using Nethermind.Core.Extensions;
 using Nethermind.Crypto;
@@ -37,7 +38,9 @@ namespace Nethermind.Network.Rlpx.Handshake
 
         public void Pad(IByteBuffer message)
         {
-            byte[] padding = _cryptoRandom.GenerateRandomBytes(100 + _cryptoRandom.NextInt(201));
+            int length = 100 + _cryptoRandom.NextInt(201);
+            Span<byte> padding = stackalloc byte[length];
+            _cryptoRandom.GenerateRandomBytes(padding);
             message.WriteBytes(padding);
         }
     }
