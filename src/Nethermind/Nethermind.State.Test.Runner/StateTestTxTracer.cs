5dfeeca31be2e411fd74abffecfa6bf6f621d6ac
smartprogrammer <smartprogrammer@windowslive.com>
2022-10-26 00:41:06 +0300

    Fix test

--- src/Nethermind/Nethermind.State.Test.Runner/StateTestTxTracer.cs
+++ src/Nethermind/Nethermind.State.Test.Runner/StateTestTxTracer.cs
@@ -60,13 +60,13 @@ namespace Nethermind.State.Test.Runner
             _trace.Result.GasUsed = gasSpent;
         }
 
-        public void StartOperation(int depth, long gas, Instruction opcode, int pc, bool isPostMerge = false)
+        public void StartOperation(int depth, long gas, Instruction opcode, int pc, bool isPostMerge = false, IReleaseSpec? spec = null)
         {
             _gasAlreadySetForCurrentOp = false;
             _traceEntry = new StateTestTxTraceEntry();
             _traceEntry.Pc = pc;
             _traceEntry.Operation = (byte)opcode;
-            _traceEntry.OperationName = opcode.GetName(isPostMerge);
+            _traceEntry.OperationName = opcode.GetName(isPostMerge, spec);
             _traceEntry.Gas = gas;
             _traceEntry.Depth = depth;
             _trace.Entries.Add(_traceEntry);
