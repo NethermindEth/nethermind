9fead3e143a0c1db49f672dbc3ab090f14f6d4a5
Nikita Mescheryakov <deffrian@protonmail.com>
2022-10-25 14:40:03 +0300

    make bool lowercase

--- src/Nethermind/Nethermind.Cli/NodeManager.cs
+++ src/Nethermind/Nethermind.Cli/NodeManager.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -89,6 +89,10 @@ namespace Nethermind.Cli
                     decimal totalMicroseconds = stopwatch.ElapsedTicks * (1_000_000m / Stopwatch.Frequency);
                     Colorful.Console.WriteLine($"Request complete in {totalMicroseconds}μs");
                     string? resultString = result?.ToString();
+                    if (result is bool)
+                    {
+                        resultString = resultString?.ToLower();
+                    }
                     if (resultString == "0x" || resultString is null)
                     {
                         returnValue = JsValue.Null;
