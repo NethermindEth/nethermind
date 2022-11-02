86c3097f3dcd56c9c032b189ddfeee07d1fc88e8
MarekM25 <marekm2504@gmail.com>
2022-11-02 15:27:07 +0100

    add WithdrawalApplier

---
+++ src/Nethermind/Nethermind.Consensus/Withdrawals/IWithdrawalApplier.cs
@@ -0,0 +1,9 @@
+﻿using Nethermind.Core;
+using Nethermind.Core.Specs;
+
+namespace Nethermind.Consensus.Withdrawals;
+
+public interface IWithdrawalApplier
+{
+    void ApplyWithdrawals(Block block, IReleaseSpec spec);
+}
---
+++ src/Nethermind/Nethermind.Consensus/Withdrawals/ProductionWithdrawalApplier.cs
@@ -0,0 +1,28 @@
+﻿using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Specs;
+using Nethermind.State.Proofs;
+
+namespace Nethermind.Consensus.Withdrawals;
+
+public class ProductionWithdrawalApplier : IWithdrawalApplier
+{
+    private readonly IWithdrawalApplier _validationWithdrawalApplier;
+
+    public ProductionWithdrawalApplier(IWithdrawalApplier validationWithdrawalApplier)
+    {
+        _validationWithdrawalApplier = validationWithdrawalApplier;
+    }
+
+    public void ApplyWithdrawals(Block block, IReleaseSpec spec)
+    {
+        _validationWithdrawalApplier.ApplyWithdrawals(block, spec);
+        if (spec.IsEip4895Enabled)
+        {
+            if (block.Withdrawals == null || block.Withdrawals?.Length == 0)
+                block.Header.WithdrawalsRoot = Keccak.EmptyTreeHash;
+            else
+                block.Header.WithdrawalsRoot = new WithdrawalTrie(block.Withdrawals!).RootHash;
+        }
+    }
+}
