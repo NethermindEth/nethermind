ea9ff2561898ac76588fc7ee5ed71f7d0c910633
MarekM25 <marekm2504@gmail.com>
2022-11-03 14:15:37 +0100

    fix EVM.test

--- src/Nethermind/Nethermind.Specs/Forks/15_Shanghai.cs
+++ src/Nethermind/Nethermind.Specs/Forks/15_Shanghai.cs
@@ -27,6 +27,7 @@ namespace Nethermind.Specs.Forks
         protected Shanghai()
         {
             Name = "Shanghai";
+            IsEip1153Enabled = true;
             IsEip3675Enabled = true;
             IsEip3651Enabled = true;
             IsEip3855Enabled = true;
