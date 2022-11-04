f05fd1eb9eeea98796667da5afdffc38910a1cfc
MarekM25 <marekm2504@gmail.com>
2022-11-04 14:27:02 +0100

    HasBody?

--- src/Nethermind/Nethermind.Core/BlockHeader.cs
+++ src/Nethermind/Nethermind.Core/BlockHeader.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -78,8 +78,7 @@ namespace Nethermind.Core
         public Keccak? WithdrawalsRoot { get; set; }
 
         public bool HasBody => TxRoot != Keccak.EmptyTreeHash
-            || UnclesHash != Keccak.OfAnEmptySequenceRlp
-            || WithdrawalsRoot != Keccak.EmptyTreeHash;
+                               || UnclesHash != Keccak.OfAnEmptySequenceRlp;
 
         public string SealEngineType { get; set; } = Core.SealEngineType.Ethash;
 
