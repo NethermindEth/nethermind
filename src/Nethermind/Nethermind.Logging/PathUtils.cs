7927b04333330398ad019cc2b917e6f4f1fe72eb
lukasz.rozmej <lukasz.rozmej@gmail.com>
2022-11-07 17:22:15 +0100

    fix eth_call mix_hash and beneficiary

--- src/Nethermind/Nethermind.Logging/PathUtils.cs
+++ src/Nethermind/Nethermind.Logging/PathUtils.cs
@@ -1,16 +1,16 @@
 //  Copyright (c) 2021 Demerzel Solutions Limited
 //  This file is part of the Nethermind library.
-// 
+//
 //  The Nethermind library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  The Nethermind library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
@@ -28,15 +28,17 @@ namespace Nethermind.Logging
 
         static PathUtils()
         {
-            var process = Process.GetCurrentProcess();
-            if (process.ProcessName.StartsWith("dotnet", StringComparison.InvariantCultureIgnoreCase))
+            Process process = Process.GetCurrentProcess();
+            if (process.ProcessName.StartsWith("dotnet", StringComparison.InvariantCultureIgnoreCase)
+                || process.ProcessName.Equals("ReSharperTestRunner", StringComparison.InvariantCultureIgnoreCase))
             {
                 ExecutingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                return;
             }
-
-            ExecutingDirectory = Path.GetDirectoryName(process.MainModule.FileName);
-            Console.WriteLine($"Resolved executing directory as {ExecutingDirectory}.");
+            else
+            {
+                ExecutingDirectory = Path.GetDirectoryName(process.MainModule.FileName);
+                Console.WriteLine($"Resolved executing directory as {ExecutingDirectory}.");
+            }
         }
 
         public static string GetApplicationResourcePath(this string resourcePath, string overridePrefixPath = null)
