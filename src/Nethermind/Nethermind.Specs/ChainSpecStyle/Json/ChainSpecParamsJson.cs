3794a149103c65e5531ac56ac226b1609fff9b20
MarekM25 <marekm2504@gmail.com>
2022-11-08 15:35:37 +0100

    fix extra-data

--- src/Nethermind/Nethermind.Specs/ChainSpecStyle/Json/ChainSpecParamsJson.cs
+++ src/Nethermind/Nethermind.Specs/ChainSpecStyle/Json/ChainSpecParamsJson.cs
@@ -151,7 +151,6 @@ namespace Nethermind.Specs.ChainSpecStyle.Json
         public ulong? Eip3675TransitionTimestamp { get; set; }
         public ulong? Eip3855TransitionTimestamp { get; set; }
         public ulong? Eip3860TransitionTimestamp { get; set; }
-
         public ulong? Eip4895TransitionTimestamp { get; set; }
     }
 }
