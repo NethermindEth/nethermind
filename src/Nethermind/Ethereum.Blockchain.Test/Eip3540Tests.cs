f1009a5b526eee26d87d647a4f045a7784ab31a9
Marcin Sobczak <marcindsobczak@gmail.com>
2022-11-14 17:42:27 +0100

    add Eip3540Tests

---
+++ src/Nethermind/Ethereum.Blockchain.Test/Eip3540Tests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright 2022 Demerzel Solutions Limited
+// Licensed under the LGPL-3.0. For full terms, see LICENSE-LGPL in the project root.
+
+using System.Collections.Generic;
+using Ethereum.Test.Base;
+using NUnit.Framework;
+
+namespace Ethereum.Blockchain.Test;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class Eip3540Tests : GeneralStateTestBase
+{
+    [TestCaseSource(nameof(LoadTests))]
+    public void Test(GeneralStateTest test)
+    {
+        Assert.True(RunTest(test).Pass);
+    }
+
+    public static IEnumerable<GeneralStateTest> LoadTests()
+    {
+        var loader = new TestsSourceLoader(new LoadGeneralStateTestsStrategy(), "stEIP3540");
+        return (IEnumerable<GeneralStateTest>)loader.LoadTests();
+    }
+}
