cc0944884fe4f3a1c21fa7d7c4d6ce37423ca585
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2022-11-15 11:13:58 +0100

    removed unnecessary comments

--- src/Nethermind/Nethermind.Synchronization/ParallelSync/MultiSyncModeSelector.cs
+++ src/Nethermind/Nethermind.Synchronization/ParallelSync/MultiSyncModeSelector.cs
@@ -21,12 +21,10 @@ using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
 using Nethermind.Blockchain.Synchronization;
-using Nethermind.Config;
 using Nethermind.Int256;
 using Nethermind.Logging;
 using Nethermind.State.Snap;
 using Nethermind.Synchronization.Peers;
-using Nethermind.Synchronization.Reporting;
 
 namespace Nethermind.Synchronization.ParallelSync
 {
@@ -61,7 +59,6 @@ namespace Nethermind.Synchronization.ParallelSync
         private const int StickyStateNodesDelta = 32;
 
         private readonly ISyncProgressResolver _syncProgressResolver;
-        //private readonly IConfigProvider _configurationProvider;
         private readonly ISyncPeerPool _syncPeerPool;
         private readonly ISyncConfig _syncConfig;
         private readonly IBeaconSyncStrategy _beaconSyncStrategy;
@@ -93,7 +90,6 @@ namespace Nethermind.Synchronization.ParallelSync
 
         public MultiSyncModeSelector(
             ISyncProgressResolver syncProgressResolver,
-            //IConfigProvider configProvider,
             ISyncPeerPool syncPeerPool,
             ISyncConfig syncConfig,
             IBeaconSyncStrategy beaconSyncStrategy,
@@ -107,7 +103,6 @@ namespace Nethermind.Synchronization.ParallelSync
             _syncPeerPool = syncPeerPool ?? throw new ArgumentNullException(nameof(syncPeerPool));
             _betterPeerStrategy = betterPeerStrategy ?? throw new ArgumentNullException(nameof(betterPeerStrategy));
             _syncProgressResolver = syncProgressResolver ?? throw new ArgumentNullException(nameof(syncProgressResolver));
-            //_configurationProvider = configProvider ?? throw new ArgumentNullException(nameof(configProvider));
             _needToWaitForHeaders = needToWaitForHeaders;
 
             if (syncConfig.FastSyncCatchUpHeightDelta <= FastSyncLag)
