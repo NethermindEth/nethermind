afaa15c3ee27f9eeb89eca3e631c84d32e5ac05f
Ahmad Bitar <33181301+smartprogrammer93@users.noreply.github.com>
2022-11-16 13:35:38 +0300

    Fix Hive Test issues after Nullable enabled on TxPool Project. (#4902)
    * Work
    
    * More work
    
    * reverting the revert
    
    * More Fixes
    
    * Add Unit test with the exact case that was failing Hive Test
    
    * Applying Lukasz suggestion
    
    * Small Fix
    
    * Apply final suggerstion from lukasz

--- src/Nethermind/Nethermind.TxPool/TxNonceTxPoolReserveSealer.cs
+++ src/Nethermind/Nethermind.TxPool/TxNonceTxPoolReserveSealer.cs
@@ -18,6 +18,9 @@
 using System;
 using System.Threading.Tasks;
 using Nethermind.Core;
+using Nethermind.Core.Specs;
+using Nethermind.Crypto;
+using Nethermind.Logging;
 
 namespace Nethermind.TxPool
 {
@@ -25,11 +28,16 @@ namespace Nethermind.TxPool
     public class NonceReservingTxSealer : TxSealer
     {
         private readonly ITxPool _txPool;
+        private readonly IEthereumEcdsa _ecdsa;
 
-        public NonceReservingTxSealer(ITxSigner txSigner, ITimestamper timestamper, ITxPool txPool)
+        public NonceReservingTxSealer(ITxSigner txSigner,
+            ITimestamper timestamper,
+            ITxPool txPool,
+            IEthereumEcdsa ecdsa)
             : base(txSigner, timestamper)
         {
             _txPool = txPool ?? throw new ArgumentNullException(nameof(txPool));
+            _ecdsa = ecdsa ?? throw new ArgumentNullException(nameof(ecdsa));
         }
 
         public override ValueTask Seal(Transaction tx, TxHandlingOptions txHandlingOptions)
@@ -37,6 +45,9 @@ namespace Nethermind.TxPool
             bool manageNonce = (txHandlingOptions & TxHandlingOptions.ManagedNonce) == TxHandlingOptions.ManagedNonce;
             if (manageNonce)
             {
+                tx.SenderAddress ??= _ecdsa.RecoverAddress(tx);
+                if (tx.SenderAddress is null)
+                    throw new ArgumentNullException(nameof(tx.SenderAddress));
                 tx.Nonce = _txPool.ReserveOwnTransactionNonce(tx.SenderAddress);
                 txHandlingOptions |= TxHandlingOptions.AllowReplacingSignature;
             }
