ab7c76e36a53214ad838a0e549d8652264604ce5
Ruben Buniatyan <rubo@users.noreply.github.com>
2022-11-21 20:04:45 +0100

    Add missing file headers

--- src/Nethermind/Nethermind.Core.Test/Builders/WithdrawalBuilder.cs
+++ src/Nethermind/Nethermind.Core.Test/Builders/WithdrawalBuilder.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
 using Nethermind.Int256;
 
 namespace Nethermind.Core.Test.Builders;
