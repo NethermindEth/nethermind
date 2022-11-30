8617a036474f2636b88fda0ffe6da4b2b3311869
smartprogrammer <smartprogrammer@windowslive.com>
2022-11-30 15:20:33 +0300

    fix merge

--- src/Nethermind/Nethermind.Specs/TestSpecProvider.cs
+++ src/Nethermind/Nethermind.Specs/TestSpecProvider.cs
@@ -22,12 +22,6 @@ namespace Nethermind.Specs
             GenesisSpec = initialSpecToReturn;
         }
 
-        public TestSpecProvider(IReleaseSpec initialSpecToReturn, IReleaseSpec finalSpecToReturn)
-        {
-            SpecToReturn = finalSpecToReturn;
-            GenesisSpec = initialSpecToReturn;
-        }
-
         public void UpdateMergeTransitionInfo(long? blockNumber, UInt256? terminalTotalDifficulty = null)
         {
             if (blockNumber is not null)
