5693711ce49abc9ba4590443272f4205542dc7d5
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2022-12-05 14:07:42 +0100

    add validation to txparser tool

--- src/Nethermind/Nethermind.TxParser/Program.cs
+++ src/Nethermind/Nethermind.TxParser/Program.cs
@@ -1,11 +1,10 @@
-// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
-// SPDX-License-Identifier: LGPL-3.0-only
-
 using Nethermind.Core;
 using Nethermind.Core.Extensions;
 using Nethermind.Serialization.Rlp;
 using Nethermind.Crypto;
 using Nethermind.Logging;
+using Nethermind.Consensus.Validators;
+using Nethermind.Specs.Forks;
 
 while (true)
 {
@@ -15,14 +14,19 @@ while (true)
 
     try
     {
-        Transaction tx = Rlp.Decode<Transaction>(Bytes.FromHexString(input), RlpBehaviors.SkipTypedWrapping);
-        EthereumEcdsa ecdsa = new(ChainId.Mainnet, SimpleConsoleLogManager.Instance);
-        Address? sender = ecdsa.RecoverAddress(tx);
-        if (sender == null)
-        {
-            throw new InvalidDataException("Could not recover sender address");
+        Transaction tx = Rlp.Decode<Transaction>(Bytes.FromHexString(input));
+        TxValidator txValidator = new TxValidator(ChainId.Mainnet);
+        if(txValidator.IsWellFormed(tx, GrayGlacier.Instance)) {
+            EthereumEcdsa ecdsa = new(ChainId.Mainnet, SimpleConsoleLogManager.Instance);
+            Address? sender = ecdsa.RecoverAddress(tx);
+            if (sender == null)
+            {
+                throw new InvalidDataException("Could not recover sender address");
+            }
+            Console.WriteLine(sender);
+        } else {
+            throw new InvalidDataException("Transaction is not well formed");
         }
-        Console.WriteLine(sender);
     }
     catch (Exception e)
     {
