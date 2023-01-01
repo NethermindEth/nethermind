550fd955295ebc74b24412f0b6a8a3d0c06e1120
Ruben Buniatyan <rubo@users.noreply.github.com>
2023-01-01 16:41:50 +0100

    Add missing file headers

--- src/Nethermind/Nethermind.Consensus.Test/MiningConfigurationTests.cs
+++ src/Nethermind/Nethermind.Consensus.Test/MiningConfigurationTests.cs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
 using Nethermind.Core.Exceptions;
 using Nethermind.Init.Steps;
 using NUnit.Framework;
--- src/Nethermind/Nethermind.Mining.Test/MiningConfigTest.cs
+++ src/Nethermind/Nethermind.Mining.Test/MiningConfigTest.cs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
 using System.Text;
 using Nethermind.Consensus;
 using Nethermind.Core.Exceptions;
