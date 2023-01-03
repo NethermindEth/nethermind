49994c1a6d8466b0ba8f89ab9aacd0667e6ad3ee
smartprogrammer <smartprogrammer@windowslive.com>
2023-01-03 10:38:17 +0300

    remove using

--- src/Nethermind/Nethermind.Evm/CodeDepositHandler.cs
+++ src/Nethermind/Nethermind.Evm/CodeDepositHandler.cs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
-using Microsoft.IdentityModel.Tokens;
 using Nethermind.Core.Specs;
 using Nethermind.Evm.CodeAnalysis;
 using Nethermind.Evm.EOF;
