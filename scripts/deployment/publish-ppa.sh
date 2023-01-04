32d36b23ed40dd858456fd3d122e8309989ecb62
Ruben Buniatyan <rubo@users.noreply.github.com>
2023-01-04 20:48:12 +0100

    Consolidate scripts

--- scripts/deployment/publish-ppa.sh
+++ scripts/deployment/publish-ppa.sh
@@ -2,29 +2,30 @@
 # SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 # SPDX-License-Identifier: LGPL-3.0-only
 
-#exit when any command fails
-RELEASES="bionic focal jammy kinetic"
-
 set -e
-echo $VERSION > ver
-FIXED_VERSION=$(awk -F. '{ print $1"."$2$3$4"0"}' ver)
 
-cd $GITHUB_WORKSPACE/nethermind
-for release in ${RELEASES}; do
+for release in "bionic" "focal" "jammy" "kinetic"
+do
+  echo "Building package for $release"
+  
+  changelog="nethermind ($VERSION) $release; urgency=high\n"
+  changelog+="  * Nethermind v$VERSION\n"
+  changelog+=" -- Nethermind <devops@nethermind.io>  $(date -R)"
 
-  echo "nethermind ($FIXED_VERSION) $release; urgency=high
+  cd $SCRIPTS_PATH
+  echo -e "$changelog" > debian/changelog
 
-  * Nethermind client ($FIXED_VERSION release)
-
- -- Nethermind <devops@nethermind.io>  $( date -R )" > $GITHUB_WORKSPACE/nethermind/build/debian/changelog
-  cd build/
   debuild -S -uc -us
   cd ..
-  echo 'Signing package'
-  debsign -p 'gpg --batch --yes --no-tty --pinentry-mode loopback --passphrase-file /home/runner/work/nethermind/PASSPHRASE' -S -k$PPA_GPG_KEYID nethermind_${FIXED_VERSION}_source.changes
-  echo 'Uploading'
-  dput -f ppa:nethermindeth/nethermind nethermind_${FIXED_VERSION}_source.changes
-  echo "Publishing $release release to PPA complete"
-  echo 'Cleanup'
-  rm nethermind_$FIXED_VERSION*
+
+  echo "Signing package"
+  debsign -p "gpg --batch --yes --no-tty --pinentry-mode loopback --passphrase-file $GITHUB_WORKSPACE/PASSPHRASE" \
+    -S -k$PPA_GPG_KEYID nethermind_${VERSION}_source.changes
+
+  echo "Uploading package"
+  dput -f ppa:nethermindeth/nethermind nethermind_${VERSION}_source.changes
+  
+  rm nethermind_$VERSION*
 done
+
+echo "Publishing completed"
