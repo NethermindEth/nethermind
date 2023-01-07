97646ca26469f5425373bbab38237e0c27b63e18
Ruben Buniatyan <rubo@users.noreply.github.com>
2023-01-07 18:52:41 +0100

    Restore PPA package version hack

--- scripts/deployment/publish-ppa.sh
+++ scripts/deployment/publish-ppa.sh
@@ -6,7 +6,11 @@ set -e
 
 echo "Building package"
 
-changelog="nethermind ($VERSION) jammy; urgency=high\n"
+# This hack must be removed once we reach the v2
+echo $VERSION > ver
+FIXED_VERSION=$(awk -F. '{ print $1"."$2$3$4"0"}' ver)
+
+changelog="nethermind ($FIXED_VERSION) jammy; urgency=high\n"
 changelog+="  * Nethermind v$VERSION\n"
 changelog+=" -- Nethermind <devops@nethermind.io>  $(date -R)"
 
@@ -18,11 +22,11 @@ cd ..
 
 echo "Signing package"
 debsign -p "gpg --batch --yes --no-tty --pinentry-mode loopback --passphrase-file $GITHUB_WORKSPACE/PASSPHRASE" \
-  -S -k$PPA_GPG_KEYID nethermind_${VERSION}_source.changes
+  -S -k$PPA_GPG_KEYID nethermind_${FIXED_VERSION}_source.changes
 
 echo "Uploading package"
-dput -f ppa:nethermindeth/nethermind nethermind_${VERSION}_source.changes
+dput -f ppa:nethermindeth/nethermind nethermind_${FIXED_VERSION}_source.changes
   
-rm nethermind_$VERSION*
+rm nethermind_$FIXED_VERSION*
 
 echo "Publishing completed"
