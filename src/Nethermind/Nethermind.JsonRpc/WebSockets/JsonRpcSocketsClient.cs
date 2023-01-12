0e8c3ac96f38303608e2d6167c754362ceede46a
Amirul Ashraf <asdacap@gmail.com>
2023-01-12 22:10:09 +0800

    Fix whitespace

--- src/Nethermind/Nethermind.JsonRpc/WebSockets/JsonRpcSocketsClient.cs
+++ src/Nethermind/Nethermind.JsonRpc/WebSockets/JsonRpcSocketsClient.cs
@@ -46,8 +46,8 @@ namespace Nethermind.JsonRpc.WebSockets
             Closed?.Invoke(this, EventArgs.Empty);
         }
 
-        private static readonly byte[] _jsonOpeningBracket = {Convert.ToByte('{')};
-        private static readonly byte[] _jsonComma = {Convert.ToByte(',')};
+        private static readonly byte[] _jsonOpeningBracket = { Convert.ToByte('{') };
+        private static readonly byte[] _jsonComma = { Convert.ToByte(',') };
         private static readonly byte[] _jsonClosingBracket = { Convert.ToByte('}') };
 
         public override async Task ProcessAsync(ArraySegment<byte> data)
