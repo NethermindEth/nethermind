6c76daf55139b329fbd3abc26250aee9b3d23203
Amirul Ashraf <asdacap@gmail.com>
2023-01-16 20:17:03 +0800

    Fix wrong bracket

--- src/Nethermind/Nethermind.Runner/JsonRpc/Startup.cs
+++ src/Nethermind/Nethermind.Runner/JsonRpc/Startup.cs
@@ -35,9 +35,9 @@ namespace Nethermind.Runner.JsonRpc
 {
     public class Startup
     {
-        private static readonly byte _jsonOpeningBracket = Convert.ToByte('{');
+        private static readonly byte _jsonOpeningBracket = Convert.ToByte('[');
         private static readonly byte _jsonComma = Convert.ToByte(',');
-        private static readonly byte _jsonClosingBracket = Convert.ToByte('}');
+        private static readonly byte _jsonClosingBracket = Convert.ToByte(']');
 
         public void ConfigureServices(IServiceCollection services)
         {
