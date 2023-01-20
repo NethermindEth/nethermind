6e4d39aac0e2509ca73609c1844dfb13fb471fc7
Marcin Sobczak <marcindsobczak@gmail.com>
2023-01-20 20:44:38 +0100

    simplification

--- src/Nethermind/Nethermind.Network.Test/P2P/Subprotocols/NodeData/NodeDataProtocolHandlerTests.cs
+++ src/Nethermind/Nethermind.Network.Test/P2P/Subprotocols/NodeData/NodeDataProtocolHandlerTests.cs
@@ -2,15 +2,12 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System.Collections.Generic;
-using System.Net;
 using System.Threading;
 using DotNetty.Buffers;
 using FluentAssertions;
 using Nethermind.Blockchain.Synchronization;
-using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Test.Builders;
-using Nethermind.Core.Timers;
 using Nethermind.Logging;
 using Nethermind.Network.P2P;
 using Nethermind.Network.P2P.Subprotocols;
@@ -19,7 +16,6 @@ using Nethermind.Network.P2P.Subprotocols.NodeData.Messages;
 using Nethermind.Network.Rlpx;
 using Nethermind.Network.Test.Builders;
 using Nethermind.Stats;
-using Nethermind.Stats.Model;
 using Nethermind.Synchronization;
 using NSubstitute;
 using NUnit.Framework;
@@ -30,31 +26,18 @@ public class NodeDataProtocolHandlerTests
 {
     private ISession _session;
     private IMessageSerializationService _svc;
-    private ISyncServer _syncServer;
-    private Block _genesisBlock;
     private NodeDataProtocolHandler _handler;
 
     [SetUp]
     public void Setup()
     {
         _svc = Build.A.SerializationService().WithNodeData().TestObject;
-
-        NetworkDiagTracer.IsEnabled = true;
-
         _session = Substitute.For<ISession>();
-        Node node = new(TestItem.PublicKeyA, new IPEndPoint(IPAddress.Broadcast, 30303));
-        _session.Node.Returns(node);
-        _syncServer = Substitute.For<ISyncServer>();
-
-        _genesisBlock = Build.A.Block.Genesis.TestObject;
-        _syncServer.Head.Returns(_genesisBlock.Header);
-        _syncServer.Genesis.Returns(_genesisBlock.Header);
-        ITimerFactory timerFactory = Substitute.For<ITimerFactory>();
         _handler = new NodeDataProtocolHandler(
             _session,
             _svc,
-            new NodeStatsManager(timerFactory, LimboLogs.Instance),
-            _syncServer,
+            Substitute.For<INodeStatsManager>(),
+            Substitute.For<ISyncServer>(),
             LimboLogs.Instance);
         _handler.Init();
     }
