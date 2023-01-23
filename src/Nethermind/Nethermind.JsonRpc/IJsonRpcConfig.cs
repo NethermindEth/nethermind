7524282251391a4b52f1d8348d6f83ad03739b6a
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2023-01-23 10:19:10 +0100

    Increase batch limit to 1000

--- src/Nethermind/Nethermind.JsonRpc/IJsonRpcConfig.cs
+++ src/Nethermind/Nethermind.JsonRpc/IJsonRpcConfig.cs
@@ -128,7 +128,7 @@ namespace Nethermind.JsonRpc
 
         [ConfigItem(
             Description = "Limit batch size for batched json rpc call",
-            DefaultValue = "100")]
+            DefaultValue = "1000")]
         int MaxBatchSize { get; set; }
 
         [ConfigItem(
