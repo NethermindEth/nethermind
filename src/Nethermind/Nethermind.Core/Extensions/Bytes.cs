266ff51885d7a3d16ca127239e3e46c76d93fb78
Kamil Chodoła <kamil@nethermind.io>
2023-02-01 13:20:43 +0100

    Bring Old Version back for comparision in benchmarks + Benchmarks for 4 scenarios

--- src/Nethermind/Nethermind.Core/Extensions/Bytes.cs
+++ src/Nethermind/Nethermind.Core/Extensions/Bytes.cs
@@ -864,6 +864,69 @@ namespace Nethermind.Core.Extensions
             return Convert.FromHexString(spanned);
         }
 
+        [DebuggerStepThrough]
+        public static byte[] FromHexStringOld(string hexString)
+        {
+            if (hexString is null)
+            {
+                throw new ArgumentNullException($"{nameof(hexString)}");
+            }
+
+            int startIndex = hexString.StartsWith("0x") ? 2 : 0;
+            bool odd = hexString.Length % 2 == 1;
+            int numberChars = hexString.Length - startIndex + (odd ? 1 : 0);
+            byte[] bytes = new byte[numberChars / 2];
+            for (int i = 0; i < numberChars; i += 2)
+            {
+                if (odd && i == 0)
+                {
+                    bytes[0] += FromHexNibble2Table[(byte)hexString[startIndex]];
+                }
+                else if (odd)
+                {
+                    bytes[i / 2] += FromHexNibble1Table[(byte)hexString[i + startIndex - 1]];
+                    bytes[i / 2] += FromHexNibble2Table[(byte)hexString[i + startIndex]];
+                }
+                else
+                {
+                    bytes[i / 2] += FromHexNibble1Table[(byte)hexString[i + startIndex]];
+                    bytes[i / 2] += FromHexNibble2Table[(byte)hexString[i + startIndex + 1]];
+                }
+            }
+
+            return bytes;
+        }
+
+        private static byte[] FromHexNibble1Table =
+        {
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 0, 16,
+            32, 48, 64, 80, 96, 112, 128, 144, 255, 255,
+            255, 255, 255, 255, 255, 160, 176, 192, 208, 224,
+            240, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 160, 176, 192,
+            208, 224, 240
+        };
+
+        private static byte[] FromHexNibble2Table =
+        {
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 0, 1,
+            2, 3, 4, 5, 6, 7, 8, 9, 255, 255,
+            255, 255, 255, 255, 255, 10, 11, 12, 13, 14,
+            15, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 10, 11, 12,
+            13, 14, 15
+        };
+
         [SuppressMessage("ReSharper", "NonReadonlyMemberInGetHashCode")]
         public static int GetSimplifiedHashCode(this byte[] bytes)
         {
