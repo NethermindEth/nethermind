5ae47281ccea7506f58fd1b5fab745a77da10881
lukasz.rozmej <lukasz.rozmej@gmail.com>
2023-02-16 14:56:54 +0100

    simplify/optimize

--- src/Nethermind/Nethermind.JsonRpc/Modules/Eth/TransactionsOption.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Eth/TransactionsOption.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -12,15 +13,12 @@ public class TransactionsOption : IJsonRpcParam
 
     public void ReadJson(JsonSerializer serializer, string jsonValue)
     {
-        if (jsonValue.ToLower().Equals("true") || jsonValue.ToLower().Equals("false"))
-        {
-            IncludeTransactions = jsonValue.ToLower().Equals("true");
-        }
-        else
-        {
-            JObject jObject = serializer.Deserialize<JObject>(jsonValue.ToJsonTextReader());
-            IncludeTransactions = GetIncludeTransactions(jObject["includeTransactions"]);
-        }
+        bool isTrue = string.Equals(jsonValue, bool.TrueString, StringComparison.InvariantCultureIgnoreCase);
+        bool isFalse = string.Equals(jsonValue, bool.FalseString, StringComparison.InvariantCultureIgnoreCase);
+
+        IncludeTransactions = isTrue || isFalse
+            ? isTrue
+            : GetIncludeTransactions(serializer.Deserialize<JObject>(jsonValue.ToJsonTextReader())["includeTransactions"]);
     }
 
     private static bool GetIncludeTransactions(JToken? token) => token switch
