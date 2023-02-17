47d15dddcebf826fbaf0c750a501ffd38bf7d848
Pandapip1 <gavinnjohn@gmail.com>
2023-02-16 21:04:21 -0500

    Fix some errors and other issues

--- src/Nethermind/Nethermind.Evm.Test/Eip5920Tests.cs
+++ src/Nethermind/Nethermind.Evm.Test/Eip5920Tests.cs
@@ -3,32 +3,39 @@
 
 using FluentAssertions;
 using NUnit.Framework;
+using System;
+using Nethermind.Int256;
+using System.Linq;
 
 namespace Nethermind.Evm.Test
 {
     public class Eip5920Tests : VirtualMachineTestsBase
     {
+        Nethermind.Core.Address zeroAddress = new Nethermind.Core.Address("0x0000000000000000000000000000000000000000");
+        Nethermind.Core.Address address1 = new Nethermind.Core.Address("0x000000f000000000000000000000000000000000");
+        Nethermind.Core.Address address2 = new Nethermind.Core.Address("0xf000001000000000000000000000000000000000");
+        
         [Test]
         public void TestBurn()
         {
             // Prepare bytecode to burn 100 wei
             byte[] code = Prepare.EvmCode
                 .PushData(100)
-                .PushData("0x0000000000000000000000000000000000000000")
+                .PushData(zeroAddress)
                 .Op(Instruction.PAY)
                 .Done;
             
             // Prepare address to have 150 wei
-            TestState.CreateAccount(TestItem.AddressF, 150);
+            TestState.CreateAccount(address1, 150);
 
-            // Execute bytecode in the context of addressF
-            TestAllTracerWithOutput result = Execute(code, TestItem.AddressF);
+            // Execute bytecode in the context of address1
+            TestAllTracerWithOutput result = Execute(code, address1);
 
             // Validate
             result.Error.Should().BeNull();
-            result.StatusCode.Should().Be(1);
-            TestState.GetBalance(TestItem.AddressF).Should().Be(50);
-            TestState.GetBalance("0x0000000000000000000000000000000000000000").Should().Be(0);
+            result.StatusCode.Should().Be(StatusCode.Success);
+            TestState.GetBalance(address1).Should().Be(50);
+            TestState.GetBalance(zeroAddress).Should().Be(0);
         }
 
         [Test]
@@ -37,63 +44,66 @@ namespace Nethermind.Evm.Test
             // Prepare bytecode to burn 100 wei
             byte[] code = Prepare.EvmCode
                 .PushData(100)
-                .PushData("0x0000000000000000000000000000000000000000")
+                .PushData(zeroAddress)
                 .Op(Instruction.PAY)
                 .Done;
 
             // Prepare address to have 70 wei
-            TestState.CreateAccount(TestItem.AddressF, 70);
+            TestState.CreateAccount(address1, 70);
 
-            // Execute bytecode in the context of addressF
-            TestAllTracerWithOutput result = Execute(code, TestItem.AddressF);
+            // Execute bytecode in the context of address1
+            TestAllTracerWithOutput result = Execute(code, address1);
 
             // Validate
             result.Error.Should().NotBeNull();
+            TestState.GetBalance(address1).Should().Be(70);
         }
 
         [Test]
         public void TestTransfer()
         {
-            // Prepare bytecode to transfer 100 wei from addressF to addressG
+            // Prepare bytecode to transfer 100 wei from address1 to address2
             byte[] code = Prepare.EvmCode
                 .PushData(100)
-                .PushData(TestItem.AddressG)
+                .PushData(address2)
                 .Op(Instruction.PAY)
                 .Done;
 
-            // Prepare addressF to have 150 wei and addressG to have 20 wei
-            TestState.CreateAccount(TestItem.AddressF, 150);
-            TestState.CreateAccount(TestItem.AddressG, 20);
+            // Prepare address1 to have 150 wei and address2 to have 20 wei
+            TestState.CreateAccount(address1, 150);
+            TestState.CreateAccount(address2, 20);
 
-            // Execute bytecode in the context of addressF
-            TestAllTracerWithOutput result = Execute(code, TestItem.AddressF);
+            // Execute bytecode in the context of address1
+            TestAllTracerWithOutput result = Execute(code, address1);
 
             // Validate
             result.Error.Should().BeNull();
-            result.StatusCode.Should().Be(1);
-            TestState.GetBalance(TestItem.AddressF).Should().Be(50);
-            TestState.GetBalance(TestItem.AddressG).Should().Be(120);
+            result.StatusCode.Should().Be(StatusCode.Success);
+            TestState.GetBalance(address1).Should().Be(50);
+            TestState.GetBalance(address2).Should().Be(120);
         }
 
         [Test]
         public void TestTransferInsufficientBalance()
         {
-            // Prepare bytecode to transfer 100 wei from addressF to addressG
+            // Prepare bytecode to transfer 100 wei from address1 to address2
             byte[] code = Prepare.EvmCode
                 .PushData(100)
-                .PushData(TestItem.AddressG)
+                .PushData(address2)
                 .Op(Instruction.PAY)
                 .Done;
 
-            // Prepare addressF to have 70 wei and addressG to have 20 wei
-            TestState.CreateAccount(TestItem.AddressF, 70);
-            TestState.CreateAccount(TestItem.AddressG, 20);
+            // Prepare address1 to have 70 wei and address2 to have 20 wei
+            TestState.CreateAccount(address1, 70);
+            TestState.CreateAccount(address2, 20);
 
-            // Execute bytecode in the context of addressF
-            TestAllTracerWithOutput result = Execute(code, TestItem.AddressF);
+            // Execute bytecode in the context of address1
+            TestAllTracerWithOutput result = Execute(code, address1);
 
             // Validate
             result.Error.Should().NotBeNull();
+            TestState.GetBalance(address1).Should().Be(70);
+            TestState.GetBalance(address2).Should().Be(20);
         }
     }
 }
--- src/Nethermind/Nethermind.Specs/Forks/17_Cancun.cs
+++ src/Nethermind/Nethermind.Specs/Forks/17_Cancun.cs
@@ -14,7 +14,6 @@ namespace Nethermind.Specs.Forks
         {
             Name = "Cancun";
             IsEip1153Enabled = true;
-            IsEip5920Enabled = true;
         }
 
         public new static IReleaseSpec Instance => LazyInitializer.EnsureInitialized(ref _instance, () => new Cancun());
