c2c04201310b7a2be2a606dd269d189902a699d3
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-02-22 14:31:45 +0530

    Revert "remove Rlp.Encode"
    This reverts commit 61b5e2f6d23575f4babf047c11904a60a61fb0d8.

--- src/Nethermind/Nethermind.Serialization.Rlp/BlockInfoDecoder.cs
+++ src/Nethermind/Nethermind.Serialization.Rlp/BlockInfoDecoder.cs
@@ -8,7 +8,7 @@ using Nethermind.Int256;
 
 namespace Nethermind.Serialization.Rlp
 {
-    public class BlockInfoDecoder : IRlpStreamDecoder<BlockInfo>
+    public class BlockInfoDecoder : IRlpStreamDecoder<BlockInfo>, IRlpValueDecoder<BlockInfo>
     {
         public BlockInfo? Decode(RlpStream rlpStream, RlpBehaviors rlpBehaviors = RlpBehaviors.None)
         {
@@ -71,6 +71,12 @@ namespace Nethermind.Serialization.Rlp
                 stream.Encode((int)item.Metadata);
             }
         }
+
+        public Rlp Encode(BlockInfo? item, RlpBehaviors rlpBehaviors = RlpBehaviors.None)
+        {
+            throw new NotImplementedException();
+        }
+
         private int GetContentLength(BlockInfo? item, RlpBehaviors rlpBehaviors = RlpBehaviors.None)
         {
             if (item == null)
