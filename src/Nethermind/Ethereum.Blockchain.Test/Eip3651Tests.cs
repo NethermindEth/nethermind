6f1f8166f631ca7dc516130afa3ca25fb2a449ed
Marcin Sobczak <77129288+marcindsobczak@users.noreply.github.com>
2023-02-27 15:38:50 +0100

    Update ethereum tests, add `Eip3651Tests` (#5345)
    * add Eip3651Tests
    
    * update ethereum tests
    
    * fix file encoding

---
+++ src/Nethermind/Ethereum.Blockchain.Test/Eip3651Tests.cs
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Collections.Generic;
+using Ethereum.Test.Base;
+using NUnit.Framework;
+
+namespace Ethereum.Blockchain.Test;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class Eip3651Tests : GeneralStateTestBase
+{
+    [TestCaseSource(nameof(LoadTests))]
+    public void Test(GeneralStateTest test)
+    {
+        Assert.True(RunTest(test).Pass);
+    }
+
+    public static IEnumerable<GeneralStateTest> LoadTests()
+    {
+        var loader = new TestsSourceLoader(new LoadGeneralStateTestsStrategy(), "stEIP3651");
+        return (IEnumerable<GeneralStateTest>)loader.LoadTests();
+    }
+}
