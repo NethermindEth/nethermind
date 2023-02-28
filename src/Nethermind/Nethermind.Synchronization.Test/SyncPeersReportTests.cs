41db5258e8cacf3b4b400ac260f7d4a0e5d17b3a
lukasz.rozmej <lukasz.rozmej@gmail.com>
2023-02-28 16:34:54 +0100

    fix whitespace

--- src/Nethermind/Nethermind.Synchronization.Test/SyncPeersReportTests.cs
+++ src/Nethermind/Nethermind.Synchronization.Test/SyncPeersReportTests.cs
@@ -162,9 +162,9 @@ namespace Nethermind.Synchronization.Test
 
             string expectedResult =
                 "== Header ==" + Environment.NewLine +
-                "===[Active][Sleep ][Peer(ProtocolVersion/Head/Host:Port/Direction)][Transfer Speeds (L/H/B/R/N/S)      ][Client Info (Name/Version/Operating System/Language)     ]"  + Environment.NewLine +
-                "--------------------------------------------------------------------------------------------------------------------------------------------------------------"  + Environment.NewLine +
-                "   [HBRNSW][      ][Peer|eth99|    9999|      127.0.0.1: 3030| Out][     |     |     |     |     |     ][]"  + Environment.NewLine +
+                "===[Active][Sleep ][Peer(ProtocolVersion/Head/Host:Port/Direction)][Transfer Speeds (L/H/B/R/N/S)      ][Client Info (Name/Version/Operating System/Language)     ]" + Environment.NewLine +
+                "--------------------------------------------------------------------------------------------------------------------------------------------------------------" + Environment.NewLine +
+                "   [HBRNSW][      ][Peer|eth99|    9999|      127.0.0.1: 3030| Out][     |     |     |     |     |     ][]" + Environment.NewLine +
                 "   [      ][HBRNSW][Peer|eth99|    9999|      127.0.0.1: 3030|  In][     |     |     |     |     |     ][]";
 
             SyncPeersReport report = new(syncPeerPool, Substitute.For<INodeStatsManager>(), NoErrorLimboLogs.Instance);
