c4fa3929b8f33257d309db30a0f2e2decabad282
scooletz <scooletz@gmail.com>
2023-03-03 10:58:25 +0100

    more fine grained locking

--- src/Nethermind/Nethermind.Trie/Pruning/TrieStore.cs
+++ src/Nethermind/Nethermind.Trie/Pruning/TrieStore.cs
@@ -464,14 +464,14 @@ namespace Nethermind.Trie.Pruning
                 {
                     try
                     {
-                        lock (_dirtyNodes)
+                        while (!_pruningTaskCancellationTokenSource.IsCancellationRequested && _pruningStrategy.ShouldPrune(MemoryUsedByDirtyCache))
                         {
-                            using (_dirtyNodes.AllNodes.AcquireLock())
+                            lock (_dirtyNodes)
                             {
-                                if (_logger.IsDebug) _logger.Debug($"Locked {nameof(TrieStore)} for pruning.");
-
-                                while (!_pruningTaskCancellationTokenSource.IsCancellationRequested && _pruningStrategy.ShouldPrune(MemoryUsedByDirtyCache))
+                                using (_dirtyNodes.AllNodes.AcquireLock())
                                 {
+                                    if (_logger.IsDebug) _logger.Debug($"Locked {nameof(TrieStore)} for pruning.");
+
                                     _dirtyNodes.PruneCache();
 
                                     if (_pruningTaskCancellationTokenSource.IsCancellationRequested || !CanPruneCacheFurther())
