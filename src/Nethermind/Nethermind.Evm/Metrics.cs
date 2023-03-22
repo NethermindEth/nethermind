191fda0ca02ec9f0e2b334b0ab96851d97088534
scooletz <scooletz@gmail.com>
2023-03-22 10:47:42 +0100

    evm metrics

--- src/Nethermind/Nethermind.Evm/Metrics.cs
+++ src/Nethermind/Nethermind.Evm/Metrics.cs
@@ -8,72 +8,72 @@ namespace Nethermind.Evm
 {
     public class Metrics
     {
-        [Description("Number of EVM exceptions thrown by contracts.")]
         [CounterMetric]
+        [Description("Number of EVM exceptions thrown by contracts.")]
         public static long EvmExceptions { get; set; }
 
-        [Description("Number of SELFDESTRUCT calls.")]
         [CounterMetric]
+        [Description("Number of SELFDESTRUCT calls.")]
         public static long SelfDestructs { get; set; }
 
-        [Description("Number of calls to other contracts.")]
         [CounterMetric]
+        [Description("Number of calls to other contracts.")]
         public static long Calls { get; set; }
 
-        [Description("Number of SLOAD opcodes executed.")]
         [CounterMetric]
+        [Description("Number of SLOAD opcodes executed.")]
         public static long SloadOpcode { get; set; }
 
-        [Description("Number of SSTORE opcodes executed.")]
         [CounterMetric]
+        [Description("Number of SSTORE opcodes executed.")]
         public static long SstoreOpcode { get; set; }
 
-        [Description("Number of TLOAD opcodes executed.")]
         [CounterMetric]
+        [Description("Number of TLOAD opcodes executed.")]
         public static long TloadOpcode { get; set; }
 
-        [Description("Number of TSTORE opcodes executed.")]
         [CounterMetric]
+        [Description("Number of TSTORE opcodes executed.")]
         public static long TstoreOpcode { get; set; }
 
-        [Description("Number of MODEXP precompiles executed.")]
         [CounterMetric]
+        [Description("Number of MODEXP precompiles executed.")]
         public static long ModExpOpcode { get; set; }
 
-        [Description("Number of BLOCKHASH opcodes executed.")]
         [CounterMetric]
+        [Description("Number of BLOCKHASH opcodes executed.")]
         public static long BlockhashOpcode { get; set; }
 
-        [Description("Number of BN256_MUL precompile calls.")]
         [CounterMetric]
+        [Description("Number of BN256_MUL precompile calls.")]
         public static long Bn256MulPrecompile { get; set; }
 
-        [Description("Number of BN256_ADD precompile calls.")]
         [CounterMetric]
+        [Description("Number of BN256_ADD precompile calls.")]
         public static long Bn256AddPrecompile { get; set; }
 
-        [Description("Number of BN256_PAIRING precompile calls.")]
         [CounterMetric]
+        [Description("Number of BN256_PAIRING precompile calls.")]
         public static long Bn256PairingPrecompile { get; set; }
 
-        [Description("Number of EC_RECOVERY precompile calls.")]
         [CounterMetric]
+        [Description("Number of EC_RECOVERY precompile calls.")]
         public static long EcRecoverPrecompile { get; set; }
 
-        [Description("Number of MODEXP precompile calls.")]
         [CounterMetric]
+        [Description("Number of MODEXP precompile calls.")]
         public static long ModExpPrecompile { get; set; }
 
-        [Description("Number of RIPEMD160 precompile calls.")]
         [CounterMetric]
+        [Description("Number of RIPEMD160 precompile calls.")]
         public static long Ripemd160Precompile { get; set; }
 
-        [Description("Number of SHA256 precompile calls.")]
         [CounterMetric]
+        [Description("Number of SHA256 precompile calls.")]
         public static long Sha256Precompile { get; set; }
 
-        [Description("Number of Point Evaluation precompile calls.")]
         [CounterMetric]
+        [Description("Number of Point Evaluation precompile calls.")]
         public static long PointEvaluationPrecompile { get; set; }
     }
 }
