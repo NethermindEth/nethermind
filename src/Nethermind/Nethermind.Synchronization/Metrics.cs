c1c4af1f74a33608bf425c42050a170be4f00ce9
Szymon Kulec <scooletz@gmail.com>
2023-03-23 14:23:52 +0100

    Introduction of System.Diagnostics.Metrics (#5441)
    * minor refactor
    
    * abstract data accessor from members in MetricsController
    
    * first instrument introduced
    
    * callbacks
    
    * bench_precompiles
    
    * description from instruments
    
    * undo counters added in Metrics
    
    * meters and gauges are not created on the basis of metrics
    
    * comments
    
    * different metrics measurements
    
    * formatting
    
    * AccountAbstraction metrics
    
    * AccountAbstraction.Network metrics
    
    * blockchain metrics
    
    * AuRa
    
    * db metrics
    
    * evm metrics
    
    * json rpc metrics
    
    * more
    
    * more
    
    * format

--- src/Nethermind/Nethermind.Synchronization/Metrics.cs
+++ src/Nethermind/Nethermind.Synchronization/Metrics.cs
@@ -2,59 +2,77 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System.ComponentModel;
+using Nethermind.Core.Attributes;
 
 namespace Nethermind.Synchronization
 {
     public static class Metrics
     {
+        [GaugeMetric]
         [Description("Headers downloaded in fast blocks stage")]
         public static decimal FastHeaders;
 
+        [GaugeMetric]
         [Description("Bodies downloaded in fast blocks stage")]
         public static long FastBodies;
 
+        [GaugeMetric]
         [Description("Receipts downloaded in fast blocks stage")]
         public static long FastReceipts;
 
+        [GaugeMetric]
         [Description("State synced in bytes")]
         public static long StateSynced;
 
+        [CounterMetric]
         [Description("Requests sent for state nodes sync")]
         public static long StateSyncRequests;
 
+        [CounterMetric]
         [Description("State trie nodes synced")]
         public static long SyncedStateTrieNodes;
 
+        [CounterMetric]
         [Description("Storage trie nodes synced")]
         public static long SyncedStorageTrieNodes;
 
+        [CounterMetric]
         [Description("Synced bytecodes")]
         public static long SyncedCodes;
 
+        [CounterMetric]
         [Description("State synced via SNAP Sync in bytes")]
         public static long SnapStateSynced;
 
+        [CounterMetric]
         [Description("Synced accounts via SNAP Sync")]
         public static long SnapSyncedAccounts;
 
+        [CounterMetric]
         [Description("Synced storage slots via SNAP Sync")]
         public static long SnapSyncedStorageSlots;
 
+        [CounterMetric]
         [Description("Synced bytecodes via SNAP Sync")]
         public static long SnapSyncedCodes;
 
+        [GaugeMetric]
         [Description("Number of sync peers.")]
         public static long SyncPeers;
 
+        [GaugeMetric]
         [Description("Number of priority peers.")]
         public static long PriorityPeers;
 
+        [GaugeMetric]
         [Description("State branch progress (percentage of completed branches at second level).")]
         public static long StateBranchProgress;
 
+        [GaugeMetric]
         [Description("Requests sent for processing by the witness state sync")]
         public static long WitnessStateRequests;
 
+        [GaugeMetric]
         [Description("Requests sent for processing by the witness block sync")]
         public static long WitnessBlockRequests;
     }
