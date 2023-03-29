e0aa992af7b9161101554bf7e8247d1749db9e03
Ruben Buniatyan <rubo@users.noreply.github.com>
2023-03-29 22:23:27 +0200

    Restore and deprecate `disableMemory` in favor of `enableMemory`

--- src/Nethermind/Nethermind.Evm/Tracing/GethStyle/GethTraceOptions.cs
+++ src/Nethermind/Nethermind.Evm/Tracing/GethStyle/GethTraceOptions.cs
@@ -1,31 +1,35 @@
 // SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using Nethermind.Core.Crypto;
 using Newtonsoft.Json;
 
-namespace Nethermind.Evm.Tracing.GethStyle
+namespace Nethermind.Evm.Tracing.GethStyle;
+
+public record GethTraceOptions
 {
-    public record GethTraceOptions
-    {
-        [JsonProperty("disableStack")]
-        public bool DisableStack { get; init; }
+    [JsonProperty("disableMemory")]
+    [Obsolete("Use EnableMemory instead.")]
+    public bool DisableMemory { get => !EnableMemory; init => EnableMemory = !value; }
 
-        [JsonProperty("disableStorage")]
-        public bool DisableStorage { get; init; }
+    [JsonProperty("disableStack")]
+    public bool DisableStack { get; init; }
 
-        [JsonProperty("enableMemory")]
-        public bool EnableMemory { get; init; }
+    [JsonProperty("disableStorage")]
+    public bool DisableStorage { get; init; }
 
-        [JsonProperty("timeout")]
-        public string Timeout { get; init; }
+    [JsonProperty("enableMemory")]
+    public bool EnableMemory { get; init; }
 
-        [JsonProperty("tracer")]
-        public string Tracer { get; init; }
+    [JsonProperty("timeout")]
+    public string Timeout { get; init; }
 
-        [JsonProperty("txHash")]
-        public Keccak? TxHash { get; init; }
+    [JsonProperty("tracer")]
+    public string Tracer { get; init; }
 
-        public static GethTraceOptions Default { get; } = new();
-    }
+    [JsonProperty("txHash")]
+    public Keccak? TxHash { get; init; }
+
+    public static GethTraceOptions Default { get; } = new();
 }
