40c43ebbe9f41d2b1ccaf868078b286a89503948
lukasz.rozmej <lukasz.rozmej@gmail.com>
2023-03-30 09:06:32 +0200

    Change how to execute commands

--- src/Nethermind/Nethermind.Runner/EnvironmentVariables/CommandRunner.cs
+++ src/Nethermind/Nethermind.Runner/EnvironmentVariables/CommandRunner.cs
@@ -11,26 +11,26 @@ public static class CommandRunner
 {
     public static (int exitCode, string output, string error) RunCommand(string command)
     {
-        ProcessStartInfo info = new(GetFileName(), GetArguments(command))
+        static string GetFileName() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "cmd.exe" : "/bin/bash";
+
+        ProcessStartInfo info = new(GetFileName())
         {
             UseShellExecute = false,
             CreateNoWindow = true,
             RedirectStandardOutput = true,
-            RedirectStandardError = true
+            RedirectStandardError = true,
+            RedirectStandardInput = true,
         };
         Process? process = Process.Start(info);
         if (process is not null)
         {
+            process.StandardInput.WriteLine(command);
+            process.StandardInput.Close();
             process.WaitForExit();
             return (process.ExitCode, process.StandardOutput.ReadToEnd(), process.StandardError.ReadToEnd());
         }
 
         return (-1, $"Couldn't start process {info.FileName} {info.Arguments}", string.Empty);
     }
-
-    private static string GetArguments(string command) =>
-        RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? $"/C {command}" : $"-c \"{command}\"";
-
-    private static string GetFileName() =>
-        RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "cmd.exe" : "bash";
 }
