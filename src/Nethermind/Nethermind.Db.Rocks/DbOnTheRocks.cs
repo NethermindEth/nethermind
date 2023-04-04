0ab3cd16845f55814c804eb9855f6f92bdfaf5cf
Amirul Ashraf <asdacap@gmail.com>
2023-04-04 20:18:38 +0800

    Additional rocksdb options

--- src/Nethermind/Nethermind.Db.Rocks/DbOnTheRocks.cs
+++ src/Nethermind/Nethermind.Db.Rocks/DbOnTheRocks.cs
@@ -88,6 +88,11 @@ public class DbOnTheRocks : IDbWithSpan, ITunableDb
         _rocksDbNative = rocksDbNative ?? RocksDbSharp.Native.Instance;
         _perTableDbConfig = new PerTableDbConfig(dbConfig, _settings);
         _db = Init(basePath, rocksDbSettings.DbPath, dbConfig, logManager, columnFamilies, rocksDbSettings.DeleteOnStart);
+
+        if (_perTableDbConfig.AdditionalSettings != null)
+        {
+            ApplyOptions(_perTableDbConfig.AdditionalSettings);
+        }
     }
 
     protected virtual RocksDb DoOpen(string path, (DbOptions Options, ColumnFamilies? Families) db)
