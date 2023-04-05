fc2bddc32e2b39fe93f14319015d1b0d7578150e
Amirul Ashraf <asdacap@gmail.com>
2023-04-05 13:15:08 +0800

    Perf/batched trie visitor (#5475)

--- src/Nethermind/Nethermind.Trie/VisitingOptions.cs
+++ src/Nethermind/Nethermind.Trie/VisitingOptions.cs
@@ -21,5 +21,14 @@ namespace Nethermind.Trie
         /// Maximum number of threads that will be used to visit the trie.
         /// </summary>
         public int MaxDegreeOfParallelism { get; init; } = 1;
+
+        /// <summary>
+        /// Specify memory budget to run a batched trie visitor. Significantly reduce read iops as memory budget
+        /// increase. Not effective below a certain amount, and above a certain amount it has no measurable difference
+        /// or the size of the db can't fill a queue of that size. For goerli, its 256MB to 6GB. For mainnet, its 1GB to
+        /// 12 GB. Effect may be larger on system with lower RAM due to bigger portion of uncached files, or system
+        /// with slower SSD. Set to 0 to disable batched trie visitor.
+        /// </summary>
+        public long FullScanMemoryBudget { get; set; }
     }
 }
