c5bc9ad2127f3fce6d269a4d4b700eb8d6a8b80f
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-04-14 09:12:55 +0530

    fix snap

--- src/Nethermind/Nethermind.Synchronization/SnapSync/SnapProvider.cs
+++ src/Nethermind/Nethermind.Synchronization/SnapSync/SnapProvider.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using Microsoft.Extensions.ObjectPool;
@@ -160,7 +161,8 @@ namespace Nethermind.Synchronization.SnapSync
 
         public AddRangeResult AddStorageRange(long blockNumber, PathWithAccount pathWithAccount, Keccak expectedRootHash, Keccak? startingHash, PathWithStorageSlot[] slots, byte[][]? proofs = null)
         {
-            ITrieStore store = _trieStorePool.Get();
+            Debug.Assert(pathWithAccount is not null, "path based storage need the account for the storage tree");
+            ITrieStore store = _pathBasedTrieStorePool.Get();
             StorageTree tree = new(store, _logManager, pathWithAccount.Path);
             try
             {
