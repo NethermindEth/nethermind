ee8e5e50ecdcbe88812d47f1144a07668965dfe7
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-04-26 18:26:21 +0530

    add new storage encoding scheme

--- src/Nethermind/Nethermind.Trie.Test/HexPrefixTests.cs
+++ src/Nethermind/Nethermind.Trie.Test/HexPrefixTests.cs
@@ -127,6 +127,18 @@ namespace Nethermind.Trie.Test
         }
 
         [Test]
+        public void Nibbles_encoding_decoding_for_path_based_tree()
+        {
+            byte[] nibbles = Enumerable.Repeat((byte)1, 64).ToArray();
+            byte[] result = Nibbles.NibblesToByteStorage(nibbles);
+            Nibbles.BytesToNibblesStorage(result).Should().BeEquivalentTo(nibbles);
+
+            byte[] nibbles2 = Enumerable.Repeat((byte)1, 5).ToArray();
+            byte[] result2 = Nibbles.NibblesToByteStorage(nibbles2);
+            Nibbles.BytesToNibblesStorage(result2).Should().BeEquivalentTo(nibbles2);
+        }
+
+        [Test]
         public void Nibbles_to_byte_and_reverse()
         {
             List<byte[]> keys = new List<byte[]>
--- src/Nethermind/Nethermind.Trie/NibbleExtensions.cs
+++ src/Nethermind/Nethermind.Trie/NibbleExtensions.cs
@@ -2,11 +2,12 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
+using System.Buffers;
 using System.Diagnostics;
+using Nethermind.Core.Extensions;
 
 namespace Nethermind.Trie
 {
-    [DebuggerStepThrough]
     public static class Nibbles
     {
         private static readonly byte PathPointerOdd = 0xfe;
@@ -133,6 +134,57 @@ namespace Nethermind.Trie
             }
 
             return bytes;
+        }
+
+        public static byte[] NibblesToByteStorage(Span<byte> nibbles)
+        {
+            int oddity = nibbles.Length % 2;
+            byte[] bytes = new byte[nibbles.Length / 2 + 1];
+            for (int i = 0; i < bytes.Length - 1; i++)
+            {
+                bytes[i + 1] = ToByte(nibbles[2 * i + oddity], nibbles[2 * i + 1 + oddity]);
+            }
+            if (oddity == 1)
+            {
+                bytes[0] = ToByte(1, nibbles[0]);
+            }
+            return bytes;
+        }
+
+        public static byte[] BytesToNibblesStorage(Span<byte> bytes)
+        {
+            Span<byte> nibbles = stackalloc byte[bytes.Length * 2];
+            BytesToNibbleBytes(bytes, nibbles);
+            int oddity = nibbles[0];
+            return oddity == 1 ? nibbles[1..].ToArray() : nibbles[2..].ToArray();
+        }
+
+        public static byte[] CompactToHexEncode(byte[] compactPath)
+        {
+            if (compactPath.Length == 0)
+            {
+                return compactPath;
+            }
+            int nibblesCount = compactPath.Length * 2 + 1;
+            byte[] array = ArrayPool<byte>.Shared.Rent(nibblesCount);
+            try
+            {
+                Span<byte> nibbles = array.Slice(0, nibblesCount);
+                BytesToNibbleBytes(compactPath, nibbles.Slice(0, 2 * compactPath.Length));
+                nibbles[^1] = 16;
+
+                if (nibbles[0] < 2)
+                {
+                    nibbles = nibbles[..^1];
+                }
+
+                int chop = 2 - (nibbles[0] & 1);
+                return nibbles[chop..].ToArray();
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(array);
+            }
         }
     }
 }
