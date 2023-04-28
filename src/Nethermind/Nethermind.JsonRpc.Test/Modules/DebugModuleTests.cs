0ce534f01468fe8718c857a3445492e73f230730
Ruben Buniatyan <rubo@users.noreply.github.com>
2023-04-28 22:27:13 +0200

    Fix Debug module tests

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/DebugModuleTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/DebugModuleTests.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -260,7 +261,7 @@ namespace Nethermind.JsonRpc.Test.Modules
                             },
                             Opcode = "STOP",
                             ProgramCounter = 0,
-                            Stack = { },
+                            Stack = Array.Empty<string>(),
                             Storage = new Dictionary<string, string>()
                             {
                                 {
