c0b03877aa9b1caf5f13309b644e6873aa36c8a0
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-05-01 17:35:35 +0530

    fix proof boundry node

--- src/Nethermind/Nethermind.Trie/Pruning/TrieStoreByPath.cs
+++ src/Nethermind/Nethermind.Trie/Pruning/TrieStoreByPath.cs
@@ -116,7 +116,7 @@ namespace Nethermind.Trie.Pruning
             EnsureCommitSetExistsForBlock(blockNumber);
 
             if (_logger.IsTrace) _logger.Trace($"Committing {nodeCommitInfo} at {blockNumber}");
-            if (!nodeCommitInfo.IsEmptyBlockMarker && !nodeCommitInfo.Node.IsBoundaryProofNode)
+            if (!nodeCommitInfo.IsEmptyBlockMarker)
             {
                 TrieNode node = nodeCommitInfo.Node!;
 
