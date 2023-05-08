7312b9c49dd11fa56cb1ec88680bc630fd08c978
benaadams <thundercat@illyriad.co.uk>
2023-05-08 11:35:18 +0100

    Per sec counts aren't useful for Evm metrics

--- src/Nethermind/Nethermind.Evm/Metrics.cs
+++ src/Nethermind/Nethermind.Evm/Metrics.cs
@@ -8,87 +8,87 @@ namespace Nethermind.Evm;
 
 public class Metrics
 {
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of EXTCODESIZE opcodes executed.")]
     public static long ExtCodeSizeOpcode;
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of EXTCODESIZE ISZERO optimizations.")]
     public static long ExtCodeSizeOptimizedIsZero;
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of EXTCODESIZE GT optimizations.")]
     public static long ExtCodeSizeOptimizedGT;
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of EXTCODESIZE EQ optimizations.")]
     public static long ExtCodeSizeOptimizedEQ;
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of EVM exceptions thrown by contracts.")]
     public static long EvmExceptions { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of SELFDESTRUCT calls.")]
     public static long SelfDestructs { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of calls to other contracts.")]
     public static long Calls { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of SLOAD opcodes executed.")]
     public static long SloadOpcode { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of SSTORE opcodes executed.")]
     public static long SstoreOpcode { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of TLOAD opcodes executed.")]
     public static long TloadOpcode { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of TSTORE opcodes executed.")]
     public static long TstoreOpcode { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of MODEXP precompiles executed.")]
     public static long ModExpOpcode { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of BLOCKHASH opcodes executed.")]
     public static long BlockhashOpcode { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of BN254_MUL precompile calls.")]
     public static long Bn254MulPrecompile { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of BN254_ADD precompile calls.")]
     public static long Bn254AddPrecompile { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of BN254_PAIRING precompile calls.")]
     public static long Bn254PairingPrecompile { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of EC_RECOVERY precompile calls.")]
     public static long EcRecoverPrecompile { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of MODEXP precompile calls.")]
     public static long ModExpPrecompile { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of RIPEMD160 precompile calls.")]
     public static long Ripemd160Precompile { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of SHA256 precompile calls.")]
     public static long Sha256Precompile { get; set; }
 
-    [CounterMetric]
+    [GaugeMetric]
     [Description("Number of Point Evaluation precompile calls.")]
     public static long PointEvaluationPrecompile { get; set; }
 }
