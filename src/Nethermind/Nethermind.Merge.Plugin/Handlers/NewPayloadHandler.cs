1c9a3801d86c924f636f99191bab8c716a022411
MarekM25 <marekm2504@gmail.com>
2023-05-22 09:42:40 +0200

    Working on pivot tests

--- src/Nethermind/Nethermind.Merge.Plugin/Handlers/NewPayloadHandler.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Handlers/NewPayloadHandler.cs
@@ -109,7 +109,8 @@ public class NewPayloadHandler : IAsyncHandler<ExecutionPayload, PayloadStatusV1
             return NewPayloadV1Result.Invalid(lastValidHash, $"Block {request} is known to be a part of an invalid chain.");
         }
 
-        if (block.Header.Number <= _syncConfig.PivotNumberParsed)
+        bool headIsZero = (_blockTree.Head?.Number ?? 0) == 0;
+        if (headIsZero && block.Header.Number <= _syncConfig.PivotNumberParsed)
         {
             if (_logger.IsInfo) _logger.Info($"Pre-pivot block, ignored and returned Syncing. Result of {requestStr}.");
             return NewPayloadV1Result.Syncing;
