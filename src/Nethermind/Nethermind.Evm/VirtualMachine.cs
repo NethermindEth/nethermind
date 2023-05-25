008fa97e0e3dd06ceb27b76383992c182a89f53a
Demuirgos <ayman.bouchareb@outlook.fr>
2023-05-25 17:19:11 +0100

    applied suggested changes

--- src/Nethermind/Nethermind.Evm/VirtualMachine.cs
+++ src/Nethermind/Nethermind.Evm/VirtualMachine.cs
@@ -580,9 +580,9 @@ public class VirtualMachine : IVirtualMachine
             CallResult callResult = new(output.ToArray(), success, !success);
             return callResult;
         }
-        catch (DllNotFoundException dllImportException)
+        catch (DllNotFoundException exception)
         {
-            if (_logger.IsDebug) _logger.Error($"Precompiled contract ({precompile.GetType()}) dll failed to load", dllImportException);
+            if (_logger.IsError) _logger.Error($"Failed to load one of the dependencies of {precompile.GetType()} precompile", exception);
             throw;
         }
         catch (Exception exception)
