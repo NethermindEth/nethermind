ef1c34e6fe0f496e97f21ca46242e00d9316feae
Ruben Buniatyan <rubo@users.noreply.github.com>
2023-06-14 18:02:18 +0200

    Add tests validating a slot by timestamp (#5792)

--- src/Nethermind/Nethermind.Specs.Test/ChainSpecStyle/ChainSpecBasedSpecProviderTests.cs
+++ src/Nethermind/Nethermind.Specs.Test/ChainSpecStyle/ChainSpecBasedSpecProviderTests.cs
@@ -22,6 +22,8 @@ namespace Nethermind.Specs.Test.ChainSpecStyle;
 [TestFixture]
 public class ChainSpecBasedSpecProviderTests
 {
+    private const ulong GnosisBlockTime = 5;
+
     [TestCase(0, null, false)]
     [TestCase(0, 0ul, false)]
     [TestCase(0, 4660ul, false)]
@@ -140,6 +142,9 @@ public class ChainSpecBasedSpecProviderTests
         Assert.That(provider.GenesisSpec.DifficultyBombDelay, Is.EqualTo(long.MaxValue));
         Assert.That(provider.ChainId, Is.EqualTo(BlockchainIds.Sepolia));
         Assert.That(provider.NetworkId, Is.EqualTo(BlockchainIds.Sepolia));
+
+        GetTransitionTimestamps(chainSpec.Parameters).Should().AllSatisfy(
+            t => ValidateSlotByTimestamp(t, SepoliaSpecProvider.BeaconChainGenesisTimestamp).Should().BeTrue());
     }
 
     [Test]
@@ -201,6 +206,9 @@ public class ChainSpecBasedSpecProviderTests
         Assert.That(provider.TerminalTotalDifficulty, Is.EqualTo(GoerliSpecProvider.Instance.TerminalTotalDifficulty));
         Assert.That(provider.ChainId, Is.EqualTo(BlockchainIds.Goerli));
         Assert.That(provider.NetworkId, Is.EqualTo(BlockchainIds.Goerli));
+
+        GetTransitionTimestamps(chainSpec.Parameters).Should().AllSatisfy(
+            t => ValidateSlotByTimestamp(t, GoerliSpecProvider.BeaconChainGenesisTimestamp).Should().BeTrue());
     }
 
     [Test]
@@ -230,6 +238,9 @@ public class ChainSpecBasedSpecProviderTests
         provider.GetSpec((1, ChiadoSpecProvider.ShanghaiTimestamp - 1)).MaxCodeSize.Should().Be(long.MaxValue);
         provider.GetSpec((1, ChiadoSpecProvider.ShanghaiTimestamp)).MaxCodeSize.Should().Be(24576L);
         provider.GetSpec((1, ChiadoSpecProvider.ShanghaiTimestamp)).MaxInitCodeSize.Should().Be(2 * 24576L);
+
+        GetTransitionTimestamps(chainSpec.Parameters).Should().AllSatisfy(
+            t => ValidateSlotByTimestamp(t, ChiadoSpecProvider.BeaconChainGenesisTimestamp, GnosisBlockTime).Should().BeTrue());
     }
 
     [Test]
@@ -340,6 +351,9 @@ public class ChainSpecBasedSpecProviderTests
         Assert.That(provider.TerminalTotalDifficulty, Is.EqualTo(MainnetSpecProvider.Instance.TerminalTotalDifficulty));
         Assert.That(provider.ChainId, Is.EqualTo(BlockchainIds.Mainnet));
         Assert.That(provider.NetworkId, Is.EqualTo(BlockchainIds.Mainnet));
+
+        GetTransitionTimestamps(chainSpec.Parameters).Should().AllSatisfy(
+            t => ValidateSlotByTimestamp(t, MainnetSpecProvider.BeaconChainGenesisTimestamp).Should().BeTrue());
     }
 
     [Flags]
@@ -777,4 +791,21 @@ public class ChainSpecBasedSpecProviderTests
         });
         TestTransitions((40001L, 1000000024), r => { r.IsEip1153Enabled = true; });
     }
+
+    private static IEnumerable<ulong> GetTransitionTimestamps(ChainParameters parameters) => parameters.GetType()
+        .Properties()
+        .Where(p => p.Name.EndsWith("TransitionTimestamp", StringComparison.Ordinal))
+        .Select(p => (ulong?)p.GetValue(parameters))
+        .Where(t => t is not null)
+        .Select(t => t!.Value);
+
+    /// <summary>
+    /// Validates the timestamp specified by making sure the resulting slot is a multiple of 8192.
+    /// </summary>
+    /// <param name="timestamp">The timestamp to validate</param>
+    /// <param name="genesisTimestamp">The network's genesis timestamp</param>
+    /// <param name="blockTime">The network's block time in seconds</param>
+    /// <returns><c>true</c> if the timestamp is valid; otherwise, <c>false</c>.</returns>
+    private static bool ValidateSlotByTimestamp(ulong timestamp, ulong genesisTimestamp, ulong blockTime = 12) =>
+        timestamp > genesisTimestamp && (timestamp - genesisTimestamp) / blockTime % 0x2000 == 0;
 }
