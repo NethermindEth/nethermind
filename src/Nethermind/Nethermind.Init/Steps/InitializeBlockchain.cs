9d77e9bd3f3d79c5e5e1be344c715c6f75e0a022
Alexey Osipov <me@flcl.me>
2023-06-15 00:20:51 +0300

    Add a safe check

--- src/Nethermind/Nethermind.Init/Steps/InitializeBlockchain.cs
+++ src/Nethermind/Nethermind.Init/Steps/InitializeBlockchain.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO.Abstractions;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Nethermind.Api;
@@ -306,11 +307,15 @@ namespace Nethermind.Init.Steps
                         api.PruningTrigger.Add(pruningTrigger);
                     }
 
-                    IDriveInfo drive = api.FileSystem.GetDriveInfos(pruningDbPath)[0];
-                    FullPruner pruner = new(fullPruningDb, api.PruningTrigger, pruningConfig, api.BlockTree!,
-                        stateReader, api.ProcessExit!, ChainSizes.CreateChainSizeInfo(api.ChainSpec.ChainId),
-                        drive, api.LogManager);
-                    api.DisposeStack.Push(pruner);
+                    IDriveInfo[] driveInfos = api.FileSystem.GetDriveInfos(pruningDbPath);
+                    if (driveInfos.Any())
+                    {
+                        IDriveInfo drive = driveInfos[0];
+                        FullPruner pruner = new(fullPruningDb, api.PruningTrigger, pruningConfig, api.BlockTree!,
+                            stateReader, api.ProcessExit!, ChainSizes.CreateChainSizeInfo(api.ChainSpec.ChainId),
+                            drive, api.LogManager);
+                        api.DisposeStack.Push(pruner);
+                    }
                 }
             }
         }
