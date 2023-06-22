aa47daea3f4a64c010a8beafc26214bc76d1e608
lukasz.rozmej <lukasz.rozmej@gmail.com>
2023-06-22 11:15:05 +0200

    fix test

--- src/Nethermind/Nethermind.Synchronization.Test/SyncReportTest.cs
+++ src/Nethermind/Nethermind.Synchronization.Test/SyncReportTest.cs
@@ -88,15 +88,15 @@ namespace Nethermind.Synchronization.Test
 
             if (setBarriers)
             {
-                logger.Received(1).Info("Old Headers    0 / 100 (  0.00 %) | queue         0 | current         0.00 Blk/s | total         0.00 Blk/s");
-                logger.Received(1).Info("Old Bodies     0 / 70 (  0.00 %) | queue         0 | current         0.00 Blk/s | total         0.00 Blk/s");
-                logger.Received(1).Info("Old Receipts   0 / 65 (  0.00 %) | queue         0 | current         0.00 Blk/s | total         0.00 Blk/s");
+                logger.Received(1).Info("Old Headers    0 / 100 (  0.00 %) | queue         0 | current            0 Blk/s | total            0 Blk/s");
+                logger.Received(1).Info("Old Bodies     0 / 70 (  0.00 %) | queue         0 | current            0 Blk/s | total            0 Blk/s");
+                logger.Received(1).Info("Old Receipts   0 / 65 (  0.00 %) | queue         0 | current            0 Blk/s | total            0 Blk/s");
             }
             else
             {
-                logger.Received(1).Info("Old Headers    0 / 100 (  0.00 %) | queue         0 | current         0.00 Blk/s | total         0.00 Blk/s");
-                logger.Received(1).Info("Old Bodies     0 / 100 (  0.00 %) | queue         0 | current         0.00 Blk/s | total         0.00 Blk/s");
-                logger.Received(1).Info("Old Receipts   0 / 100 (  0.00 %) | queue         0 | current         0.00 Blk/s | total         0.00 Blk/s");
+                logger.Received(1).Info("Old Headers    0 / 100 (  0.00 %) | queue         0 | current            0 Blk/s | total            0 Blk/s");
+                logger.Received(1).Info("Old Bodies     0 / 100 (  0.00 %) | queue         0 | current            0 Blk/s | total            0 Blk/s");
+                logger.Received(1).Info("Old Receipts   0 / 100 (  0.00 %) | queue         0 | current            0 Blk/s | total            0 Blk/s");
             }
         }
     }
