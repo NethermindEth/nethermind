c9ef655bbe09167227664693f8d655657241e323
lukasz.rozmej <lukasz.rozmej@gmail.com>
2023-06-22 09:42:11 +0200

    Shorten forkchoice hashes

--- src/Nethermind/Nethermind.Merge.Plugin/Data/ForkchoiceStateV1.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Data/ForkchoiceStateV1.cs
@@ -36,9 +36,9 @@ public class ForkchoiceStateV1
     /// <remarks>Can be <see cref="Keccak.Zero"/> when transition block is not finalized yet.</remarks>
     public Keccak FinalizedBlockHash { get; set; }
 
-    public override string ToString() => $"ForkChoice: Head: {HeadBlockHash}, Safe: {SafeBlockHash}, Finalized: {FinalizedBlockHash}";
+    public override string ToString() => $"ForkChoice: {{ Head: {HeadBlockHash.ToShortString()}, Safe: {SafeBlockHash.ToShortString()}, Finalized: {FinalizedBlockHash.ToShortString()} }}";
     public string ToString(long? headNumber, long? safeNumber, long? finalizedNumber) =>
         headNumber is null && safeNumber is null && finalizedNumber is null
             ? ToString()
-            : $"ForkChoice: Head: {headNumber} ({HeadBlockHash}), Safe: {safeNumber} ({SafeBlockHash}), Finalized: {finalizedNumber} ({FinalizedBlockHash})";
+            : $"ForkChoice: {{ Head: {headNumber} ({HeadBlockHash.ToShortString()}), Safe: {safeNumber} ({SafeBlockHash.ToShortString()}), Finalized: {finalizedNumber} ({FinalizedBlockHash.ToShortString()}) }}";
 }
