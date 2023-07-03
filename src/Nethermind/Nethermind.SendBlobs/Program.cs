3168e91c1a22f536932a8f0402f7d007ddb51478
Alexey Osipov <me@flcl.me>
2023-07-03 13:04:15 +0300

    Add more args

--- src/Nethermind/Nethermind.SendBlobs/Program.cs
+++ src/Nethermind/Nethermind.SendBlobs/Program.cs
@@ -13,14 +13,21 @@ using Nethermind.Serialization.Json;
 using Nethermind.Serialization.Rlp;
 using Org.BouncyCastle.Utilities.Encoders;
 
-// send-blobs <url-without-auth> <transactions-count-1-blob-each> <secret-key> <receiver-address>
-// send-blobs http://localhost:8545 5 0x0000000000000000000000000000000000000000000000000000000000000000 0x000000000000000000000000000000000000f1c1
+// send-blobs <url-without-auth> <transactions-send-formula 10x1,4x2,3x6> <secret-key> <receiver-address>
+// send-blobs http://localhost:8545 5 0x0000000000000000000000000000000000000000000000000000000000000000 0x000000000000000000000000000000000000f1c1 100 100
 
 string rpcUrl = args[0];
-int blobTxCount = int.Parse(args[1]);
+(int count, int perTx)[] blobTxCounts = args[1].Split(',')
+    .Select(x => x.Contains("x") ? (int.Parse(x.Split('x')[0]), int.Parse(x.Split('x')[1])) : (int.Parse(x), 1))
+    .ToArray();
 string privateKeyString = args[2];
 string receiver = args[3];
 
+ulong maxFeePerDataGas = 1000;
+if (args.Length > 4) ulong.TryParse(args[4], out maxFeePerDataGas);
+
+ulong feeMultiplier = 4;
+if (args.Length > 5) ulong.TryParse(args[5], out feeMultiplier);
 
 await KzgPolynomialCommitments.InitializeAsync();
 
@@ -35,80 +42,90 @@ INodeManager nodeManager = new NodeManager(engine, serializer, cliConsole, logMa
 nodeManager.SwitchUri(new Uri(rpcUrl));
 
 string? nonceString = await nodeManager.Post<string>("eth_getTransactionCount", privateKey.Address, "latest");
-if(nonceString is null)
+if (nonceString is null)
 {
     logger.Error("Unable to get nonce");
     return;
 }
-ulong nonce = Convert.ToUInt64(nonceString, nonceString.StartsWith("0x") ? 16 : 10);
 
 string? chainIdString = await nodeManager.Post<string>("eth_chainId") ?? "1";
 ulong chainId = Convert.ToUInt64(chainIdString, chainIdString.StartsWith("0x") ? 16 : 10);
 
-string? gasPriceRes = await nodeManager.Post<string>("eth_gasPrice") ?? "1";
-UInt256 gasPrice = (UInt256)Convert.ToUInt64(gasPriceRes, gasPriceRes.StartsWith("0x") ? 16 : 10);
-
-string? gasPriceTipRes = await nodeManager.Post<string>("eth_maxPriorityFeePerGas") ?? "1";
-UInt256 gasPriceTip = (UInt256)Convert.ToUInt64(gasPriceTipRes, gasPriceTipRes.StartsWith("0x") ? 16 : 10);
-
 Signer signer = new Signer(chainId, privateKey, new OneLoggerLogManager(logger));
 TxDecoder txDecoder = new();
 
-while (blobTxCount > 0)
+ulong nonce = Convert.ToUInt64(nonceString, nonceString.StartsWith("0x") ? 16 : 10);
+
+foreach ((int count, int perTx) btxc in blobTxCounts)
 {
-    blobTxCount--;
-    const int blobCount = 1;
-
-    byte[][] blobs = new byte[blobCount][];
-    byte[][] commitments = new byte[blobCount][];
-    byte[][] proofs = new byte[blobCount][];
-    byte[][] blobhashes = new byte[blobCount][];
-
-    for (int blobIndex = 0; blobIndex < blobCount; blobIndex++)
+    (int txCount, int blobCount) = btxc;
+    while (txCount > 0)
     {
-        blobs[blobIndex] = new byte[Ckzg.Ckzg.BytesPerBlob];
-        new Random().NextBytes(blobs[blobIndex]);
-        for (int i = 0; i < Ckzg.Ckzg.BytesPerBlob; i += 32)
+        txCount--;
+
+        byte[][] blobs = new byte[blobCount][];
+        byte[][] commitments = new byte[blobCount][];
+        byte[][] proofs = new byte[blobCount][];
+        byte[][] blobhashes = new byte[blobCount][];
+
+        for (int blobIndex = 0; blobIndex < blobCount; blobIndex++)
         {
-            blobs[blobIndex][i] = 0;
+            blobs[blobIndex] = new byte[Ckzg.Ckzg.BytesPerBlob];
+            new Random().NextBytes(blobs[blobIndex]);
+            for (int i = 0; i < Ckzg.Ckzg.BytesPerBlob; i += 32)
+            {
+                blobs[blobIndex][i] = 0;
+            }
+
+            commitments[blobIndex] = new byte[Ckzg.Ckzg.BytesPerCommitment];
+            proofs[blobIndex] = new byte[Ckzg.Ckzg.BytesPerProof];
+            blobhashes[blobIndex] = new byte[32];
+
+            KzgPolynomialCommitments.KzgifyBlob(
+                blobs[blobIndex].AsSpan(),
+                commitments[blobIndex].AsSpan(),
+                proofs[blobIndex].AsSpan(),
+                blobhashes[blobIndex].AsSpan());
         }
+
         
-        commitments[blobIndex] = new byte[Ckzg.Ckzg.BytesPerCommitment];
-        proofs[blobIndex] = new byte[Ckzg.Ckzg.BytesPerProof];
-        blobhashes[blobIndex] = new byte[32];
+        string? gasPriceRes = await nodeManager.Post<string>("eth_gasPrice") ?? "1";
+        UInt256 gasPrice = (UInt256)Convert.ToUInt64(gasPriceRes, gasPriceRes.StartsWith("0x") ? 16 : 10);
 
-        KzgPolynomialCommitments.KzgifyBlob(
-            blobs[blobIndex].AsSpan(),
-            commitments[blobIndex].AsSpan(),
-            proofs[blobIndex].AsSpan(),
-            blobhashes[blobIndex].AsSpan());
+        string? maxPriorityFeePerGasRes = await nodeManager.Post<string>("eth_maxPriorityFeePerGas") ?? "1";
+        UInt256 maxPriorityFeePerGas = (UInt256)Convert.ToUInt64(maxPriorityFeePerGasRes, maxPriorityFeePerGasRes.StartsWith("0x") ? 16 : 10);
+
+        Console.WriteLine($"Nonce: {nonce}, GasPrice: {gasPrice}, MaxPriorityFeePerGas: {maxPriorityFeePerGas}");
+
+        Transaction tx = new()
+        {
+            Type = TxType.Blob,
+            ChainId = chainId,
+            Nonce = nonce,
+            GasLimit = GasCostOf.Transaction,
+            GasPrice = gasPrice * feeMultiplier,
+            DecodedMaxFeePerGas = gasPrice * feeMultiplier,
+            MaxFeePerDataGas = maxFeePerDataGas,
+            Value = 0,
+            To = new Address(receiver),
+            BlobVersionedHashes = blobhashes,
+            NetworkWrapper = new ShardBlobNetworkWrapper(blobs, commitments, proofs),
+        };
+
+        await signer.Sign(tx);
+
+
+        string txRlp = Hex.ToHexString(txDecoder
+            .Encode(tx, RlpBehaviors.InMempoolForm | RlpBehaviors.SkipTypedWrapping).Bytes);
+
+        string? result = await nodeManager.Post<string>("eth_sendRawTransaction", "0x" + txRlp);
+
+        Console.WriteLine("Result:" + result);
+
+        nonce++;
+        //if (delayBetweenTransactions != 0)
+        //{
+        //    await Task.Delay(delayBetweenTransactions); // delay in milliseconds
+        //}
     }
-
-    Transaction tx = new()
-    {
-        Type = TxType.Blob,
-        ChainId = chainId,
-        Nonce = nonce,
-        GasLimit = GasCostOf.Transaction,
-        GasPrice = gasPrice,
-        DecodedMaxFeePerGas = gasPrice + gasPriceTip,
-        MaxFeePerDataGas = 100,
-        Value = 0,
-        To = new Address(receiver),
-        BlobVersionedHashes = blobhashes,
-        NetworkWrapper = new ShardBlobNetworkWrapper(blobs, commitments, proofs),
-    };
-
-    await signer.Sign(tx);
-
-    
-    string txRlp = Hex.ToHexString(txDecoder
-        .Encode(tx, RlpBehaviors.InMempoolForm | RlpBehaviors.SkipTypedWrapping).Bytes);
-
-    string? result = await nodeManager.Post<string>("eth_sendRawTransaction", "0x" + txRlp);
-
-    Console.WriteLine("Result:" + result);
-
-    nonce++;
-    //await Task.Delay(500); // delay in milliseconds
 }
--- src/Nethermind/Nethermind.SendBlobs/README.md
+++ src/Nethermind/Nethermind.SendBlobs/README.md
@@ -1,11 +1,35 @@
 # Send Blobs tool
 
-## Building
+## Run using docker
+
+```
+docker run ghcr.io/flcl42/send-blobs:latest http://localhost:8545 5 0x0000000000000000000000000000000000000000000000000000000000000000 0x000000000000000000000000000000000000f1c1
+docker run ghcr.io/flcl42/send-blobs:latest http://localhost:8545 5x6 0x0000000000000000000000000000000000000000000000000000000000000000 0x000000000000000000000000000000000000f1c1 10000 4
+```
+
+## Usage
+
+```sh
+./Nethermind.SendBlobs http://localhost:8545 # url-without-auth> \
+                       1000,5x6,100x2        # transaction count: just a number or a list of tx-count x blob-count \
+                       0x0000..0000          # secret-key \
+                       0x0000..0042          # receiver-address \
+                       10000                 # data gas price limit, 1000 by default \
+                       4                     # fee multiplier to compete with other txs in the pool, 4 by default
+# more example
+# send 5 transactions
+./Nethermind.SendBlobs http://localhost:8545 5 0x0000000000000000000000000000000000000000000000000000000000000000 0x000000000000000000000000000000000000f1c1 10000 4
+# send several trasnactions with 1 blob, then with 6 blobs and with 2 blobs in the end
+./Nethermind.SendBlobs http://localhost:8545 10x1,10x6,10x2 0x0000000000000000000000000000000000000000000000000000000000000000 0x000000000000000000000000000000000000f1c1 10000 4
+```
+
+## Build
 
 ```sh
 apt install libsnappy-dev dotnet-sdk-7.0 -y
 cd ./nethermind/src/Nethermind/Nethermind.SendBlobs
 dotnet publish --sc -o .
+./Nethermind.SendBlobs
 ```
 
 or via docker
@@ -16,9 +40,4 @@ docker build . -f ./src/Nethermind/Nethermind.SendBlobs/Dockerfile -t send-blobs
 docker run send-blobs ... # args sample below
 ```
 
-## Usage
 
-```sh
-./Nethermind.SendBlobs <url-without-auth> <transactions-count-1-blob-each> <secret-key> <receiver-address>
-./Nethermind.SendBlobs http://localhost:8545 5 0x0000000000000000000000000000000000000000000000000000000000000000 0x000000000000000000000000000000000000f1c1
-```
