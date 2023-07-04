f5481c7801c3b782f1012b518d1ec74f54954d24
Marcin Sobczak <marcindsobczak@gmail.com>
2023-07-04 13:28:28 +0200

    Divide GetPooledTxs request to few affordable ones

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V65/PooledTxsRequestor.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V65/PooledTxsRequestor.cs
@@ -7,6 +7,7 @@ using Nethermind.Core;
 using Nethermind.Core.Caching;
 using Nethermind.Core.Collections;
 using Nethermind.Core.Crypto;
+using Nethermind.Network.P2P.Subprotocols.Eth.V62.Messages;
 using Nethermind.Network.P2P.Subprotocols.Eth.V65.Messages;
 using Nethermind.TxPool;
 
@@ -30,8 +31,7 @@ namespace Nethermind.Network.P2P.Subprotocols.Eth.V65
 
             if (discoveredTxHashes.Count != 0)
             {
-                send(new GetPooledTransactionsMessage(discoveredTxHashes));
-                Metrics.Eth65GetPooledTransactionsRequested++;
+                RequestPooledTransactions(send, discoveredTxHashes);
             }
         }
 
@@ -42,9 +42,38 @@ namespace Nethermind.Network.P2P.Subprotocols.Eth.V65
 
             if (discoveredTxHashes.Count != 0)
             {
-                GetPooledTransactionsMessage msg65 = new(discoveredTxHashes);
-                send(new V66.Messages.GetPooledTransactionsMessage() { EthMessage = msg65 });
-                Metrics.Eth66GetPooledTransactionsRequested++;
+                RequestPooledTransactionsEth66(send, discoveredTxHashes);
+            }
+        }
+
+        public void RequestTransactionsEth68(Action<V66.Messages.GetPooledTransactionsMessage> send, IReadOnlyList<Keccak> hashes, IReadOnlyList<int> sizes)
+        {
+            using ArrayPoolList<(Keccak Hash, int Size)> discoveredTxHashesAndSizes = new(hashes.Count);
+            AddMarkUnknownHashesEth68(hashes, sizes, discoveredTxHashesAndSizes);
+
+            if (discoveredTxHashesAndSizes.Count != 0)
+            {
+                int packetSizeLeft = TransactionsMessage.MaxPacketSize;
+                using ArrayPoolList<Keccak> hashesToRequest = new(discoveredTxHashesAndSizes.Count);
+
+                for (int i = 0; i < discoveredTxHashesAndSizes.Count; i++)
+                {
+                    int txSize = discoveredTxHashesAndSizes[i].Size;
+
+                    if (txSize > packetSizeLeft && hashesToRequest.Count > 0)
+                    {
+                        RequestPooledTransactionsEth66(send, hashesToRequest);
+                        hashesToRequest.Clear();
+                    }
+
+                    hashesToRequest.Add(discoveredTxHashesAndSizes[i].Hash);
+                    packetSizeLeft -= txSize;
+                }
+
+                if (hashesToRequest.Count > 0)
+                {
+                    RequestPooledTransactionsEth66(send, hashesToRequest);
+                }
             }
         }
 
@@ -59,6 +88,31 @@ namespace Nethermind.Network.P2P.Subprotocols.Eth.V65
                     discoveredTxHashes.Add(hash);
                 }
             }
+        }
+        private void AddMarkUnknownHashesEth68(IReadOnlyList<Keccak> hashes, IReadOnlyList<int> sizes, ArrayPoolList<(Keccak, int)> discoveredTxHashesAndSizes)
+        {
+            int count = hashes.Count;
+            for (int i = 0; i < count; i++)
+            {
+                Keccak hash = hashes[i];
+                if (!_txPool.IsKnown(hash) && _pendingHashes.Set(hash))
+                {
+                    discoveredTxHashesAndSizes.Add((hash, sizes[i]));
+                }
+            }
+        }
+
+        private void RequestPooledTransactions(Action<GetPooledTransactionsMessage> send, IReadOnlyList<Keccak> hashesToRequest)
+        {
+            send(new GetPooledTransactionsMessage(hashesToRequest));
+            Metrics.Eth65GetPooledTransactionsRequested++;
+        }
+
+        private void RequestPooledTransactionsEth66(Action<V66.Messages.GetPooledTransactionsMessage> send, IReadOnlyList<Keccak> hashesToRequest)
+        {
+            GetPooledTransactionsMessage msg65 = new(hashesToRequest);
+            send(new V66.Messages.GetPooledTransactionsMessage() { EthMessage = msg65 });
+            Metrics.Eth66GetPooledTransactionsRequested++;
         }
     }
 }
