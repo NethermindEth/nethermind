fd186a9ea31bc80341d95fbb18252d4e5ed8a080
Lautaro Emanuel <emlautarom1@gmail.com>
2023-07-11 09:55:43 -0300

    Lift formatter to class field

--- tools/Nethermind.Tools.Kute/MetricsConsumer/JsonMetricsConsumer.cs
+++ tools/Nethermind.Tools.Kute/MetricsConsumer/JsonMetricsConsumer.cs
@@ -1,20 +1,21 @@
 // SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using App.Metrics.Formatters;
 using App.Metrics.Formatters.Json;
 
 namespace Nethermind.Tools.Kute.MetricsConsumer;
 
 public class JsonMetricsConsumer : IMetricsConsumer
 {
+    private readonly IMetricsOutputFormatter _formatter = new MetricsJsonOutputFormatter();
+
     public async Task ConsumeMetrics(Metrics metrics)
     {
         var snapshot = metrics.Snapshot;
-        var formatter = new MetricsJsonOutputFormatter();
-
         using (var stream = Console.OpenStandardOutput())
         {
-            await formatter.WriteAsync(stream, snapshot);
+            await _formatter.WriteAsync(stream, snapshot);
         }
     }
 }
--- tools/Nethermind.Tools.Kute/MetricsConsumer/PrettyReportMetricsConsumer.cs
+++ tools/Nethermind.Tools.Kute/MetricsConsumer/PrettyReportMetricsConsumer.cs
@@ -1,20 +1,21 @@
 // SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using App.Metrics.Formatters;
 using App.Metrics.Formatters.Ascii;
 
 namespace Nethermind.Tools.Kute.MetricsConsumer;
 
 class PrettyReportMetricsConsumer : IMetricsConsumer
 {
+    private readonly IMetricsOutputFormatter _formatter = new MetricsTextOutputFormatter();
+
     public async Task ConsumeMetrics(Metrics metrics)
     {
         var snapshot = metrics.Snapshot;
-        var formatter = new MetricsTextOutputFormatter();
-
         using (var stream = Console.OpenStandardOutput())
         {
-            await formatter.WriteAsync(stream, snapshot);
+            await _formatter.WriteAsync(stream, snapshot);
         }
     }
 }
