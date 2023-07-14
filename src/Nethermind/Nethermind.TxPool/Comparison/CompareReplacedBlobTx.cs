45115fa5ff42c27ab6c550362f09a2da77112b8c
Marcin Sobczak <marcindsobczak@gmail.com>
2023-07-14 13:02:19 +0200

    adjust blob replacement comparer

--- src/Nethermind/Nethermind.TxPool/Comparison/CompareReplacedBlobTx.cs
+++ src/Nethermind/Nethermind.TxPool/Comparison/CompareReplacedBlobTx.cs
@@ -3,7 +3,6 @@
 
 using System.Collections.Generic;
 using Nethermind.Core;
-using Nethermind.Int256;
 
 namespace Nethermind.TxPool.Comparison;
 
@@ -13,41 +12,22 @@ public class CompareReplacedBlobTx : IComparer<Transaction?>
 
     public CompareReplacedBlobTx() { }
 
-
-    //ToDo: it's copy-pasted std comparer, need to be adjusted for a needs of blobs
-
-
-    // To replace old transaction, new transaction needs to have fee higher by at least 10% (1/10) of current fee.
-    // It is required to avoid acceptance and propagation of transaction with almost the same fee as replaced one.
-    private const int PartOfFeeRequiredToIncrease = 10;
-
+    // To replace old blob transaction, new transaction needs to have fee at least 2x higher than current fee.
+    // 2x higher must be MaxPriorityFeePerGas, MaxFeePerGas and MaxFeePerDataGas
     public int Compare(Transaction? x, Transaction? y)
     {
         if (ReferenceEquals(x, y)) return 0;
         if (ReferenceEquals(null, y)) return 1;
         if (ReferenceEquals(null, x)) return -1;
 
-        // always allow replacement of zero fee txs (in legacy txs MaxFeePerGas equals GasPrice)
-        if (y.MaxFeePerGas.IsZero) return -1;
+        // always allow replacement of zero fee txs
+        if (y.MaxFeePerGas.IsZero) return -1; //ToDo: do we need it?
 
-        if (!x.Supports1559 && !y.Supports1559)
-        {
-            y.GasPrice.Divide(PartOfFeeRequiredToIncrease, out UInt256 bumpGasPrice);
-            int gasPriceResult = (y.GasPrice + bumpGasPrice).CompareTo(x.GasPrice);
-            // return -1 (replacement accepted) if fee bump is exactly by PartOfFeeRequiredToIncrease
-            // never return 0 - it's allowed or not
-            return gasPriceResult != 0 ? gasPriceResult : bumpGasPrice > 0 ? -1 : 1;
-        }
+        if (y.MaxFeePerGas * 2 > x.MaxFeePerGas) return 1;
+        if (y.MaxPriorityFeePerGas * 2 > x.MaxPriorityFeePerGas) return 1;
+        if (y.MaxFeePerDataGas * 2 > x.MaxFeePerDataGas) return 1;
 
-        /* MaxFeePerGas for legacy will be GasPrice and MaxPriorityFeePerGas will be GasPrice too
-        so we can compare legacy txs without any problems */
-        y.MaxFeePerGas.Divide(PartOfFeeRequiredToIncrease, out UInt256 bumpMaxFeePerGas);
-        if (y.MaxFeePerGas + bumpMaxFeePerGas > x.MaxFeePerGas) return 1;
-
-        y.MaxPriorityFeePerGas.Divide(PartOfFeeRequiredToIncrease, out UInt256 bumpMaxPriorityFeePerGas);
-        int result = (y.MaxPriorityFeePerGas + bumpMaxPriorityFeePerGas).CompareTo(x.MaxPriorityFeePerGas);
-        // return -1 (replacement accepted) if fee bump is exactly by PartOfFeeRequiredToIncrease
-        // never return 0 - it's allowed or not
-        return result != 0 ? result : (bumpMaxFeePerGas > 0 && bumpMaxPriorityFeePerGas > 0) ? -1 : 1;
+        // if we are here, it means that all new fees are at least 2x higher than old ones, so replacement is allowed
+        return -1;
     }
 }
