77bf9ce4e944290b33b259a7f02c9601a0d495fe
lukasz.rozmej <lukasz.rozmej@gmail.com>
2023-07-18 14:45:29 +0200

    more tries

--- src/Nethermind/Nethermind.Synchronization/Trie/TrieNodeRecovery.cs
+++ src/Nethermind/Nethermind.Synchronization/Trie/TrieNodeRecovery.cs
@@ -30,7 +30,7 @@ public abstract class TrieNodeRecovery<TRequest>
 
     public async Task<byte[]?> Recover(TRequest request)
     {
-        if (_logger.IsWarn) _logger.Warn($"Missing trie node, trying to recover from network");
+        if (_logger.IsWarn) _logger.Warn("Missing trie node, trying to recover from network");
         byte[]? checkKeyRecoveriesResults = await RecoverCore(request);
         if (checkKeyRecoveriesResults is not null) return checkKeyRecoveriesResults;
 
@@ -43,27 +43,30 @@ public abstract class TrieNodeRecovery<TRequest>
     {
         using CancellationTokenSource cts = new(Timeouts.Eth);
         List<Recovery> keyRecoveries = GenerateKeyRecoveries(request, cts);
-        byte[]? checkKeyRecoveriesResults = await CheckKeyRecoveriesResults(keyRecoveries, cts);
+        byte[]? checkKeyRecoveriesResults = await CheckKeyRecoveriesResults(keyRecoveries);
+        cts.Cancel();
+        if (checkKeyRecoveriesResults is not null)
+        {
+            if (_logger.IsWarn) _logger.Warn("Recovered!");
+        }
         return checkKeyRecoveriesResults;
     }
 
-    protected async Task<byte[]?> CheckKeyRecoveriesResults(List<Recovery> keyRecoveries, CancellationTokenSource cts)
+    protected async Task<byte[]?> CheckKeyRecoveriesResults(List<Recovery> keyRecoveries)
     {
         while (keyRecoveries.Count > 0)
         {
-            Task<byte[]> task = await Task.WhenAny(keyRecoveries.Select(kr => kr.Task!));
-            byte[]? result = await task;
-            int index = keyRecoveries.FindIndex(r => r.Task == task);
-            if (result is null)
+            Task<(Recovery, byte[]?)> task = await Task.WhenAny(keyRecoveries.Select(kr => kr.Task!));
+            (Recovery Recovery, byte[]? Data) result = await task;
+            if (result.Data is null)
             {
-                _logger.Warn($"Got empty response from peer {keyRecoveries[index].Peer}");
-                keyRecoveries.RemoveAt(index);
+                _logger.Warn($"Got empty response from peer {result.Recovery.Peer}");
+                keyRecoveries.Remove(result.Recovery);
             }
             else
             {
-                _logger.Warn($"Successfully recovered from peer {keyRecoveries[index].Peer} with {result.Length} bytes!");
-                cts.Cancel();
-                return result;
+                _logger.Warn($"Successfully recovered from peer {result.Recovery.Peer} with {result.Data.Length} bytes!");
+                return result.Data;
             }
         }
 
@@ -76,7 +79,7 @@ public abstract class TrieNodeRecovery<TRequest>
         if (_logger.IsWarn) _logger.Warn($"Allocated {keyRecoveries.Count} peers (out of {_syncPeerPool!.InitializedPeers.Count()} initialized peers)");
         foreach (Recovery keyRecovery in keyRecoveries)
         {
-            keyRecovery.Task = RecoverRlpFromPeer(keyRecovery.Peer, requestedHashes, cts);
+            keyRecovery.Task = RecoverRlpFromPeer(keyRecovery, requestedHashes, cts);
         }
 
         return keyRecoveries;
@@ -109,11 +112,13 @@ public abstract class TrieNodeRecovery<TRequest>
 
     protected virtual bool CanAllocatePeer(ISyncPeer peer) => peer.HeadNumber >= (_blockTree.Head?.Number ?? 0);
 
-    private async Task<byte[]?> RecoverRlpFromPeer(ISyncPeer peer, TRequest request, CancellationTokenSource cts)
+    private async Task<(Recovery, byte[]?)> RecoverRlpFromPeer(Recovery recovery, TRequest request, CancellationTokenSource cts)
     {
+        ISyncPeer peer = recovery.Peer;
+
         try
         {
-            return await RecoverRlpFromPeerBase(peer, request, cts);
+            return (recovery, await RecoverRlpFromPeerBase(peer, request, cts));
         }
         catch (OperationCanceledException)
         {
@@ -124,7 +129,7 @@ public abstract class TrieNodeRecovery<TRequest>
             if (_logger.IsError) _logger.Error($"Could not recover from {peer}", e);
         }
 
-        return null;
+        return (recovery, null);
     }
 
     protected abstract Task<byte[]?> RecoverRlpFromPeerBase(ISyncPeer peer, TRequest request, CancellationTokenSource cts);
@@ -132,6 +137,6 @@ public abstract class TrieNodeRecovery<TRequest>
     protected class Recovery
     {
         public ISyncPeer Peer { get; init; } = null!;
-        public Task<byte[]?>? Task { get; set; }
+        public Task<(Recovery, byte[]?)>? Task { get; set; }
     }
 }
