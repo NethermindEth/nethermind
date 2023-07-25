36748d696fd6a11dd2143f8edf0923a07acb2687
Marcin Sobczak <marcindsobczak@gmail.com>
2023-07-25 18:11:58 +0200

    adjust benchmarks

--- src/Nethermind/Nethermind.Network.Benchmark/Eth62ProtocolHandlerBenchmarks.cs
+++ src/Nethermind/Nethermind.Network.Benchmark/Eth62ProtocolHandlerBenchmarks.cs
@@ -57,7 +57,7 @@ namespace Nethermind.Network.Benchmarks
             var specProvider = MainnetSpecProvider.Instance;
             TxPool.TxPool txPool = new TxPool.TxPool(
                 ecdsa,
-                new BlobTxStorage(new MemDb()),
+                new BlobTxStorage(new MemDb(), new MemDb()),
                 new ChainHeadInfoProvider(new FixedForkActivationChainHeadSpecProvider(MainnetSpecProvider.Instance), tree, stateProvider),
                 new TxPoolConfig(),
                 new TxValidator(TestBlockchainIds.ChainId),
