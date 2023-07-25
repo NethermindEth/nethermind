620d35493a90113642eea4367522456a99193b07
Amirul Ashraf <asdacap@gmail.com>
2023-07-25 10:41:47 +0000

    Fix test

--- src/Nethermind/Nethermind.Network.Discovery/IDiscoveryConfig.cs
+++ src/Nethermind/Nethermind.Network.Discovery/IDiscoveryConfig.cs
@@ -11,79 +11,79 @@ public interface IDiscoveryConfig : IConfig
     /// <summary>
     /// Kademlia - k
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "16")]
     int BucketSize { get; set; }
 
     /// <summary>
-    /// Buckets count
+    /// Buckets count.
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DisabledForCli = true)]
     int BucketsCount { get; set; }
 
     /// <summary>
     /// Kademlia - alpha
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "3")]
     int Concurrency { get; }
 
     /// <summary>
     /// Kademlia - b
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DisabledForCli = true)]
     int BitsPerHop { get; }
 
     /// <summary>
     /// Max Discovery Rounds
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "8")]
     int MaxDiscoveryRounds { get; }
 
     /// <summary>
     /// Eviction check interval in ms
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "75")]
     int EvictionCheckInterval { get; }
 
     /// <summary>
     /// Send Node Timeout in ms
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "500")]
     int SendNodeTimeout { get; }
 
     /// <summary>
     /// Pong Timeout in ms
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "15000")]
     int PongTimeout { get; set; }
 
     /// <summary>
     /// Boot Node Pong Timeout in ms
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "100000")]
     int BootnodePongTimeout { get; }
 
     /// <summary>
     /// Pong Timeout in ms
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "3")]
     int PingRetryCount { get; }
 
     /// <summary>
     /// Time between running discovery processes in milliseconds
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "30000")]
     int DiscoveryInterval { get; }
 
     /// <summary>
     /// Time between persisting discovered nodes in milliseconds
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "180000")]
     int DiscoveryPersistenceInterval { get; }
 
     /// <summary>
     /// Time between discovery cycles in milliseconds
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "50")]
     int DiscoveryNewCycleWaitTime { get; }
 
     /// <summary>
@@ -94,18 +94,18 @@ public interface IDiscoveryConfig : IConfig
     /// <summary>
     /// Timeout for closing UDP channel in milliseconds
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "5000")]
     int UdpChannelCloseTimeout { get; }
 
     /// <summary>
     /// Maximum count of NodeLifecycleManagers stored in memory
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "8000")]
     int MaxNodeLifecycleManagersCount { get; }
 
     /// <summary>
     /// Count of NodeLifecycleManagers to remove in one cleanup cycle
     /// </summary>
-    [ConfigItem]
+    [ConfigItem(DefaultValue = "4000")]
     int NodeLifecycleManagersCleanupCount { get; }
 }
