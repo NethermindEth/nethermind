b4a0138f6566931f74b3f651ad9b5dd6e1a4501d
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2023-08-08 16:48:38 +0200

    Fix loading plugins with configs (#5981)
    * Consider plugins when searching for Nethermind types, remove logs from BoundedModule to not break plugin registrations
    
    * fix whitespace
    
    * try fix some tests
    
    * rename
    
    * Move to file scope namespaces and refactor
    
    * fix test, remove serialized from JsonRpc tests output
    
    * Move to file scope namespace
    
    ---------
    
    Co-authored-by: Ruben Buniatyan <rubo@users.noreply.github.com>

--- src/Nethermind/Nethermind.JsonRpc/Modules/BoundedModulePool.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/BoundedModulePool.cs
@@ -1,92 +1,84 @@
 // SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System;
 using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 using Nethermind.JsonRpc.Exceptions;
-using Nethermind.Logging;
-using ILogger = Nethermind.Logging.ILogger;
 
-namespace Nethermind.JsonRpc.Modules
+namespace Nethermind.JsonRpc.Modules;
+
+public class BoundedModulePool<T> : IRpcModulePool<T> where T : IRpcModule
 {
-    public class BoundedModulePool<T> : IRpcModulePool<T> where T : IRpcModule
+    private readonly int _timeout;
+    private readonly T _shared;
+    private readonly Task<T> _sharedAsTask;
+    private readonly ConcurrentQueue<T> _pool = new();
+    private readonly SemaphoreSlim _semaphore;
+    private int _rpcQueuedCalls = 0;
+    private readonly int _requestQueueLimit = 0;
+    private bool RequestLimitEnabled => _requestQueueLimit > 0;
+
+    public BoundedModulePool(IRpcModuleFactory<T> factory, int exclusiveCapacity, int timeout, int requestQueueLimit = 0)
     {
-        private readonly int _timeout;
-        private readonly T _shared;
-        private readonly Task<T> _sharedAsTask;
-        private readonly ConcurrentQueue<T> _pool = new();
-        private readonly SemaphoreSlim _semaphore;
-        private readonly ILogger _logger;
-        private int _rpcQueuedCalls = 0;
-        private readonly int _requestQueueLimit = 0;
-        private bool RequestLimitEnabled => _requestQueueLimit > 0;
+        _requestQueueLimit = requestQueueLimit;
+        _timeout = timeout;
+        Factory = factory;
 
-        public BoundedModulePool(IRpcModuleFactory<T> factory, int exclusiveCapacity, int timeout, ILogManager logManager, int requestQueueLimit = 0)
+        _semaphore = new SemaphoreSlim(exclusiveCapacity);
+        for (int i = 0; i < exclusiveCapacity; i++)
         {
-            _requestQueueLimit = requestQueueLimit;
-            _logger = logManager.GetClassLogger();
-            _timeout = timeout;
-            Factory = factory;
-
-            _semaphore = new SemaphoreSlim(exclusiveCapacity);
-            for (int i = 0; i < exclusiveCapacity; i++)
-            {
-                _pool.Enqueue(Factory.Create());
-            }
-
-            _shared = factory.Create();
-            _sharedAsTask = Task.FromResult(_shared);
+            _pool.Enqueue(Factory.Create());
         }
 
-        public Task<T> GetModule(bool canBeShared) => canBeShared ? _sharedAsTask : SlowPath();
-
-        private async Task<T> SlowPath()
-        {
-            if (RequestLimitEnabled && _rpcQueuedCalls > _requestQueueLimit)
-            {
-                throw new LimitExceededException($"Unable to start new queued requests for {typeof(T).Name}. Too many queued requests. Queued calls {_rpcQueuedCalls}.");
-            }
-
-            IncrementRpcQueuedCalls();
-            if (_logger.IsTrace)
-                _logger.Trace($"{typeof(T).Name} Queued RPC requests {_rpcQueuedCalls}");
-
-            if (!await _semaphore.WaitAsync(_timeout))
-            {
-                DecrementRpcQueuedCalls();
-                throw new ModuleRentalTimeoutException($"Unable to rent an instance of {typeof(T).Name}. Too many concurrent requests.");
-            }
-
-            DecrementRpcQueuedCalls();
-            _pool.TryDequeue(out T result);
-            return result;
-        }
-
-        private void IncrementRpcQueuedCalls()
-        {
-            if (RequestLimitEnabled)
-                Interlocked.Increment(ref _rpcQueuedCalls);
-        }
-
-        private void DecrementRpcQueuedCalls()
-        {
-            if (RequestLimitEnabled)
-                Interlocked.Decrement(ref _rpcQueuedCalls);
-        }
-
-        public void ReturnModule(T module)
-        {
-            if (ReferenceEquals(module, _shared))
-            {
-                return;
-            }
-
-            _pool.Enqueue(module);
-            _semaphore.Release();
-        }
-
-        public IRpcModuleFactory<T> Factory { get; }
+        _shared = factory.Create();
+        _sharedAsTask = Task.FromResult(_shared);
     }
+
+    public Task<T> GetModule(bool canBeShared) => canBeShared ? _sharedAsTask : SlowPath();
+
+    private async Task<T> SlowPath()
+    {
+        if (RequestLimitEnabled && _rpcQueuedCalls > _requestQueueLimit)
+        {
+            throw new LimitExceededException($"Unable to start new queued requests for {typeof(T).Name}. Too many queued requests. Queued calls {_rpcQueuedCalls}.");
+        }
+
+        IncrementRpcQueuedCalls();
+
+        if (!await _semaphore.WaitAsync(_timeout))
+        {
+            DecrementRpcQueuedCalls();
+            throw new ModuleRentalTimeoutException($"Unable to rent an instance of {typeof(T).Name}. Too many concurrent requests.");
+        }
+
+        DecrementRpcQueuedCalls();
+        _pool.TryDequeue(out T result);
+        return result;
+    }
+
+    private void IncrementRpcQueuedCalls()
+    {
+        if (RequestLimitEnabled)
+            Interlocked.Increment(ref _rpcQueuedCalls);
+    }
+
+    private void DecrementRpcQueuedCalls()
+    {
+        if (RequestLimitEnabled)
+            Interlocked.Decrement(ref _rpcQueuedCalls);
+    }
+
+    public void ReturnModule(T module)
+    {
+        if (ReferenceEquals(module, _shared))
+        {
+            return;
+        }
+
+        _pool.Enqueue(module);
+        _semaphore.Release();
+    }
+
+    public IRpcModuleFactory<T> Factory { get; }
 }
--- src/Nethermind/Nethermind.JsonRpc/Modules/IRpcModuleProviderExtensions.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/IRpcModuleProviderExtensions.cs
@@ -2,43 +2,39 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
-using Nethermind.Logging;
 
-namespace Nethermind.JsonRpc.Modules
+namespace Nethermind.JsonRpc.Modules;
+
+public static class IRpcModuleProviderExtensions
 {
-    public static class IRpcModuleProviderExtensions
+    private static readonly int _cpuCount = Environment.ProcessorCount;
+
+    public static void RegisterBounded<T>(
+        this IRpcModuleProvider rpcModuleProvider,
+        ModuleFactoryBase<T> factory,
+        int maxCount,
+        int timeout,
+        int requestQueueLimit = 0)
+        where T : IRpcModule
     {
-        private static readonly int _cpuCount = Environment.ProcessorCount;
+        rpcModuleProvider.Register(new BoundedModulePool<T>(factory, maxCount, timeout, requestQueueLimit));
+    }
 
-        public static void RegisterBounded<T>(
-            this IRpcModuleProvider rpcModuleProvider,
-            ModuleFactoryBase<T> factory,
-            int maxCount,
-            int timeout,
-            ILogManager logManager,
-            int requestQueueLimit = 0)
-            where T : IRpcModule
-        {
-            rpcModuleProvider.Register(new BoundedModulePool<T>(factory, maxCount, timeout, logManager, requestQueueLimit));
-        }
+    public static void RegisterBoundedByCpuCount<T>(
+        this IRpcModuleProvider rpcModuleProvider,
+        ModuleFactoryBase<T> factory,
+        int timeout)
+        where T : IRpcModule
+    {
+        RegisterBounded(rpcModuleProvider, factory, _cpuCount, timeout);
+    }
 
-        public static void RegisterBoundedByCpuCount<T>(
-            this IRpcModuleProvider rpcModuleProvider,
-            ModuleFactoryBase<T> factory,
-            int timeout,
-            ILogManager logManager)
-            where T : IRpcModule
-        {
-            RegisterBounded(rpcModuleProvider, factory, _cpuCount, timeout, logManager);
-        }
-
-        public static void RegisterSingle<T>(
-            this IRpcModuleProvider rpcModuleProvider,
-            T module,
-            bool allowExclusive = true)
-            where T : IRpcModule
-        {
-            rpcModuleProvider.Register(new SingletonModulePool<T>(module, allowExclusive));
-        }
+    public static void RegisterSingle<T>(
+        this IRpcModuleProvider rpcModuleProvider,
+        T module,
+        bool allowExclusive = true)
+        where T : IRpcModule
+    {
+        rpcModuleProvider.Register(new SingletonModulePool<T>(module, allowExclusive));
     }
 }
