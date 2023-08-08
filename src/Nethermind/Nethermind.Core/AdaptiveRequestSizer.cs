9f13361a9dd6e84e131217318a59c3456fcdc768
Amirul Ashraf <asdacap@gmail.com>
2023-08-08 18:41:10 +0800

    Change sizer

--- src/Nethermind/Nethermind.Core/AdaptiveRequestSizer.cs
+++ src/Nethermind/Nethermind.Core/AdaptiveRequestSizer.cs
@@ -3,21 +3,22 @@
 
 using System;
 using System.Diagnostics;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Nethermind.Core;
 
 /// <summary>
-/// Encapsulate the pattern of adjusting request size depending on the latency of the request.
-/// If the latency is lower that _lowerLatencyWatermark, the request size is increased,
-/// if the latency is higher than _upperLatencyWatermark, the request size is decreased.
+/// Encapsulate the pattern of adjusting request size depending on another metrics (usually the latency)
+/// If the metric is lower that _lowerLatencyWatermark, the request size is increased,
+/// if the metric is higher than _upperLatencyWatermark, the request size is decreased.
 /// </summary>
 public class AdaptiveRequestSizer
 {
     private readonly int _minRequestLimit;
     private readonly int _maxRequestLimit;
-    private readonly TimeSpan _lowerLatencyWatermark;
-    private readonly TimeSpan _upperLatencyWatermark;
+    private readonly int _lowerWatermark;
+    private readonly int _upperWatermark;
     private readonly double _adjustmentFactor;
 
     public int RequestSize { get; private set; }
@@ -25,36 +26,54 @@ public class AdaptiveRequestSizer
     public AdaptiveRequestSizer(
         int minRequestLimit,
         int maxRequestLimit,
-        TimeSpan lowerLatencyWatermark,
-        TimeSpan upperLatencyWatermark,
+        int lowerWatermark,
+        int upperWatermark,
         double adjustmentFactor = 2.0
     )
     {
         _maxRequestLimit = maxRequestLimit;
         _minRequestLimit = minRequestLimit;
-        _lowerLatencyWatermark = lowerLatencyWatermark;
-        _upperLatencyWatermark = upperLatencyWatermark;
+        _lowerWatermark = lowerWatermark;
+        _upperWatermark = upperWatermark;
         _adjustmentFactor = adjustmentFactor;
 
         RequestSize = _minRequestLimit;
     }
 
     /// <summary>
-    /// Adjust the RequestSize depending on the latency of the request and if the request failed.
+    /// Adjust the RequestSize depending on the latency of the request (in millis) and if the request failed.
     /// </summary>
     /// <param name="func"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
     public async Task<T> MeasureLatency<T>(Func<int, Task<T>> func)
     {
-        // Record bytes limit so that in case multiple concurrent request happens, we do not multiply the
+        return await Measure(async (requestSize) =>
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            T result = await func(requestSize);
+            return (result, (int)sw.ElapsedMilliseconds);
+        });
+    }
+
+    /// <summary>
+    /// Adjust the RequestSize depending on the metrics of the request and if the request failed.
+    /// </summary>
+    /// <param name="func"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public async Task<T> Measure<T>(Func<int, Task<(T, int)>> func)
+    {
+        // Record starting limit so that in case multiple concurrent request happens, we do not multiply the
         // limit on top of other adjustment, so only the last adjustment will stick, which is fine.
         int startingRequestSize = RequestSize;
         bool failed = false;
-        Stopwatch sw = Stopwatch.StartNew();
+        int metric = 0;
         try
         {
-            return await func(startingRequestSize);
+            (T response, int m) = await func(startingRequestSize);
+            metric = m;
+            return response;
         }
         catch (Exception)
         {
@@ -63,16 +82,15 @@ public class AdaptiveRequestSizer
         }
         finally
         {
-            sw.Stop();
             if (failed)
             {
                 RequestSize = _minRequestLimit;
             }
-            else if (sw.Elapsed < _lowerLatencyWatermark)
+            else if (metric < _lowerWatermark)
             {
                 RequestSize = Math.Min((int)(startingRequestSize * _adjustmentFactor), _maxRequestLimit);
             }
-            else if (sw.Elapsed > _upperLatencyWatermark && startingRequestSize > _minRequestLimit)
+            else if (metric > _upperWatermark && startingRequestSize > _minRequestLimit)
             {
                 RequestSize = (int)(startingRequestSize / _adjustmentFactor);
             }
