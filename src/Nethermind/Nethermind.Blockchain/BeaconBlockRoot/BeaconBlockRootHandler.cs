59073c3bb0bd21ef615266e254053a36dedc47c2
Alexey Osipov <me@flcl.me>
2023-08-08 18:35:47 +0300

    Remove balance set

--- src/Nethermind/Nethermind.Blockchain/BeaconBlockRoot/BeaconBlockRootHandler.cs
+++ src/Nethermind/Nethermind.Blockchain/BeaconBlockRoot/BeaconBlockRootHandler.cs
@@ -1,18 +1,13 @@
 // SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Nethermind.Core.Specs;
 using Nethermind.Core;
 using Nethermind.Evm.Precompiles.Stateful;
 using Nethermind.Int256;
 using Nethermind.State;
 using static Nethermind.Evm.Precompiles.Stateful.BeaconBlockRootPrecompile;
-
+using Nethermind.Core.Crypto;
 
 namespace Nethermind.Consensus.BeaconBlockRoot;
 public class BeaconBlockRootHandler : IBeaconBlockRootHandler
@@ -23,8 +18,7 @@ public class BeaconBlockRootHandler : IBeaconBlockRootHandler
 
         var timestamp = (UInt256)block.Timestamp;
         var parentBeaconBlockRoot = block.ParentBeaconBlockRoot;
-
-        stateProvider.CreateAccountIfNotExists(BeaconBlockRootPrecompile.Address, 1);
+        if (parentBeaconBlockRoot is null && block.Header.IsGenesis) parentBeaconBlockRoot = Keccak.Zero;
 
         UInt256.Mod(timestamp, HISTORICAL_ROOTS_LENGTH, out UInt256 timestampReduced);
         UInt256 rootIndex = timestampReduced + HISTORICAL_ROOTS_LENGTH;
