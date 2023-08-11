ec2d0f55ce6a3a454958c7025772c03aee202fb5
Amirul Ashraf <asdacap@gmail.com>
2023-08-12 01:30:53 +0800

    Missed a method

--- src/Nethermind/Nethermind.Synchronization/Synchronizer.cs
+++ src/Nethermind/Nethermind.Synchronization/Synchronizer.cs
@@ -349,37 +349,6 @@ namespace Nethermind.Synchronization
                     _receiptsFeed?.FeedTask ?? Task.CompletedTask));
         }
 
-        public void WatchForExit(
-            IProcessExitSource exitSource,
-            TimeSpan? exitConditionDuration = null
-        )
-        {
-            // Usually there are time where the mode changed to WaitingForBlock temporarily. So there need to be a small
-            // wait to make sure the sync more really is completed.
-            exitConditionDuration ??= TimeSpan.FromSeconds(5);
-
-            DateTime lastExitConditionTime = DateTime.MaxValue;
-            _syncMode.Changed += ((sender, args) =>
-            {
-                if (args.Current is SyncMode.WaitingForBlock or SyncMode.None)
-                {
-                    if (lastExitConditionTime == DateTime.MaxValue)
-                    {
-                        lastExitConditionTime = DateTime.Now;
-                    }
-                    else if (DateTime.Now - lastExitConditionTime > exitConditionDuration)
-                    {
-                        _logger.Info($"Sync finished. Exiting....");
-                        exitSource.Exit(0);
-                    }
-                }
-                else
-                {
-                    lastExitConditionTime = DateTime.MaxValue;
-                }
-            });
-        }
-
         public void Dispose()
         {
             CancellationTokenExtensions.CancelDisposeAndClear(ref _syncCancellation);
