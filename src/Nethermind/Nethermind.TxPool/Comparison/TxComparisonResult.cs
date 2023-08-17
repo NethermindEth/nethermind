ff01c02959cad02d930faa5d2f4ddeaef9d65a20
Marcin Sobczak <marcindsobczak@gmail.com>
2023-08-17 13:58:50 +0200

    fix whitespaces and file encodings

--- src/Nethermind/Nethermind.TxPool/Comparison/TxComparisonResult.cs
+++ src/Nethermind/Nethermind.TxPool/Comparison/TxComparisonResult.cs
@@ -1,9 +1,9 @@
-﻿// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
 namespace Nethermind.TxPool.Comparison;
 
-public readonly struct  TxComparisonResult
+public readonly struct TxComparisonResult
 {
     public const int NotDecided = 0;
     public const int KeepOld = 1;
