077f888619428bcab8ec00487ea44ce8398b8015
Lautaro Emanuel <emlautarom1@gmail.com>
2023-08-21 17:02:47 -0300

    Add new tests

--- src/Nethermind/Nethermind.Evm.Test/TransactionSubstateTests.cs
+++ src/Nethermind/Nethermind.Evm.Test/TransactionSubstateTests.cs
@@ -4,6 +4,7 @@
 using System;
 using FluentAssertions;
 using Nethermind.Core;
+using Nethermind.Core.Extensions;
 using NUnit.Framework;
 
 namespace Nethermind.Evm.Test
@@ -41,6 +42,70 @@ namespace Nethermind.Evm.Test
                 true,
                 true);
             transactionSubstate.Error.Should().Be("Reverted 0x0506070809");
+        }
+
+        [Test]
+        [Description($"Replace with {nameof(should_return_proper_revert_error_when_revert_custom_error)} once fixed")]
+        public void should_return_proper_revert_error_when_revert_custom_error_badly_implemented()
+        {
+            // See: https://github.com/NethermindEth/nethermind/issues/6024
+
+            string input =
+                "0x220266b600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000001741413231206469646e2774207061792070726566756e64000000000000000000";
+            byte[] data = Bytes.FromHexString(input);
+            ReadOnlyMemory<byte> readOnlyMemory = new(data);
+            TransactionSubstate transactionSubstate = new(
+                readOnlyMemory,
+                0,
+                new ArraySegment<Address>(),
+                new LogEntry[] { },
+                true,
+                true);
+            transactionSubstate.Error.Should().Be($"Reverted {input}");
+        }
+
+        [Test]
+        [Ignore("Badly implemented")]
+        public void should_return_proper_revert_error_when_revert_custom_error()
+        {
+            byte[] data = {
+                0x22, 0x02, 0x66, 0xb6, // Keccak of 'FailedOp(uint256,string) == 0x220266b6'
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x17,
+                0x41, 0x41, 0x32, 0x31, 0x20, 0x64, 0x69, 0x64, 0x6e, 0x27, 0x74, 0x20, 0x70, 0x61, 0x79, 0x20, 0x70, 0x72, 0x65, 0x66, 0x75, 0x6e, 0x64, // "AA21 didn't pay prefund"
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            ReadOnlyMemory<byte> readOnlyMemory = new(data);
+            TransactionSubstate transactionSubstate = new(
+                readOnlyMemory,
+                0,
+                new ArraySegment<Address>(),
+                new LogEntry[] { },
+                true,
+                true);
+            transactionSubstate.Error.Should().Be("Reverted 0x41413231206469646e2774207061792070726566756e64");
+        }
+
+        [Test]
+        [Ignore("Badly implemented in several clients (geth, erigon, Nethermind)")]
+        public void should_return_proper_revert_error_eip_140_test_case()
+        {
+            // See: https://eips.ethereum.org/EIPS/eip-140
+
+            byte[] data = {
+                0x6c, 0x72, 0x65, 0x76, 0x65, 0x72, 0x74, 0x65, 0x64, 0x20, 0x64, 0x61, 0x74, 0x61, // "lreverted data'
+                0x60, 0x00, 0x55, 0x7f,
+                0x72, 0x65, 0x76, 0x65, 0x72, 0x74, 0x20, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, // "revert message"
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x60, 0x00, 0x52, 0x60, 0x0e, 0x60, 0x00, 0xfd,
+            };
+            ReadOnlyMemory<byte> readOnlyMemory = new(data);
+            TransactionSubstate transactionSubstate = new(
+                readOnlyMemory,
+                0,
+                new ArraySegment<Address>(),
+                new LogEntry[] { },
+                true,
+                true);
+            transactionSubstate.Error.Should().Be("Reverted 0x726576657274206d657373616765");
         }
     }
 }
