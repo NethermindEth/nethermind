b899b3f6b531ef7b56f5f23a329ea607c34f5aba
Lautaro Emanuel <emlautarom1@gmail.com>
2023-09-04 17:26:16 -0300

    Add more tests
    - Restructure tests (IPC, WebSockets)

--- src/Nethermind/Nethermind.JsonRpc.Test/JsonRpcSocketClientTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/JsonRpcSocketClientTests.cs
@@ -4,11 +4,14 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Threading;
 using System.Threading.Tasks;
 using Nethermind.Evm.Tracing.GethStyle;
 using Nethermind.JsonRpc.Modules;
 using Nethermind.JsonRpc.Modules.DebugModule;
 using Nethermind.JsonRpc.WebSockets;
+using Nethermind.Logging;
 using Nethermind.Serialization.Json;
 using Nethermind.Sockets;
 using NUnit.Framework;
@@ -19,12 +22,9 @@ public class JsonRpcSocketClientTests
 {
     private static readonly object _bigObject = BuildRandomBigObject(100_000);
 
-    [Test]
-    public async Task Can_Handle_Very_Large_Objects()
+    class UsingIpc
     {
-        IPEndPoint ipEndPoint = IPEndPoint.Parse("127.0.0.1:1337");
-
-        Task<int> receiveSerialized = Task.Run(async () =>
+        private static async Task<T> MockServer<T>(IPEndPoint ipEndPoint, Func<Socket, Task<T>> func)
         {
             using Socket socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             socket.Bind(ipEndPoint);
@@ -32,46 +32,154 @@ public class JsonRpcSocketClientTests
 
             Socket handler = await socket.AcceptAsync();
 
-            byte[] buffer = new byte[1024];
-            int totalRead = 0;
+            return await func(handler);
+        }
 
-            int read;
-            while ((read = await handler.ReceiveAsync(buffer)) != 0)
-            {
-                totalRead += read;
-            }
-            return totalRead;
-        });
-
-        Task<int> sendJsonRpcResult = Task.Run(async () =>
+        [Test]
+        public async Task Can_handle_very_large_objects()
         {
-            using Socket socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            await socket.ConnectAsync(ipEndPoint);
+            IPEndPoint ipEndPoint = IPEndPoint.Parse("127.0.0.1:1337");
 
-            ISocketHandler handler = new IpcSocketsHandler(socket);
-            JsonRpcSocketsClient client = new JsonRpcSocketsClient(
-                clientName: "TestClient",
-                handler: handler,
-                endpointType: RpcEndpoint.IPC,
-                jsonRpcProcessor: null!,
-                jsonRpcService: null!,
-                jsonRpcLocalStats: null!,
-                jsonSerializer: new EthereumJsonSerializer(converters: new GethLikeTxTraceConverter())
-            );
-            JsonRpcResult result = JsonRpcResult.Single(
-                new JsonRpcSuccessResponse()
+            Task<int> receiveSerialized = MockServer(ipEndPoint, async socket =>
+            {
+                byte[] buffer = new byte[1024];
+                int totalRead = 0;
+
+                int read;
+                while ((read = await socket.ReceiveAsync(buffer)) != 0)
                 {
-                    MethodName = nameof(Can_Handle_Very_Large_Objects), Id = "42", Result = _bigObject
-                }, default);
+                    totalRead += read;
+                }
+                return totalRead;
+            });
 
-            return await client.SendJsonRpcResult(result);
-        });
+            Task<int> sendJsonRpcResult = Task.Run(async () =>
+            {
+                using Socket socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                await socket.ConnectAsync(ipEndPoint);
 
-        await Task.WhenAll(sendJsonRpcResult, receiveSerialized);
-        int sent = sendJsonRpcResult.Result;
-        int received = receiveSerialized.Result;
-        Console.WriteLine($"Sent: ${sent}, Received: ${received}");
-        Assert.That(sent, Is.EqualTo(received));
+                ISocketHandler handler = new IpcSocketsHandler(socket);
+                JsonRpcSocketsClient client = new JsonRpcSocketsClient(
+                    clientName: "TestClient",
+                    handler: handler,
+                    endpointType: RpcEndpoint.IPC,
+                    jsonRpcProcessor: null!,
+                    jsonRpcService: null!,
+                    jsonRpcLocalStats: null!,
+                    jsonSerializer: new EthereumJsonSerializer(converters: new GethLikeTxTraceConverter())
+                );
+                JsonRpcResult result = JsonRpcResult.Single(
+                    new JsonRpcSuccessResponse()
+                    {
+                        MethodName = "mock", Id = "42", Result = _bigObject
+                    }, default);
+
+                return await client.SendJsonRpcResult(result);
+            });
+
+            await Task.WhenAll(sendJsonRpcResult, receiveSerialized);
+            int sent = sendJsonRpcResult.Result;
+            int received = receiveSerialized.Result;
+            Assert.That(sent, Is.EqualTo(received));
+        }
+
+        [Test]
+        [TestCase(2)]
+        [TestCase(5)]
+        public async Task Can_send_multiple_messages(int messageCount)
+        {
+            IPEndPoint ipEndPoint = IPEndPoint.Parse("127.0.0.1:1337");
+
+            Task<int> receiveSerialized = MockServer(ipEndPoint, async socket =>
+            {
+                byte[] buffer = new byte[1024];
+                int totalRead = 0;
+
+                int read;
+                while ((read = await socket.ReceiveAsync(buffer)) != 0)
+                {
+                    totalRead += read;
+                }
+                return totalRead;
+            });
+
+            Task<int> sendJsonRpcResult = Task.Run(async () =>
+            {
+                using Socket socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                await socket.ConnectAsync(ipEndPoint);
+
+                ISocketHandler handler = new IpcSocketsHandler(socket);
+                JsonRpcSocketsClient client = new JsonRpcSocketsClient(
+                    clientName: "TestClient",
+                    handler: handler,
+                    endpointType: RpcEndpoint.IPC,
+                    jsonRpcProcessor: null!,
+                    jsonRpcService: null!,
+                    jsonRpcLocalStats: null!,
+                    jsonSerializer: new EthereumJsonSerializer(converters: new GethLikeTxTraceConverter())
+                );
+                JsonRpcResult result = JsonRpcResult.Single(
+                    new JsonRpcSuccessResponse()
+                    {
+                        MethodName = "mock", Id = "42",
+                    }, default);
+
+                int totalBytesSent = 0;
+                for (int i = 0; i < messageCount; i++)
+                {
+                    totalBytesSent += await client.SendJsonRpcResult(result);
+                }
+
+                return totalBytesSent;
+            });
+
+            await Task.WhenAll(sendJsonRpcResult, receiveSerialized);
+            int sent = sendJsonRpcResult.Result;
+            int received = receiveSerialized.Result;
+            Assert.That(sent, Is.EqualTo(received));
+        }
+    }
+
+    class UsingWebSockets
+    {
+        [Test]
+        [TestCase(2)]
+        [TestCase(10)]
+        public async Task Can_send_multiple_messages(int messageCount)
+        {
+            Task<int> sendMessages = Task.Run(async () =>
+            {
+                using ClientWebSocket socket = new ClientWebSocket();
+                await socket.ConnectAsync(new Uri("ws://localhost:1337/"), CancellationToken.None);
+
+                using ISocketHandler handler = new WebSocketHandler(socket, NullLogManager.Instance);
+                using JsonRpcSocketsClient client = new JsonRpcSocketsClient(
+                    clientName: "TestClient",
+                    handler: handler,
+                    endpointType: RpcEndpoint.Ws,
+                    jsonRpcProcessor: null!,
+                    jsonRpcService: null!,
+                    jsonRpcLocalStats: null!,
+                    jsonSerializer: new EthereumJsonSerializer()
+                );
+                JsonRpcResult result = JsonRpcResult.Single(
+                    new JsonRpcSuccessResponse
+                    {
+                        MethodName = "mock", Id = "42", Result = BuildRandomBigObject(1000)
+                    }, default);
+
+                for (int i = 0; i < messageCount; i++)
+                {
+                    await client.SendJsonRpcResult(result);
+                }
+
+                return messageCount;
+            });
+
+            await Task.WhenAll(sendMessages);
+            int sent = sendMessages.Result;
+            Assert.That(sent, Is.EqualTo(messageCount));
+        }
     }
 
     private static object BuildRandomBigObject(int length)
