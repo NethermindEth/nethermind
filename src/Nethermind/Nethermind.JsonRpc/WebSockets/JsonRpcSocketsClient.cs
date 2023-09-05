fd906be08f87918e850408ac7f0b1fba13298786
Lautaro Emanuel <emlautarom1@gmail.com>
2023-09-05 13:20:00 -0300

    Rollback

--- src/Nethermind/Nethermind.JsonRpc/WebSockets/JsonRpcSocketsClient.cs
+++ src/Nethermind/Nethermind.JsonRpc/WebSockets/JsonRpcSocketsClient.cs
@@ -160,32 +160,40 @@ namespace Nethermind.JsonRpc.WebSockets
 
         private async Task<int> SendJsonRpcResultEntry(JsonRpcResult.Entry result, bool endOfMessage = true)
         {
-            using JsonRpcResult.Entry entry = result;
-
-            await using Stream stream = _handler.SendUsingStream();
-            await using Stream buffered = new BufferedStream(stream);
-            await using CounterStream resultData = new CounterStream(buffered);
-
-            try
+            void SerializeTimeoutException(MemoryStream stream, JsonRpcResult.Entry result)
             {
-                _jsonSerializer.SerializeWaitForEnumeration(resultData, result.Response);
-            }
-            catch (Exception e) when (e is OperationCanceledException || e.InnerException is OperationCanceledException)
-            {
-                _jsonSerializer.Serialize(
-                    resultData,
-                    _jsonRpcService.GetErrorResponse(
-                        ErrorCodes.Timeout,
-                        "Request was canceled due to enabled timeout.",
-                        result.Response.Id,
-                        result.Response.MethodName
-                    )
-                );
+                _jsonSerializer.Serialize(stream, _jsonRpcService.GetErrorResponse(
+                    ErrorCodes.Timeout,
+                    "Request was canceled due to enabled timeout.",
+                    result.Response.Id,
+                    result.Response.MethodName));
             }
 
-            // ? What if we write more than int.MaxValue.
-            // Result could be negative
-            return (int)resultData.WrittenBytes;
+            using (result)
+            {
+                using MemoryStream resultData = new();
+
+                try
+                {
+                    _jsonSerializer.SerializeWaitForEnumeration(resultData, result.Response);
+                }
+                catch (Exception e) when (e.InnerException is OperationCanceledException)
+                {
+                    SerializeTimeoutException(resultData, result);
+                }
+                catch (OperationCanceledException)
+                {
+                    SerializeTimeoutException(resultData, result);
+                }
+
+                if (resultData.TryGetBuffer(out ArraySegment<byte> data))
+                {
+                    await _handler.SendRawAsync(data, endOfMessage);
+                    return data.Count;
+                }
+
+                return (int)resultData.Length;
+            }
         }
     }
 }
