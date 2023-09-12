4898350a0e336da174573382a976942b087c0636
Lautaro Emanuel <emlautarom1@gmail.com>
2023-09-12 16:11:20 -0300

    Add 'by_ref' tests

--- src/Nethermind/Nethermind.Blockchain.Test/Filters/AddressFilterTests.cs
+++ src/Nethermind/Nethermind.Blockchain.Test/Filters/AddressFilterTests.cs
@@ -26,11 +26,29 @@ public class AddressFilterTests
     }
 
     [Test]
+    public void Accepts_a_specific_address_by_ref()
+    {
+        AddressFilter filter = new AddressFilter(_address1);
+
+        AddressStructRef @ref = _address1.ToStructRef();
+        filter.Accepts(ref @ref).Should().BeTrue();
+    }
+
+    [Test]
     public void Rejects_different_address()
     {
         AddressFilter filter = new AddressFilter(_address1);
 
         filter.Accepts(_address2).Should().BeFalse();
+    }
+
+    [Test]
+    public void Rejects_different_address_by_ref()
+    {
+        AddressFilter filter = new AddressFilter(_address1);
+
+        AddressStructRef address2Ref = _address2.ToStructRef();
+        filter.Accepts(ref address2Ref).Should().BeFalse();
     }
 
     [Test]
@@ -41,6 +59,19 @@ public class AddressFilterTests
         filter.Accepts(_address1).Should().BeTrue();
         filter.Accepts(_address2).Should().BeTrue();
         filter.Accepts(_address3).Should().BeTrue();
+    }
+
+    [Test]
+    public void Accepts_any_address_when_set_is_null_by_ref()
+    {
+        AddressFilter filter = new AddressFilter(addresses: null!);
+
+        AddressStructRef address1Ref = _address1.ToStructRef();
+        AddressStructRef address2Ref = _address2.ToStructRef();
+        AddressStructRef address3Ref = _address3.ToStructRef();
+        filter.Accepts(ref address1Ref).Should().BeTrue();
+        filter.Accepts(ref address2Ref).Should().BeTrue();
+        filter.Accepts(ref address3Ref).Should().BeTrue();
     }
 
     [Test]
@@ -55,6 +86,20 @@ public class AddressFilterTests
     }
 
     [Test]
+    public void Accepts_any_address_when_set_is_empty_by_ref()
+    {
+        HashSet<Address> addresses = new();
+        AddressFilter filter = new AddressFilter(addresses);
+
+        AddressStructRef address1Ref = _address1.ToStructRef();
+        AddressStructRef address2Ref = _address2.ToStructRef();
+        AddressStructRef address3Ref = _address3.ToStructRef();
+        filter.Accepts(ref address1Ref).Should().BeTrue();
+        filter.Accepts(ref address2Ref).Should().BeTrue();
+        filter.Accepts(ref address3Ref).Should().BeTrue();
+    }
+
+    [Test]
     public void Accepts_only_addresses_in_a_set()
     {
         HashSet<Address> addresses = new()
@@ -66,5 +111,22 @@ public class AddressFilterTests
         filter.Accepts(_address1).Should().BeTrue();
         filter.Accepts(_address2).Should().BeFalse();
         filter.Accepts(_address3).Should().BeTrue();
+    }
+
+    [Test]
+    public void Accepts_only_addresses_in_a_set_by_ref()
+    {
+        HashSet<Address> addresses = new()
+        {
+            _address1, _address3
+        };
+        AddressFilter filter = new AddressFilter(addresses);
+
+        AddressStructRef address1Ref = _address1.ToStructRef();
+        AddressStructRef address2Ref = _address2.ToStructRef();
+        AddressStructRef address3Ref = _address3.ToStructRef();
+        filter.Accepts(ref address1Ref).Should().BeTrue();
+        filter.Accepts(ref address2Ref).Should().BeFalse();
+        filter.Accepts(ref address3Ref).Should().BeTrue();
     }
 }
