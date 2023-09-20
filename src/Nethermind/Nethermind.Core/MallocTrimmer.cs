f003cd6b21e789f85e53d629c6c410f77b015e77
Amirul Ashraf <asdacap@gmail.com>
2023-09-20 16:22:26 +0800

    Comment

--- src/Nethermind/Nethermind.Core/MallocTrimmer.cs
+++ src/Nethermind/Nethermind.Core/MallocTrimmer.cs
@@ -33,7 +33,14 @@ public class MallocTrimmer
                 await Task.Delay(_delay, cancellationToken);
 
                 // Unlike a GC, this seems to be single threaded, going through each arena one by one.
-                // It does however, lock the arena.
+                // It does however, lock the arena. On 32 thread machine, 256 arena, this take between 300ms to 1 second
+                // in total, so about 1 to 4 ms for each arena, which is the possible hang when allocating via malloc.
+                // This does not apply to managed code though, so this is largely the concern of rocksdb when reading
+                // from uncached block.
+                //
+                // Unlike the standard free(), in addition to always clearing fastbin and consolidating chunks, this
+                // also MADV_DONTNEED large enough free section of the heap. This also means private/virtual memory
+                // does not go down, but RSS and GC load does.
                 long startTime = Stopwatch.GetTimestamp();
                 if (_logger.IsDebug) _logger.Debug("Trimming malloc heaps");
                 bool wasReleased = _mallocHelper.MallocTrim((uint)1.MiB());
