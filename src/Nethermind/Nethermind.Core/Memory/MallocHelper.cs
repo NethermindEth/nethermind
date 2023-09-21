0e4be2fa19eeddb9265c611118f3e508935f6a33
Amirul Ashraf <asdacap@gmail.com>
2023-09-21 15:12:22 +0800

    Perf/set malloc ops (#6110)
    * Set malloc options
    
    * Use a different setting
    
    * Adjust config
    
    * Split malloc calls to a separate file
    
    * More unit test and minor cleanup
    
    * Whitespace
    
    * Check if linux

---
+++ src/Nethermind/Nethermind.Core/Memory/MallocHelper.cs
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Nethermind.Core.Memory;
+
+/// <summary>
+/// Wrapper around malloc apis
+/// </summary>
+public class MallocHelper
+{
+    [DllImport("libc")]
+    private static extern int mallopt(int opts, int value);
+
+    private static MallocHelper? _instance;
+    public static MallocHelper Instance => _instance ??= new MallocHelper();
+
+    public bool MallOpt(Option option, int value)
+    {
+        // Windows can't find libc and osx does not have the method for some reason
+        // FreeBSD uses jemalloc by default anyway....
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return true;
+
+        return mallopt((int)option, value) == 1;
+    }
+
+    public enum Option : int
+    {
+        M_MMAP_THRESHOLD = -3
+    }
+}
