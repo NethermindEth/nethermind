12bfa821818a20bc74a17946728e61633d03ae77
MarekM25 <marekm2504@gmail.com>
2023-09-27 11:02:22 +0200

    Update address

--- src/Nethermind/Nethermind.Blockchain/BeaconBlockRoot/BeaconBlockRootHandler.cs
+++ src/Nethermind/Nethermind.Blockchain/BeaconBlockRoot/BeaconBlockRootHandler.cs
@@ -17,7 +17,7 @@ namespace Nethermind.Blockchain.BeaconBlockRoot;
 public class BeaconBlockRootHandler : IBeaconBlockRootHandler
 {
     private readonly ITransactionProcessor _processor;
-    private static Address Default4788Address = new Address("0xbEAC020008aFF7331c0A389CB2AAb67597567d7a");
+    private static Address Default4788Address = new Address("0x000f3df6d732807ef1319fb7b8bb8522d0beac02");
     private readonly ILogger _logger;
     private const long GasLimit = 30_000_000L;
     public BeaconBlockRootHandler(
