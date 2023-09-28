0d262a2dd1c6730b8fd46119055dcef654f03ec6
Lautaro Emanuel <emlautarom1@gmail.com>
2023-09-28 16:25:39 -0300

    Add more `AccessList` tests

--- src/Nethermind/Nethermind.Core.Test/Eip2930/AccessListTests.cs
+++ src/Nethermind/Nethermind.Core.Test/Eip2930/AccessListTests.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using System.Collections.Generic;
 using FluentAssertions;
 using Nethermind.Core.Eip2930;
@@ -106,5 +107,90 @@ public class AccessListTests
         };
 
         accessList.AsDictionary().Should().BeEquivalentTo(expected);
+    }
+
+    [Test]
+    public void Multiple_addresses_no_storage()
+    {
+        Address address1 = TestItem.AddressA;
+        Address address2 = TestItem.AddressB;
+
+        AccessList accessList = new AccessList.Builder()
+            .AddAddress(address1)
+            .AddAddress(address2)
+            .Build();
+
+        IReadOnlyDictionary<Address, IReadOnlySet<UInt256>> expected = new Dictionary<Address, IReadOnlySet<UInt256>>
+        {
+            { address1, new HashSet<UInt256>() },
+            { address2, new HashSet<UInt256>() }
+        };
+
+        accessList.AsDictionary().Should().BeEquivalentTo(expected);
+    }
+
+    [Test]
+    public void Multiple_addresses_with_storage()
+    {
+        Address address1 = TestItem.AddressA;
+        Address address2 = TestItem.AddressB;
+        UInt256 storageKey1 = (UInt256)1;
+        UInt256 storageKey2 = (UInt256)2;
+        UInt256 storageKey3 = (UInt256)3;
+
+        AccessList accessList = new AccessList.Builder()
+            .AddAddress(address1)
+            .AddStorage(storageKey1)
+            .AddStorage(storageKey2)
+            .AddAddress(address2)
+            .AddStorage(storageKey3)
+            .Build();
+
+        IReadOnlyDictionary<Address, IReadOnlySet<UInt256>> expected = new Dictionary<Address, IReadOnlySet<UInt256>>
+        {
+            { address1, new HashSet<UInt256> { storageKey1, storageKey2} },
+            { address2, new HashSet<UInt256> { storageKey3 } }
+        };
+
+        accessList.AsDictionary().Should().BeEquivalentTo(expected);
+    }
+
+    [Test]
+    public void Multiple_duplicated_addresses_with_storage()
+    {
+        Address address1 = TestItem.AddressA;
+        Address address2 = TestItem.AddressB;
+        UInt256 storageKey1 = (UInt256)1;
+        UInt256 storageKey2 = (UInt256)2;
+        UInt256 storageKey3 = (UInt256)3;
+
+        AccessList accessList = new AccessList.Builder()
+            .AddAddress(address1)
+            .AddStorage(storageKey1)
+            .AddStorage(storageKey2)
+            .AddAddress(address2)
+            .AddStorage(storageKey3)
+            .AddAddress(address1)
+            .AddStorage(storageKey1)
+            .AddAddress(address2)
+            .Build();
+
+        IReadOnlyDictionary<Address, IReadOnlySet<UInt256>> expected = new Dictionary<Address, IReadOnlySet<UInt256>>
+        {
+            { address1, new HashSet<UInt256> { storageKey1, storageKey2} },
+            { address2, new HashSet<UInt256> { storageKey3 } }
+        };
+
+        accessList.AsDictionary().Should().BeEquivalentTo(expected);
+    }
+
+    [Test]
+    public void Invalid_storage_when_no_previous_address()
+    {
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            AccessList.Builder builder = new AccessList.Builder()
+                .AddStorage(UInt256.Zero);
+        });
     }
 }
