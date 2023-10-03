ba6bf75c4a4651b3907c1b3c68f87cd24024a8e9
ak88 <anders.holmbjerg@hotmail.com>
2023-10-03 19:37:53 +0200

    removed break transaction

--- src/Nethermind/Nethermind.Serialization.Rlp/TxDecoder.cs
+++ src/Nethermind/Nethermind.Serialization.Rlp/TxDecoder.cs
@@ -314,16 +314,7 @@ namespace Nethermind.Serialization.Rlp
             stream.Encode(item.Value);
             stream.Encode(item.Data);
             _accessListDecoder.Encode(stream, item.AccessList, rlpBehaviors);
-            if(item.MaxFeePerDataGas.Value == 42_000_000_000)
-            {
-                byte[] brokenDataGas = new byte[33];
-                Array.Fill(brokenDataGas, (byte)0xff);
-                stream.Encode(brokenDataGas);
-            }
-            else
-            {
-                stream.Encode(item.MaxFeePerDataGas.Value);
-            }
+            stream.Encode(item.MaxFeePerDataGas.Value);
             stream.Encode(item.BlobVersionedHashes);
         }
 
