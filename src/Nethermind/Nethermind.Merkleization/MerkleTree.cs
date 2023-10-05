a634e07257ade754e0ae56394c9a42dbb36fdfb6
Nikita Mescheryakov <root@nikitam.io>
2023-10-05 21:14:05 +0300

    Revert "Add root"
    This reverts commit 11279604c76d0ca0bd8ea114377be4971a82a7bb.

--- src/Nethermind/Nethermind.Merkleization/MerkleTree.cs
+++ src/Nethermind/Nethermind.Merkleization/MerkleTree.cs
@@ -144,7 +144,6 @@ public abstract class MerkleTree : IMerkleList
 
     public MerkleTree(IKeyValueStore<ulong, byte[]> keyValueStore)
     {
-        Root = new Root();
         _keyValueStore = keyValueStore ?? throw new ArgumentNullException(nameof(keyValueStore));
 
         byte[]? countBytes = _keyValueStore[_countKey];
