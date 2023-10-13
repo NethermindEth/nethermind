f993be1331f01449f55e23ef7a93fe124cc6f547
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-10-13 13:21:54 +0200

    Add keccak for computing

---
+++ src/Nethermind/Nethermind.Core/Crypto/Keccak.cs
@@ -0,0 +1,184 @@
+// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Nethermind.Core.Crypto
+{
+    [DebuggerStepThrough]
+    [DebuggerDisplay("{ToString()}")]
+    public static class ValueKeccak
+    {
+        /// <returns>
+        ///     <string>0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470</string>
+        /// </returns>
+        public static readonly ValueCommitment OfAnEmptyString = InternalCompute(new byte[] { });
+
+        /// <returns>
+        ///     <string>0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347</string>
+        /// </returns>
+        public static readonly ValueCommitment OfAnEmptySequenceRlp = InternalCompute(new byte[] { 192 });
+
+        /// <summary>
+        ///     0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421
+        /// </summary>
+        public static readonly ValueCommitment EmptyTreeHash = InternalCompute(new byte[] { 128 });
+
+        /// <returns>
+        ///     <string>0x0000000000000000000000000000000000000000000000000000000000000000</string>
+        /// </returns>
+        public static ValueCommitment Zero { get; } = default;
+
+        /// <summary>
+        ///     <string>0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</string>
+        /// </summary>
+        public static ValueCommitment MaxValue { get; } = new("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+
+        [DebuggerStepThrough]
+        public static ValueCommitment Compute(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return OfAnEmptyString;
+            }
+
+            return InternalCompute(System.Text.Encoding.UTF8.GetBytes(input));
+        }
+
+        [DebuggerStepThrough]
+        public static ValueCommitment Compute(ReadOnlySpan<byte> input)
+        {
+            if (input.Length == 0)
+            {
+                return OfAnEmptyString;
+            }
+
+            Unsafe.SkipInit(out ValueCommitment keccak);
+            KeccakHash.ComputeHashBytesToSpan(input, MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref keccak, 1)));
+            return keccak;
+        }
+
+        internal static ValueCommitment InternalCompute(byte[] input)
+        {
+            Unsafe.SkipInit(out ValueCommitment keccak);
+            KeccakHash.ComputeHashBytesToSpan(input, MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref keccak, 1)));
+            return keccak;
+        }
+    }
+
+    [DebuggerStepThrough]
+    public class Keccak
+    {
+        public const int Size = 32;
+
+        public const int MemorySize =
+            MemorySizes.SmallObjectOverhead -
+            MemorySizes.RefSize +
+            Size;
+
+        /// <returns>
+        ///     <string>0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470</string>
+        /// </returns>
+        public static readonly Commitment OfAnEmptyString = new Commitment(ValueKeccak.InternalCompute(new byte[] { }));
+
+        /// <returns>
+        ///     <string>0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347</string>
+        /// </returns>
+        public static readonly Commitment OfAnEmptySequenceRlp = new Commitment(ValueKeccak.InternalCompute(new byte[] { 192 }));
+
+        /// <summary>
+        ///     0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421
+        /// </summary>
+        public static Commitment EmptyTreeHash = new Commitment(ValueKeccak.InternalCompute(new byte[] { 128 }));
+
+        /// <returns>
+        ///     <string>0x0000000000000000000000000000000000000000000000000000000000000000</string>
+        /// </returns>
+        public static Commitment Zero { get; } = new(new byte[Size]);
+
+        /// <summary>
+        ///     <string>0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</string>
+        /// </summary>
+        public static Commitment MaxValue { get; } = new("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+
+
+        [DebuggerStepThrough]
+        public static Commitment Compute(byte[]? input)
+        {
+            if (input is null || input.Length == 0)
+            {
+                return OfAnEmptyString;
+            }
+
+            return new Commitment(KeccakHash.ComputeHashBytes(input));
+        }
+
+        [DebuggerStepThrough]
+        public static Commitment Compute(ReadOnlySpan<byte> input)
+        {
+            return new Commitment(ValueKeccak.Compute(input));
+        }
+
+        [DebuggerStepThrough]
+        public static Commitment Compute(string input)
+        {
+            return new Commitment(ValueKeccak.Compute(input));
+        }
+    }
+
+    public ref struct KeccakStructRef
+    {
+        public const int Size = 32;
+
+        public int MemorySize => MemorySizes.ArrayOverhead + Size;
+
+        [DebuggerStepThrough]
+        public static CommitmentStructRef Compute(byte[]? input)
+        {
+            if (input is null || input.Length == 0)
+            {
+                return new CommitmentStructRef(Keccak.OfAnEmptyString.Bytes);
+            }
+
+            var result = new CommitmentStructRef();
+            KeccakHash.ComputeHashBytesToSpan(input, result.Bytes);
+            return result;
+        }
+
+        [DebuggerStepThrough]
+        public static CommitmentStructRef Compute(Span<byte> input)
+        {
+            if (input.Length == 0)
+            {
+                return new CommitmentStructRef(Keccak.OfAnEmptyString.Bytes);
+            }
+
+            var result = new CommitmentStructRef();
+            KeccakHash.ComputeHashBytesToSpan(input, result.Bytes);
+            return result;
+        }
+
+        private static CommitmentStructRef InternalCompute(Span<byte> input)
+        {
+            var result = new CommitmentStructRef();
+            KeccakHash.ComputeHashBytesToSpan(input, result.Bytes);
+            return result;
+        }
+
+        [DebuggerStepThrough]
+        public static CommitmentStructRef Compute(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return new CommitmentStructRef(Keccak.OfAnEmptyString.Bytes);
+            }
+
+            var result = new CommitmentStructRef();
+            KeccakHash.ComputeHashBytesToSpan(System.Text.Encoding.UTF8.GetBytes(input), result.Bytes);
+            return result;
+        }
+    }
+}
