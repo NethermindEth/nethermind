064f0d6038188ccf577adb4bc2c01c5bd5470788
benaadams <thundercat@illyriad.co.uk>
2023-10-24 15:25:42 +0100

    Fix trace entry

--- src/Nethermind/Nethermind.Evm/Tracing/GethStyle/GethTxTraceEntry.cs
+++ src/Nethermind/Nethermind.Evm/Tracing/GethStyle/GethTxTraceEntry.cs
@@ -17,27 +17,27 @@ public class GethTxTraceEntry
     {
     }
 
-    public int Depth { get; set; }
-
-    public string? Error { get; set; }
-
-    [JsonConverter(typeof(LongRawJsonConverter))]
-    public long Gas { get; set; }
-
-    public long GasCost { get; set; }
-
-    public IList<string>? Memory { get; set; }
-
-    [JsonPropertyName("op")]
-    public string? Opcode { get; set; }
-
     [JsonPropertyName("pc")]
     [JsonConverter(typeof(LongRawJsonConverter))]
     public long ProgramCounter { get; set; }
 
+    [JsonPropertyName("op")]
+    public string? Opcode { get; set; }
+
+    [JsonConverter(typeof(LongRawJsonConverter))]
+    public long Gas { get; set; }
+
+    [JsonConverter(typeof(LongRawJsonConverter))]
+    public long GasCost { get; set; }
+
+    public int Depth { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    public string? Error { get; set; }
 
     public IList<string>? Stack { get; set; }
 
+    public IList<string>? Memory { get; set; }
 
     public Dictionary<string, string>? Storage { get; set; }
 
