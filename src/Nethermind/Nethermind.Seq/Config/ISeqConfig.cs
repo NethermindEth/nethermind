3fc97b12a33e52a6e9d76c96cf1041bacfb2f7d6
Ruben Buniatyan <rubo@users.noreply.github.com>
2023-11-08 22:41:31 +0100

    Replace doc generator (#6252)

--- src/Nethermind/Nethermind.Seq/Config/ISeqConfig.cs
+++ src/Nethermind/Nethermind.Seq/Config/ISeqConfig.cs
@@ -3,18 +3,17 @@
 
 using Nethermind.Config;
 
-namespace Nethermind.Seq.Config
+namespace Nethermind.Seq.Config;
+
+[ConfigCategory(Description = "Configuration of the Prometheus + Grafana metrics publication. Documentation of the required setup is not yet ready (but the metrics do work and are used by the dev team)")]
+public interface ISeqConfig : IConfig
 {
-    [ConfigCategory(Description = "Configuration of the Prometheus + Grafana metrics publication. Documentation of the required setup is not yet ready (but the metrics do work and are used by the dev team)")]
-    public interface ISeqConfig : IConfig
-    {
-        [ConfigItem(Description = "Minimal level of log events which will be sent to Seq instance.", DefaultValue = "Off")]
-        string MinLevel { get; }
+    [ConfigItem(Description = "The min log level to sent to Seq.", DefaultValue = "Off")]
+    string MinLevel { get; }
 
-        [ConfigItem(Description = "Seq instance URL.", DefaultValue = "\"http://localhost:5341")]
-        string ServerUrl { get; }
+    [ConfigItem(Description = "The Seq instance URL.", DefaultValue = "http://localhost:5341")]
+    string ServerUrl { get; }
 
-        [ConfigItem(Description = "API key used for log events ingestion to Seq instance", DefaultValue = "")]
-        string ApiKey { get; }
-    }
+    [ConfigItem(Description = "The Seq API key.", DefaultValue = "")]
+    string ApiKey { get; }
 }
