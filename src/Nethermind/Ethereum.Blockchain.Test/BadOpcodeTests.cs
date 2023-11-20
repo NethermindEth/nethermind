117fc11853bddfc3ff43fa70e8c7905e954aa5e2
Lautaro Emanuel <emlautarom1@gmail.com>
2023-11-20 16:26:36 -0300

    Formatting

--- src/Nethermind/Ethereum.Blockchain.Test/BadOpcodeTests.cs
+++ src/Nethermind/Ethereum.Blockchain.Test/BadOpcodeTests.cs
@@ -13,11 +13,11 @@ namespace Ethereum.Blockchain.Test
     public class BadOpcodeTests : GeneralStateTestBase
     {
         // ToDo: investigate the reason, more likely test setup issue
-         private static List<string> ignoredTests = new List<string>()
-         {
-             "badOpcodes_d25g0v0_",
-             "undefinedOpcodeFirstByte_d0g0v0_"
-         };
+        private static List<string> ignoredTests = new List<string>()
+        {
+            "badOpcodes_d25g0v0_",
+            "undefinedOpcodeFirstByte_d0g0v0_"
+        };
 
         [TestCaseSource(nameof(LoadTests))]
         [Retry(3)]
@@ -29,7 +29,7 @@ namespace Ethereum.Blockchain.Test
         public static IEnumerable<GeneralStateTest> LoadTests()
         {
             var loader = new TestsSourceLoader(new LoadGeneralStateTestsStrategy(), "stBadOpcode");
-            return ((IEnumerable<GeneralStateTest>)loader.LoadTests()).Where(x=> !ignoredTests.Contains(x.Name));
+            return ((IEnumerable<GeneralStateTest>)loader.LoadTests()).Where(x => !ignoredTests.Contains(x.Name));
         }
     }
 }
