eaafdb37278567aa8d6fbd2837efcec4ab8e919e
lukasz.rozmej <lukasz.rozmej@gmail.com>
2023-11-28 10:23:48 +0100

    Change license headers on tracer and link to go-ethereum

--- src/Nethermind/Nethermind.Evm/Data/JSTracers/bigramTracer.js
+++ src/Nethermind/Nethermind.Evm/Data/JSTracers/bigramTracer.js
@@ -1,18 +1,7 @@
-// Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+// Tracer originally developed by go-ethereum: https://github.com/ethereum/go-ethereum/blob/58297e339b26d09a0c21e550ee4b6ed6205cedcd/eth/tracers/js/internal/tracers/bigram_tracer.js
 
 {
     // hist is the counters of opcode bigrams
