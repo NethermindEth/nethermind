732490d2a37a6c4c087bd1462df8c8044a85854d
benaadams <thundercat@illyriad.co.uk>
2023-12-08 06:10:08 +0000

    Add BigIntegerConverterTests back

---
+++ src/Nethermind/Nethermind.Core.Test/Json/BigIntegerConverterTests.cs
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.IO;
+using System.Numerics;
+using System.Text.Json;
+
+using Nethermind.Serialization.Json;
+using NUnit.Framework;
+
+namespace Nethermind.Core.Test.Json
+{
+    [TestFixture]
+    public class BigIntegerConverterTests : ConverterTestBase<BigInteger>
+    {
+        static BigIntegerConverter converter = new BigIntegerConverter();
+        static JsonSerializerOptions options = new JsonSerializerOptions { Converters = { converter } };
+
+        public void Test_roundtrip()
+        {
+            TestConverter(int.MaxValue, (integer, bigInteger) => integer.Equals(bigInteger), converter);
+            TestConverter(BigInteger.One, (integer, bigInteger) => integer.Equals(bigInteger), converter);
+            TestConverter(BigInteger.Zero, (integer, bigInteger) => integer.Equals(bigInteger), converter);
+        }
+
+        [Test]
+        public void Can_read_0()
+        {
+            BigInteger result = JsonSerializer.Deserialize<BigInteger>("0", options);
+            Assert.That(result, Is.EqualTo(BigInteger.Parse("0")));
+        }
+
+        [Test]
+        public void Can_read_1()
+        {
+            BigInteger result = JsonSerializer.Deserialize<BigInteger>("1", options);
+            Assert.That(result, Is.EqualTo(BigInteger.Parse("1")));
+        }
+    }
+}
---
+++ src/Nethermind/Nethermind.Core.Test/Json/NullableBigIntegerConverterTests.cs
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.IO;
+using System.Numerics;
+using System.Text.Json;
+
+using Nethermind.Serialization.Json;
+using NUnit.Framework;
+
+namespace Nethermind.Core.Test.Json
+{
+    [TestFixture]
+    public class NullableBigIntegerConverterTests : ConverterTestBase<BigInteger?>
+    {
+        static NullableBigIntegerConverter converter = new NullableBigIntegerConverter();
+        static JsonSerializerOptions options = new JsonSerializerOptions { Converters = { converter } };
+        public void Test_roundtrip()
+        {
+            TestConverter(null, (integer, bigInteger) => integer.Equals(bigInteger), converter);
+            TestConverter(int.MaxValue, (integer, bigInteger) => integer.Equals(bigInteger), converter);
+            TestConverter(BigInteger.One, (integer, bigInteger) => integer.Equals(bigInteger), converter);
+            TestConverter(BigInteger.Zero, (integer, bigInteger) => integer.Equals(bigInteger), converter);
+        }
+
+        [Test]
+        public void Can_read_0()
+        {
+            BigInteger? result = JsonSerializer.Deserialize<BigInteger?>("0", options);
+            Assert.That(result, Is.EqualTo(BigInteger.Parse("0")));
+        }
+
+        [Test]
+        public void Can_read_1()
+        {
+            BigInteger? result = JsonSerializer.Deserialize<BigInteger?>("1", options);
+            Assert.That(result, Is.EqualTo(BigInteger.Parse("1")));
+        }
+
+        [Test]
+        public void Can_read_null()
+        {
+            BigInteger? result = JsonSerializer.Deserialize<BigInteger?>("null", options);
+            Assert.That(result, Is.EqualTo(null));
+        }
+    }
+}
