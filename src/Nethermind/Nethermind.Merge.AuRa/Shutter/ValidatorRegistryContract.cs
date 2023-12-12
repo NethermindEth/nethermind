c60175cf28e0626e92d56ff0c83f9d8ba246b135
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2023-12-12 13:31:07 +0000

    implement compute validator registry message prefix

--- src/Nethermind/Nethermind.Merge.AuRa/Shutter/ValidatorRegistryContract.cs
+++ src/Nethermind/Nethermind.Merge.AuRa/Shutter/ValidatorRegistryContract.cs
@@ -2,10 +2,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Buffers.Binary;
 using Nethermind.Abi;
 using Nethermind.Blockchain.Contracts;
 using Nethermind.Core;
@@ -15,13 +12,49 @@ namespace Nethermind.Merge.AuRa.Shutter;
 
 public class ValidatorRegistryContract : CallableContract, IValidatorRegistryContract
 {
+    private static readonly string FUNCTION_NAME = "update";
+    private static readonly byte VALIDATOR_REGISTRY_MESSAGE_VERSION = 0;
+
     public ValidatorRegistryContract(ITransactionProcessor transactionProcessor, IAbiEncoder abiEncoder, Address contractAddress)
         : base(transactionProcessor, abiEncoder, contractAddress)
     {
     }
 
+    // def compute_registration_message(validator_index: uint64, nonce: uint64):
+    //     return compute_registry_message_prefix(validator_index, nonce) + b"\x01"
+
+    // def compute_deregistration_message(validator_index: uint64, nonce: uint64):
+    //     return compute_registry_message_prefix(validator_index, nonce) + b"\x00"
+
+    // def compute_registry_message_prefix(validator_index: uint64, nonce: uint64):
+    //     return VALIDATOR_REGISTRY_MESSAGE_VERSION + CHAIN_ID.to_bytes(8, "big") + VALIDATOR_REGISTRY_ADDRESS + validator_index.to_bytes(8, "big") + nonce.to_bytes(8, "big")
+
+    private byte[] ComputeRegistryMessagePrefix(UInt64 validatorIndex, UInt64 nonce, bool isRegistration)
+    {
+        Span<byte> registryMessagePrefix = stackalloc byte[46];
+
+        registryMessagePrefix[0] = VALIDATOR_REGISTRY_MESSAGE_VERSION;
+        BinaryPrimitives.WriteUInt64BigEndian(registryMessagePrefix.Slice(3), BlockchainIds.Gnosis);
+        Span<byte> addressSpan = registryMessagePrefix.Slice(9);
+        addressSpan = ContractAddress!.Bytes;
+        BinaryPrimitives.WriteUInt64BigEndian(registryMessagePrefix.Slice(31), validatorIndex);
+        BinaryPrimitives.WriteUInt64BigEndian(registryMessagePrefix.Slice(39), nonce);
+
+        if (isRegistration)
+        {
+            registryMessagePrefix[45] = 1;
+        }
+
+        return registryMessagePrefix.ToArray();
+    }
+
     public void Deregister(BlockHeader blockHeader, byte[] message, byte[] signature)
     {
+        Address sender = new("0x0");
+        long gasLimit = 100;
+        object[] arguments = new object[0];
+
+        var transaction = GenerateTransaction<GeneratedTransaction>(FUNCTION_NAME, sender, gasLimit, blockHeader, arguments);
         throw new NotImplementedException();
     }
 
