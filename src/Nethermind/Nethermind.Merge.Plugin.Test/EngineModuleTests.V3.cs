f90c5a1cfdf58fe2e1ec51ae50e774f4a6979ec8
benaadams <thundercat@illyriad.co.uk>
2023-12-12 22:13:56 +0000

    invalid params

--- src/Nethermind/Nethermind.Merge.Plugin.Test/EngineModuleTests.V3.cs
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/EngineModuleTests.V3.cs
@@ -155,7 +155,7 @@ public partial class EngineModuleTests
 
         ResultWrapper<PayloadStatusV1> result = await rpcModule.engine_newPayloadV3(payload, blobVersionedHashes, payload.ParentBeaconBlockRoot);
 
-        Assert.That(result.ErrorCode, Is.EqualTo(ErrorCodes.None));
+        Assert.That(result.ErrorCode, Is.EqualTo(expectedPayloadStatus == PayloadStatus.Invalid ? ErrorCodes.InvalidParams : ErrorCodes.None));
         result.Data.Status.Should().Be(expectedPayloadStatus);
     }
 
