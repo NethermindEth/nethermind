b9b75c6cfaf6a549622fc481552556719fb51a61
Amirul Ashraf <asdacap@gmail.com>
2024-01-03 16:10:12 +0800

    Use long for tiny tree path

--- src/Nethermind/Nethermind.Trie/Pruning/TinyTreePath.cs
+++ src/Nethermind/Nethermind.Trie/Pruning/TinyTreePath.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using Nethermind.Core.Crypto;
 
 namespace Nethermind.Trie;
@@ -10,36 +11,33 @@ namespace Nethermind.Trie;
 /// <summary>
 /// Like TreePath, but tiny. Fit in 8 byte, like a long. Can only represent 14 nibble.
 /// </summary>
-[InlineArray(8)]
 public struct TinyTreePath
 {
     public const int MaxNibbleLength = 14;
 
-    byte _byte;
+    long _data;
+
+    // Eh.. readonly?
+    private Span<byte> AsSpan => MemoryMarshal.AsBytes(MemoryMarshal.CreateSpan(ref _data, 1));
 
     public TinyTreePath(in TreePath path)
     {
         if (path.Length > MaxNibbleLength) throw new InvalidOperationException("Unable to represent more than 14 nibble");
         Span<byte> pathSpan = path.Path.BytesAsSpan;
-        for (int i = 0; i < 7; i++)
-        {
-            this[i] = pathSpan[i];
-        }
-
-        this[7] = (byte)path.Length;
+        Span<byte> selfSpan = AsSpan;
+        pathSpan[..7].CopyTo(selfSpan);
+        selfSpan[7] = (byte)path.Length;
     }
 
-    public readonly int Length => this[7];
+    public int Length => AsSpan[7];
 
-    public readonly TreePath ToTreePath()
+    public TreePath ToTreePath()
     {
         ValueHash256 rawPath = Keccak.Zero;
         Span<byte> pathSpan = rawPath.BytesAsSpan;
-        for (int i = 0; i < 7; i++)
-        {
-            pathSpan[i] = this[i];
-        }
+        Span<byte> selfSpan = AsSpan;
+        selfSpan[..7].CopyTo(pathSpan);
 
-        return new TreePath(rawPath, Length);
+        return new TreePath(rawPath, selfSpan[7]);
     }
 }
