9b5a3f2988036b616730e07a7c59afe278cbfa24
Klaudia Jazgar <67744705+kjazgar@users.noreply.github.com>
2024-01-05 20:14:37 +0100

    Feature/debug get bad blocks (#3838)
    Co-authored-by: lukasz.rozmej <lukasz.rozmej@gmail.com>
    Co-authored-by: Marc <Marchhill@users.noreply.github.com>

--- src/Nethermind/Nethermind.Init/Steps/RegisterRpcModules.cs
+++ src/Nethermind/Nethermind.Init/Steps/RegisterRpcModules.cs
@@ -116,6 +116,7 @@ public class RegisterRpcModules : IStep
         if (_api.KeyStore is null) throw new StepDependencyException(nameof(_api.KeyStore));
         if (_api.PeerPool is null) throw new StepDependencyException(nameof(_api.PeerPool));
         if (_api.WitnessRepository is null) throw new StepDependencyException(nameof(_api.WitnessRepository));
+        if (_api.BadBlocksStore is null) throw new StepDependencyException(nameof(_api.BadBlocksStore));
 
         ProofModuleFactory proofModuleFactory = new(_api.WorldStateManager, _api.BlockTree, _api.BlockPreprocessor, _api.ReceiptFinder, _api.SpecProvider, _api.LogManager);
         rpcModuleProvider.RegisterBounded(proofModuleFactory, 2, rpcConfig.Timeout);
@@ -133,6 +134,7 @@ public class RegisterRpcModules : IStep
             _api.ConfigProvider,
             _api.SpecProvider,
             _api.SyncModeSelector,
+            _api.BadBlocksStore,
             _api.FileSystem,
             _api.LogManager);
         rpcModuleProvider.RegisterBoundedByCpuCount(debugModuleFactory, rpcConfig.Timeout);
