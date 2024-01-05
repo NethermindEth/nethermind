300c56ffec75a39ce8405857e61f280e14fc905e
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-01-05 18:19:42 +0000

    started implementing hash_to_curve

--- src/Nethermind/Nethermind.Crypto/Bls.cs
+++ src/Nethermind/Nethermind.Crypto/Bls.cs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Security.Cryptography;
@@ -18,6 +19,7 @@ namespace Nethermind.Crypto
         internal static readonly BigInteger BaseFieldOrder = new([0x1a,0x01,0x11,0xea,0x39,0x7f,0xe6,0x9a,0x4b,0x1b,0xa7,0xb6,0x43,0x4b,0xac,0xd7,0x64,0x77,0x4b,0x84,0xf3,0x85,0x12,0xbf,0x67,0x30,0xd2,0xa0,0xf6,0xb0,0xf6,0x24,0x1e,0xab,0xff,0xfe,0xb1,0x53,0xff,0xff,0xb9,0xfe,0xff,0xff,0xff,0xff,0xaa,0xab], true, true);
         internal static readonly byte[] SubgroupOrder = [0x73,0xed,0xa7,0x53,0x29,0x9d,0x7d,0x48,0x33,0x39,0xd8,0x08,0x09,0xa1,0xd8,0x05,0x53,0xbd,0xa4,0x02,0xff,0xfe,0x5b,0xfe,0xff,0xff,0xff,0xff,0x00,0x00,0x00,0x01];
         internal static readonly byte[] SubgroupOrderMinusOne = [0x73,0xed,0xa7,0x53,0x29,0x9d,0x7d,0x48,0x33,0x39,0xd8,0x08,0x09,0xa1,0xd8,0x05,0x53,0xbd,0xa4,0x02,0xff,0xfe,0x5b,0xfe,0xff,0xff,0xff,0xff,0x00,0x00,0x00,0x00];
+        // delimiter
         internal static readonly byte[] Cryptosuite = ASCIIEncoding.ASCII.GetBytes("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_");
 
         public static Signature Sign(PrivateKey privateKey, ReadOnlySpan<byte> message)
@@ -92,6 +94,11 @@ namespace Nethermind.Crypto
                 [0x17,0xF1,0xD3,0xA7,0x31,0x97,0xD7,0x94,0x26,0x95,0x63,0x8C,0x4F,0xA9,0xAC,0x0F,0xC3,0x68,0x8C,0x4F,0x97,0x74,0xB9,0x05,0xA1,0x4E,0x3A,0x3F,0x17,0x1B,0xAC,0x58,0x6C,0x55,0xE8,0x3F,0xF9,0x7A,0x1A,0xEF,0xFB,0x3A,0xF0,0x0A,0xDB,0x22,0xC6,0xBB],
                 [0x08,0xB3,0xF4,0x81,0xE3,0xAA,0xA0,0xF1,0xA0,0x9E,0x30,0xED,0x74,0x1D,0x8A,0xE4,0xFC,0xF5,0xE0,0x95,0xD5,0xD0,0x0A,0xF6,0x00,0xDB,0x18,0xCB,0x2C,0x04,0xB3,0xED,0xD0,0x3C,0xC7,0x44,0xA2,0x88,0x8A,0xE4,0x0C,0xAA,0x23,0x29,0x46,0xC5,0xE7,0xE1]
             );
+            private static readonly UInt256 HEff = 0xd201000000010001;
+
+            // L = ceil((ceil(log2(p)) + k) / 8), where k is the security parameter of the suite (e.g., k = 128).
+            // ceil(log2(p)) = 381, m = 2, k = 128
+            private static Index InputLength = 64;
 
             public G1(ReadOnlySpan<byte> X, ReadOnlySpan<byte> Y)
             {
@@ -108,20 +115,99 @@ namespace Nethermind.Crypto
                 return x.ToBigEndian() * Generator;
             }
 
+            // public static G1 HashToCurve(ReadOnlySpan<byte> msg)
+            // {
+            //     Span<byte> data = stackalloc byte[msg.Length + Cryptosuite.Length];
+            //     Cryptosuite.CopyTo(data);
+            //     msg.CopyTo(data[Cryptosuite.Length..]);
+            //     return SHA256.HashData(data) * Generator;
+            // }
+
             public static G1 HashToCurve(ReadOnlySpan<byte> msg)
             {
-                Span<byte> data = stackalloc byte[msg.Length + Cryptosuite.Length];
-                Cryptosuite.CopyTo(data);
-                msg.CopyTo(data[Cryptosuite.Length..]);
-                return SHA256.HashData(data) * Generator;
+                List<byte[]> u = HashToField(msg, 2);
+                G1 q1 = MapToCurve(u[0]);
+                G1 q2 = MapToCurve(u[1]);
+                return ClearCofactor(q1 + q2);
+            }
+
+            private static G1 ClearCofactor(G1 p)
+            {
+                return HEff * p;
+            }
+
+            private static List<byte[]> HashToField(ReadOnlySpan<byte> msg, int count)
+            {
+                // 1. len_in_bytes = count * m * L
+                // 2. uniform_bytes = expand_message(msg, DST, len_in_bytes)
+                // 3. for i in (0, ..., count - 1):
+                // 4.   for j in (0, ..., m - 1):
+                // 5.     elm_offset = L * (j + i * m)
+                // 6.     tv = substr(uniform_bytes, elm_offset, L)
+                // 7.     e_j = OS2IP(tv) mod p
+                // 8.   u_i = (e_0, ..., e_(m - 1))
+                // 9. return (u_0, ..., u_(count - 1))
+                int lenInBytes = count * InputLength.Value;
+                Span<byte> uniformBytes = stackalloc byte[lenInBytes];
+                ExpandMessage(msg, Cryptosuite, lenInBytes, uniformBytes);
+                for (int i = 0; i < count; i++)
+                {
+                    int elmOffset = InputLength.Value + i;
+                    ReadOnlySpan<byte> tv = uniformBytes[elmOffset..InputLength];
+                }
+
+                return [];
+            }
+
+            private static void ExpandMessage(ReadOnlySpan<byte> msg, ReadOnlySpan<byte> dst, int lenInBytes, Span<byte> res)
+            {
+                // 1.  ell = ceil(len_in_bytes / b_in_bytes)
+                // 2.  ABORT if ell > 255
+                // 3.  DST_prime = DST || I2OSP(len(DST), 1)
+                // 4.  Z_pad = I2OSP(0, s_in_bytes)
+                // 5.  l_i_b_str = I2OSP(len_in_bytes, 2)
+                // 6.  msg_prime = Z_pad || msg || l_i_b_str || I2OSP(0, 1) || DST_prime
+                // 7.  b_0 = H(msg_prime)
+                // 8.  b_1 = H(b_0 || I2OSP(1, 1) || DST_prime)
+                // 9.  for i in (2, ..., ell):
+                // 10.    b_i = H(strxor(b_0, b_(i - 1)) || I2OSP(i, 1) || DST_prime)
+                // 11. uniform_bytes = b_1 || ... || b_ell
+                // 12. return substr(uniform_bytes, 0, len_in_bytes)
+                int ell = (lenInBytes / 32) + (lenInBytes % 32 == 0 ? 0 : 1);
+                if (ell > 255)
+                {
+                    throw new Exception();
+                }
+                Span<byte> dstPrime = stackalloc byte[10];
+                Span<byte> msgPrime = stackalloc byte[10];
+                Span<byte> hashInput = stackalloc byte[10];
+
+                byte[] b0 = SHA256.HashData(msgPrime);
+                b0.CopyTo(res);
+
+                if (ell >= 1)
+                {
+                    b0.CopyTo(hashInput);
+                    byte[] b1 = SHA256.HashData(msgPrime);
+                    b1.CopyTo(res[32..]);
+                }
+
+                for (int i = 2; i < ell; i++)
+                {
+                    byte[] b = SHA256.HashData(hashInput);
+                    b.CopyTo(res[(i*32)..]);
+                    hashInput[..32].Xor(b.AsSpan());
+                }
             }
 
             public static G1 FromSignature(Signature signature)
             {
                 // decompresss curve point
                 bool sign = (signature.Bytes[0] & 0x20) == 0x20;
+                byte tmp = signature.Bytes[0];
                 signature.Bytes[0] &= 0x1F; // mask out top 3 bits
                 G1 P = new(signature.Bytes, Twist(signature.Bytes));
+                signature.Bytes[0] = tmp;
                 return sign ? P : -P;
             }
 
@@ -189,6 +275,15 @@ namespace Nethermind.Crypto
                 p.Encode(encoded[..128]);
                 q.Encode(encoded[128..]);
                 Pairings.BlsG1Add(encoded, output);
+                return new G1(output[16..64], output[80..]);
+            }
+
+            private static G1 MapToCurve(byte[] Fp)
+            {
+                Span<byte> encoded = stackalloc byte[64];
+                Span<byte> output = stackalloc byte[128];
+                Fp.CopyTo(encoded[16..]);
+                Pairings.BlsMapToG1(encoded, output);
                 return new G1(output[16..64], output[80..]);
             }
 
