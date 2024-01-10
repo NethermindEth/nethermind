d8346ccfc27a228c961c3ac8ac3b50528b97e1c8
benaadams <thundercat@illyriad.co.uk>
2024-01-10 05:22:08 +0000

    Vectorize BytesToNibbleBytes

--- src/Nethermind/Nethermind.Trie/NibbleExtensions.cs
+++ src/Nethermind/Nethermind.Trie/NibbleExtensions.cs
@@ -4,6 +4,9 @@
 using System;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
 
 namespace Nethermind.Trie
 {
@@ -18,26 +21,46 @@ namespace Nethermind.Trie
         public static Nibble[] FromBytes(ReadOnlySpan<byte> bytes)
         {
             Nibble[] nibbles = new Nibble[2 * bytes.Length];
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                nibbles[i * 2] = new Nibble((byte)((bytes[i] & 240) >> 4));
-                nibbles[i * 2 + 1] = new Nibble((byte)(bytes[i] & 15));
-            }
-
+            BytesToNibbleBytes(bytes, MemoryMarshal.AsBytes(nibbles.AsSpan()));
             return nibbles;
         }
 
-        public static void BytesToNibbleBytes(ReadOnlySpan<byte> bytes, Span<byte> nibbles)
+        public unsafe static void BytesToNibbleBytes(ReadOnlySpan<byte> bytes, Span<byte> nibbles)
         {
             if (nibbles.Length != 2 * bytes.Length)
             {
                 ThrowArgumentException();
             }
 
-            for (int i = 0; i < bytes.Length; i++)
+            var length = bytes.Length / sizeof(Vector128<byte>) * sizeof(Vector128<byte>);
+            if (Vector128.IsHardwareAccelerated && length > 0)
             {
-                nibbles[i * 2] = (byte)((bytes[i] & 240) >> 4);
-                nibbles[i * 2 + 1] = (byte)(bytes[i] & 15);
+                var input = MemoryMarshal.Cast<byte, Vector128<byte>>(bytes.Slice(0, length));
+                ref var output = ref Unsafe.As<byte, Vector128<ushort>>(ref MemoryMarshal.GetReference(nibbles));
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Vector128<byte> value = input[i];
+                    (Vector128<ushort> lower0, Vector128<ushort> upper0) = Vector128.Widen(Vector128.BitwiseAnd(value, Vector128.Create((byte)0xf)));
+                    lower0 = Vector128.Shuffle(lower0.AsByte(), Vector128.Create((byte)1, 0, 1, 2, 1, 4, 1, 6, 1, 8, 1, 10, 1, 12, 1, 14)).AsUInt16();
+                    upper0 = Vector128.Shuffle(upper0.AsByte(), Vector128.Create((byte)1, 0, 1, 2, 1, 4, 1, 6, 1, 8, 1, 10, 1, 12, 1, 14)).AsUInt16();
+                    (Vector128<ushort> lower1, Vector128<ushort> upper1) = Vector128.Widen(Vector128.BitwiseAnd(value, Vector128.Create((byte)0xf0)));
+                    lower1 = Vector128.ShiftRightLogical(lower1.AsByte(), 4).AsUInt16();
+                    upper1 = Vector128.ShiftRightLogical(upper1.AsByte(), 4).AsUInt16();
+
+                    lower0 = Vector128.BitwiseOr(lower0, lower1);
+                    upper0 = Vector128.BitwiseOr(upper0, upper1);
+
+                    Unsafe.Add(ref output, i * 2) = lower0;
+                    Unsafe.Add(ref output, i * 2 + 1) = upper0;
+                }
+            }
+
+            for (int i = length; i < bytes.Length; i++)
+            {
+                int value = Unsafe.Add(ref MemoryMarshal.GetReference(bytes), i);
+                Unsafe.Add(ref MemoryMarshal.GetReference(nibbles), i * 2) = (byte)(value >> 4);
+                Unsafe.Add(ref MemoryMarshal.GetReference(nibbles), i * 2 + 1) = (byte)(value & 15);
             }
 
             [DoesNotReturn]
