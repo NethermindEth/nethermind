c59d9bff4d5192fd82bcbdaa84af3cd9c74b1f88
Ahmad Bitar <33181301+smartprogrammer93@users.noreply.github.com>
2024-01-15 18:34:31 +0300

    [Fix]{Config] old default barrier being ignoreed in clients upgraded to new versions but were initailly synced with older versions (1.19.3 and below)) (#6527)

--- src/Nethermind/Nethermind.Synchronization/FastBlocks/BarrierSyncFeed.cs
+++ src/Nethermind/Nethermind.Synchronization/FastBlocks/BarrierSyncFeed.cs
@@ -15,6 +15,8 @@ namespace Nethermind.Synchronization.FastBlocks;
 public abstract class BarrierSyncFeed<T> : ActivatedSyncFeed<T>
 {
     internal const int DepositContractBarrier = 11052984;
+    internal const int OldBarrierDefaultExtraRange = 8192;
+
     protected abstract long? LowestInsertedNumber { get; }
     protected abstract int BarrierWhenStartedMetadataDbKey { get; }
     protected abstract long SyncConfigBarrierCalc { get; }
@@ -32,7 +34,7 @@ public abstract class BarrierSyncFeed<T> : ActivatedSyncFeed<T>
     protected bool WithinOldBarrierDefault => _specProvider.ChainId == BlockchainIds.Mainnet
         && _barrierWhenStarted == DepositContractBarrier
         && LowestInsertedNumber <= DepositContractBarrier
-        && LowestInsertedNumber > DepositContractBarrier - GethSyncLimits.MaxBodyFetch; // this is intentional. using this as an approxamation assuming a minimum of 1 receipt in per block in case of receipts
+        && LowestInsertedNumber > DepositContractBarrier - OldBarrierDefaultExtraRange; // this is intentional. this is a magic number as to the amount of possible blocks that had been synced. We noticed on previous versions that the client synced a bit below the default barrier by more than just the GethRequest limit (128).
 
     public BarrierSyncFeed(IDb metadataDb, ISpecProvider specProvider, ILogger logger)
     {
