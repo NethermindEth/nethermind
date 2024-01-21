7848236b6e586dd20ebbdfa1f0dfa0a543b7497c
lukasz.rozmej <lukasz.rozmej@gmail.com>
2024-01-22 00:51:00 +0100

    more fixes

--- src/Nethermind/Nethermind.Evm/TransactionSubstate.cs
+++ src/Nethermind/Nethermind.Evm/TransactionSubstate.cs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
+using System.Collections.Frozen;
 using System.Collections.Generic;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
@@ -25,7 +26,7 @@ public class TransactionSubstate
     public static readonly byte[] ErrorFunctionSelector = Keccak.Compute("Error(string)").BytesToArray()[..RevertPrefix];
     public static readonly byte[] PanicFunctionSelector = Keccak.Compute("Panic(uint256)").BytesToArray()[..RevertPrefix];
 
-    private readonly IDictionary<UInt256, string> PanicReasons = new Dictionary<UInt256, string>
+    private static readonly FrozenDictionary<UInt256, string> PanicReasons = new Dictionary<UInt256, string>
     {
         { 0x00, "generic panic" },
         { 0x01, "assert(false)" },
@@ -37,7 +38,7 @@ public class TransactionSubstate
         { 0x32, "out-of-bounds access of an array or bytesN" },
         { 0x41, "out of memory" },
         { 0x51, "uninitialized function" },
-    };
+    }.ToFrozenDictionary();
 
     public bool IsError => Error is not null && !ShouldRevert;
     public string? Error { get; }
@@ -91,54 +92,56 @@ public class TransactionSubstate
         );
     }
 
-    private static string DefaultErrorMessage(ReadOnlySpan<byte> span) => span.ToHexString(true);
+    private static string DefaultErrorMessage(ReadOnlySpan<byte> span) => System.Text.Encoding.UTF8.GetString(span);
 
     private string? TryGetErrorMessage(ReadOnlySpan<byte> span)
     {
-        if (span.Length < RevertPrefix) goto UTF;
-        ReadOnlySpan<byte> prefix = span.TakeAndMove(RevertPrefix);
-        UInt256 start, length;
-
-        if (prefix.SequenceEqual(PanicFunctionSelector))
+        try
         {
-            if (span.Length < WordSize) goto UTF;
+            if (span.Length < RevertPrefix) return null;
+            ReadOnlySpan<byte> prefix = span.TakeAndMove(RevertPrefix);
+            UInt256 start, length;
 
-            UInt256 panicCode = new(span.TakeAndMove(WordSize), isBigEndian: true);
-            if (!PanicReasons.TryGetValue(panicCode, out string panicReason))
+            if (prefix.SequenceEqual(PanicFunctionSelector))
             {
-                return $"unknown panic code ({panicCode.ToHexString(skipLeadingZeros: true)})";
+                if (span.Length < WordSize) return null;
+
+                UInt256 panicCode = new(span.TakeAndMove(WordSize), isBigEndian: true);
+                if (!PanicReasons.TryGetValue(panicCode, out string panicReason))
+                {
+                    return $"unknown panic code ({panicCode.ToHexString(skipLeadingZeros: true)})";
+                }
+
+                return panicReason;
             }
 
-            return panicReason;
-        }
+            if (span.Length < WordSize * 2) return null;
 
-        if (prefix.SequenceEqual(ErrorFunctionSelector))
+            if (prefix.SequenceEqual(ErrorFunctionSelector))
+            {
+                start = new UInt256(span.TakeAndMove(WordSize), isBigEndian: true);
+                if (start != WordSize) return null;
+
+                length = new UInt256(span.TakeAndMove(WordSize), isBigEndian: true);
+                if (length > span.Length) return null;
+
+                ReadOnlySpan<byte> binaryMessage = span.TakeAndMove((int)length);
+                return System.Text.Encoding.UTF8.GetString(binaryMessage);
+
+            }
+
+            start = new UInt256(span.Slice(0, WordSize), isBigEndian: true);
+            if (UInt256.AddOverflow(start, WordSize, out UInt256 lengthOffset) || lengthOffset > span.Length) return null;
+
+            length = new UInt256(span.Slice((int)start, WordSize), isBigEndian: true);
+            if (UInt256.AddOverflow(lengthOffset, length, out UInt256 endOffset) || endOffset != span.Length) return null;
+
+            span = span.Slice((int)lengthOffset, (int)length);
+            return System.Text.Encoding.UTF8.GetString(span);
+        }
+        catch (OverflowException) // shouldn't happen, just for being safe
         {
-            if (span.Length < WordSize * 2) goto UTF;
-
-            start = new UInt256(span.TakeAndMove(WordSize), isBigEndian: true);
-            if (start != WordSize) goto UTF;
-
-            length = new UInt256(span.TakeAndMove(WordSize), isBigEndian: true);
-            if (length > span.Length) goto UTF;
-
-            ReadOnlySpan<byte> binaryMessage = span.TakeAndMove((int)length);
-            string message = System.Text.Encoding.UTF8.GetString(binaryMessage);
-
-            return message;
+            return null;
         }
-
-        if (span.Length < WordSize * 2) goto UTF;
-
-        start = new UInt256(span.Slice(0, WordSize), isBigEndian: true);
-        if (UInt256.AddOverflow(start, WordSize, out UInt256 lengthOffset) || lengthOffset > span.Length) goto UTF;
-
-        length = new UInt256(span.Slice((int)start, WordSize), isBigEndian: true);
-        if (UInt256.AddOverflow(lengthOffset, length, out UInt256 endOffset) || endOffset != span.Length) goto UTF;
-
-        return span.Slice((int)lengthOffset, (int)length).ToHexString(true);
-
-        UTF:
-        return System.Text.Encoding.UTF8.GetString(span);
     }
 }
