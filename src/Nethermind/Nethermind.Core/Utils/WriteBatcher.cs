f4e414e60c71aa8e1936af25296f0356eb15fb60
Amirul Ashraf <asdacap@gmail.com>
2024-01-21 21:31:01 +0800

    Whitespace

--- src/Nethermind/Nethermind.Core/Utils/WriteBatcher.cs
+++ src/Nethermind/Nethermind.Core/Utils/WriteBatcher.cs
@@ -10,7 +10,7 @@ namespace Nethermind.Core.Utils;
 /// <summary>
 /// Batches writes into a set of concurrent batches. For cases where throughput matter, but not atomicity.
 /// </summary>
-public class WriteBatcher: IWriteBatch
+public class WriteBatcher : IWriteBatch
 {
     private long _counter = 0;
     private readonly ConcurrentQueue<IWriteBatch> _batches = new();
