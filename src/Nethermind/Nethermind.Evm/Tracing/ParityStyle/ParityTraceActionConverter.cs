0bcefe70916cb1d4a95610eebeafd46ce4dc359a
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2024-01-24 12:12:09 +0100

    fix ParityTraceActionConverter to output init on create (#6606)
    (cherry picked from commit 1ef82afb6d87b255059d93c473d07e44beb30699)

--- src/Nethermind/Nethermind.Evm/Tracing/ParityStyle/ParityTraceActionConverter.cs
+++ src/Nethermind/Nethermind.Evm/Tracing/ParityStyle/ParityTraceActionConverter.cs
@@ -83,6 +83,11 @@ namespace Nethermind.Evm.Tracing.ParityStyle
                     reader.Read();
                     value.Input = JsonSerializer.Deserialize<byte[]?>(ref reader, options);
                 }
+                else if (reader.ValueTextEquals("init"u8))
+                {
+                    reader.Read();
+                    value.Input = JsonSerializer.Deserialize<byte[]?>(ref reader, options);
+                }
                 else if (reader.ValueTextEquals("result"u8))
                 {
                     reader.Read();
@@ -162,10 +167,18 @@ namespace Nethermind.Evm.Tracing.ParityStyle
             writer.WritePropertyName("gas"u8);
             JsonSerializer.Serialize(writer, value.Gas, options);
 
-            writer.WritePropertyName("input"u8);
-            JsonSerializer.Serialize(writer, value.Input, options);
-            writer.WritePropertyName("to"u8);
-            JsonSerializer.Serialize(writer, value.To, options);
+            if (value.CallType == "create")
+            {
+                writer.WritePropertyName("init"u8);
+                JsonSerializer.Serialize(writer, value.Input, options);
+            }
+            else
+            {
+                writer.WritePropertyName("input"u8);
+                JsonSerializer.Serialize(writer, value.Input, options);
+                writer.WritePropertyName("to"u8);
+                JsonSerializer.Serialize(writer, value.To, options);
+            }
 
             writer.WritePropertyName("value"u8);
             JsonSerializer.Serialize(writer, value.Value, options);
