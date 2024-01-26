9e64f24cc4c2fbafe12f0d7977f7d492d0947053
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2024-01-26 15:48:59 +0100

    Fix/trace store minor serialization issue (#6619)

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/Trace/ParityTraceActionSerializationTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/Trace/ParityTraceActionSerializationTests.cs
@@ -36,5 +36,24 @@ namespace Nethermind.JsonRpc.Test.Modules.Trace
 
             TestToJson(action, "{\"callType\":null,\"from\":null,\"gas\":\"0x0\",\"input\":null,\"to\":null,\"value\":\"0x0\"}", ParityTraceActionConverter.Instance);
         }
+
+        [Test]
+        public void Can_serialize_error_without_converter()
+        {
+            ParityTraceAction action = new()
+            {
+                From = TestItem.AddressA,
+                Gas = 12345,
+                Input = [6, 7, 8, 9, 0],
+                To = TestItem.AddressB,
+                Value = 24680,
+                CallType = "call",
+                TraceAddress = [1, 3, 5, 7],
+                Error = "error",
+                Result = null
+            };
+
+            TestToJson(action, "{\"traceAddress\":[1,3,5,7],\"callType\":\"call\",\"includeInTrace\":true,\"isPrecompiled\":false,\"from\":\"0xb7705ae4c6f81b66cdb323c65f4e8133690fc099\",\"to\":\"0x942921b14f1b1c385cd7e0cc2ef7abe5598c8358\",\"gas\":\"0x3039\",\"value\":\"0x6068\",\"input\":\"0x0607080900\",\"result\":null,\"subtraces\":[],\"error\":\"error\"}");
+        }
     }
 }
