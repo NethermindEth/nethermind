3f38c73b03d3f69eddde702d487d663e87a57929
ak88 <anders.holmbjerg@hotmail.com>
2024-02-08 17:06:42 +0100

    external signer

--- src/Nethermind/Nethermind.Consensus/ExternalSigner.cs
+++ src/Nethermind/Nethermind.Consensus/ExternalSigner.cs
@@ -3,7 +3,10 @@
 
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
+using Nethermind.Core.Extensions;
 using Nethermind.Crypto;
+using Nethermind.JsonRpc.Client;
+using Org.BouncyCastle.Utilities.Encoders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,26 +16,43 @@ using System.Threading.Tasks;
 namespace Nethermind.Consensus;
 public class ExternalSigner : ISigner
 {
-    private readonly string jsonRpcUrl;
+    private readonly IJsonRpcClient rpcClient;
 
-    //public ExternalSigner(IJsonRpcClient rpcClient)
-    //{
-    //    this.jsonRpcUrl = jsonRpcUrl;
+    private ExternalSigner(IJsonRpcClient rpcClient)
+    {
+        this.rpcClient = rpcClient;
+        CanSign = true;
+    }
 
-    //}
-    public PrivateKey? Key => throw new NotImplementedException();
+    public static async Task<ExternalSigner> Create(IJsonRpcClient jsonRpcClient)
+    {
+        ExternalSigner signer =new (jsonRpcClient);
+        await signer.SetSignerAddress();
+        return signer;
+    }
 
-    public Address Address => throw new NotImplementedException();
+    public Address Address { get; private set; }
 
-    public bool CanSign => throw new NotImplementedException();
+    public bool CanSign { get; }
+
+    public PrivateKey? Key => throw new InvalidOperationException("Private keys cannot be exposed.");
 
     public Signature Sign(Hash256 message)
     {
-        throw new NotImplementedException();
+        //TODO handle async
+        var signed = rpcClient.Post<string>("account_signData", "application/clique", Address.ToString(), message).GetAwaiter().GetResult();
+        return new Signature(Bytes.FromHexString(signed));
     }
 
     public ValueTask Sign(Transaction tx)
     {
         throw new NotImplementedException();
+    }
+
+    private async Task SetSignerAddress()
+    {
+        //TODO sort the list and validate
+        var accounts = await rpcClient.Post<string[]>("account_list");
+        Address = new Address(accounts[0]);
     }
 }
