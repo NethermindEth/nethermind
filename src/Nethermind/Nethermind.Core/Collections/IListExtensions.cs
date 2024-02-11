c65f8a2dbbb0b069fb732009cd4ae274124457c3
Ben Adams <thundercat@illyriad.co.uk>
2024-02-11 15:51:03 +0000

    Reduce StackList indirections (#6688)

--- src/Nethermind/Nethermind.Core/Collections/IListExtensions.cs
+++ src/Nethermind/Nethermind.Core/Collections/IListExtensions.cs
@@ -57,29 +57,6 @@ namespace Nethermind.Core.Collections
             return ~lower;
         }
 
-        /// <summary>
-        /// Performs a binary search on the specified collection.
-        /// </summary>
-        /// <typeparam name="TItem">The type of the item.</typeparam>
-        /// <param name="list">The list to be searched.</param>
-        /// <param name="value">The value to search for.</param>
-        /// <returns></returns>
-        public static int BinarySearch<TItem>(this IList<TItem> list, TItem value) => BinarySearch(list, value, Comparer<TItem>.Default);
-
-        /// <summary>
-        /// Performs a binary search on the specified collection.
-        /// </summary>
-        /// <typeparam name="TItem">The type of the item.</typeparam>
-        /// <param name="list">The list to be searched.</param>
-        /// <param name="value">The value to search for.</param>
-        /// <param name="comparer">The comparer that is used to compare the value
-        /// with the list items.</param>
-        /// <returns></returns>
-        public static int BinarySearch<TItem>(this IList<TItem> list, TItem value, IComparer<TItem> comparer) => list.BinarySearch(value, comparer.Compare);
-
-        public static bool TryGetSearchedItem<TComparable>(this IList<TComparable> list, in TComparable activation, out TComparable? item) where TComparable : IComparable<TComparable> =>
-            list.TryGetSearchedItem(activation, (b, c) => b.CompareTo(c), out item);
-
         public static bool TryGetForBlock(this IList<long> list, in long blockNumber, out long item) =>
             list.TryGetSearchedItem(blockNumber, (b, c) => b.CompareTo(c), out item);
 
