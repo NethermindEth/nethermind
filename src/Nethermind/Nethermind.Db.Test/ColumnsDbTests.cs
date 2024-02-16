deeef3059c471761aded2ddb531e4d600690b41e
Amirul Ashraf <asdacap@gmail.com>
2024-02-16 16:59:13 +0800

    Wait sometime for memtable size

--- src/Nethermind/Nethermind.Db.Test/ColumnsDbTests.cs
+++ src/Nethermind/Nethermind.Db.Test/ColumnsDbTests.cs
@@ -76,7 +76,7 @@ public class ColumnsDbTests
         colA.Set(TestItem.KeccakA, TestItem.KeccakA.BytesToArray());
         colB.Set(TestItem.KeccakA, TestItem.KeccakB.BytesToArray());
 
-        _db.GatherMetric().MemtableSize.Should().Be(22544);
+        Assert.That(() => _db.GatherMetric().MemtableSize, Is.EqualTo(22544).After(1000, 10));
     }
 
     [Test]
