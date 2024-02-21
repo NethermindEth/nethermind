9c97b0b08d1c4b417e906e13c43c45e781e6bd4a
Tanishq Jasoria <jasoriatanishq@gmail.com>
2024-02-21 11:14:27 +0530

    add verkle test blockchain

--- src/Nethermind/Nethermind.Core.Test/Blockchain/BasicTestBlockchain.cs
+++ src/Nethermind/Nethermind.Core.Test/Blockchain/BasicTestBlockchain.cs
@@ -30,3 +30,24 @@ public class BasicTestBlockchain : TestBlockchain
     }
 }
 
+
+public class BasicTestVerkleBlockchain : TestVerkleBlockchain
+{
+    public static async Task<BasicTestVerkleBlockchain> Create()
+    {
+        BasicTestVerkleBlockchain chain = new BasicTestVerkleBlockchain();
+        await chain.Build();
+        return chain;
+    }
+
+    protected override Task AddBlocksOnStart() => Task.CompletedTask;
+
+    public async Task BuildSomeBlocks(int numOfBlocks)
+    {
+        for (int i = 0; i < numOfBlocks; i++)
+        {
+            await AddBlock(Core.Test.Builders.Build.A.Transaction.WithTo(TestItem.AddressD)
+                .SignedAndResolved(TestItem.PrivateKeyA).TestObject);
+        }
+    }
+}
