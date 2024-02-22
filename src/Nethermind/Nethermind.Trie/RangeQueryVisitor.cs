c317f37d0f9e8af834d81e014dd72ef7cafb9f7e
Amirul Ashraf <asdacap@gmail.com>
2024-02-22 20:45:24 +0800

    Fix snap server throwing error on range after last node

--- src/Nethermind/Nethermind.Trie/RangeQueryVisitor.cs
+++ src/Nethermind/Nethermind.Trie/RangeQueryVisitor.cs
@@ -147,15 +147,18 @@ public class RangeQueryVisitor : ITreeVisitor<TreePathContext>, IDisposable
         }
 
         TreePath rightmostPath = _rightmostLeafPath;
-        for (int i = 64; i >= 0; i--)
+        if (rightmostPath.Length != 0)
         {
-            if (!_rightmostNodes[i].HasValue) continue;
+            for (int i = 64; i >= 0; i--)
+            {
+                if (!_rightmostNodes[i].HasValue) continue;
 
-            (TreePath path, TrieNode node) = _rightmostNodes[i].Value;
-            rightmostPath.TruncateMut(i);
-            if (rightmostPath != path) continue;
+                (TreePath path, TrieNode node) = _rightmostNodes[i].Value;
+                rightmostPath.TruncateMut(i);
+                if (rightmostPath != path) continue;
 
-            proofs.Add(node.FullRlp.ToArray());
+                proofs.Add(node.FullRlp.ToArray());
+            }
         }
 
         return proofs.ToArray();
