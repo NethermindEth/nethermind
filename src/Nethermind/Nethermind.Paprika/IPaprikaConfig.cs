f1f9c150ac9db62bc5750624219d2966a27d3585
Ben Adams <thundercat@illyriad.co.uk>
2024-02-27 14:20:40 +0000

    Formatting

--- src/Nethermind/Nethermind.Paprika/IPaprikaConfig.cs
+++ src/Nethermind/Nethermind.Paprika/IPaprikaConfig.cs
@@ -11,13 +11,13 @@ public interface IPaprikaConfig : IConfig
     [ConfigItem(Description = "The depths of reads that should be cached for State data. If a read takes more than this number blocks to traverse, try to cache it", DefaultValue = "8")]
     public ushort CacheStateBeyond { get; set; }
 
-    [ConfigItem(Description = "The total budget of entries to be used per block for state", DefaultValue  = "5000")]
+    [ConfigItem(Description = "The total budget of entries to be used per block for state", DefaultValue = "5000")]
     public int CacheStatePerBlock { get; set; }
 
     [ConfigItem(Description = "The depths of reads that should be cached for Merkle data. If a read takes more than this number blocks to traverse, try to cache it", DefaultValue = "8")]
     public ushort CacheMerkleBeyond { get; set; }
 
-    [ConfigItem(Description = "The total budget of entries to be used per block", DefaultValue  = "5000")]
+    [ConfigItem(Description = "The total budget of entries to be used per block", DefaultValue = "5000")]
     public int CacheMerklePerBlock { get; set; }
 }
 
