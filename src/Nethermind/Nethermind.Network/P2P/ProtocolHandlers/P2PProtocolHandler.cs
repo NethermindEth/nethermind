4cd5a6d1248e25858824f400c4191fde6b50b854
Marcin Sobczak <marcindsobczak@gmail.com>
2024-02-27 13:07:27 +0100

    set eth67 and eth68 as default versions

--- src/Nethermind/Nethermind.Network/P2P/ProtocolHandlers/P2PProtocolHandler.cs
+++ src/Nethermind/Nethermind.Network/P2P/ProtocolHandlers/P2PProtocolHandler.cs
@@ -43,6 +43,8 @@ public class P2PProtocolHandler : ProtocolHandlerBase, IPingSender, IP2PProtocol
     public static readonly IEnumerable<Capability> DefaultCapabilities = new Capability[]
     {
         new(Protocol.Eth, 66),
+        new(Protocol.Eth, 67),
+        new(Protocol.Eth, 68),
         new(Protocol.NodeData, 1)
     };
 
