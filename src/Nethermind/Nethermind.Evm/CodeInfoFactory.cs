79edebb4d5f771c5cc36e012f82bbe28a0c189ec
Demuirgos <ayman.bouchareb@outlook.fr>
2024-03-20 16:14:02 +0000

    fix build

--- src/Nethermind/Nethermind.Evm/CodeInfoFactory.cs
+++ src/Nethermind/Nethermind.Evm/CodeInfoFactory.cs
@@ -3,15 +3,16 @@
 
 using Nethermind.Core.Specs;
 using Nethermind.Evm.EOF;
+using System;
 
 namespace Nethermind.Evm.CodeAnalysis;
 
 public static class CodeInfoFactory
 {
-    public static ICodeInfo CreateCodeInfo(byte[] code, IReleaseSpec spec)
+    public static ICodeInfo CreateCodeInfo(Memory<byte> code, IReleaseSpec spec)
     {
         CodeInfo codeInfo = new(code);
-        return spec.IsEofEnabled && EvmObjectFormat.IsValidEof(code, EvmObjectFormat.ValidationStrategy.Validate, out EofHeader? header)
+        return spec.IsEofEnabled && EvmObjectFormat.IsValidEof(code.Span, EvmObjectFormat.ValidationStrategy.Validate, out EofHeader? header)
             ? new EofCodeInfo(codeInfo, header.Value)
             : codeInfo;
     }
