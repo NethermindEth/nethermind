ff5ceda5b1961fc67c771d38caeeb4004059ba43
Alexey Osipov <me@flcl.me>
2024-03-29 17:48:04 +0300

    Fix code; improve README

--- tools/HiveToScripts/Program.cs
+++ tools/HiveToScripts/Program.cs
@@ -1,85 +1,109 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 bool doLog = true;
 
-string hiveLogsDirectory = args.Length > 0 ? args[0] : "T:\\logs";
-string outputDirectory = args.Length > 1 ? args[1] : "T:\\output";
+string hiveLogsDirectory = args.Length > 0 ? args[0] : "logs";
+
+if(Directory.Exists(Path.Combine(hiveLogsDirectory, "logs")))
+{
+    hiveLogsDirectory = Path.Combine(hiveLogsDirectory, "logs");
+}
+
+string outputDirectory = args.Length > 1 ? args[1] : "output";
 string jsonrpcUrl = args.Length > 2 ? args[2] : "http://localhost:8551";
 
-string simulatorFilePath = Path.Combine(outputDirectory, Directory.GetFiles(hiveLogsDirectory, "*-simulator-*.log").Single());
+string[] simulatorFilePaths = Directory.GetFiles(hiveLogsDirectory, "*-simulator-*.log").Select(f => Path.Combine(outputDirectory, f)).ToArray();
 
-using StreamReader simulatorFile = File.OpenText(simulatorFilePath);
-
-if (Directory.Exists(outputDirectory))
+foreach (string simulatorFilePath in simulatorFilePaths)
 {
-    foreach (string f in Directory.EnumerateFiles(outputDirectory))
+    using StreamReader simulatorFile = File.OpenText(simulatorFilePath);
+
+    if (Directory.Exists(outputDirectory))
     {
-        File.Delete(f);
-    }
-}
-Directory.CreateDirectory(outputDirectory);
-if (doLog) Console.WriteLine($"Running...");
-HashSet<string> createdFiles = new();
-
-Regex starter = new Regex("^>>\\s+\\(([a-f0-9]+)\\)\\s+");
-
-string? line;
-int fileCounter = 0;
-
-// let's read line by line file that may be 2GB in size
-while ((line = simulatorFile.ReadLine()) is not null)
-{
-    if (!line.StartsWith(">>"))
-    {
-        continue;
-    }
-
-    string container = starter.Match(line).Groups[1].Value;
-    string value = starter.Replace(line, "");
-
-    string filename = Path.Combine(outputDirectory, "hive.{0}.{1}");
-    string cfgFilename = Path.Combine(outputDirectory, $"hive.{container}-config.cfg");
-    string specFilename = Path.Combine(outputDirectory, $"hive.{container}-spec.json");
-
-    if (!createdFiles.Contains(cfgFilename))
-    {
-        createdFiles.Add(cfgFilename);
-        string elOutputFilenamePath = Path.Combine(hiveLogsDirectory, Directory.GetFiles(hiveLogsDirectory, $"nethermind/client-{container}*.log").Single());
-        using StreamReader elOutputFile = File.OpenText(elOutputFilenamePath);
-        string? elLine;
-
-        elOutputFile.ReadLine();
-        string spec = "";
-        do
+        foreach (string f in Directory.EnumerateFiles(outputDirectory))
         {
-            elLine = elOutputFile.ReadLine();
-            spec += elLine + "\n";
+            File.Delete(f);
         }
-        while (elLine != "}");
-
-        elOutputFile.ReadLine();
-        string cfg = "";
-        do
-        {
-            elLine = elOutputFile.ReadLine();
-            cfg += elLine + "\n";
-        }
-        while (elLine != "}");
-
-        File.WriteAllText(cfgFilename, cfg);
-
-        File.WriteAllText(specFilename, spec);
     }
-    //var values = lines.Where(l => l.key == key).Select(x => x.value).ToList();
+    Directory.CreateDirectory(outputDirectory);
+    if (doLog) Console.WriteLine($"Running...");
+    HashSet<string> createdFiles = [];
 
-    string json = value;
+    string testListFile = Path.Combine(outputDirectory, "__tests.txt");
+    HashSet<string> reportedContainers = [];
 
-    if (json.Length > 7500)
+    Regex starter = new("^>>\\s+\\(([a-f0-9]+)\\)\\s+");
+
+    string? line;
+    int fileCounter = 0;
+
+    // let's read line by line file that may be 2GB in size
+    while ((line = simulatorFile.ReadLine()) is not null)
     {
-        string dataFile = string.Format(filename, container, $"data-{++fileCounter}.txt");
-        File.WriteAllText(dataFile, json);
+        if (!line.StartsWith(">>"))
+        {
+            if(line.StartsWith("Start test"))
+            {
+                File.AppendAllText(testListFile, $"{line.Replace("Start test ", "")}\n");
+            }
+            continue;
+        }
 
-        File.AppendAllText(string.Format(filename, container, "http"), @$"
+        string container = starter.Match(line).Groups[1].Value;
+        if (!reportedContainers.Contains(container))
+        {
+            reportedContainers.Add(container);
+            File.AppendAllText(testListFile, $"{container}\n");
+        }
+
+        string value = starter.Replace(line, "");
+
+        string filename = Path.Combine(outputDirectory, "{0}.{1}");
+        string cfgFilename = Path.Combine(outputDirectory, $"{container}.cfg");
+        string specFilename = Path.Combine(outputDirectory, $"{container}.json");
+
+        if (!createdFiles.Contains(cfgFilename))
+        {
+            createdFiles.Add(cfgFilename);
+            string elOutputFilenamePath = Directory.GetFiles(hiveLogsDirectory, $"client-{container}*.log", SearchOption.AllDirectories).Single();
+            if (elOutputFilenamePath is not null)
+            {
+                elOutputFilenamePath = Path.Combine(hiveLogsDirectory, elOutputFilenamePath);
+                using StreamReader elOutputFile = File.OpenText(elOutputFilenamePath);
+                string? elLine;
+
+                elOutputFile.ReadLine();
+                string spec = "";
+                do
+                {
+                    elLine = elOutputFile.ReadLine();
+                    spec += elLine + "\n";
+                }
+                while (elLine != "}");
+
+                elOutputFile.ReadLine();
+                string cfg = "";
+                do
+                {
+                    elLine = elOutputFile.ReadLine();
+                    cfg += elLine + "\n";
+                }
+                while (elLine != "}");
+
+                File.WriteAllText(cfgFilename, cfg);
+
+                File.WriteAllText(specFilename, spec);
+            }
+        }
+
+        string json = value;
+
+        if (json.Length > 7500)
+        {
+            string dataFile = string.Format(filename, container, $"data-{++fileCounter}.txt");
+            File.WriteAllText(dataFile, json);
+
+            File.AppendAllText(string.Format(filename, container, "http"), @$"
 ###
 POST {jsonrpcUrl}
 Content-Type: application/json
@@ -87,39 +111,40 @@ Content-Type: application/json
 <@ {dataFile}
 ");
 
-        File.AppendAllText(string.Format(filename, container, "ps1"), @$"
+            File.AppendAllText(string.Format(filename, container, "ps1"), @$"
 $json = Get-Content {dataFile} -Raw
 Invoke-WebRequest -Method POST -Uri {jsonrpcUrl} -Headers @{{'content-type'='application/json'}} -Body $json | Select-Object -expand RawContent
 ");
 
 
-        File.AppendAllText(string.Format(filename, container, "sh"), @$"
+            File.AppendAllText(string.Format(filename, container, "sh"), @$"
 curl -s --request POST --url {jsonrpcUrl} --header 'content-type: application/json' --data '@{dataFile}'
 sleep 1; echo ""
 ");
-    }
-    else
-    {
-        File.AppendAllText(string.Format(filename, container, "http"), @$"
+        }
+        else
+        {
+            File.AppendAllText(string.Format(filename, container, "http"), @$"
 ###
 POST {jsonrpcUrl}
 Content-Type: application/json
 
 {json}
 ");
-        json = json.Replace("\r", " ").Replace("\n", " ").Replace("'", "''");
+            json = json.Replace("\r", " ").Replace("\n", " ").Replace("'", "''");
 
-        File.AppendAllText(string.Format(filename, container, "ps1"), @$"
+            File.AppendAllText(string.Format(filename, container, "ps1"), @$"
 Invoke-WebRequest -Method POST -Uri {jsonrpcUrl} -Headers @{{'content-type'='application/json'}} -Body '{json}' | Select-Object -expand RawContent
 ");
 
-        File.AppendAllText(string.Format(filename, container, "sh"), @$"
+            File.AppendAllText(string.Format(filename, container, "sh"), @$"
 curl -s --request POST --url {jsonrpcUrl} --header 'content-type: application/json' --data '{json}'
 sleep 1; echo """"
 ");
 
+        }
+
     }
-
 }
-if (doLog) Console.WriteLine($"Done!");
 
+if (doLog) Console.WriteLine($"Done!");
--- tools/HiveToScripts/README.md
+++ tools/HiveToScripts/README.md
@@ -1,5 +1,21 @@
+# Hive test run to scripts
 
-```
-dotnet build .
-.\bin\Debug\net8.0\HiveToScripts.exe T:\logs\ T:\exp\ http://localhost:8550
-```
+Hive produces verbose output while running tests, it's enough to reproduce test cases by running queries logged by the runner.
+The only barrier is log trimming, which is turned off in flcl42/hive repo and will be soon disabled in case of **log level > 3** in the main hive repo.
+So to extract test cases in form of script, the following needs to be done:
+
+0. In case of pipeline usage download the logs and unpack them
+1. Confirm the logs were not trimmed
+2. Typically the directory has a `logs` entry that contains `*-simulator-*.log` files - it's the first input for the tool<br>
+   Output directory and JSONRPC URL should be passed also:
+   ```
+   dotnet build .
+   .\bin\Debug\net8.0\HiveToScripts.exe T:\logs T:\exp http://localhost:8550
+   ```
+3. The tool will generate:
+   - A l ist of tests and their containers is in`__tests.txt`
+   - '.ps1/.sh/.http' files, that are named after containers run by hive and big data alongside
+   - Configs and specifications extracted from container logs
+4. Start execution client, passing location of config and spec(use launchsettings.json for the runner or jsut pass the args):
+   ```nethermind -c T:\\output\\0c0ffee0.cfg --Init.ChainSpecPath=T:\\output\\0c0ffee0.json --log TRACE``
+5. Execute the corresponding scripts. GL!
