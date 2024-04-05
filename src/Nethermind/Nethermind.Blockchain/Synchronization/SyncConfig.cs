393c2d6bf424f22b76fec0d1c1361eced51680c3
Tanishq Jasoria <jasoriatanishq@gmail.com>
2024-04-05 11:09:29 +0530

    fix starting the client

--- src/Nethermind/Nethermind.Blockchain/Synchronization/SyncConfig.cs
+++ src/Nethermind/Nethermind.Blockchain/Synchronization/SyncConfig.cs
@@ -64,7 +64,7 @@ namespace Nethermind.Blockchain.Synchronization
         public bool ExitOnSynced { get; set; } = false;
         public int ExitOnSyncedWaitTimeSec { get; set; } = 60;
         public int MallocTrimIntervalSec { get; set; } = 300;
-        public bool? SnapServingEnabled { get; set; } = null;
+        public bool? SnapServingEnabled { get; set; } = false;
         public int MultiSyncModeSelectorLoopTimerMs { get; set; } = 1000;
         public bool TrieHealing { get; set; } = true;
         public bool VerkleSync { get; set; } = false;
