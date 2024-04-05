59f3fe9245d9017793d368c4860325db9755c0fa
Alexey Osipov <me@flcl.me>
2024-04-05 14:38:52 +0300

    Separate failed tests; handle container-less tests

--- tools/HiveToScripts/Program.cs
+++ tools/HiveToScripts/Program.cs
@@ -16,6 +16,7 @@ Example:
 ");
     return;
 }
+if (doLog) Console.WriteLine($"Running...");
 
 string hiveLogsDirectory = args.Length > 0 ? args[0] : "logs";
 
@@ -30,28 +31,68 @@ string jsonrpcUrl = args.Length > 2 ? args[2] : "http://localhost:8551";
 string[] simulatorFilePaths = Directory.GetFiles(hiveLogsDirectory, "*-simulator-*.log").Select(f => Path.Combine(outputDirectory, f)).ToArray();
 ConcurrentDictionary<string, TextWriter> fileDescriptors = new();
 
+if (Directory.Exists(outputDirectory))
+{
+    foreach (string f in Directory.EnumerateFiles(outputDirectory))
+    {
+        File.Delete(f);
+    }
+}
+Directory.CreateDirectory(outputDirectory);
+
+Regex starter = new("^>>\\s+(\\(([a-f0-9]+)\\)\\s)?");
+
+HashSet<string> successContainers = [];
+
+foreach (string simulatorFilePath in simulatorFilePaths)
+{
+    HashSet<string> currentSuccessContainers = [];
+    using StreamReader simulatorFile = File.OpenText(simulatorFilePath);
+
+    string? line;
+    // let's read line by line file that may be 2GB in size
+    while ((line = simulatorFile.ReadLine()) is not null)
+    {
+        Match match = starter.Match(line);
+        if (match.Success)
+        {
+            currentSuccessContainers.Add(match.Groups[2].Success ? match.Groups[2].Value : "default");
+            continue;
+        }
+        if (line.StartsWith("FAIL "))
+        {
+            currentSuccessContainers.Clear();
+            continue;
+        }
+        if (line.StartsWith("End test "))
+        {
+            foreach (var csc in currentSuccessContainers)
+            {
+                successContainers.Add(csc);
+            }
+            currentSuccessContainers.Clear();
+            continue;
+        }
+    }
+}
+
+string successOutputDirectory = Path.Combine(outputDirectory, "__ok");
+
+if (successContainers.Any())
+{
+    Directory.CreateDirectory(successOutputDirectory);
+}
+
+int fileCounter = 0;
+HashSet<string> createdFiles = [];
+
+using StreamWriter testListWriter = new(Path.Combine(outputDirectory, "__tests.txt"));
+HashSet<string> reportedContainers = [];
+
 foreach (string simulatorFilePath in simulatorFilePaths)
 {
     using StreamReader simulatorFile = File.OpenText(simulatorFilePath);
-
-    if (Directory.Exists(outputDirectory))
-    {
-        foreach (string f in Directory.EnumerateFiles(outputDirectory))
-        {
-            File.Delete(f);
-        }
-    }
-    Directory.CreateDirectory(outputDirectory);
-    if (doLog) Console.WriteLine($"Running...");
-    HashSet<string> createdFiles = [];
-
-    using StreamWriter testListWriter = new(Path.Combine(outputDirectory, "__tests.txt"));
-    HashSet<string> reportedContainers = [];
-
-    Regex starter = new("^>>\\s+\\(([a-f0-9]+)\\)\\s+");
-
     string? line;
-    int fileCounter = 0;
 
     // let's read line by line file that may be 2GB in size
     while ((line = simulatorFile.ReadLine()) is not null)
@@ -65,7 +106,7 @@ foreach (string simulatorFilePath in simulatorFilePaths)
             continue;
         }
 
-        string container = starter.Match(line).Groups[1].Value;
+        string container = starter.Match(line).Groups[2].Success ? starter.Match(line).Groups[2].Value : "default";
         if (!reportedContainers.Contains(container))
         {
             reportedContainers.Add(container);
@@ -74,14 +115,16 @@ foreach (string simulatorFilePath in simulatorFilePaths)
 
         string value = starter.Replace(line, "");
 
-        string filename = Path.Combine(outputDirectory, "{0}.{1}");
-        string cfgFilename = Path.Combine(outputDirectory, $"{container}.cfg");
-        string specFilename = Path.Combine(outputDirectory, $"{container}.json");
+        string currentDirectory = successContainers.Contains(container) ? successOutputDirectory : outputDirectory;
+
+        string filename = Path.Combine(currentDirectory, $"{container}.{{0}}");
+        string cfgFilename = Path.Combine(currentDirectory, $"{container}.cfg");
+        string specFilename = Path.Combine(currentDirectory, $"{container}.json");
 
         if (!createdFiles.Contains(cfgFilename))
         {
             createdFiles.Add(cfgFilename);
-            string elOutputFilenamePath = Directory.GetFiles(hiveLogsDirectory, $"client-{container}*.log", SearchOption.AllDirectories).Single();
+            string elOutputFilenamePath = Directory.GetFiles(hiveLogsDirectory, $"client-{(container == "default" ? "" : container)}*.log", SearchOption.AllDirectories).Single();
             if (elOutputFilenamePath is not null)
             {
                 elOutputFilenamePath = Path.Combine(hiveLogsDirectory, elOutputFilenamePath);
@@ -116,10 +159,10 @@ foreach (string simulatorFilePath in simulatorFilePaths)
 
         if (json.Length > 7500)
         {
-            string dataFile = string.Format(filename, container, $"data-{++fileCounter}.txt");
+            string dataFile = string.Format(filename, $"data-{++fileCounter}.txt");
             File.WriteAllText(dataFile, json);
 
-            fileDescriptors.GetOrAdd(string.Format(filename, container, "http"), OpenFile).Write(@$"
+            fileDescriptors.GetOrAdd(string.Format(filename, "http"), OpenFile).Write(@$"
 ###
 POST {jsonrpcUrl}
 Content-Type: application/json
@@ -127,19 +170,19 @@ Content-Type: application/json
 <@ {dataFile}
 ");
 
-            fileDescriptors.GetOrAdd(string.Format(filename, container, "ps1"), OpenFile).Write(@$"
+            fileDescriptors.GetOrAdd(string.Format(filename, "ps1"), OpenFile).Write(@$"
 $json = Get-Content {dataFile} -Raw
 Invoke-WebRequest -Method POST -Uri {jsonrpcUrl} -Headers @{{'content-type'='application/json'}} -Body $json | Select-Object -expand RawContent
 ");
 
-            fileDescriptors.GetOrAdd(string.Format(filename, container, "sh"), OpenFile).Write(@$"
+            fileDescriptors.GetOrAdd(string.Format(filename, "sh"), OpenFile).Write(@$"
 curl -s --request POST --url {jsonrpcUrl} --header 'content-type: application/json' --data '@{dataFile}'
 sleep 1; echo ""
 ");
         }
         else
         {
-            fileDescriptors.GetOrAdd(string.Format(filename, container, "http"), OpenFile).Write(@$"
+            fileDescriptors.GetOrAdd(string.Format(filename, "http"), OpenFile).Write(@$"
 ###
 POST {jsonrpcUrl}
 Content-Type: application/json
@@ -148,11 +191,11 @@ Content-Type: application/json
 ");
             json = json.Replace("\r", " ").Replace("\n", " ").Replace("'", "''");
 
-            fileDescriptors.GetOrAdd(string.Format(filename, container, "ps1"), OpenFile).Write(@$"
+            fileDescriptors.GetOrAdd(string.Format(filename, "ps1"), OpenFile).Write(@$"
 Invoke-WebRequest -Method POST -Uri {jsonrpcUrl} -Headers @{{'content-type'='application/json'}} -Body '{json}' | Select-Object -expand RawContent
 ");
 
-            fileDescriptors.GetOrAdd(string.Format(filename, container, "sh"), OpenFile).Write(@$"
+            fileDescriptors.GetOrAdd(string.Format(filename, "sh"), OpenFile).Write(@$"
 curl -s --request POST --url {jsonrpcUrl} --header 'content-type: application/json' --data '{json}'
 sleep 1; echo """"
 ");
