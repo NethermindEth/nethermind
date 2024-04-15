e79d9d5762e510c07e3cb3d7a166a93e0cb44dc5
lukasz.rozmej <lukasz.rozmej@gmail.com>
2024-04-15 13:17:46 +0200

    Increase Will_Trigger_ReorgBoundaryEvent_On_Prune delay to fix CI

--- src/Nethermind/Nethermind.Trie.Test/Pruning/TreeStoreTests.cs
+++ src/Nethermind/Nethermind.Trie.Test/Pruning/TreeStoreTests.cs
@@ -876,7 +876,7 @@ namespace Nethermind.Trie.Test.Pruning
 
                 if (i > 4)
                 {
-                    Assert.That(() => reorgBoundary, Is.EqualTo(i - 3).After(1000, 1));
+                    Assert.That(() => reorgBoundary, Is.EqualTo(i - 3).After(5000, 1));
                 }
                 else
                 {
