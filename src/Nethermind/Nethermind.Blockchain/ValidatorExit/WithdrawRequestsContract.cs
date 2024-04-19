9a41250756918eeec00bb5cd3b2311e6c6e27f10
MarekM25 <marekm2504@gmail.com>
2024-04-19 17:49:02 +0200

    wip

---
+++ src/Nethermind/Nethermind.Blockchain/ValidatorExit/WithdrawRequestsContract.cs
@@ -0,0 +1,32 @@
+﻿// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Reflection.Metadata;
+using Nethermind.Blockchain.Contracts;
+using Nethermind.Core.Specs;
+using Nethermind.Evm;
+using Nethermind.Evm.Tracing;
+using Nethermind.State;
+
+namespace Nethermind.Blockchain.ValidatorExit;
+
+public class WithdrawRequestsContract : Contract
+{
+    ValidatorExit[] CalculateValidatorExits(IReleaseSpec spec, IWorldState state)
+    {
+        CallOutputTracer tracer = new();
+
+        try
+        {
+            _transactionProcessor.Execute(transaction, new BlockExecutionContext(header), tracer);
+            result = tracer.ReturnValue;
+            return tracer.StatusCode == StatusCode.Success;
+        }
+        catch (Exception)
+        {
+            result = null;
+            return false;
+        }
+    }
+}
