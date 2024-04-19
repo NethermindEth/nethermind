00f7248e99fc8699de9ea1d6855463e6e4c429f5
Tanishq Jasoria <jasoriatanishq@gmail.com>
2024-04-19 19:57:46 +0530

    fix other solutions

--- src/Nethermind/Ethereum.Test.Base/TestBlockhashProvider.cs
+++ src/Nethermind/Ethereum.Test.Base/TestBlockhashProvider.cs
@@ -3,13 +3,15 @@
 
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
+using Nethermind.Core.Specs;
 using Nethermind.Evm;
+using Nethermind.State;
 
 namespace Ethereum.Test.Base
 {
     public class TestBlockhashProvider : IBlockhashProvider
     {
-        public Hash256 GetBlockhash(BlockHeader currentBlock, in long number)
+        public Hash256 GetBlockhash(BlockHeader currentBlock, in long number, IReleaseSpec spec, IWorldState stateProvider)
         {
             if (number != 0)
                 return Keccak.Zero;
--- src/Nethermind/Nethermind.Evm.Benchmark/TestBlockhashProvider.cs
+++ src/Nethermind/Nethermind.Evm.Benchmark/TestBlockhashProvider.cs
@@ -3,12 +3,14 @@
 
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
+using Nethermind.Core.Specs;
+using Nethermind.State;
 
 namespace Nethermind.Evm.Benchmark
 {
     public class TestBlockhashProvider : IBlockhashProvider
     {
-        public Hash256 GetBlockhash(BlockHeader currentBlock, in long number)
+        public Hash256 GetBlockhash(BlockHeader currentBlock, in long number, IReleaseSpec spec, IWorldState stateProvider)
         {
             return Keccak.Compute(number.ToString());
         }
