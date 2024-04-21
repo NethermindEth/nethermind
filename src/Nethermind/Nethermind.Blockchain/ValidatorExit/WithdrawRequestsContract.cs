4857702a38b56a0054e8eb2d2ef946b9bb2f0bdd
MarekM25 <marekm2504@gmail.com>
2024-04-21 12:25:12 +0200

    working on system call

--- src/Nethermind/Nethermind.Blockchain/ValidatorExit/WithdrawRequestsContract.cs
+++ src/Nethermind/Nethermind.Blockchain/ValidatorExit/WithdrawRequestsContract.cs
@@ -2,31 +2,57 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
+using System.Collections.Generic;
 using System.Reflection.Metadata;
 using Nethermind.Blockchain.Contracts;
+using Nethermind.Core;
 using Nethermind.Core.Specs;
+using Nethermind.Crypto;
 using Nethermind.Evm;
 using Nethermind.Evm.Tracing;
+using Nethermind.Evm.TransactionProcessing;
+using Nethermind.Int256;
 using Nethermind.State;
 
 namespace Nethermind.Blockchain.ValidatorExit;
 
-public class WithdrawRequestsContract : Contract
+public class WithdrawRequestsHandler
 {
-    ValidatorExit[] CalculateValidatorExits(IReleaseSpec spec, IWorldState state)
+    private readonly ITransactionProcessor _transactionProcessor;
+    private const long GasLimit = 30_000_000L;
+
+    public WithdrawRequestsHandler(
+        ITransactionProcessor transactionProcessor)
+    {
+        _transactionProcessor = transactionProcessor;
+    }
+    ValidatorExit[] CalculateValidatorExits(IReleaseSpec spec, IWorldState state, BlockHeader header)
     {
         CallOutputTracer tracer = new();
 
         try
         {
+            Transaction? transaction = new()
+            {
+                Value = UInt256.Zero,
+                Data = Array.Empty<byte>(),
+                To = spec.Eip7002ContractAddress, // ToDo set default address
+                SenderAddress = Address.SystemUser,
+                GasLimit = GasLimit,
+                GasPrice = UInt256.Zero,
+            };
+            transaction.Hash = transaction.CalculateHash();
+
             _transactionProcessor.Execute(transaction, new BlockExecutionContext(header), tracer);
-            result = tracer.ReturnValue;
-            return tracer.StatusCode == StatusCode.Success;
+            var result = tracer.ReturnValue;
+            var withdrawalRequests = new List<ValidatorExit>();
+
+
+            return withdrawalRequests.ToArray();
         }
         catch (Exception)
         {
-            result = null;
-            return false;
+            return null;
         }
     }
 }
