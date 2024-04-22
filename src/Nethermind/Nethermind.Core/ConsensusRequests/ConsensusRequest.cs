019261a99780e5a269408828a9672af0890f4381
MarekM25 <marekm2504@gmail.com>
2024-04-22 13:34:06 +0200

    add jsonIgnore

--- src/Nethermind/Nethermind.Core/ConsensusRequests/ConsensusRequest.cs
+++ src/Nethermind/Nethermind.Core/ConsensusRequests/ConsensusRequest.cs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 
+using System.Text.Json.Serialization;
+
 namespace Nethermind.Core.ConsensusRequests;
 
 public enum RequestsType
@@ -12,11 +14,24 @@ public enum RequestsType
 
 public class ConsensusRequest
 {
+    [JsonIgnore]
     public RequestsType Type { get; protected set; }
+
+    [JsonIgnore]
     public ulong AmountField { get; protected set; }
+
+    [JsonIgnore]
     public Address? SourceAddressField { get; protected set; }
+
+    [JsonIgnore]
     public byte[]? PubKeyField { get; set; }
+
+    [JsonIgnore]
     public byte[]? WithdrawalCredentialsField { get; protected set; }
+
+    [JsonIgnore]
     public byte[]? SignatureField { get; protected set; }
+
+    [JsonIgnore]
     public ulong? IndexField { get; protected set; }
 }
