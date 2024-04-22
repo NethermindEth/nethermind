0e55089cc006cd6f95ab4c5aa2787cc6c1b39427
MarekM25 <marekm2504@gmail.com>
2024-04-22 15:07:07 +0200

    cosmetic

--- src/Nethermind/Nethermind.Core/Eip2935Constants.cs
+++ src/Nethermind/Nethermind.Core/Eip2935Constants.cs
@@ -6,7 +6,7 @@ using Nethermind.Int256;
 namespace Nethermind.Core;
 
 /// <summary>
-/// Represents the <see href="https://eips.ethereum.org/EIPS/eip-2935#specification">EIP-4788</see> parameters.
+/// Represents the <see href="https://eips.ethereum.org/EIPS/eip-2935#specification">EIP-2935</see> parameters.
 /// </summary>
 public static class Eip2935Constants
 {
