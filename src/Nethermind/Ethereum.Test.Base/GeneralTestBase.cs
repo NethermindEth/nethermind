f044dfeea4c52760ea0fdce7704ca594fd7278ef
Tanishq Jasoria <jasoriatanishq@gmail.com>
2024-04-25 13:53:08 +0530

    fix ethereum tests

--- src/Nethermind/Ethereum.Test.Base/GeneralTestBase.cs
+++ src/Nethermind/Ethereum.Test.Base/GeneralTestBase.cs
@@ -66,7 +66,7 @@ namespace Ethereum.Test.Base
 
             TrieStore trieStore = new(stateDb, _logManager);
             WorldState stateProvider = new(trieStore, codeDb, _logManager);
-            IBlockhashProvider blockhashProvider = new TestBlockhashProvider(specProvider);
+            IBlockhashProvider blockhashProvider = new TestBlockhashProvider();
             IVirtualMachine virtualMachine = new VirtualMachine(
                 blockhashProvider,
                 specProvider,
--- src/Nethermind/Ethereum.Test.Base/TestBlockhashProvider.cs
+++ src/Nethermind/Ethereum.Test.Base/TestBlockhashProvider.cs
@@ -3,26 +3,17 @@
 
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
-using Nethermind.Core.Specs;
 using Nethermind.Evm;
-using Nethermind.State;
 
 namespace Ethereum.Test.Base
 {
     public class TestBlockhashProvider : IBlockhashProvider
     {
-        private readonly ISpecProvider _specProvider;
-        public TestBlockhashProvider(ISpecProvider specProvider)
-        {
-            _specProvider = specProvider;
-        }
-
         public Hash256 GetBlockhash(BlockHeader currentBlock, in long number)
         {
-            IReleaseSpec? spec = _specProvider.GetSpec(currentBlock);
-            return Keccak.Compute(spec.IsBlockHashInStateAvailable
-                ? (Eip2935Constants.RingBufferSize + number).ToString()
-                : (number).ToString());
+            if (number != 0)
+                return Keccak.Zero;
+            return Keccak.Compute(number.ToString());
         }
     }
 }
