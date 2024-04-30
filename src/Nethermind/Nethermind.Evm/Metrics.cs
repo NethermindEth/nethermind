d5c37a02d686325f6c01212d0c861f97ae9870b7
Ben Adams <thundercat@illyriad.co.uk>
2024-04-30 13:42:24 +0100

    Add mgaspersec to metrics reported to grafana (#6963)

--- src/Nethermind/Nethermind.Evm/Metrics.cs
+++ src/Nethermind/Nethermind.Evm/Metrics.cs
@@ -16,7 +16,6 @@ public class Metrics
     [Description("Number of EVM exceptions thrown by contracts.")]
     public static long EvmExceptions { get; set; }
 
-    [CounterMetric]
     [Description("Number of SELFDESTRUCT calls.")]
     public static long SelfDestructs { get; set; }
 
@@ -32,61 +31,50 @@ public class Metrics
     [Description("Number of SSTORE opcodes executed.")]
     public static long SstoreOpcode { get; set; }
 
-    [CounterMetric]
     [Description("Number of TLOAD opcodes executed.")]
     public static long TloadOpcode { get; set; }
 
-    [CounterMetric]
     [Description("Number of TSTORE opcodes executed.")]
     public static long TstoreOpcode { get; set; }
 
-    [CounterMetric]
     [Description("Number of MCOPY opcodes executed.")]
     public static long MCopyOpcode { get; set; }
 
-    [CounterMetric]
     [Description("Number of MODEXP precompiles executed.")]
     public static long ModExpOpcode { get; set; }
 
-    [CounterMetric]
     [Description("Number of BLOCKHASH opcodes executed.")]
     public static long BlockhashOpcode { get; set; }
 
-    [CounterMetric]
     [Description("Number of BN254_MUL precompile calls.")]
     public static long Bn254MulPrecompile { get; set; }
 
-    [CounterMetric]
     [Description("Number of BN254_ADD precompile calls.")]
     public static long Bn254AddPrecompile { get; set; }
 
-    [CounterMetric]
     [Description("Number of BN254_PAIRING precompile calls.")]
     public static long Bn254PairingPrecompile { get; set; }
 
-    [CounterMetric]
     [Description("Number of EC_RECOVERY precompile calls.")]
     public static long EcRecoverPrecompile { get; set; }
 
-    [CounterMetric]
     [Description("Number of MODEXP precompile calls.")]
     public static long ModExpPrecompile { get; set; }
 
-    [CounterMetric]
     [Description("Number of RIPEMD160 precompile calls.")]
     public static long Ripemd160Precompile { get; set; }
 
-    [CounterMetric]
     [Description("Number of SHA256 precompile calls.")]
     public static long Sha256Precompile { get; set; }
 
-    [CounterMetric]
     [Description("Number of Point Evaluation precompile calls.")]
     public static long PointEvaluationPrecompile { get; set; }
 
+    [CounterMetric]
     [Description("Number of calls made to addresses without code.")]
     public static long EmptyCalls { get; set; }
 
+    [CounterMetric]
     [Description("Number of contract create calls.")]
     public static long Creates { get; set; }
     internal static long Transactions { get; set; }
