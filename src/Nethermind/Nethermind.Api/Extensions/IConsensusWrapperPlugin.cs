5761fc5744ccb5001f7527896a058597dbbc5784
Amirul Ashraf <asdacap@gmail.com>
2024-05-02 12:37:52 +0800

    Address comment

--- src/Nethermind/Nethermind.Api/Extensions/IConsensusWrapperPlugin.cs
+++ src/Nethermind/Nethermind.Api/Extensions/IConsensusWrapperPlugin.cs
@@ -12,6 +12,11 @@ namespace Nethermind.Api.Extensions
     {
         Task<IBlockProducer> InitBlockProducer(IBlockProducerFactory baseBlockProducerFactory, IBlockProductionTrigger blockProductionTrigger, ITxSource? txSource);
 
+        /// <summary>
+        /// Priorities for ordering multiple plugin. Only used to determine the wrapping order of block production.
+        /// </summary>
+        int Priority => 0;
+
         bool Enabled { get; }
     }
 }
