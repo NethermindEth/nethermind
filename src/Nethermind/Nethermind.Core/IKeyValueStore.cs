c3579b5ccb47244dd0efc1feedd07258b27fcaf4
yerke26 <yerkebulan@nethermind.io>
2024-05-08 16:48:12 +0500

    changed to IsNull

--- src/Nethermind/Nethermind.Core/IKeyValueStore.cs
+++ src/Nethermind/Nethermind.Core/IKeyValueStore.cs
@@ -33,7 +33,7 @@ namespace Nethermind.Core
         bool KeyExists(ReadOnlySpan<byte> key)
         {
             Span<byte> span = GetSpan(key);
-            bool result = !span.IsNullOrEmpty();
+            bool result = !span.IsNull();
             DangerousReleaseMemory(span);
             return result;
         }
