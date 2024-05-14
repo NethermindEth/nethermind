f36f7bd73415594654cb2df2fd88bf2dd6be293a
Rohit Ranjan <rohitrjn629@gmail.com>
2024-05-14 12:01:59 +0530

    remove Uint64 extensions

---
+++ src/Nethermind/Ethereum.Blockchain.Pyspec.Test/PragueStateTests.cs
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Collections.Generic;
+using System.Linq;
+using Ethereum.Test.Base;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Ethereum.Blockchain.Pyspec.Test;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+[Explicit("Tests are not ready yet")]
+public class PragueStateTests : GeneralStateTestBase
+{
+    [TestCaseSource(nameof(LoadTests))]
+    public void Test(GeneralStateTest test) => RunTest(test).Pass.Should().BeTrue();
+
+    private static IEnumerable<GeneralStateTest> LoadTests()
+    {
+        TestsSourceLoader loader = new(new LoadPyspecTestsStrategy(), $"fixtures/state_tests/prague");
+        return loader.LoadTests().Cast<GeneralStateTest>();
+    }
+}
