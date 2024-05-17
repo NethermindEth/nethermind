18f43b77f9ec56c37d9856516d95f56bb95d0e74
Ahmad Bitar <smartprogrammer@windowslive.com>
2024-05-17 09:23:55 +0300

    Nehermind.light project added

---
+++ src/Nethermind/Nethermind.Light/Ethereum/Api/ApiBuilder.cs
@@ -0,0 +1,97 @@
+// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Nethermind.Api;
+using Nethermind.Api.Extensions;
+using Nethermind.ApiBase.Extensions;
+using Nethermind.Config;
+using Nethermind.Consensus;
+using Nethermind.Core;
+using Nethermind.JsonRpc.Data;
+using Nethermind.Logging;
+using Nethermind.Serialization.Json;
+using Nethermind.Specs.ChainSpecStyle;
+using ILogger = Nethermind.Logging.InterfaceLogger;
+
+namespace Nethermind.Runner.Ethereum.Api
+{
+    public class ApiBuilder
+    {
+        private readonly IConfigProvider _configProvider;
+        private readonly IJsonSerializer _jsonSerializer;
+        private readonly ILogManager _logManager;
+        private readonly Nethermind.Logging.ILogger _logger;
+        private readonly IInitConfig _initConfig;
+
+        public ApiBuilder(IConfigProvider configProvider, ILogManager logManager)
+        {
+            _logManager = logManager ?? throw new ArgumentNullException(nameof(logManager));
+            _logger = _logManager.GetClassLogger();
+            _configProvider = configProvider ?? throw new ArgumentNullException(nameof(configProvider));
+            _initConfig = configProvider.GetConfig<IInitConfig>();
+            _jsonSerializer = new EthereumJsonSerializer();
+        }
+
+        public INethermindApi Create(params IConsensusPlugin[] consensusPlugins) =>
+            Create((IEnumerable<IConsensusPlugin>)consensusPlugins);
+
+        public INethermindApi Create(IEnumerable<IConsensusPlugin> consensusPlugins)
+        {
+            ChainSpec chainSpec = LoadChainSpec(_jsonSerializer);
+            bool wasCreated = Interlocked.CompareExchange(ref _apiCreated, 1, 0) == 1;
+            if (wasCreated)
+            {
+                throw new NotSupportedException("Creation of multiple APIs not supported.");
+            }
+
+            string engine = chainSpec.SealEngineType;
+            IConsensusPlugin? enginePlugin = consensusPlugins.FirstOrDefault(p => p.SealEngineType == engine);
+
+            INethermindApi nethermindApi =
+                enginePlugin?.CreateApi(_configProvider, _jsonSerializer, _logManager, chainSpec) as INethermindApi ??
+                new NethermindApi(_configProvider, _jsonSerializer, _logManager, chainSpec);
+            nethermindApi.SealEngineType = engine;
+            nethermindApi.SpecProvider = new ChainSpecBasedSpecProvider(chainSpec, _logManager);
+            nethermindApi.GasLimitCalculator = new FollowOtherMiners(nethermindApi.SpecProvider);
+
+            SetLoggerVariables(chainSpec);
+
+            return nethermindApi;
+        }
+
+        private int _apiCreated;
+
+        private ChainSpec LoadChainSpec(IJsonSerializer ethereumJsonSerializer)
+        {
+            bool hiveEnabled = Environment.GetEnvironmentVariable("NETHERMIND_HIVE_ENABLED")?.ToLowerInvariant() == "true";
+            bool hiveChainSpecExists = File.Exists(_initConfig.HiveChainSpecPath);
+
+            string chainSpecFile;
+            if (hiveEnabled && hiveChainSpecExists)
+                chainSpecFile = _initConfig.HiveChainSpecPath;
+            else
+                chainSpecFile = _initConfig.ChainSpecPath;
+
+            if (_logger.IsDebug) _logger.Debug($"Loading chain spec from {chainSpecFile}");
+
+            ThisNodeInfo.AddInfo("Chainspec    :", $"{chainSpecFile}");
+
+            IChainSpecLoader loader = new ChainSpecLoader(ethereumJsonSerializer);
+            ChainSpec chainSpec = loader.LoadEmbeddedOrFromFile(chainSpecFile, _logger);
+            TransactionForRpc.DefaultChainId = chainSpec.ChainId;
+            return chainSpec;
+        }
+
+        private void SetLoggerVariables(ChainSpec chainSpec)
+        {
+            _logManager.SetGlobalVariable("chain", chainSpec.Name);
+            _logManager.SetGlobalVariable("chainId", chainSpec.ChainId);
+            _logManager.SetGlobalVariable("engine", chainSpec.SealEngineType);
+        }
+    }
+}
---
+++ src/Nethermind/Nethermind.Light/Ethereum/Steps/StartRpc.cs
@@ -0,0 +1,108 @@
+// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Nethermind.Api;
+using Nethermind.Core;
+using Nethermind.Core.Authentication;
+using Nethermind.Init.Steps;
+using Nethermind.JsonRpc;
+using Nethermind.JsonRpc.Modules;
+using Nethermind.JsonRpc.WebSockets;
+using Nethermind.Logging;
+using Nethermind.Runner.JsonRpc;
+using Nethermind.Serialization.Json;
+
+namespace Nethermind.Runner.Ethereum.Steps
+{
+    [RunnerStepDependencies(typeof(InitializeNetwork), typeof(RegisterRpcModules), typeof(RegisterPluginRpcModules))]
+    public class StartRpc : IStep
+    {
+        private readonly INethermindApi _api;
+
+        public StartRpc(INethermindApi api)
+        {
+            _api = api;
+        }
+
+        public async Task Execute(CancellationToken cancellationToken)
+        {
+            IJsonRpcConfig jsonRpcConfig = _api.Config<IJsonRpcConfig>();
+            ILogger logger = _api.LogManager.GetClassLogger();
+
+            if (jsonRpcConfig.Enabled)
+            {
+                IInitConfig initConfig = _api.Config<IInitConfig>();
+                IJsonRpcUrlCollection jsonRpcUrlCollection = new JsonRpcUrlCollection(_api.LogManager, jsonRpcConfig, initConfig.WebSocketsEnabled);
+
+                IRpcModuleProvider rpcModuleProvider = _api.RpcModuleProvider!;
+                JsonRpcService jsonRpcService = new(rpcModuleProvider, _api.LogManager, jsonRpcConfig);
+
+                IJsonSerializer jsonSerializer = new EthereumJsonSerializer();
+                IRpcAuthentication auth = jsonRpcConfig.UnsecureDevNoRpcAuthentication || !jsonRpcUrlCollection.Values.Any(u => u.IsAuthenticated)
+                    ? NoAuthentication.Instance
+                    : JwtAuthentication.FromFile(jsonRpcConfig.JwtSecretFile, _api.Timestamper, logger);
+
+
+                JsonRpcProcessor jsonRpcProcessor = new(
+                    jsonRpcService,
+                    jsonRpcConfig,
+                    _api.FileSystem,
+                    _api.LogManager);
+
+
+                if (initConfig.WebSocketsEnabled)
+                {
+                    JsonRpcWebSocketsModule webSocketsModule = new(
+                        jsonRpcProcessor,
+                        jsonRpcService,
+                        _api.JsonRpcLocalStats!,
+                        _api.LogManager,
+                        jsonSerializer,
+                        jsonRpcUrlCollection,
+                        auth,
+                        jsonRpcConfig.MaxBatchResponseBodySize);
+
+                    _api.WebSocketsManager!.AddModule(webSocketsModule, true);
+                }
+
+                Bootstrap.Instance.JsonRpcService = jsonRpcService;
+                Bootstrap.Instance.LogManager = _api.LogManager;
+                Bootstrap.Instance.JsonSerializer = jsonSerializer;
+                Bootstrap.Instance.JsonRpcLocalStats = _api.JsonRpcLocalStats!;
+                Bootstrap.Instance.JsonRpcAuthentication = auth;
+
+                JsonRpcRunner? jsonRpcRunner = new(
+                    jsonRpcProcessor,
+                    jsonRpcUrlCollection,
+                    _api.WebSocketsManager!,
+                    _api.ConfigProvider,
+                    auth,
+                    _api.LogManager,
+                    _api);
+
+                await jsonRpcRunner.Start(cancellationToken).ContinueWith(x =>
+                {
+                    if (x.IsFaulted && logger.IsError)
+                        logger.Error("Error during jsonRpc runner start", x.Exception);
+                }, cancellationToken);
+
+                JsonRpcIpcRunner jsonIpcRunner = new(jsonRpcProcessor, _api.ConfigProvider,
+                    _api.LogManager, _api.JsonRpcLocalStats!, jsonSerializer, _api.FileSystem);
+                jsonIpcRunner.Start(cancellationToken);
+
+#pragma warning disable 4014
+                _api.DisposeStack.Push(
+                    new Reactive.AnonymousDisposable(() => jsonRpcRunner.StopAsync())); // do not await
+                _api.DisposeStack.Push(jsonIpcRunner); // do not await
+#pragma warning restore 4014
+            }
+            else
+            {
+                if (logger.IsInfo) logger.Info("Json RPC is disabled");
+            }
+        }
+    }
+}
