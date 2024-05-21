4de42a32885c78f4a8d6ba2fe75c112cd2e233dd
lukasz.rozmej <lukasz.rozmej@gmail.com>
2024-05-22 01:21:50 +0200

    add object pool, fix sharing state by warm-up processing

--- src/Nethermind/Nethermind.Consensus/Processing/IBlockCachePreWarmer.cs
+++ src/Nethermind/Nethermind.Consensus/Processing/IBlockCachePreWarmer.cs
@@ -1,14 +1,15 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.ObjectPool;
 using Microsoft.FSharp.Collections;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Evm;
 using Nethermind.Evm.Tracing;
 using Nethermind.Evm.TransactionProcessing;
-using Nethermind.State;
 
 namespace Nethermind.Consensus.Processing;
 
@@ -19,14 +20,28 @@ public interface IBlockCachePreWarmer
 
 public class BlockCachePreWarmer(ReadOnlyTxProcessingEnvFactory envFactory) : IBlockCachePreWarmer
 {
+    private readonly ObjectPool<ReadOnlyTxProcessingEnv> _envPool = new DefaultObjectPool<ReadOnlyTxProcessingEnv>(new ReadOnlyTxProcessingEnvPooledObjectPolicy(envFactory), Environment.ProcessorCount);
+
     public void PreWarmCaches(Block suggestedBlock, Hash256 parentStateRoot)
     {
-        ReadOnlyTxProcessingEnv env = envFactory.Create();
         Parallel.ForEach(suggestedBlock.Transactions, tx =>
         {
-            using IReadOnlyTransactionProcessor? transactionProcessor = env.Build(parentStateRoot);
-            transactionProcessor.Execute(tx, new BlockExecutionContext(suggestedBlock.Header.Clone()), NullTxTracer.Instance);
+            ReadOnlyTxProcessingEnv env = _envPool.Get();
+            try
+            {
+                using IReadOnlyTransactionProcessor? transactionProcessor = env.Build(parentStateRoot);
+                transactionProcessor.Trace(tx, new BlockExecutionContext(suggestedBlock.Header.Clone()), NullTxTracer.Instance);
+            }
+            finally
+            {
+                _envPool.Return(env);
+            }
         });
+    }
 
+    private class ReadOnlyTxProcessingEnvPooledObjectPolicy(ReadOnlyTxProcessingEnvFactory envFactory) : IPooledObjectPolicy<ReadOnlyTxProcessingEnv>
+    {
+        public ReadOnlyTxProcessingEnv Create() => envFactory.Create();
+        public bool Return(ReadOnlyTxProcessingEnv obj) => true;
     }
 }
