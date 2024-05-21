6e46f06e4161e0ac6150a8e9fd9963021f45243d
Rohit Ranjan <rohitrjn629@gmail.com>
2024-05-21 11:44:24 +0530

    add withdrawal modal test case

--- src/Nethermind/Nethermind.Evm.Test/WithdrawalRequestProcessorTest.cs
+++ src/Nethermind/Nethermind.Evm.Test/WithdrawalRequestProcessorTest.cs
@@ -4,27 +4,57 @@
 using System.Linq;
 using FluentAssertions;
 using Nethermind.Consensus.Requests;
+using Nethermind.Core;
 using Nethermind.Core.ConsensusRequests;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
 using Nethermind.Core.Specs;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Crypto;
+using Nethermind.Db;
 using Nethermind.Evm;
 using Nethermind.Evm.TransactionProcessing;
 using Nethermind.Int256;
 using Nethermind.Logging;
 using Nethermind.Specs;
-using Nethermind.Specs.Test;
 using Nethermind.State;
+using Nethermind.Trie.Pruning;
 using NSubstitute;
 using NUnit.Framework;
 
 
-namespace Nethermind.Consensus.Test;
+namespace Nethermind.Evm.Test;
 
 public class WithdrawalRequestProcessorTests
 {
+
+    private ISpecProvider _specProvider;
+    private IEthereumEcdsa _ethereumEcdsa;
+    private TransactionProcessor _transactionProcessor;
+    private IWorldState _stateProvider;
+
+    private static readonly UInt256 AccountBalance = 1.Ether();
+
+    private readonly Address eip7002Account = new(Bytes.FromHexString("0x00a3ca265ebcb825b45f985a16cefb49958ce017"));
+
+    [SetUp]
+    public void Setup()
+    {
+        _specProvider = MainnetSpecProvider.Instance;
+        MemDb stateDb = new();
+        TrieStore trieStore = new(stateDb, LimboLogs.Instance);
+        _stateProvider = new WorldState(trieStore, new MemDb(), LimboLogs.Instance);
+        _stateProvider.CreateAccount(eip7002Account, AccountBalance);
+        _stateProvider.Commit(_specProvider.GenesisSpec);
+        _stateProvider.CommitTree(0);
+        _stateProvider.StateRoot =new Hash256(Bytes.FromHexString("0x2b4d2dada0e987cf9ad952f7a0f962c371073a2900d42da2778a4f8087d66811"));
+
+        VirtualMachine virtualMachine = new(new TestBlockhashProvider(_specProvider), _specProvider, LimboLogs.Instance);
+        _transactionProcessor = new TransactionProcessor(_specProvider, _stateProvider, virtualMachine, LimboLogs.Instance);
+        _ethereumEcdsa = new EthereumEcdsa(_specProvider.ChainId, LimboLogs.Instance);
+    }
+
+
     [Test]
     [Explicit("This test is not yet implemented")]
     public void ShouldProcessWithdrawalRequest()
@@ -32,9 +62,7 @@ public class WithdrawalRequestProcessorTests
 
         IReleaseSpec spec = Substitute.For<IReleaseSpec>();
         spec.IsEip7002Enabled.Returns(true);
-        spec.Eip7002ContractAddress.Returns(
-            new Core.Address(Bytes.FromHexString("0x00a3ca265ebcb825b45f985a16cefb49958ce017"))
-        );
+        spec.Eip7002ContractAddress.Returns(eip7002Account);
 
         Core.Block block = Build.A.Block
                 .WithHeader(
@@ -74,36 +102,16 @@ public class WithdrawalRequestProcessorTests
                 )
                 .TestObject;
 
-        IWorldState state = Substitute.For<IWorldState>();
-        state.StateRoot.Returns(new Hash256(Bytes.FromHexString("0x2b4d2dada0e987cf9ad952f7a0f962c371073a2900d42da2778a4f8087d66811")));
-        state.AccountExists(spec.Eip7002ContractAddress).Returns(true);
-
-        ILogManager logManager = Substitute.For<ILogManager>();
-        
-
-        ISpecProvider specProvider = MainnetSpecProvider.Instance;
-        IBlockhashProvider blockhashProvider = Substitute.For<IBlockhashProvider>();
-
-        IVirtualMachine virtualMachine = new VirtualMachine(
-            blockhashProvider,
-            specProvider,
-            logManager
-        );
-
-        var ecdsa = new EthereumEcdsa(1, logManager);
-
-        WithdrawalRequestsProcessor withdrawalRequestsProcessor = new(
-            new SystemTxProcessor(spec, state, virtualMachine, ecdsa, logManager.GetClassLogger())
-        );
+        WithdrawalRequestsProcessor withdrawalRequestsProcessor = new(transactionProcessor: _transactionProcessor);
 
         var withdrawalRequest = new WithdrawalRequest()
         {
-            SourceAddress = new Core.Address(Bytes.FromHexString("0x0000000000000000000000000000000000000200")),
+            SourceAddress = new Address(Bytes.FromHexString("0x0000000000000000000000000000000000000200")),
             ValidatorPubkey = Bytes.FromHexString("000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001"),
             Amount = 0
         };
 
-        var withdrawalRequests = withdrawalRequestsProcessor.ReadWithdrawalRequests(spec, state, block).ToList();
+        var withdrawalRequests = withdrawalRequestsProcessor.ReadWithdrawalRequests(spec, _stateProvider, block).ToList();
 
         Assert.That(withdrawalRequests, Has.Count.EqualTo(1));
 
