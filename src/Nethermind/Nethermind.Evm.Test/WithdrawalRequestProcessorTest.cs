b125a2ae98220f8018350437d86c927ec0e44543
Rohit Ranjan <rohitrjn629@gmail.com>
2024-05-22 11:06:25 +0530

    add withdrawal request processor test

--- src/Nethermind/Nethermind.Evm.Test/WithdrawalRequestProcessorTest.cs
+++ src/Nethermind/Nethermind.Evm.Test/WithdrawalRequestProcessorTest.cs
@@ -6,13 +6,12 @@ using FluentAssertions;
 using Nethermind.Consensus.Requests;
 using Nethermind.Core;
 using Nethermind.Core.ConsensusRequests;
-using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
 using Nethermind.Core.Specs;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Crypto;
 using Nethermind.Db;
-using Nethermind.Evm;
+using Nethermind.Evm.Tracing;
 using Nethermind.Evm.TransactionProcessing;
 using Nethermind.Int256;
 using Nethermind.Logging;
@@ -30,12 +29,12 @@ public class WithdrawalRequestProcessorTests
 
     private ISpecProvider _specProvider;
     private IEthereumEcdsa _ethereumEcdsa;
-    private TransactionProcessor _transactionProcessor;
+    private ITransactionProcessor _transactionProcessor;
     private IWorldState _stateProvider;
 
     private static readonly UInt256 AccountBalance = 1.Ether();
 
-    private readonly Address eip7002Account = new(Bytes.FromHexString("0x00a3ca265ebcb825b45f985a16cefb49958ce017"));
+    private readonly Address eip7002Account = Eip7002Constants.WithdrawalRequestPredeployAddress;
 
     [SetUp]
     public void Setup()
@@ -47,16 +46,24 @@ public class WithdrawalRequestProcessorTests
         _stateProvider.CreateAccount(eip7002Account, AccountBalance);
         _stateProvider.Commit(_specProvider.GenesisSpec);
         _stateProvider.CommitTree(0);
-        _stateProvider.StateRoot =new Hash256(Bytes.FromHexString("0x2b4d2dada0e987cf9ad952f7a0f962c371073a2900d42da2778a4f8087d66811"));
 
         VirtualMachine virtualMachine = new(new TestBlockhashProvider(_specProvider), _specProvider, LimboLogs.Instance);
-        _transactionProcessor = new TransactionProcessor(_specProvider, _stateProvider, virtualMachine, LimboLogs.Instance);
+
+        _transactionProcessor = Substitute.For<ITransactionProcessor>();
+
+        _transactionProcessor.Execute(Arg.Any<Transaction>(), Arg.Any<BlockExecutionContext>(), Arg.Any<CallOutputTracer>())
+            .Returns(ci =>
+            {
+                CallOutputTracer tracer = ci.Arg<CallOutputTracer>();
+                tracer.ReturnValue = Bytes.FromHexString("a94f5374fce5edbc8e2a8697c15331677e6ebf0b0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002ffffffffffffffffa94f5374fce5edbc8e2a8697c15331677e6ebf0b0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004ffffffffffffffffa94f5374fce5edbc8e2a8697c15331677e6ebf0b0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000050000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006ffffffffffffffffa94f5374fce5edbc8e2a8697c15331677e6ebf0b0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000070000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008ffffffffffffffffa94f5374fce5edbc8e2a8697c15331677e6ebf0b0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000090000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000affffffffffffffffa94f5374fce5edbc8e2a8697c15331677e6ebf0b00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b0000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000cffffffffffffffffa94f5374fce5edbc8e2a8697c15331677e6ebf0b00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000d0000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000effffffffffffffffa94f5374fce5edbc8e2a8697c15331677e6ebf0b00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000f0000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010ffffffffffffffff");
+                return new TransactionResult();
+            });
+
         _ethereumEcdsa = new EthereumEcdsa(_specProvider.ChainId, LimboLogs.Instance);
     }
 
 
     [Test]
-    [Explicit("This test is not yet implemented")]
     public void ShouldProcessWithdrawalRequest()
     {
 
@@ -64,56 +71,20 @@ public class WithdrawalRequestProcessorTests
         spec.IsEip7002Enabled.Returns(true);
         spec.Eip7002ContractAddress.Returns(eip7002Account);
 
-        Core.Block block = Build.A.Block
-                .WithHeader(
-                    Build.A.BlockHeader
-                        .WithParentHash(
-                            new Hash256(Bytes.FromHexString("0x825d0a25f8989de79a07be308f626a275930df1d1a41df7211ed67ce0cacb1c7"))
-                        )
-                        .WithUnclesHash(
-                            new Hash256(Bytes.FromHexString("0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347"))
-                        )
-                        .WithBeneficiary(
-                            new Core.Address(Bytes.FromHexString("0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba"))
-                        )
-                        .WithDifficulty(UInt256.Zero)
-                        .WithNumber(1)
-                        .WithGasLimit(100000000000000000)
-                        .WithTimestamp(12)
-                        .WithHash(
-                            new Hash256(Bytes.FromHexString("0x4f01465e6b7a381227d736639f72e645b7023a223bb4e1df7fcd11555bb789be"))
-                        )
-                        .WithMixHash(
-                            new Hash256("0x0000000000000000000000000000000000000000000000000000000000000000")
-                        )
-                        .WithReceiptsRoot(
-                            new Hash256(Bytes.FromHexString("0x12a36f1d9611048f267288cd54c8c3f96cdfcdbaf3547eb91cfa71012c6b4120"))
-                        )
-                        .WithRequestsRoot(
-                            new Hash256(Bytes.FromHexString("0xe922b6c8c2a1aef3e0886455d4b791e8351d6213d38814aa5a1170e5e1af59a1"))
-                        )
-                        .WithWithdrawalsRoot(
-                            new Hash256(Bytes.FromHexString("0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"))
-                        )
-                        .WithTransactionsRoot(
-                            new Hash256(Bytes.FromHexString("0x6415cfd997ac8058810eb6866dfcf25f23da303840f34adddf68bef6c95dbf5d"))
-                        )
-                    .TestObject
-                )
-                .TestObject;
+        Block block = Build.A.Block.TestObject;
 
         WithdrawalRequestsProcessor withdrawalRequestsProcessor = new(transactionProcessor: _transactionProcessor);
 
         var withdrawalRequest = new WithdrawalRequest()
         {
-            SourceAddress = new Address(Bytes.FromHexString("0x0000000000000000000000000000000000000200")),
+            SourceAddress = new Address(Bytes.FromHexString("0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b")),
             ValidatorPubkey = Bytes.FromHexString("000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001"),
             Amount = 0
         };
 
         var withdrawalRequests = withdrawalRequestsProcessor.ReadWithdrawalRequests(spec, _stateProvider, block).ToList();
 
-        Assert.That(withdrawalRequests, Has.Count.EqualTo(1));
+        Assert.That(withdrawalRequests, Has.Count.EqualTo(16));
 
         WithdrawalRequest withdrawalRequestResult = withdrawalRequests[0];
 
