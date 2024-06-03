57443b07e94f2795a2119def61e92c68f8015d73
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-06-03 17:26:19 +0100

    fix merge block producer

--- src/Nethermind/Nethermind.Merge.Plugin/MergePlugin.BlockProducer.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/MergePlugin.BlockProducer.cs
@@ -54,7 +54,7 @@ namespace Nethermind.Merge.Plugin
                     ? baseBlockProducerFactory.InitBlockProducer(txSource)
                     : null;
                 _manualTimestamper ??= new ManualTimestamper();
-                BlockProducerEnv blockProducerEnv = _api.BlockProducerEnvFactory.Create();
+                BlockProducerEnv blockProducerEnv = CreateBlockProducerEnv();
 
                 _api.SealEngine = new MergeSealEngine(_api.SealEngine, _poSSwitcher, _api.SealValidator, _api.LogManager);
                 _api.Sealer = _api.SealEngine;
