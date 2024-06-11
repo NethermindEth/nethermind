9830ab38d3720d2158e6b6d4c2e5b622f57f54f7
Alex Bespalov <alexb5dh@gmail.com>
2024-06-11 22:32:54 +0300

    Update and fix tests

--- src/Nethermind/Nethermind.Evm.Test/Secp256r1PrecompilePrecompileTests.cs
+++ src/Nethermind/Nethermind.Evm.Test/Secp256r1PrecompilePrecompileTests.cs
@@ -17,10 +17,6 @@ namespace Nethermind.Evm.Test
             "0000000000000000000000000000000000000000000000000000000000000001"
         );
 
-        private static readonly byte[] InvalidAnswer = Bytes.FromHexString(
-            "0000000000000000000000000000000000000000000000000000000000000000"
-        );
-
         [Test] // https://github.com/paradigmxyz/alphanet/blob/main/crates/precompile/src/secp256r1.rs#L137
         [TestCase(
             "4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e",
@@ -67,21 +63,25 @@ namespace Nethermind.Evm.Test
             var bytes = Bytes.FromHexString(input);
             (ReadOnlyMemory<byte> output, bool success) = Secp256r1Precompile.Instance.Run(bytes, Prague.Instance);
             success.Should().BeTrue();
-            output.ToArray().Should().BeEquivalentTo(isValid ? ValidAnswer : InvalidAnswer);
+            output.ToArray().Should().BeEquivalentTo(isValid ? ValidAnswer : []);
         }
 
         [Test]
+        [TestCase(
+            ""
+        )]
         [TestCase(
             "4cee90eb86eaa050036147a12d49004b6a"
         )]
         [TestCase(
             "4cee90eb86eaa050036147a12d49004b6a958b991cfd78f16537fe6d1f4afd10273384db08bdfc843562a22b0626766686f6aec8247599f40bfe01bec0e0ecf17b4319559022d4d9bf007fe929943004eb4866760dedf319"
         )]
-        public void Fails_On_Incorrect_Input(string input)
+        public void Produces_Empty_Output_On_Invalid_Input(string input)
         {
             var bytes = Bytes.FromHexString(input);
-            (ReadOnlyMemory<byte> _, bool success) = Secp256r1Precompile.Instance.Run(bytes, Prague.Instance);
-            success.Should().BeFalse();
+            (ReadOnlyMemory<byte> output, bool success) = Secp256r1Precompile.Instance.Run(bytes, Prague.Instance);
+            success.Should().BeTrue();
+            output.Should().Be(ReadOnlyMemory<byte>.Empty);
         }
     }
 }
