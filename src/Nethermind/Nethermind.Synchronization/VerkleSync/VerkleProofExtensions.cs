d5af02662ec5f02b0eabdae40f9381f8cae70e69
Tanishq Jasoria <jasoriatanishq@gmail.com>
2024-06-14 12:51:31 +0530

    Add tests

--- src/Nethermind/Nethermind.Synchronization/VerkleSync/VerkleProofExtensions.cs
+++ src/Nethermind/Nethermind.Synchronization/VerkleSync/VerkleProofExtensions.cs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText:2023 Demerzel Solutions Limited
 // SPDX-License-Identifier:LGPL-3.0-only
 
+using System;
+using System.Runtime.InteropServices.ComTypes;
 using Nethermind.Core.Verkle;
 using Nethermind.Serialization.Rlp;
 using Nethermind.Verkle.Tree.Serializers;
@@ -11,8 +13,11 @@ public static class VerkleProofExtensions
 {
     public static byte[] EncodeRlp(this VerkleProof proof)
     {
+        Console.WriteLine($"THis is encoding rlp for proofs");
+        if (proof.CommsSorted.Length == 0) return Array.Empty<byte>();
         VerkleProofSerializer ser = VerkleProofSerializer.Instance;
         var encodedData = new RlpStream(Rlp.LengthOfSequence(ser.GetLength(proof, RlpBehaviors.None)));
+        Console.WriteLine($"THis is encoding rlp for proofs - actual encoding");
         ser.Encode(encodedData, proof);
         return encodedData.Data.ToArray()!;
     }
