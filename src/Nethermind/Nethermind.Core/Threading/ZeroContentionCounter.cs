c5011954efd940f052804b248cfda3cc4fc46d05
Ben Adams <thundercat@illyriad.co.uk>
2024-06-20 22:38:10 +0100

    Use typed compiled expression

--- src/Nethermind/Nethermind.Core/Threading/ZeroContentionCounter.cs
+++ src/Nethermind/Nethermind.Core/Threading/ZeroContentionCounter.cs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading;
 
@@ -9,24 +11,75 @@ public class ZeroContentionCounter
 {
     private ThreadLocal<BoxedLong> _threadLocal = new(() => new BoxedLong(), trackAllValues: true);
 
-    private static FieldInfo _linkedSlot = typeof(ThreadLocal<BoxedLong>).GetField("_linkedSlot", BindingFlags.NonPublic | BindingFlags.Instance)!;
-    private static FieldInfo _next = _linkedSlot.FieldType.GetField("_next", BindingFlags.NonPublic | BindingFlags.Instance)!;
-    private static FieldInfo _value = _next.FieldType.GetField("_value", BindingFlags.NonPublic | BindingFlags.Instance)!;
+    private static Func<ThreadLocal<BoxedLong>, long> _totalDelegate = CreateTotalDelegate();
 
     public long GetTotalValue()
     {
-        object? linkedSlot = _linkedSlot.GetValue(_threadLocal);
-        if (linkedSlot == null)
-        {
-            return 0;
-        }
+        return _totalDelegate(_threadLocal);
+    }
 
-        long total = 0;
-        for (linkedSlot = _next.GetValue(linkedSlot); linkedSlot != null; linkedSlot = _next.GetValue(linkedSlot))
-        {
-            total += (_value.GetValue(linkedSlot) as BoxedLong)!.Value;
-        }
-        return total;
+    private static Func<ThreadLocal<BoxedLong>, long> CreateTotalDelegate()
+    {
+        FieldInfo linkedSlot = typeof(ThreadLocal<BoxedLong>).GetField("_linkedSlot", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        FieldInfo next = linkedSlot.FieldType.GetField("_next", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        FieldInfo value = next.FieldType.GetField("_value", BindingFlags.NonPublic | BindingFlags.Instance)!;
+
+        // Parameters
+        ParameterExpression threadLocalParam = Expression.Parameter(typeof(ThreadLocal<BoxedLong>), "threadLocal");
+
+        // Fields
+        MemberExpression linkedSlotField = Expression.Field(threadLocalParam, linkedSlot);
+
+        // Variables
+        ParameterExpression linkedSlotVar = Expression.Variable(linkedSlotField.Type, "linkedSlot");
+        ParameterExpression totalVar = Expression.Variable(typeof(long), "total");
+
+        // Assignments
+        BinaryExpression assignLinkedSlot = Expression.Assign(linkedSlotVar, linkedSlotField);
+        BinaryExpression assignTotal = Expression.Assign(totalVar, Expression.Constant(0L));
+        BinaryExpression assignNextSlot = Expression.Assign(linkedSlotVar, Expression.Field(linkedSlotVar, next));
+
+        // Labels
+        LabelTarget breakLabel = Expression.Label(typeof(long), "breakLabel");
+
+        ConditionalExpression breakCondition =
+            Expression.IfThen(
+                Expression.Equal(linkedSlotVar, Expression.Constant(null)),
+                Expression.Break(breakLabel, totalVar)
+            );
+
+        // Loop body
+        BlockExpression loopBody = Expression.Block(
+            breakCondition,
+            Expression.AddAssign(
+                totalVar,
+                Expression.Property(
+                    Expression.Field(linkedSlotVar, value), typeof(BoxedLong),
+                    nameof(BoxedLong.Value)
+                )
+            ),
+            assignNextSlot
+        );
+
+        // Loop
+        LoopExpression loop = Expression.Loop(loopBody);
+
+        // Block
+        BlockExpression block = Expression.Block(
+            new[] { linkedSlotVar, totalVar },
+            assignLinkedSlot,
+            assignTotal,
+            breakCondition,
+            assignNextSlot,
+            loop,
+            Expression.Label(breakLabel, totalVar)
+        );
+
+        // Lambda
+        Expression<Func<ThreadLocal<BoxedLong>, long>> lambda =
+            Expression.Lambda<Func<ThreadLocal<BoxedLong>, long>>(block, threadLocalParam);
+
+        return lambda.Compile();
     }
 
     public void Increment(int value = 1) => _threadLocal.Value!.Increment(value);
@@ -35,7 +88,7 @@ public class ZeroContentionCounter
     private class BoxedLong
     {
         private long _value;
-        public ref long Value => ref _value;
+        public long Value => _value;
         public void Increment(int value) => _value += value;
     }
 }
