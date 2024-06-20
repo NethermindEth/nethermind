280638e12a9ccc02ee3946d86808461263f2a477
Alexey Osipov <me@flcl.me>
2024-06-20 14:27:25 +0300

    Compilation fix

--- src/Nethermind/Nethermind.Optimism/OptimismPlugin.cs
+++ src/Nethermind/Nethermind.Optimism/OptimismPlugin.cs
@@ -177,7 +177,7 @@ public class OptimismPlugin : IConsensusPlugin, ISynchronizationPlugin, IInitial
             _api.LogManager
         );
 
-        _ = new(
+        _ = new PivotUpdator(
             _api.BlockTree,
             _api.Synchronizer.SyncModeSelector,
             _api.SyncPeerPool,
