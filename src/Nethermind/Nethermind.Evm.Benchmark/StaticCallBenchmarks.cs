be7fc223f528b82a537eb2e632370ce10633832e
Demuirgos <ayman.bouchareb@outlook.fr>
2024-06-20 23:42:33 +0100

    fix build issue

--- src/Nethermind/Nethermind.Evm.Benchmark/StaticCallBenchmarks.cs
+++ src/Nethermind/Nethermind.Evm.Benchmark/StaticCallBenchmarks.cs
@@ -98,7 +98,7 @@ namespace Nethermind.Evm.Benchmark
                 codeInfo: new CodeInfo(Bytecode),
                 value: 0,
                 transferValue: 0,
-                txExecutionContext: new TxExecutionContext(_header, Address.Zero, 0, null, []),
+                txExecutionContext: new TxExecutionContext(_header, Address.Zero, 0, []),
                 inputData: default
             );
 
