a4eb787d760b582025780e24a5ea67e366a4b64f
Ben Adams <thundercat@illyriad.co.uk>
2024-06-22 12:32:53 +0100

    formatting

--- src/Nethermind/Nethermind.Synchronization/ParallelSync/MultiSyncModeSelector.cs
+++ src/Nethermind/Nethermind.Synchronization/ParallelSync/MultiSyncModeSelector.cs
@@ -289,7 +289,7 @@ namespace Nethermind.Synchronization.ParallelSync
         /// <param name="best">Snapshot of the best known states</param>
         /// <returns>A string describing the state of sync</returns>
         private static string BuildStateString(Snapshot best) => best.Block == best.Header ?
-            $"block: {best.Block} | target: {best.TargetBlock} | peer: {best.Peer.Block}":
+            $"block: {best.Block} | target: {best.TargetBlock} | peer: {best.Peer.Block}" :
             $"block: {best.Block} | header: {best.Header} | target: {best.TargetBlock} | peer: {best.Peer.Block}";
 
         private static string BuildStateStringDebug(Snapshot best) =>
