0653775dc0f8f7a926bcdff90441b62985641849
Jorge Mederos <46798594+jmederosalvarado@users.noreply.github.com>
2024-06-27 18:20:25 +0200

    Gigachain L1Origin store and decoding (#7227)

---
+++ src/Nethermind/Nethermind.Taiko/L1Origin.cs
@@ -0,0 +1,12 @@
+using Nethermind.Core.Crypto;
+using Nethermind.Int256;
+
+namespace Nethermind.Taiko;
+
+public class L1Origin(UInt256 blockID, Hash256? l2BlockHash, long l1BlockHeight, Hash256 l1BlockHash)
+{
+    public UInt256 BlockID { get; set; } = blockID;
+    public Hash256? L2BlockHash { get; set; } = l2BlockHash;
+    public long L1BlockHeight { get; set; } = l1BlockHeight;
+    public Hash256 L1BlockHash { get; set; } = l1BlockHash;
+}
