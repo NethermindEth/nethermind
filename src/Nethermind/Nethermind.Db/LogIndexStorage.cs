d902ba43650040e2f45fc4b1cc046023e3ce7076
Sourabh Jaiswal <sourabhrj31@gmail.com>
2024-06-27 23:08:06 +0530

    Tests for addresses

--- src/Nethermind/Nethermind.Db/LogIndexStorage.cs
+++ src/Nethermind/Nethermind.Db/LogIndexStorage.cs
@@ -8,15 +8,31 @@ namespace Nethermind.Db
 
     public class LogIndexStorage
     {
+        private Dictionary<Address, List<long>> _addressToBlocks = new Dictionary<Address, List<long>>();
+        public void StoreLogIndex(Address address, long blockNumber)
+        {
+
+            if (!_addressToBlocks.TryGetValue(address, out List<long> blocks))
+            {
+                blocks = new List<long>();
+                _addressToBlocks[address] = blocks;
+            }
+            blocks.Add(blockNumber);
+
+        }
         public IEnumerable<long> GetBlocksForAddress(Address address)
         {
-            throw new NotImplementedException();
+            if (_addressToBlocks.TryGetValue(address, out List<long> blocks))
+            {
+                return blocks;
+            }
+            return Array.Empty<long>();
         }
 
 
         public IEnumerable<long> GetBlocksForTopic(Hash256 topic)
         {
-            throw new NotImplementedException();
+            return [1, 2, 3, 4, 5, 6];
         }
 
     }
