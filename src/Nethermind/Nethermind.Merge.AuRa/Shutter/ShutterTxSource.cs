42f03b76adef191baa8d0117015e33317781cf2c
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-07-03 17:36:19 +0100

    use correct slot for bui lding log

--- src/Nethermind/Nethermind.Merge.AuRa/Shutter/ShutterTxSource.cs
+++ src/Nethermind/Nethermind.Merge.AuRa/Shutter/ShutterTxSource.cs
@@ -56,7 +56,7 @@ public class ShutterTxSource(
 
         // atomic fetch
         ShutterTransactions shutterTransactions = _shutterTransactions;
-        if (_logger.IsInfo) _logger.Info($"Building Shutter block for slot {shutterTransactions.Slot} with {shutterTransactions.Transactions.Length} transactions.");
+        if (_logger.IsInfo) _logger.Info($"Building Shutter block for slot {nextSlot} with {shutterTransactions.Transactions.Length} transactions.");
         if (shutterTransactions.Slot == nextSlot)
         {
             return shutterTransactions.Transactions;
