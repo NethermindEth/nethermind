8b2af7bacf024360df9b2b0c331a1acc9498ea4f
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-07-05 13:23:08 +0100

    add log if try to build block in non-validator mode

--- src/Nethermind/Nethermind.Merge.AuRa/Shutter/ShutterTxSource.cs
+++ src/Nethermind/Nethermind.Merge.AuRa/Shutter/ShutterTxSource.cs
@@ -43,6 +43,7 @@ public class ShutterTxSource(
     {
         if (!shutterConfig.Validator)
         {
+            if (_logger.IsDebug) _logger.Debug($"Not building Shutter block since running in non-validator mode.");
             return [];
         }
 
