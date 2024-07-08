fde2df5814169c3f09458b7afdcc0251f312a771
Alexey Osipov <me@flcl.me>
2024-07-08 11:16:50 +0300

    Fix encoding

--- src/Nethermind/Nethermind.Taiko/TaikoTxProcessor.cs
+++ src/Nethermind/Nethermind.Taiko/TaikoTxProcessor.cs
@@ -61,7 +61,7 @@ public class TaikoTransactionProcessor(
 
     protected override void PayFees(Transaction tx, BlockHeader header, IReleaseSpec spec, ITxTracer tracer, in TransactionSubstate substate, in long spentGas, in UInt256 premiumPerGas, in byte statusCode)
     {
-        base.PayFees(tx, header, new TaikoAnchorTxReleaseSpec(spec, tx.IsAnchorTx? null: TaikoL2Address), tracer, substate, spentGas, premiumPerGas, statusCode);
+        base.PayFees(tx, header, new TaikoAnchorTxReleaseSpec(spec, tx.IsAnchorTx ? null : TaikoL2Address), tracer, substate, spentGas, premiumPerGas, statusCode);
     }
 
     protected override TransactionResult IncrementNonce(Transaction tx, BlockHeader header, IReleaseSpec spec, ITxTracer tracer, ExecutionOptions opts)
