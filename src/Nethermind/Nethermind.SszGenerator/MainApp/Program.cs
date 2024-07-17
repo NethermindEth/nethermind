90ba128c13640e2f14e3e4404cb37b7d5b91f5bd
LLHOYH <llhoyh01@gmail.com>
2024-07-17 15:51:39 +0800

    basic type ssz conversion is working

--- src/Nethermind/Nethermind.SszGenerator/MainApp/Program.cs
+++ src/Nethermind/Nethermind.SszGenerator/MainApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using static System.Console;
+using Nethermind.Serialization.Ssz;
 
 
 namespace Program{
@@ -14,6 +15,20 @@ namespace Program{
             WriteLine("Hello, World!");
             var mainApp = new MainApp();
             WriteLine($"Class Name: {mainApp.GetClassName()}");
+
+            byte[] buffer = new byte[100];
+            Span<byte> span = buffer;
+
+            byte valueByte=4;
+
+            byte[] value1 = {1,2,3,4};
+            byte[] value2 = {5,6,7,8};
+            // int offset=0;
+
+            Ssz.Encode(span,valueByte);
+            Ssz.Encode(span,valueByte);
+
+            WriteLine(string.Join(", ",buffer));
         }
     }
 }
