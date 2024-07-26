bf7747076a3afdca9d8e826fa8c8b2f73caaea07
Lautaro Emanuel <emlautarom1@gmail.com>
2024-07-26 15:52:53 -0300

    Remove unused imports

--- src/Nethermind/Nethermind.Optimism/Rpc/OptimismEthRpcModule.cs
+++ src/Nethermind/Nethermind.Optimism/Rpc/OptimismEthRpcModule.cs
@@ -1,9 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System;
 using System.Linq;
-using System.Security;
 using System.Threading.Tasks;
 using Nethermind.Blockchain.Find;
 using Nethermind.Blockchain.Receipts;
