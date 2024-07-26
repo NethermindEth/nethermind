031f91d5605f8545d4021333b5cc803fbbe17dfa
LLHOYH <llhoyh01@gmail.com>
2024-07-27 01:45:34 +0800

    fixed minor bug for length calculation

--- src/Nethermind/Ethereum.Test.Base/IndexesJson.cs
+++
@@ -1,9 +0,0 @@
-namespace Ethereum.Test.Base
-{
-    public class IndexesJson
-    {
-        public int Data { get; set; }
-        public int Gas { get; set; }
-        public int Value { get; set; }
-    }
-}
--- src/Nethermind/Nethermind.Api/Extensions/PluginConfig.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
-// SPDX-License-Identifier: LGPL-3.0-only
-
-namespace Nethermind.Api.Extensions;
-
-public class PluginConfig : IPluginConfig
-{
-    public string[] PluginOrder { get; set; } = { "Clique", "Aura", "Ethash", "Optimism", "AuRaMerge", "Merge", "MEV", "HealthChecks", "Hive" };
-}
