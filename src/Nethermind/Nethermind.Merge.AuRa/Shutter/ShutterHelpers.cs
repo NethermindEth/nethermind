f13de6a61b3d4e0de3097458db84511a3963ff41
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-08-01 11:49:36 +0100

    slotLength use totalMilliseconds

--- src/Nethermind/Nethermind.Merge.AuRa/Shutter/ShutterHelpers.cs
+++ src/Nethermind/Nethermind.Merge.AuRa/Shutter/ShutterHelpers.cs
@@ -9,7 +9,7 @@ public static class ShutterHelpers
     public static (ulong slot, ulong slotOffset) GetBuildingSlotAndOffset(ulong slotTimestampMs, ulong genesisTimestampMs, TimeSpan slotLength)
     {
         ulong slotTimeSinceGenesis = slotTimestampMs - genesisTimestampMs;
-        ulong buildingSlot = slotTimeSinceGenesis / (ulong)slotLength.Milliseconds;
+        ulong buildingSlot = slotTimeSinceGenesis / (ulong)slotLength.TotalMilliseconds;
         ulong offset = (ulong)DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - slotTimestampMs;
         return (buildingSlot, offset);
     }
