50ec957204cc512de4c86326f3732fdc659a3701
Alex Bespalov <alexb5dh@gmail.com>
2024-08-03 02:25:00 +0300

    Try explicit warmup for `Secp256r1Precompile`

--- src/Nethermind/Nethermind.Runner/Program.cs
+++ src/Nethermind/Nethermind.Runner/Program.cs
@@ -28,6 +28,7 @@ using Nethermind.Core;
 using Nethermind.Core.Collections;
 using Nethermind.Core.Exceptions;
 using Nethermind.Db.Rocks;
+using Nethermind.Evm.Precompiles;
 using Nethermind.Hive;
 using Nethermind.Init.Snapshot;
 using Nethermind.KeyStore.Config;
@@ -38,6 +39,7 @@ using Nethermind.Runner.Ethereum.Api;
 using Nethermind.Runner.Logging;
 using Nethermind.Seq.Config;
 using Nethermind.Serialization.Json;
+using Nethermind.Specs;
 using Nethermind.UPnP.Plugin;
 using NLog;
 using NLog.Config;
@@ -58,6 +60,11 @@ public static class Program
 
     public static void Main(string[] args)
     {
+        var watch = Stopwatch.StartNew();
+        var input = Convert.FromHexString("4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e");
+        for (var i = 0; i < 100; i++) Secp256r1Precompile.Instance.Run(input, new ReleaseSpec());
+        _logger.Info($"Warmed up Secp256r1Precompile in {watch.Elapsed}");
+
         // Increase regex cache size as more added in log coloring matches
         Regex.CacheSize = 128;
 #if !DEBUG
