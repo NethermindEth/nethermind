a7ac123fd86b655748a2ac16d9cc2c7ba946970a
Alex Bespalov <alexb5dh@gmail.com>
2024-08-03 18:42:55 +0300

    Reduce warmup to 20 runs

--- src/Nethermind/Nethermind.Evm/Precompiles/Secp256r1Precompile.cs
+++ src/Nethermind/Nethermind.Evm/Precompiles/Secp256r1Precompile.cs
@@ -22,7 +22,7 @@ public class Secp256r1Precompile : IPrecompile<Secp256r1Precompile>
         var input = Convert.FromHexString(
             "4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e"
         );
-        for (var i = 0; i < 200; i++) Instance.Run(input, releaseSpec);
+        for (var i = 0; i < 20; i++) Instance.Run(input, releaseSpec);
     }
 
     private static IntPtr OnResolvingUnmanagedDll(Assembly context, string name)
