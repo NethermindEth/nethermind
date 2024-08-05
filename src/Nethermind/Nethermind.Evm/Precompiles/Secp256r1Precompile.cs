f59ce1488691b779d5ee8a0d40bf9aef58cb6183
Alex Bespalov <alexb5dh@gmail.com>
2024-08-05 23:52:46 +0300

    Temporary additional logging

--- src/Nethermind/Nethermind.Evm/Precompiles/Secp256r1Precompile.cs
+++ src/Nethermind/Nethermind.Evm/Precompiles/Secp256r1Precompile.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Buffers;
+using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.Loader;
@@ -23,7 +24,10 @@ public class Secp256r1Precompile : IPrecompile<Secp256r1Precompile>
         var input = Convert.FromHexString(
             "4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e"
         );
+
+        var watch = Stopwatch.StartNew();
         for (var i = 0; i < 20; i++) Instance.Run(input, releaseSpec);
+        Console.WriteLine($"[secp256r1] Warmed up in {watch.Elapsed}");
     }
 
     private static IntPtr OnResolvingUnmanagedDll(Assembly context, string name)
@@ -77,13 +81,28 @@ public class Secp256r1Precompile : IPrecompile<Secp256r1Precompile>
 
     public (ReadOnlyMemory<byte>, bool) Run(in ReadOnlyMemory<byte> inputData, IReleaseSpec releaseSpec)
     {
-        Metrics.Secp256r1Precompile++;
+        var count = Metrics.Secp256r1Precompile++;
+        var watch = Stopwatch.StartNew();
 
-        using MemoryHandle pin = inputData.Pin();
-        unsafe
+        try
         {
-            GoSlice slice = new((IntPtr) pin.Pointer, inputData.Length);
-            return (VerifyBytes(slice) != 0 ? ValidResult : null, true);
+            using MemoryHandle pin = inputData.Pin();
+            unsafe
+            {
+                GoSlice slice = new((IntPtr) pin.Pointer, inputData.Length);
+                var res = VerifyBytes(slice);
+
+                TimeSpan elapsed = watch.Elapsed;
+                if (elapsed.TotalMilliseconds > 200)
+                    Console.WriteLine($"[secp256r1][{count}] Finished: {Convert.ToHexString(inputData.Span)} -> {res} in {elapsed}");
+
+                return (res != 0 ? ValidResult : null, true);
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"[secp256r1][{count}] Failed: {exception.Message} in {watch.Elapsed}");
+            throw;
         }
     }
 }
