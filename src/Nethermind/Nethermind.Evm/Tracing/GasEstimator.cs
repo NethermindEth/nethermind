1e5fb574957d7ecba4a1a5508cb163981ea448e5
Rohit Ranjan <rohitrjn629@gmail.com>
2024-08-06 17:10:05 +0530

    fix eth_gasEstimate issue

--- src/Nethermind/Nethermind.Evm/Tracing/GasEstimator.cs
+++ src/Nethermind/Nethermind.Evm/Tracing/GasEstimator.cs
@@ -45,7 +45,7 @@ namespace Nethermind.Evm.Tracing
 
             // Calculate and return additional gas required in case of insufficient funds.
             UInt256 senderBalance = _stateProvider.GetBalance(tx.SenderAddress);
-            if (tx.Value != UInt256.Zero && tx.Value > senderBalance)
+            if (tx.Value != UInt256.Zero && tx.Value > senderBalance && tx.SenderAddress != Address.SystemUser)
             {
                 return gasTracer.CalculateAdditionalGasRequired(tx, releaseSpec);
             }
