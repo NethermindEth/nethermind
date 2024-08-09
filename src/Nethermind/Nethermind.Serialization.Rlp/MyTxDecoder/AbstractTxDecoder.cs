57da69d665a68a128518b17064855f9a22f8f15f
Lautaro Emanuel <emlautarom1@gmail.com>
2024-08-09 15:23:54 -0300

    Add `Encode` to API

---
+++ src/Nethermind/Nethermind.Serialization.Rlp/MyTxDecoder/AbstractTxDecoder.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Runtime.CompilerServices;
+using Nethermind.Core;
+
+namespace Nethermind.Serialization.Rlp.MyTxDecoder;
+
+// NOTE: Due to requiring covariant return types we need to use `abstract class` instead of `interface`
+public abstract class AbstractTxDecoder
+{
+    // NOTE: Implementations can (and will) return more specific types than `Transaction`
+    public abstract Transaction Decode(Span<byte> transactionSequence, RlpStream rlpStream, RlpBehaviors rlpBehaviors);
+
+    public abstract void Encode(Transaction item, RlpStream stream, RlpBehaviors rlpBehaviors = RlpBehaviors.None, bool forSigning = false, bool isEip155Enabled = false, ulong chainId = 0);
+}
+
