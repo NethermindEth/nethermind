e99d627d5eb47022519d93e038a2466ec2f272d0
Lautaro Emanuel <emlautarom1@gmail.com>
2024-08-14 13:14:02 -0300

    Add `int GetLength` to `ITxDecoder`

--- src/Nethermind/Nethermind.Serialization.Rlp/MyTxDecoder/ITxDecoder.cs
+++ src/Nethermind/Nethermind.Serialization.Rlp/MyTxDecoder/ITxDecoder.cs
@@ -14,4 +14,6 @@ interface ITxDecoder
     public void Decode(ref Transaction? transaction, int txSequenceStart, ReadOnlySpan<byte> transactionSequence, ref Rlp.ValueDecoderContext decoderContext, RlpBehaviors rlpBehaviors = RlpBehaviors.None);
 
     public void EncodeTx(Transaction? item, RlpStream stream, RlpBehaviors rlpBehaviors = RlpBehaviors.None, bool forSigning = false, bool isEip155Enabled = false, ulong chainId = 0);
+
+    public int GetLength(Transaction tx, RlpBehaviors rlpBehaviors);
 }
