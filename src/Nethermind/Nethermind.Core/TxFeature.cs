f98acc6477dc81349b1a509eb4d2e4a434463179
Lautaro Emanuel <emlautarom1@gmail.com>
2024-08-15 16:09:19 -0300

    Delete `TxFeature`
    - Not used for now

--- src/Nethermind/Nethermind.Core/TxFeature.cs
+++
@@ -1,8 +0,0 @@
-namespace Nethermind.Core;
-
-public sealed class TxFeature
-{
-    public static readonly TxFeature AccessList = new();
-    public static readonly TxFeature EIP1559 = new();
-    public static readonly TxFeature Blob = new();
-}
