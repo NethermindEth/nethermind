b5d5dd68afb55495975420c566312c60998b1a6a
Ben Adams <thundercat@illyriad.co.uk>
2024-08-15 14:03:40 +0100

    Update tests

--- src/Nethermind/Ethereum.Blockchain.Pyspec.Test/PragueBlockchainTests.cs
+++ src/Nethermind/Ethereum.Blockchain.Pyspec.Test/PragueBlockchainTests.cs
@@ -21,7 +21,7 @@ public class PragueBlockChainTests : BlockchainTestBase
         TestsSourceLoader loader = new(new LoadPyspecTestsStrategy()
         {
             ArchiveName = "fixtures_eip7692.tar.gz",
-            ArchiveVersion = "eip7692@v1.0.7"
+            ArchiveVersion = "eip7692@v1.0.8"
         }, $"fixtures/blockchain_tests/prague");
         return loader.LoadTests().Cast<BlockchainTest>();
     }
