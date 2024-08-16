e603b095113b855800c25ae1ce3c0daaacab755b
Alex Bespalov <alexb5dh@gmail.com>
2024-08-16 20:18:31 +0300

    Initialize on first run

--- src/Nethermind/Nethermind.Evm/Precompiles/Secp256r1Precompile.cs
+++ src/Nethermind/Nethermind.Evm/Precompiles/Secp256r1Precompile.cs
@@ -11,14 +11,11 @@ using System.Security.Cryptography;
 using Nethermind.Core;
 using Nethermind.Core.Extensions;
 using Nethermind.Core.Specs;
-using Nethermind.Specs;
 
 namespace Nethermind.Evm.Precompiles;
 
 public class Secp256r1Precompile : IPrecompile<Secp256r1Precompile>
 {
-    static Secp256r1Precompile() => EnsureInitialized();
-
     private static bool _initialized;
 
     private static void EnsureInitialized()
@@ -27,17 +24,7 @@ public class Secp256r1Precompile : IPrecompile<Secp256r1Precompile>
 
         //Environment.SetEnvironmentVariable("GODEBUG", "gctrace=1");
         Environment.SetEnvironmentVariable("GOGC", "1000");
-
         AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly())!.ResolvingUnmanagedDll += OnResolvingUnmanagedDll;
-
-        var releaseSpec = new ReleaseSpec();
-        var input = Convert.FromHexString(
-            "4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e"
-        );
-
-        var watch = Stopwatch.StartNew();
-        Instance.Run(input, releaseSpec);
-        Console.WriteLine($"[secp256r1] Warmed up in {watch.Elapsed}");
 
         _initialized = true;
     }
@@ -93,6 +80,8 @@ public class Secp256r1Precompile : IPrecompile<Secp256r1Precompile>
 
     public (ReadOnlyMemory<byte>, bool) Run(in ReadOnlyMemory<byte> inputData, IReleaseSpec releaseSpec)
     {
+        EnsureInitialized();
+
         var count = ++Metrics.Secp256r1Precompile;
         var watch = Stopwatch.StartNew();
 
@@ -101,37 +90,17 @@ public class Secp256r1Precompile : IPrecompile<Secp256r1Precompile>
 
         try
         {
-            if (count <= 10)
+            using MemoryHandle pin = inputData.Pin();
+            unsafe
             {
-                if (inputData.Length != 160)
-                    return (null, true);
+                GoSlice slice = new((IntPtr) pin.Pointer, inputData.Length);
+                var res = VerifyBytes(slice);
 
-                ReadOnlySpan<byte> bytes = inputData.Span;
-                ReadOnlySpan<byte> hash = bytes[..32], sig = bytes[32..96];
-                ReadOnlySpan<byte> x = bytes[96..128], y = bytes[128..160];
+                TimeSpan elapsed = watch.Elapsed;
+                if (elapsed.TotalMicroseconds > 500)
+                    Console.WriteLine($"[{DateTime.UtcNow:T}] [secp256r1][{count:D5}] Finished: {Convert.ToHexString(inputData.Span)} -> {res} in {elapsed.TotalMicroseconds}µs");
 
-                using var ecdsa = ECDsa.Create(new ECParameters
-                {
-                    Curve = ECCurve.NamedCurves.nistP256,
-                    Q = new() { X = x.ToArray(), Y = y.ToArray() }
-                });
-
-                return (ecdsa.VerifyHash(hash, sig) ? ValidResult : null, true);
-            }
-            else
-            {
-                using MemoryHandle pin = inputData.Pin();
-                unsafe
-                {
-                    GoSlice slice = new((IntPtr) pin.Pointer, inputData.Length);
-                    var res = VerifyBytes(slice);
-
-                    TimeSpan elapsed = watch.Elapsed;
-                    if (elapsed.TotalMicroseconds > 500)
-                        Console.WriteLine($"[{DateTime.UtcNow:T}] [secp256r1][{count:D5}] Finished: {Convert.ToHexString(inputData.Span)} -> {res} in {elapsed.TotalMicroseconds}µs");
-
-                    return (res != 0 ? ValidResult : null, true);
-                }
+                return (res != 0 ? ValidResult : null, true);
             }
         }
         catch (Exception exception)
