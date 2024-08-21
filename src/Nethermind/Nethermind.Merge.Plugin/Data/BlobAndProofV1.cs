6e2cb34b1efcce4392f349ab99aa9e4e80ff2a6b
Marcin Sobczak <marcindsobczak@gmail.com>
2024-08-21 10:06:37 +0200

    move BlobAndProofV1 back to merge plugin

--- src/Nethermind/Nethermind.Merge.Plugin/Data/BlobAndProofV1.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Data/BlobAndProofV1.cs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-namespace Nethermind.TxPool;
+namespace Nethermind.Merge.Plugin.Data;
 
 public class BlobAndProofV1(byte[] blob, byte[] proof)
 {
