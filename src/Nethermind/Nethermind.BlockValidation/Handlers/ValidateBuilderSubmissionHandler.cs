ae4357fb351248a6064730b9125c62185d8e39b6
Rohit Ranjan <rohitrjn629@gmail.com>
2024-08-27 12:34:03 +0530

    verify blobs

--- src/Nethermind/Nethermind.BlockValidation/Handlers/ValidateBuilderSubmissionHandler.cs
+++ src/Nethermind/Nethermind.BlockValidation/Handlers/ValidateBuilderSubmissionHandler.cs
@@ -1,10 +1,12 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System.Linq;
 using System.Threading.Tasks;
 using Nethermind.BlockValidation.Data;
-using Nethermind.Consensus.Validators;
 using Nethermind.Core;
+using Nethermind.Crypto;
+using Nethermind.Int256;
 using Nethermind.JsonRpc;
 using Nethermind.Logging;
 using Nethermind.Merge.Plugin.Data;
@@ -18,6 +20,49 @@ public class ValidateSubmissionHandler
     public ValidateSubmissionHandler(ILogManager logManager)
     {
         _logger = logManager.GetClassLogger();
+    }
+
+    private bool ValidateBlobsBundle(Transaction[] transactions, BlobsBundleV1 blobsBundle, out string? error)
+    {
+        // get sum of length of blobs of each transaction
+        int totalBlobsLength = transactions.Sum(t => t.BlobVersionedHashes!.Length);
+
+        if (totalBlobsLength != blobsBundle.Blobs.Length)
+        {
+            error = $"Total blobs length mismatch. Expected {totalBlobsLength} but got {blobsBundle.Blobs.Length}";
+            return false;
+        }
+
+        if (totalBlobsLength != blobsBundle.Commitments.Length)
+        {
+            error = $"Total commitments length mismatch. Expected {totalBlobsLength} but got {blobsBundle.Commitments.Length}";
+            return false;
+        }
+
+        if (totalBlobsLength != blobsBundle.Proofs.Length)
+        {
+            error = $"Total proofs length mismatch. Expected {totalBlobsLength} but got {blobsBundle.Proofs.Length}";
+            return false;
+        }
+
+        if (!KzgPolynomialCommitments.AreProofsValid(blobsBundle.Proofs, blobsBundle.Commitments, blobsBundle.Blobs))
+        {
+            error = "Invalid KZG proofs";
+            return false;
+        }
+
+        error = null;
+
+        _logger.Info($"Validated blobs bundle with {totalBlobsLength} blobs, commitments: {blobsBundle.Commitments.Length}, proofs: {blobsBundle.Proofs.Length}");
+
+        return true;
+    }
+
+    private bool ValidatePayload(Block block, Address feeRecipient, UInt256 expectedProfit, ulong registerGasLimit, out string? error)
+    {
+        // TODO: Implement this method
+        error = null;
+        return true;
     }
 
     private bool ValidateBlock(Block block, BidTrace message, ulong registerGasLimit, out string? error)
@@ -48,6 +93,16 @@ public class ValidateSubmissionHandler
             return false;
         }
 
+        Address feeRecipient = message.ProposerFeeRecipient;
+        UInt256 expectedProfit = message.Value;
+
+        if (!ValidatePayload(block, feeRecipient, expectedProfit, registerGasLimit, out error))
+        {
+            return false;
+        }
+
+        _logger.Info($"Validated block Hash: {block.Header.Hash} Number: {block.Header.Number} ParentHash: {block.Header.ParentHash}");
+
         return true;
     }
 
@@ -66,6 +121,17 @@ public class ValidateSubmissionHandler
             return BlockValidationResult.Invalid($"Block {payload} coud not be parsed as a block");
         }
 
+        if (block is not null && !ValidateBlock(block, request.BlockRequest.Message, request.RegisterGasLimit, out string? error))
+        {
+            if (_logger.IsWarn) _logger.Warn($"Invalid block. Result of {payloadStr}. Error: {error}");
+            return BlockValidationResult.Invalid(error ?? "Block validation failed");
+        }
+
+        if (block is not null && !ValidateBlobsBundle(block.Transactions, blobsBundle, out string? blobsError))
+        {
+            if (_logger.IsWarn) _logger.Warn($"Invalid blobs bundle. Result of {payloadStr}. Error: {blobsError}");
+            return BlockValidationResult.Invalid(blobsError ?? "Blobs bundle validation failed");
+        }
 
 
         return BlockValidationResult.Valid();
