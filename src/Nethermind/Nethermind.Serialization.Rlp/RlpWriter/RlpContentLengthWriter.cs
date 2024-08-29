180368c5bb8e8165812be76bf64f75dcbcd62b2d
Lautaro Emanuel <emlautarom1@gmail.com>
2024-08-29 11:54:25 -0300

    - Prefer property - Update overloads

--- src/Nethermind/Nethermind.Serialization.Rlp/RlpWriter/RlpContentLengthWriter.cs
+++ src/Nethermind/Nethermind.Serialization.Rlp/RlpWriter/RlpContentLengthWriter.cs
@@ -7,54 +7,62 @@ using Nethermind.Int256;
 
 namespace Nethermind.Serialization.Rlp.RlpWriter;
 
-sealed class RlpContentLengthWriter : IRlpWriter
+public sealed class RlpContentLengthWriter : IRlpWriter
 {
-    private int _contentLength = 0;
+    public int ContentLength { get; private set; } = 0;
 
-    public int ContentLength => _contentLength;
+    public void Write(byte value)
+    {
+        ContentLength += Rlp.LengthOf(value);
+    }
 
     public void Write(int value)
     {
-        _contentLength += Rlp.LengthOf(value);
+        ContentLength += Rlp.LengthOf(value);
     }
 
     public void Write(ulong value)
     {
-        _contentLength += Rlp.LengthOf(value);
+        ContentLength += Rlp.LengthOf(value);
     }
 
     public void Write(ReadOnlySpan<byte> value)
     {
-        _contentLength += Rlp.LengthOf(value);
+        ContentLength += Rlp.LengthOf(value);
     }
 
     public void Write(UInt256 value)
     {
-        _contentLength += Rlp.LengthOf(value);
+        ContentLength += Rlp.LengthOf(value);
     }
 
     public void Write(long value)
     {
-        _contentLength += Rlp.LengthOf(value);
+        ContentLength += Rlp.LengthOf(value);
     }
 
     public void Write(Address value)
     {
-        _contentLength += Rlp.LengthOf(value);
+        ContentLength += Rlp.LengthOf(value);
     }
 
     public void Write(Memory<byte>? value)
     {
-        _contentLength += Rlp.LengthOf(value);
+        ContentLength += Rlp.LengthOf(value);
     }
 
     public void Write(byte[] value)
     {
-        _contentLength += Rlp.LengthOf(value);
+        ContentLength += Rlp.LengthOf(value);
     }
 
     public void Write(Rlp value)
     {
-        _contentLength += value.Length;
+        ContentLength += value.Length;
+    }
+
+    public void Write(byte[]?[] value)
+    {
+        ContentLength += Rlp.LengthOf(value);
     }
 }
