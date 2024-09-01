4848c43babda155967ee7fe8d6db389e82aed1f9
FuzzysTodd <fuzzystodd@gmail.com>
2024-09-01 00:32:32 -0400

    Major

---
+++ tenderly/bitcoincobra/contracts/ERC721Creator.sol
@@ -0,0 +1,41 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @author: manifold.xyz
+
+import "@openzeppelin/contracts/proxy/Proxy.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/StorageSlot.sol";
+
+contract ERC721Creator is Proxy {
+    
+    constructor(string memory name, string memory symbol) {
+        assert(_IMPLEMENTATION_SLOT == bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1));
+        StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = 0xe4E4003afE3765Aca8149a82fc064C0b125B9e5a;
+        Address.functionDelegateCall(
+            0xe4E4003afE3765Aca8149a82fc064C0b125B9e5a,
+            abi.encodeWithSignature("initialize(string,string)", name, symbol)
+        );
+    }
+        
+    /**
+     * @dev Storage slot with the address of the current implementation.
+     * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1, and is
+     * validated in the constructor.
+     */
+    bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    /**
+     * @dev Returns the current implementation address.
+     */
+     function implementation() public view returns (address) {
+        return _implementation();
+    }
+
+    function _implementation() internal override view returns (address) {
+        return StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value;
+    }    
+
+}
