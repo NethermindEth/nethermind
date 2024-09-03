f6f0c922cdd02e97891cd24ec60f7e69996071d3
Lautaro Emanuel <emlautarom1@gmail.com>
2024-09-03 16:32:23 -0300

    Reorder imports (avoid diff)

--- src/Nethermind/Nethermind.Optimism/OptimismTxValidator.cs
+++ src/Nethermind/Nethermind.Optimism/OptimismTxValidator.cs
@@ -1,10 +1,10 @@
 // SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Diagnostics.CodeAnalysis;
 using Nethermind.Core;
 using Nethermind.Core.Specs;
 using Nethermind.TxPool;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Nethermind.Optimism;
 
