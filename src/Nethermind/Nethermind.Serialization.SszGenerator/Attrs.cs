8685599547fca9f37de896afcde7e268b2fdc8fc
Alexey Osipov <me@flcl.me>
2024-09-03 11:37:12 +0300

    SSZ codegen

---
+++ src/Nethermind/Nethermind.Serialization.SszGenerator/Attrs.cs
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+namespace Nethermind.Serialization.Ssz;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public class SszSerializableAttribute : Attribute;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class SszListAttribute(int limit) : Attribute
+{
+    public int Limit { get; } = limit;
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class SszVectorAttribute(int length) : Attribute
+{
+    public int Length { get; } = length;
+}
