14d3cae898e67390cd3adf320f1782579e99bed1
Lautaro Emanuel <emlautarom1@gmail.com>
2024-09-10 16:12:25 -0300

    Initial `IRpcTransactionConverter`

---
+++ src/Nethermind/Nethermind.Facade/Eth/RpcTransaction/IRpcTransactionConverter.cs
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using Nethermind.Core;
+
+namespace Nethermind.Facade.Eth.RpcTransaction;
+
+public interface IRpcTransactionConverter
+{
+    IRpcTransaction FromTransaction(Transaction tx, TxReceipt receipt);
+}
+
+public class ComposeTransactionConverter : IRpcTransactionConverter
+{
+    private readonly IRpcTransactionConverter?[] _converters = new IRpcTransactionConverter?[Transaction.MaxTxType + 1];
+
+    public ComposeTransactionConverter RegisterConverter(TxType txType, IRpcTransactionConverter converter)
+    {
+        _converters[(byte)txType] = converter;
+        return this;
+    }
+
+    public IRpcTransaction FromTransaction(Transaction tx, TxReceipt receipt)
+    {
+        var converter = _converters[(byte)tx.Type] ?? throw new ArgumentException("No converter for transaction type");
+        return converter.FromTransaction(tx, receipt);
+    }
+}
