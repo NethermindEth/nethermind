9ba80a2017c6b4febfb2bc81574124cd69a68530
Tanishq Jasoria <jasoriatanishq@gmail.com>
2024-09-10 16:35:51 +0100

    use rust for pedersen hash

--- src/Nethermind/Nethermind.Verkle.Tree.Test/PedersenHashTests.cs
+++ src/Nethermind/Nethermind.Verkle.Tree.Test/PedersenHashTests.cs
@@ -62,7 +62,7 @@ namespace Nethermind.Verkle.Tree.Test
         [Test]
         public void PedersenHashTreeKey0()
         {
-            byte[] hash = PedersenHash.Hash(_testAddressBytesZero, UInt256.Zero);
+            byte[] hash = PedersenHash.HashRust(_testAddressBytesZero, UInt256.Zero);
             hash[31] = 0;
             Convert.ToHexString(hash).Should().BeEquivalentTo("1A100684FD68185060405F3F160E4BB6E034194336B547BDAE323F888D533200");
         }
@@ -70,8 +70,8 @@ namespace Nethermind.Verkle.Tree.Test
         [Test]
         public void PedersenHashTreeKey1()
         {
-            Span<byte> address32 = Convert.FromHexString("71562b71999873DB5b286dF957af199Ec94617f7");
-            byte[] hash = PedersenHash.Hash(address32, UInt256.Zero);
+            byte[] address32 = Convert.FromHexString("71562b71999873DB5b286dF957af199Ec94617f7");
+            byte[] hash = PedersenHash.HashRust(address32, UInt256.Zero);
             hash[31] = 0;
             Convert.ToHexString(hash).Should().BeEquivalentTo("1540DFAD7755B40BE0768C6AA0A5096FBF0215E0E8CF354DD928A17834646600");
         }
---
+++ src/Nethermind/Nethermind.Verkle/VerkleCrypto.cs
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+namespace Nethermind.Verkle;
+
+public static class VerkleCrypto
+{
+    private static readonly IntPtr _rust = RustVerkleLib.VerkleContextNew();
+
+    public static void PedersenHash(byte[] address, byte[] treeIndexLe, byte[] outHas) =>
+        RustVerkleLib.VerklePedersenhash(_rust, address, treeIndexLe, outHas);
+
+    public static void MSM(byte[] input, UIntPtr length, byte[] outHash) =>
+        RustVerkleLib.VerkleMSM(_rust, input, length, outHash);
+
+    public static void Prove(byte[] input, UIntPtr length, byte[] outHash) =>
+        RustVerkleLib.VerkleProve(_rust, input, length, outHash);
+
+
+    public static void Verify(byte[] input, UIntPtr length) => RustVerkleLib.VerkleVerify(_rust, input, length);
+
+    public static void ProveUncompressed(byte[] input, UIntPtr length, byte[] outHash) =>
+        RustVerkleLib.VerkleProveUncompressed(_rust, input, length, outHash);
+
+    public static void VerifyUncompressed(byte[] input, UIntPtr length) =>
+        RustVerkleLib.VerkleVerifyUncompressed(_rust, input, length);
+}
