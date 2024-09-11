cdd4f23bd3a9d94004065d9e0fa16d60e6ab267f
Lautaro Emanuel <emlautarom1@gmail.com>
2024-09-11 16:51:59 -0300

    Use annotation for `IRpcTransaction`

--- src/Nethermind/Nethermind.Facade/Eth/RpcTransaction/IRpcTransaction.cs
+++ src/Nethermind/Nethermind.Facade/Eth/RpcTransaction/IRpcTransaction.cs
@@ -7,11 +7,12 @@ using System.Text.Json.Serialization;
 
 namespace Nethermind.Facade.Eth.RpcTransaction;
 
+[JsonConverter(typeof(JsonConverterImpl))]
 public interface IRpcTransaction
 {
-    public static readonly JsonConverter<IRpcTransaction> JsonConverter = new ConverterImpl();
+    public static readonly JsonConverter<IRpcTransaction> JsonConverter = new JsonConverterImpl();
 
-    private class ConverterImpl : JsonConverter<IRpcTransaction>
+    private class JsonConverterImpl : JsonConverter<IRpcTransaction>
     {
         public override IRpcTransaction? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => throw new NotSupportedException();
         public override void Write(Utf8JsonWriter writer, IRpcTransaction value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, value.GetType(), options);
--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/RpcTransaction/RpcTransactionTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/RpcTransaction/RpcTransactionTests.cs
@@ -12,7 +12,7 @@ namespace Nethermind.JsonRpc.Test.Modules.RpcTransaction;
 
 public class RpcTransactionTests
 {
-    private readonly IJsonSerializer _serializer = new EthereumJsonSerializer([IRpcTransaction.JsonConverter]);
+    private readonly IJsonSerializer _serializer = new EthereumJsonSerializer();
 
     private readonly IRpcTransactionConverter _converter = new ComposeTransactionConverter()
         .RegisterConverter(TxType.Legacy, RpcLegacyTransaction.Converter)
