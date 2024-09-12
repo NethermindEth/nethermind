01dadcccb16302cc4bd72a3f52879d56c618abff
Alexey Osipov <me@flcl.me>
2024-09-12 18:59:15 +0300

    Add license headers

--- src/Nethermind/Nethermind.Taiko/BlockMetadata.cs
+++ src/Nethermind/Nethermind.Taiko/BlockMetadata.cs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Int256;
