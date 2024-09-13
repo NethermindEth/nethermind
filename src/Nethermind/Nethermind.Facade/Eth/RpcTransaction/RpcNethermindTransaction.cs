fe8e7e7bd3a6fae48e7aab070face43ac73de168
Lautaro Emanuel <emlautarom1@gmail.com>
2024-09-13 14:24:15 -0300

    Define `RpcNethermindTransaction` as base class

---
+++ src/Nethermind/Nethermind.Facade/Eth/RpcTransaction/RpcNethermindTransaction.cs
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Text.Json.Serialization;
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+
+namespace Nethermind.Facade.Eth.RpcTransaction;
+
+/// <summary>
+/// Base class for all Nethermind RPC Transactions.
+/// All fields are optional since they're not part of the Ethereum JSON RPC spec.
+/// </summary>
+public abstract class RpcNethermindTransaction : IRpcTransaction
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    public Hash256? Hash { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    public long? TransactionIndex { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    public Hash256? BlockHash { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    public long? BlockNumber { get; set; }
+
+    [JsonConstructor]
+    public RpcNethermindTransaction() { }
+
+    public RpcNethermindTransaction(Transaction transaction)
+    {
+        Hash = transaction.Hash;
+    }
+}
