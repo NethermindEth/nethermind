044fa8164194a37bb5a3b24bc11446531be96807
Ben Adams <thundercat@illyriad.co.uk>
2024-09-15 18:48:57 +0100

    Precompile: return out of gas rather than throwing exception

--- src/Nethermind/Nethermind.Evm/VirtualMachine.cs
+++ src/Nethermind/Nethermind.Evm/VirtualMachine.cs
@@ -189,7 +189,7 @@ internal sealed class VirtualMachine<TLogger> : IVirtualMachine where TLogger : 
 
                     callResult = ExecutePrecompile(currentState, spec);
 
-                    if (!callResult.PrecompileSuccess.Value)
+                    if (callResult.IsException || !callResult.PrecompileSuccess.Value)
                     {
                         if (currentState.IsPrecompile && currentState.IsTopLevel)
                         {
@@ -578,8 +578,7 @@ internal sealed class VirtualMachine<TLogger> : IVirtualMachine where TLogger : 
 
         if (!UpdateGas(checked(baseGasCost + blobGasCost), ref gasAvailable))
         {
-            Metrics.EvmExceptions++;
-            throw new OutOfGasException();
+            return CallResult.OutOfGasException;
         }
 
         state.GasAvailable = gasAvailable;
