1141bdeda7d97c7f97f017479d0674956816cebe
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-09-16 11:48:36 +0100

    improve shutter config

--- src/Nethermind/Nethermind.Shutter/Config/IShutterConfig.cs
+++ src/Nethermind/Nethermind.Shutter/Config/IShutterConfig.cs
@@ -11,63 +11,73 @@ namespace Nethermind.Shutter.Config;
 
 public interface IShutterConfig : IConfig
 {
+    private const string DefaultP2PAddresses =
+@"/ip4/139.59.130.109/tcp/23003/p2p/12D3KooWRZoofMsnpsjkgvfPQUyGXZQnn7EVnb4tw4ghNfwMnnsj,
+/ip4/167.71.169.248/tcp/23003/p2p/12D3KooWGH3VxoSQXZ6wUuCmsv5caGQnhwfGejbkXH6uS2r7sehA,
+/ip4/139.59.130.109/tcp/23003/p2p/12D3KooWNxTiw7CvD1fuyye5P8qPhKTTrRBW6wwZwMdqdTxjYF2H,
+/ip4/178.128.192.239/tcp/23003/p2p/12D3KooWCdpkipTiuzVMfkV7yLLgqbFeAL8WmEP78hCoBGBYLugN,
+/ip4/45.55.192.248/tcp/23003/p2p/12D3KooWMPuubKqksfMxvLwEBDScaopTdvPLr5J5SMmBEo2zkcMz,
+/ip4/178.128.126.237/tcp/23003/p2p/12D3KooWAg1pGUDAfFWSZftpN3JjBfLUCGLQcZApJHv2VntdMS9U";
+
     [ConfigItem(Description = "Whether to enable Shutter.", DefaultValue = "false")]
     bool Enabled { get; set; }
-
-    [ConfigItem(Description = "Whether to build Shutter blocks.", DefaultValue = "true")]
-    bool Validator { get; set; }
-
-    [ConfigItem(Description = "The address of the Shutter sequencer contract.",
-        DefaultValue = "null")]
-    string? SequencerContractAddress { get; set; }
-
-    [ConfigItem(Description = "The address of the Shutter validator registry contract.",
-        DefaultValue = "null")]
-    string? ValidatorRegistryContractAddress { get; set; }
-
-    [ConfigItem(Description = "The address of the Shutter key broadcast contract.",
-        DefaultValue = "null")]
-    string? KeyBroadcastContractAddress { get; set; }
-
-    [ConfigItem(Description = "The address of the Shutter keyper set manager contract.",
-        DefaultValue = "null")]
-    string? KeyperSetManagerContractAddress { get; set; }
-
-    [ConfigItem(Description = "The p2p addresses of the Shutter keypers.",
-        DefaultValue = "null")]
-    string[]? KeyperP2PAddresses { get; set; }
-
-    [ConfigItem(Description = "The port to connect to Shutter P2P network with.",
-        DefaultValue = "23102")]
-    int P2PPort { get; set; }
 
     [ConfigItem(Description = "The filepath of the validator info json file.",
         DefaultValue = "null")]
     string? ValidatorInfoFile { get; set; }
 
+    [ConfigItem(Description = "The address of the Shutter sequencer contract.",
+        DefaultValue = "0xc5C4b277277A1A8401E0F039dfC49151bA64DC2E")]
+    string? SequencerContractAddress { get; set; }
+
+    [ConfigItem(Description = "The address of the Shutter validator registry contract.",
+        DefaultValue = "0xefCC23E71f6bA9B22C4D28F7588141d44496A6D6")]
+    string? ValidatorRegistryContractAddress { get; set; }
+
+    [ConfigItem(Description = "The address of the Shutter key broadcast contract.",
+        DefaultValue = "0x626dB87f9a9aC47070016A50e802dd5974341301")]
+    string? KeyBroadcastContractAddress { get; set; }
+
+    [ConfigItem(Description = "The address of the Shutter keyper set manager contract.",
+        DefaultValue = "0x7C2337f9bFce19d8970661DA50dE8DD7d3D34abb")]
+    string? KeyperSetManagerContractAddress { get; set; }
+
+    [ConfigItem(Description = "The p2p addresses of the Shutter keypers.",
+        DefaultValue = DefaultP2PAddresses)]
+    string[]? KeyperP2PAddresses { get; set; }
+
+    [ConfigItem(Description = "Instance ID of Shutter keyper set.",
+        DefaultValue = "1000")]
+    ulong InstanceID { get; set; }
+
+    [ConfigItem(Description = "The port to connect to Shutter P2P network with.",
+        DefaultValue = "23102")]
+    int P2PPort { get; set; }
+
     [ConfigItem(Description = "The Shutter P2P protocol version.",
-        DefaultValue = "/shutter/0.1.0")]
+        DefaultValue = "/shutter/0.1.0", HiddenFromDocs = true)]
     string? P2PProtocolVersion { get; set; }
 
     [ConfigItem(Description = "The Shutter P2P agent version.",
-        DefaultValue = "github.com/shutter-network/rolling-shutter/rolling-shutter")]
+        DefaultValue = "github.com/shutter-network/rolling-shutter/rolling-shutter",
+        HiddenFromDocs = true)]
     string? P2PAgentVersion { get; set; }
 
     [ConfigItem(Description = "The Shutter validator registry message version.",
-        DefaultValue = "0")]
+        DefaultValue = "0", HiddenFromDocs = true)]
     ulong ValidatorRegistryMessageVersion { get; set; }
 
-    [ConfigItem(Description = "Instance ID of Shutter keyper set.",
-        DefaultValue = "0")]
-    ulong InstanceID { get; set; }
-
     [ConfigItem(Description = "The maximum amount of gas to use on Shutter transactions.",
-        DefaultValue = "10000000")]
+        DefaultValue = "10000000", HiddenFromDocs = true)]
     int EncryptedGasLimit { get; set; }
 
     [ConfigItem(Description = "Maximum amount of milliseconds into the slot to wait for Shutter keys before building block.",
-        DefaultValue = "1666")]
+        DefaultValue = "1666", HiddenFromDocs = true)]
     ushort MaxKeyDelay { get; }
+
+    [ConfigItem(Description = "Whether to build Shutter blocks or just give metrics on Shutter transactions.",
+        DefaultValue = "true", HiddenFromDocs = true)]
+    bool Validator { get; set; }
 
     public void Validate()
     {
