54a59ba17717d86050fe941f63f2d088812e98f5
ak88 <anders@nethermind.io>
2024-09-20 14:08:58 +0200

    Apply suggestions from code review

--- src/Nethermind/Nethermind.Evm.Benchmark/MultipleUnsignedOperations.cs
+++ src/Nethermind/Nethermind.Evm.Benchmark/MultipleUnsignedOperations.cs
@@ -76,7 +76,7 @@ public class MultipleUnsignedOperations
         _stateProvider.Commit(_spec);
 
         Console.WriteLine(MuirGlacier.Instance);
-        CodeInfoRepository codeInfoRepository = new(0);
+        CodeInfoRepository codeInfoRepository = new(BlockchainIds.Mainnet);
         _virtualMachine = new VirtualMachine(_blockhashProvider, MainnetSpecProvider.Instance, codeInfoRepository, new OneLoggerLogManager(NullLogger.Instance));
 
         _environment = new ExecutionEnvironment
