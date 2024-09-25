86c5c77fe47e17bdad5323cadfdf2475e2fe4765
Lautaro Emanuel <emlautarom1@gmail.com>
2024-09-25 16:41:54 -0300

    Merge test suites
    - No need to have them separated

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/RpcTransaction/TransactionForRpcTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/RpcTransaction/TransactionForRpcTests.cs
@@ -4,9 +4,12 @@
 using System;
 using System.Text.Json;
 using FluentAssertions;
+using FluentAssertions.Json;
 using Nethermind.Core;
+using Nethermind.Core.Crypto;
 using Nethermind.Facade.Eth.RpcTransaction;
 using Nethermind.Serialization.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace Nethermind.JsonRpc.Test.Modules.RpcTransaction;
@@ -27,6 +30,32 @@ public class TransactionForRpcTests
     public void SetUp()
     {
         AccessListTransactionForRpc.DefaultChainId = BlockchainIds.Mainnet;
+    }
+
+    [Test]
+    public void R_and_s_are_quantity_and_not_data()
+    {
+        byte[] r = new byte[32];
+        byte[] s = new byte[32];
+        r[1] = 1;
+        s[2] = 2;
+
+        Transaction tx = new()
+        {
+            Signature = new Signature(r, s, 27)
+        };
+
+        var txForRpc = TransactionForRpc.FromTransaction(tx);
+
+        EthereumJsonSerializer serializer = new();
+        string serialized = serializer.Serialize(txForRpc);
+
+        var json = JObject.Parse(serialized);
+        var expectedS = JObject.Parse("""{ "s": "0x20000000000000000000000000000000000000000000000000000000000"}""");
+        var expectedR = JObject.Parse("""{ "r": "0x1000000000000000000000000000000000000000000000000000000000000"}""");
+
+        json.Should().ContainSubtree(expectedS);
+        json.Should().ContainSubtree(expectedR);
     }
 
     [TestCaseSource(nameof(Transactions))]
