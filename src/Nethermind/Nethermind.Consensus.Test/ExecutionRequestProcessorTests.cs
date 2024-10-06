812025111551b98ffb56a6565342b254693f2fad
Rohit Ranjan <rohitrjn629@gmail.com>
2024-10-07 01:42:48 +0530

    add executionRequests processor tests

---
+++ src/Nethermind/Nethermind.Consensus.Test/ExecutionRequestProcessorTests.cs
@@ -0,0 +1,117 @@
+// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Linq;
+using Nethermind.Abi;
+using Nethermind.Consensus.ExecutionRequests;
+using Nethermind.Core;
+using Nethermind.Core.ExecutionRequest;
+using Nethermind.Core.Extensions;
+using Nethermind.Core.Specs;
+using Nethermind.Core.Test.Builders;
+using Nethermind.Db;
+using Nethermind.Evm;
+using Nethermind.Evm.Tracing;
+using Nethermind.Evm.TransactionProcessing;
+using Nethermind.Int256;
+using Nethermind.Logging;
+using Nethermind.Specs;
+using Nethermind.State;
+using Nethermind.Trie.Pruning;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Nethermind.Consensus.Test;
+
+public class ExecutionProcessorTests
+{
+    private ISpecProvider _specProvider;
+    private ITransactionProcessor _transactionProcessor;
+    private WorldState _stateProvider;
+    private static readonly UInt256 AccountBalance = 1.Ether();
+    private readonly Address DepositContractAddress = Eip6110Constants.MainnetDepositContractAddress;
+    private readonly Address eip7002Account = Eip7002Constants.WithdrawalRequestPredeployAddress;
+    private readonly Address eip7251Account = Eip7251Constants.ConsolidationRequestPredeployAddress;
+    private IReleaseSpec spec;
+    private readonly AbiSignature _depositEventABI = new("DepositEvent", AbiType.DynamicBytes, AbiType.DynamicBytes, AbiType.DynamicBytes, AbiType.DynamicBytes, AbiType.DynamicBytes);
+    private readonly AbiEncoder _abiEncoder = AbiEncoder.Instance;
+
+    ExecutionRequest[] executionDepositRequests = [TestItem.ExecutionRequestA, TestItem.ExecutionRequestB, TestItem.ExecutionRequestC];
+    ExecutionRequest[] executionWithdrawalRequests = [TestItem.ExecutionRequestD, TestItem.ExecutionRequestE, TestItem.ExecutionRequestF];
+    ExecutionRequest[] excutionConsolidationRequests = [TestItem.ExecutionRequestG, TestItem.ExecutionRequestH, TestItem.ExecutionRequestI];
+
+    [SetUp]
+    public void Setup()
+    {
+        
+
+        _specProvider = MainnetSpecProvider.Instance;
+        MemDb stateDb = new();
+        TrieStore trieStore = new(stateDb, LimboLogs.Instance);
+
+        _stateProvider = new WorldState(trieStore, new MemDb(), LimboLogs.Instance);
+        _stateProvider.CreateAccount(eip7002Account, AccountBalance);
+        _stateProvider.CreateAccount(eip7251Account, AccountBalance);
+        _stateProvider.Commit(_specProvider.GenesisSpec);
+        _stateProvider.CommitTree(0);
+
+        spec = _specProvider.GetFinalSpec();
+
+        spec.RequestsEnabled.Returns(true);
+        spec.DepositsEnabled.Returns(true);
+        spec.WithdrawalRequestsEnabled.Returns(true);
+        spec.ConsolidationRequestsEnabled.Returns(true);
+
+        spec.DepositContractAddress.Returns(DepositContractAddress);
+        spec.Eip7002ContractAddress.Returns(eip7002Account);
+        spec.Eip7251ContractAddress.Returns(eip7251Account);
+
+        _transactionProcessor = Substitute.For<ITransactionProcessor>();
+
+        _transactionProcessor.Execute(Arg.Any<Transaction>(), Arg.Any<BlockExecutionContext>(), Arg.Any<CallOutputTracer>())
+            .Returns(ci =>
+            {
+                Transaction transaction = ci.Arg<Transaction>();
+                CallOutputTracer tracer = ci.Arg<CallOutputTracer>();
+                if(transaction.To == eip7002Account)
+                {
+                    tracer.ReturnValue = executionWithdrawalRequests.FlatEncode();
+                } else if(transaction.To == eip7251Account){
+                    tracer.ReturnValue = excutionConsolidationRequests.FlatEncode();
+                } else {
+                    tracer.ReturnValue = Array.Empty<byte>();
+                }
+                return new TransactionResult();
+            });
+    }
+
+    [Test]
+    public void ShouldProcessExecutionRequests()
+    {
+        Block block = Build.A.Block.TestObject;
+        ExecutionRequestsProcessor executionRequestProcessor = new(_transactionProcessor);
+
+        TxReceipt[] txReceipts = [
+            Build.A.Receipt.WithLogs(
+                Build.A.LogEntry.WithData(
+                    _abiEncoder.Encode(AbiEncodingStyle.None, _depositEventABI, [ TestItem.PublicKeyA.Bytes, TestItem.AddressA.Bytes, BitConverter.GetBytes((ulong)1_000_000_000), TestItem.KeccakA.Bytes.ToArray(), BitConverter.GetBytes((ulong)1)])
+                ).WithAddress(DepositContractAddress).TestObject,
+                Build.A.LogEntry.WithData(
+                    _abiEncoder.Encode(AbiEncodingStyle.None, _depositEventABI, [ TestItem.PublicKeyB.Bytes, TestItem.AddressB.Bytes, BitConverter.GetBytes((ulong)2_000_000_000), TestItem.KeccakB.Bytes.ToArray(), BitConverter.GetBytes((ulong)2)])
+                ).WithAddress(DepositContractAddress).TestObject,
+                Build.A.LogEntry.WithData(
+                    _abiEncoder.Encode(AbiEncodingStyle.None, _depositEventABI, [ TestItem.PublicKeyC.Bytes, TestItem.AddressC.Bytes, BitConverter.GetBytes((ulong)3_000_000_000), TestItem.KeccakC.Bytes.ToArray(), BitConverter.GetBytes((ulong)3)])
+                ).WithAddress(DepositContractAddress).TestObject
+            ).TestObject
+        ];
+
+
+        executionRequestProcessor.ProcessExecutionRequests(block, _stateProvider, txReceipts, spec);
+
+        var processedRequests = block.ExecutionRequests.ToList();
+
+        Assert.That(processedRequests, Has.Count.EqualTo(9));
+        Assert.That(processedRequests, Is.EquivalentTo(executionDepositRequests.Concat(executionWithdrawalRequests).Concat(excutionConsolidationRequests)));
+    }
+}
--- src/Nethermind/Nethermind.Consensus/ExecutionRequests/ExecutionRequestProcessor.cs
+++ src/Nethermind/Nethermind.Consensus/ExecutionRequests/ExecutionRequestProcessor.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Nethermind.Abi;
 using Nethermind.Core;
-using Nethermind.Core.Crypto;
 using Nethermind.Core.ExecutionRequest;
 using Nethermind.Core.Extensions;
 using Nethermind.Core.Specs;
