e0c4a599d8f0ee1c8e3ce13d1c8620d4e3255a23
ak88 <anders@nethermind.io>
2024-10-07 16:00:46 +0200

    EIP-7702 and EIP-7251 (#7459)
    Co-authored-by: MarekM25 <marekm2504@gmail.com>
    Co-authored-by: Rohit Ranjan <rohitrjn629@gmail.com>
    Co-authored-by: Ben Adams <thundercat@illyriad.co.uk>
    Co-authored-by: Kamil Chodoła <43241881+kamilchodola@users.noreply.github.com>
    Co-authored-by: nethermind-machine <76107542+nethermind-machine@users.noreply.github.com>
    Co-authored-by: LukaszRozmej <LukaszRozmej@users.noreply.github.com>
    Co-authored-by: Daniel Kyutae Jung <45947071+pxyxyrus@users.noreply.github.com>
    Co-authored-by: Ahmad Bitar <33181301+smartprogrammer93@users.noreply.github.com>
    Co-authored-by: Lukasz Rozmej <lukasz.rozmej@gmail.com>
    Co-authored-by: Amirul Ashraf <asdacap@gmail.com>
    Co-authored-by: yerke26 <160018032+yerke26@users.noreply.github.com>
    Co-authored-by: yeerke <ezhumalin01@gmail.com>
    Co-authored-by: Ruben Buniatyan <rubo@users.noreply.github.com>
    Co-authored-by: Alexey <me@flcl.me>
    Co-authored-by: Nikita Mescheryakov <root@nikitam.io>
    Co-authored-by: Lautaro Emanuel <31224949+emlautarom1@users.noreply.github.com>
    Co-authored-by: Marcos Antonio Maceo <35319980+stdevMac@users.noreply.github.com>
    Co-authored-by: Yaroslav Kukharuk <i.kukharuk@gmail.com>
    Co-authored-by: Ahmad Bitar <amb014@diyarme.com>
    Co-authored-by: Oleg Jakushkin <oleg.jakushkin@gmail.com>
    Co-authored-by: Kamil Chodoła <kamil@nethermind.io>
    Co-authored-by: healthyyyoung <healthyoung@proton.me>
    Co-authored-by: Ahmad Bitar <smartprogrammer@windowslive.com>

--- src/Nethermind/Nethermind.TxPool/TxPool.cs
+++ src/Nethermind/Nethermind.TxPool/TxPool.cs
@@ -103,7 +103,7 @@ namespace Nethermind.TxPool
             _headInfo = chainHeadInfoProvider ?? throw new ArgumentNullException(nameof(chainHeadInfoProvider));
             _txPoolConfig = txPoolConfig;
             _blobReorgsSupportEnabled = txPoolConfig.BlobsSupport.SupportsReorgs();
-            _accounts = _accountCache = new AccountCache(_headInfo.AccountStateProvider);
+            _accounts = _accountCache = new AccountCache(_headInfo.ReadOnlyStateProvider);
             _specProvider = _headInfo.SpecProvider;
 
             MemoryAllowance.MemPoolSize = txPoolConfig.Size;
@@ -124,34 +124,36 @@ namespace Nethermind.TxPool
 
             _headInfo.HeadChanged += OnHeadChange;
 
-            _preHashFilters = new IIncomingTxFilter[]
-            {
+            _preHashFilters =
+            [
                 new NotSupportedTxFilter(txPoolConfig, _logger),
                 new GasLimitTxFilter(_headInfo, txPoolConfig, _logger),
                 new PriorityFeeTooLowFilter(_logger),
                 new FeeTooLowFilter(_headInfo, _transactions, _blobTransactions, thereIsPriorityContract, _logger),
                 new MalformedTxFilter(_specProvider, validator, _logger)
-            };
+            ];
 
-            List<IIncomingTxFilter> postHashFilters = new()
-            {
+            List<IIncomingTxFilter> postHashFilters =
+            [
                 new NullHashTxFilter(), // needs to be first as it assigns the hash
                 new AlreadyKnownTxFilter(_hashCache, _logger),
                 new UnknownSenderFilter(ecdsa, _logger),
-                new TxTypeTxFilter(_transactions, _blobTransactions), // has to be after UnknownSenderFilter as it uses sender
+                new TxTypeTxFilter(_transactions,
+                    _blobTransactions), // has to be after UnknownSenderFilter as it uses sender
                 new BalanceZeroFilter(thereIsPriorityContract, _logger),
                 new BalanceTooLowFilter(_transactions, _blobTransactions, _logger),
                 new LowNonceFilter(_logger), // has to be after UnknownSenderFilter as it uses sender
                 new FutureNonceFilter(txPoolConfig),
                 new GapNonceFilter(_transactions, _blobTransactions, _logger),
-            };
+                new RecoverAuthorityFilter(ecdsa)
+            ];
 
             if (incomingTxFilter is not null)
             {
                 postHashFilters.Add(incomingTxFilter);
             }
 
-            postHashFilters.Add(new DeployedCodeFilter(_specProvider));
+            postHashFilters.Add(new DeployedCodeFilter(chainHeadInfoProvider.ReadOnlyStateProvider, chainHeadInfoProvider.CodeInfoRepository, _specProvider));
 
             _postHashFilters = postHashFilters.ToArray();
 
@@ -304,6 +306,7 @@ namespace Nethermind.TxPool
             long discoveredForPendingTxs = 0;
             long discoveredForHashCache = 0;
             long eip1559Txs = 0;
+            long eip7702Txs = 0;
             long blobTxs = 0;
             long blobs = 0;
 
@@ -333,6 +336,11 @@ namespace Nethermind.TxPool
                     }
                 }
 
+                if (blockTx.Type == TxType.SetCode)
+                {
+                    eip7702Txs++;
+                }
+
                 if (!IsKnown(txHash))
                 {
                     discoveredForHashCache++;
@@ -355,6 +363,7 @@ namespace Nethermind.TxPool
                 Metrics.DarkPoolRatioLevel1 = (float)discoveredForHashCache / transactionsInBlock;
                 Metrics.DarkPoolRatioLevel2 = (float)discoveredForPendingTxs / transactionsInBlock;
                 Metrics.Eip1559TransactionsRatio = (float)eip1559Txs / transactionsInBlock;
+                Metrics.Eip7702TransactionsInBlock = eip7702Txs;
                 Metrics.BlobTransactionsInBlock = blobTxs;
                 Metrics.BlobsInBlock = blobs;
             }
@@ -905,6 +914,7 @@ Total Evicted:          {Metrics.PendingTransactionsEvicted,24:N0}
 ------------------------------------------------
 Ratios in last block:
 * Eip1559 Transactions: {Metrics.Eip1559TransactionsRatio,24:P5}
+* Eip7702 Transactions: {Metrics.Eip7702TransactionsInBlock,24:P5}
 * DarkPool Level1:      {Metrics.DarkPoolRatioLevel1,24:P5}
 * DarkPool Level2:      {Metrics.DarkPoolRatioLevel2,24:P5}
 Amounts:
