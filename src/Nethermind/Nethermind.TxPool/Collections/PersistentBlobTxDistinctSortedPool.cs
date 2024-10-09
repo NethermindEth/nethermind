eb0af37749d6f1020f37786768a8a11e99249c8f
Marcin Sobczak <marcindsobczak@gmail.com>
2024-10-09 13:18:32 +0200

    fix

--- src/Nethermind/Nethermind.TxPool/Collections/PersistentBlobTxDistinctSortedPool.cs
+++ src/Nethermind/Nethermind.TxPool/Collections/PersistentBlobTxDistinctSortedPool.cs
@@ -53,12 +53,12 @@ public class PersistentBlobTxDistinctSortedPool : BlobTxDistinctSortedPool
         stopwatch.Stop();
     }
 
-    public override bool TryInsert(ValueHash256 hash, Transaction fullBlobTx, out Transaction? removed)
+    protected override bool InsertCore(ValueHash256 key, Transaction value, AddressAsKey groupKey)
     {
-        if (base.TryInsert(fullBlobTx.Hash, new LightTransaction(fullBlobTx), out removed))
+        if (base.InsertCore(key, value, groupKey))
         {
-            _blobTxCache.Set(fullBlobTx.Hash, fullBlobTx);
-            _blobTxStorage.Add(fullBlobTx);
+            _blobTxCache.Set(value.Hash, value);
+            _blobTxStorage.Add(value);
             return true;
         }
 
