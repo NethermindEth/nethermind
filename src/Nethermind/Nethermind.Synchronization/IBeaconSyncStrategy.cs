c02b8393a81788e8c6dc2f4c2e3e159bb212a5a8
Marcin Sobczak <marcindsobczak@gmail.com>
2024-10-16 13:59:16 +0200

    fix tests

--- src/Nethermind/Nethermind.Synchronization/IBeaconSyncStrategy.cs
+++ src/Nethermind/Nethermind.Synchronization/IBeaconSyncStrategy.cs
@@ -24,7 +24,7 @@ namespace Nethermind.Synchronization
 
     public interface IBeaconSyncStrategy
     {
-        bool ShouldBeInBeaconHeaders(bool canBeInBeaconHeaders);
+        bool ShouldBeInBeaconHeaders(bool canBeInBeaconHeaders = false);
         bool ShouldBeInBeaconModeControl();
         bool IsBeaconSyncFinished(BlockHeader? blockHeader);
 
