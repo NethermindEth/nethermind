e1026071b0387af6b329452baf75c3b2b3f4b602
Alex Bespalov <alexb5dh@gmail.com>
2024-10-17 00:45:47 +0300

    Code cleanup

--- src/Nethermind/Nethermind.Core/Extensions/EncodingExtensions.cs
+++ src/Nethermind/Nethermind.Core/Extensions/EncodingExtensions.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
+// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
@@ -10,7 +10,6 @@ namespace Nethermind.Core.Extensions;
 
 public static class EncodingExtensions
 {
-    // TODO add unit tests
     public static bool TryGetString(this Encoding encoding, ReadOnlySequence<byte> bytes, [NotNullWhen(true)] out string? result)
     {
         try
