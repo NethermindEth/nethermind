a70e1feaa68909bfb5cc9b22b684bd4c9d97cdc5
Ahmad Bitar <smartprogrammer@windowslive.com>
2024-10-18 00:15:02 +0300

    real fixes for tests + one missing TxValidator.cs fix

--- src/Nethermind/Ethereum.Test.Base/AuthorizationListJson.cs
+++ src/Nethermind/Ethereum.Test.Base/AuthorizationListJson.cs
@@ -7,11 +7,11 @@ using Nethermind.Int256;
 namespace Ethereum.Test.Base;
 public class AuthorizationListJson
 {
-    public ulong ChainId { get; set; }
+    public UInt256 ChainId { get; set; }
     public Address Address { get; set; }
-    public ulong Nonce { get; set; }
+    public UInt256 Nonce { get; set; }
     public ulong V { get; set; }
-    public UInt256 R { get; set; }
-    public UInt256 S { get; set; }
+    public string R { get; set; }
+    public string S { get; set; }
     public Address Signer { get; set; }
 }
