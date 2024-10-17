99d56af0c2921d823163bef97923072d3ddd84bc
ak88 <anders.holmbjerg@hotmail.com>
2024-10-17 22:01:57 +0200

    revert test change

--- src/Nethermind/Ethereum.Test.Base/AuthorizationListJson.cs
+++ src/Nethermind/Ethereum.Test.Base/AuthorizationListJson.cs
@@ -7,11 +7,11 @@ using Nethermind.Int256;
 namespace Ethereum.Test.Base;
 public class AuthorizationListJson
 {
-    public UInt256 ChainId { get; set; }
+    public ulong ChainId { get; set; }
     public Address Address { get; set; }
-    public UInt256 Nonce { get; set; }
+    public ulong Nonce { get; set; }
     public ulong V { get; set; }
-    public byte[] R { get; set; }
-    public byte[] S { get; set; }
+    public UInt256 R { get; set; }
+    public UInt256 S { get; set; }
     public Address Signer { get; set; }
 }
