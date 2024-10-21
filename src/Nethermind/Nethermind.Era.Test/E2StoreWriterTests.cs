bf82a668525d7c41222f66ba8ccbd6ab8739b100
Amirul Ashraf <asdacap@gmail.com>
2024-10-21 17:19:44 +0800

    Slight cleanup

--- src/Nethermind/Nethermind.Era.Test/E2StoreWriterTests.cs
+++ src/Nethermind/Nethermind.Era.Test/E2StoreWriterTests.cs
@@ -13,6 +13,8 @@ using Snappier;
 namespace Nethermind.Era1.Test;
 internal class E2StoreWriterTests
 {
+    byte[] TestBytes = new byte[] { 0x0f, 0xf0, 0xff, 0xff };
+
     [TestCase(EntryTypes.Version)]
     [TestCase(EntryTypes.CompressedHeader)]
     [TestCase(EntryTypes.CompressedBody)]
@@ -62,7 +64,7 @@ internal class E2StoreWriterTests
         using MemoryStream stream = new MemoryStream();
         using E2StoreWriter sut = new E2StoreWriter(stream);
 
-        await sut.WriteEntry(EntryTypes.CompressedHeader, new byte[] { 0xff, 0xff, 0xff, 0xff });
+        await sut.WriteEntry(EntryTypes.CompressedHeader, TestBytes);
         byte[] bytes = stream.ToArray();
 
         Assert.That(bytes[6], Is.EqualTo(0));
@@ -75,11 +77,10 @@ internal class E2StoreWriterTests
         using MemoryStream stream = new MemoryStream();
         using E2StoreWriter sut = new E2StoreWriter(stream);
 
-        byte[] bytes = new byte[] { 0x0f, 0xf0, 0xff, 0xff };
-        await sut.WriteEntry(EntryTypes.CompressedHeader, bytes);
+        await sut.WriteEntry(EntryTypes.CompressedHeader, TestBytes);
         byte[] result = stream.ToArray();
 
-        Assert.That(new ArraySegment<byte>(result, E2StoreWriter.HeaderSize, bytes.Length), Is.EquivalentTo(bytes));
+        Assert.That(new ArraySegment<byte>(result, E2StoreWriter.HeaderSize, TestBytes.Length), Is.EquivalentTo(TestBytes));
     }
 
     [Test]
@@ -87,9 +88,8 @@ internal class E2StoreWriterTests
     {
         using MemoryStream stream = new MemoryStream();
         using E2StoreWriter sut = new E2StoreWriter(stream);
-        byte[] bytes = new byte[] { 0x0f, 0xf0, 0xff, 0xff };
 
-        await sut.WriteEntryAsSnappy(EntryTypes.CompressedHeader, bytes);
+        await sut.WriteEntryAsSnappy(EntryTypes.CompressedHeader, TestBytes);
         stream.Position = E2StoreWriter.HeaderSize;
         using var snappy = new SnappyStream(stream, System.IO.Compression.CompressionMode.Decompress);
         byte[] buffer = new byte[32];
@@ -102,33 +102,10 @@ internal class E2StoreWriterTests
     {
         using MemoryStream stream = new MemoryStream();
         using E2StoreWriter sut = new E2StoreWriter(stream);
-        byte[] bytes = new byte[] { 0x0f, 0xf0, 0xff, 0xff };
 
-        int result = await sut.WriteEntryAsSnappy(EntryTypes.CompressedHeader, bytes);
+        int result = await sut.WriteEntryAsSnappy(EntryTypes.CompressedHeader, TestBytes);
 
         Assert.That(result, Is.EqualTo(stream.Length));
-    }
-
-    [Test]
-    public async Task ReadEntryValue_ReadingValueBytesOfEntry_ReturnsBytes()
-    {
-        string tmpFile = Path.GetTempFileName();
-        E2StoreWriter sut = new E2StoreWriter(File.OpenWrite(tmpFile));
-        byte[] bytes = new byte[] { 0x0f, 0xf0, 0xff, 0xff };
-        await sut.WriteEntry(EntryTypes.Accumulator, bytes);
-        sut.Dispose();
-
-        using E2StoreReader reader = new E2StoreReader(tmpFile);
-        IByteBuffer buffer = UnpooledByteBufferAllocator.Default.Buffer(bytes.Length);
-        try
-        {
-            (var readBytes, _) = reader.ReadEntryAndDecode(0, buf => buf.ReadAllBytesAsArray(), EntryTypes.Accumulator);
-            Assert.That(readBytes, Is.EquivalentTo(bytes));
-        }
-        finally
-        {
-            buffer.Release();
-        }
     }
 
     [Test]
@@ -136,15 +113,13 @@ internal class E2StoreWriterTests
     {
         string tmpFile = Path.GetTempFileName();
         E2StoreWriter sut = new E2StoreWriter(File.OpenWrite(tmpFile));
-        byte[] bytes = new byte[] { 0x0f, 0xf0, 0xff, 0xff };
-        await sut.WriteEntry(EntryTypes.Accumulator, bytes);
+        await sut.WriteEntry(EntryTypes.Accumulator, TestBytes);
         sut.Dispose();
 
         using E2StoreReader reader = new E2StoreReader(tmpFile);
-        (var readBytes, long _) = reader.ReadEntryAndDecode(0, buf => buf.ReadAllBytesAsArray(), EntryTypes.Accumulator);
-        Assert.That(readBytes, Is.EquivalentTo(bytes));
-
-        Assert.That(readBytes.Length, Is.EqualTo(bytes.Length));
+        (var readBytes, _) = reader.ReadEntryAndDecode(0, buf => buf.ReadAllBytesAsArray(), EntryTypes.Accumulator);
+        Assert.That(readBytes, Is.EquivalentTo(TestBytes));
+        Assert.That(readBytes.Length, Is.EqualTo(TestBytes.Length));
     }
 
     [Test]
@@ -152,60 +127,16 @@ internal class E2StoreWriterTests
     {
         string tmpFile = Path.GetTempFileName();
         using E2StoreWriter sut = new E2StoreWriter(File.OpenWrite(tmpFile));
-        byte[] bytes = new byte[] { 0x0f, 0xf0, 0xff, 0xff };
         MemoryStream compressed = new();
         using SnappyStream snappy = new SnappyStream(compressed, System.IO.Compression.CompressionMode.Compress);
-        snappy.Write(bytes);
+        snappy.Write(TestBytes);
         snappy.Flush();
-        byte[] compressedBytes = compressed.ToArray();
         long position = sut.Position;
-        await sut.WriteEntry(EntryTypes.CompressedHeader, compressedBytes);
+        await sut.WriteEntry(EntryTypes.CompressedHeader, compressed.ToArray());
         sut.Dispose();
 
         using E2StoreReader reader = new E2StoreReader(tmpFile);
-        IByteBuffer buffer = UnpooledByteBufferAllocator.Default.Buffer(32);
-        try
-        {
-            (var readBytes, _) = await reader.ReadSnappyCompressedEntryAndDecode(position, buf => buf.ReadAllBytesAsArray(), EntryTypes.CompressedHeader, default);
-            Assert.That(readBytes, Is.EquivalentTo(bytes));
-        }
-        finally
-        {
-            buffer.Release();
-        }
+        (var readBytes, _) = await reader.ReadSnappyCompressedEntryAndDecode(position, buf => buf.ReadAllBytesAsArray(), EntryTypes.CompressedHeader, default);
+        Assert.That(readBytes, Is.EquivalentTo(TestBytes));
     }
-
-    [Test]
-    public void Test()
-    {
-        //TODO possible optimization avoid alloc snappy stream by not disposing the stream and reusing like this
-        using MemoryStream stream = new();
-        byte[] bytes = new byte[] { 0x0f, 0xf0, 0xff, 0xff };
-        byte[] bytes1 = new byte[] { 0x1, 0x2, 0x3, 0x4 };
-        MemoryStream compressed = new();
-        using SnappyStream snappy = new SnappyStream(compressed, System.IO.Compression.CompressionMode.Compress);
-        snappy.Write(bytes);
-        snappy.Flush();
-        byte[] compressedBytes = compressed.ToArray();
-        compressed.Seek(0, SeekOrigin.Begin);
-        using SnappyStream snappyDecom = new SnappyStream(compressed, System.IO.Compression.CompressionMode.Decompress);
-        var decomBytes = new byte[bytes.Length];
-        snappyDecom.Read(decomBytes, 0, decomBytes.Length);
-        decomBytes.Should().BeEquivalentTo(bytes);
-
-        compressed.SetLength(0);
-        var snappyHeader = new byte[]
-        {
-            0xff, 0x06, 0x00, 0x00, 0x73, 0x4e, 0x61, 0x50, 0x70, 0x59
-        };
-        compressed.Write(snappyHeader, 0, snappyHeader.Length);
-        snappy.Write(bytes1);
-        snappy.Flush();
-        compressed.Seek(0, SeekOrigin.Begin);
-
-        snappyDecom.Read(decomBytes, 0, decomBytes.Length);
-        decomBytes.Should().BeEquivalentTo(bytes1);
-
-    }
-
 }
