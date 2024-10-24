80d22b9281f59b5d9b857f9bf30153cdeba4b88e
ak88 <anders.holmbjerg@hotmail.com>
2024-10-24 23:49:31 +0200

    benchmark fix

--- src/Nethermind/Nethermind.Evm.Benchmark/EvmBenchmarks.cs
+++ src/Nethermind/Nethermind.Evm.Benchmark/EvmBenchmarks.cs
@@ -40,7 +40,6 @@ namespace Nethermind.Evm.Benchmark
 
             TrieStore trieStore = new(new MemDb(), new OneLoggerLogManager(NullLogger.Instance));
             IKeyValueStore codeDb = new MemDb();
-
             _stateProvider = new WorldState(trieStore, codeDb, new OneLoggerLogManager(NullLogger.Instance));
             _stateProvider.CreateAccount(Address.Zero, 1000.Ether());
             _stateProvider.Commit(_spec);
@@ -59,7 +58,7 @@ namespace Nethermind.Evm.Benchmark
                 inputData: default
             );
 
-            _evmState = new EvmState(long.MaxValue, _environment, ExecutionType.TRANSACTION, true, _stateProvider.TakeSnapshot(), false);
+            _evmState = new EvmState(long.MaxValue, _environment, ExecutionType.TRANSACTION, _stateProvider.TakeSnapshot());
         }
 
         [Benchmark]
