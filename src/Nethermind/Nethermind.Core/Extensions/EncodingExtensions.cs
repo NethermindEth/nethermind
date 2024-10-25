c98b0dafc2b69ca9a66b6864ed67dd23086f015d
Alex Bespalov <alexb5dh@gmail.com>
2024-10-25 12:54:29 +0300

    Handle multi-segment sequence via `LimitedArrayBufferWriter` and catching `ArgumentException`

--- src/Nethermind/Nethermind.Core/Extensions/EncodingExtensions.cs
+++ src/Nethermind/Nethermind.Core/Extensions/EncodingExtensions.cs
@@ -10,17 +10,56 @@ namespace Nethermind.Core.Extensions;
 
 public static class EncodingExtensions
 {
+    private class LimitedArrayBufferWriter<T>(T[] buffer, int limit) : IBufferWriter<T>
+    {
+        private int _index;
+
+        public void Advance(int count)
+        {
+            if (count < 0)
+                throw new ArgumentException(null, nameof(count));
+
+            _index += count;
+        }
+
+        public Memory<T> GetMemory(int sizeHint = 0) => buffer.AsMemory(_index, limit - _index);
+
+        public Span<T> GetSpan(int sizeHint = 0) => buffer.AsSpan(_index, limit - _index);
+    }
+
+    private static string GetStringSlice(this Encoding encoding, ReadOnlySpan<byte> span, Span<char> chars, out bool completed)
+    {
+        encoding.GetDecoder().Convert(span, chars, true, out _, out int charsUsed, out completed);
+        return new(chars[..charsUsed]);
+    }
+
+    private static string GetStringSliceMultiSegment(this Encoding encoding, ReadOnlySequence<byte> sequence, char[] charArray, int charCount,
+        out bool completed)
+    {
+        try
+        {
+            var writer = new LimitedArrayBufferWriter<char>(charArray, charCount);
+            encoding.GetDecoder().Convert(sequence, writer, true, out long charsUsed, out completed);
+            return new(charArray.AsSpan(0, (int)charsUsed));
+        }
+        catch (ArgumentException exception) when (exception.ParamName == "chars")
+        {
+            completed = false;
+            return  new(charArray.AsSpan(0, charCount));
+        }
+    }
+
     public static bool TryGetStringSlice(this Encoding encoding, ReadOnlySequence<byte> sequence, int charCount,
         out bool completed, [NotNullWhen(true)] out string? result)
     {
-        char[] charsArray = ArrayPool<char>.Shared.Rent(charCount);
+        char[] charArray = ArrayPool<char>.Shared.Rent(charCount);
 
         try
         {
-            Span<char> chars = charsArray.AsSpan(0, charCount);
-            ReadOnlySpan<byte> first = sequence.FirstSpan;
-            encoding.GetDecoder().Convert(first, chars, true, out _, out int charsUsed, out completed);
-            result = new(chars[..charsUsed]);
+            result = sequence.IsSingleSegment
+                ? GetStringSlice(encoding, sequence.FirstSpan, charArray.AsSpan(0, charCount), out completed)
+                : GetStringSliceMultiSegment(encoding, sequence, charArray, charCount, out completed);
+
             return true;
         }
         catch (Exception)
@@ -31,7 +70,7 @@ public static class EncodingExtensions
         }
         finally
         {
-            ArrayPool<char>.Shared.Return(charsArray);
+            ArrayPool<char>.Shared.Return(charArray);
         }
     }
 
