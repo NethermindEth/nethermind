7f6c76c22db9daa73cf1a0833e6d8bcb1dd738de
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-10-28 13:04:36 +0000

    implement bls aggregate signatures

--- src/Nethermind/Nethermind.Crypto/BlsSigner.cs
+++ src/Nethermind/Nethermind.Crypto/BlsSigner.cs
@@ -8,6 +8,7 @@ using Nethermind.Core.Collections;
 
 namespace Nethermind.Crypto;
 
+using G1 = Bls.P1;
 using G1Affine = Bls.P1Affine;
 using G2 = Bls.P2;
 using G2Affine = Bls.P2Affine;
@@ -15,6 +16,7 @@ using GT = Bls.PT;
 
 public static class BlsSigner
 {
+    public const int PkCompressedSz = 384 / 8;
     private static readonly byte[] Cryptosuite = Encoding.UTF8.GetBytes("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_");
     private const int InputLength = 64;
 
@@ -25,6 +27,29 @@ public static class BlsSigner
         p.HashTo(message, Cryptosuite);
         p.SignWith(sk);
         return new(p.Compress());
+    }
+
+    [SkipLocalsInit]
+    public static Signature SignAggregate(ReadOnlySpan<byte> skBytes, ReadOnlySpan<byte> message)
+    {
+        if (skBytes.Length % 32 != 0)
+        {
+            throw new Bls.BlsException(Bls.ERROR.WRONGSIZE);
+        }
+
+        G2 p = new(stackalloc long[G2.Sz]);
+        G2 agg = new(stackalloc long[G2.Sz]);
+        agg.Zero();
+
+        for (int i = 0; i < skBytes.Length; i += 32)
+        {
+            Bls.SecretKey sk = new(skBytes.Slice(i, 32));
+            p.HashTo(message, Cryptosuite);
+            p.SignWith(sk);
+            agg.Aggregate(p.ToAffine());
+        }
+
+        return new(agg.Compress());
     }
 
     [SkipLocalsInit]
@@ -51,6 +76,27 @@ public static class BlsSigner
             // point not on curve
             return false;
         }
+    }
+
+    [SkipLocalsInit]
+    public static bool VerifyAggregate(ReadOnlySpan<byte> publicKeys, Signature signature, ReadOnlySpan<byte> message)
+    {
+        if (publicKeys.Length % PkCompressedSz != 0)
+        {
+            throw new Bls.BlsException(Bls.ERROR.WRONGSIZE);
+        }
+
+        G1Affine pk = new(stackalloc long[G1Affine.Sz]);
+        G1 agg = new(stackalloc long[G1.Sz]);
+        agg.Zero();
+
+        for (int i = 0; i < publicKeys.Length; i += PkCompressedSz)
+        {
+            pk.Decode(publicKeys.Slice(i, PkCompressedSz));
+            agg.Aggregate(pk);
+        }
+
+        return Verify(agg.ToAffine(), signature, message);
     }
 
     // Compressed G2 point
