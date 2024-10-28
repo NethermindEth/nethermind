db90356c50a630530a7302806bdbb1984e75c60d
Ruben Buniatyan <rubo@users.noreply.github.com>
2024-10-28 20:18:54 +0100

    Revert `FileResponseTracer.cs`

--- tools/Nethermind.Tools.Kute/ResponseTracer/FileResponseTracer.cs
+++ tools/Nethermind.Tools.Kute/ResponseTracer/FileResponseTracer.cs
@@ -5,15 +5,20 @@ using System.Text.Json;
 
 namespace Nethermind.Tools.Kute.ResponseTracer;
 
-public class FileResponseTracer(string tracesFilePath) : IResponseTracer
+public class FileResponseTracer : IResponseTracer
 {
-    private readonly string _tracesFilePath = tracesFilePath ?? throw new ArgumentNullException(nameof(tracesFilePath));
+    private readonly string _tracesFilePath;
+
+    public FileResponseTracer(string tracesFilePath)
+    {
+        _tracesFilePath = tracesFilePath;
+    }
 
     public async Task TraceResponse(JsonDocument? response)
     {
         await using StreamWriter sw = File.Exists(_tracesFilePath)
             ? File.AppendText(_tracesFilePath)
-            : File.CreateText(_tracesFilePath!);
+            : File.CreateText(_tracesFilePath);
 
         await sw.WriteLineAsync(response?.RootElement.ToString() ?? "null");
     }
