55c90d6ec5f4770c1e5bfe7bf414273436ebe1cc
yerke26 <yerkebulan@nethermind.io>
2024-10-30 02:49:23 +0600

    add t8n input arguments

--- tools/Evm/Evm/Program.cs
+++ tools/Evm/Evm/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.CommandLine;
+using Evm.t8n;
+
+namespace Evm;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var rootCmd = new RootCommand { Name = "Evm" };
+
+        T8NCommand.Configure(ref rootCmd);
+
+        await rootCmd.InvokeAsync(args);
+    }
+}
