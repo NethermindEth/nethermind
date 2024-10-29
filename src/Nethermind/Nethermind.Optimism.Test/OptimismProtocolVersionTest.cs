2773bf213d2f72be697beb08402e6918a8f15142
Lautaro Emanuel <emlautarom1@gmail.com>
2024-10-29 18:03:55 -0300

    Change to discriminated union
    - Use abstract + sealed

--- src/Nethermind/Nethermind.Optimism.Test/OptimismProtocolVersionTest.cs
+++ src/Nethermind/Nethermind.Optimism.Test/OptimismProtocolVersionTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using FluentAssertions;
 using Nethermind.Core.Extensions;
@@ -8,70 +9,93 @@ namespace Nethermind.Optimism.Test;
 
 public class OptimismProtocolVersionTest
 {
-    private static IEnumerable<(string, OptimismProtocolVersionV0)> V0ParseCases()
+    private static IEnumerable<(string, OptimismProtocolVersion.V0)> V0ParseCases()
     {
-        yield return ("0x0000000000000000000000000000000000000000000000000000000000000000", new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0));
-        yield return ("0x0000000000000000000000000000000000000000000000000000000000000100", new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 1));
-        yield return ("0x0000000000000000000000000000000000000000000000000000010000000000", new OptimismProtocolVersionV0(new byte[8], 0, 0, 1, 0));
-        yield return ("0x0000000000000000000000000000000000000400000003000000020000000100", new OptimismProtocolVersionV0(new byte[8], 4, 3, 2, 1));
-        yield return ("0x0000000000000000000000000000000000000000000064000000020000000000", new OptimismProtocolVersionV0(new byte[8], 0, 100, 2, 0));
-        yield return ("0x000000000000004f502d6d6f6400000000002a00000000000000020000000100", new OptimismProtocolVersionV0([(byte)'O', (byte)'P', (byte)'-', (byte)'m', (byte)'o', (byte)'d', 0, 0], 42, 0, 2, 1));
-        yield return ("0x00000000000000626574612e3132330000000100000000000000000000000000", new OptimismProtocolVersionV0([(byte)'b', (byte)'e', (byte)'t', (byte)'a', (byte)'.', (byte)'1', (byte)'2', (byte)'3'], 1, 0, 0, 0));
-        yield return ("0x0000000000000061620100000000000000002a00000000000000020000000000", new OptimismProtocolVersionV0([(byte)'a', (byte)'b', 1, 0, 0, 0, 0, 0], 42, 0, 2, 0));
-        yield return ("0x0000000000000001020304050607080000002a00000000000000020000000000", new OptimismProtocolVersionV0([1, 2, 3, 4, 5, 6, 7, 8], 42, 0, 2, 0));
+        yield return ("0x0000000000000000000000000000000000000000000000000000000000000000", new(new byte[8], 0, 0, 0, 0));
+        yield return ("0x0000000000000000000000000000000000000000000000000000000000000100", new(new byte[8], 0, 0, 0, 1));
+        yield return ("0x0000000000000000000000000000000000000000000000000000010000000000", new(new byte[8], 0, 0, 1, 0));
+        yield return ("0x0000000000000000000000000000000000000400000003000000020000000100", new(new byte[8], 4, 3, 2, 1));
+        yield return ("0x0000000000000000000000000000000000000000000064000000020000000000", new(new byte[8], 0, 100, 2, 0));
+        yield return ("0x000000000000004f502d6d6f6400000000002a00000000000000020000000100", new([(byte)'O', (byte)'P', (byte)'-', (byte)'m', (byte)'o', (byte)'d', 0, 0], 42, 0, 2, 1));
+        yield return ("0x00000000000000626574612e3132330000000100000000000000000000000000", new([(byte)'b', (byte)'e', (byte)'t', (byte)'a', (byte)'.', (byte)'1', (byte)'2', (byte)'3'], 1, 0, 0, 0));
+        yield return ("0x0000000000000061620100000000000000002a00000000000000020000000000", new([(byte)'a', (byte)'b', 1, 0, 0, 0, 0, 0], 42, 0, 2, 0));
+        yield return ("0x0000000000000001020304050607080000002a00000000000000020000000000", new([1, 2, 3, 4, 5, 6, 7, 8], 42, 0, 2, 0));
     }
-
     [TestCaseSource(nameof(V0ParseCases))]
-    public void Parse_OptimismProtocolVersionV0((string HexString, OptimismProtocolVersionV0 Expected) testCase)
+    public void Parse_OptimismProtocolVersionV0((string HexString, OptimismProtocolVersion.V0 Expected) testCase)
     {
         var bytes = Bytes.FromHexString(testCase.HexString);
-        var actual = IOptimismProtocolVersion.Read(bytes);
+        var actual = OptimismProtocolVersion.Read(bytes);
 
         actual.Should().Be(testCase.Expected);
     }
 
-    private static IEnumerable<(OptimismProtocolVersionV0, OptimismProtocolVersionV0, int)> V0CompareCases()
+    private static IEnumerable<(OptimismProtocolVersion.V0, OptimismProtocolVersion.V0, int)> V0CompareCases()
     {
         // Pre-release
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 1), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 2), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, uint.MaxValue), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), 0);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 42), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 42), 0);
+        yield return (new(new byte[8], 0, 0, 0, 0), new(new byte[8], 0, 0, 0, 1), 1);
+        yield return (new(new byte[8], 0, 0, 0, 0), new(new byte[8], 0, 0, 0, 2), 1);
+        yield return (new(new byte[8], 0, 0, 0, 0), new(new byte[8], 0, 0, 0, uint.MaxValue), 1);
+        yield return (new(new byte[8], 0, 0, 0, 0), new(new byte[8], 0, 0, 0, 0), 0);
+        yield return (new(new byte[8], 0, 0, 0, 42), new(new byte[8], 0, 0, 0, 42), 0);
 
         // Patch
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 1, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 2, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 1, 0), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, uint.MaxValue, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, uint.MaxValue - 1, 0), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), 0);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 101, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 101, 0), 0);
+        yield return (new(new byte[8], 0, 0, 1, 0), new(new byte[8], 0, 0, 0, 0), 1);
+        yield return (new(new byte[8], 0, 0, 2, 0), new(new byte[8], 0, 0, 1, 0), 1);
+        yield return (new(new byte[8], 0, 0, uint.MaxValue, 0), new(new byte[8], 0, 0, uint.MaxValue - 1, 0), 1);
+        yield return (new(new byte[8], 0, 0, 0, 0), new(new byte[8], 0, 0, 0, 0), 0);
+        yield return (new(new byte[8], 0, 0, 101, 0), new(new byte[8], 0, 0, 101, 0), 0);
 
         // Minor
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 1, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 2, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 1, 0, 0), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, uint.MaxValue, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, uint.MaxValue - 1, 0, 0), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), 0);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 200, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 200, 0, 0), 0);
+        yield return (new(new byte[8], 0, 1, 0, 0), new(new byte[8], 0, 0, 0, 0), 1);
+        yield return (new(new byte[8], 0, 2, 0, 0), new(new byte[8], 0, 1, 0, 0), 1);
+        yield return (new(new byte[8], 0, uint.MaxValue, 0, 0), new(new byte[8], 0, uint.MaxValue - 1, 0, 0), 1);
+        yield return (new(new byte[8], 0, 0, 0, 0), new(new byte[8], 0, 0, 0, 0), 0);
+        yield return (new(new byte[8], 0, 200, 0, 0), new(new byte[8], 0, 200, 0, 0), 0);
 
         // Major
-        yield return (new OptimismProtocolVersionV0(new byte[8], 1, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 2, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 1, 0, 0, 0), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], uint.MaxValue, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], uint.MaxValue - 1, 0, 0, 0), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 0, 0, 0, 0), 0);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 300, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 300, 0, 0, 0), 0);
+        yield return (new(new byte[8], 1, 0, 0, 0), new(new byte[8], 0, 0, 0, 0), 1);
+        yield return (new(new byte[8], 2, 0, 0, 0), new(new byte[8], 1, 0, 0, 0), 1);
+        yield return (new(new byte[8], uint.MaxValue, 0, 0, 0), new(new byte[8], uint.MaxValue - 1, 0, 0, 0), 1);
+        yield return (new(new byte[8], 0, 0, 0, 0), new(new byte[8], 0, 0, 0, 0), 0);
+        yield return (new(new byte[8], 300, 0, 0, 0), new(new byte[8], 300, 0, 0, 0), 0);
 
         // Mixed
-        yield return (new OptimismProtocolVersionV0(new byte[8], 1, 2, 3, 4), new OptimismProtocolVersionV0(new byte[8], 1, 2, 3, 4), 0);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 1, 2, 4, 3), new OptimismProtocolVersionV0(new byte[8], 1, 2, 3, 4), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 1, 3, 3, 4), new OptimismProtocolVersionV0(new byte[8], 1, 2, 3, 4), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 2, 2, 3, 4), new OptimismProtocolVersionV0(new byte[8], 1, 2, 3, 4), 1);
-        yield return (new OptimismProtocolVersionV0(new byte[8], 2, 0, 0, 0), new OptimismProtocolVersionV0(new byte[8], 1, 9, 9, 0), 1);
+        yield return (new(new byte[8], 1, 2, 3, 4), new(new byte[8], 1, 2, 3, 4), 0);
+        yield return (new(new byte[8], 1, 2, 4, 3), new(new byte[8], 1, 2, 3, 4), 1);
+        yield return (new(new byte[8], 1, 3, 3, 4), new(new byte[8], 1, 2, 3, 4), 1);
+        yield return (new(new byte[8], 2, 2, 3, 4), new(new byte[8], 1, 2, 3, 4), 1);
+        yield return (new(new byte[8], 2, 0, 0, 0), new(new byte[8], 1, 9, 9, 0), 1);
     }
-
     [TestCaseSource(nameof(V0CompareCases))]
-    public void Compare_OptimismProtocolVersionV0((OptimismProtocolVersionV0 Left, OptimismProtocolVersionV0 Right, int Expected) testCase)
+    public void Compare_OptimismProtocolVersionV0((OptimismProtocolVersion.V0 Left, OptimismProtocolVersion.V0 Right, int Expected) testCase)
     {
         testCase.Left.CompareTo(testCase.Right).Should().Be(testCase.Expected);
         testCase.Right.CompareTo(testCase.Left).Should().Be(testCase.Expected * -1);
+    }
+
+    private static IEnumerable<byte[]> InvalidByteArrays()
+    {
+        yield return new byte[8];
+        yield return new byte[16];
+        yield return new byte[24];
+    }
+    [TestCaseSource(nameof(InvalidByteArrays))]
+    public void OptimismProtocolVersion_Throws_Invalid_Length(byte[] bytes)
+    {
+        Action read = () => OptimismProtocolVersion.Read(bytes);
+        read.Should().Throw<OptimismProtocolVersion.ParseException>();
+    }
+
+    [TestCase(1)]
+    [TestCase(5)]
+    [TestCase(byte.MaxValue)]
+    public void OptimismProtocolVersion_Throws_Unknown_Version(byte version)
+    {
+        var bytes = new byte[32];
+        bytes[0] = version;
+
+        Action read = () => OptimismProtocolVersion.Read(bytes);
+        read.Should().Throw<OptimismProtocolVersion.ParseException>();
     }
 }
--- src/Nethermind/Nethermind.Optimism/Rpc/OptimismProtocolVersion.cs
+++ src/Nethermind/Nethermind.Optimism/Rpc/OptimismProtocolVersion.cs
@@ -4,127 +4,134 @@
 using System;
 using System.Buffers.Binary;
 using System.Linq;
-using System.Threading.Tasks;
 using Nethermind.Core.Extensions;
 
 namespace Nethermind.Optimism.Rpc;
 
-public interface IOptimismProtocolVersion : IComparable<IOptimismProtocolVersion>
+public abstract class OptimismProtocolVersion : IEquatable<OptimismProtocolVersion>, IComparable<OptimismProtocolVersion>
 {
-    public static IOptimismProtocolVersion Read(ReadOnlySpan<byte> span)
+    public class ParseException(string message) : Exception(message);
+
+    private OptimismProtocolVersion() { }
+
+    public static OptimismProtocolVersion Read(ReadOnlySpan<byte> span)
     {
-        if (span.Length != 32) throw new OptimismProtocolVersionParseException($"Expected 32 bytes, got {span.Length}");
+        if (span.Length < 32) throw new ParseException("Expected at least 32 bytes");
 
         var version = span[0];
         return version switch
         {
-            0 => OptimismProtocolVersionV0.Read(span[1..]),
-            _ => throw new OptimismProtocolVersionParseException($"Unsupported version {version}")
+            0 => V0.Read(span),
+            _ => throw new ParseException($"Unsupported version: {version}")
         };
     }
-}
 
-public sealed record OptimismSuperchainSignal
-{
-    public IOptimismProtocolVersion Recommended { get; }
-    public IOptimismProtocolVersion Required { get; }
+    public abstract void Write(Span<byte> span);
 
-    public OptimismSuperchainSignal(IOptimismProtocolVersion recommended, IOptimismProtocolVersion required)
+    public abstract int CompareTo(OptimismProtocolVersion? other);
+
+    public abstract bool Equals(OptimismProtocolVersion? other);
+
+    public override bool Equals(object? obj) => obj is OptimismProtocolVersion other && Equals(other);
+    public abstract override int GetHashCode();
+
+    public sealed class V0 : OptimismProtocolVersion
     {
-        Recommended = recommended;
-        Required = required;
-    }
-}
+        public byte[] Build { get; }
+        public UInt32 Major { get; }
+        public UInt32 Minor { get; }
+        public UInt32 Patch { get; }
+        public UInt32 PreRelease { get; }
 
-public interface IOptimismSuperchainSignalHandler
-{
-    IOptimismProtocolVersion CurrentVersion { get; }
-
-    Task OnBehindRecommended();
-    Task OnBehindRequired();
-}
-
-public class OptimismProtocolVersionParseException(string message) : Exception(message) { }
-
-public sealed class OptimismProtocolVersionV0 : IOptimismProtocolVersion
-{
-    public byte[] Build { get; }
-    public UInt32 Major { get; }
-    public UInt32 Minor { get; }
-    public UInt32 Patch { get; }
-    public UInt32 PreRelease { get; }
-
-    public OptimismProtocolVersionV0(ReadOnlySpan<byte> build, UInt32 major, UInt32 minor, UInt32 patch, UInt32 preRelease)
-    {
-        Build = build.ToArray();
-        Major = major;
-        Minor = minor;
-        Patch = patch;
-        PreRelease = preRelease;
-    }
-
-    public static OptimismProtocolVersionV0 Read(ReadOnlySpan<byte> span)
-    {
-        var reserved = span.TakeAndMove(7);
-        if (!reserved.IsZero()) throw new OptimismProtocolVersionParseException("Expected reserved bytes to be zero");
-
-        var build = span.TakeAndMove(8);
-        var major = BinaryPrimitives.ReadUInt32BigEndian(span.TakeAndMove(4));
-        var minor = BinaryPrimitives.ReadUInt32BigEndian(span.TakeAndMove(4));
-        var patch = BinaryPrimitives.ReadUInt32BigEndian(span.TakeAndMove(4));
-        var preRelease = BinaryPrimitives.ReadUInt32BigEndian(span.TakeAndMove(4));
-
-        return new OptimismProtocolVersionV0(build, major, minor, patch, preRelease);
-    }
-
-    public void Write(Span<byte> span)
-    {
-        span.TakeAndMove(7);
-
-        Build.CopyTo(span.TakeAndMove(8));
-        BinaryPrimitives.WriteUInt32BigEndian(span.TakeAndMove(4), Major);
-        BinaryPrimitives.WriteUInt32BigEndian(span.TakeAndMove(4), Minor);
-        BinaryPrimitives.WriteUInt32BigEndian(span.TakeAndMove(4), Patch);
-        BinaryPrimitives.WriteUInt32BigEndian(span.TakeAndMove(4), PreRelease);
-    }
-
-    public int CompareTo(IOptimismProtocolVersion? other)
-    {
-        if (ReferenceEquals(this, other)) return 0;
-
-        if (other is null) return 1;
-
-        if (other is not OptimismProtocolVersionV0 otherVersion)
+        public V0(ReadOnlySpan<byte> build, UInt32 major, UInt32 minor, UInt32 patch, UInt32 preRelease)
         {
-            throw new ArgumentException("Object is not a valid OptimismProtocolVersionV0", nameof(other));
+            Build = build.ToArray();
+            Major = major;
+            Minor = minor;
+            Patch = patch;
+            PreRelease = preRelease;
         }
 
-        var majorComparison = Major.CompareTo(otherVersion.Major);
-        if (majorComparison != 0) return majorComparison;
-
-        var minorComparison = Minor.CompareTo(otherVersion.Minor);
-        if (minorComparison != 0) return minorComparison;
-
-        var patchComparison = Patch.CompareTo(otherVersion.Patch);
-        if (patchComparison != 0) return patchComparison;
-
-        return (PreRelease, otherVersion.PreRelease) switch
+        public new static V0 Read(ReadOnlySpan<byte> span)
         {
-            (0, 0) => 0,
-            (0, _) => 1,
-            (_, 0) => -1,
-            _ => PreRelease.CompareTo(otherVersion.PreRelease)
-        };
+            var version = span[0];
+            if (version != 0) throw new ParseException($"Expected version 0, got {version}");
+
+            var reserved = span.TakeAndMove(7);
+            if (!reserved.IsZero()) throw new ParseException("Expected reserved bytes to be zero");
+
+            var build = span.TakeAndMove(8);
+            var major = BinaryPrimitives.ReadUInt32BigEndian(span.TakeAndMove(4));
+            var minor = BinaryPrimitives.ReadUInt32BigEndian(span.TakeAndMove(4));
+            var patch = BinaryPrimitives.ReadUInt32BigEndian(span.TakeAndMove(4));
+            var preRelease = BinaryPrimitives.ReadUInt32BigEndian(span.TakeAndMove(4));
+
+            return new V0(build, major, minor, patch, preRelease);
+        }
+
+        public override void Write(Span<byte> span)
+        {
+            span[0] = 0;
+
+            span.TakeAndMove(7);
+
+            Build.CopyTo(span.TakeAndMove(8));
+            BinaryPrimitives.WriteUInt32BigEndian(span.TakeAndMove(4), Major);
+            BinaryPrimitives.WriteUInt32BigEndian(span.TakeAndMove(4), Minor);
+            BinaryPrimitives.WriteUInt32BigEndian(span.TakeAndMove(4), Patch);
+            BinaryPrimitives.WriteUInt32BigEndian(span.TakeAndMove(4), PreRelease);
+        }
+
+        public override int CompareTo(OptimismProtocolVersion? other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+
+            if (other is null) return 1;
+
+            if (other is not V0 otherVersion)
+                throw new ArgumentException("Object is not a valid OptimismProtocolVersion.V0", nameof(other));
+
+            var majorComparison = Major.CompareTo(otherVersion.Major);
+            if (majorComparison != 0) return majorComparison;
+
+            var minorComparison = Minor.CompareTo(otherVersion.Minor);
+            if (minorComparison != 0) return minorComparison;
+
+            var patchComparison = Patch.CompareTo(otherVersion.Patch);
+            if (patchComparison != 0) return patchComparison;
+
+            return (PreRelease, otherVersion.PreRelease) switch
+            {
+                (0, 0) => 0,
+                (0, _) => 1,
+                (_, 0) => -1,
+                _ => PreRelease.CompareTo(otherVersion.PreRelease)
+            };
+        }
+
+        public override bool Equals(OptimismProtocolVersion? other)
+        {
+            if (ReferenceEquals(this, other)) return true;
+            if (other is null) return false;
+            if (other is not V0 otherVersion) return false;
+
+            return Build.SequenceEqual(otherVersion.Build)
+                   && Major == otherVersion.Major
+                   && Minor == otherVersion.Minor
+                   && Patch == otherVersion.Patch
+                   && PreRelease == otherVersion.PreRelease;
+        }
+
+        public override bool Equals(object? obj) => ReferenceEquals(this, obj) || obj is V0 other && Equals(other);
+
+        public override int GetHashCode() => HashCode.Combine(Build, Major, Minor, Patch, PreRelease);
     }
+}
 
-    private bool Equals(OptimismProtocolVersionV0 other) =>
-        Build.SequenceEqual(other.Build)
-            && Major == other.Major
-            && Minor == other.Minor
-            && Patch == other.Patch
-            && PreRelease == other.PreRelease;
-
-    public override bool Equals(object? obj) => ReferenceEquals(this, obj) || obj is OptimismProtocolVersionV0 other && Equals(other);
-
-    public override int GetHashCode() => HashCode.Combine(Build, Major, Minor, Patch, PreRelease);
+public sealed class OptimismSuperchainSignal(
+    OptimismProtocolVersion recommended,
+    OptimismProtocolVersion required)
+{
+    public OptimismProtocolVersion Recommended { get; } = recommended;
+    public OptimismProtocolVersion Required { get; } = required;
 }
