e279da6f00f1f106515311194aabe5950b5bb58e
Lautaro Emanuel <emlautarom1@gmail.com>
2024-10-30 10:52:24 -0300

    Test renaming

--- src/Nethermind/Nethermind.Optimism.Test/OptimismProtocolVersionTest.cs
+++ src/Nethermind/Nethermind.Optimism.Test/OptimismProtocolVersionTest.cs
@@ -10,7 +10,7 @@ namespace Nethermind.Optimism.Test;
 [Parallelizable(ParallelScope.All)]
 public class OptimismProtocolVersionTest
 {
-    private static IEnumerable<(string, OptimismProtocolVersion.V0)> V0ParseCases()
+    private static IEnumerable<(string, OptimismProtocolVersion.V0)> V0ReadWriteCases()
     {
         yield return ("0x0000000000000000000000000000000000000000000000000000000000000000", new(new byte[8], 0, 0, 0, 0));
         yield return ("0x0000000000000000000000000000000000000000000000000000000000000100", new(new byte[8], 0, 0, 0, 1));
@@ -22,8 +22,8 @@ public class OptimismProtocolVersionTest
         yield return ("0x0000000000000061620100000000000000002a00000000000000020000000000", new([(byte)'a', (byte)'b', 1, 0, 0, 0, 0, 0], 42, 0, 2, 0));
         yield return ("0x0000000000000001020304050607080000002a00000000000000020000000000", new([1, 2, 3, 4, 5, 6, 7, 8], 42, 0, 2, 0));
     }
-    [TestCaseSource(nameof(V0ParseCases))]
-    public void ReadWrite_OptimismProtocolVersionV0((string HexString, OptimismProtocolVersion.V0 Expected) testCase)
+    [TestCaseSource(nameof(V0ReadWriteCases))]
+    public void OptimismProtocolVersionV0_ReadWrite((string HexString, OptimismProtocolVersion.V0 Expected) testCase)
     {
         var bytes = Bytes.FromHexString(testCase.HexString);
         var actual = OptimismProtocolVersion.Read(bytes);
@@ -74,7 +74,7 @@ public class OptimismProtocolVersionTest
         yield return (new(new byte[8], 2, 0, 0, 0), new(new byte[8], 1, 9, 9, 0), 1);
     }
     [TestCaseSource(nameof(V0CompareCases))]
-    public void Compare_OptimismProtocolVersionV0((OptimismProtocolVersion.V0 Left, OptimismProtocolVersion.V0 Right, int Expected) testCase)
+    public void OptimismProtocolVersionV0_Compare((OptimismProtocolVersion.V0 Left, OptimismProtocolVersion.V0 Right, int Expected) testCase)
     {
         testCase.Left.CompareTo(testCase.Right).Should().Be(testCase.Expected);
         testCase.Right.CompareTo(testCase.Left).Should().Be(testCase.Expected * -1);
