0861f6666e9f257120da569e3a6656d7ff72c255
Amirul Ashraf <asdacap@gmail.com>
2024-10-31 15:45:08 +0800

    Should block processing queue

--- src/Nethermind/Nethermind.Synchronization.Test/SynchronizerModuleTests.cs
+++ src/Nethermind/Nethermind.Synchronization.Test/SynchronizerModuleTests.cs
@@ -2,8 +2,12 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Autofac;
+using FluentAssertions;
 using Nethermind.Blockchain.Synchronization;
+using Nethermind.Consensus.Processing;
 using Nethermind.Core;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Db;
@@ -18,13 +22,12 @@ namespace Nethermind.Synchronization.Test;
 
 public class SynchronizerModuleTests
 {
-    [Test]
-    public void TestOnTreeSyncFinish_CallVisit()
-    {
+    public IContainer CreateTestContainer() {
         ITreeSync treeSync = Substitute.For<ITreeSync>();
         IStateReader stateReader = Substitute.For<IStateReader>();
+        IBlockProcessingQueue blockQueue = Substitute.For<IBlockProcessingQueue>();
 
-        using IContainer container = new ContainerBuilder()
+        return new ContainerBuilder()
             .AddModule(new SynchronizerModule(new SyncConfig()
             {
                 FastSync = true,
@@ -33,11 +36,62 @@ public class SynchronizerModuleTests
             .AddKeyedSingleton(DbNames.Code, Substitute.For<IDb>())
             .AddSingleton(stateReader)
             .AddSingleton(treeSync)
+            .AddSingleton(blockQueue)
             .AddSingleton<ILogManager>(LimboLogs.Instance)
             .Build();
+    }
+
+    [Test]
+    public void TestOnTreeSyncFinish_CallVisit()
+    {
+        IContainer ctx = CreateTestContainer();
+        ITreeSync treeSync = ctx.Resolve<ITreeSync>();
+        IStateReader stateReader = ctx.Resolve<IStateReader>();
 
         treeSync.OnVerifyPostSyncCleanup += Raise.EventWith(null, new ITreeSync.VerifyPostSyncCleanupEventArgs(TestItem.KeccakA));
 
-        stateReader.Received().RunTreeVisitor(Arg.Any<ITreeVisitor>(), Arg.Is(TestItem.KeccakA), Arg.Any<VisitingOptions>());
+        stateReader
+            .Received()
+            .RunTreeVisitor(Arg.Any<ITreeVisitor>(), Arg.Is(TestItem.KeccakA), Arg.Any<VisitingOptions>());
+    }
+
+    [Test]
+    public async Task TestOnTreeSyncFinish_BlockProcessingQueue_UntilFinished()
+    {
+        IContainer ctx = CreateTestContainer();
+        ITreeSync treeSync = ctx.Resolve<ITreeSync>();
+        IStateReader stateReader = ctx.Resolve<IStateReader>();
+        IBlockProcessingQueue blockQueue = ctx.Resolve<IBlockProcessingQueue>();
+
+        ManualResetEvent treeVisitorBlocker = new ManualResetEvent(false);
+
+        stateReader
+            .When(sr => sr.RunTreeVisitor(Arg.Any<ITreeVisitor>(), Arg.Is(TestItem.KeccakA), Arg.Any<VisitingOptions>()))
+            .Do((ci) =>
+            {
+                treeVisitorBlocker.WaitOne();
+            });
+
+        Task triggerTask = Task.Run(() =>
+        {
+            treeSync.OnVerifyPostSyncCleanup += Raise.EventWith(null, new ITreeSync.VerifyPostSyncCleanupEventArgs(TestItem.KeccakA));
+        });
+
+        await Task.Delay(100);
+
+        Task blockQueueTask = Task.Run(() =>
+        {
+            blockQueue.BlockRemoved +=
+                Raise.EventWith(null, new BlockRemovedEventArgs(null!, ProcessingResult.Success));
+        });
+
+        await Task.Delay(100);
+
+        blockQueueTask.IsCompleted.Should().BeFalse();
+        treeVisitorBlocker.Set();
+
+        await triggerTask;
+        await blockQueueTask;
+        blockQueue.BlockRemoved += Raise.EventWith(null, new BlockRemovedEventArgs(null!, ProcessingResult.Success));
     }
 }
--- src/Nethermind/Nethermind.Synchronization/FastSync/VerifyStateOnStateSyncFinished.cs
+++ src/Nethermind/Nethermind.Synchronization/FastSync/VerifyStateOnStateSyncFinished.cs
@@ -1,9 +1,10 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System.Threading;
 using Autofac;
 using Autofac.Features.AttributeFilters;
-using Nethermind.Core;
+using Nethermind.Consensus.Processing;
 using Nethermind.Core.Crypto;
 using Nethermind.Db;
 using Nethermind.Logging;
@@ -12,7 +13,12 @@ using Nethermind.Trie;
 
 namespace Nethermind.Synchronization.FastSync;
 
-public class VerifyStateOnStateSyncFinished(ITreeSync treeSync, IStateReader stateReader, [KeyFilter(DbNames.Code)] IDb codeDb, ILogManager logManager) : IStartable
+public class VerifyStateOnStateSyncFinished(
+    IBlockProcessingQueue processingQueue,
+    ITreeSync treeSync,
+    IStateReader stateReader,
+    [KeyFilter(DbNames.Code)] IDb codeDb,
+    ILogManager logManager) : IStartable
 {
     private readonly ILogger _logger = logManager.GetClassLogger<VerifyStateOnStateSyncFinished>();
 
@@ -23,9 +29,28 @@ public class VerifyStateOnStateSyncFinished(ITreeSync treeSync, IStateReader sta
 
     private void TreeSyncOnOnVerifyPostSyncCleanup(object? sender, ITreeSync.VerifyPostSyncCleanupEventArgs e)
     {
-        Hash256 rootNode = e.Root;
-        _logger!.Info("Collecting trie stats and verifying that no nodes are missing...");
-        TrieStats stats = stateReader.CollectStats(rootNode, codeDb, logManager);
-        _logger.Info($"Stats after finishing state" + stats);
+        ManualResetEvent processingBlocker = new ManualResetEvent(false);
+
+        processingQueue.BlockRemoved += ProcessingQueueOnBlockRemoved;
+
+        try
+        {
+            Hash256 rootNode = e.Root;
+            _logger!.Info("Collecting trie stats and verifying that no nodes are missing...");
+            TrieStats stats = stateReader.CollectStats(rootNode, codeDb, logManager);
+            _logger.Info($"Stats after finishing state" + stats);
+        }
+        finally
+        {
+            processingBlocker.Set();
+            processingQueue.BlockRemoved -= ProcessingQueueOnBlockRemoved;
+        }
+
+        return;
+
+        void ProcessingQueueOnBlockRemoved(object? o, BlockRemovedEventArgs blockRemovedEventArgs)
+        {
+            processingBlocker.WaitOne();
+        }
     }
 }
