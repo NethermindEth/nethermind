a8e8154257d05feaee8602b7f89db8edce4eb3dc
Amirul Ashraf <asdacap@gmail.com>
2024-11-01 09:11:35 +0800

    Fix misleading  cli

--- src/Nethermind/Nethermind.Era1/IEraConfig.cs
+++ src/Nethermind/Nethermind.Era1/IEraConfig.cs
@@ -7,24 +7,24 @@ namespace Nethermind.Era1;
 
 public interface IEraConfig : IConfig
 {
-    [ConfigItem(Description = "Directory of era1 archives to be imported before starting in full sync mode.", DefaultValue = "", HiddenFromDocs = false)]
+    [ConfigItem(Description = "Directory of era1 archives to be imported.", DefaultValue = "", HiddenFromDocs = false)]
     public string? ImportDirectory { get; set; }
 
     [ConfigItem(Description = "Directory of archive export.", DefaultValue = "", HiddenFromDocs = false)]
     public string? ExportDirectory { get; set; }
 
-    [ConfigItem(Description = "Start block to export", DefaultValue = "0", HiddenFromDocs = false)]
+    [ConfigItem(Description = "Block number to import/export from.", DefaultValue = "0", HiddenFromDocs = false)]
     long From { get; set; }
 
-    [ConfigItem(Description = "End block to export. Set to 0 for Head.", DefaultValue = "0", HiddenFromDocs = false)]
+    [ConfigItem(Description = "Block number to import/export to.", DefaultValue = "0", HiddenFromDocs = false)]
     long To { get; set; }
 
-    [ConfigItem(Description = "Trusted accumulator file.", DefaultValue = "null", HiddenFromDocs = false)]
+    [ConfigItem(Description = "Accumulator file to be used for trusting era files.", DefaultValue = "null", HiddenFromDocs = false)]
     string? TrustedAccumulatorFile { get; set; }
 
     [ConfigItem(Description = "Max era1 size.", DefaultValue = "8192", HiddenFromDocs = true)]
     int MaxEra1Size { get; set; }
 
-    [ConfigItem(Description = "Network name used for era directory naming. When null, it will imply from block.", DefaultValue = "null", HiddenFromDocs = true)]
+    [ConfigItem(Description = "Network name used for era directory naming. When null, it will imply from network.", DefaultValue = "null", HiddenFromDocs = true)]
     string? NetworkName { get; set; }
 }
