e4efbd5b385953220d302701e42bf5c878316f3b
anhnhgutech <anhnh@gu-tech.com>
2024-11-05 17:16:35 +0700

    fix format code

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/AdminModuleTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/AdminModuleTests.cs
@@ -45,6 +45,11 @@ public class AdminModuleTests
 
         IStaticNodesManager staticNodesManager = Substitute.For<IStaticNodesManager>();
         Enode enode = new(_enodeString);
+        ChainSpec chainSpec = new()
+        {
+            Parameters = new ChainParameters()
+        };
+
         _adminRpcModule = new AdminRpcModule(
             _blockTree,
             _networkConfig,
@@ -52,8 +57,8 @@ public class AdminModuleTests
             staticNodesManager,
             enode,
             _exampleDataDir,
-            new ManualPruningTrigger(), 
-            new ChainParameters());
+            new ManualPruningTrigger(),
+            chainSpec.Parameters);
 
         _serializer = new EthereumJsonSerializer();
     }
