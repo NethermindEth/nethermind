8bc8d8f1171e56ce7ced2199fa1a5bfd4e72211a
yerke26 <160018032+yerke26@users.noreply.github.com>
2024-11-05 09:06:39 +0500

    Implement t8n execution (#7695)

--- tools/Evm/T8n/T8nInputProcessor.cs
+++ tools/Evm/T8n/T8nInputProcessor.cs
@@ -37,11 +37,10 @@ public static class T8nInputProcessor
             DisableStack = arguments.TraceNoStack
         };
 
-        T8nTest test = new(spec, specProvider)
+        T8nTest test = new(spec, specProvider, inputData.Env.CurrentCoinbase)
         {
             Alloc = inputData.Alloc ?? [],
-            Transactions = inputData.GetTransactions(TxDecoder),
-            CurrentCoinbase = inputData.Env.CurrentCoinbase,
+            Transactions = inputData.GetTransactions(TxDecoder, specProvider.ChainId),
             CurrentGasLimit = inputData.Env.CurrentGasLimit,
             CurrentTimestamp = inputData.Env.CurrentTimestamp,
             CurrentNumber = inputData.Env.CurrentNumber,
