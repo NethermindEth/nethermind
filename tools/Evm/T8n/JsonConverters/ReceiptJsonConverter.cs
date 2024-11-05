65f637ffd726eaf03bf48e814f2dd0ff4843ed7b
yerke26 <yerkebulan@nethermind.io>
2024-11-05 11:04:06 +0600

    t8n output processing

---
+++ tools/Evm/T8n/JsonConverters/ReceiptJsonConverter.cs
@@ -0,0 +1,66 @@
+// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Extensions;
+using Nethermind.Int256;
+using Nethermind.Serialization.Json;
+
+namespace Evm.T8n.JsonConverters;
+
+public class ReceiptJsonConverter : JsonConverter<TxReceipt>
+{
+    private readonly EthereumJsonSerializer _ethereumJsonSerializer = new();
+
+    public override TxReceipt Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return _ethereumJsonSerializer.Deserialize<TxReceipt>(ref reader);
+    }
+
+    public override void Write(Utf8JsonWriter writer, TxReceipt receipt, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+
+        if (receipt.TxType != TxType.Legacy)
+        {
+            writer.WritePropertyName("type");
+            JsonSerializer.Serialize(writer, receipt.TxType, options);
+        }
+        writer.WritePropertyName("root");
+        ByteArrayConverter.Convert(writer, receipt.PostTransactionState != null ? receipt.PostTransactionState.Bytes : Bytes.ZeroByte.ToArray());
+        var status = receipt.StatusCode;
+        writer.WritePropertyName("status");
+        if (status == 0)
+        {
+            writer.WriteRawValue("\"0x0\""u8, skipInputValidation: true);
+        }
+        else
+        {
+            ByteArrayConverter.Convert(writer, MemoryMarshal.CreateReadOnlySpan(ref status, 1));
+        }
+
+        writer.WritePropertyName("cumulativeGasUsed");
+        JsonSerializer.Serialize(writer, receipt.GasUsedTotal, options);
+        writer.WritePropertyName("logsBloom");
+        JsonSerializer.Serialize(writer, receipt.Bloom, options);
+        writer.WriteNull("logs");
+        writer.WritePropertyName("transactionHash");
+        JsonSerializer.Serialize(writer, receipt.TxHash, options);
+        writer.WritePropertyName("contractAddress");
+        JsonSerializer.Serialize(writer, receipt.ContractAddress ?? Address.Zero, options);
+        writer.WritePropertyName("gasUsed");
+        JsonSerializer.Serialize(writer, receipt.GasUsed, options);
+        writer.WritePropertyName("blockHash");
+        JsonSerializer.Serialize(writer, receipt.BlockHash ?? Keccak.Zero, options);
+
+        writer.WritePropertyName("transactionIndex");
+        JsonSerializer.Serialize(writer, UInt256.Parse(receipt.Index.ToString(), NumberStyles.Integer), options);
+
+        writer.WriteEndObject();
+    }
+}
--- tools/Evm/T8n/T8nCommand.cs
+++ tools/Evm/T8n/T8nCommand.cs
@@ -2,11 +2,25 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System.CommandLine;
+using System.Text.Json;
+using Evm.T8n.Errors;
+using Evm.T8n.JsonConverters;
+using Evm.T8n.JsonTypes;
+using Nethermind.Serialization.Json;
 
 namespace Evm.T8n;
 
 public static class T8nCommand
 {
+    private static readonly EthereumJsonSerializer _ethereumJsonSerializer = new();
+    private const string Stdout = "stdout";
+
+    static T8nCommand()
+    {
+        EthereumJsonSerializer.AddConverter(new ReceiptJsonConverter());
+        EthereumJsonSerializer.AddConverter(new AccountStateJsonConverter());
+    }
+
     public static void Configure(ref CliRootCommand rootCmd)
     {
         CliCommand cmd = T8nCommandOptions.CreateCommand();
@@ -15,9 +29,62 @@ public static class T8nCommand
         {
             var arguments = T8nCommandArguments.FromParseResult(parseResult);
 
-            T8nExecutor.Execute(arguments);
+            T8nOutput t8nOutput = new();
+            try
+            {
+                T8nExecutionResult t8nExecutionResult = T8nExecutor.Execute(arguments);
+
+                if (arguments.OutputAlloc == Stdout) t8nOutput.Alloc = t8nExecutionResult.Accounts;
+                else WriteToFile(arguments.OutputAlloc, arguments.OutputBaseDir, t8nExecutionResult.Accounts);
+
+                if (arguments.OutputResult == Stdout) t8nOutput.Result = t8nExecutionResult.PostState;
+                else WriteToFile(arguments.OutputResult, arguments.OutputBaseDir, t8nExecutionResult.PostState);
+
+                if (arguments.OutputBody == Stdout) t8nOutput.Body = t8nExecutionResult.TransactionsRlp;
+                else if (arguments.OutputBody is not null)
+                {
+                    WriteToFile(arguments.OutputBody, arguments.OutputBaseDir, t8nExecutionResult.TransactionsRlp);
+                }
+
+                if (t8nOutput.Body is not null || t8nOutput.Alloc is not null || t8nOutput.Result is not null)
+                {
+                    Console.WriteLine(_ethereumJsonSerializer.Serialize(t8nOutput, true));
+                }
+            }
+            catch (T8nException e)
+            {
+                t8nOutput = new T8nOutput(e.Message, e.ExitCode);
+            }
+            catch (IOException e)
+            {
+                t8nOutput = new T8nOutput(e.Message, T8nErrorCodes.ErrorIO);
+            }
+            catch (JsonException e)
+            {
+                t8nOutput = new T8nOutput(e.Message, T8nErrorCodes.ErrorJson);
+            }
+            catch (Exception e)
+            {
+                t8nOutput = new T8nOutput(e.Message, T8nErrorCodes.ErrorEvm);
+            }
+            finally
+            {
+                Environment.ExitCode = t8nOutput.ExitCode;
+                if (t8nOutput.ErrorMessage != null)
+                {
+                    Console.WriteLine(t8nOutput.ErrorMessage);
+                }
+            }
         });
 
         rootCmd.Add(cmd);
+    }
+
+    private static void WriteToFile(string filename, string? basedir, object outputObject)
+    {
+        FileInfo fileInfo = new(basedir + filename);
+        Directory.CreateDirectory(fileInfo.DirectoryName!);
+        using StreamWriter writer = new(fileInfo.FullName);
+        writer.Write(_ethereumJsonSerializer.Serialize(outputObject, true));
     }
 }
