412fdbcdd0555974e31a9f20c5131dbe7bb06807
Lautaro Emanuel <emlautarom1@gmail.com>
2024-11-11 14:08:30 -0300

    Add concrete `Eip1559Spec : IEip1559Spec`

--- src/Nethermind/Nethermind.Core/Specs/IEip1559Spec.cs
+++ src/Nethermind/Nethermind.Core/Specs/IEip1559Spec.cs
@@ -22,4 +22,28 @@ namespace Nethermind.Core.Specs
         public UInt256 BaseFeeMaxChangeDenominator { get; }
         public long ElasticityMultiplier { get; }
     }
+
+    public sealed class Eip1559Spec : IEip1559Spec
+    {
+        public bool IsEip1559Enabled { get; set; }
+        public long Eip1559TransitionBlock { get; set; }
+        public Address? FeeCollector { get; set; }
+        public UInt256? Eip1559BaseFeeMinValue { get; set; }
+        public UInt256 ForkBaseFee { get; set; }
+        public UInt256 BaseFeeMaxChangeDenominator { get; set; }
+        public long ElasticityMultiplier { get; set; }
+
+        public Eip1559Spec() { }
+
+        public Eip1559Spec(IEip1559Spec spec)
+        {
+            IsEip1559Enabled = spec.IsEip1559Enabled;
+            Eip1559TransitionBlock = spec.Eip1559TransitionBlock;
+            FeeCollector = spec.FeeCollector;
+            Eip1559BaseFeeMinValue = spec.Eip1559BaseFeeMinValue;
+            ForkBaseFee = spec.ForkBaseFee;
+            BaseFeeMaxChangeDenominator = spec.BaseFeeMaxChangeDenominator;
+            ElasticityMultiplier = spec.ElasticityMultiplier;
+        }
+    }
 }
