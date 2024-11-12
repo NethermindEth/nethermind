a66b3670deab1392bc7dd85dc36ef6f8c64f522f
Lautaro Emanuel <emlautarom1@gmail.com>
2024-11-12 12:49:21 -0300

    Rename file

--- src/Nethermind/Nethermind.Optimism/OptimismEIP1559Parameters.cs
+++ src/Nethermind/Nethermind.Optimism/OptimismEIP1559Parameters.cs
@@ -5,6 +5,7 @@ using System;
 using System.Buffers.Binary;
 using Nethermind.Core;
 using Nethermind.Core.Extensions;
+using Nethermind.Optimism.Rpc;
 
 namespace Nethermind.Optimism;
 
@@ -34,11 +35,11 @@ public readonly struct EIP1559Parameters
     }
 }
 
-public static class BlockHeaderExtensions
+public static class EIP1559ParametersExtensions
 {
     public static EIP1559Parameters DecodeEIP1559Parameters(this BlockHeader header)
     {
-        if (header.ExtraData.Length < 9) throw new ArgumentException($"{header.ExtraData} data must be at least 9 bytes long");
+        if (header.ExtraData.Length < 9) throw new ArgumentException($"{nameof(header.ExtraData)} data must be at least 9 bytes long");
         // TODO: Add check for `there is no additional data beyond these 9 bytes` (whatever that means): https://github.com/roberto-bayardo/op-geth/blob/6c32375dda12d3f0b8f3498404f00fe1ae872547/consensus/misc/eip1559/eip1559.go#L112-L114
 
         ReadOnlySpan<byte> extraData = header.ExtraData.AsSpan();
