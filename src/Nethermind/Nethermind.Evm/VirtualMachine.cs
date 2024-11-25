89b17e7315027419cedec9b48bd8af20b5df8ff4
ak88 <anders.holmbjerg@hotmail.com>
2024-11-25 10:27:48 +0100

    fix

--- src/Nethermind/Nethermind.Evm/VirtualMachine.cs
+++ src/Nethermind/Nethermind.Evm/VirtualMachine.cs
@@ -2092,7 +2092,7 @@ internal sealed class VirtualMachine<TLogger> : IVirtualMachine where TLogger : 
         if (delegation is not null)
         {
             //If the account has been delegated only the first two bytes of the delegation header counts as size 
-            result = Eip7702Constants.FirstTwoBytesOfHeader.Count;
+            result = (UInt256)Eip7702Constants.FirstTwoBytesOfHeader.Length;
         }
         stack.PushUInt256(in result);
     }
