7ab6c2ece7e56550a90f37019d7a687ba53a2af1
Amirul Ashraf <asdacap@gmail.com>
2024-11-25 22:01:56 +0800

    Formatting

--- src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
+++ src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
@@ -9,8 +9,16 @@ public class DbConfig : IDbConfig
 {
     public static DbConfig Default = new DbConfig();
 
+
     public ulong SharedBlockCacheSize { get; set; } = (ulong)256.MiB();
     public bool SkipMemoryHintSetting { get; set; } = false;
+
+    public bool WriteAheadLogSync { get; set; } = false;
+    public bool EnableDbStatistics { get; set; } = false;
+    public bool EnableMetricsUpdater { get; set; } = false;
+    public uint StatsDumpPeriodSec { get; set; } = 600;
+
+
 
     public ulong WriteBufferSize { get; set; } = (ulong)16.MiB();
     public uint WriteBufferNumber { get; set; } = 2;
@@ -19,32 +27,32 @@ public class DbConfig : IDbConfig
     public ulong? ReadAheadSize { get; set; } = (ulong)256.KiB();
 
     public string? AdditionalRocksDbOptions { get; set; } =
-          "compression=kSnappyCompression;"
-        + "optimize_filters_for_hits=true;"
-        + "memtable_whole_key_filtering=true;"
-        + "memtable_prefix_bloom_size_ratio=0.02;"
-        + "advise_random_on_open=true;"
-        + "min_write_buffer_to_merge=1;"
+        "compression=kSnappyCompression;" +
+        "optimize_filters_for_hits=true;" +
+        "memtable_whole_key_filtering=true;" +
+        "memtable_prefix_bloom_size_ratio=0.02;" +
+        "advise_random_on_open=true;" +
+        "min_write_buffer_to_merge=1;" +
 
         // Target size of each SST file. Increase to reduce number of file. Default is 64MB.
-        + "target_file_size_base=64000000;"
+        "target_file_size_base=64000000;" +
 
         // The first level size. Should be WriteBufferSize * WriteBufferNumber or you'll have higher write amp,
         // but lowering this to match write buffer will make the LSM have more level, so you'll have more read amp.
-        + "max_bytes_for_level_base=256000000;"
+        "max_bytes_for_level_base=256000000;" +
 
         // Note, this is before compression. On disk size may be lower. The on disk size is the minimum amount of read
         // each io will do. On most SSD, the minimum read size is 4096 byte. So don't set it to lower than that, unless
         // you have an optane drive or some kind of RAM disk. Lower block size also means bigger index size.
-        + "block_based_table_factory.block_size=16000;"
+        "block_based_table_factory.block_size=16000;" +
 
         // No significant downside. Just set it.
-        + "block_based_table_factory.pin_l0_filter_and_index_blocks_in_cache=true;"
+        "block_based_table_factory.pin_l0_filter_and_index_blocks_in_cache=true;" +
 
         // Make the index in cache have higher priority, so it is kept more in cache.
-        + "block_based_table_factory.cache_index_and_filter_blocks_with_high_priority=true;"
+        "block_based_table_factory.cache_index_and_filter_blocks_with_high_priority=true;" +
 
-        + "block_based_table_factory.format_version=5;"
+        "block_based_table_factory.format_version=5;" +
 
         // Two level index split the index into two level. First index point to second level index, which actually
         // point to the block, which get bsearched to the value. This means potentially two iop instead of one per
@@ -52,17 +60,15 @@ public class DbConfig : IDbConfig
         // processing time more consistent. So its enabled by default. That said, if you got the RAM, maybe disable
         // this.
         // See https://rocksdb.org/blog/2017/05/12/partitioned-index-filter.html
-        + "block_based_table_factory.index_type=kTwoLevelIndexSearch;"
-        + "block_based_table_factory.partition_filters=true;"
-        + "block_based_table_factory.metadata_block_size=4096;"
+        "block_based_table_factory.index_type=kTwoLevelIndexSearch;" +
+        "block_based_table_factory.partition_filters=true;" +
+        "block_based_table_factory.metadata_block_size=4096;" +
 
-        + "block_based_table_factory.filter_policy=bloom_filter:10;"
-        ;
+        "block_based_table_factory.filter_policy=bloom_filter:10;" +
+        "";
 
     public bool? VerifyChecksum { get; set; } = true;
-    public int MinWriteBufferNumberToMerge { get; set; } = 1;
     public ulong? RowCacheSize { get; set; } = null;
-    public long? MaxWriteBufferSizeToMaintain { get; set; } = null;
     public bool EnableFileWarmer { get; set; } = false;
     public double CompressibilityHint { get; set; } = 1.0;
 
@@ -72,12 +78,16 @@ public class DbConfig : IDbConfig
     public uint ReceiptsDbWriteBufferNumber { get; set; } = 2;
     public ulong ReceiptsDbBlockCacheSize { get; set; } = (ulong)8.MiB();
     public double ReceiptsDbCompressibilityHint { get; set; } = 0.35;
-    public string? ReceiptsDbAdditionalRocksDbOptions { get; set; } = "compaction_pri=kOldestLargestSeqFirst;optimize_filters_for_hits=false;";
+    public string? ReceiptsDbAdditionalRocksDbOptions { get; set; } =
+        "compaction_pri=kOldestLargestSeqFirst;" +
+        "optimize_filters_for_hits=false;";
 
     public ulong BlocksDbWriteBufferSize { get; set; } = (ulong)64.MiB();
     public uint BlocksDbWriteBufferNumber { get; set; } = 2;
     public ulong BlocksDbBlockCacheSize { get; set; } = (ulong)32.MiB();
-    public string? BlocksDbAdditionalRocksDbOptions { get; set; } = "compaction_pri=kOldestLargestSeqFirst;optimize_filters_for_hits=false;";
+    public string? BlocksDbAdditionalRocksDbOptions { get; set; } =
+        "compaction_pri=kOldestLargestSeqFirst;" +
+        "optimize_filters_for_hits=false;";
 
     public ulong HeadersDbWriteBufferSize { get; set; } = (ulong)8.MiB();
     public uint HeadersDbWriteBufferNumber { get; set; } = 2;
@@ -102,7 +112,8 @@ public class DbConfig : IDbConfig
     public ulong BlockInfosDbWriteBufferSize { get; set; } = (ulong)4.MiB();
     public uint BlockInfosDbWriteBufferNumber { get; set; } = 2;
     public ulong BlockInfosDbBlockCacheSize { get; set; } = (ulong)16.MiB();
-    public string? BlockInfosDbAdditionalRocksDbOptions { get; set; } = "compaction_pri=kOldestLargestSeqFirst";
+    public string? BlockInfosDbAdditionalRocksDbOptions { get; set; } =
+        "compaction_pri=kOldestLargestSeqFirst";
 
     public ulong PendingTxsDbWriteBufferSize { get; set; } = (ulong)4.MiB();
     public uint PendingTxsDbWriteBufferNumber { get; set; } = 4;
@@ -113,7 +124,12 @@ public class DbConfig : IDbConfig
     public uint CodeDbWriteBufferNumber { get; set; } = 2;
     public ulong CodeDbBlockCacheSize { get; set; } = 0;
     public ulong? CodeDbRowCacheSize { get; set; } = (ulong)16.MiB();
-    public string? CodeDbAdditionalRocksDbOptions { get; set; } = "prefix_extractor=capped:16;block_based_table_factory.index_type=kHashSearch;block_based_table_factory.block_size=4096;memtable=prefix_hash:1000000;allow_concurrent_memtable_write=false;";
+    public string? CodeDbAdditionalRocksDbOptions { get; set; } =
+        "prefix_extractor=capped:16;" +
+        "block_based_table_factory.index_type=kHashSearch;" +
+        "block_based_table_factory.block_size=4096;" +
+        "memtable=prefix_hash:1000000;" +
+        "allow_concurrent_memtable_write=false;";
 
     public ulong BloomDbWriteBufferSize { get; set; } = (ulong)1.KiB();
     public uint BloomDbWriteBufferNumber { get; set; } = 4;
@@ -129,29 +145,29 @@ public class DbConfig : IDbConfig
     public uint StateDbWriteBufferNumber { get; set; } = 4;
     public ulong StateDbBlockCacheSize { get; set; }
     public bool? StateDbVerifyChecksum { get; set; }
-    public int StateDbMinWriteBufferNumberToMerge { get; set; } = 2;
     public ulong? StateDbRowCacheSize { get; set; }
-    public long? StateDbMaxWriteBufferSizeToMaintain { get; set; }
     public bool StateDbEnableFileWarmer { get; set; } = false;
     public double StateDbCompressibilityHint { get; set; } = 0.45;
 
     public string? StateDbAdditionalRocksDbOptions { get; set; } =
-          "compression=kLZ4Compression;"
+        // LZ4 seems to be slightly faster here
+        "compression=kLZ4Compression;" +
 
         // MaxBytesForLevelMultiplier is 10 by default. Lowering this will deepens the LSM, which may reduce write
         // amplification (unless the LSM is too deep), at the expense of read performance. But then, you have bloom
         // filter anyway, and recently written keys are likely to be read and they tend to be at the top of the LSM
         // tree which means they are more cacheable, so at that point you are trading CPU for cacheability.
-        + "max_bytes_for_level_multiplier=30;"
-        + "max_bytes_for_level_base=350;"
+        // These two config make the LSM level to be no more than 3 until the database grow to about 250GB.
+        "max_bytes_for_level_multiplier=30;" +
+        "max_bytes_for_level_base=350000000;" +
 
         // Causes file size to double per level. Lower total number of file.
-        + "target_file_size_multiplier=2;"
+        "target_file_size_multiplier=2;" +
 
         // This is basically useless on write only database. However, for halfpath with live pruning, flatdb, or
         // (maybe?) full sync where keys are deleted, replaced, or re-inserted, two memtable can merge together
         // resulting in a reduced total memtable size to be written. This does seems to reduce sync throughput though.
-        + "min_write_buffer_number_to_merge=2;"
+        "min_write_buffer_number_to_merge=2;" +
 
         // Default value is 16.
         // So each block consist of several "restart" and each "restart" is BlockRestartInterval number of key.
@@ -159,22 +175,16 @@ public class DbConfig : IDbConfig
         // through a minimum of "BlockRestartInterval" number of key, probably. That is my understanding.
         // Reducing this is likely going to improve CPU usage at the cost of increased uncompressed size, which effect
         // cache utilization.
-        + "block_based_table_factory.block_restart_interval=4;"
+        "block_based_table_factory.block_restart_interval=4;" +
 
         // This adds a hashtable-like index per block (the 32kb block)
         // In, this reduce CPU and therefore latency under high block cache hit scenario.
         // It seems to increase disk space use by about 1 GB.
-        + "block_based_table_factory.data_block_index_type=kDataBlockBinaryAndHash;"
-        + "block_based_table_factory.data_block_hash_table_util_ratio=0.5;"
+        "block_based_table_factory.data_block_index_type=kDataBlockBinaryAndHash;" +
+        "block_based_table_factory.data_block_hash_table_util_ratio=0.5;" +
 
-        + "block_based_table_factory.block_size=32000;"
+        "block_based_table_factory.block_size=32000;" +
 
-        + "block_based_table_factory.filter_policy=bloom_filter:15;"
-        ;
-
-    public bool WriteAheadLogSync { get; set; } = false;
-
-    public bool EnableDbStatistics { get; set; } = false;
-    public bool EnableMetricsUpdater { get; set; } = false;
-    public uint StatsDumpPeriodSec { get; set; } = 600;
+        "block_based_table_factory.filter_policy=bloom_filter:15;" +
+        "";
 }
--- src/Nethermind/Nethermind.Db.Rocks/Config/IDbConfig.cs
+++ src/Nethermind/Nethermind.Db.Rocks/Config/IDbConfig.cs
@@ -11,6 +11,19 @@ public interface IDbConfig : IConfig
     ulong SharedBlockCacheSize { get; set; }
     public bool SkipMemoryHintSetting { get; set; }
 
+    /// <summary>
+    /// Enables DB Statistics - https://github.com/facebook/rocksdb/wiki/Statistics
+    /// It can has a RocksDB performance hit between 5 and 10%.
+    /// </summary>
+    bool EnableDbStatistics { get; set; }
+    bool EnableMetricsUpdater { get; set; }
+    /// <summary>
+    /// If not zero, dump rocksdb.stats to LOG every stats_dump_period_sec
+    /// Default: 600 (10 min)
+    /// </summary>
+    uint StatsDumpPeriodSec { get; set; }
+
+
     ulong WriteBufferSize { get; set; }
     uint WriteBufferNumber { get; set; }
     ulong BlockCacheSize { get; set; }
@@ -19,7 +32,6 @@ public interface IDbConfig : IConfig
     ulong? ReadAheadSize { get; set; }
     string? AdditionalRocksDbOptions { get; set; }
     bool? VerifyChecksum { get; set; }
-    int MinWriteBufferNumberToMerge { get; set; }
     ulong? RowCacheSize { get; set; }
     bool EnableFileWarmer { get; set; }
     double CompressibilityHint { get; set; }
@@ -62,22 +74,8 @@ public interface IDbConfig : IConfig
     uint StateDbWriteBufferNumber { get; set; }
     ulong StateDbBlockCacheSize { get; set; }
     bool? StateDbVerifyChecksum { get; set; }
-    int StateDbMinWriteBufferNumberToMerge { get; set; }
     ulong? StateDbRowCacheSize { get; set; }
-    long? StateDbMaxWriteBufferSizeToMaintain { get; set; }
     bool StateDbEnableFileWarmer { get; set; }
     double StateDbCompressibilityHint { get; set; }
     string? StateDbAdditionalRocksDbOptions { get; set; }
-
-    /// <summary>
-    /// Enables DB Statistics - https://github.com/facebook/rocksdb/wiki/Statistics
-    /// It can has a RocksDB performance hit between 5 and 10%.
-    /// </summary>
-    bool EnableDbStatistics { get; set; }
-    bool EnableMetricsUpdater { get; set; }
-    /// <summary>
-    /// If not zero, dump rocksdb.stats to LOG every stats_dump_period_sec
-    /// Default: 600 (10 min)
-    /// </summary>
-    uint StatsDumpPeriodSec { get; set; }
 }
