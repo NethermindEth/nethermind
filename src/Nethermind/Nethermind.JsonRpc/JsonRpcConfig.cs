d01639f91c82a9952d148eda0d78a3d2e39732dd
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-11-28 01:11:45 +0530

    dotnet format

--- src/Nethermind/Nethermind.JsonRpc/JsonRpcConfig.cs
+++ src/Nethermind/Nethermind.JsonRpc/JsonRpcConfig.cs
@@ -10,17 +10,11 @@ namespace Nethermind.JsonRpc;
 public class JsonRpcConfig : IJsonRpcConfig
 {
     public static readonly JsonRpcConfig Default = new();
-
     private int? _webSocketsPort;
-
     public bool Enabled { get; set; }
-
     public string Host { get; set; } = "127.0.0.1";
-
     public int Timeout { get; set; } = 20000;
-
     public int RequestQueueLimit { get; set; } = 500;
-
     public string RpcRecorderBaseFilePath { get; set; } = "logs/rpc.{counter}.txt";
 
     public RpcRecorderState RpcRecorderState { get; set; } = RpcRecorderState.None;
@@ -36,29 +30,17 @@ public class JsonRpcConfig : IJsonRpcConfig
     public string? IpcUnixDomainSocketPath { get; set; } = null;
 
     public string[] EnabledModules { get; set; } = ModuleType.DefaultModules.ToArray();
-
-    public string[] AdditionalRpcUrls { get; set; } = Array.Empty<string>();
-
+    public string[] AdditionalRpcUrls { get; set; } = [];
     public long? GasCap { get; set; } = 100000000;
-
     public int ReportIntervalSeconds { get; set; } = 300;
-
     public bool BufferResponses { get; set; }
-
     public string CallsFilterFilePath { get; set; } = "Data/jsonrpc.filter";
-
     public long? MaxRequestBodySize { get; set; } = 30000000;
-
     public int MaxLogsPerResponse { get; set; } = 20_000;
-
     public int? EthModuleConcurrentInstances { get; set; } = null;
-
-    public string JwtSecretFile { get; set; } = null;
-
+    public string JwtSecretFile { get; set; } = "keystore/jwt-secret";
     public bool UnsecureDevNoRpcAuthentication { get; set; }
-
     public int? MaxLoggedRequestParametersCharacters { get; set; } = null;
-
     public string[]? MethodsLoggingFiltering { get; set; } =
     {
         "engine_newPayloadV1",
@@ -67,20 +49,12 @@ public class JsonRpcConfig : IJsonRpcConfig
         "engine_forkchoiceUpdatedV1",
         "engine_forkchoiceUpdatedV2"
     };
-
     public string EngineHost { get; set; } = "127.0.0.1";
-
     public int? EnginePort { get; set; } = null;
-
     public string[] EngineEnabledModules { get; set; } = ModuleType.DefaultEngineModules.ToArray();
-
     public int MaxBatchSize { get; set; } = 1024;
-
     public long? MaxBatchResponseBodySize { get; set; } = 32.MiB();
-
     public long? MaxSimulateBlocksCap { get; set; } = 256;
-
     public int EstimateErrorMargin { get; set; } = 150;
-
-    public string[] CorsOrigins { get; set; } = { "*" };
-}
+    public string[] CorsOrigins { get; set; } = ["*"];
+};
