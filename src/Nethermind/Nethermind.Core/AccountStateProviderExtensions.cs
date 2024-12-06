f871e7210c6237a7bc797540039ab9c7675a79ba
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2024-12-06 13:44:08 +0100

    Add StateMerkleizationTime Metrics (#7858)

--- src/Nethermind/Nethermind.Core/AccountStateProviderExtensions.cs
+++ src/Nethermind/Nethermind.Core/AccountStateProviderExtensions.cs
@@ -1,8 +1,11 @@
 // SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System.Runtime.CompilerServices;
+
 namespace Nethermind.Core
 {
+    [SkipLocalsInit]
     public static class AccountStateProviderExtensions
     {
         public static bool HasCode(this IAccountStateProvider stateProvider, Address address) =>
