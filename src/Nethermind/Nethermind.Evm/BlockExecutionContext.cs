1d05358571cc7d4802b138cb0573073ab096d3c2
yerke26 <yerkebulan@nethermind.io>
2024-12-09 15:15:45 +0500

    revert BlockExecutionContext

--- src/Nethermind/Nethermind.Evm/BlockExecutionContext.cs
+++ src/Nethermind/Nethermind.Evm/BlockExecutionContext.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using Nethermind.Core;
 using Nethermind.Int256;
 
@@ -14,6 +15,14 @@ public readonly struct BlockExecutionContext
     public BlockExecutionContext(BlockHeader blockHeader)
     {
         Header = blockHeader;
+        if (blockHeader?.ExcessBlobGas is not null)
+        {
+            if (!BlobGasCalculator.TryCalculateFeePerBlobGas(blockHeader.ExcessBlobGas.Value, out UInt256 feePerBlobGas))
+            {
+                throw new OverflowException("Blob gas price calculation led to overflow.");
+            }
+            BlobBaseFee = feePerBlobGas;
+        }
     }
 
     public BlockExecutionContext(BlockHeader blockHeader, UInt256 forceBlobBaseFee)
