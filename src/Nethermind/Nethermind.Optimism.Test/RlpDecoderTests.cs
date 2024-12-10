8beefaf1d9e953461dc1c7c85a62097d83e995d6
Lautaro Emanuel <emlautarom1@gmail.com>
2024-12-10 12:05:01 -0300

    Add test for problematic Tx

--- src/Nethermind/Nethermind.Optimism.Test/RlpDecoderTests.cs
+++ src/Nethermind/Nethermind.Optimism.Test/RlpDecoderTests.cs
@@ -3,6 +3,7 @@
 
 using FluentAssertions;
 using Nethermind.Core;
+using Nethermind.Core.Extensions;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Serialization.Rlp;
 using NUnit.Framework;
@@ -18,6 +19,7 @@ public class RlpDecoderTests
     {
         _decoder = TxDecoder.Instance;
         _decoder.RegisterDecoder(new OptimismTxDecoder<Transaction>());
+        _decoder.RegisterDecoder(new OptimismLegacyTxDecoder());
     }
 
     [Test]
@@ -48,5 +50,21 @@ public class RlpDecoderTests
         Transaction? decodedTx = Rlp.Decode<Transaction?>(rlpStream);
 
         decodedTx.Should().NotBeNull();
+    }
+
+    [Test]
+    public void Can_decode_Legacy_Empty_Signature()
+    {
+        _decoder.RegisterDecoder(new OptimismTxDecoder<Transaction>());
+
+        // See: https://github.com/NethermindEth/nethermind/issues/7880
+        var hexBytes =
+            "f901c9830571188083030d4094420000000000000000000000000000000000000780b901a4cbd4ece9000000000000000000000000420000000000000000000000000000000000001000000000000000000000000099c9fc46f92e8a1c0dec1b1747d010903e884be10000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000005711800000000000000000000000000000000000000000000000000000000000000e4662a633a000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec700000000000000000000000094b008aa00579c1307b0ef2c499ad98a8ce58e58000000000000000000000000117274dde02bc94006185af87d78beab28ceae06000000000000000000000000117274dde02bc94006185af87d78beab28ceae06000000000000000000000000000000000000000000000000000000000c3d8b8000000000000000000000000000000000000000000000000000000000000000c0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000808080";
+        var bytes = Bytes.FromHexString(hexBytes);
+        var context = bytes.AsRlpValueContext();
+
+        var transaction = _decoder.Decode(ref context);
+
+        transaction.Should().NotBeNull();
     }
 }
