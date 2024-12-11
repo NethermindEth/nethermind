b9b354d034750af1dde6e1087bd5230a1c969dc2
ak88 <anders@nethermind.io>
2024-12-11 19:44:11 +0100

    7702 changes for devnet 5 (#7781)
    Co-authored-by: Lukasz Rozmej <lukasz.rozmej@gmail.com>

--- src/Nethermind/Nethermind.Core/Eip7702Constants.cs
+++ src/Nethermind/Nethermind.Core/Eip7702Constants.cs
@@ -1,15 +1,24 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using Nethermind.Core.Crypto;
 using System;
 
 namespace Nethermind.Core;
 public static class Eip7702Constants
 {
+    private readonly static byte[] _delegationHeader = [0xef, 0x01, 0x00];
     public const byte Magic = 0x05;
-    public static ReadOnlySpan<byte> DelegationHeader => [0xef, 0x01, 0x00];
+    public static ReadOnlySpan<byte> DelegationHeader => _delegationHeader.AsSpan();
+    /// <summary>
+    /// Any code reading operation will only act on the first two bytes of the header
+    /// </summary>
+    public static ReadOnlyMemory<byte> FirstTwoBytesOfHeader => _delegationHeader.AsMemory(0, 2);
+
     private static readonly int HeaderLength = DelegationHeader.Length;
     public static bool IsDelegatedCode(ReadOnlySpan<byte> code) =>
         code.Length == HeaderLength + Address.Size
         && DelegationHeader.SequenceEqual(code[..DelegationHeader.Length]);
+
+    public static readonly Hash256 HashOfDelegationCode = Keccak.Compute(FirstTwoBytesOfHeader.Span);
 }
