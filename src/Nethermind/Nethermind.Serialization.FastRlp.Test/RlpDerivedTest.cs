2d57ef390d241d998194f483580891d7318a2c04
Lautaro Emanuel <emlautarom1@gmail.com>
2024-12-20 17:46:41 -0300

    Move record

--- src/Nethermind/Nethermind.Serialization.FastRlp.Test/RlpDerivedTest.cs
+++ src/Nethermind/Nethermind.Serialization.FastRlp.Test/RlpDerivedTest.cs
@@ -2,16 +2,22 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using FluentAssertions;
+// Automatically imports all source-generated instances
 using Nethermind.Serialization.FastRlp.Derived;
+// Imports `RlpSerializable` attribute
+using Nethermind.Serialization.FastRlp.Generator;
 
 namespace Nethermind.Serialization.FastRlp.Test;
+
+[RlpSerializable]
+public record Player(int Id, string Username);
 
 public class RlpDerivedTest
 {
     [Test]
     public void ReadDerivedRecord()
     {
-        var player = new Player(42, "SuperUser");
+        var player = new Player(Id: 42, Username: "SuperUser");
         ReadOnlySpan<byte> rlp = Rlp.Write((ref RlpWriter w) => w.Write(player));
 
         var decoded = Rlp.Read(rlp, (scoped ref RlpReader r) => r.ReadPlayer());
