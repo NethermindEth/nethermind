f3d2da55b5742abf27da2c759e9905a26b331383
Ben Adams <thundercat@illyriad.co.uk>
2024-12-21 07:26:54 +0000

    async

--- src/Nethermind/Nethermind.Network.Test/PeerManagerTests.cs
+++ src/Nethermind/Nethermind.Network.Test/PeerManagerTests.cs
@@ -729,7 +729,7 @@ namespace Nethermind.Network.Test
 
                 foreach (Session session in clone)
                 {
-                    session.MarkDisconnected(DisconnectReason.Other, DisconnectType.Remote, "test");
+                    _ = session.MarkDisconnected(DisconnectReason.Other, DisconnectType.Remote, "test");
                 }
             }
 
