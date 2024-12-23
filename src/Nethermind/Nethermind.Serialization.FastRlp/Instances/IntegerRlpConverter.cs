e51c97853336e3949f995eb4b768607d6058a41e
Lautaro Emanuel <emlautarom1@gmail.com>
2024-12-23 12:29:55 -0300

    Add explicit converters for IntXX
    - Required as Generics in Collections

--- src/Nethermind/Nethermind.Serialization.FastRlp/Instances/IntegerRlpConverter.cs
+++ src/Nethermind/Nethermind.Serialization.FastRlp/Instances/IntegerRlpConverter.cs
@@ -5,6 +5,34 @@ using System;
 
 namespace Nethermind.Serialization.FastRlp.Instances;
 
+public abstract class Int16RlpConverter : IRlpConverter<Int16>
+{
+    public static Int16 Read(ref RlpReader reader) => reader.ReadInteger<Int16>();
+
+    public static void Write(ref RlpWriter writer, Int16 value) => writer.Write(value);
+}
+
+public abstract class Int32RlpConverter : IRlpConverter<Int32>
+{
+    public static Int32 Read(ref RlpReader reader) => reader.ReadInteger<Int32>();
+
+    public static void Write(ref RlpWriter writer, Int32 value) => writer.Write(value);
+}
+
+public abstract class Int64RlpConverter : IRlpConverter<Int64>
+{
+    public static Int64 Read(ref RlpReader reader) => reader.ReadInteger<Int64>();
+
+    public static void Write(ref RlpWriter writer, Int64 value) => writer.Write(value);
+}
+
+public abstract class Int128RlpConverter : IRlpConverter<Int128>
+{
+    public static Int128 Read(ref RlpReader reader) => reader.ReadInteger<Int128>();
+
+    public static void Write(ref RlpWriter writer, Int128 value) => writer.Write(value);
+}
+
 // NOTE: No need for `Write` overloads since they're covered by generic primitives
 // `Read` methods are provided for a consistent API (instead of using generics primitives)
 public static class IntegerRlpConverterExt
