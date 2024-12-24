03325a044c9804fc8ed18745424c3016765dc80c
Lautaro Emanuel <emlautarom1@gmail.com>
2024-12-24 10:36:13 -0300

    Make `Read` calls static

--- src/Nethermind/Nethermind.Serialization.FastRlp.Test/RlpDerivedTest.cs
+++ src/Nethermind/Nethermind.Serialization.FastRlp.Test/RlpDerivedTest.cs
@@ -32,7 +32,7 @@ public class RlpDerivedTest
         var player = new Player(Id: 42, Username: "SuperUser");
         ReadOnlySpan<byte> rlp = Rlp.Write((ref RlpWriter w) => w.Write(player));
 
-        var decoded = Rlp.Read(rlp, (scoped ref RlpReader r) => r.ReadPlayer());
+        var decoded = Rlp.Read(rlp, static (scoped ref RlpReader r) => r.ReadPlayer());
         decoded.Should().BeEquivalentTo(player);
     }
 
@@ -42,7 +42,7 @@ public class RlpDerivedTest
         var player = new PlayerWithFriends(Id: 42, Username: "SuperUser", Friends: ["ana", "bob"]);
         ReadOnlySpan<byte> rlp = Rlp.Write((ref RlpWriter w) => w.Write(player));
 
-        var decoded = Rlp.Read(rlp, (scoped ref RlpReader r) => r.ReadPlayerWithFriends());
+        var decoded = Rlp.Read(rlp, static (scoped ref RlpReader r) => r.ReadPlayerWithFriends());
         decoded.Should().BeEquivalentTo(player);
     }
 
@@ -56,7 +56,7 @@ public class RlpDerivedTest
         });
         ReadOnlySpan<byte> rlp = Rlp.Write((ref RlpWriter w) => w.Write(player));
 
-        var decoded = Rlp.Read(rlp, (scoped ref RlpReader r) => r.ReadPlayerWithScores());
+        var decoded = Rlp.Read(rlp, static (scoped ref RlpReader r) => r.ReadPlayerWithScores());
         decoded.Should().BeEquivalentTo(player);
     }
 
@@ -72,7 +72,7 @@ public class RlpDerivedTest
         ]);
         ReadOnlySpan<byte> rlp = Rlp.Write((ref RlpWriter w) => w.Write(tree));
 
-        var decoded = Rlp.Read(rlp, (scoped ref RlpReader r) => r.ReadTree());
+        var decoded = Rlp.Read(rlp, static (scoped ref RlpReader r) => r.ReadTree());
         decoded.Should().BeEquivalentTo(tree);
     }
 }
