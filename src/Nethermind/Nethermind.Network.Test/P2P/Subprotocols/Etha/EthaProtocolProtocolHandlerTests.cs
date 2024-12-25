7a4231bf0cbc874b91b8fe3eb36e1b8ddce297da
crStiv <cryptostiv7@gmail.com>
2024-12-25 23:38:00 +0100

    test(network): add tests for EthaProtocolHandler
    Added unit tests for the etha protocol message handling:
    - Test for GetShardedBlocks message processing

---
+++ src/Nethermind/Nethermind.Network.Test/P2P/Subprotocols/Etha/EthaProtocolProtocolHandlerTests.cs
@@ -0,0 +1,55 @@
+using FluentAssertions;
+using Nethermind.Blockchain;
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Test.Builders;
+using Nethermind.Logging;
+using Nethermind.Network.P2P.Subprotocols.Etha.Messages;
+using Nethermind.Stats;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Nethermind.Network.Test.P2P.Subprotocols.Etha
+{
+    [TestFixture]
+    public class EthaProtocolHandlerTests
+    {
+        private IBlockTree _blockTree;
+        private IMessageSerializationService _serializationService;
+        private INodeStatsManager _statsManager;
+        private ILogManager _logManager;
+        private EthaProtocolHandler _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _blockTree = Substitute.For<IBlockTree>();
+            _serializationService = Substitute.For<IMessageSerializationService>();
+            _statsManager = Substitute.For<INodeStatsManager>();
+            _logManager = LimboLogs.Instance;
+            
+            _handler = new EthaProtocolHandler(
+                _blockTree,
+                _serializationService,
+                _statsManager,
+                _logManager);
+        }
+
+        [Test]
+        public void Handle_GetShardedBlocks_returns_requested_blocks()
+        {
+            // Arrange
+            Block block = Build.A.Block.WithNumber(1).TestObject;
+            _blockTree.FindBlock(block.Hash).Returns(block);
+            
+            var message = new GetShardedBlocksMessage(new[] { block.Hash });
+
+            // Act
+            _handler.HandleMessage(new Packet(EthaMessageCode.GetShardedBlocks, _serializationService.Serialize(message)));
+
+            // Assert
+            _serializationService.Received().Serialize(Arg.Is<ShardedBlocksMessage>(m => 
+                m.Blocks.Length == 1 && m.Blocks[0] == block));
+        }
+    }
+}
