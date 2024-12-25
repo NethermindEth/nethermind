042985b4bd106159f0476b6049a9880b8f6d353f
crStiv <cryptostiv7@gmail.com>
2024-12-25 23:25:45 +0100

    feat(network): add EthaMessageCode for EIP-7801
    Added message codes for the etha protocol (GetShardedBlocks, ShardedBlocks, NewShardedBlock)

---
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Etha/EthaMessageCode.cs
@@ -0,0 +1,9 @@
+namespace Nethermind.Network.P2P.Subprotocols.Etha
+{
+    public static class EthaMessageCode
+    {
+        public const int GetShardedBlocks = 0x00;
+        public const int ShardedBlocks = 0x01;
+        public const int NewShardedBlock = 0x02;
+    }
+} 
