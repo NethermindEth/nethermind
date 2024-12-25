a50553b5c10b51dc214e4c3917dbf1c7bd96c250
crStiv <cryptostiv7@gmail.com>
2024-12-26 00:05:58 +0100

    Update EthaMessageSerializerTests.cs

--- src/Nethermind/Nethermind.Network.Test/P2P/Subprotocols/Etha/Messages/EthaMessageSerializerTests.cs
+++ src/Nethermind/Nethermind.Network.Test/P2P/Subprotocols/Etha/Messages/EthaMessageSerializerTests.cs
@@ -36,5 +36,19 @@ namespace Nethermind.Network.Test.P2P.Subprotocols.Etha.Messages
             deserialized.Blocks.Length.Should().Be(message.Blocks.Length);
             deserialized.Blocks[0].Hash.Should().Be(message.Blocks[0].Hash);
         }
+
+        [Test]
+        public void Can_serialize_and_deserialize_NewShardedBlockMessage()
+        {
+            var serializer = new NewShardedBlockMessageSerializer();
+            var block = Build.A.Block.TestObject;
+            var message = new NewShardedBlockMessage(block);
+
+            RlpStream rlpStream = new RlpStream();
+            serializer.Serialize(rlpStream, message);
+
+            var deserialized = serializer.Deserialize(new RlpStream(rlpStream.Data));
+            deserialized.Block.Hash.Should().Be(message.Block.Hash);
+        }
     }
 } 
