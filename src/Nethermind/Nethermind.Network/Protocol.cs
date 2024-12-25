692aac859548a3431552d2d713e5f5cdf547671a
crStiv <cryptostiv7@gmail.com>
2024-12-25 23:50:48 +0100

    Create Protocol.cs

---
+++ src/Nethermind/Nethermind.Network/Protocol.cs
@@ -0,0 +1,9 @@
+public static class Protocol
+{
+    public const string P2P = "p2p";
+    public const string Eth = "eth";
+    public const string Les = "les";
+    public const string Snap = "snap";
+    public const string NodeData = "nd";
+    public const string Etha = "etha";
+} 
