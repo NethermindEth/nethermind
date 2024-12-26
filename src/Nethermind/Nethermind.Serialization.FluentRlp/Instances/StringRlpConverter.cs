fea1856dcfa282ee9b6302e2e72e0546e565614e
Lautaro Emanuel <emlautarom1@gmail.com>
2024-12-26 11:47:01 -0300

    Use field Encoding

--- src/Nethermind/Nethermind.Serialization.FluentRlp/Instances/StringRlpConverter.cs
+++ src/Nethermind/Nethermind.Serialization.FluentRlp/Instances/StringRlpConverter.cs
@@ -15,7 +15,7 @@ public abstract class StringRlpConverter : IRlpConverter<string>
     public static string Read(ref RlpReader reader)
     {
         ReadOnlySpan<byte> obj = reader.ReadBytes();
-        return Encoding.UTF8.GetString(obj);
+        return Encoding.GetString(obj);
     }
 
     public static void Write(ref RlpWriter writer, string value)
