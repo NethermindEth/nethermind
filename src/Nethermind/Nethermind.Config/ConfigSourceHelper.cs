99fac30863cd1c9ecbd7142df5592a37358d5089
Ruben Buniatyan <rubo@users.noreply.github.com>
2025-01-03 19:39:02 +0100

    Fix null string handling

--- src/Nethermind/Nethermind.Config/ConfigSourceHelper.cs
+++ src/Nethermind/Nethermind.Config/ConfigSourceHelper.cs
@@ -99,7 +99,7 @@ namespace Nethermind.Config
         }
 
         private static bool IsNullString(string valueString) =>
-            string.IsNullOrEmpty(valueString) || valueString.Equals("null", StringComparison.OrdinalIgnoreCase);
+            valueString?.Equals("null", StringComparison.OrdinalIgnoreCase) ?? true;
 
         public static object GetDefault(Type type) => type.IsValueType ? (false, Activator.CreateInstance(type)) : (false, null);
 
