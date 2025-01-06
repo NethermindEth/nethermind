c3d05d5cf82cd4e8fcd60ea7d4e0e0d4d36d9bc6
crStiv <cryptostiv7@gmail.com>
2025-01-06 21:29:10 +0100

    Update GetShardedBlocksMessage.cs

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Etha/Messages/GetShardedBlocksMessage.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Etha/Messages/GetShardedBlocksMessage.cs
@@ -3,12 +3,30 @@ using Nethermind.Network.P2P;
 
 namespace Nethermind.Network.P2P.Subprotocols.Etha.Messages
 {
+    /// <summary>
+    /// Message for requesting sharded blocks by their hashes.
+    /// </summary>
     public class GetShardedBlocksMessage : P2PMessage
     {
+        /// <summary>
+        /// Gets the packet type for the message.
+        /// </summary>
         public override int PacketType => EthaMessageCode.GetShardedBlocks;
+
+        /// <summary>
+        /// Gets the protocol identifier.
+        /// </summary>
+        public override string Protocol => "etha";
         
-        public Hash256[] BlockHashes { get; set; }
+        /// <summary>
+        /// Gets the array of block hashes to request.
+        /// </summary>
+        public Hash256[] BlockHashes { get; }
         
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetShardedBlocksMessage"/> class.
+        /// </summary>
+        /// <param name="blockHashes">The array of block hashes to request.</param>
         public GetShardedBlocksMessage(Hash256[] blockHashes)
         {
             BlockHashes = blockHashes;
