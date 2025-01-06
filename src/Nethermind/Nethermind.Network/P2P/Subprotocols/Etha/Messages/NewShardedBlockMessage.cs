b2fbf444e72b6352ebc2d120876a9321f71ae67e
crStiv <cryptostiv7@gmail.com>
2025-01-06 21:29:41 +0100

    Update NewShardedBlockMessage.cs

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Etha/Messages/NewShardedBlockMessage.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Etha/Messages/NewShardedBlockMessage.cs
@@ -3,15 +3,33 @@ using Nethermind.Network.P2P;
 
 namespace Nethermind.Network.P2P.Subprotocols.Etha.Messages
 {
+    /// <summary>
+    /// Message announcing a new sharded block.
+    /// </summary>
     public class NewShardedBlockMessage : P2PMessage
     {
+        /// <summary>
+        /// Gets the packet type for the message.
+        /// </summary>
         public override int PacketType => EthaMessageCode.NewShardedBlock;
+
+        /// <summary>
+        /// Gets the protocol identifier.
+        /// </summary>
+        public override string Protocol => "etha";
         
-        public Block Block { get; set; }
+        /// <summary>
+        /// Gets the new block being announced.
+        /// </summary>
+        public Block Block { get; }
         
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NewShardedBlockMessage"/> class.
+        /// </summary>
+        /// <param name="block">The new block to announce.</param>
         public NewShardedBlockMessage(Block block)
         {
             Block = block;
         }
     }
-}
+} 
