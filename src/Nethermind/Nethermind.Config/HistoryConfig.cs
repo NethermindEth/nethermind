6a4ab0017d6fa3ac0eeceb7d78331a38c4e67612
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2025-01-10 13:46:26 +0000

    create config

---
+++ src/Nethermind/Nethermind.Config/HistoryConfig.cs
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+namespace Nethermind.Config;
+
+public class HistoryConfig : IHistoryConfig
+{
+    public ulong? HistoryPruneEpochs { get; set; }
+    public bool DropPreMerge { get; set; }
+} 
---
+++ src/Nethermind/Nethermind.Config/IHistoryConfig.cs
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+namespace Nethermind.Config;
+
+public interface IHistoryConfig : IConfig
+{
+    // For EIP-4444 set default to 82125
+    [ConfigItem(
+        Description = "The number of epochs to retain historical blocks and receipts. Set to null for unlimited retention.",
+        DefaultValue = "null")]
+    ulong? HistoryPruneEpochs { get; set; }
+
+    [ConfigItem(
+        Description = "Whether to drop pre-merge blocks and receipts.",
+        DefaultValue = "false")]
+    bool DropPreMerge { get; set; }
+} 
