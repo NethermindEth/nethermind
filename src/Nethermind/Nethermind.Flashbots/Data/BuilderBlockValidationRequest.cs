7646fa6ea5f5f37892fcf76a198a2dac011d673f
mralj <nikola.mratinic@gmail.com>
2025-01-14 13:16:15 +0100

    maybe fix

--- src/Nethermind/Nethermind.Flashbots/Data/BuilderBlockValidationRequest.cs
+++ src/Nethermind/Nethermind.Flashbots/Data/BuilderBlockValidationRequest.cs
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Text.Json.Serialization;
 using Nethermind.Core.Crypto;
+using Newtonsoft.Json;
 
 namespace Nethermind.Flashbots.Data;
 
@@ -12,12 +12,16 @@ public class BuilderBlockValidationRequest
     /// The block hash of the parent beacon block.
     /// <see cref=https://github.com/flashbots/builder/blob/df9c765067d57ab4b2d0ad39dbb156cbe4965778/eth/block-validation/api.go#L198"/>
     /// </summary>
-    [JsonRequired]
+    [System.Text.Json.Serialization.JsonRequired]
+    [JsonProperty("parent_beacon_block_root")]
     public required Hash256 ParentBeaconBlockRoot { get; set; }
 
-    [JsonRequired]
+    [JsonProperty("register_gas_limit")]
+    [System.Text.Json.Serialization.JsonRequired]
     public long RegisterGasLimit { get; set; }
 
-    [JsonRequired]
+    [System.Text.Json.Serialization.JsonRequired]
+    [JsonProperty("block_request")]
+
     public required SubmitBlockRequest BlockRequest { get; set; }
 }
