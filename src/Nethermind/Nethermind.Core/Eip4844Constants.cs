6e318a832e3ed12c2f2e626b303b56ea985649e1
Tanishq Jasoria <jasoriatanishq@gmail.com>
2025-01-16 11:03:43 +0530

    whitespace

--- src/Nethermind/Nethermind.Core/Eip4844Constants.cs
+++ src/Nethermind/Nethermind.Core/Eip4844Constants.cs
@@ -22,7 +22,7 @@ public class Eip4844Constants
     /// Gets the <c>BLOB_GASPRICE_UPDATE_FRACTION</c> parameter.
     /// </summary>
     /// <remarks>Defaults to 3338477.</remarks>
-    public static UInt256 DefaultBlobGasPriceUpdateFraction{ get; private set; } = 3338477;
+    public static UInt256 DefaultBlobGasPriceUpdateFraction { get; private set; } = 3338477;
 
     /// <summary>
     /// Gets the <c>MIN_BLOB_GASPRICE</c> parameter, in wei.
