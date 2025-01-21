1ad0ea85e582add666a315592aff2e713fca6c39
yerke26 <yerkebulan@nethermind.io>
2025-01-21 22:39:35 +0700

    fix SizeTxFilter

--- src/Nethermind/Nethermind.TxPool/Filters/SizeTxFilter.cs
+++ src/Nethermind/Nethermind.TxPool/Filters/SizeTxFilter.cs
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using Nethermind.Core;
-using Nethermind.Core.Extensions;
-using Nethermind.Core.Specs;
 using Nethermind.Logging;
 
 namespace Nethermind.TxPool.Filters;
@@ -11,12 +9,10 @@ namespace Nethermind.TxPool.Filters;
 /// <summary>
 /// Ignores transactions that exceed configured max transaction size limit.
 /// </summary>
-internal sealed class SizeTxFilter(ITxPoolConfig txPoolConfig, ILogger logger, ISpecProvider specProvider) : IIncomingTxFilter
+internal sealed class SizeTxFilter(ITxPoolConfig txPoolConfig, ILogger logger) : IIncomingTxFilter
 {
     private readonly long _configuredMaxTxSize = txPoolConfig.MaxTxSize ?? long.MaxValue;
-    private readonly long _configuredMaxBlobTxSize = txPoolConfig.MaxBlobTxSize is null
-        ? long.MaxValue
-        : txPoolConfig.MaxBlobTxSize.Value + (long)specProvider.GetTransitionsMaxBlobGas();
+    private readonly long _configuredMaxBlobTxSize = txPoolConfig.MaxBlobTxSize ?? long.MaxValue;
 
     public AcceptTxResult Accept(Transaction tx, ref TxFilteringState state, TxHandlingOptions txHandlingOptions)
     {
