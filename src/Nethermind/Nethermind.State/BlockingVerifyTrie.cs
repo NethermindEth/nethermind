3b90fea30ebbd4043ed113f120ef8b621fbf13d3
Amirul Ashraf <asdacap@gmail.com>
2025-01-21 10:29:26 +0800

    Fix verifytrie does not block processing. (#8070)

--- src/Nethermind/Nethermind.State/BlockingVerifyTrie.cs
+++ src/Nethermind/Nethermind.State/BlockingVerifyTrie.cs
@@ -3,76 +3,50 @@
 
 using System;
 using System.Threading;
-using System.Threading.Tasks;
 using Autofac.Features.AttributeFilters;
-using Nethermind.Config;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Db;
 using Nethermind.Logging;
-using Nethermind.State;
 using Nethermind.Trie;
 using Nethermind.Trie.Pruning;
 
-namespace Nethermind.Synchronization.FastSync;
+namespace Nethermind.State;
 
-internal class BlockingVerifyTrie(
-    ITrieStore trieStore,
-    IStateReader stateReader,
-    [KeyFilter(DbNames.Code)] IDb codeDb,
-    IProcessExitSource exitSource,
-    ILogManager logManager) : IBlockingVerifyTrie
+public class BlockingVerifyTrie
 {
-    private readonly ILogger _logger = logManager.GetClassLogger<BlockingVerifyTrie>();
+    private readonly ILogger _logger;
 
-    private bool _alreadyRunning = false;
+    private readonly ITrieStore _trieStore;
+    private readonly IStateReader _stateReader;
+    private readonly IDb _codeDb;
+    private readonly ILogManager _logManager;
 
-    public bool TryStartVerifyTrie(BlockHeader stateAtBlock)
+    public BlockingVerifyTrie(
+        ITrieStore trieStore,
+        IStateReader stateReader,
+        [KeyFilter(DbNames.Code)] IDb codeDb,
+        ILogManager logManager)
     {
-        if (Interlocked.CompareExchange(ref _alreadyRunning, true, false))
+        if (trieStore is IReadOnlyTrieStore)
         {
-            return false;
+            throw new InvalidOperationException("TrieStore must not be read only to be able to block processing.");
         }
 
-        Task.Factory.StartNew(() =>
-        {
-            try
-            {
-                _logger!.Info("Collecting trie stats and verifying that no nodes are missing...");
-
-                Hash256 rootNode = stateAtBlock.StateRoot;
-
-                // This is to block processing as with halfpath old nodes will be removed
-                using IBlockCommitter? _ = trieStore.BeginBlockCommit(stateAtBlock.Number + 1);
-
-                TrieStats stats = stateReader.CollectStats(rootNode, codeDb, logManager, exitSource.Token);
-                if (stats.MissingNodes > 0)
-                {
-                    if (_logger.IsError) _logger.Error($"Missing node found!");
-                }
-
-                if (_logger.IsInfo) _logger.Info($"Stats after finishing state \n" + stats);
-            }
-            catch (OperationCanceledException)
-            {
-            }
-            catch (Exception e)
-            {
-                if (_logger.IsError) _logger.Error($"Error in verify trie", e);
-            }
-
-        }, TaskCreationOptions.LongRunning);
-
-        return true;
+        _trieStore = trieStore;
+        _stateReader = stateReader;
+        _codeDb = codeDb;
+        _logManager = logManager;
+        _logger = logManager.GetClassLogger<BlockingVerifyTrie>();
     }
 
     public bool VerifyTrie(BlockHeader stateAtBlock, CancellationToken cancellationToken)
     {
         // This is to block processing as with halfpath old nodes will be removed
-        using IBlockCommitter? _ = trieStore.BeginBlockCommit(stateAtBlock.Number + 1);
+        using IBlockCommitter? _ = _trieStore.BeginBlockCommit(stateAtBlock.Number + 1);
 
         Hash256 rootNode = stateAtBlock.StateRoot;
-        TrieStats stats = stateReader.CollectStats(rootNode, codeDb, logManager, cancellationToken);
+        TrieStats stats = _stateReader.CollectStats(rootNode, _codeDb, _logManager, cancellationToken);
         if (stats.MissingNodes > 0)
         {
             if (_logger.IsError) _logger.Error($"Missing node found!");
