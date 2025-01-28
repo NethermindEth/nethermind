2124634bbcbb5678a62407ea4b347978f9b5fb05
Lautaro Emanuel <emlautarom1@gmail.com>
2025-01-28 11:02:56 -0300

    Cleanup script

--- scripts/superchain.py
+++ scripts/superchain.py
@@ -60,12 +60,7 @@ def to_nethermind_accounts(genesis):
 
     result = {}
     for address, account in alloc.items():
-        k = f"0x{address}"
-        v = account
-        if "code" in v and v["code"].startswith("0x"):
-            v["code"] = v["code"][2:]
-
-        result[k] = v
+        result[f"0x{address}"] = account
 
     return result
 
@@ -168,6 +163,7 @@ def to_nethermind_chainspec(chain_name, l1, superchain, chain, genesis):
             }
         ),
         "nodes": [
+            # From: https://github.com/ethereum-optimism/op-geth/blob/1ab39853a132dbf098bc34436510473d27ac2dc9/params/bootnodes.go
             "enode://d860a01f9722d78051619d1e2351aba3f43f943f6f00718d1b9baa4101932a1f5011f16bb2b1bb35db20d6fe28fa0bf09636d26a87d31de9ec6203eeedb1f666@18.138.108.67:30303",
             "enode://22a8232c3abc76a16ae9d6c3b164f98775fe226f0917b0ca871128a74a8e9630b458460865bab457221f1d448dd9791d24c4e5d88786180ac185df813a68d4de@3.209.45.79:30303",
             "enode://2b252ab6a1d0f971d9722cb839a42cb81db019ba44c08754628ab4a823487071b5695317c8ccd085219c3a03af063495b2f1da8d18218da2d6a82981b45e6ffc@65.108.70.101:30303",
