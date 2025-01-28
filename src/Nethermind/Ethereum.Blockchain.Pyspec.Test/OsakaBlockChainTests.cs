50e12205bc6ce61072c46cbe248692bab8c7a416
Ben Adams <thundercat@illyriad.co.uk>
2025-01-28 17:37:10 +0000

    Update Eof tests

--- src/Nethermind/Ethereum.Blockchain.Pyspec.Test/OsakaBlockChainTests.cs
+++ src/Nethermind/Ethereum.Blockchain.Pyspec.Test/OsakaBlockChainTests.cs
@@ -1,20 +1,20 @@
-// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Ethereum.Test.Base;
-using FluentAssertions;
 using NUnit.Framework;
 
 namespace Ethereum.Blockchain.Pyspec.Test;
 
 [TestFixture]
 [Parallelizable(ParallelScope.All)]
-public class OsakaStateTests : GeneralStateTestBase
+public class OsakaBlockChainTests : BlockchainTestBase
 {
     [TestCaseSource(nameof(LoadTests))]
-    public void Test(GeneralStateTest test) => RunTest(test).Pass.Should().BeTrue();
+    public async Task Test(BlockchainTest test) => await RunTest(test);
 
     private static IEnumerable<TestCaseData> LoadTests()
     {
@@ -22,8 +22,8 @@ public class OsakaStateTests : GeneralStateTestBase
         {
             ArchiveName = "fixtures_eip7692.tar.gz",
             ArchiveVersion = "eip7692@v2.1.0"
-        }, $"fixtures/state_tests/osaka");
-        return loader.LoadTests().Cast<GeneralStateTest>().Select(t => new TestCaseData(t)
+        }, $"fixtures/blockchain_tests/osaka");
+        return loader.LoadTests().OfType<BlockchainTest>().Select(t => new TestCaseData(t)
             .SetName(t.Name)
             .SetCategory(t.Category));
     }
