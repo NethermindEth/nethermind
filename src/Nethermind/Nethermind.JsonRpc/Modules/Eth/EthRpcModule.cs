5506f831117bc7f85b6934ea1384c406f7302a7d
Rohit Ranjan <rohitrjn629@gmail.com>
2025-02-03 16:14:47 +0530

    Merge eth_simulate_v1 to master (#8153)
    Co-authored-by: Lukasz Rozmej <lukasz.rozmej@gmail.com>

--- src/Nethermind/Nethermind.JsonRpc/Modules/Eth/EthRpcModule.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Eth/EthRpcModule.cs
@@ -343,7 +343,7 @@ public partial class EthRpcModule(
             .ExecuteTx(transactionCall, blockParameter, stateOverride);
 
     public ResultWrapper<IReadOnlyList<SimulateBlockResult>> eth_simulateV1(SimulatePayload<TransactionForRpc> payload, BlockParameter? blockParameter = null) =>
-        new SimulateTxExecutor(_blockchainBridge, _blockFinder, _rpcConfig, _secondsPerSlot)
+        new SimulateTxExecutor(_blockchainBridge, _blockFinder, _rpcConfig)
             .Execute(payload, blockParameter);
 
     public ResultWrapper<UInt256?> eth_estimateGas(TransactionForRpc transactionCall, BlockParameter? blockParameter, Dictionary<Address, AccountOverride>? stateOverride = null) =>
