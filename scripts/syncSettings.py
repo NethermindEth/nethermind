48aaec3f2b26bdcb46ba7ab0e7ddf3baac81cef6
Kamil Chodoła <43241881+kamilchodola@users.noreply.github.com>
2025-02-04 10:09:36 +0100

    Apply isPoS for update script so TD is only updated for non-PoS chains

--- scripts/syncSettings.py
+++ scripts/syncSettings.py
@@ -22,113 +22,135 @@ configs = {
     "mainnet": {
         "url": "api.etherscan.io",
         "blockReduced": 1000,
-        "multiplierRequirement": 1000
+        "multiplierRequirement": 1000,
+        "isPoS": True
     },
     "gnosis": {
         "url": "https://rpc.gnosischain.com",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": True
     },
     "chiado": {
         "url": "https://rpc.chiadochain.net",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": True
     },
     "sepolia": {
         "url": "api-sepolia.etherscan.io",
         "blockReduced": 1000,
-        "multiplierRequirement": 1000
+        "multiplierRequirement": 1000,
+        "isPoS": True
     },
     "energyweb": {
         "url": "https://rpc.energyweb.org",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": False
     },
     "volta": {
         "url": "https://volta-rpc.energyweb.org",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": False
     },
     "exosama": {
         "url": "https://rpc.exosama.com",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": False
     },
     "joc-mainnet": {
         "url": "https://rpc-1.japanopenchain.org:8545",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": False
     },
     "joc-testnet": {
         "url": "https://rpc-1.testnet.japanopenchain.org:8545",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": False
     },
     "base-mainnet": {
         "url": "https://mainnet.base.org",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": True
     },
     "base-sepolia": {
         "url": "https://sepolia.base.org",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": True
     },
     "op-mainnet": {
         "url": "https://mainnet.optimism.io",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": True
     },
     "op-sepolia": {
         "url": "https://sepolia.optimism.io",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": True
     },
     "linea-mainnet": {
         "url": "https://rpc.linea.build",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": False
     },
     "linea-sepolia": {
         "url": "https://rpc.sepolia.linea.build",
         "blockReduced": 8192,
-        "multiplierRequirement": 10000
+        "multiplierRequirement": 10000,
+        "isPoS": False
     }
 }
 
-def fastBlocksSettings(configuration, apiUrl, blockReduced, multiplierRequirement):
+def fastBlocksSettings(configuration, apiUrl, blockReduced, multiplierRequirement, isPoS):
     if "etherscan" in apiUrl:
-        latestBlock = int(json.loads(subprocess.getoutput(f'curl --silent "https://{apiUrl}/api?module=proxy&action=eth_blockNumber&apikey={key}"'))['result'],16)
+        latestBlock = int(json.loads(subprocess.getoutput(
+            f'curl --silent "https://{apiUrl}/api?module=proxy&action=eth_blockNumber&apikey={key}"'))['result'], 16)
     else:
-        data = '{"id":0,"jsonrpc":"2.0","method": "eth_blockNumber","params": []}'
-
-        response = requests.post(apiUrl, headers=headers, data=data).text
+        data_req = '{"id":0,"jsonrpc":"2.0","method": "eth_blockNumber","params": []}'
+        response = requests.post(apiUrl, headers=headers, data=data_req).text
         latestBlock = int(json.loads(response)['result'], 16)
 
     baseBlock = latestBlock - blockReduced
     baseBlock = baseBlock - baseBlock % multiplierRequirement
 
     if "etherscan" in apiUrl:
-        pivot = json.loads(subprocess.getoutput(f'curl --silent "https://{apiUrl}/api?module=proxy&action=eth_getBlockByNumber&tag={hex(baseBlock)}&boolean=true&apikey={key}"'))
+        pivot = json.loads(subprocess.getoutput(
+            f'curl --silent "https://{apiUrl}/api?module=proxy&action=eth_getBlockByNumber&tag={hex(baseBlock)}&boolean=true&apikey={key}"'))
     else:
-        data = '{"id":0,"jsonrpc":"2.0","method": "eth_getBlockByNumber","params": ["' +str(hex(baseBlock))+ '", false]}'
-        pivot = json.loads(requests.post(apiUrl, headers=headers, data=data).text)
+        data_req = '{"id":0,"jsonrpc":"2.0","method": "eth_getBlockByNumber","params": ["' + str(hex(baseBlock)) + '", false]}'
+        pivot = json.loads(requests.post(apiUrl, headers=headers, data=data_req).text)
 
     pivotHash = pivot['result']['hash']
     pivotTotalDifficulty = int(pivot['result'].get('totalDifficulty', '0x0'), 16)
-    print(configuration + 'LatestBlock: ' + str(latestBlock))
-    print(configuration + 'PivotNumber: ' + str(baseBlock))
-    print(configuration + 'PivotHash: ' + str(pivotHash))
-    print(configuration + 'PivotTotalDifficulty: ' + str(pivotTotalDifficulty))
-    data = {}
+
+    print(configuration + ' LatestBlock: ' + str(latestBlock))
+    print(configuration + ' PivotNumber: ' + str(baseBlock))
+    print(configuration + ' PivotHash: ' + str(pivotHash))
+    if not isPoS:
+      print(configuration + ' PivotTotalDifficulty: ' + str(pivotTotalDifficulty))
+
     with open(f'{configsPath}/{configuration}.json', 'r') as mainnetCfg:
         data = json.load(mainnetCfg)
-        data['Sync']['PivotNumber'] = baseBlock
-        data['Sync']['PivotHash'] = pivotHash
+
+    data['Sync']['PivotNumber'] = baseBlock
+    data['Sync']['PivotHash'] = pivotHash
+  
+    if not isPoS:
         data['Sync']['PivotTotalDifficulty'] = str(pivotTotalDifficulty)
-        with open(f'{configsPath}/{configuration}.json', 'w') as mainnetCfgChanged:
-            json.dump(data, mainnetCfgChanged, indent=2)
+
+    with open(f'{configsPath}/{configuration}.json', 'w') as mainnetCfgChanged:
+        json.dump(data, mainnetCfgChanged, indent=2)
 
 for config, value in configs.items():
     print(emoji.emojize(f"{config.capitalize()} section                                     :white_check_mark: "))
-    fastBlocksSettings(config, value['url'], value['blockReduced'], value['multiplierRequirement'])
+    fastBlocksSettings(config, value['url'], value['blockReduced'], value['multiplierRequirement'], value['isPoS'])
