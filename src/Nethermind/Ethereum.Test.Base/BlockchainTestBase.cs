9c277fb7a66b981ccfff64f7c3364714c3e15a27
Ben Adams <thundercat@illyriad.co.uk>
2025-02-05 17:54:43 +0000

    Dial back test logging

--- src/Nethermind/Ethereum.Test.Base/BlockchainTestBase.cs
+++ src/Nethermind/Ethereum.Test.Base/BlockchainTestBase.cs
@@ -38,8 +38,7 @@ namespace Ethereum.Test.Base;
 
 public abstract class BlockchainTestBase
 {
-    private static InterfaceLogger _logger = new NUnitLogger(LogLevel.Trace);
-    // private static ILogManager _logManager = new OneLoggerLogManager(_logger);
+    private static InterfaceLogger _logger = new NUnitLogger(LogLevel.Info);
     private static ILogManager _logManager = LimboLogs.Instance;
     private static ISealValidator Sealer { get; }
     private static DifficultyCalculatorWrapper DifficultyCalculator { get; }
