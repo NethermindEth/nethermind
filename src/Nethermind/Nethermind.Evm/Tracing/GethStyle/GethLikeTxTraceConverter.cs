e98ccb0cf6094a674e942189c2600ef868037d61
Pavlo Rytikov <pavlo@nethermind.io>
2025-02-06 17:49:43 +0200

    Add TxHash to GethLikeCustomTrace, update tracers to fill it, fix tests

--- src/Nethermind/Nethermind.Evm/Tracing/GethStyle/GethLikeTxTraceConverter.cs
+++ src/Nethermind/Nethermind.Evm/Tracing/GethStyle/GethLikeTxTraceConverter.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Nethermind.Serialization.Json;
 
 namespace Nethermind.Evm.Tracing.GethStyle;
 
@@ -31,14 +32,23 @@ public class GethLikeTxTraceConverter : JsonConverter<GethLikeTxTrace>
         {
             writer.WriteStartObject();
 
-            writer.WritePropertyName("gas"u8);
-            JsonSerializer.Serialize(writer, value.Gas, options);
+            NumberConversion? previousValue = ForcedNumberConversion.ForcedConversion.Value;
+            ForcedNumberConversion.ForcedConversion.Value = NumberConversion.Raw;
+            try
+            {
+                writer.WritePropertyName("gas"u8);
+                JsonSerializer.Serialize(writer, value.Gas, options);
+            }
+            finally
+            {
+                ForcedNumberConversion.ForcedConversion.Value = previousValue;
+            }
 
             writer.WritePropertyName("failed"u8);
             JsonSerializer.Serialize(writer, value.Failed, options);
 
             writer.WritePropertyName("returnValue"u8);
-            JsonSerializer.Serialize(writer, value.ReturnValue, options);
+            ByteArrayConverter.Convert(writer, value.ReturnValue, skipLeadingZeros: false, addHexPrefix: false);
 
             writer.WritePropertyName("structLogs"u8);
             JsonSerializer.Serialize(writer, value.Entries, options);
@@ -47,90 +57,3 @@ public class GethLikeTxTraceConverter : JsonConverter<GethLikeTxTrace>
         }
     }
 }
-/*
-public class GethLikeTxTraceConverter : JsonConverter<GethLikeTxTrace>
-{
-    public override GethLikeTxTrace ReadJson(
-        JsonReader reader,
-        Type objectType,
-        GethLikeTxTrace existingValue,
-        bool hasExistingValue,
-        JsonSerializer serializer) => throw new NotSupportedException();
-
-    public override void WriteJson(JsonWriter writer, GethLikeTxTrace value, JsonSerializer serializer)
-    {
-        if (value is null)
-        {
-            writer.WriteNull();
-        }
-        else if (value.CustomTracerResult is not null)
-        {
-            serializer.Serialize(writer, value.CustomTracerResult);
-        }
-        else
-        {
-            writer.WriteStartObject();
-
-            writer.WriteProperty("gas", value.Gas, serializer);
-            writer.WriteProperty("failed", value.Failed);
-            writer.WriteProperty("returnValue", value.ReturnValue, serializer);
-
-            writer.WritePropertyName("structLogs");
-            WriteEntries(writer, value.Entries, serializer);
-
-            writer.WriteEndObject();
-        }
-    }
-
-    private static void WriteEntries(JsonWriter writer, List<GethTxTraceEntry> entries, JsonSerializer _)
-    {
-        writer.WriteStartArray();
-        foreach (GethTxTraceEntry entry in entries)
-        {
-            writer.WriteStartObject();
-            writer.WriteProperty("pc", entry.ProgramCounter);
-            writer.WriteProperty("op", entry.Opcode);
-            writer.WriteProperty("gas", entry.Gas);
-            writer.WriteProperty("gasCost", entry.GasCost);
-            writer.WriteProperty("depth", entry.Depth);
-            writer.WriteProperty("error", entry.Error);
-            writer.WritePropertyName("stack");
-            writer.WriteStartArray();
-            foreach (string stackItem in entry.Stack)
-            {
-                writer.WriteValue(stackItem);
-            }
-
-            writer.WriteEndArray();
-
-            if (entry.Memory is not null)
-            {
-                writer.WritePropertyName("memory");
-                writer.WriteStartArray();
-                foreach (string memory in entry.Memory)
-                {
-                    writer.WriteValue(memory);
-                }
-                writer.WriteEndArray();
-            }
-
-            if (entry.Storage is not null)
-            {
-                writer.WritePropertyName("storage");
-                writer.WriteStartObject();
-
-                foreach (var item in entry.Storage.OrderBy(s => s.Key))
-                {
-                    writer.WriteProperty(item.Key, item.Value);
-                }
-
-                writer.WriteEndObject();
-            }
-
-            writer.WriteEndObject();
-        }
-
-        writer.WriteEndArray();
-    }
-}
-*/
