54791c38c909648716b054bf16e50aa9d34a406b
Rohit Ranjan <rohitrjn629@gmail.com>
2025-02-06 22:21:47 +0530

    apply rubo suggestions

--- src/Nethermind/Nethermind.Flashbots/IFlashbotsConfig.cs
+++ src/Nethermind/Nethermind.Flashbots/IFlashbotsConfig.cs
@@ -10,22 +10,21 @@ public interface IFlashbotsConfig : IConfig
     [ConfigItem(Description = "Whether to enable the Flashbots endpoints.", DefaultValue = "false")]
     bool Enabled { get; set; }
 
-    [ConfigItem(Description = "If set to true, proposer payment is calculated as a balance difference of the fee recipient", DefaultValue = "false")]
+    [ConfigItem(Description = "Whether to calculate the proposer payment as a balance difference of the fee recipient.", DefaultValue = "false")]
     public bool UseBalanceDiffProfit { get; set; }
 
-    [ConfigItem(Description = "If set to true, withdrawals to the fee recipient are excluded from the balance delta", DefaultValue = "false")]
+    [ConfigItem(Description = "Whether to exclude the withdrawals to the fee recipient from the balance difference.", DefaultValue = "false")]
     public bool ExcludeWithdrawals { get; set; }
 
-    [ConfigItem(Description = "If set to true, the pre-warmer will be enabled", DefaultValue = "true")]
+    [ConfigItem(Description = "Whether to enable the pre-warmer.", DefaultValue = "true")]
     public bool EnablePreWarmer { get; set; }
 
-    [ConfigItem(Description = "If set to true, the validation will be enabled", DefaultValue = "false")]
+    [ConfigItem(Description = "Whether to enable validation.", DefaultValue = "false")]
     public bool EnableValidation { get; set; }
 
     [ConfigItem(
     Description = """
-        The number of concurrent instances for non-sharable calls:
-        - `flashbots_validateBuilderSubmissionV3`
+        The number of concurrent instances for non-sharable calls for `flashbots_validateBuilderSubmissionV3`
         This limits the load on the CPU and I/O to reasonable levels. If the limit is exceeded, HTTP 503 is returned along with the JSON-RPC error. Defaults to the number of logical processors.
         """)]
     int? FlashbotsModuleConcurrentInstances { get; set; }
