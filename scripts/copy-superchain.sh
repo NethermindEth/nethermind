28b17e4c13629006cc3f84a958f3fdcc9322c33b
Lautaro Emanuel <emlautarom1@gmail.com>
2025-02-07 12:21:07 -0300

    Delete copy script
    - Not needed

--- scripts/copy-superchain.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
-# SPDX-License-Identifier: LGPL-3.0-only
-
-set -euo pipefail
-
-cp output/chainspec/* ../src/Nethermind/Chains
-cp output/runner/* ../src/Nethermind/Nethermind.Runner/configs
