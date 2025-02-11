920c85d77583b7c20df7cce40a9c3130d110a037
Ruben Buniatyan <rubo@users.noreply.github.com>
2025-02-11 18:35:03 +0100

    Fix build errors

--- tools/Evm/Program.cs
+++ tools/Evm/Program.cs
@@ -1,13 +1,20 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using Evm.T8n;
+namespace Evm.T8n;
+
 using System.CommandLine;
 
-CliRootCommand rootCmd = [];
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        CliRootCommand rootCmd = [];
 
-T8nCommand.Configure(ref rootCmd);
+        T8nCommand.Configure(ref rootCmd);
 
-CliConfiguration cli = new(rootCmd);
+        CliConfiguration cli = new(rootCmd);
 
-return cli.Invoke(args);
+        return cli.Invoke(args);
+    }
+}
--- tools/SendBlobs/Program.cs
+++ tools/SendBlobs/Program.cs
@@ -1,16 +1,24 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using SendBlobs;
+namespace SendBlobs;
+
 using System.CommandLine;
 
-CliRootCommand rootCommand = [];
+public static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        CliRootCommand rootCommand = [];
 
-SetupCli.SetupExecute(rootCommand);
-SetupCli.SetupDistributeCommand(rootCommand);
-SetupCli.SetupReclaimCommand(rootCommand);
-SetupCli.SetupSendFileCommand(rootCommand);
+        SetupCli.SetupExecute(rootCommand);
+        SetupCli.SetupDistributeCommand(rootCommand);
+        SetupCli.SetupReclaimCommand(rootCommand);
+        SetupCli.SetupSendFileCommand(rootCommand);
 
-CliConfiguration cli = new(rootCommand);
+        CliConfiguration cli = new(rootCommand);
 
-return await cli.InvokeAsync(args);
+        return await cli.InvokeAsync(args);
+    }
+}
+
