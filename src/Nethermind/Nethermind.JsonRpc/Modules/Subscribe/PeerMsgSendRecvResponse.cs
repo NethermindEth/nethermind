10312b61afd155b91e28b6463fcdcb368c685e52
StevenChongHuo <47324183+StevenChongHuo@users.noreply.github.com>
2025-02-11 23:11:04 +0800

    Implement admin_peerEvents for rpc module (#7999)
    Co-authored-by: Lukasz Rozmej <lukasz.rozmej@gmail.com>
    Co-authored-by: Ruben Buniatyan <rubo@users.noreply.github.com>

---
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Subscribe/PeerMsgSendRecvResponse.cs
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Text.Json.Serialization;
+using System.Runtime.CompilerServices;
+using Nethermind.JsonRpc.Modules.Admin;
+using System;
+
+namespace Nethermind.JsonRpc.Modules.Subscribe;
+
+public class PeerMsgSendRecvResponse
+{
+    protected PeerMsgSendRecvResponse()
+    {
+
+    }
+
+    public PeerMsgSendRecvResponse(EventArgs eventArgs, string subscripionType, string? e)
+    {
+        Type = subscripionType;
+        Error = e;
+    }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string Type { get; set; }
+    public string? Error { get; set; }
+}
