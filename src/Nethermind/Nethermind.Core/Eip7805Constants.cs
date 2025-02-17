e2fae0c2ea753395bbfa94b49e3a417ffc408416
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2025-02-17 17:33:21 +0000

    add 7805 constants file

---
+++ src/Nethermind/Nethermind.Core/Eip7805Constants.cs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+namespace Nethermind.Core;
+
+public static class Eip7805Constants
+{
+    public const int MaxBytesPerInclusionList = 8192;
+    // 32 bytes as conservative lower bound for transaction size, used for allocating buffers
+    public const int MaxTransactionsPerInclusionList = MaxBytesPerInclusionList / 32;
+}
