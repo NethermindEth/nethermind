e1f004b5c9919cce0171a04c7671522f144249dc
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2025-02-17 13:07:50 +0000

    use txpooltxsource explicitly in GetInclusionListHandler

--- src/Nethermind/Nethermind.Merge.Plugin/Handlers/GetInclusionListTransactionsHandler.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Handlers/GetInclusionListTransactionsHandler.cs
@@ -6,13 +6,13 @@ using Nethermind.JsonRpc;
 using Nethermind.Serialization.Rlp;
 using Nethermind.Blockchain;
 using System.Collections.Generic;
-using Nethermind.Consensus.Transactions;
+using Nethermind.Consensus.Producers;
 
 namespace Nethermind.Merge.Plugin.Handlers;
 
 public class GetInclusionListTransactionsHandler(
     IBlockTree blockTree,
-    ITxSource inclusionListTxSource)
+    TxPoolTxSource txPoolTxSource)
     : IHandler<byte[][]>
 {
     private const int MaxILSizeBytes = 8000;
@@ -20,7 +20,7 @@ public class GetInclusionListTransactionsHandler(
     public ResultWrapper<byte[][]> Handle()
     {
         // todo: get top transactions from txpool or something else?
-        IEnumerable<Transaction> txs = inclusionListTxSource.GetTransactions(blockTree.Head!.Header, long.MaxValue);
+        IEnumerable<Transaction> txs = txPoolTxSource.GetTransactions(blockTree.Head!.Header, long.MaxValue);
         byte[][] txBytes = [.. DecodeTransactionsUpToLimit(txs)];
         return ResultWrapper<byte[][]>.Success(txBytes);
     }
@@ -34,7 +34,7 @@ public class GetInclusionListTransactionsHandler(
             size += txBytes.Length;
             if (size > MaxILSizeBytes)
             {
-                break;
+                yield break;
             }
             yield return txBytes;
         }
