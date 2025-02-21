d63c81330eb99fbcecd3fa49de58946f798621ed
Demuirgos <ayman.bouchareb@outlook.fr>
2025-02-21 02:56:42 +0100

    + fix Extcodes interaction with CodeInfoRepo

--- src/Nethermind/Nethermind.Evm/VirtualMachine.cs
+++ src/Nethermind/Nethermind.Evm/VirtualMachine.cs
@@ -2314,7 +2314,7 @@ public sealed class VirtualMachine<TLogger, TOptimizing> : IVirtualMachine
             !UpdateMemoryCost(vmState, ref gasAvailable, in outputOffset, outputLength) ||
             !UpdateGas(gasExtra, ref gasAvailable)) return EvmExceptionType.OutOfGas;
 
-        CodeInfo codeInfo = vmState.Env.TxExecutionContext.CodeInfoRepository.GetCachedCodeInfo(state, codeSource, spec);
+        CodeInfo codeInfo = vmState.Env.TxExecutionContext.CodeInfoRepository.GetCachedCodeInfoFollowsDelegation(state, codeSource, spec);
         codeInfo.AnalyseInBackgroundIfRequired();
 
         if (spec.Use63Over64Rule)
