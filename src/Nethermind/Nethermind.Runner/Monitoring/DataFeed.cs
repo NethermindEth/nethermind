5d489fd14f7bcccc841c91a5be91a65280aa6fb2
Ben Adams <thundercat@illyriad.co.uk>
2025-02-21 01:40:56 +0000

    Versions

--- src/Nethermind/Nethermind.Runner/Monitoring/DataFeed.cs
+++ src/Nethermind/Nethermind.Runner/Monitoring/DataFeed.cs
@@ -52,7 +52,7 @@ public class DataFeed
         ArgumentNullException.ThrowIfNull(_blockTree);
         ArgumentNullException.ThrowIfNull(_syncPeerPool);
 
-        api.BlockchainProcessor.NewProcessingStatistics += OnNewProcessingStatistics;
+        api.MainProcessingContext.BlockchainProcessor.NewProcessingStatistics += OnNewProcessingStatistics;
         _blockTree.OnForkChoiceUpdated += OnForkChoiceUpdated;
         ConsoleHelpers.LineWritten += OnConsoleLineWritten;
         _ = StartTxFlowRefresh();
