b58812f5085daf2f57642af9e33e8792a92a2df9
Alex Bespalov <alexb5dh@gmail.com>
2025-02-24 01:52:17 +0200

    [WIP] secpr256r1 benchmarks

---
+++ src/Nethermind/Nethermind.Precompiles.Benchmark/Secp256r1Benchmark.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Collections.Generic;
+using Nethermind.Evm.Precompiles;
+
+namespace Nethermind.Precompiles.Benchmark;
+
+public class Secp256r1Benchmark: PrecompileBenchmarkBase
+{
+    protected override IEnumerable<IPrecompile> Precompiles => [Secp256r1Precompile.Instance];
+    protected override string InputsDirectory => "secp256r1";
+}
