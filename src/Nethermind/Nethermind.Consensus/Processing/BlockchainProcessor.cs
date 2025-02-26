0e938b9c4af076265d99655a6352c079df92739a
Amirul Ashraf <asdacap@gmail.com>
2025-02-26 10:17:28 +0800

    Add limit to branch size.

--- src/Nethermind/Nethermind.Consensus/Processing/BlockchainProcessor.cs
+++ src/Nethermind/Nethermind.Consensus/Processing/BlockchainProcessor.cs
@@ -58,7 +58,7 @@ public sealed class BlockchainProcessor : IBlockchainProcessor, IBlockProcessing
     private DateTime _lastProcessedBlock;
 
     private int _currentRecoveryQueueSize;
-    private const int MaxBlocksDuringFastSyncTransition = 8192;
+    private const int MaxBranchSize = 8192;
     private readonly CompositeBlockTracer _compositeBlockTracer = new();
     private readonly Stopwatch _stopwatch = new();
 
@@ -579,6 +579,11 @@ public sealed class BlockchainProcessor : IBlockchainProcessor, IBlockProcessing
         do
         {
             iterations++;
+            if (iterations > MaxBranchSize)
+            {
+                throw new InvalidOperationException($"Maximum size of branch reached ({MaxBranchSize}). This is unexpected.");
+            }
+
             if (!options.ContainsFlag(ProcessingOptions.Trace))
             {
                 blocksToBeAddedToMain.Add(toBeProcessed);
