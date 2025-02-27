01b983286869f09253bf7659fbd3d7ded748401a
ak88 <anders.holmbjerg@hotmail.com>
2025-02-27 14:01:21 +0100

    add default options for taiko db

--- src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
+++ src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
@@ -234,19 +234,8 @@ public class DbConfig : IDbConfig
 
         "";
     public string? StateDbAdditionalRocksDbOptions { get; set; }
-    
-    public string L1OriginDbRocksDbOptions { get; set; } =
-        "write_buffer_size=4000000;" +
-        "block_based_table_factory.block_cache=16000000;" +
-        "optimize_filters_for_hits=false;" +
-        "prefix_extractor=capped:8;" +
-        "block_based_table_factory.index_type=kHashSearch;" +
-        "block_based_table_factory.block_size=4096;" +
-        "memtable=prefix_hash:1000000;" +
-        // Bloom crash with kHashSearch index
-        "block_based_table_factory.filter_policy=null;" +
-        "allow_concurrent_memtable_write=false;";
+
+    public string L1OriginDbRocksDbOptions { get; set; } = "";
 
     public string? L1OriginDbAdditionalRocksDbOptions { get; set; }
-
 }
