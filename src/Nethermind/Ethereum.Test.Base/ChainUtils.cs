6aa28dc2fddc0987461317df8f15db49a824527f
lukasz.rozmej <lukasz.rozmej@gmail.com>
2025-03-04 12:41:54 +0100

    simplify ChainUtils

--- src/Nethermind/Ethereum.Test.Base/ChainUtils.cs
+++ src/Nethermind/Ethereum.Test.Base/ChainUtils.cs
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using Nethermind.Consensus.Validators;
-using Nethermind.Core;
 using Nethermind.Core.Specs;
 using Nethermind.Specs;
 using Nethermind.Specs.Forks;
@@ -12,31 +10,12 @@ namespace Ethereum.Test.Base;
 
 public static class ChainUtils
 {
-    private static readonly TxValidator MainnetTxValidator = new(MainnetSpecProvider.Instance.ChainId);
-    private static readonly TxValidator GnosisTxValidator = new(GnosisSpecProvider.Instance.ChainId);
-
     public static IReleaseSpec? ResolveSpec(IReleaseSpec? spec, ulong chainId)
     {
-        if (chainId != GnosisSpecProvider.Instance.ChainId)
-        {
-            return spec;
-        }
-
-        if (spec == Cancun.Instance)
-        {
-            return CancunGnosis.Instance;
-        }
-        if (spec == Prague.Instance)
-        {
-            return PragueGnosis.Instance;
-        }
-
-        return spec;
-    }
-
-    public static ValidationResult ValidateTransaction(Transaction transaction, IReleaseSpec spec)
-    {
-        return transaction.ChainId == GnosisSpecProvider.Instance.ChainId ?
-            GnosisTxValidator.IsWellFormed(transaction, spec) : MainnetTxValidator.IsWellFormed(transaction, spec);
+        return chainId == GnosisSpecProvider.Instance.ChainId
+            ? spec == Cancun.Instance ? CancunGnosis.Instance
+            : spec == Prague.Instance ? PragueGnosis.Instance
+            : spec
+            : spec;
     }
 }
