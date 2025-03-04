d31c7e0c3ab24b41273f4ac31820a4d442c3bebf
Amirul Ashraf <asdacap@gmail.com>
2025-03-05 06:55:42 +0800

    Fix small comment

--- src/Nethermind/Nethermind.Init/Steps/StartMonitoring.cs
+++ src/Nethermind/Nethermind.Init/Steps/StartMonitoring.cs
@@ -16,7 +16,8 @@ using Type = System.Type;
 
 namespace Nethermind.Init.Steps;
 
-[RunnerStepDependencies(typeof(InitializeNetwork))]
+// [RunnerStepDependencies(typeof(InitializeNetwork))]
+[RunnerStepDependencies()]
 public class StartMonitoring : IStep
 {
     private readonly IApiWithNetwork _api;
