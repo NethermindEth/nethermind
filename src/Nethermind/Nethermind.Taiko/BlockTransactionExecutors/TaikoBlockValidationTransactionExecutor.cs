13f59813fc07b59440087a5fa6c8767bb5493b49
Ahmad Bitar <smartprogrammer@windowslive.com>
2025-03-07 05:32:16 +0300

    format

--- src/Nethermind/Nethermind.Taiko/BlockTransactionExecutors/TaikoBlockValidationTransactionExecutor.cs
+++ src/Nethermind/Nethermind.Taiko/BlockTransactionExecutors/TaikoBlockValidationTransactionExecutor.cs
@@ -25,7 +25,7 @@ public class TaikoBlockValidationTransactionExecutor(
     protected override void ProcessTransaction(in BlockExecutionContext blkCtx, Transaction currentTx, int i, BlockReceiptsTracer receiptsTracer, ProcessingOptions processingOptions)
     {
         if ((currentTx.SenderAddress?.Equals(TaikoBlockValidator.GoldenTouchAccount) ?? false) && i == 0)
-                currentTx.IsAnchorTx = true;
+            currentTx.IsAnchorTx = true;
         base.ProcessTransaction(in blkCtx, currentTx, i, receiptsTracer, processingOptions);
     }
 }
