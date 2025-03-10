83d83602f283079ce4a946e43ea2b0bc795b1c78
Siddharth Vaderaa <svaderaa@gmail.com>
2025-03-10 20:24:52 +0100

    Remove cancellation token from file writing task

--- tools/PatternAnalyzer/Nethermind.PatternAnalyzer.Plugin/Tracer/PatternAnalyzerFileTracer.cs
+++ tools/PatternAnalyzer/Nethermind.PatternAnalyzer.Plugin/Tracer/PatternAnalyzerFileTracer.cs
@@ -101,7 +101,7 @@ public class PatternAnalyzerFileTracer : BlockTracerBase<PatternAnalyzerTxTrace,
                     throw;
                 }
             }
-        },_ct);
+        });
 
         _fileTracingQueue.Add(task);
 
