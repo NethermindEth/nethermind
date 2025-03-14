ff7c6b10353f7cd8ca6aec3afb53395332e71fbe
Lautaro Emanuel <emlautarom1@gmail.com>
2025-03-14 17:27:03 -0300

    Accept single values

--- src/Nethermind/Nethermind.Optimism/CL/Decoding/IChannelStorage.cs
+++ src/Nethermind/Nethermind.Optimism/CL/Decoding/IChannelStorage.cs
@@ -1,13 +1,18 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System;
-using Nethermind.Optimism.CL.Decoding;
-
 namespace Nethermind.Optimism.CL.Decoding;
 
 public interface IChannelStorage
 {
-    void ConsumeFrames(Frame[] frames);
     BatchV1[]? GetReadyBatches();
+    void ConsumeFrame(Frame frames);
+
+    void ConsumeFrames(Frame[] frames)
+    {
+        foreach (var frame in frames)
+        {
+            ConsumeFrame(frame);
+        }
+    }
 }
