473efaade2d53d1e93bfcbe0bef6a3b2399301d1
Alex Bespalov <alexb5dh@gmail.com>
2025-03-22 00:05:18 +0200

    ~ Disable caching for benchmark

--- src/Nethermind/Nethermind.Evm/CodeInfoRepository.cs
+++ src/Nethermind/Nethermind.Evm/CodeInfoRepository.cs
@@ -57,9 +57,7 @@ public class CodeInfoRepository : ICodeInfoRepository
 
     public CodeInfoRepository(ConcurrentDictionary<PreBlockCaches.PrecompileCacheKey, (byte[], bool)>? precompileCache = null)
     {
-        _localPrecompiles = precompileCache is null
-            ? _precompiles
-            : _precompiles.ToFrozenDictionary(kvp => kvp.Key, kvp => CreateCachedPrecompile(kvp, precompileCache));
+        _localPrecompiles = _precompiles;
     }
 
     public CodeInfo GetCachedCodeInfo(IWorldState worldState, Address codeSource, IReleaseSpec vmSpec, out Address? delegationAddress)
