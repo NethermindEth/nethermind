e217e98488b5b7ffd21934a4793007bf1881e649
Nikita Meshcheriakov <root@nikitam.io>
2025-03-24 19:30:58 +0300

    Fix bug

--- src/Nethermind/Nethermind.Optimism/CL/Decoding/DecodingPipeline.cs
+++ src/Nethermind/Nethermind.Optimism/CL/Decoding/DecodingPipeline.cs
@@ -30,21 +30,33 @@ public class DecodingPipeline : IDecodingPipeline
         var buffer = new Memory<byte>(new byte[BlobDecoder.MaxBlobDataSize]);
         while (!token.IsCancellationRequested)
         {
+            buffer.Clear();
             var blob = await _inputChannel.Reader.ReadAsync(token);
 
-            var read = BlobDecoder.DecodeBlob(blob, buffer.Span);
-            foreach (var frame in FrameDecoder.DecodeFrames(buffer[..read]))
+            try
             {
-                _channelStorage.ConsumeFrame(frame);
-            }
-
-            var batches = _channelStorage.GetReadyBatches();
-            if (batches is not null)
-            {
-                foreach (var batch in batches)
+                var read = BlobDecoder.DecodeBlob(blob, buffer.Span);
+                foreach (var frame in FrameDecoder.DecodeFrames(buffer[..read]))
                 {
-                    await _outputChannel.Writer.WriteAsync(batch, token);
+                    _channelStorage.ConsumeFrame(frame);
                 }
+
+                var batches = _channelStorage.GetReadyBatches();
+                if (batches is not null)
+                {
+                    foreach (var batch in batches)
+                    {
+                        await _outputChannel.Writer.WriteAsync(batch, token);
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+
+            }
+            catch (Exception e)
+            {
+                if (_logger.IsWarn) _logger.Warn($"Unhandled exception in decoding pipeline: {e}");
             }
         }
     }
--- src/Nethermind/Nethermind.Optimism/CL/Decoding/IFrameQueue.cs
+++ src/Nethermind/Nethermind.Optimism/CL/Decoding/IFrameQueue.cs
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using Nethermind.Optimism.CL.Decoding;
-
 namespace Nethermind.Optimism.CL.Decoding;
 
 public interface IFrameQueue
