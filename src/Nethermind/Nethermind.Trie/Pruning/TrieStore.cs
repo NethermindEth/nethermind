193b5693825c98beeff818450be1dcf79544afbc
Amirul Ashraf <asdacap@gmail.com>
2025-03-25 18:30:56 +0800

    Update src/Nethermind/Nethermind.Trie/Pruning/TrieStore.cs
    Co-authored-by: Ben {chmark} Adams <thundercat@illyriad.co.uk>

--- src/Nethermind/Nethermind.Trie/Pruning/TrieStore.cs
+++ src/Nethermind/Nethermind.Trie/Pruning/TrieStore.cs
@@ -281,9 +281,9 @@ public class TrieStore : ITrieStore, IPruningTrieStore
     {
         // When enabled, the shard have dictionaries for tracking past path hash also.
         // So the same path need to be in the same shard for the remove logic to work.
-        uint baseSize = _livePruningEnabled
-            ? BinaryPrimitives.ReadUInt32BigEndian(path.Path.Bytes[..4])
-            : BinaryPrimitives.ReadUInt32BigEndian(hash.Bytes[..4]);
+        uint baseSize = (uint)_livePruningEnabled
+            ? path..GetHashCode()
+            : hash..GetHashCode();
 
         // Using some early bits so that nodes of similar prefix would get put in the same shard.
         uint shardIdx = baseSize >> (32 - _shardBit);
