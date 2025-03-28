701978db3481424d2090c135196ba467b657ea64
Marcin Sobczak <marcindsobczak@gmail.com>
2025-03-28 13:23:29 +0100

    fix cancun caps

--- src/Nethermind/Nethermind.Merge.Plugin/Handlers/EngineRpcCapabilitiesProvider.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Handlers/EngineRpcCapabilitiesProvider.cs
@@ -46,7 +46,6 @@ public class EngineRpcCapabilitiesProvider : IRpcCapabilitiesProvider
             _capabilities[nameof(IEngineRpcModule.engine_forkchoiceUpdatedV3)] = (spec.IsEip4844Enabled, spec.IsEip4844Enabled);
             _capabilities[nameof(IEngineRpcModule.engine_newPayloadV3)] = (spec.IsEip4844Enabled, spec.IsEip4844Enabled);
             _capabilities[nameof(IEngineRpcModule.engine_getBlobsV1)] = (spec.IsEip4844Enabled, false);
-            _capabilities[nameof(IEngineRpcModule.engine_getBlobsV2)] = (spec.IsEip4844Enabled, false);
             #endregion
 
             #region Prague
