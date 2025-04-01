cbd1102dcb6bdc6ad6fd2d2c96ba8afd52610290
Siddharth Vaderaa <svaderaa@gmail.com>
2025-04-01 13:22:00 +0200

    ..

--- tools/PatternAnalyzer/Nethermind.PatternAnalyzer.Plugin/Analyzer/CMSketchConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace Nethermind.PatternAnalyzer.Plugin.Analyzer;
-
-public class CmSketchConfig
-{
-    public int? Buckets { get; set; }
-    public int? HashFunctions { get; set; }
-    public double? MinConfidence { get; set; }
-    public double? MaxError { get; set; }
-}
