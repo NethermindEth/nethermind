8a5605fc5b0b21024ecbd8c504cb18c10013ccc1
Marcin Sobczak <marcindsobczak@gmail.com>
2025-04-07 13:25:02 +0200

    add regression test for blobs bundle v2

---
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/EngineModuleTests.V5.cs
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Threading.Tasks;
+using Nethermind.Core;
+using Nethermind.Core.Extensions;
+using Nethermind.Crypto;
+using Nethermind.Evm;
+using Nethermind.JsonRpc;
+using Nethermind.Merge.Plugin.Data;
+using Nethermind.Specs.Forks;
+using NUnit.Framework;
+
+namespace Nethermind.Merge.Plugin.Test;
+
+public partial class EngineModuleTests
+{
+    [Test]
+    public async Task GetPayloadV5_should_return_all_the_blobs([Values (0, 1, 2, 3, 4)] int blobTxCount, [Values (true, false)] bool oneBlobPerTx)
+    {
+        (IEngineRpcModule rpcModule, string? payloadId, _, _) = await BuildAndGetPayloadV3Result(Osaka.Instance, blobTxCount, oneBlobPerTx: oneBlobPerTx);
+        ResultWrapper<GetPayloadV5Result?> result = await rpcModule.engine_getPayloadV5(Bytes.FromHexString(payloadId!));
+        BlobsBundleV2 getPayloadResultBlobsBundle = result.Data!.BlobsBundle!;
+        Assert.That(result.Data.ExecutionPayload.BlobGasUsed, Is.EqualTo(BlobGasCalculator.CalculateBlobGas(blobTxCount)));
+        Assert.That(getPayloadResultBlobsBundle.Blobs!.Length, Is.EqualTo(blobTxCount));
+        Assert.That(getPayloadResultBlobsBundle.Commitments!.Length, Is.EqualTo(blobTxCount));
+        Assert.That(getPayloadResultBlobsBundle.Proofs!.Length, Is.EqualTo(blobTxCount * Ckzg.Ckzg.CellsPerExtBlob));
+        ShardBlobNetworkWrapper wrapper = new ShardBlobNetworkWrapper(getPayloadResultBlobsBundle.Blobs,
+            getPayloadResultBlobsBundle.Commitments, getPayloadResultBlobsBundle.Proofs, ProofVersion.V2);
+        Assert.That(IBlobProofsManager.For(ProofVersion.V2).ValidateProofs(wrapper), Is.True);
+    }
+}
