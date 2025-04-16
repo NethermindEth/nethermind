f40817c51e3879db97d49238c83216f03a8f8f34
Alex Bespalov <alexb5dh@gmail.com>
2025-04-16 04:51:01 +0300

    Potential fix for failing tests

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V69/Messages/ReceiptMessageDecoder69.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V69/Messages/ReceiptMessageDecoder69.cs
@@ -5,6 +5,7 @@ using Nethermind.Serialization.Rlp;
 
 namespace Nethermind.Network.P2P.Subprotocols.Eth.V69.Messages;
 
+[Rlp.SkipGlobalRegistration] // Created explicitly
 public class ReceiptMessageDecoder69 : ReceiptMessageDecoder
 {
     public ReceiptMessageDecoder69() : base(includeBloom: false) { }
