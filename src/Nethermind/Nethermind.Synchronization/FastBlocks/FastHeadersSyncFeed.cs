6ea37b9430d54462b51e84ecfbbcf8647faf5c15
Cardo <richardgrecoson@gmail.com>
2025-05-06 07:38:18 +0100

    Double check other client limits (#8530)
    * Double check other client limits
    
    - Moved the SyncLimits classes from Nethermind.Synchronization.SyncLimits to Nethermind.Stats.SyncLimits.
    - Updated all import statements in files that referenced the old namespace.
    - Updated NodeStatsLight.GetCurrentRequestLimit to use the client-specific limits from SyncLimits.
    - Added TODOs for future enhancements to TreeSync.
    - Fixed the test that used the SyncLimits classes.
    
    Fixes #8475
    
    * Fix failing CI test related sync limit update
    
    * Update ParitySyncLimits.cs
    
    ---------
    
    Co-authored-by: Amirul Ashraf <asdacap@gmail.com>
    Co-authored-by: Ben {chmark} Adams <thundercat@illyriad.co.uk>

--- src/Nethermind/Nethermind.Synchronization/FastBlocks/FastHeadersSyncFeed.cs
+++ src/Nethermind/Nethermind.Synchronization/FastBlocks/FastHeadersSyncFeed.cs
@@ -24,7 +24,7 @@ using Nethermind.Stats.Model;
 using Nethermind.Synchronization.ParallelSync;
 using Nethermind.Synchronization.Peers;
 using Nethermind.Synchronization.Reporting;
-using Nethermind.Synchronization.SyncLimits;
+using Nethermind.Stats.SyncLimits;
 
 namespace Nethermind.Synchronization.FastBlocks
 {
