8b64ad4030d078490603aeb98a24380228a54f45
Lautaro Emanuel <emlautarom1@gmail.com>
2025-05-14 16:43:38 -0300

    Document `SyncStatus`

--- src/Nethermind/Nethermind.Optimism/CL/Rpc/OptimismSyncStatus.cs
+++ src/Nethermind/Nethermind.Optimism/CL/Rpc/OptimismSyncStatus.cs
@@ -1,15 +1,9 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Threading.Tasks;
-using Nethermind.Core.Crypto;
-using Nethermind.Facade.Eth;
-using Nethermind.JsonRpc;
-using Nethermind.JsonRpc.Modules;
 using Nethermind.Optimism.CL;
 
 namespace Nethermind.Optimism.Cl.Rpc;
-
 
 /// <summary>
 /// Represents a snapshot of the rollup driver.
@@ -19,16 +13,58 @@ namespace Nethermind.Optimism.Cl.Rpc;
 /// </remarks>
 public sealed record OptimismSyncStatus
 {
-    // L1
+    #region L1
+    /// <summary>
+    /// Is the L1 block that the derivation process is last idled at.
+	/// This may not be fully derived into L2 data yet.
+	/// The safe L2 blocks were produced/included fully from the L1 chain up to _but excluding_ this L1 block.
+	/// If the node is synced, this matches the HeadL1, minus the verifier confirmation distance.
+    /// </summary>
     public required L1BlockRef CurrentL1 { get; init; }
-    public required L1BlockRef CurrentL1Finalized { get; init; }
+
+    /// <summary>
+    /// CurrentL1Finalized is a legacy sync-status attribute. This is deprecated.
+    /// A previous version of the L1 finalization-signal was updated only after the block was retrieved by number.
+    /// This attribute just matches FinalizedL1 now.
+    /// </summary>
+    public L1BlockRef CurrentL1Finalized => FinalizedL1;
+
+    /// <summary>
+    /// HeadL1 is the perceived head of the L1 chain, no confirmation distance.
+	/// The head is not guaranteed to build on the other L1 sync status fields,
+	/// as the node may be in progress of resetting to adapt to a L1 reorg.
+    /// </summary>
     public required L1BlockRef HeadL1 { get; init; }
     public required L1BlockRef SafeL1 { get; init; }
     public required L1BlockRef FinalizedL1 { get; init; }
-    // L2
+    #endregion
+
+    #region L2
+    /// <summary>
+    /// The absolute tip of the L2 chain,
+	/// pointing to block data that has not been submitted to L1 yet.
+	/// The sequencer is building this, and verifiers may also be ahead of the
+	/// SafeL2 block if they sync blocks via p2p or other offchain sources.
+	/// This is considered to only be local-unsafe post-interop, see CrossUnsafe for cross-L2 guarantees.
+    /// </summary>
     public required L2BlockRef UnsafeL2 { get; init; }
+
+    /// <summary>
+    /// Points to the L2 block that was derived from the L1 chain.
+	/// This point may still reorg if the L1 chain reorgs.
+	/// This is considered to be cross-safe post-interop, see LocalSafe to ignore cross-L2 guarantees.
+    /// </summary>
     public required L2BlockRef SafeL2 { get; init; }
+
+    /// <summary>
+    /// Points to the L2 block that was derived fully from finalized L1 information, thus irreversible.
+    /// </summary>
     public required L2BlockRef FinalizedL2 { get; init; }
+
+    /// <summary>
+    /// Points to the L2 block processed from the batch, but not consolidated to the safe block yet.
+    /// </summary>
     public required L2BlockRef PendingSafeL2 { get; init; }
     public required L2BlockRef QueuedUnsafeL2 { get; init; }
+    #endregion
 }
