b92b472fad5eac4b102c685eb1d6a9418d5873f3
Amirul Ashraf <asdacap@gmail.com>
2025-05-19 12:16:01 +0800

    More attempt to fix test

--- src/Nethermind/Nethermind.Trie/Pruning/RawScopedTrieStore.cs
+++ src/Nethermind/Nethermind.Trie/Pruning/RawScopedTrieStore.cs
@@ -1,8 +1,10 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
 
@@ -65,60 +67,73 @@ public class RawScopedTrieStore(INodeStorage nodeStorage, Hash256? address = nul
     }
 }
 
-public class RawTrieStore(INodeStorage nodeStorage, bool isReadOnly = false) : ITrieStore, IReadOnlyTrieStore
+public class RawTrieStore(INodeStorage nodeStorage, bool isReadOnly = false) : IPruningTrieStore, IReadOnlyTrieStore
 {
     public RawTrieStore(IKeyValueStoreWithBatching kv) : this(new NodeStorage(kv))
     {
     }
 
-    public void Dispose()
+    void IDisposable.Dispose()
     {
     }
 
-    public ICommitter BeginCommit(Hash256? address, TrieNode? root, WriteFlags writeFlags)
+    ICommitter IScopableTrieStore.BeginCommit(Hash256? address, TrieNode? root, WriteFlags writeFlags)
     {
         if (isReadOnly) return NullCommitter.Instance;
         return new RawScopedTrieStore.Committer(nodeStorage, address, writeFlags);
     }
 
-    public TrieNode FindCachedOrUnknown(Hash256? address, in TreePath path, Hash256 hash)
+    TrieNode IScopableTrieStore.FindCachedOrUnknown(Hash256? address, in TreePath path, Hash256 hash)
     {
         return new TrieNode(NodeType.Unknown, hash);
     }
 
-    public byte[]? LoadRlp(Hash256? address, in TreePath path, Hash256 hash, ReadFlags flags = ReadFlags.None)
+    byte[]? IScopableTrieStore.LoadRlp(Hash256? address, in TreePath path, Hash256 hash, ReadFlags flags)
     {
         byte[]? ret = nodeStorage.Get(address, path, hash, flags);
         if (ret is null) throw new MissingTrieNodeException("Node missing", address, path, hash);
         return ret;
     }
 
-    public byte[]? TryLoadRlp(Hash256? address, in TreePath path, Hash256 hash, ReadFlags flags = ReadFlags.None)
+    byte[]? IScopableTrieStore.TryLoadRlp(Hash256? address, in TreePath path, Hash256 hash, ReadFlags flags)
     {
         return nodeStorage.Get(address, path, hash, flags);
     }
 
-    public bool IsPersisted(Hash256? address, in TreePath path, in ValueHash256 keccak)
+    bool IScopableTrieStore.IsPersisted(Hash256? address, in TreePath path, in ValueHash256 keccak)
     {
         return nodeStorage.KeyExists(address, path, keccak);
     }
 
-    public INodeStorage.KeyScheme Scheme { get; } = nodeStorage.Scheme;
-    public bool HasRoot(Hash256 stateRoot)
+    INodeStorage.KeyScheme IScopableTrieStore.Scheme { get; } = nodeStorage.Scheme;
+
+    bool ITrieStore.HasRoot(Hash256 stateRoot)
     {
         return nodeStorage.KeyExists(null, TreePath.Empty, stateRoot);
     }
 
-    public IScopedTrieStore GetTrieStore(Hash256? address)
+    IScopedTrieStore ITrieStore.GetTrieStore(Hash256? address)
     {
         return new RawScopedTrieStore(nodeStorage, address);
     }
 
-    public IBlockCommitter BeginBlockCommit(long blockNumber)
+    IBlockCommitter ITrieStore.BeginBlockCommit(long blockNumber)
     {
         return NullCommitter.Instance;
     }
 
-    public IReadOnlyTrieStore AsReadOnly(INodeStorage? store = null) =>
+    void IPruningTrieStore.PersistCache(CancellationToken cancellationToken)
+    {
+    }
+
+    IReadOnlyTrieStore IPruningTrieStore.AsReadOnly(INodeStorage? store) =>
         new RawTrieStore(nodeStorage, true);
+
+    event EventHandler<ReorgBoundaryReached>? IPruningTrieStore.ReorgBoundaryReached
+    {
+        add => throw new Exception("Unsupported operation");
+        remove => throw new Exception("Unsupported operation");
+    }
+
+    IReadOnlyKeyValueStore IPruningTrieStore.TrieNodeRlpStore { get; }
 }
