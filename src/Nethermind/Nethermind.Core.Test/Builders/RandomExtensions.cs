c8adf12e0b00509b408f2909219a8b9357051af3
Alex Bespalov <alexb5dh@gmail.com>
2025-05-20 19:48:06 +0300

    Build fix

--- src/Nethermind/Nethermind.Core.Test/Builders/RandomExtensions.cs
+++ src/Nethermind/Nethermind.Core.Test/Builders/RandomExtensions.cs
@@ -12,11 +12,4 @@ public static class RandomExtensions
     public static T NextValue<T>(this Random random, IReadOnlyList<T> values) => values[random.Next(values.Count)];
 
     public static T NextValue<T>(this ICryptoRandom random, IReadOnlyList<T> values) => values[random.NextInt(values.Count)];
-
-    public static byte[] NextBytes(this Random random, int length)
-    {
-        var result = new byte[length];
-        random.NextBytes(result);
-        return result;
-    }
 }
--- src/Nethermind/Nethermind.Db.Test/LogIndexStorageTests.cs
+++ src/Nethermind/Nethermind.Db.Test/LogIndexStorageTests.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MathNet.Numerics.Random;
 using Nethermind.Core;
 using Nethermind.Core.Collections;
 using Nethermind.Core.Crypto;
