76ae3fc6a9326efcf316708983915e28095a4b2d
Amirul Ashraf <asdacap@gmail.com>
2025-05-22 15:22:54 +0800

    Consistency

--- src/Nethermind/Nethermind.Synchronization/ParallelSync/NoopSyncFeed.cs
+++ src/Nethermind/Nethermind.Synchronization/ParallelSync/NoopSyncFeed.cs
@@ -42,5 +42,5 @@ public class NoopSyncFeed<T> : ISyncFeed<T>
     }
 
     public bool IsFinished { get; } = true;
-    public string FeedName => "noop";
+    public string FeedName => nameof(NoopSyncFeed<T>);
 }
