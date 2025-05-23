016a6f82d79057474a905cc5a93881976d72770f
Marc <Marchhill@users.noreply.github.com>
2025-05-23 12:47:58 +0100

    Implement EIP-7825: Transaction Gas Limit Cap (#8663)
    * initial implementation, txpool limit
    
    * fix formatting
    
    * block validity
    
    * enforce in TxValidator
    
    * restore tx processing
    
    * add txpool test
    
    * add to osaka fork and add txvalidator test
    
    * fix whitespace
    
    * revert accidental changes
    
    * remove from osaka
    
    * fix whitespace
    
    * remove redundant txpool check
    
    * construct ExpectedChainIdTxValidator only once
    
    * move spec check inside GetTxGasLimitCap
    
    ---------
    
    Co-authored-by: lukasz.rozmej <lukasz.rozmej@gmail.com>

--- src/Nethermind/Nethermind.Core/Specs/ReleaseSpecDecorator.cs
+++ src/Nethermind/Nethermind.Core/Specs/ReleaseSpecDecorator.cs
@@ -89,6 +89,7 @@ public class ReleaseSpecDecorator(IReleaseSpec spec) : IReleaseSpec
     public virtual bool IsOpHoloceneEnabled => spec.IsOpHoloceneEnabled;
     public virtual bool IsOpIsthmusEnabled => spec.IsOpIsthmusEnabled;
     public virtual bool IsEip7623Enabled => spec.IsEip7623Enabled;
+    public virtual bool IsEip7825Enabled => spec.IsEip7825Enabled;
     public virtual ulong WithdrawalTimestamp => spec.WithdrawalTimestamp;
     public virtual ulong Eip4844TransitionTimestamp => spec.Eip4844TransitionTimestamp;
 
