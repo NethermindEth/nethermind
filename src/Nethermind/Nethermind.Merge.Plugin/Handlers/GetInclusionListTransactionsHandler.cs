552dd317e82854a2322179d64579f345aa1abceb
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2025-05-30 16:21:37 +0100

    separate IL builder class

--- src/Nethermind/Nethermind.Merge.Plugin/Handlers/GetInclusionListTransactionsHandler.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Handlers/GetInclusionListTransactionsHandler.cs
@@ -1,12 +1,8 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System;
 using Nethermind.Core;
 using Nethermind.JsonRpc;
-using System.Collections.Generic;
-using Nethermind.Consensus.Decoders;
-using Nethermind.Core.Extensions;
 using Nethermind.Core.Collections;
 using Nethermind.TxPool;
 
@@ -15,7 +11,7 @@ namespace Nethermind.Merge.Plugin.Handlers;
 public class GetInclusionListTransactionsHandler(ITxPool? txPool)
     : IHandler<ArrayPoolList<byte[]>>
 {
-    private readonly Random _rnd = new();
+    private readonly InclusionListBuilder? inclusionListBuilder = txPool is null ? null : new(txPool);
 
     public ResultWrapper<ArrayPoolList<byte[]>> Handle()
     {
@@ -24,37 +20,8 @@ public class GetInclusionListTransactionsHandler(ITxPool? txPool)
             return ResultWrapper<ArrayPoolList<byte[]>>.Success(ArrayPoolList<byte[]>.Empty());
         }
 
-        // get highest priority fee transactions from txpool up to limit
-        Transaction[] txs = txPool.GetPendingTransactions();
-        var orderedTxs = OrderTransactions(txs);
-        ArrayPoolList<byte[]> txBytes = new(Eip7805Constants.MaxTransactionsPerInclusionList, DecodeTransactionsUpToLimit(orderedTxs));
+        ArrayPoolList<byte[]> txBytes = new(Eip7805Constants.MaxTransactionsPerInclusionList, inclusionListBuilder!.GetInclusionList());
         return ResultWrapper<ArrayPoolList<byte[]>>.Success(txBytes);
     }
 
-    private IEnumerable<Transaction> OrderTransactions(IEnumerable<Transaction> txs)
-        => txs.Shuffle(_rnd, Eip7805Constants.MaxTransactionsPerInclusionList);
-
-    private static IEnumerable<byte[]> DecodeTransactionsUpToLimit(IEnumerable<Transaction> txs)
-    {
-        int size = 0;
-        foreach (Transaction tx in txs)
-        {
-            byte[] txBytes = InclusionListDecoder.Encode(tx);
-
-            // skip tx if it's too big to fit in the inclusion list
-            if (size + txBytes.Length > Eip7805Constants.MaxBytesPerInclusionList)
-            {
-                continue;
-            }
-
-            size += txBytes.Length;
-            yield return txBytes;
-
-            // impossible to fit another tx in the inclusion list
-            if (size + Eip7805Constants.MinTransactionSizeBytesUpper > Eip7805Constants.MaxBytesPerInclusionList)
-            {
-                yield break;
-            }
-        }
-    }
 }
