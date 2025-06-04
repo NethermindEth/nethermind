002b6b35213510331ae3792d9a5c774580c2e14f
Lautaro Emanuel <emlautarom1@gmail.com>
2025-06-04 15:49:45 -0300

    Implement in terms of UInt256

--- src/Nethermind/Nethermind.Experimental.Abi/V2/AbiType.cs
+++ src/Nethermind/Nethermind.Experimental.Abi/V2/AbiType.cs
@@ -1,36 +1,48 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Buffers.Binary;
+using Nethermind.Int256;
 
 namespace Nethermind.Experimental.Abi.V2;
 
 // NOTE: Make `partial` to allow for extension
 public static class AbiType
 {
-    public static readonly IAbi<UInt32> UInt32 = new()
+    public static readonly IAbi<UInt256> UInt256 = new()
     {
-        Name = "uint32",
+        Name = "uint256",
         Read = r =>
         {
             var bytes = r.ReadBytes(32);
-            return BinaryPrimitives.ReadUInt32BigEndian(bytes[^sizeof(UInt32)..]);
+            return new UInt256(bytes, isBigEndian: true);
         },
         Write = (w, v) =>
         {
-            Span<byte> padding = stackalloc byte[32 - sizeof(UInt32)];
-            w.Write(padding);
-            Span<byte> bytes = stackalloc byte[sizeof(UInt32)];
-            BinaryPrimitives.WriteUInt32BigEndian(bytes, v);
+            Span<byte> bytes = stackalloc byte[32];
+            v.ToBigEndian(bytes);
             w.Write(bytes);
         }
+    };
+
+    public static readonly IAbi<UInt64> UInt64 = new()
+    {
+        Name = "uint64",
+        Read = r => (UInt64)UInt256.Read(r),
+        Write = (w, v) => UInt256.Write(w, v),
+    };
+
+    public static readonly IAbi<UInt32> UInt32 = new()
+    {
+        Name = "uint32",
+        Read = r => (UInt32)UInt256.Read(r),
+        Write = (w, v) => UInt256.Write(w, v),
     };
 
     public static readonly IAbi<Boolean> Bool = new()
     {
         Name = "bool",
-        Read = r => UInt32.Read(r) != 0,
-        Write = (w, v) => UInt32.Write(w, v ? 1u : 0u)
+        Read = r => UInt256.Read(r) != 0,
+        Write = (w, v) => UInt256.Write(w, v ? 1u : 0u)
     };
 
     public static IAbi<(T1, T2)> Tuple<T1, T2>(IAbi<T1> abi1, IAbi<T2> abi2) => new()
