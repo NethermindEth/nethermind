afadb8c08efc2c4c6acf38289b865b097b287dbb
Lautaro Emanuel <emlautarom1@gmail.com>
2025-06-05 12:00:17 -0300

    Single argument Encode/Decode

--- src/Nethermind/Nethermind.Experimental.Abi/V2/Abi.cs
+++ src/Nethermind/Nethermind.Experimental.Abi/V2/Abi.cs
@@ -9,6 +9,17 @@ public class AbiException : Exception;
 
 public static class Abi
 {
+    public static byte[] Encode<T>(AbiSignature<T> signature, T arg)
+    {
+        using var buffer = new MemoryStream();
+        using var w = new BinaryWriter(buffer);
+
+        w.Write(signature.MethodId());
+        signature.Abi1.Write(w, arg);
+
+        return buffer.ToArray();
+    }
+
     public static byte[] Encode<T1, T2>(AbiSignature<T1, T2> signature, T1 arg1, T2 arg2)
     {
         using var buffer = new MemoryStream();
@@ -19,6 +30,18 @@ public static class Abi
         signature.Abi2.Write(w, arg2);
 
         return buffer.ToArray();
+    }
+
+    public static T Decode<T>(AbiSignature<T> signature, byte[] source)
+    {
+        using var r = new BinaryReader(new MemoryStream(source));
+
+        var id = r.ReadBytes(AbiSignature.IdLength);
+        if (!Bytes.AreEqual(id, signature.MethodId())) throw new AbiException();
+
+        T arg = signature.Abi1.Read(r);
+
+        return arg;
     }
 
     public static (T1, T2) Decode<T1, T2>(AbiSignature<T1, T2> signature, byte[] source)
