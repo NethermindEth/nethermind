4822583f079df7b73021e09015e4cfac91adddd7
Lautaro Emanuel <emlautarom1@gmail.com>
2025-06-05 12:00:54 -0300

    Add `Array` of fixed length and `BytesM`

--- src/Nethermind/Nethermind.Experimental.Abi/V2/AbiType.cs
+++ src/Nethermind/Nethermind.Experimental.Abi/V2/AbiType.cs
@@ -8,6 +8,12 @@ namespace Nethermind.Experimental.Abi.V2;
 // NOTE: Make `partial` to allow for extension
 public static class AbiType
 {
+    private static int PadTo32(int length)
+    {
+        int rem = length % 32;
+        return rem == 0 ? length : length + (32 - rem);
+    }
+
     public static readonly IAbi<UInt256> UInt256 = new()
     {
         Name = "uint256",
@@ -57,6 +63,56 @@ public static class AbiType
         Name = "bool",
         Read = r => UInt256.Read(r) != 0,
         Write = (w, v) => UInt256.Write(w, v ? 1u : 0u)
+    };
+
+    public static IAbi<T[]> Array<T>(IAbi<T> elements, int length) => new()
+    {
+        Name = $"{elements.Name}[{length}]",
+        Read = r =>
+        {
+            var array = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = elements.Read(r);
+            }
+            return array;
+        },
+        Write = (w, array) =>
+        {
+            if (array.Length != length) throw new AbiException();
+
+            foreach (var item in array)
+            {
+                elements.Write(w, item);
+            }
+        }
+    };
+
+    public static IAbi<byte[]> BytesM(int length) => new()
+    {
+        Name = $"bytes{length}",
+        Read = r =>
+        {
+            var bytes = r.ReadBytes(length);
+
+            var padding = PadTo32(length) - length;
+            if (padding > 0)
+            {
+                r.ReadBytes(padding); // Skip padding bytes
+            }
+
+            return bytes;
+        },
+        Write = (w, bytes) =>
+        {
+            w.Write(bytes);
+
+            var padding = PadTo32(length) - bytes.Length;
+            if (padding > 0)
+            {
+                w.Write(stackalloc byte[padding]);
+            }
+        }
     };
 
     public static IAbi<(T1, T2)> Tuple<T1, T2>(IAbi<T1> abi1, IAbi<T2> abi2) => new()
