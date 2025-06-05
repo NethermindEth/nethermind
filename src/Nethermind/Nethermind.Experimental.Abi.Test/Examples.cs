1e8d3cc62f66e456469bac480ade00be540dc22f
Lautaro Emanuel <emlautarom1@gmail.com>
2025-06-05 12:01:03 -0300

    Add `Bar` example

--- src/Nethermind/Nethermind.Experimental.Abi.Test/Examples.cs
+++ src/Nethermind/Nethermind.Experimental.Abi.Test/Examples.cs
@@ -9,6 +9,26 @@ namespace Nethermind.Experimental.Abi.Test;
 public class Examples
 {
     [Test]
+    public void Bar()
+    {
+        var signature = new AbiSignature("bar")
+            .Arg(AbiType.Array(AbiType.BytesM(3), length: 2));
+
+        var expectedMethodId = Bytes.FromHexString("0xfce353f6");
+        signature.MethodId().Should().BeEquivalentTo(expectedMethodId);
+
+        byte[] encoded = V2.Abi.Encode(signature, [[(byte)'a', (byte)'b', (byte)'c'], [(byte)'d', (byte)'e', (byte)'f']]);
+
+        var expected = Bytes.FromHexString("0xfce353f6" + // `MethodId`
+                                           "6162630000000000000000000000000000000000000000000000000000000000" + // "abc"
+                                           "6465660000000000000000000000000000000000000000000000000000000000"); // "def"
+
+        encoded.Should().BeEquivalentTo(expected);
+        var args = V2.Abi.Decode(signature, encoded);
+        args.Should().BeEquivalentTo(new byte[][] { [(byte)'a', (byte)'b', (byte)'c'], [(byte)'d', (byte)'e', (byte)'f'] });
+    }
+
+    [Test]
     public void Baz()
     {
         var signature = new AbiSignature("baz")
