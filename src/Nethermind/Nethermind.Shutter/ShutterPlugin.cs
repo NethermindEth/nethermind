85ca38dd67b55b4f2ff70850af59d0805bc75664
Amirul Ashraf <asdacap@gmail.com>
2025-06-05 13:34:57 +0800

    Fix shutter test

--- src/Nethermind/Nethermind.Shutter/ShutterPlugin.cs
+++ src/Nethermind/Nethermind.Shutter/ShutterPlugin.cs
@@ -84,6 +84,7 @@ public class ShutterPluginModule : Module
     {
         builder
             .AddSingleton(CreateShutterApi)
+            .Bind<IShutterApi, ShutterApi>()
             .AddDecorator<IBlockProducerTxSourceFactory, ShutterAdditionalBlockProductionTxSource>()
             ;
     }
