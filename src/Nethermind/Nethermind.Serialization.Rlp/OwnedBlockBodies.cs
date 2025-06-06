749f125c9b49867a7b7cfb930d4b61fcea8ff1bc
Ben Adams <thundercat@illyriad.co.uk>
2025-06-06 05:32:31 +0100

    Revert "Disconnect when connected"
    This reverts commit bb527029026bb1e69290aff8c7a6cfebccf71276.

--- src/Nethermind/Nethermind.Serialization.Rlp/OwnedBlockBodies.cs
+++ src/Nethermind/Nethermind.Serialization.Rlp/OwnedBlockBodies.cs
@@ -45,11 +45,7 @@ public class OwnedBlockBodies : IDisposable, IReadOnlyList<BlockBody?>
             {
                 Hash256? _ = tx.Hash; // Just need to trigger hash calculation
                 // Disconnect from any backing
-                if (!MemoryMarshal.TryGetArray(tx.Data, out ArraySegment<byte> segment) ||
-                    segment.Offset != 0 || segment.Count != tx.Data.Length)
-                {
-                    tx.Data = tx.Data.Span.ToArray();
-                }
+                tx.Data = tx.Data.ToArray();
             }
         }
 
