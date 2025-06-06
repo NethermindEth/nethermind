f4d031e8972681f37270565531b8ba7790d00190
Ruben Buniatyan <rubo@users.noreply.github.com>
2025-06-07 00:10:43 +0200

    Fix calls/traces serialization and tests

--- src/Nethermind/Nethermind.Facade/Proxy/Models/Simulate/SimulateBlockResult.cs
+++ src/Nethermind/Nethermind.Facade/Proxy/Models/Simulate/SimulateBlockResult.cs
@@ -2,17 +2,33 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Nethermind.Core;
 using Nethermind.Core.Specs;
 using Nethermind.Facade.Eth;
 
 namespace Nethermind.Facade.Proxy.Models.Simulate;
 
+/// <summary>
+/// Represents the successful result of a simulation for the JSON-RPC serialization.
+/// </summary>
+/// <remarks>
+/// Serializes <see cref="Calls"/> when used in <c>eth_simulateV1</c>
+/// or <see cref="Traces"/> when used in <c>debug_simulateV1</c>.
+/// </remarks>
 public class SimulateBlockResult<TTrace>(Block source, bool includeFullTransactionData, ISpecProvider specProvider)
     : BlockForRpc(source, includeFullTransactionData, specProvider)
 {
-    public IReadOnlyCollection<TTrace> Calls { get; set; } = [];
-    public bool ShouldSerializeCalls() => typeof(TTrace) == typeof(SimulateCallResult);
-    public IReadOnlyCollection<TTrace> Traces => Calls;
-    public bool ShouldSerializeTraces() => !ShouldSerializeCalls();
+    private static bool ShouldSerializeCalls => typeof(TTrace) == typeof(SimulateCallResult);
+    private IReadOnlyCollection<TTrace> _calls = [];
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public IReadOnlyCollection<TTrace> Calls
+    {
+        get { return ShouldSerializeCalls ? _calls : null; }
+        set { _calls = value; }
+    }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public IReadOnlyCollection<TTrace> Traces => ShouldSerializeCalls ? null : _calls;
 }
