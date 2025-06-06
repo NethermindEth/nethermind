e5ebd42878093909c569d4d728430da48ebbd163
Marc <Marchhill@users.noreply.github.com>
2025-06-06 10:44:56 +0100

    Update src/Nethermind/Nethermind.Config/IHistoryConfig.cs
    Co-authored-by: Ahmad Bitar <33181301+smartprogrammer93@users.noreply.github.com>

--- src/Nethermind/Nethermind.Config/IHistoryConfig.cs
+++ src/Nethermind/Nethermind.Config/IHistoryConfig.cs
@@ -9,7 +9,7 @@ public interface IHistoryConfig : IConfig
 
     // For EIP-4444 set default to 82125
     [ConfigItem(
-        Description = "The number of epochs to retain historical blocks and receipts. Set to null for unlimited retention. Must be at least 82125.",
+        Description = "The number of epochs to retain historical blocks and receipts. Set to null for unlimited retention. For mainnet this must be at least 82125.",
         DefaultValue = "null")]
     ulong? HistoryRetentionEpochs { get; set; }
 
