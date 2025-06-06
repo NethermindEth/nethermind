d448819f9be396041cf4e6f962e01a25f6a38695
Lautaro Emanuel <emlautarom1@gmail.com>
2025-06-06 13:40:20 -0300

    Formatting

--- src/Nethermind/Nethermind.Experimental.Abi.Test/Examples.cs
+++ src/Nethermind/Nethermind.Experimental.Abi.Test/Examples.cs
@@ -20,9 +20,10 @@ public class Examples
 
         byte[] encoded = V2.Abi.Encode(signature, [[(byte)'a', (byte)'b', (byte)'c'], [(byte)'d', (byte)'e', (byte)'f']]);
 
-        var expected = Bytes.FromHexString("0xfce353f6" + // `MethodId`
-                                           "6162630000000000000000000000000000000000000000000000000000000000" + // "abc"
-                                           "6465660000000000000000000000000000000000000000000000000000000000"); // "def"
+        var expected = Bytes.FromHexString(
+            "0xfce353f6" + // `MethodId`
+            "6162630000000000000000000000000000000000000000000000000000000000" + // "abc"
+            "6465660000000000000000000000000000000000000000000000000000000000"); // "def"
 
         encoded.Should().BeEquivalentTo(expected);
         var args = V2.Abi.Decode(signature, encoded);
@@ -41,9 +42,10 @@ public class Examples
 
         byte[] encoded = V2.Abi.Encode(signature, 69u, true);
 
-        var expected = Bytes.FromHexString("0xcdcd77c0" + // `MethodId`
-                                           "0000000000000000000000000000000000000000000000000000000000000045" + // `69`
-                                           "0000000000000000000000000000000000000000000000000000000000000001"); // `true`
+        var expected = Bytes.FromHexString(
+            "0xcdcd77c0" + // `MethodId`
+            "0000000000000000000000000000000000000000000000000000000000000045" + // `69`
+            "0000000000000000000000000000000000000000000000000000000000000001"); // `true`
         encoded.Should().BeEquivalentTo(expected);
 
         (UInt32 a, bool b) = V2.Abi.Decode(signature, encoded);
