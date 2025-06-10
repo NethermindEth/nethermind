30f98bb08d507fc71a1b04fa6df3b0aa95a28ce2
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-06-10 16:43:28 +0200

    copy to releasespec

--- src/Nethermind/Nethermind.Specs/ChainSpecStyle/ChainParameters.cs
+++ src/Nethermind/Nethermind.Specs/ChainSpecStyle/ChainParameters.cs
@@ -142,7 +142,7 @@ public class ChainParameters
     public ulong? Eip7825TransitionTimestamp { get; set; }
     public ulong? Eip7918TransitionTimestamp { get; set; }
     public ulong? Eip7934TransitionTimestamp { get; set; }
-    public int? Eip7934MaxRlpBlockSize { get; set; }
+    public int Eip7934MaxRlpBlockSize { get; set; }
 
     public SortedSet<BlobScheduleSettings>? BlobSchedule { get; set; } = [];
 
