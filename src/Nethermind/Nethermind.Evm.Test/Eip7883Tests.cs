5067f1aa77050b152119b33e8734890c463232d9
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-06-10 09:37:30 +0200

    whitespace

--- src/Nethermind/Nethermind.Evm.Test/Eip7883Tests.cs
+++ src/Nethermind/Nethermind.Evm.Test/Eip7883Tests.cs
@@ -48,39 +48,39 @@ public class Eip7883Tests
     {
         // eip enabled test cases
         yield return new Eip7883TestCase
-        { Lp=1, FusakaEnabled = true, BaseLength = 32, ExpLength = 32, ModulusLength = 32, Result = 500L };
+        { Lp = 1, FusakaEnabled = true, BaseLength = 32, ExpLength = 32, ModulusLength = 32, Result = 500L };
         yield return new Eip7883TestCase
-        { Lp=2, FusakaEnabled = true, BaseLength = 32, ExpLength = 32, ModulusLength = 1024, Result = 10922L };
+        { Lp = 2, FusakaEnabled = true, BaseLength = 32, ExpLength = 32, ModulusLength = 1024, Result = 10922L };
         yield return new Eip7883TestCase
-        { Lp=3, FusakaEnabled = true, BaseLength = 32, ExpLength = 1024, ModulusLength = 32, Result = 84650L };
+        { Lp = 3, FusakaEnabled = true, BaseLength = 32, ExpLength = 1024, ModulusLength = 32, Result = 84650L };
         yield return new Eip7883TestCase
-        { Lp=4, FusakaEnabled = true, BaseLength = 1024, ExpLength = 32, ModulusLength = 32, Result = 10922L };
+        { Lp = 4, FusakaEnabled = true, BaseLength = 1024, ExpLength = 32, ModulusLength = 32, Result = 10922L };
         yield return new Eip7883TestCase
-        { Lp=5, FusakaEnabled = true, BaseLength = 32, ExpLength = 1024, ModulusLength = 1024, Result = 173364565L };
+        { Lp = 5, FusakaEnabled = true, BaseLength = 32, ExpLength = 1024, ModulusLength = 1024, Result = 173364565L };
         yield return new Eip7883TestCase
-        { Lp=6, FusakaEnabled = true, BaseLength = 10000, ExpLength = 1024, ModulusLength = 32, Result = long.MaxValue };
+        { Lp = 6, FusakaEnabled = true, BaseLength = 10000, ExpLength = 1024, ModulusLength = 32, Result = long.MaxValue };
         yield return new Eip7883TestCase
-        { Lp=7, FusakaEnabled = true, BaseLength = 1024, ExpLength = 10000, ModulusLength = 1024, Result = long.MaxValue };
+        { Lp = 7, FusakaEnabled = true, BaseLength = 1024, ExpLength = 10000, ModulusLength = 1024, Result = long.MaxValue };
         yield return new Eip7883TestCase
-        { Lp=8, FusakaEnabled = true, BaseLength = 1024, ExpLength = 1024, ModulusLength = 10000, Result = long.MaxValue };
+        { Lp = 8, FusakaEnabled = true, BaseLength = 1024, ExpLength = 1024, ModulusLength = 10000, Result = long.MaxValue };
 
         // eip disabled test cases
         yield return new Eip7883TestCase
-        { Lp=9, FusakaEnabled = false, BaseLength = 32, ExpLength = 32, ModulusLength = 32, Result = 200L };
+        { Lp = 9, FusakaEnabled = false, BaseLength = 32, ExpLength = 32, ModulusLength = 32, Result = 200L };
         yield return new Eip7883TestCase
-        { Lp=10, FusakaEnabled = false, BaseLength = 32, ExpLength = 32, ModulusLength = 10000, Result = 520833L };
+        { Lp = 10, FusakaEnabled = false, BaseLength = 32, ExpLength = 32, ModulusLength = 10000, Result = 520833L };
         yield return new Eip7883TestCase
-        { Lp=11, FusakaEnabled = false, BaseLength = 32, ExpLength = 10000, ModulusLength = 32, Result = 425301L };
+        { Lp = 11, FusakaEnabled = false, BaseLength = 32, ExpLength = 10000, ModulusLength = 32, Result = 425301L };
         yield return new Eip7883TestCase
-        { Lp=12, FusakaEnabled = false, BaseLength = 10000, ExpLength = 32, ModulusLength = 32, Result = 520833L };
+        { Lp = 12, FusakaEnabled = false, BaseLength = 10000, ExpLength = 32, ModulusLength = 32, Result = 520833L };
         yield return new Eip7883TestCase
-        { Lp=13, FusakaEnabled = false, BaseLength = 32, ExpLength = 10000, ModulusLength = 10000, Result = 41533333333L };
+        { Lp = 13, FusakaEnabled = false, BaseLength = 32, ExpLength = 10000, ModulusLength = 10000, Result = 41533333333L };
         yield return new Eip7883TestCase
-        { Lp=14, FusakaEnabled = false, BaseLength = 10000, ExpLength = 10000, ModulusLength = 32, Result = 41533333333L };
+        { Lp = 14, FusakaEnabled = false, BaseLength = 10000, ExpLength = 10000, ModulusLength = 32, Result = 41533333333L };
         yield return new Eip7883TestCase
-        { Lp=15, FusakaEnabled = false, BaseLength = 10000, ExpLength = 32, ModulusLength = 10000, Result = 520833L };
+        { Lp = 15, FusakaEnabled = false, BaseLength = 10000, ExpLength = 32, ModulusLength = 10000, Result = 520833L };
         yield return new Eip7883TestCase
-        { Lp=16, FusakaEnabled = false, BaseLength = 10000, ExpLength = 10000, ModulusLength = 10000, Result = 41533333333L };
+        { Lp = 16, FusakaEnabled = false, BaseLength = 10000, ExpLength = 10000, ModulusLength = 10000, Result = 41533333333L };
     }
 
     private static ReadOnlyMemory<byte> PrepareInput(UInt256 baseLength, UInt256 expLength, UInt256 modulusLength)
