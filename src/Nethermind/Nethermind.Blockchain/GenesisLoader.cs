27f9308662cba70e8dce7ccf3ad377dfc197f604
Hugo Demeyere <demeyere.hugo@gmail.com>
2025-06-10 14:57:23 +0900

    fix(formatting): from 4 to 2 spaces indent

--- src/Nethermind/Nethermind.Blockchain/GenesisLoader.cs
+++ src/Nethermind/Nethermind.Blockchain/GenesisLoader.cs
@@ -51,44 +51,44 @@ public class GenesisLoader(
     }
 
     private void Preallocate(Block genesis)
+    {
+        _transactionProcessor.SetBlockExecutionContext(new BlockExecutionContext(genesis.Header, specProvider.GetSpec(genesis.Header)));
+        foreach ((Address address, ChainSpecAllocation allocation) in _chainSpec.Allocations.OrderBy(static a => a.Key))
         {
-            _transactionProcessor.SetBlockExecutionContext(new BlockExecutionContext(genesis.Header, specProvider.GetSpec(genesis.Header)));
-            foreach ((Address address, ChainSpecAllocation allocation) in _chainSpec.Allocations.OrderBy(static a => a.Key))
+            _stateProvider.CreateAccount(address, allocation.Balance, allocation.Nonce);
+
+            if (allocation.Code is not null)
             {
-                _stateProvider.CreateAccount(address, allocation.Balance, allocation.Nonce);
+                _stateProvider.InsertCode(address, allocation.Code, _specProvider.GenesisSpec, true);
+            }
 
-                if (allocation.Code is not null)
+            if (allocation.Storage is not null)
+            {
+                foreach (KeyValuePair<UInt256, byte[]> storage in allocation.Storage)
                 {
-                    _stateProvider.InsertCode(address, allocation.Code, _specProvider.GenesisSpec, true);
+                    _stateProvider.Set(new StorageCell(address, storage.Key),
+                        storage.Value.WithoutLeadingZeros().ToArray());
                 }
+            }
 
-                if (allocation.Storage is not null)
+            if (allocation.Constructor is not null)
+            {
+                Transaction constructorTransaction = new SystemTransaction()
                 {
-                    foreach (KeyValuePair<UInt256, byte[]> storage in allocation.Storage)
-                    {
-                        _stateProvider.Set(new StorageCell(address, storage.Key),
-                            storage.Value.WithoutLeadingZeros().ToArray());
-                    }
-                }
+                    SenderAddress = address,
+                    Data = allocation.Constructor,
+                    GasLimit = genesis.GasLimit
+                };
 
-                if (allocation.Constructor is not null)
+                CallOutputTracer outputTracer = new();
+                _transactionProcessor.Execute(constructorTransaction, outputTracer);
+
+                if (outputTracer.StatusCode != StatusCode.Success)
                 {
-                    Transaction constructorTransaction = new SystemTransaction()
-                    {
-                        SenderAddress = address,
-                        Data = allocation.Constructor,
-                        GasLimit = genesis.GasLimit
-                    };
-
-                    CallOutputTracer outputTracer = new();
-                    _transactionProcessor.Execute(constructorTransaction, outputTracer);
-
-                    if (outputTracer.StatusCode != StatusCode.Success)
-                    {
-                        throw new InvalidOperationException(
-                            $"Failed to initialize constructor for address {address}. Error: {outputTracer.Error}");
-                    }
+                    throw new InvalidOperationException(
+                        $"Failed to initialize constructor for address {address}. Error: {outputTracer.Error}");
                 }
             }
         }
+    }
 }
