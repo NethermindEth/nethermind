031b210da1c790bdeb67ef84f6f9ed413f1ec4ee
Lautaro Emanuel <emlautarom1@gmail.com>
2025-06-20 11:05:35 -0300

    Initial test project
    - Include required GRPC dependencies

---
+++ src/Nethermind/Nethermind.Network.Optimum.Test/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using GetOptimum;
+using Grpc.Core;
+using Grpc.Net.Client;
+using NUnit.Framework;
+
+namespace Nethermind.Network.Optimum.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task SubscribeForever()
+    {
+        using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+        var client = new OptimumNodeService.OptimumNodeServiceClient(channel);
+
+        using var call = client.Subscribe(new SubscribeRequest { Topic = "test-topic" });
+
+        await foreach (var response in call.ResponseStream.ReadAllAsync())
+        {
+            Console.WriteLine($"[response]: {response.Message}");
+        }
+    }
+}
