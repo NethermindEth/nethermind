5724a7af44f4bf253dd0b6501a9c5a797259ad9d
Lautaro Emanuel <emlautarom1@gmail.com>
2025-06-23 14:38:17 -0300

    Initial `GatewaySubscribeToTopic`

---
+++ src/Nethermind/Nethermind.Network.Optimum.Test/GatewayClient.cs
@@ -0,0 +1,90 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using GetOptimum.Gateway.Proto;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace Nethermind.Network.Optimum.Test;
+
+public interface IGatewayClient
+{
+    IAsyncEnumerable<GatewayMessage> SubscribeToTopic(string topic, double threshold, CancellationToken cancellation = default);
+}
+
+public sealed record GrpcGatewayClientOptions
+{
+    public required string ClientId { get; init; }
+    public required Uri RestEndpoint { get; init; }
+    public required Uri GrpcEndpoint { get; init; }
+}
+
+public sealed class GrpcGatewayClient(
+    HttpClient httpClient,
+    GrpcGatewayClientOptions options
+) : IGatewayClient
+{
+    public async IAsyncEnumerable<GatewayMessage> SubscribeToTopic(
+        string topic,
+        double threshold,
+        [EnumeratorCancellation] CancellationToken token = default)
+    {
+        var subscribeRequest = new
+        {
+            client_id = options.ClientId,
+            topic,
+            threshold
+        };
+        var subscribeResponse = await httpClient.SendAsync(new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = options.RestEndpoint,
+            Content = JsonContent.Create(subscribeRequest)
+        }, token);
+        subscribeResponse.EnsureSuccessStatusCode();
+
+        using var channel = GrpcChannel.ForAddress(options.GrpcEndpoint, new GrpcChannelOptions
+        {
+            Credentials = ChannelCredentials.Insecure,
+            MaxReceiveMessageSize = int.MaxValue,
+            MaxSendMessageSize = int.MaxValue,
+            HttpHandler = new SocketsHttpHandler
+            {
+                EnableMultipleHttp2Connections = true,
+                KeepAlivePingPolicy = HttpKeepAlivePingPolicy.Always,
+                // TODO: For now we'll make this constants. Consider making them part of `GrpcGatewayClientOptions`
+                KeepAlivePingDelay = TimeSpan.FromMinutes(2),
+                KeepAlivePingTimeout = TimeSpan.FromSeconds(20),
+            }
+        });
+        var client = new GatewayStream.GatewayStreamClient(channel);
+
+        using var call = client.ClientStream(cancellationToken: token);
+
+        await call.RequestStream.WriteAsync(new GatewayMessage { ClientId = options.ClientId }, token);
+
+        // NOTE: Required due to limitations of the C# compiler.
+        // See: https://github.com/dotnet/csharplang/issues/8414
+        while (!token.IsCancellationRequested)
+        {
+            GatewayMessage current;
+            try
+            {
+                await call.ResponseStream.MoveNext(token).ConfigureAwait(false);
+                current = call.ResponseStream.Current;
+            }
+            catch (RpcException e) when (e.InnerException is OperationCanceledException)
+            {
+                throw e.InnerException;
+            }
+
+            yield return current;
+        }
+    }
+}
--- src/Nethermind/Nethermind.Network.Optimum.Test/Tests.cs
+++ src/Nethermind/Nethermind.Network.Optimum.Test/Tests.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
-using GetOptimum;
-using Grpc.Core;
-using Grpc.Net.Client;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Nethermind.Network.Optimum.Test;
@@ -15,16 +18,30 @@ public class Tests
     }
 
     [Test]
-    public async Task SubscribeForever()
+    public async Task GatewaySubscribeToTopic()
     {
-        using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-        var client = new OptimumNodeService.OptimumNodeServiceClient(channel);
-
-        using var call = client.Subscribe(new SubscribeRequest { Topic = "test-topic" });
-
-        await foreach (var response in call.ResponseStream.ReadAllAsync())
+        using var httpClient = new HttpClient();
+        var options = new GrpcGatewayClientOptions
         {
-            Console.WriteLine($"[response]: {response.Message}");
+            ClientId = "nethermind-optimum-test-client",
+            RestEndpoint = new Uri("http://localhost:8081/api/subscribe"),
+            GrpcEndpoint = new Uri("http://localhost:50051")
+        };
+        var client = new GrpcGatewayClient(httpClient, options);
+
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+        var messages = client.SubscribeToTopic(topic: "test", threshold: 0.9, cts.Token);
+        int receivedCount = 0;
+        try
+        {
+            await foreach (var message in messages)
+            {
+                receivedCount++;
+            }
         }
+        catch (OperationCanceledException) { }
+
+        receivedCount.Should().BeGreaterThan(0, "Expected to receive at least one message from the gateway");
     }
 }
