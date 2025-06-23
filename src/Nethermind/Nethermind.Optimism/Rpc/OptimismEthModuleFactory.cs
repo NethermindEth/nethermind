05fd51997db90e576d2da8c27909630db6cac64a
Amirul Ashraf <asdacap@gmail.com>
2025-06-24 07:35:16 +0800

    Slight cleanup

--- src/Nethermind/Nethermind.Optimism/Rpc/OptimismEthModuleFactory.cs
+++ src/Nethermind/Nethermind.Optimism/Rpc/OptimismEthModuleFactory.cs
@@ -45,8 +45,6 @@ public class OptimismEthModuleFactory : ModuleFactoryBase<IOptimismEthRpcModule>
     private readonly IProtocolsManager _protocolsManager;
     private readonly ulong? _secondsPerSlot;
     private readonly IJsonRpcClient? _sequencerRpcClient;
-    private readonly IOptimismConfig _config;
-    private readonly ILogger _logger;
 
     public OptimismEthModuleFactory(IJsonRpcConfig rpcConfig,
         IBlockchainBridgeFactory blockchainBridgeFactory,
@@ -71,38 +69,37 @@ public class OptimismEthModuleFactory : ModuleFactoryBase<IOptimismEthRpcModule>
     )
     {
         _secondsPerSlot = blocksConfig.SecondsPerSlot;
-        _config = config;
-        _logManager = logManager ?? throw new ArgumentNullException(nameof(logManager));
-        _stateReader = stateReader ?? throw new ArgumentNullException(nameof(stateReader));
-        _blockchainBridgeFactory = blockchainBridgeFactory ?? throw new ArgumentNullException(nameof(blockchainBridgeFactory));
-        _txPool = txPool ?? throw new ArgumentNullException(nameof(txPool));
-        _txSender = txSender ?? throw new ArgumentNullException(nameof(txSender));
-        _wallet = wallet ?? throw new ArgumentNullException(nameof(wallet));
-        _rpcConfig = rpcConfig ?? throw new ArgumentNullException(nameof(rpcConfig));
-        _specProvider = specProvider ?? throw new ArgumentNullException(nameof(specProvider));
-        _gasPriceOracle = gasPriceOracle ?? throw new ArgumentNullException(nameof(gasPriceOracle));
-        _ethSyncingInfo = ethSyncingInfo ?? throw new ArgumentNullException(nameof(ethSyncingInfo));
-        _feeHistoryOracle = feeHistoryOracle ?? throw new ArgumentNullException(nameof(feeHistoryOracle));
-        _ecdsa = ecdsa ?? throw new ArgumentNullException(nameof(ecdsa));
-        _blockFinder = blockFinder ?? throw new ArgumentNullException(nameof(blockFinder));
-        _receiptFinder = receiptFinder ?? throw new ArgumentNullException(nameof(receiptFinder));
-        _opSpecHelper = opSpecHelper ?? throw new ArgumentNullException(nameof(opSpecHelper));
-        _protocolsManager = protocolsManager ?? throw new ArgumentNullException(nameof(protocolsManager));
-        _logger = logManager.GetClassLogger<OptimismEthModuleFactory>();
+        _logManager = logManager;
+        _stateReader = stateReader;
+        _blockchainBridgeFactory = blockchainBridgeFactory;
+        _txPool = txPool;
+        _txSender = txSender;
+        _wallet = wallet;
+        _rpcConfig = rpcConfig;
+        _specProvider = specProvider;
+        _gasPriceOracle = gasPriceOracle;
+        _ethSyncingInfo = ethSyncingInfo;
+        _feeHistoryOracle = feeHistoryOracle;
+        _ecdsa = ecdsa;
+        _blockFinder = blockFinder;
+        _receiptFinder = receiptFinder;
+        _opSpecHelper = opSpecHelper;
+        _protocolsManager = protocolsManager;
 
-        if (_config.SequencerUrl is null && _logger.IsWarn)
+        ILogger logger = logManager.GetClassLogger<OptimismEthModuleFactory>();
+        if (config.SequencerUrl is null && logger.IsWarn)
         {
-            _logger.Warn("SequencerUrl is not set. Nethermind will behave as a Sequencer");
+            logger.Warn("SequencerUrl is not set. Nethermind will behave as a Sequencer");
         }
-        BasicJsonRpcClient? sequencerJsonRpcClient = _config.SequencerUrl is not null
-            ? new(new Uri(_config.SequencerUrl), jsonSerializer, logManager)
+
+        BasicJsonRpcClient? sequencerJsonRpcClient = config.SequencerUrl is not null
+            ? new(new Uri(config.SequencerUrl), jsonSerializer, logManager)
             : null;
         _sequencerRpcClient = sequencerJsonRpcClient;
 
         ITxSigner txSigner = new WalletTxSigner(wallet, specProvider.ChainId);
         TxSealer sealer = new(txSigner, timestamper);
         _sealer = sealer;
-
     }
 
     public override IOptimismEthRpcModule Create()
