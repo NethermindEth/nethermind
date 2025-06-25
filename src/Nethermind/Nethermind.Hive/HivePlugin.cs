27e293d13e473f288a29ea659ddef781c047bb39
Amirul Ashraf <asdacap@gmail.com>
2025-06-25 17:45:15 +0800

    Hive

--- src/Nethermind/Nethermind.Hive/HivePlugin.cs
+++ src/Nethermind/Nethermind.Hive/HivePlugin.cs
@@ -2,26 +2,19 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
-using System.Threading;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Core;
 using Nethermind.Api;
 using Nethermind.Api.Extensions;
-using Nethermind.Logging;
+using Nethermind.Api.Steps;
+using Nethermind.Core;
 
 namespace Nethermind.Hive;
 
 public class HivePlugin(IHiveConfig hiveConfig) : INethermindPlugin
 {
-    private INethermindApi _api = null!;
-    private ILogger _logger;
-    private readonly CancellationTokenSource _disposeCancellationToken = new();
-
-    public ValueTask DisposeAsync()
-    {
-        _disposeCancellationToken.Cancel();
-        _disposeCancellationToken.Dispose();
-        return ValueTask.CompletedTask;
-    }
+    public ValueTask DisposeAsync() => ValueTask.CompletedTask;
 
     public string Name => "Hive";
 
@@ -29,45 +22,20 @@ public class HivePlugin(IHiveConfig hiveConfig) : INethermindPlugin
 
     public string Author => "Nethermind";
 
-    public Task Init(INethermindApi api)
-    {
-        _api = api ?? throw new ArgumentNullException(nameof(api));
-        _logger = _api.LogManager.GetClassLogger();
+    public Task Init(INethermindApi api) => Task.CompletedTask;
 
-        return Task.CompletedTask;
-    }
+    public Task InitNetworkProtocol() => Task.CompletedTask;
 
-    public async Task InitNetworkProtocol()
-    {
-        ArgumentNullException.ThrowIfNull(_api.BlockTree);
-        ArgumentNullException.ThrowIfNull(_api.BlockProcessingQueue);
-        ArgumentNullException.ThrowIfNull(_api.ConfigProvider);
-        ArgumentNullException.ThrowIfNull(_api.LogManager);
-        ArgumentNullException.ThrowIfNull(_api.FileSystem);
-        ArgumentNullException.ThrowIfNull(_api.BlockValidator);
-
-        _api.TxPool!.AcceptTxWhenNotSynced = true;
-
-        _api.TxGossipPolicy.Policies.Clear();
-
-        HiveRunner hiveRunner = new(
-            _api.BlockTree,
-            _api.BlockProcessingQueue,
-            _api.ConfigProvider,
-            _api.LogManager.GetClassLogger(),
-            _api.FileSystem,
-            _api.BlockValidator
-        );
-
-        if (_logger.IsInfo) _logger.Info("Hive is starting");
-
-        await hiveRunner.Start(_disposeCancellationToken.Token);
-    }
-
-    public Task InitRpcModules()
-    {
-        return Task.CompletedTask;
-    }
+    public Task InitRpcModules() => Task.CompletedTask;
 
     public bool Enabled => hiveConfig.Enabled;
+
+    public IModule Module => new HiveModule();
+}
+
+public class HiveModule: Module
+{
+    protected override void Load(ContainerBuilder builder) => builder
+        .AddSingleton<HiveRunner>()
+        .AddStep(typeof(HiveStep));
 }
