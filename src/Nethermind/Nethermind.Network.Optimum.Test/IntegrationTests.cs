dc146064dd85bfcd8b96dff5646f868ca6d399c2
Lautaro Emanuel <emlautarom1@gmail.com>
2025-06-25 17:12:41 -0300

    Remove irrelevant comment
    - By reusing channels we get concurrency without mayor drawbacks

--- src/Nethermind/Nethermind.Network.Optimum.Test/IntegrationTests.cs
+++ src/Nethermind/Nethermind.Network.Optimum.Test/IntegrationTests.cs
@@ -94,15 +94,4 @@ public class IntegrationTests
 
         receivedCount.Should().BeGreaterThan(0, "Expected to receive at least one message from the gateway");
     }
-
-    // NOTE: The current implementation, both for Gateway and Node, uses a single gRPC connection per topic.
-    // In an ideal scenario, we would multiplex multiple topics over a single gRPC connection.
-    // The main benefit is to reduce the network load.
-    // However, this has some drawbacks:
-    // - The Gateway does not implement a mechanism to unsubscribe from a topic, so the same gRPC stream will continue sending messages from topic we might no longer be interested in.
-    // - The Node implements a mechanism to unsubscribe from a topic, but managing topic from our side implies managing subscriptions. It can be done, but the implementation is more complex and potentially more fragile.
-    //      For example, the gRPC library is not thread safe (https://learn.microsoft.com/en-us/aspnet/core/grpc/services?view=aspnetcore-9.0#multi-threading-with-grpc-streaming-methods), that is, we cannot read/write to the same gRPC stream from multiple threads.
-    //
-    // TODO: Ask how many topics are we interested in.
-    // If the number is relatively low we can use multiple connections, otherwise we need to multiplex.
 }
