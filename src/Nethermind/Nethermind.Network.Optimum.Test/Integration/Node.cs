74f17ada69caa5bb4931e5f1ae77c541405f137e
Lautaro Emanuel <emlautarom1@gmail.com>
2025-06-26 16:59:09 -0300

    Initial Node integration test
    - Use Testcontainers
    - Test for publish/subscribe
    - Include valid identity file

--- src/Nethermind/Nethermind.Network.Optimum.Test/Integration/Node.cs
+++ src/Nethermind/Nethermind.Network.Optimum.Test/Integration/Node.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
 using FluentAssertions;
 using Grpc.Net.Client;
 using NUnit.Framework;
@@ -14,6 +16,43 @@ namespace Nethermind.Network.Optimum.Test.Integration;
 
 public class Node
 {
+    private IContainer _container;
+    private Uri _grpcEndpoint;
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        using var p2pKeyStream = typeof(Node).Assembly.GetManifestResourceStream("identity/p2p.key");
+        var identityFile = new byte[p2pKeyStream!.Length];
+        p2pKeyStream.ReadExactly(identityFile, 0, identityFile.Length);
+
+        _container = new ContainerBuilder()
+            .WithImage("getoptimum/p2pnode:latest")
+            .WithResourceMapping(identityFile, "/identity/p2p.key")
+            .WithEnvironment("IDENTITY_DIR", "/identity")
+            .WithEnvironment("NODE_MODE", "optimum")
+            .WithEnvironment("SIDECAR_PORT", "33212") // Port for the grpc bidirectional streaming
+            .WithEnvironment("API_PORT", "9090") // Port for the REST API
+            .WithEnvironment("OPTIMUM_PORT", "7070") // Port for Optimum
+            .WithPortBinding(33212, assignRandomHostPort: true)
+            .WithPortBinding(9090, assignRandomHostPort: true)
+            .WithPortBinding(7070, assignRandomHostPort: true)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r
+                .ForPort(9090)
+                .ForPath("/api/v1/health")))
+            .Build();
+        await _container.StartAsync();
+
+        _grpcEndpoint = new Uri($"http://{_container.Hostname}:{_container.GetMappedPublicPort(33212)}");
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await _container.StopAsync();
+        await _container.DisposeAsync();
+    }
+
     [Test]
     public async Task SubscribeToTopic()
     {
@@ -56,10 +95,10 @@ public class Node
     [Test]
     public async Task PublishAndSubscribeToTopic()
     {
-        using var grpcChannel = GrpcChannel.ForAddress(new Uri("http://localhost:33221"), Configuration.DefaultGrpcChannelOptions);
+        using var grpcChannel = GrpcChannel.ForAddress(_grpcEndpoint, Configuration.DefaultGrpcChannelOptions);
 
         var client = new GrpcOptimumNodeClient(grpcChannel);
-        var topic = "test";
+        var topic = Guid.NewGuid().ToString();
 
         var sentMessages = Enumerable.Range(0, 10)
             .Select(_ => Encoding.UTF8.GetBytes($"msg = {Guid.NewGuid()}"))
