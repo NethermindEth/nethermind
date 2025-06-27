ba3066d17ce71a206ffb8a54d9837e16bca6588d
Ben Adams <thundercat@illyriad.co.uk>
2025-06-27 13:57:13 +0100

    Optimize JwtAuthentication.Authenticate

---
+++ src/Nethermind/Nethermind.Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+
+namespace Nethermind.Core.Extensions;
+public static class DateTimeExtensions
+{
+    public static long ToUnixTimeSeconds(this DateTime date)
+        => (date - DateTime.UnixEpoch).Ticks / TimeSpan.TicksPerSecond;
+}
