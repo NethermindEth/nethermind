2017c999fef0efb2e23dd784b2be94438f961849
Nikita Meshcheriakov <root@nikitam.io>
2025-06-29 14:19:11 +0300

    Remove InternalsVisibleTo

--- src/Nethermind/Nethermind.Evm/State/Snapshot.cs
+++ src/Nethermind/Nethermind.Evm/State/Snapshot.cs
@@ -1,10 +1,6 @@
 // SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Nethermind.Evm.Test")]
-
 namespace Nethermind.Evm.State;
 
 /// <summary>
