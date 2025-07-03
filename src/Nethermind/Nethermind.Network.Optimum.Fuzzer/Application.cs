a8ae25a815bda18425ab12cd783b4ac851d9ad36
Lautaro Emanuel <emlautarom1@gmail.com>
2025-07-03 12:20:42 -0300

    Extend CLI options

--- src/Nethermind/Nethermind.Network.Optimum.Fuzzer/Application.cs
+++ src/Nethermind/Nethermind.Network.Optimum.Fuzzer/Application.cs
@@ -1,6 +1,10 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Nethermind.Network.Optimum.Fuzzer;
 
 public class Application(FuzzerOptions options)
--- src/Nethermind/Nethermind.Network.Optimum.Fuzzer/Program.cs
+++ src/Nethermind/Nethermind.Network.Optimum.Fuzzer/Program.cs
@@ -1,70 +1,87 @@
 ﻿// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using System.CommandLine;
+using System.Threading.Tasks;
 
 namespace Nethermind.Network.Optimum.Fuzzer;
 
 public sealed record FuzzerOptions
 {
+    public required Uri GrpcEndpoint { get; init; }
     public required int MessageSize { get; init; }
     public required int MessageCount { get; init; }
     public required int PublisherCount { get; init; }
     public required int SubscriberCount { get; init; }
     public required TimeSpan PublisherDelay { get; init; }
     public required TimeSpan Timeout { get; init; }
-    public required TimeSpan Retries { get; init; }
+    public required TimeSpan Runs { get; init; }
 
     public static FuzzerOptions FromParseResult(ParseResult p)
     {
         return new FuzzerOptions
         {
+            GrpcEndpoint = p.GetValue(FuzzerCliOptions.GrpcEndpoint)!,
             MessageSize = p.GetValue(FuzzerCliOptions.MessageSize),
             MessageCount = p.GetValue(FuzzerCliOptions.MessageCount),
             PublisherCount = p.GetValue(FuzzerCliOptions.PublisherCount),
             SubscriberCount = p.GetValue(FuzzerCliOptions.SubscriberCount),
-            PublisherDelay = p.GetValue(FuzzerCliOptions.PublisherDelay),
-            Timeout = p.GetValue(FuzzerCliOptions.Timeout),
-            Retries = p.GetValue(FuzzerCliOptions.Retries)
+            PublisherDelay = TimeSpan.FromMilliseconds(p.GetValue(FuzzerCliOptions.PublisherDelay)),
+            Timeout = TimeSpan.FromMilliseconds(p.GetValue(FuzzerCliOptions.Timeout)),
+            Runs = TimeSpan.FromMilliseconds(p.GetValue(FuzzerCliOptions.Runs))
         };
     }
 }
 
 public static class FuzzerCliOptions
 {
+    public static readonly Option<Uri> GrpcEndpoint = new("--address")
+    {
+        Description = "The Optimum Node gRPC endpoint",
+        Required = true,
+    };
+
     public static readonly Option<int> MessageSize = new("--message-size")
     {
-        Description = ""
+        Description = "The size (in bytes) of each message to send",
+        DefaultValueFactory = (_) => 1024 // Default to 1kb
     };
 
     public static readonly Option<int> MessageCount = new("--message-count")
     {
-        Description = "",
+        Description = "The number of messages to send per publisher",
+        DefaultValueFactory = (_) => 100_000 // Default to 100_000 messages
     };
 
     public static readonly Option<int> PublisherCount = new("--publisher-count")
     {
-        Description = "",
+        Description = "The number of concurrent publishers",
+        DefaultValueFactory = (_) => 1 // Default to 1 publisher
     };
 
     public static readonly Option<int> SubscriberCount = new("--subscriber-count")
     {
-        Description = "",
+        Description = "The number of concurrent subscribers",
+        DefaultValueFactory = (_) => 1 // Default to 1 subscriber
     };
 
-    public static readonly Option<TimeSpan> PublisherDelay = new("--publisher")
+    public static readonly Option<int> PublisherDelay = new("--publisher-timeout")
     {
-        Description = "",
+        Description = "Delay between publishing messages (milliseconds)",
+        DefaultValueFactory = (_) => 1 // 1000 microseconds
     };
 
-    public static readonly Option<TimeSpan> Timeout = new("--timeout")
+    public static readonly Option<int> Timeout = new("--timeout")
     {
-        Description = "",
+        Description = "Timeout for each run (milliseconds)",
+        DefaultValueFactory = (_) => 60_000 // 1 minute
     };
 
-    public static readonly Option<TimeSpan> Retries = new("--retries")
+    public static readonly Option<int> Runs = new("--runs")
     {
-        Description = "",
+        Description = "Number of times to repeat each run",
+        DefaultValueFactory = (_) => 1
     };
 }
 
@@ -74,13 +91,14 @@ public static class Program
     {
         var rootCommand = new RootCommand("Optimum Node gRPC fuzzer")
         {
+            FuzzerCliOptions.GrpcEndpoint,
             FuzzerCliOptions.MessageSize,
             FuzzerCliOptions.MessageCount,
             FuzzerCliOptions.PublisherCount,
             FuzzerCliOptions.SubscriberCount,
             FuzzerCliOptions.PublisherDelay,
             FuzzerCliOptions.Timeout,
-            FuzzerCliOptions.Retries
+            FuzzerCliOptions.Runs
         };
 
         rootCommand.SetAction(async (parseResult, token) =>
