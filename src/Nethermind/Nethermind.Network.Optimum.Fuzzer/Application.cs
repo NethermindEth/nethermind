da3b60a86b269169f6544955aaa070c38a83efb5
Lautaro Emanuel <emlautarom1@gmail.com>
2025-07-03 14:29:00 -0300

    Small cleanup

--- src/Nethermind/Nethermind.Network.Optimum.Fuzzer/Application.cs
+++ src/Nethermind/Nethermind.Network.Optimum.Fuzzer/Application.cs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
@@ -10,11 +9,11 @@ using Microsoft.Extensions.Logging;
 
 namespace Nethermind.Network.Optimum.Fuzzer;
 
-public class Application(FuzzerOptions options, ILogger logger)
+public sealed class Application(FuzzerOptions options, ILogger logger)
 {
     public Task RunAsync(CancellationToken token)
     {
-        logger.LogInformation("{options}", options);
+        logger.LogInformation("{FuzzerOptions}", options);
 
         using var grpcChannel = GrpcChannel.ForAddress(options.GrpcEndpoint, Options.DefaultGrpcChannelOptions);
 
