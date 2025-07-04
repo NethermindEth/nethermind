f013df688886869842f3f11b6cfc7c4b9d5c37ce
Amirul Ashraf <asdacap@gmail.com>
2025-07-04 20:04:44 +0800

    Remove strange

--- src/Nethermind/Nethermind.Network.Discovery.Test/Kademlia/Hash256XorUtilsTests.cs
+++ src/Nethermind/Nethermind.Network.Discovery.Test/Kademlia/Hash256XorUtilsTests.cs
@@ -96,23 +96,4 @@ public class Hash256XorUtilsTests
 
         Hash256XorUtils.Compare(h1, h2, h3).Should().BeLessThan(0);
     }
-
-    [TestCase]
-    public void Strange()
-    {
-        ValueHash256 a = new ValueHash256("0x1a0c466f5d75e4d8ad6765d5f519dbc82b7c343b37f88500ec5e64005393b30d");
-        ValueHash256 b = new ValueHash256("0x82bf3eb6be6c2d15511b0dc6c68c97bad52b834b11656c6104af44123e565a3d");
-
-        Vector<byte> aBig = new Vector<byte>(a.BytesAsSpan);
-        Vector<byte> bBig = new Vector<byte>(b.BytesAsSpan);
-
-        ValueHash256 xored = new ValueHash256();
-        (aBig ^ bBig).CopyTo(xored.BytesAsSpan);
-
-        Console.Error.WriteLine($"The three {a} {b} {xored}");
-
-        // Hash256DistanceCalculator calculator = new Hash256DistanceCalculator();
-        // Console.Error.WriteLine($"Distance {calculator.CalculateDistance(a, b)} {calculator.BigIntLogDist(a, b)}");
-        // Console.Error.WriteLine($"Distanceb {calculator.BigIntDist(a, b)} ");
-    }
 }
