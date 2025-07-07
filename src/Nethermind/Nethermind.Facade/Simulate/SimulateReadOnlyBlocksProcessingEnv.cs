33450abe38284c0ed3d2a1f6bd4795decf50a01d
Nikita Meshcheriakov <root@nikitam.io>
2025-07-07 11:24:35 +0300

    Fix build

--- src/Nethermind/Nethermind.Facade/Simulate/SimulateReadOnlyBlocksProcessingEnv.cs
+++ src/Nethermind/Nethermind.Facade/Simulate/SimulateReadOnlyBlocksProcessingEnv.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Threading;
 using Nethermind.Blockchain;
+using Nethermind.Blockchain.Tracing;
 using Nethermind.Consensus.Processing;
 using Nethermind.Core;
 using Nethermind.Core.Specs;
