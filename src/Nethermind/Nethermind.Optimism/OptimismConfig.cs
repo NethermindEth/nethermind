150aecfac5870f7bcfd3d59f1d504aff1eceaab9
rubo <rubo@users.noreply.github.com>
2025-07-15 11:01:33 +0000

    Remove Optimism stuff

--- src/Nethermind/Nethermind.Optimism/OptimismConfig.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
-// SPDX-License-Identifier: LGPL-3.0-only
-
-namespace Nethermind.Optimism;
-
-public class OptimismConfig : IOptimismConfig
-{
-    public string? SequencerUrl { get; set; } = null;
-}
