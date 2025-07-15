cf3b1e92763bb799109a91e4f6686844e8944b90
Siddharth Vaderaa <svaderaa@gmail.com>
2025-07-15 22:34:01 +0200

    Fix ilevm executes only when not tracing

--- src/Nethermind/Nethermind.Evm/VirtualMachine.cs
+++ src/Nethermind/Nethermind.Evm/VirtualMachine.cs
@@ -1131,7 +1131,7 @@ public sealed unsafe partial class VirtualMachine(
             vmState.Memory.Save(in localPreviousDest, previousCallOutput);
         }
 
-        if (env.CodeInfo.IlMetadata.IsPrecompiled && (typeof(TTracingInst) != typeof(OnFlag)))
+        if (env.CodeInfo.IlMetadata.IsPrecompiled && !_txTracer.IsTracing)
         {
             Metrics.IlvmAotPrecompiledCalls++; // this will treat continuations as new calls
 
