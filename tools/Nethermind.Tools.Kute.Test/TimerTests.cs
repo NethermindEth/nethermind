675ffb66d558e72d7290de2ce1fd8a82bc2e25d2
Lautaro Emanuel <emlautarom1@gmail.com>
2025-07-18 17:04:17 -0300

    Add `Timer`

---
+++ tools/Nethermind.Tools.Kute.Test/TimerTests.cs
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Nethermind.Tools.Kute.Test;
+
+public class TimerTests
+{
+    [Test]
+    public async Task Timer_ComputesElapsedTime()
+    {
+        var t = new Timer();
+        using (t.Time())
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+        }
+
+        t.Elapsed.Should().BeGreaterThan(TimeSpan.FromMilliseconds(90));
+        t.Elapsed.Should().BeLessThan(TimeSpan.FromMilliseconds(110));
+    }
+
+    [Test]
+    public async Task Timer_AddsAllElapsedTimes()
+    {
+        var t = new Timer();
+        using (t.Time())
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(50));
+        }
+        using (t.Time())
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(50));
+        }
+
+        t.Elapsed.Should().BeGreaterThan(TimeSpan.FromMilliseconds(90));
+        t.Elapsed.Should().BeLessThan(TimeSpan.FromMilliseconds(110));
+    }
+}
