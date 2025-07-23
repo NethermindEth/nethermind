b0fae2a8613a3ce9132ecdc5214388cf9b2bf3b8
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-07-23 17:50:01 +0100

    add metrics

--- src/Nethermind/Nethermind.History/Metrics.cs
+++ src/Nethermind/Nethermind.History/Metrics.cs
@@ -11,4 +11,16 @@ public static class Metrics
     [GaugeMetric]
     [Description("The number of the oldest block stored.")]
     public static long OldestStoredBlockNumber { get; set; }
+
+    [CounterMetric]
+    [Description("The number of the historical blocks that have been pruned (since restart).")]
+    public static long BlocksPruned { get; set; }
+
+    [GaugeMetric]
+    [Description("The cutoff timestamp from which historical blocks will be pruned.")]
+    public static ulong? PruningCutoffTimestamp { get; set; }
+
+    [GaugeMetric]
+    [Description("The cutoff block number from which historical blocks will be pruned.")]
+    public static long? PruningCutoffBlocknumber { get; set; }
 }
