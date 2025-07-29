88c773e376da1f9a3e0ce5e4f7d17bf04e754c4f
Alex Bespalov <alexb5dh@gmail.com>
2025-07-29 20:46:18 +0300

    Tests fix

--- src/Nethermind/Nethermind.Db.Test/LogIndex/LogIndexStorageComplexTests.cs
+++ src/Nethermind/Nethermind.Db.Test/LogIndex/LogIndexStorageComplexTests.cs
@@ -13,7 +13,6 @@ using MathNet.Numerics.Random;
 using Nethermind.Core;
 using Nethermind.Core.Collections;
 using Nethermind.Core.Crypto;
-using Nethermind.Core.Test;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Db.Rocks;
 using Nethermind.Db.Rocks.Config;
@@ -70,9 +69,7 @@ namespace Nethermind.Db.Test.LogIndex
 
             Directory.CreateDirectory(_dbPath);
 
-            var config = new DbConfig();
-            var configFactory = new RocksDbConfigFactory(new DbConfig(), new PruningConfig(), new TestHardwareInfo(0), LimboLogs.Instance);
-            _dbFactory = new RocksDbFactory(configFactory, config, new TestLogManager(), _dbPath);
+            _dbFactory = new RocksDbFactory(new DbConfig(), new TestLogManager(), _dbPath);
         }
 
         [TearDown]
