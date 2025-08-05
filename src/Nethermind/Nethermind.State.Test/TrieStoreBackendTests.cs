83d81822ca580b9e4be412d44aeb43f06cd93d40
Amirul Ashraf <asdacap@gmail.com>
2025-08-05 15:38:17 +0800

    Add small test

---
+++ src/Nethermind/Nethermind.State.Test/TrieStoreBackendTests.cs
@@ -0,0 +1,71 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using FluentAssertions;
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Test;
+using Nethermind.Core.Test.Builders;
+using Nethermind.Evm.State;
+using Nethermind.Logging;
+using Nethermind.State;
+using NUnit.Framework;
+
+namespace Nethermind.Store.Test;
+
+public class TrieStoreBackendTests
+{
+    [Test]
+    public void Test_CanSaveToState()
+    {
+        TestMemDb kv = new TestMemDb();
+        IWorldStateBackend backend = new TrieStoreBackend(new TestRawTrieStore(kv), LimboLogs.Instance);
+
+        Hash256 stateRoot;
+        using (var scope = backend.BeginScope(null))
+        {
+            scope.StateTree.Get(TestItem.AddressA).Should().Be(null);
+            scope.StateTree.Set(TestItem.AddressA, new Account(100, 100));
+            scope.Commit(1);
+            stateRoot = scope.StateTree.RootHash;
+        }
+
+        stateRoot.Should().NotBe(Keccak.EmptyTreeHash);
+        kv.WritesCount.Should().Be(1);
+
+        using (var scope = backend.BeginScope(Build.A.BlockHeader.WithStateRoot(stateRoot).WithNumber(1).TestObject))
+        {
+            scope.StateTree.Get(TestItem.AddressA).Balance.Should().Be(100);
+        }
+    }
+
+    [Test]
+    public void Test_CanSaveToStorage()
+    {
+        TestMemDb kv = new TestMemDb();
+        IWorldStateBackend backend = new TrieStoreBackend(new TestRawTrieStore(kv), LimboLogs.Instance);
+
+        Hash256 stateRoot;
+        using (var scope = backend.BeginScope(null))
+        {
+            scope.StateTree.Get(TestItem.AddressA).Should().Be(null);
+
+            var storage = scope.CreateStorageTree(TestItem.AddressA);
+            storage.Set(1, [1, 2, 3]);
+            storage.UpdateRootHash();
+            scope.StateTree.Set(TestItem.AddressA, new Account(100, 100, storage.RootHash, Hash256.Zero));
+
+            scope.Commit(1);
+            stateRoot = scope.StateTree.RootHash;
+        }
+
+        stateRoot.Should().NotBe(Keccak.EmptyTreeHash);
+        kv.WritesCount.Should().Be(2);
+
+        using (var scope = backend.BeginScope(Build.A.BlockHeader.WithStateRoot(stateRoot).WithNumber(1).TestObject))
+        {
+            var storage = scope.CreateStorageTree(TestItem.AddressA);
+            storage.Get(1).Should().BeEquivalentTo([1, 2, 3]);
+        }
+    }
+}
