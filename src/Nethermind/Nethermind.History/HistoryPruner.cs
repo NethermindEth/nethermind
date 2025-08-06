4e491f4689dd89a04551704bdc0d9b78e8d09f2b
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-08-06 17:39:42 +0100

    try disabling block processing queue pruning

--- src/Nethermind/Nethermind.History/HistoryPruner.cs
+++ src/Nethermind/Nethermind.History/HistoryPruner.cs
@@ -98,7 +98,7 @@ public class HistoryPruner : IHistoryPruner
                 Metrics.PruningCutoffTimestamp = null;
             }
 
-            blockProcessingQueue.ProcessingQueueEmpty += OnBlockProcessorQueueEmpty;
+            // blockProcessingQueue.ProcessingQueueEmpty += OnBlockProcessorQueueEmpty;
         }
     }
 
