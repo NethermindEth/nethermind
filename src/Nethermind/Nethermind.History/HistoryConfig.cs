123a417c3df1db37e07b2822ad6dca0987d735d0
Marc <Marchhill@users.noreply.github.com>
2025-08-12 16:58:43 +0100

    Disable history pruner by default (#9125)
    disable history pruner by default
    
    Co-authored-by: Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>

--- src/Nethermind/Nethermind.History/HistoryConfig.cs
+++ src/Nethermind/Nethermind.History/HistoryConfig.cs
@@ -1,12 +1,12 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-namespace Nethermind.Config;
+namespace Nethermind.History;
 
 public class HistoryConfig : IHistoryConfig
 {
     public bool Enabled => Pruning != PruningModes.Disabled;
 
-    public PruningModes Pruning { get; set; } = PruningModes.UseAncientBarriers;
+    public PruningModes Pruning { get; set; } = PruningModes.Disabled;
     public long RetentionEpochs { get; set; } = 82125;
 }
