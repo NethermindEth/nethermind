7b82a84f7138ae36d5af98a4ce2f77e84d77dcc5
Alex Bespalov <alexb5dh@gmail.com>
2025-08-18 21:51:05 +0300

    Indexing stopping fix

--- src/Nethermind/Nethermind.Facade/Find/ILogIndexService.cs
+++ src/Nethermind/Nethermind.Facade/Find/ILogIndexService.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using Nethermind.Core.ServiceStopper;
 
 namespace Nethermind.Facade.Find
 {
-    public interface ILogIndexService : IAsyncDisposable
+    public interface ILogIndexService : IAsyncDisposable, IStoppableService
     {
-        string Description { get; }
         Task StartAsync();
-        Task StopAsync();
     }
 }
--- src/Nethermind/Nethermind.Init/Steps/StartLogIndexService.cs
+++ src/Nethermind/Nethermind.Init/Steps/StartLogIndexService.cs
@@ -5,17 +5,19 @@ using System.Threading;
 using System.Threading.Tasks;
 using Nethermind.Api;
 using Nethermind.Api.Steps;
+using Nethermind.Core.ServiceStopper;
 using Nethermind.Facade.Find;
 
 namespace Nethermind.Init.Steps
 {
     [RunnerStepDependencies(typeof(StartBlockProcessor))]
-    public class StartLogIndexService(IBasicApi api, ILogIndexService logIndexService) : IStep
+    public class StartLogIndexService(IBasicApi api, IServiceStopper serviceStopper, ILogIndexService logIndexService) : IStep
     {
         public async Task Execute(CancellationToken cancellationToken)
         {
             await logIndexService.StartAsync();
             api.DisposeStack.Push(logIndexService);
+            serviceStopper.AddStoppable(logIndexService);
         }
 
         public bool MustInitialize => false;
