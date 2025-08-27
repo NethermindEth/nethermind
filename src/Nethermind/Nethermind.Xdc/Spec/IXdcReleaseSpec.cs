343b5666ad5777ccbdbb8b364e610634bf5da863
ak88 <anders.holmbjerg@hotmail.com>
2025-08-27 17:40:10 +0200

    small correction

--- src/Nethermind/Nethermind.Xdc/Spec/IXdcReleaseSpec.cs
+++ src/Nethermind/Nethermind.Xdc/Spec/IXdcReleaseSpec.cs
@@ -1,8 +1,10 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using Nethermind.Core.Specs;
+
 namespace Nethermind.Xdc.Spec;
 
-internal interface IXdcReleaseSpec
+internal interface IXdcReleaseSpec : IReleaseSpec
 {
 }
