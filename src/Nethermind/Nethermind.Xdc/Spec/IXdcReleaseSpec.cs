2c88f1d9c427b0e4071bd9f3137b8b1b4c32b8a7
ak88 <anders.holmbjerg@hotmail.com>
2025-08-27 17:34:33 +0200

    Added basic spec classes

---
+++ src/Nethermind/Nethermind.Xdc/Spec/IXdcReleaseSpec.cs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+namespace Nethermind.Xdc.Spec;
+
+internal interface IXdcReleaseSpec
+{
+}
---
+++ src/Nethermind/Nethermind.Xdc/Spec/XdcChainSpecEngineParameters.cs
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using Nethermind.Core;
+using Nethermind.Int256;
+using Nethermind.Specs.ChainSpecStyle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nethermind.Xdc.Spec;
+internal class XdcChainSpecEngineParameters : IChainSpecEngineParameters
+{
+    public string EngineName => SealEngineType;
+    public string SealEngineType => Core.SealEngineType.XDPoS;
+    public int Epoch { get; set; }
+    public int Gap { get; set; }
+    public bool SkipV1Validation { get; set; }
+    public Address FoundationWalletAddr { get; set; }
+    public int Reward { get; set; }
+}
+
