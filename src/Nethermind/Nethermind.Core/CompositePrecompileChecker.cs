cb4737c2a3b04660653ae768050cd6cc314989d7
stavrosvl7 <stavrosvl7@gmail.com>
2025-08-28 14:34:04 +0300

    Improvements

--- src/Nethermind/Nethermind.Core/CompositePrecompileChecker.cs
+++ src/Nethermind/Nethermind.Core/CompositePrecompileChecker.cs
@@ -1,15 +1,31 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Linq;
+using System;
 using Nethermind.Core.Specs;
 
 namespace Nethermind.Core;
 
-public class CompositePrecompileChecker(params IPrecompileChecker[] checkers) : IPrecompileChecker
+public class CompositePrecompileChecker : IPrecompileChecker
 {
+    private readonly IPrecompileChecker[] _checkers;
+
+    public CompositePrecompileChecker(params IPrecompileChecker[] checkers)
+    {
+        if (checkers is null || checkers.Length == 0)
+            throw new ArgumentException("At least one precompile checker must be provided", nameof(checkers));
+
+        _checkers = checkers;
+    }
+
     public bool IsPrecompile(Address address, IReleaseSpec spec)
     {
-        return checkers.Any(checker => checker.IsPrecompile(address, spec));
+        foreach (var pc in _checkers)
+        {
+            if (pc.IsPrecompile(address, spec))
+                return true;
+        }
+
+        return false;
     }
 }
