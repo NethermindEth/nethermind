5f9618448baec85b599006c37d6d9ef243ec59a6
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-08-29 17:50:30 +0100

    more tidying

--- src/Nethermind/Nethermind.Core/BlockAccessList/BlockAccessList.cs
+++ src/Nethermind/Nethermind.Core/BlockAccessList/BlockAccessList.cs
@@ -6,7 +6,6 @@ using Nethermind.Int256;
 
 namespace Nethermind.Core.BlockAccessLists;
 
-// StorageChange: [block_access_index, new_value]
 public struct StorageChange
 {
     public ushort BlockAccessIndex { get; set; }
@@ -18,29 +17,24 @@ public struct StorageRead
     public byte[] Key { get; set; }
 }
 
-// BalanceChange: [block_access_index, post_balance]
 public struct BalanceChange
 {
     public ushort BlockAccessIndex { get; set; }
     public UInt256 PostBalance { get; set; }
 }
 
-// NonceChange: [block_access_index, new_nonce]
 public struct NonceChange
 {
     public ushort BlockAccessIndex { get; set; }
     public ulong NewNonce { get; set; }
 }
 
-// CodeChange: [block_access_index, new_code]
 public struct CodeChange
 {
     public ushort BlockAccessIndex { get; set; }
     public byte[] NewCode { get; set; }
 }
 
-// SlotChanges: [slot, [changes]]
-// All changes to a single storage slot
 public struct SlotChanges()
 {
     public byte[] Slot { get; set; } = [];
@@ -50,20 +44,10 @@ public struct SlotChanges()
 public struct AccountChanges(Address address)
 {
     public byte[] Address { get; set; } = address.Bytes;
-
-    // Storage changes (slot -> [tx_index -> new_value])
     public SortedDictionary<byte[], SlotChanges> StorageChanges { get; set; } = [];
-
-    // Read-only storage keys
     public List<StorageRead> StorageReads { get; set; } = [];
-
-    // Balance changes ([tx_index -> post_balance])
     public List<BalanceChange> BalanceChanges { get; set; } = [];
-
-    // Nonce changes ([tx_index -> new_nonce])
     public List<NonceChange> NonceChanges { get; set; } = [];
-
-    // Code changes ([tx_index -> new_code])
     public List<CodeChange> CodeChanges { get; set; } = [];
 }
 
