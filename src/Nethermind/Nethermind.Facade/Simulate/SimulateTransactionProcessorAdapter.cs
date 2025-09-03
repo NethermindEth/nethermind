e30c7ad37d63f200210fd5b0d56eea9420477670
Nikita Meshcheriakov <root@nikitam.io>
2025-09-03 17:37:46 +0300

    add logging

--- src/Nethermind/Nethermind.Facade/Simulate/SimulateTransactionProcessorAdapter.cs
+++ src/Nethermind/Nethermind.Facade/Simulate/SimulateTransactionProcessorAdapter.cs
@@ -7,15 +7,18 @@ using Nethermind.Crypto;
 using Nethermind.Evm;
 using Nethermind.Evm.Tracing;
 using Nethermind.Evm.TransactionProcessing;
+using Nethermind.Logging;
 
 namespace Nethermind.Facade.Simulate;
 
-public class SimulateTransactionProcessorAdapter(ITransactionProcessor transactionProcessor, SimulateRequestState simulateRequestState) : ITransactionProcessorAdapter
+public class SimulateTransactionProcessorAdapter(ITransactionProcessor transactionProcessor, SimulateRequestState simulateRequestState, ILogManager logManager) : ITransactionProcessorAdapter
 {
     private int _currentTxIndex = 0;
+    private readonly ILogger _logger = logManager.GetClassLogger();
     public TransactionResult Execute(Transaction transaction, ITxTracer txTracer)
     {
         // The gas limit per tx go down as the block is processed.
+        _logger.Error($"GAS INFO: {simulateRequestState.TxsWithExplicitGas[_currentTxIndex]}, transaction: {transaction.GasLimit}, idx: {_currentTxIndex}, left: {simulateRequestState.TotalGasLeft}");
         if (!simulateRequestState.TxsWithExplicitGas[_currentTxIndex])
         {
             transaction.GasLimit = simulateRequestState.TotalGasLeft;
