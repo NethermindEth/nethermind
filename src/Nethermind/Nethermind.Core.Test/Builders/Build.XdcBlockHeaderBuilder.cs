02e1e859e2b5528b1bf49676d1c7bf25570eda1e
ak88 <anders@nethermind.io>
2025-09-04 14:36:46 +0200

    Removed xdc ref in core (#9242)
    removed xdc ref in core

--- src/Nethermind/Nethermind.Core.Test/Builders/Build.XdcBlockHeaderBuilder.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
-// SPDX-License-Identifier: LGPL-3.0-only
-
-namespace Nethermind.Core.Test.Builders;
-
-public partial class Build
-{
-    public XdcBlockHeaderBuilder XdcBlockHeader => new();
-}
