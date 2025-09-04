8d25f60c08443af35bcfa9984afd8ea732a93011
Alexey Osipov <me@flcl.me>
2025-09-04 11:37:30 +0300

    Fix a csproj

--- src/Nethermind/Ethereum.Transaction.Test/TransactionJsonTest.cs
+++ src/Nethermind/Ethereum.Transaction.Test/TransactionJsonTest.cs
@@ -9,31 +9,30 @@ using Nethermind.Int256;
 using Nethermind.Serialization.Json;
 using NUnit.Framework;
 
-namespace Ethereum.Blockchain.Test
+namespace Ethereum.Blockchain.Test;
+
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class TransactionJsonTest : GeneralStateTestBase
 {
-    [TestFixture]
-    [Parallelizable(ParallelScope.All)]
-    public class TransactionJsonTest : GeneralStateTestBase
+    [Test]
+    public void Can_load_access_lists()
     {
-        [Test]
-        public void Can_load_access_lists()
-        {
-            const string lists =
-                "{\"accessLists\": [[{\"address\": \"0x0001020304050607080900010203040506070809\", \"storageKeys\": [\"0x00\", \"0x01\"]}]]}";
+        const string lists =
+            "{\"accessLists\": [[{\"address\": \"0x0001020304050607080900010203040506070809\", \"storageKeys\": [\"0x00\", \"0x01\"]}]]}";
 
-            EthereumJsonSerializer serializer = new EthereumJsonSerializer();
-            TransactionJson txJson = serializer.Deserialize<TransactionJson>(lists);
-            txJson.SecretKey = TestItem.PrivateKeyA.KeyBytes;
-            txJson.Value = new UInt256[1];
-            txJson.GasLimit = new long[1];
-            txJson.Data = new byte[1][];
-            txJson.AccessLists.Should().NotBeNull();
-            txJson.AccessLists[0][0].Address.Should()
-                .BeEquivalentTo(new Address("0x0001020304050607080900010203040506070809"));
-            txJson.AccessLists[0][0].StorageKeys[1][0].Should().Be((byte)1);
+        EthereumJsonSerializer serializer = new EthereumJsonSerializer();
+        TransactionJson txJson = serializer.Deserialize<TransactionJson>(lists);
+        txJson.SecretKey = TestItem.PrivateKeyA.KeyBytes;
+        txJson.Value = new UInt256[1];
+        txJson.GasLimit = new long[1];
+        txJson.Data = new byte[1][];
+        txJson.AccessLists.Should().NotBeNull();
+        txJson.AccessLists[0][0].Address.Should()
+            .BeEquivalentTo(new Address("0x0001020304050607080900010203040506070809"));
+        txJson.AccessLists[0][0].StorageKeys[1][0].Should().Be((byte)1);
 
-            Transaction tx = JsonToEthereumTest.Convert(new PostStateJson { Indexes = new IndexesJson() }, txJson);
-            tx.AccessList.Should().NotBeNull();
-        }
+        Nethermind.Core.Transaction tx = JsonToEthereumTest.Convert(new PostStateJson { Indexes = new IndexesJson() }, txJson);
+        tx.AccessList.Should().NotBeNull();
     }
 }
