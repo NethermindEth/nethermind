810de07d8d4cbb8d3140f9258854e7df81362dbe
Alexey Osipov <me@flcl.me>
2025-09-09 10:43:25 +0300

    Update to 5.0.0

--- src/Nethermind/Ethereum.Blockchain.Pyspec.Test/Constants.cs
+++ src/Nethermind/Ethereum.Blockchain.Pyspec.Test/Constants.cs
@@ -5,6 +5,6 @@ namespace Ethereum.Blockchain.Pyspec.Test;
 public class Constants
 {
     public const string ARCHIVE_URL_TEMPLATE = "https://github.com/ethereum/execution-spec-tests/releases/download/{0}/{1}";
-    public const string DEFAULT_ARCHIVE_VERSION = "v4.5.0";
+    public const string DEFAULT_ARCHIVE_VERSION = "v5.0.0";
     public const string DEFAULT_ARCHIVE_NAME = "fixtures_develop.tar.gz";
 }
