0a50628241244911d3215a49adfe8cb0570b8526
ak88 <anders.holmbjerg@hotmail.com>
2025-09-09 09:23:58 +0200

    seal validators

---
+++ src/Nethermind/Nethermind.Xdc.Test/XdcSealValidatorTests.cs
@@ -0,0 +1,100 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using DotNetty.Codecs;
+using Nethermind.Consensus;
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Specs;
+using Nethermind.Core.Test.Builders;
+using Nethermind.Crypto;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nethermind.Xdc.Test;
+internal class XdcSealValidatorTests
+{
+    [Test]
+    public void ValidateSeal_NotXdcHeader_ThrowArgumentException()
+    {
+        XdcSealValidator validator = new XdcSealValidator(Substitute.For<ISnapshotManager>(), Substitute.For<ISpecProvider>());
+        BlockHeader header = new BlockHeader(
+            parentHash: Hash256.Zero,
+            unclesHash: Hash256.Zero,
+            beneficiary: Address.Zero,
+            difficulty: 0,
+            number: 0,
+            gasLimit: 0,
+            timestamp: 0,
+            extraData: Array.Empty<byte>());
+
+        Assert.That(() => validator.ValidateSeal(header), Throws.InstanceOf<ArgumentException>());
+    }
+
+    [Test]
+    public void ValidateSeal_ValidSeal_ReturnsTrue()
+    {
+        EthereumEcdsa ecdsa = new EthereumEcdsa(0);
+        XdcSealValidator validator = new XdcSealValidator(Substitute.For<ISnapshotManager>(), Substitute.For<ISpecProvider>());
+        XdcBlockHeader header = Build.A.XdcBlockHeader()
+        .TestObject;
+        header.Beneficiary = TestItem.AddressA;
+        header.Validator = ecdsa.Sign(TestItem.PrivateKeyA, header).BytesWithRecovery;
+
+        Assert.That(validator.ValidateSeal(header), Is.True);
+    }
+
+    public static IEnumerable<TestCaseData> InvalidSignatureCases()
+    {
+        XdcBlockHeader header =
+            Build.A.XdcBlockHeader()
+            .TestObject;
+        header.Beneficiary = TestItem.AddressA;
+        yield return new TestCaseData(header, new byte[0]);
+        yield return new TestCaseData(header, new byte[65]);
+        yield return new TestCaseData(header, new byte[66]);
+        byte[] extralongSignature = new byte[66];
+        var keyASig = new EthereumEcdsa(0).Sign(TestItem.PrivateKeyA, header).BytesWithRecovery;
+        keyASig.CopyTo(extralongSignature, 0);
+        yield return new TestCaseData(header, extralongSignature);
+        var keyBSig = new EthereumEcdsa(0).Sign(TestItem.PrivateKeyA, header).BytesWithRecovery;
+        yield return new TestCaseData(header, keyBSig);
+    }
+
+    [TestCaseSource(nameof(InvalidSignatureCases))]
+    public void ValidateSeal_SignatureIsInvalid_ReturnsFalse(XdcBlockHeader header, byte[] validatorSig)
+    {
+        EthereumEcdsa ecdsa = new EthereumEcdsa(0);
+        XdcSealValidator validator = new XdcSealValidator(Substitute.For<ISnapshotManager>(), Substitute.For<ISpecProvider>());
+        header.Validator = validatorSig;
+
+        Assert.That(validator.ValidateSeal(header), Is.False);
+    }
+
+    [Test]
+    public void ValidateParams_NotXdcHeader_ThrowArgumentException()
+    {
+        XdcSealValidator validator = new XdcSealValidator(Substitute.For<ISnapshotManager>(), Substitute.For<ISpecProvider>());
+        BlockHeader header = Build.A.BlockHeader.TestObject;
+
+        Assert.That(() => validator.ValidateSeal(header), Throws.InstanceOf<ArgumentException>());
+    }
+
+    [TestCase]
+    public void ValidateParams_()
+    {
+        EthereumEcdsa ecdsa = new EthereumEcdsa(0);
+        XdcSealValidator validator = new XdcSealValidator(Substitute.For<ISnapshotManager>(), Substitute.For<ISpecProvider>());
+
+        XdcBlockHeader parent = Build.A.XdcBlockHeader().TestObject;
+        XdcBlockHeader header = Build.A.XdcBlockHeader().TestObject;
+
+        Assert.That(validator.ValidateParams(parent, header, ), Is.False);
+    }
+
+}
--- src/Nethermind/Nethermind.Xdc/XdcSealValidator.cs
+++ src/Nethermind/Nethermind.Xdc/XdcSealValidator.cs
@@ -3,32 +3,120 @@
 
 using Nethermind.Consensus;
 using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Specs;
+using Nethermind.Crypto;
+using Nethermind.Serialization.Rlp;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nethermind.Xdc;
-internal class XdcSealValidator(ISnapshotManager snapshotManager) : ISealValidator
+internal class XdcSealValidator(ISnapshotManager snapshotManager, ISpecProvider specProvider) : ISealValidator
 {
+    private EthereumEcdsa _ethereumEcdsa = new(0); //Ignore chainId since we don't sign transactions here
+    private XdcHeaderDecoder _headerDecoder = new();
+
     public bool ValidateParams(BlockHeader parent, BlockHeader header, bool isUncle = false)
     {
-        throw new NotImplementedException();
+        return ValidateParams(parent, header, out _);
     }
 
+    public bool ValidateParams(BlockHeader parent, BlockHeader header, out string error)
+    {
+        if (header is not XdcBlockHeader xdcHeader)
+            throw new ArgumentException($"Only type of {nameof(XdcBlockHeader)} is allowed, but got type {header.GetType().Name}.", nameof(header));
+        if (xdcHeader.ExtraConsensusData() is null)
+        {
+            error = "ExtraData doesn't contain required consensus data.";
+            return false;
+        }
+
+        ImmutableSortedSet<Address> masternodes;
+
+        if (xdcHeader.IsEpochSwitch(specProvider))
+        {
+            if (xdcHeader.Nonce != XdcConstants.NonceDropVoteValue)
+            {
+                error = "Vote nonce in checkpoint block non-zero.";
+                return false;
+            }
+            if (xdcHeader.Validators is null || xdcHeader.Validators.Length == 0)
+            {
+                error = "Empty validators list on epoch switch block.";
+                return false;
+            }
+            if (xdcHeader.Validators.Length % Address.Size != 0)
+            {
+                error = "Invalid signer list on checkpoint block.";
+                return false;
+            }
+
+            //TODO init masternodes by reading from most recent checkpoint
+            masternodes = snapshotManager.GetMasternodes(xdcHeader);
+            if (!xdcHeader.ValidatorsAddress.SetEquals(masternodes))
+            {
+                error = "Validators does not match what's stored in snapshot minus its penalty.";
+                return false;
+            }
+
+            if (!xdcHeader.PenaltiesAddress.SetEquals(snapshotManager.GetPenalties(xdcHeader)))
+            {
+                error = "Penalties does not match.";
+                return false;
+            }
+        }
+        else
+        {
+            if (xdcHeader.Validators?.Length != 0)
+            {
+                error = "Validators are not empty in non-epoch switch header.";
+                return false;
+            }
+            if (xdcHeader.Penalties?.Length != 0)
+            {
+                error = "Penalties are not empty in non-epoch switch header.";
+                return false;
+            }
+            //TODO get masternodes from snapshot
+            masternodes = snapshotManager.GetMasternodes(xdcHeader);
+        }
+
+        if (!masternodes.Contains(header.Author))
+        {
+            error = "Block author is not in the masternode list.";
+            return false;
+        }
+
+        ulong currentLeaderIndex = (xdcHeader.ExtraConsensusData().Round % (ulong)specProvider.GetXdcSpec(xdcHeader).EpochLength % (ulong)masternodes.Count);
+        if (masternodes[(int)currentLeaderIndex] != header.Author)
+        {
+            error = "Block author is not the current leader.";
+            return false;
+        }
+
+        error = null;
+        return true;
+    }
+
+    public bool ValidateSeal(BlockHeader header) => ValidateSeal(header, false);
     public bool ValidateSeal(BlockHeader header, bool force)
     {
         if (header is not XdcBlockHeader xdcHeader)
             throw new ArgumentException($"Only type of {nameof(XdcBlockHeader)} is allowed, but got type {header.GetType().Name}.", nameof(header));
+
         if (header.Author is null)
-            return false;
+        {
+            if (xdcHeader.Validator is null
+                || xdcHeader.Validator.Length != 65
+                 //Passing an illegal y parity to syscall will cause a fatal error and program can crash
+                 || xdcHeader.Validator[64] >= 4)
+                return false;
 
-        ImmutableSortedSet<Address> masternodes = xdcHeader.ValidatorsAddress == null ? snapshotManager.GetMasternodes(xdcHeader) : xdcHeader.ValidatorsAddress; 
+            Address signer = _ethereumEcdsa.RecoverAddress(new Signature(xdcHeader.Validator.AsSpan(0, 64), xdcHeader.Validator[64]), Keccak.Compute(_headerDecoder.Encode(xdcHeader, RlpBehaviors.ForSealing).Bytes));
 
-        if (!masternodes.Contains(header.Author))
-            return false;
-        return true;
+            header.Author = signer;
+        }
+        return xdcHeader.Beneficiary == xdcHeader.Author;
     }
 }
