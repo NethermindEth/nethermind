8ae41602201ac91b5bd0820deb7a95f2a8282444
Lautaro Emanuel <emlautarom1@gmail.com>
2025-09-09 16:30:47 -0300

    Initial `rbuilder_transact` method

--- src/Nethermind/Nethermind.Flashbots/Modules/Rbuilder/IRbuilderRpcModule.cs
+++ src/Nethermind/Nethermind.Flashbots/Modules/Rbuilder/IRbuilderRpcModule.cs
@@ -1,11 +1,16 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Serialization;
 using Nethermind.Blockchain.Find;
+using Nethermind.Blockchain.Tracing.ParityStyle;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
+using Nethermind.Facade.Eth.RpcTransaction;
+using Nethermind.Facade.Proxy.Models.Simulate;
 using Nethermind.Int256;
 using Nethermind.JsonRpc;
 using Nethermind.JsonRpc.Modules;
@@ -40,8 +45,18 @@ public interface IRbuilderRpcModule
         IsSharable = true,
         ExampleResponse = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")]
     ResultWrapper<Hash256?> rbuilder_getBlockHash(BlockParameter block);
+
+    [JsonRpcMethod(IsImplemented = true,
+        Description = "Execute a call on top of the state trie at specified block given a set of changes.",
+        IsSharable = false,
+        ExampleResponse = "0xffff")]
+    ResultWrapper<IReadOnlyList<SimulateBlockResult<ParityLikeTxTrace>>> rbuilder_transact(
+        RevmTransaction revmTransaction, BundleState bundleState);
 }
 
+/// <remarks>
+/// https://github.com/NethermindEth/rbuilder/blob/e680a898c57f2626bf75f87f0859ff8772d444cc/crates/rbuilder/src/provider/ipc_state_provider.rs#L438
+/// </remarks>
 public class AccountChange
 {
     [JsonPropertyName("nonce")]
@@ -85,4 +100,166 @@ public class AccountState
 
     [JsonPropertyName("code_hash")]
     public Hash256 CodeHash { get; set; }
+}
+
+/// <remarks>
+/// See: https://github.com/bluealloy/revm/blob/0ca6564f02004976f533cacf8821fed09d801e0a/crates/context/src/tx.rs#L22
+/// </remarks>
+public sealed class RevmTransaction
+{
+    [JsonPropertyName("tx_type")]
+    public byte TxType { get; init; } = 0;
+
+    [JsonPropertyName("caller")]
+    public Address Caller { get; init; } = Address.Zero; // TODO: Verify default
+
+    [JsonPropertyName("gas_limit")]
+    public UInt64 GasLimit { get; init; } = 30_000_000;
+
+    [JsonPropertyName("gas_price")]
+    public UInt128 GasPrice { get; init; } = 0;
+
+    [JsonPropertyName("value")]
+    public UInt256 Value { get; init; } = 0;
+
+    [JsonPropertyName("data")]
+    public byte[] Data { get; init; } = [];
+
+    [JsonPropertyName("nonce")]
+    public UInt64 Nonce { get; init; } = 0;
+
+    [JsonPropertyName("chan_id")]
+    public UInt64 ChainId { get; init; } = 1; // Mainnet chain ID is 1
+
+    [JsonPropertyName("access_list")]
+    public AccessListForRpc AccessList { get; init; } = AccessListForRpc.Empty;
+
+    [JsonPropertyName("gas_priority_fee")]
+    public UInt128? GasPriorityFee { get; init; } = null;
+
+    [JsonPropertyName("blob_hashes")]
+    public byte[][]? BlobHashes { get; init; } = [];
+
+    [JsonPropertyName("max_fee_per_blob_gas")]
+    public UInt128 MaxFeePerBlobGas { get; init; } = 0;
+
+    [JsonPropertyName("authorization_list")]
+    public AuthorizationListForRpc AuthorizationList { get; init; } = [];
+
+    public Transaction ToTransaction()
+    {
+        // TODO: Dangerous casts
+        return new Transaction
+        {
+            Type = (TxType)TxType,
+            SenderAddress = Caller,
+            GasLimit = (long)GasLimit,
+            GasPrice = (ulong)GasPrice,
+            Value = Value,
+            Data = Data,
+            Nonce = Nonce,
+            ChainId = ChainId,
+            AccessList = AccessList.ToAccessList(),
+            DecodedMaxFeePerGas = (ulong?)GasPriorityFee ?? UInt256.Zero,
+            BlobVersionedHashes = BlobHashes,
+            MaxFeePerBlobGas = (ulong)MaxFeePerBlobGas,
+            AuthorizationList = AuthorizationList.ToAuthorizationList(),
+        };
+    }
+}
+
+
+/// <remarks>
+/// See: https://github.com/bluealloy/revm/blob/0ca6564f02004976f533cacf8821fed09d801e0a/crates/database/src/states/bundle_state.rs#L408
+/// </remarks>
+public sealed class BundleState
+{
+    [JsonPropertyName("state")]
+    public IReadOnlyDictionary<Address, BundleAccount> State { get; init; } = new Dictionary<Address, BundleAccount>();
+
+    [JsonPropertyName("contracts")]
+    public IReadOnlyDictionary<byte[], object> Contracts { get; init; } = new Dictionary<byte[], object>(); // TODO: Adjust types
+
+    [JsonPropertyName("reverts")]
+    public IReadOnlyList<object> Reverts { get; init; } = []; // TODO: Adjust type
+}
+
+/// <remarks>
+/// See: https://github.com/bluealloy/revm/blob/0ca6564f02004976f533cacf8821fed09d801e0a/crates/database/src/states/bundle_account.rs#L20
+/// </remarks>
+public sealed class BundleAccount
+{
+    [JsonPropertyName("info")]
+    public AccountInfo? Info { get; init; } = new();
+
+    [JsonPropertyName("original_info")]
+    public AccountInfo? OriginalInfo { get; init; } = new();
+
+    [JsonPropertyName("storage")]
+    public IReadOnlyDictionary<UInt256, StorageSlot> Storage { get; init; } = new Dictionary<UInt256, StorageSlot>();
+
+    [JsonPropertyName("account_status")]
+    public AccountStatus AccountStatus { get; init; }
+
+    public AccountChange ToAccountChange()
+    {
+        // TODO: We might want to do this on the `rbuilder` side.
+        // See: https://github.com/NethermindEth/rbuilder/blob/e680a898c57f2626bf75f87f0859ff8772d444cc/crates/rbuilder/src/provider/ipc_state_provider.rs#L453
+        return new AccountChange
+        {
+            ChangedSlots = Storage.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.PresentValue),
+            SelfDestructed = AccountStatus
+                is AccountStatus.Destroyed
+                or AccountStatus.DestroyedChanged
+                or AccountStatus.DestroyedAgain,
+            Balance = Info?.Balance,
+            Nonce = Info?.Nonce,
+            CodeHash = Info?.CodeHash,
+        };
+    }
+}
+
+/// <remarks>
+/// https://github.com/bluealloy/revm/blob/0ca6564f02004976f533cacf8821fed09d801e0a/crates/state/src/account_info.rs#L13
+/// </remarks>
+public sealed class AccountInfo
+{
+    [JsonPropertyName("balance")]
+    public UInt256 Balance { get; init; } = UInt256.Zero;
+
+    [JsonPropertyName("nonce")]
+    public UInt64 Nonce { get; init; } = 0;
+
+    [JsonPropertyName("code_hash")]
+    public Hash256 CodeHash { get; init; } = Hash256.Zero;
+
+    [JsonPropertyName("code")]
+    public byte[]? Code { get; init; } = null;
+}
+
+/// <remarks>
+/// See: https://github.com/bluealloy/revm/blob/0ca6564f02004976f533cacf8821fed09d801e0a/crates/database/src/states/account_status.rs#L19
+/// </remarks>
+public enum AccountStatus
+{
+    LoadedNotExisting,
+    Loaded,
+    LoadedEmptyEIP161,
+    InMemoryChange,
+    Changed,
+    Destroyed,
+    DestroyedChanged,
+    DestroyedAgain,
+}
+
+/// <remarks>
+/// See: https://github.com/bluealloy/revm/blob/0ca6564f02004976f533cacf8821fed09d801e0a/crates/database/src/states/plain_account.rs#L31
+/// </remarks>
+public sealed class StorageSlot
+{
+    [JsonPropertyName("previous_or_original_value")]
+    public UInt256 PreviousOrOriginalValue { get; init; } = UInt256.Zero;
+
+    [JsonPropertyName("present_value")]
+    public UInt256 PresentValue { get; init; } = UInt256.Zero;
 }
--- src/Nethermind/Nethermind.Flashbots/Modules/Rbuilder/RbuilderRpcModule.cs
+++ src/Nethermind/Nethermind.Flashbots/Modules/Rbuilder/RbuilderRpcModule.cs
@@ -5,19 +5,29 @@ using System;
 using System.Collections.Generic;
 using Nethermind.Blockchain;
 using Nethermind.Blockchain.Find;
+using Nethermind.Blockchain.Tracing.ParityStyle;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
 using Nethermind.Core.Specs;
 using Nethermind.Evm.State;
-using Nethermind.Evm.TransactionProcessing;
+using Nethermind.Facade;
+using Nethermind.Facade.Eth.RpcTransaction;
+using Nethermind.Facade.Proxy.Models.Simulate;
+using Nethermind.Facade.Simulate;
 using Nethermind.Int256;
 using Nethermind.JsonRpc;
+using Nethermind.JsonRpc.Modules.Eth;
 using Nethermind.State;
 
 namespace Nethermind.Flashbots.Modules.Rbuilder;
 
-public class RbuilderRpcModule(IBlockFinder blockFinder, ISpecProvider specProvider, IShareableTxProcessorSource txProcessorSource, IStateReader stateReader)
+public class RbuilderRpcModule(
+    IBlockFinder blockFinder,
+    ISpecProvider specProvider,
+    IShareableTxProcessorSource txProcessorSource,
+    IStateReader stateReader,
+    IBlockchainBridge blockchainBridge)
     : IRbuilderRpcModule
 {
 
@@ -129,4 +139,124 @@ public class RbuilderRpcModule(IBlockFinder blockFinder, ISpecProvider specProvi
         BlockHeader? blockHeader = blockFinder.FindHeader(block);
         return ResultWrapper<Hash256?>.Success(blockHeader?.Hash);
     }
+
+    public ResultWrapper<IReadOnlyList<SimulateBlockResult<ParityLikeTxTrace>>> rbuilder_transact(
+        RevmTransaction revmTransaction,
+        BundleState bundleState)
+    {
+        // 1. Apply diff on worldState
+        // 2. Construct correct objects and call SimulateTxExecutor.Execute / collect traces
+        // 3. Return the result
+
+        // TODO: Too many conversions here
+        TransactionForRpc transactionForRpc = TransactionForRpc.FromTransaction(revmTransaction.ToTransaction());
+
+        BlockParameter blockParameter = new BlockParameter(0L); // TODO: Use latest block?
+        BlockHeader? blockHeader = blockFinder.FindHeader(blockParameter);
+        if (blockHeader is null)
+        {
+            return ResultWrapper<IReadOnlyList<SimulateBlockResult<ParityLikeTxTrace>>>.Fail("Block not available", ErrorCodes.ResourceNotFound);
+        }
+
+        using IReadOnlyTxProcessingScope worldScope = txProcessorSource.Build(blockHeader);
+        IWorldState worldState = worldScope.WorldState;
+        IReleaseSpec releaseSpec = specProvider.GetSpec(blockHeader);
+
+        foreach (var (address, bundleAccount) in bundleState.State)
+        {
+            var accountChange = bundleAccount.ToAccountChange();
+
+            if (accountChange.SelfDestructed)
+            {
+                worldState.DeleteAccount(address);
+            }
+
+            bool hasAccountChange = accountChange.Balance is not null
+                                    || accountChange.Nonce is not null
+                                    || accountChange.CodeHash is not null
+                                    || accountChange.ChangedSlots?.Count > 0;
+            if (!hasAccountChange) continue;
+
+            if (worldState.TryGetAccount(address, out AccountStruct account))
+            {
+                // IWorldState does not actually have set nonce or set balance.
+                // Set, its either this or changing `IWorldState` which is somewhat risky.
+                if (accountChange.Nonce is not null)
+                {
+                    worldState.SetNonce(address, accountChange.Nonce.Value);
+                }
+
+                if (accountChange.Balance is not null)
+                {
+                    UInt256 originalBalance = account.Balance;
+                    if (accountChange.Balance.Value > originalBalance)
+                    {
+                        worldState.AddToBalance(address, accountChange.Balance.Value - originalBalance, releaseSpec);
+                    }
+                    else if (accountChange.Balance.Value == originalBalance)
+                    {
+                    }
+                    else
+                    {
+                        worldState.SubtractFromBalance(address, originalBalance - accountChange.Balance.Value, releaseSpec);
+                    }
+                }
+            }
+            else
+            {
+                worldState.CreateAccountIfNotExists(address, accountChange.Balance ?? 0, accountChange.Nonce ?? 0);
+            }
+
+            if (accountChange.CodeHash is not null)
+            {
+                // Note, this also set CodeDb, but since this is a read only world state, it should do nothing.
+                worldState.InsertCode(address, accountChange.CodeHash, Array.Empty<byte>(), releaseSpec);
+            }
+
+            if (accountChange.ChangedSlots is not null)
+            {
+                foreach (var (index, value) in accountChange.ChangedSlots)
+                {
+                    ReadOnlySpan<byte> bytes = value.ToBigEndian().WithoutLeadingZeros();
+                    worldState.Set(new StorageCell(address, index), bytes.ToArray());
+                }
+            }
+        }
+
+        worldState.Commit(releaseSpec);
+        worldState.CommitTree(blockHeader.Number + 1);
+
+        var executor = new SimulateTxExecutor<ParityLikeTxTrace>(
+            blockchainBridge,
+            blockFinder,
+            new JsonRpcConfig(), // TODO: Inject
+            new ParityStyleSimulateBlockTracerFactory(types: ParityTraceTypes.StateDiff));
+
+        var payload = new SimulatePayload<TransactionForRpc>
+        {
+            BlockStateCalls =
+            [
+                new()
+                {
+                    Calls = [transactionForRpc],
+                }
+            ],
+            // TODO: We might not need these
+            TraceTransfers = true,
+            Validation = true,
+            ReturnFullTransactionObjects = true,
+            ReturnFullTransactions = true
+        };
+
+        // TODO: Can we use the optional `stateOverride` parameter instead of manually applying changes?
+        var result = executor.Execute(payload, blockParameter);
+
+        return result;
+    }
 }
+
+/*
+TODO:
+
+- What is the difference between `Evm.AccountOverride` and `AccountChange`? (ask @asdacap)
+*/
