dcb254eb3f31e852455d9ca39249047be93ebb85
Amirul Ashraf <asdacap@gmail.com>
2025-09-09 14:15:33 +0800

    use account struct

--- src/Nethermind/Nethermind.State/WorldState.cs
+++ src/Nethermind/Nethermind.State/WorldState.cs
@@ -98,7 +98,7 @@ namespace Nethermind.State
             throw new InvalidOperationException($"{nameof(IWorldState)} must only be used within scope");
         }
 
-        public Account GetAccount(Address address)
+        public AccountStruct GetAccount(Address address)
         {
             DebugGuardInScope();
             return _stateProvider.GetAccount(address);
@@ -106,7 +106,7 @@ namespace Nethermind.State
 
         bool IAccountStateProvider.TryGetAccount(Address address, out AccountStruct account)
         {
-            account = _stateProvider.GetAccount(address).ToStruct();
+            account = _stateProvider.GetAccount(address);
             return !account.IsTotallyEmpty;
         }
 
