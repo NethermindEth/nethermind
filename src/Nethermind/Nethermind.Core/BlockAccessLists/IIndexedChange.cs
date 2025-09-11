2c5a9e905530e322c83353f5232e1ac161cc1103
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-09-11 16:14:36 +0100

    add IIndexedChange

---
+++ src/Nethermind/Nethermind.Core/BlockAccessLists/IIndexedChange.cs
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+namespace Nethermind.Core.BlockAccessLists;
+
+public interface IIndexedChange
+{
+    public ushort BlockAccessIndex { get; init; }
+}
