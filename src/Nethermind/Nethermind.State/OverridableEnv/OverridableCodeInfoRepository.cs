0363c4ed4a513b59d62bea05d62a3d7aa3310935
Nikita Meshcheriakov <root@nikitam.io>
2025-09-11 11:56:32 +0300

    always precompile

--- src/Nethermind/Nethermind.State/OverridableEnv/OverridableCodeInfoRepository.cs
+++ src/Nethermind/Nethermind.State/OverridableEnv/OverridableCodeInfoRepository.cs
@@ -26,9 +26,7 @@ public class OverridableCodeInfoRepository(ICodeInfoRepository codeInfoRepositor
     }
 
     public bool IsPrecompile(Address address, IReleaseSpec spec) =>
-        _codeOverwrites.TryGetValue(address, out ICodeInfo result)
-            ? result.IsPrecompile
-            : codeInfoRepository.IsPrecompile(address, spec);
+        codeInfoRepository.IsPrecompile(address, spec);
 
     public void InsertCode(IWorldState state, ReadOnlyMemory<byte> code, Address codeOwner, IReleaseSpec spec) =>
         codeInfoRepository.InsertCode(state, code, codeOwner, spec);
