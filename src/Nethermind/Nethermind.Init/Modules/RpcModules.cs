0bafcfae441c6e8261ca4bc3050229260c01a749
Amirul Ashraf <asdacap@gmail.com>
2025-09-11 10:30:48 +0800

    Slight simplification

--- src/Nethermind/Nethermind.Init/Modules/RpcModules.cs
+++ src/Nethermind/Nethermind.Init/Modules/RpcModules.cs
@@ -82,7 +82,7 @@ public class RpcModules(IJsonRpcConfig jsonRpcConfig) : Module
                 .AddScoped<IBlockchainBridge>((ctx) => ctx.Resolve<IBlockchainBridgeFactory>().CreateBlockchainBridge())
                     .AddSingleton<IFeeHistoryOracle, FeeHistoryOracle>()
                     .AddSingleton<IFilterStore, ITimerFactory, IJsonRpcConfig>((timerFactory, rpcConfig) => new FilterStore(timerFactory, rpcConfig.FiltersTimeout))
-                    .AddSingleton<IFilterManager, IFilterStore, IMainProcessingContext, ITxPool, ILogManager>((store, processingContext, txPool, logManager) => new FilterManager(store, processingContext, txPool, logManager))
+                    .AddSingleton<IFilterManager, FilterManager>()
                     .AddSingleton<ISimulateReadOnlyBlocksProcessingEnvFactory, SimulateReadOnlyBlocksProcessingEnvFactory>()
 
             // Proof
