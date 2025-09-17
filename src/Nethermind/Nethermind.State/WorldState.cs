afb11c2fd680b0d50f1a311de403dc791f1e2a12
Amirul Ashraf <asdacap@gmail.com>
2025-09-17 10:41:21 +0800

    Rename to update state root

--- src/Nethermind/Nethermind.State/WorldState.cs
+++ src/Nethermind/Nethermind.State/WorldState.cs
@@ -211,7 +211,7 @@ namespace Nethermind.State
         public void CommitTree(long blockNumber)
         {
             DebugGuardInScope();
-            _stateProvider.UpdateStorageRootIfNeeded();
+            _stateProvider.UpdateStateRootIfNeeded();
             _currentScope.Commit(blockNumber);
             _persistentStorageProvider.ClearStorageMap();
         }
