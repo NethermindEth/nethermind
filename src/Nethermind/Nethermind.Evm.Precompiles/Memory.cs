fd5cb5d625a7bb473111ba478c27a76ad182d160
lukasz.rozmej <lukasz.rozmej@gmail.com>
2025-09-21 21:21:19 +0200

    fix

--- src/Nethermind/Nethermind.Evm.Precompiles/Memory.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
-// SPDX-License-Identifier: LGPL-3.0-only
-
-namespace Nethermind.Evm.Precompiles;
-
-public static class Memory
-{
-    Process process = Process.GetProcessesByName("notepad")[0];
-}
