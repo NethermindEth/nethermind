3ba6f62f211043a131f00ed95a0bcf9329efd2d6
lukasz.rozmej <lukasz.rozmej@gmail.com>
2025-09-23 17:24:45 +0200

    Make eth_getBlobsV1 return UnsupportedFork post Osaka

---
+++ src/Nethermind/Nethermind.Merge.Plugin/MergeErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+namespace Nethermind.Merge.Plugin;
+
+public static class MergeErrorMessages
+{
+    public const string UnsupportedFork = "Unsupported fork";
+}
