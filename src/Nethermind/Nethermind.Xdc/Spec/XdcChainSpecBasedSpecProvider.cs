fa4ae646f07f9fcb941fe746f2fd51910ee01a03
Carmen Irene Cabrera Rodríguez <49727740+cicr99@users.noreply.github.com>
2025-09-24 14:40:07 +0200

    Xdc genesis spec (#9245)
    * add initial chain spec for xdc
    
    * implement spec provider
    
    * fix xdc address format
    
    * skip xdc config file in runner tests
    
    * implement xdc plugin
    
    * enable xdc runner smoke tests
    
    * add xdcHeaderValidator in the loading pipeline
    
    * add initial chain spec for xdc
    
    * implement spec provider
    
    * fix xdc address format
    
    * skip xdc config file in runner tests
    
    * enable xdc in runner smoke tests
    
    * refactor xdc releaseSpec and specProvider
    
    * format code and update test

---
+++ src/Nethermind/Nethermind.Xdc/Spec/XdcChainSpecBasedSpecProvider.cs
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Collections.Generic;
+using Nethermind.Logging;
+using Nethermind.Specs;
+using Nethermind.Specs.ChainSpecStyle;
+
+namespace Nethermind.Xdc.Spec;
+
+public class XdcChainSpecBasedSpecProvider(ChainSpec chainSpec,
+    XdcChainSpecEngineParameters chainSpecEngineParameters,
+    ILogManager logManager)
+    : ChainSpecBasedSpecProvider(chainSpec, logManager)
+{
+    protected override ReleaseSpec CreateEmptyReleaseSpec() => new XdcReleaseSpec();
+    protected override ReleaseSpec CreateReleaseSpec(ChainSpec chainSpec, long releaseStartBlock, ulong? releaseStartTimestamp = null)
+    {
+        var releaseSpec = (XdcReleaseSpec)base.CreateReleaseSpec(chainSpec, releaseStartBlock, releaseStartTimestamp);
+
+        releaseSpec.EpochLength = chainSpecEngineParameters.Epoch;
+        releaseSpec.SwitchEpoch = chainSpecEngineParameters.SwitchEpoch;
+        releaseSpec.SwitchBlock = chainSpecEngineParameters.SwitchBlock;
+        releaseSpec.V2Configs = chainSpecEngineParameters.V2Configs;
+
+        releaseSpec.ApplyV2Config(0);
+
+        return releaseSpec;
+    }
+
+}
