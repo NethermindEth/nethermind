69d545fc57978377b4327aac353cfaf29d382111
Alex Bespalov <alexb5dh@gmail.com>
2025-10-01 21:16:53 +0300

    Remove `RequireCommitWriteBatch`

--- src/Nethermind/Nethermind.Db/DbExtensions.cs
+++ src/Nethermind/Nethermind.Db/DbExtensions.cs
@@ -3,7 +3,6 @@
 
 using System.Collections.Generic;
 using System.Linq;
-using Nethermind.Core;
 using Nethermind.Core.Crypto;
 
 namespace Nethermind.Db
@@ -21,12 +20,5 @@ namespace Nethermind.Db
             return db[k];
         }
 
-        /// <summary>
-        /// Wraps regular <see cref="IWriteBatch"/> requiring explicit call to <see cref="IWriteBatch.Commit"/> for data submission.
-        /// </summary>
-        public static IWriteBatch RequireCommit(this IWriteBatch batch)
-        {
-            return new RequireCommitWriteBatch(batch);
-        }
     }
 }
