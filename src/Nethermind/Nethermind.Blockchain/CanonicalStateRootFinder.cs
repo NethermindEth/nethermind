c5426aec2b259f78411cbc4601da550de0f696c7
Amirul Ashraf <asdacap@gmail.com>
2025-10-02 08:26:47 +0800

    Canonical state root finder

---
+++ src/Nethermind/Nethermind.Blockchain/CanonicalStateRootFinder.cs
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.State.FlatCache;
+
+namespace Nethermind.Blockchain;
+
+public class CanonicalStateRootFinder(IBlockTree blockTree): ICanonicalStateRootFinder
+{
+    public Hash256? GetCanonicalStateRootAtBlock(long blockNumber)
+    {
+        BlockHeader? header = blockTree.FindHeader(blockNumber, BlockTreeLookupOptions.RequireCanonical);
+        return header?.StateRoot;
+    }
+}
