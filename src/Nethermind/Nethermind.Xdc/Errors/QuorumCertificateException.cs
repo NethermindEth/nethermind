4c9b4d283bf8b3fba82b453eb56632d1ddc8516b
ak88 <anders.holmbjerg@hotmail.com>
2025-10-02 15:43:26 +0200

    Test fix

--- src/Nethermind/Nethermind.Xdc/Errors/QuorumCertificateException.cs
+++ src/Nethermind/Nethermind.Xdc/Errors/QuorumCertificateException.cs
@@ -2,13 +2,14 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using Nethermind.Blockchain;
+using Nethermind.Xdc.Types;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nethermind.Xdc.Types;
+namespace Nethermind.Xdc.Errors;
 internal class QuorumCertificateException : BlockchainException
 {
     public QuorumCertificateException(QuorumCertificate certificate, string message) : base(message)
--- src/Nethermind/Nethermind.Xdc/Types/EpochSwitchInfo.cs
+++ src/Nethermind/Nethermind.Xdc/Types/EpochSwitchInfo.cs
@@ -6,9 +6,9 @@ using System.Collections.Generic;
 
 namespace Nethermind.Xdc.Types;
 
-public class EpochSwitchInfo(Address[] penalties, Address[] masternodes, BlockRoundInfo epochSwitchBlockInfo)
+public class EpochSwitchInfo(Address[] masternodes, Address[] penalties, BlockRoundInfo epochSwitchBlockInfo)
 {
-    public Address[] Penalties { get; set; } = penalties;
     public Address[] Masternodes { get; set; } = masternodes;
+    public Address[] Penalties { get; set; } = penalties;
     public BlockRoundInfo EpochSwitchBlockInfo { get; set; } = epochSwitchBlockInfo;
 }
