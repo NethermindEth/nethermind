08f958e6da5fc068bf6588e8fc93d0cf974ffe72
lukasz.rozmej <lukasz.rozmej@gmail.com>
2025-10-02 13:33:58 +0200

    Make TxPoolTests Parallelizable

---
+++ src/Nethermind/Nethermind.TxPool.Test/TxPoolTestsSetup.cs
@@ -0,0 +1,17 @@
+﻿// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using Nethermind.Crypto;
+using NUnit.Framework;
+
+namespace Nethermind.TxPool.Test;
+
+[SetUpFixture]
+public class TxPoolTestsSetup
+{
+    [OneTimeSetUp]
+    public void SetupFixture()
+    {
+        KzgPolynomialCommitments.InitializeAsync().Wait();
+    }
+}
