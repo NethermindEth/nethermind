fb94fd3c6a1bfc7f84111868e9a244531365f3a1
Nikita Meshcheriakov <root@nikitam.io>
2025-10-03 16:48:12 +0300

    Empty logs

--- src/Nethermind/Nethermind.Facade/Simulate/SimulateBlockValidationTransactionsExecutor.cs
+++ src/Nethermind/Nethermind.Facade/Simulate/SimulateBlockValidationTransactionsExecutor.cs
@@ -48,6 +48,10 @@ public class SimulateBlockValidationTransactionsExecutor(
         {
             currentGasUsedTotal += txReceipt.GasUsed;
             txReceipt.GasUsedTotal = currentGasUsedTotal;
+
+            // For some reason, the logs from geth when processing the block is missing but not in the output from tracer.
+            // this cause the receipt root to be different than us. So we simulate it here.
+            txReceipt.Logs = [];
         }
 
         block.Header.GasUsed = startingGasLeft - simulateState.TotalGasLeft;
