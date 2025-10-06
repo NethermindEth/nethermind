1a523ec310f0ba9dfc5e6b96dd11255d73c876e0
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2025-10-06 12:38:04 +0100

    XDC : QuorumCertificate Manager (#9294)
    * push draft implementation  of QC manager and other related components
    
    * arg null exception
    
    * fixes
    
    * refactor and fixes
    
    * refactor
    
    * refactor
    
    * Change signatures
    
    * bit of refactor
    
    * cleanup
    
    * merged master
    
    * Test
    
    * format
    
    * fixes
    
    * format
    
    * format
    
    * persist when committing QC
    
    * merge conflicts
    
    * review comments
    
    * Test fix
    
    * merge fix
    
    ---------
    
    Co-authored-by: ak88 <anders.holmbjerg@hotmail.com>

---
+++ src/Nethermind/Nethermind.Xdc/XdcContext.cs
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using Nethermind.Blockchain;
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Xdc.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Org.BouncyCastle.Asn1.Cmp.Challenge;
+
+namespace Nethermind.Xdc;
+public class XdcContext
+{
+    public Address Leader { get; set; }
+    public int TimeoutCounter { get; set; }
+    public ulong CurrentRound { get; set; }
+    public ulong HighestSelfMindeRound { get; set; }
+    public ulong HighestVotedRound { get; set; }
+    public QuorumCertificate HighestQC { get; set; }
+    public QuorumCertificate LockQC { get; set; }
+    public TimeoutCert HighestTC { get; set; }
+    public BlockRoundInfo HighestCommitBlock { get; set; }
+    public bool IsInitialized { get; set; } = false;
+
+    public event Action<IBlockTree, ulong> NewRoundSetEvent;
+    internal void SetNewRound(IBlockTree chain, ulong round)
+    {
+        CurrentRound = round;
+        TimeoutCounter = 0;
+
+        // timer should be reset outside
+        NewRoundSetEvent.Invoke(chain, round);
+    }
+}
