2e1edbd1870cbab57cc424d1214f4b69dc6e5c96
Bilog WEB3 <155262265+Bilogweb3@users.noreply.github.com>
2025-10-06 15:30:21 +0200

    cleanup: Remove unused imports from AccountException

--- tools/SendBlobs/AccountException.cs
+++ tools/SendBlobs/AccountException.cs
@@ -2,11 +2,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SendBlobs;
 internal class AccountException : Exception
