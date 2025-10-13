957bb3d1687956bf72adb4e6f5e8bcfd550ebda8
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2025-10-13 17:13:10 +0200

    Update src/Nethermind/Nethermind.JsonRpc/IJsonRpcConfig.cs
    Co-authored-by: Ruben Buniatyan <rubo@users.noreply.github.com>

--- src/Nethermind/Nethermind.JsonRpc/IJsonRpcConfig.cs
+++ src/Nethermind/Nethermind.JsonRpc/IJsonRpcConfig.cs
@@ -53,7 +53,7 @@ public interface IJsonRpcConfig : IConfig
     [ConfigItem(Description = "The path to connect a UNIX domain socket over.")]
     string IpcUnixDomainSocketPath { get; set; }
 
-    [ConfigItem(Description = "Should Owner-only 600 (rw-------) access be applied to IPC socket file path.", DefaultValue = "true")]
+    [ConfigItem(Description = "Whether to set the IPC socket UNIX file permissions to owner-only (600).", DefaultValue = "true")]
     bool RestrictIpcSocketPermissions { get; set; }
 
     [ConfigItem(
