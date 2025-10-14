9f04712518eda34f8c906a93853ab1d935ae00cd
lukasz.rozmej <lukasz.rozmej@gmail.com>
2025-10-14 22:54:20 +0200

    Move RlpLimit to own file

---
+++ src/Nethermind/Nethermind.Serialization.Rlp/RlpLimit.cs
@@ -0,0 +1,53 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using System.Runtime.CompilerServices;
+using Nethermind.Core;
+using Nethermind.Core.Extensions;
+
+namespace Nethermind.Serialization.Rlp;
+
+public record struct RlpLimit(int Limit, string TypeName, ReadOnlyMemory<char> PropertyName)
+{
+    // We shouldn't allocate any single array bigger than 1M
+    public static readonly RlpLimit DefaultLimit = new((int)1.MiB(), "", ReadOnlyMemory<char>.Empty);
+    public static readonly RlpLimit Bloom = For<Bloom>("", Core.Bloom.ByteLength);
+    public static readonly RlpLimit L4 = new(4, "", ReadOnlyMemory<char>.Empty);
+    public static readonly RlpLimit L8 = new(8, "", ReadOnlyMemory<char>.Empty);
+    public static readonly RlpLimit L32 = new(32, "", ReadOnlyMemory<char>.Empty);
+    public static readonly RlpLimit L64 = new(65, "", ReadOnlyMemory<char>.Empty);
+    public static readonly RlpLimit L65 = new(65, "", ReadOnlyMemory<char>.Empty);
+
+    private string _collectionExpression;
+
+    public string CollectionExpression
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => _collectionExpression ??= GenerateCollectionExpression();
+    }
+
+    private string GenerateCollectionExpression() =>
+        PropertyName.IsEmpty
+            ? TypeName
+            : string.Concat(TypeName, ".", PropertyName.Span);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static RlpLimit For<T>(string propertyName, int limit) => new(limit, typeof(T).Name, propertyName.AsMemory());
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static RlpLimit For<T>(
+        object property,
+        int limit,
+        [CallerArgumentExpression("property")] string? expr = null)
+    {
+        int dotIndex = expr?.LastIndexOf('.') ?? -1;
+        ReadOnlyMemory<char> propertyName = dotIndex >= 0
+            ? expr![(dotIndex + 1)..].AsMemory()
+            : expr.AsMemory();
+
+        return new RlpLimit(limit, typeof(T).Name, propertyName);
+    }
+
+    public override string ToString() => CollectionExpression;
+}
--- src/Nethermind/Nethermind.Serialization.Rlp/RlpStream.cs
+++ src/Nethermind/Nethermind.Serialization.Rlp/RlpStream.cs
@@ -1235,49 +1235,4 @@ namespace Nethermind.Serialization.Rlp
         internal static ReadOnlySpan<byte> SingleBytes => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127];
         internal static readonly byte[][] SingleByteArrays = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [14], [15], [16], [17], [18], [19], [20], [21], [22], [23], [24], [25], [26], [27], [28], [29], [30], [31], [32], [33], [34], [35], [36], [37], [38], [39], [40], [41], [42], [43], [44], [45], [46], [47], [48], [49], [50], [51], [52], [53], [54], [55], [56], [57], [58], [59], [60], [61], [62], [63], [64], [65], [66], [67], [68], [69], [70], [71], [72], [73], [74], [75], [76], [77], [78], [79], [80], [81], [82], [83], [84], [85], [86], [87], [88], [89], [90], [91], [92], [93], [94], [95], [96], [97], [98], [99], [100], [101], [102], [103], [104], [105], [106], [107], [108], [109], [110], [111], [112], [113], [114], [115], [116], [117], [118], [119], [120], [121], [122], [123], [124], [125], [126], [127]];
     }
-
-    public record struct RlpLimit(int Limit, string TypeName, ReadOnlyMemory<char> PropertyName)
-    {
-        // We shouldn't allocate any single array bigger than 1M
-        public static readonly RlpLimit DefaultLimit = new((int)1.MiB(), "", ReadOnlyMemory<char>.Empty);
-        public static readonly RlpLimit Bloom = For<Bloom>("", Core.Bloom.ByteLength);
-        public static readonly RlpLimit L4 = new(4, "", ReadOnlyMemory<char>.Empty);
-        public static readonly RlpLimit L8 = new(8, "", ReadOnlyMemory<char>.Empty);
-        public static readonly RlpLimit L32 = new(32, "", ReadOnlyMemory<char>.Empty);
-        public static readonly RlpLimit L64 = new(65, "", ReadOnlyMemory<char>.Empty);
-        public static readonly RlpLimit L65 = new(65, "", ReadOnlyMemory<char>.Empty);
-
-        private string _collectionExpression;
-
-        public string CollectionExpression
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => _collectionExpression ??= GenerateCollectionExpression();
-        }
-
-        private string GenerateCollectionExpression() =>
-            PropertyName.IsEmpty
-                ? TypeName
-                : string.Concat(TypeName, ".", PropertyName.Span);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static RlpLimit For<T>(string propertyName, int limit) => new(limit, typeof(T).Name, propertyName.AsMemory());
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static RlpLimit For<T>(
-            object property,
-            int limit,
-            [CallerArgumentExpression("property")] string? expr = null)
-        {
-            int dotIndex = expr?.LastIndexOf('.') ?? -1;
-            ReadOnlyMemory<char> propertyName = dotIndex >= 0
-                ? expr![(dotIndex + 1)..].AsMemory()
-                : expr.AsMemory();
-
-            return new RlpLimit(limit, typeof(T).Name, propertyName);
-        }
-
-        public override string ToString() => CollectionExpression;
-    }
-
 }
