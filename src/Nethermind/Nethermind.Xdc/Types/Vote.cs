d55c1e987ecb9e7431617f0e2f55b5e1cb5a0c30
cicr99 <kr1000a@gmail.com>
2025-10-21 10:12:55 +0200

    implement pool for timeouts and votes

--- src/Nethermind/Nethermind.Xdc/Types/Vote.cs
+++ src/Nethermind/Nethermind.Xdc/Types/Vote.cs
@@ -1,18 +1,20 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using Nethermind.Core;
 using Nethermind.Core.Crypto;
-using System.Text.Json.Serialization;
+using RlpBehaviors = Nethermind.Serialization.Rlp.RlpBehaviors;
 
 namespace Nethermind.Xdc.Types;
 
-public class Vote(BlockRoundInfo proposedBlockInfo, ulong gapNumber, Signature signature = null)
+public class Vote(BlockRoundInfo proposedBlockInfo, ulong gapNumber, Signature signature = null) : IXdcPoolItem
 {
+    private readonly VoteDecoder _decoder = new();
     public BlockRoundInfo ProposedBlockInfo { get; set; } = proposedBlockInfo;
     public ulong GapNumber { get; set; } = gapNumber;
     public Signature? Signature { get; set; } = signature;
 
     public override string ToString() =>
         $"{ProposedBlockInfo.Round}:{GapNumber}:{ProposedBlockInfo.BlockNumber}";
+
+    public (ulong Round, Hash256 hash) PoolKey() => (ProposedBlockInfo.Round, Keccak.Compute(_decoder.Encode(this, RlpBehaviors.ForSealing).Bytes));
 }
---
+++ src/Nethermind/Nethermind.Xdc/XdcPool.cs
@@ -0,0 +1,81 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Collections.Generic;
+using System.Linq;
+using Nethermind.Core.Collections;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Threading;
+
+namespace Nethermind.Xdc;
+
+public class XdcPool<T> where T : IXdcPoolItem
+{
+    private readonly Dictionary<(ulong Round, Hash256 Hash), ArrayPoolList<T>> _items = new();
+    private readonly McsLock _lock = new();
+
+    public long Add(T item)
+    {
+        using var lockRelease = _lock.Acquire();
+        {
+            var key = item.PoolKey();
+            if (!_items.TryGetValue(key, out var list))
+            {
+                //128 should be enough to cover all master nodes and some extras
+                list = new ArrayPoolList<T>(128);
+                _items[key] = list;
+            }
+            if (!list.Contains(item))
+                list.Add(item);
+            return list.Count;
+        }
+    }
+
+    public void EndRound(ulong round)
+    {
+        using var lockRelease = _lock.Acquire();
+        {
+            foreach (var key in _items.Keys.ToArray())
+            {
+                if (key.Round <= round && _items.Remove(key, out ArrayPoolList<T> list))
+                {
+                    list?.Dispose();
+                }
+            }
+        }
+    }
+
+    public IReadOnlyCollection<T> GetItems(T item)
+    {
+        using var lockRelease = _lock.Acquire();
+        {
+            var key = item.PoolKey();
+            if (_items.TryGetValue(key, out ArrayPoolList<T> list))
+            {
+                //Allocating a new array since it goes outside the lock
+                return list.ToArray();
+            }
+            return [];
+        }
+    }
+
+    public long GetCount(T item)
+    {
+        using var lockRelease = _lock.Acquire();
+        {
+            var key = item.PoolKey();
+            if (_items.TryGetValue(key, out ArrayPoolList<T> list))
+            {
+                return list.Count;
+            }
+            return 0;
+        }
+    }
+}
+
+public interface IXdcPoolItem
+{
+    (ulong Round, Hash256 hash) PoolKey();
+}
