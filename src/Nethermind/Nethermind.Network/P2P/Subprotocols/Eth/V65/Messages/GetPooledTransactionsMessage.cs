8e632ce46ee225e140403ae125d844304cbfd980
Alexey Osipov <me@flcl.me>
2025-10-22 23:14:40 +0300

    Retry requesting pooled transactions (#9128)
    * PoC of delayed sending if requested
    
    * Add retry cache
    
    * Fix logs and multiple requests expiration
    
    * Add tests
    
    * Rename
    
    * Add more tests
    
    * Improve
    
    * Simplify dependencies
    
    * Improve
    
    * Rename
    
    * no requestoor draft
    
    * Use messages
    
    * Remove the requestoor
    
    * Fix tests
    
    * Fix tests
    
    * More fixes
    
    * Substitute
    
    * Disconnect peers with invalid txs
    
    * Update src/Nethermind/Nethermind.Network.Contract/Messages/IResourceRequestMessage.cs
    
    Co-authored-by: Lukasz Rozmej <lukasz.rozmej@gmail.com>
    
    * Compress code
    
    * Try pooled set
    
    * Handle as eth66
    
    * Fix review
    
    * Fix tests
    
    * No need in syncing handling
    
    * Remove redundant dispose
    
    * Review
    
    ---------
    
    Co-authored-by: Lukasz Rozmej <lukasz.rozmej@gmail.com>
    Co-authored-by: Ben {chmark} Adams <thundercat@illyriad.co.uk>

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V65/Messages/GetPooledTransactionsMessage.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V65/Messages/GetPooledTransactionsMessage.cs
@@ -1,16 +1,18 @@
-// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using Nethermind.Core;
 using Nethermind.Core.Collections;
 using Nethermind.Core.Crypto;
 
-namespace Nethermind.Network.P2P.Subprotocols.Eth.V65.Messages
-{
-    public class GetPooledTransactionsMessage(IOwnedReadOnlyList<Hash256> hashes) : HashesMessage(hashes)
-    {
-        public override int PacketType => Eth65MessageCode.GetPooledTransactions;
-        public override string Protocol => "eth";
+namespace Nethermind.Network.P2P.Subprotocols.Eth.V65.Messages;
 
-        public override string ToString() => $"{nameof(GetPooledTransactionsMessage)}({Hashes?.Count})";
-    }
+public class GetPooledTransactionsMessage(IOwnedReadOnlyList<Hash256> hashes) : HashesMessage(hashes), INew<IOwnedReadOnlyList<Hash256>, GetPooledTransactionsMessage>
+{
+    public override int PacketType => Eth65MessageCode.GetPooledTransactions;
+    public override string Protocol => "eth";
+
+    public static GetPooledTransactionsMessage New(IOwnedReadOnlyList<Hash256> arg) => new(arg);
+
+    public override string ToString() => $"{nameof(GetPooledTransactionsMessage)}({Hashes?.Count})";
 }
