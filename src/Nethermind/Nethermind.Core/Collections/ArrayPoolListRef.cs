b8fdb1711ec76e2198a399406ad1e1c6e76b1a77
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2025-10-22 14:42:35 +0200

    Update src/Nethermind/Nethermind.Core/Collections/ArrayPoolListRef.cs
    Co-authored-by: Ben {chmark} Adams <thundercat@illyriad.co.uk>

--- src/Nethermind/Nethermind.Core/Collections/ArrayPoolListRef.cs
+++ src/Nethermind/Nethermind.Core/Collections/ArrayPoolListRef.cs
@@ -84,7 +84,7 @@ public ref struct ArrayPoolListRef<T>
     public void Truncate(int newLength) => ArrayPoolListCore.Truncate(newLength, _array, ref _count);
     public readonly void Sort(Comparison<T> comparison) => ArrayPoolListCore.Sort(_array, _count, comparison);
     public readonly void Reverse() => ArrayPoolListCore.Reverse(_array, _count);
-    public ref T GetRef(int index) => ref ArrayPoolListCore.GetRef(_array, index, _count);
+    public readonly ref T GetRef(int index) => ref ArrayPoolListCore.GetRef(_array, index, _count);
     public Span<T> AsSpan() => _array.AsSpan(0, _count);
     public Memory<T> AsMemory() => new(_array, 0, _count);
     public ReadOnlyMemory<T> AsReadOnlyMemory() => new(_array, 0, _count);
