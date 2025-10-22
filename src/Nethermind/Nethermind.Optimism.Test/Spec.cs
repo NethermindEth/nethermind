09a0f9c1aac88bf25c61f276e04cfafefc2e3158
Alex Bespalov <alexb5dh@gmail.com>
2025-10-22 15:19:00 +0300

    Spec update

--- src/Nethermind/Nethermind.Optimism.Test/Spec.cs
+++ src/Nethermind/Nethermind.Optimism.Test/Spec.cs
@@ -17,13 +17,15 @@ public static class Spec
     public const ulong CanyonTimestamp = 1_300;
     public const ulong HoloceneTimeStamp = 2_000;
     public const ulong IsthmusTimeStamp = 2_100;
+    public const ulong JovianTimeStamp = 2_200;
 
     public static readonly IOptimismSpecHelper Instance =
         new OptimismSpecHelper(new OptimismChainSpecEngineParameters
         {
             CanyonTimestamp = CanyonTimestamp,
             HoloceneTimestamp = HoloceneTimeStamp,
-            IsthmusTimestamp = IsthmusTimeStamp
+            IsthmusTimestamp = IsthmusTimeStamp,
+            JovianTimestamp = JovianTimeStamp,
         });
 
     public static ISpecProvider BuildFor(BlockHeader header)
@@ -33,6 +35,7 @@ public static class Spec
         spec.IsOpHoloceneEnabled = Instance.IsHolocene(header);
         spec.IsOpGraniteEnabled = Instance.IsGranite(header);
         spec.IsOpIsthmusEnabled = Instance.IsIsthmus(header);
+        spec.IsOpJovianEnabled = Instance.IsJovian(header);
 
         var specProvider = Substitute.For<ISpecProvider>();
         specProvider.GetSpec(header).Returns(spec);
