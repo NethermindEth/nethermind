4bd692d586ae2ad3f0e9c18bd7953c80357d4165
Carmen Irene Cabrera Rodríguez <49727740+cicr99@users.noreply.github.com>
2025-10-23 16:54:56 +0200

    Xdc Pool for timeouts and votes (#9521)
    * implement pool for timeouts and votes
    
    * remove unnecessary array allocation
    
    * merged
    
    ---------
    
    Co-authored-by: ak88 <anders.holmbjerg@hotmail.com>
    Co-authored-by: ak88 <anders@nethermind.io>

--- src/Nethermind/Nethermind.Xdc/Types/Timeout.cs
+++ src/Nethermind/Nethermind.Xdc/Types/Timeout.cs
@@ -12,12 +12,10 @@ namespace Nethermind.Xdc.Types;
 public class Timeout(ulong round, Signature? signature, ulong gapNumber) : IXdcPoolItem
 {
     private readonly TimeoutDecoder _decoder = new();
-
     public ulong Round { get; set; } = round;
     public Signature? Signature { get; set; } = signature;
     public ulong GapNumber { get; set; } = gapNumber;
     public Address? Signer { get; set; }
-
     public override string ToString() => $"{Round}:{GapNumber}";
     public (ulong Round, Hash256 hash) PoolKey() => (Round, Keccak.Compute(_decoder.Encode(this, RlpBehaviors.ForSealing).Bytes));
 }
