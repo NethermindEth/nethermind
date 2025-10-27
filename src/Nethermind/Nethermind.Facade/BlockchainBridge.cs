5c0fbb39226e50a8854f86d0ec6478629b3c70a2
Tanishq Jasoria <jasoriatanishq@gmail.com>
2025-10-27 18:58:30 +0530

    another fix

--- src/Nethermind/Nethermind.Facade/BlockchainBridge.cs
+++ src/Nethermind/Nethermind.Facade/BlockchainBridge.cs
@@ -132,13 +132,8 @@ namespace Nethermind.Facade
         {
             using var scope = processingEnv.BuildAndOverride(header, stateOverride);
             CallOutputTracer callOutputTracer = new();
-            GethLikeNativeTxTracer gethLikeNativeTxTracer = new NativeCallTracer(tx, GethTraceOptions.Default);
-            CompositeTxTracer txTracer = new CompositeTxTracer(callOutputTracer, gethLikeNativeTxTracer);
             TransactionResult tryCallResult = TryCallAndRestore(scope.Component, header, tx, false,
-                txTracer.WithCancellation(cancellationToken));
-
-            GethLikeTxTrace? x = gethLikeNativeTxTracer.BuildResult();
-            Console.WriteLine(JsonSerializer.Serialize(x, EthereumJsonSerializer.JsonOptions));
+                callOutputTracer.WithCancellation(cancellationToken));
 
             return new CallOutput
             {
