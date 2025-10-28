c8e4ee19a5167352f9ee9db08762ed32473bc115
Amirul Ashraf <asdacap@gmail.com>
2025-10-28 17:35:12 +0800

    Commit file

---
+++ src/Nethermind/Nethermind.Blockchain/ReorgDepthFinalizedStateProvider.cs
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Trie.Pruning;
+
+namespace Nethermind.Blockchain;
+
+public class ReorgDepthFinalizedStateProvider(IBlockTree blockTree): IFinalizedStateProvider
+{
+    public long FinalizedBlockNumber => blockTree.BestKnownNumber - Reorganization.MaxDepth;
+    public Hash256? GetFinalizedStateRootAt(long blockNumber)
+    {
+        BlockHeader? header = blockTree.FindHeader(blockNumber, BlockTreeLookupOptions.RequireCanonical);
+        return header?.StateRoot;
+    }
+}
