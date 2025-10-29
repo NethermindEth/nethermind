371f2fbacef20780b4390fa5838a36bf3f9b6b9e
sashass1315 <sashass1315@gmail.com>
2025-10-29 17:01:28 +0200

    remove attribute

--- src/Nethermind/Nethermind.Facade/Eth/RpcTransaction/AccessListForRpc.cs
+++ src/Nethermind/Nethermind.Facade/Eth/RpcTransaction/AccessListForRpc.cs
@@ -35,7 +35,6 @@ public class AccessListForRpc
         [JsonConverter(typeof(StorageCellIndexConverter))]
         public IEnumerable<UInt256>? StorageKeys { get; set; }
 
-        [JsonConstructor]
         public Item() { }
 
         public Item(Address address, IEnumerable<UInt256>? storageKeys)
