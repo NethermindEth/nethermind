8c068fd825da32852d88111ac0cf98b8b6cf8b10
Amirul Ashraf <asdacap@gmail.com>
2025-10-29 12:23:14 +0800

    Update test

--- src/Nethermind/Nethermind.Merge.Plugin.Test/MergeFinalizedStateProviderTests.cs
+++ src/Nethermind/Nethermind.Merge.Plugin.Test/MergeFinalizedStateProviderTests.cs
@@ -54,29 +54,122 @@ public class MergeFinalizedStateProviderTests
     }
 
     [Test]
-    public void FinalizedBlockNumber_AfterTransition_WithFinalizedHeader_ReturnsHeaderNumber()
+    public void FinalizedBlockNumber_AfterTransition_WithBlockTreeFinalizedHash_ReturnsHeaderNumber()
     {
         // Arrange
         long expectedBlockNumber = 200;
-        BlockHeader finalizedHeader = Build.A.BlockHeader.WithNumber(expectedBlockNumber).TestObject;
+        Hash256 finalizedHash = TestItem.KeccakA;
+        BlockHeader finalizedHeader = Build.A.BlockHeader.WithNumber(expectedBlockNumber).WithHash(finalizedHash).TestObject;
         _poSSwitcher.TransitionFinished.Returns(true);
-        _blockTree.FindHeader(BlockParameter.Finalized).Returns(finalizedHeader);
+        _blockTree.FinalizedHash.Returns(finalizedHash);
+        _blockTree.FindHeader(finalizedHash, BlockTreeLookupOptions.None).Returns(finalizedHeader);
 
         // Act
         long result = _provider.FinalizedBlockNumber;
 
         // Assert
         result.Should().Be(expectedBlockNumber);
-        _blockTree.Received(1).FindHeader(BlockParameter.Finalized);
+        _blockTree.Received(1).FindHeader(finalizedHash, BlockTreeLookupOptions.None);
     }
 
     [Test]
-    public void FinalizedBlockNumber_AfterTransition_WithoutFinalizedHeader_DelegatesToBaseProvider()
+    public void FinalizedBlockNumber_AfterTransition_WithBlockCacheFinalizedHash_ReturnsHeaderNumber()
+    {
+        // Arrange
+        long expectedBlockNumber = 250;
+        Hash256 finalizedHash = TestItem.KeccakB;
+        BlockHeader finalizedHeader = Build.A.BlockHeader.WithNumber(expectedBlockNumber).WithHash(finalizedHash).TestObject;
+        _poSSwitcher.TransitionFinished.Returns(true);
+        _blockTree.FinalizedHash.Returns((Hash256?)null);
+        _blockCacheService.FinalizedHash.Returns(finalizedHash);
+        _blockTree.FindHeader(finalizedHash).Returns(finalizedHeader);
+
+        // Act
+        long result = _provider.FinalizedBlockNumber;
+
+        // Assert
+        result.Should().Be(expectedBlockNumber);
+        _blockTree.Received(1).FindHeader(finalizedHash);
+    }
+
+    [Test]
+    public void FinalizedBlockNumber_AfterTransition_BlockCacheHasHigherNumber_ReturnsBlockCacheNumber()
+    {
+        // Arrange
+        long blockTreeBlockNumber = 200;
+        long blockCacheBlockNumber = 250;
+        Hash256 blockTreeHash = TestItem.KeccakA;
+        Hash256 blockCacheHash = TestItem.KeccakB;
+        BlockHeader blockTreeHeader = Build.A.BlockHeader.WithNumber(blockTreeBlockNumber).WithHash(blockTreeHash).TestObject;
+        BlockHeader blockCacheHeader = Build.A.BlockHeader.WithNumber(blockCacheBlockNumber).WithHash(blockCacheHash).TestObject;
+        
+        _poSSwitcher.TransitionFinished.Returns(true);
+        _blockTree.FinalizedHash.Returns(blockTreeHash);
+        _blockTree.FindHeader(blockTreeHash, BlockTreeLookupOptions.None).Returns(blockTreeHeader);
+        _blockCacheService.FinalizedHash.Returns(blockCacheHash);
+        _blockTree.FindHeader(blockCacheHash).Returns(blockCacheHeader);
+
+        // Act
+        long result = _provider.FinalizedBlockNumber;
+
+        // Assert
+        result.Should().Be(blockCacheBlockNumber);
+    }
+
+    [Test]
+    public void FinalizedBlockNumber_AfterTransition_BlockTreeHasHigherNumber_ReturnsBlockTreeNumber()
+    {
+        // Arrange
+        long blockTreeBlockNumber = 300;
+        long blockCacheBlockNumber = 250;
+        Hash256 blockTreeHash = TestItem.KeccakA;
+        Hash256 blockCacheHash = TestItem.KeccakB;
+        BlockHeader blockTreeHeader = Build.A.BlockHeader.WithNumber(blockTreeBlockNumber).WithHash(blockTreeHash).TestObject;
+        BlockHeader blockCacheHeader = Build.A.BlockHeader.WithNumber(blockCacheBlockNumber).WithHash(blockCacheHash).TestObject;
+        
+        _poSSwitcher.TransitionFinished.Returns(true);
+        _blockTree.FinalizedHash.Returns(blockTreeHash);
+        _blockTree.FindHeader(blockTreeHash, BlockTreeLookupOptions.None).Returns(blockTreeHeader);
+        _blockCacheService.FinalizedHash.Returns(blockCacheHash);
+        _blockTree.FindHeader(blockCacheHash).Returns(blockCacheHeader);
+
+        // Act
+        long result = _provider.FinalizedBlockNumber;
+
+        // Assert
+        result.Should().Be(blockTreeBlockNumber);
+    }
+
+    [Test]
+    public void FinalizedBlockNumber_AfterTransition_BlockCacheHeaderNotFound_UsesOnlyBlockTree()
+    {
+        // Arrange
+        long expectedBlockNumber = 200;
+        Hash256 blockTreeHash = TestItem.KeccakA;
+        Hash256 blockCacheHash = TestItem.KeccakB;
+        BlockHeader blockTreeHeader = Build.A.BlockHeader.WithNumber(expectedBlockNumber).WithHash(blockTreeHash).TestObject;
+        
+        _poSSwitcher.TransitionFinished.Returns(true);
+        _blockTree.FinalizedHash.Returns(blockTreeHash);
+        _blockTree.FindHeader(blockTreeHash, BlockTreeLookupOptions.None).Returns(blockTreeHeader);
+        _blockCacheService.FinalizedHash.Returns(blockCacheHash);
+        _blockTree.FindHeader(blockCacheHash).Returns((BlockHeader?)null);
+
+        // Act
+        long result = _provider.FinalizedBlockNumber;
+
+        // Assert
+        result.Should().Be(expectedBlockNumber);
+    }
+
+    [Test]
+    public void FinalizedBlockNumber_AfterTransition_NoFinalizedHeaders_DelegatesToBaseProvider()
     {
         // Arrange
         long expectedBlockNumber = 150;
         _poSSwitcher.TransitionFinished.Returns(true);
-        _blockTree.FindHeader(BlockParameter.Finalized).Returns((BlockHeader?)null);
+        _blockTree.FinalizedHash.Returns((Hash256?)null);
+        _blockCacheService.FinalizedHash.Returns((Hash256?)null);
         _baseFinalizedStateProvider.FinalizedBlockNumber.Returns(expectedBlockNumber);
 
         // Act
@@ -84,7 +177,6 @@ public class MergeFinalizedStateProviderTests
 
         // Assert
         result.Should().Be(expectedBlockNumber);
-        _blockTree.Received(1).FindHeader(BlockParameter.Finalized);
         _ = _baseFinalizedStateProvider.Received(1).FinalizedBlockNumber;
     }
 
