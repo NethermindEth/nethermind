032bb5fcf06704c208a2ad75e639443c7358bbee
Marcin Sobczak <marcindsobczak@gmail.com>
2025-10-31 15:34:05 +0100

    Mload

--- src/Nethermind/Nethermind.Evm/Instructions/EvmInstructions.Storage.cs
+++ src/Nethermind/Nethermind.Evm/Instructions/EvmInstructions.Storage.cs
@@ -229,7 +229,7 @@ internal static partial class EvmInstructions
     public static EvmExceptionType InstructionMLoad<TTracingInst>(VirtualMachine vm, ref EvmStack stack, ref long gasAvailable, ref int programCounter)
         where TTracingInst : struct, IFlag
     {
-        gasAvailable -= GasCostOf.VeryLow;
+        gasAvailable -= vm.Spec.IsEip7904Enabled ? GasCostOf.BaseOpcode : GasCostOf.VeryLow;
 
         // Pop the memory offset; if missing, signal a stack underflow.
         if (!stack.PopUInt256(out UInt256 result)) goto StackUnderflow;
