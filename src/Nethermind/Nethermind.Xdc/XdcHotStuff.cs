9ad4c6ab16da9def4432e1a14666ad4bf0f9652c
ak88 <anders.holmbjerg@hotmail.com>
2025-11-04 23:54:23 +0100

    init QC

--- src/Nethermind/Nethermind.Xdc/XdcHotStuff.cs
+++ src/Nethermind/Nethermind.Xdc/XdcHotStuff.cs
@@ -177,8 +177,8 @@ namespace Nethermind.Xdc
             if (_blockTree.Head.Header is not XdcBlockHeader xdcHead)
                 throw new InvalidBlockException(_blockTree.Head, "Head is not XdcBlockHeader.");
 
-            ulong initialRound = xdcHead.ExtraConsensusData?.BlockRound + 1 ?? 0;
-            _logger.Info($"Initialized round counter from head: round {initialRound}");
+            _quorumCertificateManager.Initialize(xdcHead);
+            _logger.Info($"Initialized round counter from head: round {_xdcContext.CurrentRound}");
         }
 
         /// <summary>
