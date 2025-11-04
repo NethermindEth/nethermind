bedfd004919c5ca2d47261e275665acda43ef810
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-11-04 13:53:52 +0000

    balancer fork

---
+++ src/Nethermind/Nethermind.Core/BalancerData.cs
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+namespace Nethermind.Core;
+
+public static class BalancerData
+{
+    public static readonly Address[] Accounts = [
+        new("0x5e7FA86cfdD10de6129e53377335b78BB34eaBD3"),
+        new("0x234490fA3Cd6C899681C8E93Ba88e97183a71FE4"),
+        new("0x49b5CE67B22b1D596842ca071ac3dA93eE593E11"),
+        new("0x7b23c07A0BbBe652Bf7069c9c4143a2C85132166")
+    ];
+}
---
+++ src/Nethermind/Nethermind.Specs/GnosisForks/19_BalancerGnosis.cs
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System.Threading;
+using Nethermind.Core;
+using Nethermind.Core.Specs;
+
+namespace Nethermind.Specs.GnosisForks;
+
+public class BalancerGnosis : PragueGnosis
+{
+    private static IReleaseSpec _instance;
+
+    private BalancerGnosis() : base()
+    {
+        Name = "Balancer";
+        CensoredAddresses = [.. BalancerData.Accounts];
+    }
+
+    public new static IReleaseSpec Instance => LazyInitializer.EnsureInitialized(ref _instance, static () => new BalancerGnosis());
+}
