4629af515d99b00237f9e3e879ef459e7cc5e8e0
Ruben Buniatyan <rubo@users.noreply.github.com>
2025-11-08 12:09:24 +0100

    Revise hive test scripts

--- scripts/hive-results.sh
+++ scripts/hive-results.sh
@@ -2,53 +2,48 @@
 # SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
 # SPDX-License-Identifier: LGPL-3.0-only
 
-knownFailingTests=$(cat nethermind/scripts/known-failing-hive-tests.txt)
-# in some test suits this test is a client setup and in some it's a master test. So just ignore it
-launchTestName='client launch (nethermind)'
+set -euo pipefail
 
-shouldNotPass=()
-shouldPass=()
+known_fails=$(cat ./nethermind/scripts/known-failing-hive-tests.txt)
+launch_test='client launch (nethermind)' # The launch test to ignore
+should_pass=()
+should_not_pass=()
 
-for passed in "true" "false"
-do
+for passed in "true" "false"; do
   tmp=()
-  mapfile tmp < <(jq '.testCases | map_values(select(.summaryResult.pass == $p)) | map(.name) | .[]' --argjson p "$passed" -r $1)
+  mapfile tmp < <(jq '.testCases
+    | map_values(select(.summaryResult.pass == $p))
+    | map(.name)
+    | .[]' \
+    --argjson p "$passed" -r $1)
   IFS=$'\n' results=($(sort -f <<<"${tmp[*]}")); unset IFS
 
-  if ($passed == "true")
-  then
-    echo -e "\nPassed: ${#results[@]}\n"
+  if [[ "$passed" == "true" ]]; then
+    echo -e "\nPassed ${#results[@]}:\n"
 
-    for each in "${results[@]}";
-    do
-      echo -e "\033[0;32m\u2714\033[0m $each"
-      if grep -qx "$each" <<< "$knownFailingTests" && [ "$each" != "$launchTestName" ]; then
-        shouldNotPass+=("$each")
+    for each in "${results[@]}"; do
+      if grep -Fqx "$each" <<< "$known_fails" && [[ "$each" != "$launch_test" ]]; then
+        should_not_pass+=("$each")
+        echo -e "\e[90m\u2714 $each\e[0m"
+      else
+        echo -e "\e[32m\u2714\e[0m $each"
       fi
     done
   else
-    echo -e "\nFailed: ${#results[@]}\n"
+    echo -e "\nFailed ${#results[@]}:\n"
 
-    for each in "${results[@]}";
-    do
-      echo -e "\033[0;31m\u2716\033[0m $each"
-      if ! grep -qx "$each" <<< "$knownFailingTests" && [ "$each" != "$launchTestName" ]; then
-        shouldPass+=("$each")
+    for each in "${results[@]}"; do
+      if ! grep -Fqx "$each" <<< "$known_fails" && [[ "$each" != "$launch_test" ]]; then
+        should_pass+=("$each")
+        echo -e "\e[31m\u2716\e[0m $each"
+      else
+        echo -e "\e[90m\u2716 $each\e[0m"
       fi
     done
   fi
 done
 
-if [ ${#shouldPass[@]} -gt 0 ]; then
-  echo -e "\nTests expected to pass but failed ${#shouldPass[@]}\n"
-  for each in "${shouldPass[@]}"; do echo -e "$each"; done
-fi
+echo -e "\n  Unexpected passes: ${#should_not_pass[@]}"
+echo "  Unexpected fails: ${#should_pass[@]}"
 
-if [ ${#shouldNotPass[@]} -gt 0 ]; then
-  echo -e "\nTests expected to fail but passed ${#shouldNotPass[@]}\n"
-  for each in "${shouldNotPass[@]}"; do echo -e "$each"; done
-fi
-
-if [[ ${#shouldNotPass[@]} -gt 0 || ${#shouldPass[@]} -gt 0 ]]; then
-  exit 1
-fi
+(( ${#should_pass[@]} + ${#should_not_pass[@]} > 0 )) && exit 1 || exit 0
