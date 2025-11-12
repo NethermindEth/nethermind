c66ef23aea4daff11307179b72b43ca0825c2f7f
lukasz.rozmej <lukasz.rozmej@gmail.com>
2025-11-12 22:52:15 +0100

    Make ArrayPoolListCore generic

--- src/Nethermind/Nethermind.Core/Collections/ArrayPoolListRef.cs
+++ src/Nethermind/Nethermind.Core/Collections/ArrayPoolListRef.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Buffers;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Nethermind.Core.Collections;
@@ -35,9 +33,9 @@ public ref struct ArrayPoolListRef<T>
 
     public readonly int Count => _count;
     public readonly int Capacity => _capacity;
-    public void Add(T item) => ArrayPoolListCore.Add(ArrayPool<T>.Shared, ref _array, ref _capacity, ref _count, item);
+    public void Add(T item) => ArrayPoolListCore<T>.Add(ArrayPool<T>.Shared, ref _array, ref _capacity, ref _count, item);
     public void AddRange(params T[] items) => AddRange(items.AsSpan());
-    public void AddRange(params ReadOnlySpan<T> items) => ArrayPoolListCore.AddRange(ArrayPool<T>.Shared, ref _array, ref _capacity, ref _count, items);
+    public void AddRange(params ReadOnlySpan<T> items) => ArrayPoolListCore<T>.AddRange(ArrayPool<T>.Shared, ref _array, ref _capacity, ref _count, items);
 
     public void AddRange(params IEnumerable<T> items)
     {
@@ -61,31 +59,31 @@ public ref struct ArrayPoolListRef<T>
         }
     }
 
-    public void Insert(int index, T item) => ArrayPoolListCore.Insert(ArrayPool<T>.Shared, ref _array, ref _capacity, ref _count, index, item);
-    public bool Remove(T item) => ArrayPoolListCore.Remove(_array, ref _count, item);
-    public T? RemoveLast() => ArrayPoolListCore.RemoveLast(_array, ref _count);
-    public void RemoveAt(int index) => ArrayPoolListCore.RemoveAt(_array, ref _count, index, shouldThrow: true);
-    public void Clear() => ArrayPoolListCore.Clear(_array, ref _count);
-    public void ReduceCount(int newCount) => ArrayPoolListCore.ReduceCount(ArrayPool<T>.Shared, ref _array, ref _capacity, ref _count, newCount);
-    public void Truncate(int newLength) => ArrayPoolListCore.Truncate(newLength, _array, ref _count);
-    public readonly void Sort(Comparison<T> comparison) => ArrayPoolListCore.Sort(_array, _count, comparison);
-    public readonly void Reverse() => ArrayPoolListCore.Reverse(_array, _count);
-    public readonly ref T GetRef(int index) => ref ArrayPoolListCore.GetRef(_array, index, _count);
+    public void Insert(int index, T item) => ArrayPoolListCore<T>.Insert(ArrayPool<T>.Shared, ref _array, ref _capacity, ref _count, index, item);
+    public bool Remove(T item) => ArrayPoolListCore<T>.Remove(_array, ref _count, item);
+    public T? RemoveLast() => ArrayPoolListCore<T>.RemoveLast(_array, ref _count);
+    public void RemoveAt(int index) => ArrayPoolListCore<T>.RemoveAt(_array, ref _count, index, shouldThrow: true);
+    public void Clear() => ArrayPoolListCore<T>.Clear(_array, ref _count);
+    public void ReduceCount(int newCount) => ArrayPoolListCore<T>.ReduceCount(ArrayPool<T>.Shared, ref _array, ref _capacity, ref _count, newCount);
+    public void Truncate(int newLength) => ArrayPoolListCore<T>.Truncate(newLength, _array, ref _count);
+    public readonly void Sort(Comparison<T> comparison) => ArrayPoolListCore<T>.Sort(_array, _count, comparison);
+    public readonly void Reverse() => ArrayPoolListCore<T>.Reverse(_array, _count);
+    public readonly ref T GetRef(int index) => ref ArrayPoolListCore<T>.GetRef(_array, index, _count);
     public readonly Span<T> AsSpan() => _array.AsSpan(0, _count);
     public readonly Memory<T> AsMemory() => new(_array, 0, _count);
     public readonly ReadOnlyMemory<T> AsReadOnlyMemory() => new(_array, 0, _count);
 
     public readonly T this[int index]
     {
-        get => ArrayPoolListCore.Get(_array, index, _count);
-        set => ArrayPoolListCore.Set(_array, index, _count, value);
+        get => ArrayPoolListCore<T>.Get(_array, index, _count);
+        set => ArrayPoolListCore<T>.Set(_array, index, _count, value);
     }
 
-    public void Dispose() => ArrayPoolListCore.Dispose(ArrayPool<T>.Shared, ref _array, ref _count, ref _capacity);
+    public void Dispose() => ArrayPoolListCore<T>.Dispose(ArrayPool<T>.Shared, ref _array, ref _count, ref _capacity);
     public readonly PooledArrayEnumerator<T> GetEnumerator() => new(_array, _count);
-    public readonly bool Contains(T item) => ArrayPoolListCore.Contains(_array, item, _count);
-    public readonly int IndexOf(T item) => ArrayPoolListCore.IndexOf(_array, _count, item);
-    public readonly void CopyTo(T[] array, int arrayIndex) => ArrayPoolListCore.CopyTo(_array, _count, array, arrayIndex);
+    public readonly bool Contains(T item) => ArrayPoolListCore<T>.Contains(_array, item, _count);
+    public readonly int IndexOf(T item) => ArrayPoolListCore<T>.IndexOf(_array, _count, item);
+    public readonly void CopyTo(T[] array, int arrayIndex) => ArrayPoolListCore<T>.CopyTo(_array, _count, array, arrayIndex);
 
     public readonly ArrayPoolListRef<TResult> Select<TResult>(Func<T, TResult> selector)
     {
