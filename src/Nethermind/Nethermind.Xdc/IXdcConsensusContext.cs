8276f9e423c1dc2afcf8501d4fedf4294783a407
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2025-11-13 13:38:29 +0100

    Xdc : Add XdcTestBlockchain for Module testing (#9596)
    * start
    
    * more dependencies
    
    * build
    
    * rename
    
    * voting
    
    * vote task
    
    * bit of refactor
    
    * invoke block produced
    
    dont seal
    
    * refactor context
    
    * format
    
    * timeout missing
    
    * block producer
    
    * format
    
    * basic incomplete setup
    
    * test
    
    * block production test
    
    * format
    
    * merge cleanup
    
    * merged
    
    * use the constant
    
    * use timestamp from attributes
    
    * Merged
    
    * Merged fix
    
    * cleanup
    
    * fixes
    
    * cleanup
    
    * commit cert before vote
    
    propose in background
    
    * merge and refactor
    
    * rename
    
    * format
    
    * comment
    
    * refactor to continuous loop
    
    * remove TCM
    
    * Format
    
    * fix
    
    * draft implmentation
    
    * cleanup
    
    * handle regenesis block in XdcTestBlockchain
    
    * refactor to inherit from TestBlockchain
    
    * register FromXdcContainer
    
    * handle _fromContainer from base
    
    * attempt rotating signers
    
    * test fix
    
    * test fix
    
    * test and container setup fix
    
    * decoder fix
    
    * bug fixes
    
    * init QC
    
    * test xdc sealer rotation
    
    * bug fixes and working now
    
    * Format
    
    * Update packages.lock.json
    
    * remove
    
    * total difficulty fix
    
    * format
    
    * override in blocktree
    
    * fix
    
    * unittest
    
    * format
    
    * fix build issues
    
    * fix
    
    * Fix
    
    * remove test
    
    * fixes
    
    * format
    
    * cleanup
    
    * undo merge and cleanup usings
    
    * add custom genesis builder for xdc
    
    * format whitespaces
    
    * Do not insert block if invalid fork
    
    * format
    
    * reorg test
    
    * revert block processor
    
    * revert cloning of header in PrepareBlockForProcessing
    
    * reorgs tests working
    
    * fix bad db
    
    * fix seal validator
    
    * fix
    
    * format
    
    * implement add block without commiting qc and generate extra keys for testing
    
    * fix and refactor
    
    * optional params
    
    * fix
    
    * one fix and a bit of refactor
    
    * reorg test
    
    * rename
    
    * return block instead
    
    * reorg test
    
    * fixes
    
    * dont set author
    
    * 10 seconds per add block
    
    * test blockchain changes
    
    * format
    
    * fixes for hotstuff module
    
    * fix test block producer
    
    * fixes for epoch master calculation
    
    * format
    
    * rename
    
    * rollback QC test blockchain order
    
    * snapshotmanager tests fixed
    
    * format
    
    * cleanup
    
    * remove TestBlockchainTester
    
    * cleanup
    
    * small fix in header validator test
    
    ---------
    
    Co-authored-by: ak88 <anders.holmbjerg@hotmail.com>
    Co-authored-by: cicr99 <kr1000a@gmail.com>

--- src/Nethermind/Nethermind.Xdc/IXdcConsensusContext.cs
+++ src/Nethermind/Nethermind.Xdc/IXdcConsensusContext.cs
@@ -1,10 +1,8 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using Nethermind.Blockchain;
 using Nethermind.Xdc.Types;
 using System;
-using System.Threading;
 
 namespace Nethermind.Xdc;
 public interface IXdcConsensusContext
