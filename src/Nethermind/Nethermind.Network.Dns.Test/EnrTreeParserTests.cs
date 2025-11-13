eea92e9081e3fe577ff126b6d2eaf63fc2d286d9
MozirDmitriy <dmitriymozir@gmail.com>
2025-11-13 18:29:46 +0300

    fix: EnrTreeParser tests to cover enr: leaves and enrtree:// links

--- src/Nethermind/Nethermind.Network.Dns.Test/EnrTreeParserTests.cs
+++ src/Nethermind/Nethermind.Network.Dns.Test/EnrTreeParserTests.cs
@@ -26,23 +26,32 @@ public class EnrTreeParserTests
         Assert.That(actual, Is.EqualTo(enrBranchText));
     }
 
-    [TestCase("enrtree-branch:TSVUMUTQU3AMKR36PNX4ILDJJI,VPN5OWLF7Q2PBBJUSOYKPQDGFE", 2)]
-    [TestCase("enrtree-branch:", 1)]
-    public void Can_parse_leaf(string enrBranchText, int hashCount)
+    [Test]
+    public void Can_parse_leaf()
     {
-        EnrTreeBranch branch = EnrTreeParser.ParseBranch(enrBranchText);
-        string actual = branch.ToString();
-        Assert.That(branch.Hashes.Length, Is.EqualTo(hashCount));
-        Assert.That(actual, Is.EqualTo(enrBranchText));
+        const string enr = "enr:-IS4QHCYrYZbAKWCBRlAy5zzaDZXJBGkcnh4MHcBFZntXNFrdvJjX04jRzjzCBOonrkTfj499SZuOh8R33Ls8RRcy5wBgmlkgnY0gmlwhH8AAAGJc2VjcDI1NmsxoQPKY0yuDUmstAHYpMa2_oxVtw0RW_QAdpzBQA8yWM0xOIN1ZHCCdl8";
+        EnrTreeNode node = EnrTreeParser.ParseNode(enr);
+        Assert.That(node, Is.TypeOf<EnrLeaf>());
+        EnrLeaf leaf = (EnrLeaf)node;
+        Assert.That(leaf.ToString(), Is.EqualTo(enr));
+        Assert.That(leaf.Links, Is.Empty);
+        Assert.That(leaf.Refs, Is.Empty);
+        Assert.That(leaf.Records.Length, Is.EqualTo(1));
+        Assert.That(leaf.Records[0], Is.EqualTo(enr));
+        Assert.That(leaf.NodeRecord, Is.EqualTo(enr.Substring(4)));
     }
 
-    [TestCase("enrtree-branch:TSVUMUTQU3AMKR36PNX4ILDJJI,VPN5OWLF7Q2PBBJUSOYKPQDGFE", 2)]
-    [TestCase("enrtree-branch:", 1)]
-    public void Can_parse_linked_tree(string enrBranchText, int hashCount)
+    [TestCase("enrtree://all.mainnet.ethdisco.net")]
+    [TestCase("enrtree://AKA3AM6LPBYEUDMVNU3BSVQJ5AD45Y7YPOHJLEF6W26QOE4VTUDPE@all.mainnet.ethdisco.net")]
+    public void Can_parse_linked_tree(string text)
     {
-        EnrTreeBranch branch = EnrTreeParser.ParseBranch(enrBranchText);
-        string actual = branch.ToString();
-        Assert.That(branch.Hashes.Length, Is.EqualTo(hashCount));
-        Assert.That(actual, Is.EqualTo(enrBranchText));
+        EnrTreeNode node = EnrTreeParser.ParseNode(text);
+        Assert.That(node, Is.TypeOf<EnrLinkedTree>());
+        EnrLinkedTree linked = (EnrLinkedTree)node;
+        Assert.That(linked.ToString(), Is.EqualTo(text));
+        Assert.That(linked.Links.Length, Is.EqualTo(1));
+        Assert.That(linked.Links[0], Is.EqualTo(text.Substring("enrtree://".Length)));
+        Assert.That(linked.Refs, Is.Empty);
+        Assert.That(linked.Records, Is.Empty);
     }
 }
