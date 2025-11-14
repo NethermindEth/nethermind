7171720fc601db56496e87dc3af92b017427b905
Carmen Irene Cabrera Rodríguez <49727740+cicr99@users.noreply.github.com>
2025-11-14 11:59:38 +0100

    XDC- Timeout Tests (#9698)
    * start
    
    * more dependencies
    
    * build
    
    * rename
    
    * voting
    
    * vote task
    
    * bit of refactor
    
    * invoke block produced
    
    dont seal
    
    * refactor context
    
    * format
    
    * timeout missing
    
    * block producer
    
    * format
    
    * basic incomplete setup
    
    * test
    
    * block production test
    
    * format
    
    * merge cleanup
    
    * merged
    
    * use the constant
    
    * use timestamp from attributes
    
    * Merged
    
    * Merged fix
    
    * cleanup
    
    * fixes
    
    * cleanup
    
    * commit cert before vote
    
    propose in background
    
    * merge and refactor
    
    * rename
    
    * format
    
    * comment
    
    * refactor to continuous loop
    
    * remove TCM
    
    * Format
    
    * fix
    
    * draft implmentation
    
    * cleanup
    
    * handle regenesis block in XdcTestBlockchain
    
    * refactor to inherit from TestBlockchain
    
    * register FromXdcContainer
    
    * handle _fromContainer from base
    
    * attempt rotating signers
    
    * test fix
    
    * test fix
    
    * test and container setup fix
    
    * decoder fix
    
    * bug fixes
    
    * init QC
    
    * test xdc sealer rotation
    
    * bug fixes and working now
    
    * Format
    
    * Update packages.lock.json
    
    * remove
    
    * total difficulty fix
    
    * format
    
    * override in blocktree
    
    * fix
    
    * unittest
    
    * format
    
    * fix build issues
    
    * fix
    
    * Fix
    
    * remove test
    
    * fixes
    
    * format
    
    * cleanup
    
    * undo merge and cleanup usings
    
    * add custom genesis builder for xdc
    
    * format whitespaces
    
    * Do not insert block if invalid fork
    
    * format
    
    * reorg test
    
    * revert block processor
    
    * revert cloning of header in PrepareBlockForProcessing
    
    * reorgs tests working
    
    * fix bad db
    
    * fix seal validator
    
    * fix
    
    * format
    
    * implement add block without commiting qc and generate extra keys for testing
    
    * fix and refactor
    
    * optional params
    
    * fix
    
    * one fix and a bit of refactor
    
    * reorg test
    
    * rename
    
    * return block instead
    
    * reorg test
    
    * fixes
    
    * dont set author
    
    * 10 seconds per add block
    
    * test blockchain changes
    
    * format
    
    * fixes for hotstuff module
    
    * fix test block producer
    
    * fixes for epoch master calculation
    
    * format
    
    * rename
    
    * rollback QC test blockchain order
    
    * implement timeout tests
    
    * snapshotmanager tests fixed
    
    * format
    
    * cleanup
    
    * remove TestBlockchainTester
    
    * cleanup
    
    * fix gap number calculation
    
    * refactor timeout tests
    
    * refactor test
    
    * format
    
    * build fix
    
    ---------
    
    Co-authored-by: ak88 <anders.holmbjerg@hotmail.com>
    Co-authored-by: Demuirgos <ayman.bouchareb@outlook.fr>
    Co-authored-by: Ayman Bouchareb <Ayman.Bouchareb@outlook.com>

--- src/Nethermind/Nethermind.Xdc.Test/XdcHeaderValidatorTests.cs
+++ src/Nethermind/Nethermind.Xdc.Test/XdcHeaderValidatorTests.cs
@@ -72,7 +72,7 @@ public class Tests
 
         //Invalid total difficulty
         blockHeaderBuilder = CreateValidHeader();
-        blockHeaderBuilder.WithDifficulty(1);
+        blockHeaderBuilder.WithDifficulty(2);
         blockHeaderBuilder.WithTotalDifficulty(1);
         yield return [blockHeaderBuilder, false];
 
