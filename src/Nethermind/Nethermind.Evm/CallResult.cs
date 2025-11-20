561f5d202629505d1d27525072c123659b73d14d
stavrosvl7 <stavrosvl7@gmail.com>
2025-11-20 12:51:39 +0200

    make constructor private

--- src/Nethermind/Nethermind.Evm/CallResult.cs
+++ src/Nethermind/Nethermind.Evm/CallResult.cs
@@ -52,12 +52,12 @@ public unsafe partial class VirtualMachine
             FromVersion = fromVersion;
         }
 
-        public CallResult(EvmExceptionType exceptionType)
+        private CallResult(EvmExceptionType exceptionType)
         {
             StateToExecute = null;
             Output = (null, StatusCode.FailureBytes);
             PrecompileSuccess = null;
-            ShouldRevert = exceptionType == EvmExceptionType.Revert;
+            ShouldRevert = false;
             ExceptionType = exceptionType;
         }
 
