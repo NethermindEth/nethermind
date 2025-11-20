e321be37f6f256b0e25cbe341312e56f4004cb53
ak88 <anders.holmbjerg@hotmail.com>
2025-11-20 12:06:04 +0100

    test

--- src/Nethermind/Nethermind.Xdc.Test/Contracts/XdcContractTests.cs
+++ src/Nethermind/Nethermind.Xdc.Test/Contracts/XdcContractTests.cs
@@ -13,11 +13,13 @@ using Nethermind.Core.Test;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Core.Test.Db;
 using Nethermind.Crypto;
+using Nethermind.Db;
 using Nethermind.Evm;
 using Nethermind.Evm.State;
 using Nethermind.Evm.Tracing;
 using Nethermind.Evm.TransactionProcessing;
 using Nethermind.Logging;
+using Nethermind.Network;
 using Nethermind.Specs;
 using Nethermind.Specs.Forks;
 using Nethermind.State;
@@ -34,40 +36,38 @@ using static Nethermind.Consensus.Processing.AutoReadOnlyTxProcessingEnvFactory;
 namespace Nethermind.Xdc.Test;
 internal class XdcContractTests
 {
-    private ISpecProvider _specProvider;
-    private ITransactionProcessor _transactionProcessor;
-    private IWorldState _worldStateForContract;
-    private IWorldState _worldState;
-
-    [SetUp]
-    public async Task Setup()
-    {
-        var dbProvider = await TestMemDbProvider.InitAsync();
-
-        _specProvider = new TestSpecProvider(Shanghai.Instance);
-        _worldState = TestWorldStateFactory.CreateWorldStateManagerForTest(dbProvider, NullLogManager.Instance).GlobalWorldState;
-        _worldStateForContract = TestWorldStateFactory.CreateWorldStateManagerForTest(dbProvider, NullLogManager.Instance).GlobalWorldState;
-        EthereumCodeInfoRepository codeInfoRepository = new(_worldState);
-        VirtualMachine virtualMachine = new(new TestBlockhashProvider(_specProvider), _specProvider, LimboLogs.Instance);
-        _transactionProcessor = new TransactionProcessor(BlobBaseFeeCalculator.Instance, _specProvider, _worldState, virtualMachine, codeInfoRepository, LimboLogs.Instance);
-    }
-
     [Test]
     public void MasternodeVotingContract()
     {
         PrivateKey sender = TestItem.PrivateKeyA;
         PrivateKey signer = TestItem.PrivateKeyB;
         Address codeSource = TestItem.AddressC;
+
+        ISpecProvider specProvider = new TestSpecProvider(Shanghai.Instance);
+        IDbProvider memDbProvider = TestMemDbProvider.Init();
+        IWorldState stateProvider = TestWorldStateFactory.CreateForTest(memDbProvider, LimboLogs.Instance);
+
+        IReleaseSpec finalSpec = specProvider.GetFinalSpec();
+
+        using (var _ = stateProvider.BeginScope(IWorldState.PreGenesis))
+        {
+            stateProvider.CreateAccount(sender.Address, 1.Ether());
+            byte[] code = XdcContractData.XDCValidatorBin();
+            stateProvider.CreateAccountIfNotExists(codeSource, 0);
+            stateProvider.InsertCode(codeSource, ValueKeccak.Compute(code), code, Shanghai.Instance);
+            stateProvider.Commit(specProvider.GenesisSpec);
+            stateProvider.CommitTree(0);
+
+            var genesis = Build.A.BlockHeader.WithStateRoot(stateProvider.StateRoot).TestObject;
+        }
+
+        EthereumCodeInfoRepository codeInfoRepository = new(stateProvider);
+        VirtualMachine virtualMachine = new(new TestBlockhashProvider(specProvider), specProvider, LimboLogs.Instance);
+        var transactionProcessor = new TransactionProcessor(BlobBaseFeeCalculator.Instance, specProvider, stateProvider, virtualMachine, codeInfoRepository, LimboLogs.Instance);
+
         //Save caller in storage slot 0
 
-        var masterVoting = new MasternodeVotingContract(_worldState, new AbiEncoder(), codeSource, new AutoReadOnlyTxProcessingEnv(_transactionProcessor, _worldState, Substitute.For<ILifetimeScope>()));
-
-        var scope = _worldState.BeginScope(IWorldState.PreGenesis);
-        _worldState.CreateAccount(sender.Address, 1.Ether());
-        byte[] code = XdcContractData.XDCValidatorBin();
-        _worldState.CreateAccountIfNotExists(codeSource, 0);
-        _worldState.InsertCode(codeSource, ValueKeccak.Compute(code), code, Shanghai.Instance);
-        _worldState.Commit (Shanghai.Instance, true);
+        var masterVoting = new MasternodeVotingContract(stateProvider, new AbiEncoder(), codeSource, new AutoReadOnlyTxProcessingEnv(transactionProcessor, stateProvider, Substitute.For<ILifetimeScope>()));
 
         var header = Build.A.BlockHeader.TestObject;
         var candidates = masterVoting.GetCandidates(header);
