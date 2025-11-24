709f1488cf43627a0e644a578416313cb171c84a
lukasz.rozmej <lukasz.rozmej@gmail.com>
2025-11-24 12:26:32 +0100

    whitespace

--- src/Nethermind/Nethermind.Consensus/Processing/BranchProcessor.cs
+++ src/Nethermind/Nethermind.Consensus/Processing/BranchProcessor.cs
@@ -82,8 +82,8 @@ public class BranchProcessor(
             // Start prewarming as early as possible
             WaitForCacheClear();
             IReleaseSpec spec = specProvider.GetSpec(suggestedBlock.Header);
-    	    preWarmTask = PreWarmTransactions(suggestedBlock, baseBlock!, spec, backgroundCancellation.Token);
-        	Task? prefetchBlockhash = blockhashProvider.Prefetch(suggestedBlock.Header, backgroundCancellation.Token);
+            preWarmTask = PreWarmTransactions(suggestedBlock, baseBlock!, spec, backgroundCancellation.Token);
+            Task? prefetchBlockhash = blockhashProvider.Prefetch(suggestedBlock.Header, backgroundCancellation.Token);
 
             BlocksProcessing?.Invoke(this, new BlocksProcessingEventArgs(suggestedBlocks));
 
