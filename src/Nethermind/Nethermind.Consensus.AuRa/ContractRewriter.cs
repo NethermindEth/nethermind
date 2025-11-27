2713522ffdd5574e6d4c2b19c738bbee77951b71
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-11-27 15:01:42 +0000

    overwrite code on timestamp

--- src/Nethermind/Nethermind.Consensus.AuRa/ContractRewriter.cs
+++ src/Nethermind/Nethermind.Consensus.AuRa/ContractRewriter.cs
@@ -8,25 +8,43 @@ using Nethermind.Evm.State;
 
 namespace Nethermind.Consensus.AuRa;
 
-public class ContractRewriter
+public class ContractRewriter(
+    IDictionary<long, IDictionary<Address, byte[]>> contractOverrides,
+    IDictionary<ulong, IDictionary<Address, byte[]>> contractOverridesTimestamp)
 {
-    private readonly IDictionary<long, IDictionary<Address, byte[]>> _contractOverrides;
-
-    public ContractRewriter(IDictionary<long, IDictionary<Address, byte[]>> contractOverrides)
-    {
-        _contractOverrides = contractOverrides;
-    }
+    private readonly IDictionary<long, IDictionary<Address, byte[]>> _contractOverrides = contractOverrides;
+    private readonly IDictionary<ulong, IDictionary<Address, byte[]>> _contractOverridesTimestamp = contractOverridesTimestamp;
 
     public bool RewriteContracts(long blockNumber, IWorldState stateProvider, IReleaseSpec spec)
     {
         bool result = false;
         if (_contractOverrides.TryGetValue(blockNumber, out IDictionary<Address, byte[]> overrides))
         {
-            foreach (KeyValuePair<Address, byte[]> contractOverride in overrides)
+            result = InsertOverwriteCode(overrides, stateProvider, spec);
+        }
+        return result;
+    }
+
+    public bool RewriteContracts(ulong timestamp, ulong parentTimestamp, IWorldState stateProvider, IReleaseSpec spec)
+    {
+        bool result = false;
+        foreach (KeyValuePair<ulong, IDictionary<Address, byte[]>> overrides in _contractOverridesTimestamp)
+        {
+            if (timestamp >= overrides.Key && parentTimestamp < overrides.Key)
             {
-                stateProvider.InsertCode(contractOverride.Key, contractOverride.Value, spec);
-                result = true;
+                result &= InsertOverwriteCode(overrides.Value, stateProvider, spec);
             }
+        }
+        return result;
+    }
+
+    private static bool InsertOverwriteCode(IDictionary<Address, byte[]> overrides, IWorldState stateProvider, IReleaseSpec spec)
+    {
+        bool result = false;
+        foreach (KeyValuePair<Address, byte[]> contractOverride in overrides)
+        {
+            stateProvider.InsertCode(contractOverride.Key, contractOverride.Value, spec);
+            result = true;
         }
         return result;
     }
