fdfd86952e24844e9e666291fcab3233b532099c
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-11-27 11:27:57 +0000

    forward cancellation token

--- src/Nethermind/Nethermind.History/HistoryPruner.cs
+++ src/Nethermind/Nethermind.History/HistoryPruner.cs
@@ -284,7 +284,7 @@ public class HistoryPruner : IHistoryPruner
                         throw;
                     }
                 }
-            });
+            }, cancellationToken);
         }
     }
 
