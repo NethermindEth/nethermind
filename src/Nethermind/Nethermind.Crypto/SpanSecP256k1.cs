4c7d638131e9b63b700f9440136b8aa49ea5abb9
Ben Adams <thundercat@illyriad.co.uk>
2025-11-28 02:17:32 +0000

    Remove defensive copies from SecP256k1

--- src/Nethermind/Nethermind.Crypto/SpanSecP256k1.cs
+++ src/Nethermind/Nethermind.Crypto/SpanSecP256k1.cs
@@ -3,61 +3,13 @@ using System;
 namespace Nethermind.Crypto;
 
 /// <summary>
-/// Span wrapper upon SecP256k1. Try to avoid allocations if given span is of Keccak size.
+/// Span wrapper upon SecP256k1.
 /// </summary>
 public static class SpanSecP256k1
 {
-    [ThreadStatic] private static byte[]? _signMessageHash;
-    [ThreadStatic] private static byte[]? _signPrivateKey;
-
     public static byte[]? SignCompact(ReadOnlySpan<byte> messageHash, ReadOnlySpan<byte> privateKey, out int recoveryId)
-    {
-        byte[] messageHashArray;
-        if (messageHash.Length == 32)
-        {
-            _signMessageHash ??= new byte[32];
-            messageHash.CopyTo(_signMessageHash);
-            messageHashArray = _signMessageHash;
-        }
-        else
-        {
-            // Why? Dont know...
-            messageHashArray = messageHash.ToArray();
-        }
-
-        byte[] privateKeyArray;
-        if (privateKey.Length == 32)
-        {
-            _signPrivateKey ??= new byte[32];
-            privateKey.CopyTo(_signPrivateKey);
-            privateKeyArray = _signPrivateKey;
-        }
-        else
-        {
-            // Why? Dont know...
-            privateKeyArray = privateKey.ToArray();
-        }
-
-        return SecP256k1.SignCompact(messageHashArray, privateKeyArray, out recoveryId);
-    }
-
-    [ThreadStatic] private static byte[]? _recoverMessageHash;
+        => SecP256k1.SignCompact(messageHash, privateKey, out recoveryId);
 
     public static bool RecoverKeyFromCompact(Span<byte> publicKey, ReadOnlySpan<byte> messageHash, ReadOnlySpan<byte> signature, int recoveryId, bool compressed)
-    {
-        byte[] messageHashArray;
-        if (messageHash.Length == 32)
-        {
-            _recoverMessageHash ??= new byte[32];
-            messageHash.CopyTo(_recoverMessageHash);
-            messageHashArray = _recoverMessageHash;
-        }
-        else
-        {
-            // Why? Dont know...
-            messageHashArray = messageHash.ToArray();
-        }
-
-        return SecP256k1.RecoverKeyFromCompact(publicKey, messageHashArray, signature, recoveryId, compressed);
-    }
+        => SecP256k1.RecoverKeyFromCompact(publicKey, messageHash, signature, recoveryId, compressed);
 }
