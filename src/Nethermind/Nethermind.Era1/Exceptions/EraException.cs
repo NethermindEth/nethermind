f12e610eb2d7d793cd85e463c8c4089b4ede16ef
bobtajson <152420524+bobtajson@users.noreply.github.com>
2025-11-30 19:37:04 +0100

    Delete src/Nethermind/Nethermind.Era1/Exceptions/EraException.cs

--- src/Nethermind/Nethermind.Era1/Exceptions/EraException.cs
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
-// SPDX-License-Identifier: LGPL-3.0-only
-
-namespace Nethermind.Era1;
-
-public class EraException(string message) : Exception(message);
