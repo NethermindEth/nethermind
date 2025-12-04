f805bdd33347eb23a3172163ebddc7139def04c2
Lautaro Emanuel <emlautarom1@gmail.com>
2025-12-04 11:42:24 -0300

    Fix Base Mainnet test

--- src/Nethermind/Nethermind.Optimism.Test/ForkInfoTests.cs
+++ src/Nethermind/Nethermind.Optimism.Test/ForkInfoTests.cs
@@ -1,17 +1,19 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using Nethermind.Specs;
+using Nethermind.Core.Crypto;
 using NUnit.Framework;
 
 namespace Nethermind.Optimism.Test;
 
 public class ForkInfoTests
 {
-    [TestCase(15_051_000, 1_764_691_201ul , "0x1cfeafc9", 0ul, "Jovian timestamp")]
+    private static readonly Hash256 BaseMainnetGenesis = new("0xf712aa9241cc24369b143cf6dce85f0902a9731e70d66818a3a5845b296c73dd");
+
+    [TestCase(38_950_927, 1_764_691_201ul , "0x1cfeafc9", 0ul, "Base Mainnet - Jovian")]
     public void Fork_id_and_hash_as_expected(long head, ulong headTimestamp, string forkHashHex, ulong next, string description)
     {
-        Network.Test.ForkInfoTests.Test(head, headTimestamp, KnownHashes.MainnetGenesis, forkHashHex, next, description,  "op-mainnet.json.zst");
+        Network.Test.ForkInfoTests.Test(head, headTimestamp, BaseMainnetGenesis, forkHashHex, next, description,  "base-mainnet.json.zst");
     }
 
 }
