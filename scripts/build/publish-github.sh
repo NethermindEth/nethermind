5e2b70bbf367bd97c3dbc52d5bb0cb0aa73a4464
Ruben Buniatyan <rubo@users.noreply.github.com>
2025-12-14 22:44:35 +0100

    Sign release builds

--- scripts/build/publish-github.sh
+++ scripts/build/publish-github.sh
@@ -16,9 +16,31 @@ should_publish=true
 if [[ -z "$release_id" ]]; then
   echo "Drafting release $GIT_TAG"
 
-  body=$(printf \
-    '{"tag_name": "%s", "target_commitish": "%s", "name": "v%s", "body": "# Release notes\\n\\n", "draft": true, "prerelease": %s}' \
-    $GIT_TAG $GITHUB_SHA $GIT_TAG $PRERELEASE)
+  relnotes=$(cat <<'EOF'
+# Release notes
+
+### [CONTENT PLACEHOLDER]
+
+#### Build Signatures
+
+The packages are signed with the following OpenPGP key: `AD12 7976 5093 C675 9CD8 A400 24A7 7461 6F1E 617E`
+EOF
+)
+
+  body=$(jq -n \
+    --arg tag "$GIT_TAG" \
+    --arg hash "$GITHUB_SHA" \
+    --arg name "v$GIT_TAG" \
+    --arg body "$relnotes" \
+    --argjson prerelease "$PRERELEASE" \
+    '{
+      tag_name: $tag,
+      target_commitish: $hash,
+      name: $name,
+      body: $body,
+      draft: true,
+      prerelease: $prerelease
+    }')
 
   release_id=$(curl https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
     -X POST \
@@ -32,9 +54,7 @@ fi
 
 cd $GITHUB_WORKSPACE/$PACKAGE_DIR
 
-for rid in "linux-arm64" "linux-x64" "macos-arm64" "macos-x64" "windows-x64" "ref-assemblies"; do
-  file_name=$(basename *$rid*)
-
+for file_name in *.zip *.zip.asc; do
   echo "Uploading $file_name"
 
   curl https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$release_id/assets?name=$file_name \
