7ce86957a745420f0b62987cb3a4805b2edb3fd9
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-12-15 15:30:43 +0000

    start aura check rewrite by timestamp count

--- src/Nethermind/Nethermind.Consensus.AuRa/InitializationSteps/StartBlockProducerAuRa.cs
+++ src/Nethermind/Nethermind.Consensus.AuRa/InitializationSteps/StartBlockProducerAuRa.cs
@@ -26,7 +26,6 @@ using Nethermind.Consensus.Transactions;
 using Nethermind.Consensus.Validators;
 using Nethermind.Consensus.Withdrawals;
 using Nethermind.Core;
-using Nethermind.Core.Crypto;
 using Nethermind.Core.Specs;
 using Nethermind.Crypto;
 using Nethermind.Evm.State;
@@ -159,7 +158,7 @@ public class StartBlockProducerAuRa(
 
         IDictionary<long, IDictionary<Address, byte[]>> rewriteBytecode = _parameters.RewriteBytecode;
         IDictionary<ulong, IDictionary<Address, byte[]>> rewriteBytecodeTimestamp = _parameters.RewriteBytecodeTimestamp;
-        ContractRewriter? contractRewriter = rewriteBytecode?.Count > 0 ? new ContractRewriter(rewriteBytecode, rewriteBytecodeTimestamp) : null;
+        ContractRewriter? contractRewriter = rewriteBytecode?.Count > 0 || rewriteBytecodeTimestamp?.Count > 0 ? new ContractRewriter(rewriteBytecode, rewriteBytecodeTimestamp) : null;
 
         var transactionExecutor = new BlockProcessor.BlockProductionTransactionsExecutor(
             new BuildUpTransactionProcessorAdapter(txProcessor),
