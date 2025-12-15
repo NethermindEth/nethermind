d679632dc87dc959eec7788e2ccddeeae7f04417
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2025-12-15 16:31:54 +0000

    private praguegnosis

--- src/Nethermind/Nethermind.Specs/GnosisForks/18_PragueGnosis.cs
+++ src/Nethermind/Nethermind.Specs/GnosisForks/18_PragueGnosis.cs
@@ -11,7 +11,7 @@ public class PragueGnosis : Prague
 {
     private static IReleaseSpec _instance;
 
-    protected PragueGnosis()
+    private PragueGnosis()
         => ToGnosisFork(this);
 
     public static void ToGnosisFork(Prague spec)
