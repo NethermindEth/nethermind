172006d168d78547fe648c20fdefe68e80f8ab62
Alexey Osipov <me@flcl.me>
2025-12-15 13:29:22 +0300

    Add engine_getBlobsV3

--- src/Nethermind/Nethermind.Merge.Plugin/EngineRpcModule.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/EngineRpcModule.cs
@@ -34,7 +34,7 @@ public partial class EngineRpcModule : IEngineRpcModule
         IHandler<TransitionConfigurationV1, TransitionConfigurationV1> transitionConfigurationHandler,
         IHandler<IEnumerable<string>, IEnumerable<string>> capabilitiesHandler,
         IAsyncHandler<byte[][], IEnumerable<BlobAndProofV1?>> getBlobsHandler,
-        IAsyncHandler<byte[][], IEnumerable<BlobAndProofV2>?> getBlobsHandlerV2,
+        IAsyncHandler<GetBlobsHandlerV2Request, IEnumerable<BlobAndProofV2?>?> getBlobsHandlerV2,
         IEngineRequestsTracker engineRequestsTracker,
         ISpecProvider specProvider,
         GCKeeper gcKeeper,
