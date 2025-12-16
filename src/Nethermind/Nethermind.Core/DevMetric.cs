87a1777552537ebe4d3c70008486d995fe276f58
Amirul Ashraf <asdacap@gmail.com>
2025-12-16 08:05:44 +0800

    Fix metric again

--- src/Nethermind/Nethermind.Core/DevMetric.cs
+++ src/Nethermind/Nethermind.Core/DevMetric.cs
@@ -1,12 +1,15 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using Prometheus;
 
 namespace Nethermind.Core;
 
 public static class DevMetric
 {
-    // public static MetricFactory Factory = Prometheus.Metrics.WithCustomRegistry(Prometheus.Metrics.NewCustomRegistry()); // Custom registry does not publish by default
-    public static MetricFactory Factory = Prometheus.Metrics.DefaultFactory;
+    public static MetricFactory Factory =
+        Environment.GetEnvironmentVariable("ASHRAF_DEV") != null
+            ? Prometheus.Metrics.DefaultFactory
+            : Prometheus.Metrics.WithCustomRegistry(Prometheus.Metrics.NewCustomRegistry());
 }
