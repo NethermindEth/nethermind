d599543249f500f8687a31246c0046124e1e591d
lukasz.rozmej <lukasz.rozmej@gmail.com>
2025-12-17 13:10:46 +0100

    fix test

--- src/Nethermind/Nethermind.Consensus.Test/Scheduler/BackgroundTaskSchedulerTests.cs
+++ src/Nethermind/Nethermind.Consensus.Test/Scheduler/BackgroundTaskSchedulerTests.cs
@@ -171,7 +171,7 @@ public class BackgroundTaskSchedulerTests
             scheduler.TryScheduleTask("test", async (_, _) => { await Task.Delay(10); });
         }
 
-        scheduler.GetStats()["test"].Should().Be(capacity);
+        scheduler.GetStats()["test"].Should().NotBeInRange(capacity, capacity + 1);
 
         await Task.Delay(40);
 
