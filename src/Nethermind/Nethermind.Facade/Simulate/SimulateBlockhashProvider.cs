0e7872eddbe86193febe99fe643a6b48407a0ac0
Nikita Meshcheriakov <root@nikitam.io>
2025-12-23 16:12:34 -0300

    Test

--- src/Nethermind/Nethermind.Facade/Simulate/SimulateBlockhashProvider.cs
+++ src/Nethermind/Nethermind.Facade/Simulate/SimulateBlockhashProvider.cs
@@ -8,15 +8,19 @@ using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Specs;
 using Nethermind.Evm;
+using Nethermind.Logging;
 
 namespace Nethermind.Facade.Simulate;
 
-public sealed class SimulateBlockhashProvider(IBlockhashProvider blockhashProvider, IBlockTree blockTree)
+public sealed class SimulateBlockhashProvider(IBlockhashProvider blockhashProvider, IBlockTree blockTree, ILogManager logManager)
     : IBlockhashProvider
 {
+    private ILogger _logger = logManager.GetClassLogger();
+
     public Hash256? GetBlockhash(BlockHeader currentBlock, long number, IReleaseSpec spec)
     {
         long bestKnown = blockTree.BestKnownNumber;
+        _logger.Error($"Call: {blockTree.GetType().FullName}. current: {currentBlock.Number}, number: {number}, bestKnown: {bestKnown}");
         return bestKnown < number && blockTree.BestSuggestedHeader is not null
             ? blockhashProvider.GetBlockhash(blockTree.BestSuggestedHeader!, number, spec)
             : blockhashProvider.GetBlockhash(currentBlock, number, spec);
