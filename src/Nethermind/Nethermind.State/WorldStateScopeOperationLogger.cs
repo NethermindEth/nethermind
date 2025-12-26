72ba1245374537575a79a7fa9dd75238582f2c7c
Amirul Ashraf <asdacap@gmail.com>
2025-12-26 19:53:12 +0800

    Update src/Nethermind/Nethermind.State/WorldStateScopeOperationLogger.cs
    Co-authored-by: Lukasz Rozmej <lukasz.rozmej@gmail.com>

--- src/Nethermind/Nethermind.State/WorldStateScopeOperationLogger.cs
+++ src/Nethermind/Nethermind.State/WorldStateScopeOperationLogger.cs
@@ -83,10 +83,7 @@ public class WorldStateScopeOperationLogger(IWorldStateScopeProvider baseScopePr
             return bytes;
         }
 
-        public void HintGet(in UInt256 index, byte[]? value)
-        {
-            storageTree.HintGet(in index, value);
-        }
+        public void HintGet(in UInt256 index, byte[]? value) => storageTree.HintGet(in index, value);
 
         public byte[] Get(in ValueHash256 hash)
         {
