bbc32878ea4cda744b5af215ea533ce24c297ca3
ak88 <anders.holmbjerg@hotmail.com>
2026-01-07 01:18:22 +0100

    merge fix

--- src/Nethermind/Nethermind.Xdc/XdcModule.cs
+++ src/Nethermind/Nethermind.Xdc/XdcModule.cs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System;
 using Autofac;
 using Autofac.Features.AttributeFilters;
 using Nethermind.Api.Steps;
@@ -16,22 +15,13 @@ using Nethermind.Consensus.Validators;
 using Nethermind.Core;
 using Nethermind.Core.Specs;
 using Nethermind.Db;
-using Nethermind.Evm;
-using Nethermind.Evm.State;
 using Nethermind.Init.Modules;
 using Nethermind.Network;
-using Nethermind.Network.P2P.Subprotocols.Eth.V62.Messages;
 using Nethermind.Serialization.Rlp;
 using Nethermind.Specs.ChainSpecStyle;
 using Nethermind.Xdc.P2P;
 using Nethermind.Xdc.Spec;
 using Nethermind.Xdc.Contracts;
-using Nethermind.Abi;
-using Nethermind.Evm.State;
-using Nethermind.State;
-using Nethermind.Xdc.Contracts;
-using Nethermind.Xdc.Spec;
-using Nethermind.Logging;
 
 namespace Nethermind.Xdc;
 
