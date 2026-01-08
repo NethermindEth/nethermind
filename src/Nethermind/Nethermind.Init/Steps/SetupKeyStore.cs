f4bdce9f57e829bd65b977ad1d5694b0ac74221f
Alexey Osipov <me@flcl.me>
2026-01-08 14:58:59 +0300

    Update src/Nethermind/Nethermind.Init/Steps/SetupKeyStore.cs
    Co-authored-by: Lukasz Rozmej <lukasz.rozmej@gmail.com>

--- src/Nethermind/Nethermind.Init/Steps/SetupKeyStore.cs
+++ src/Nethermind/Nethermind.Init/Steps/SetupKeyStore.cs
@@ -66,7 +66,7 @@ namespace Nethermind.Init.Steps
 
             get.LogManager.SetGlobalVariable("enode", enode.ToString());
 
-            if (!string.IsNullOrEmpty(networkConfig.Bootnodes))
+            if (!string.IsNullOrWhiteSpace(networkConfig.Bootnodes))
             {
                 string publicKeyHex = nodeKey.PublicKey.ToString();
                 string pattern = $",[^,]*{publicKeyHex}[^,]*|[^,]*{publicKeyHex}[^,]*,|^[^,]*{publicKeyHex}[^,]*$";
