c1ce586d01d3b9aac63c0eaae44d58abdeb353b1
Ben Adams <thundercat@illyriad.co.uk>
2026-01-12 15:50:49 +0000

    fix

--- src/Nethermind/Nethermind.Evm.Test/VmStateTests.cs
+++ src/Nethermind/Nethermind.Evm.Test/VmStateTests.cs
@@ -217,7 +217,7 @@ namespace Nethermind.Evm.Test
         public void Can_dispose_after_init()
         {
             VmState<EthereumGasPolicy> vmState = CreateEvmState();
-            vmState.InitializeStacks();
+            vmState.InitializeStacks(out _);
             vmState.Dispose();
         }
 
