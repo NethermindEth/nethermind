1a1fdb55d40d2b945099deb21b3d100a4bc82ff1
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2026-01-19 14:35:58 +0100

    Refactor ReleaseSpec (#10251)

--- src/Nethermind/Nethermind.Specs.Test/OverridableReleaseSpec.cs
+++ src/Nethermind/Nethermind.Specs.Test/OverridableReleaseSpec.cs
@@ -16,191 +16,109 @@ namespace Nethermind.Specs.Test
     public class OverridableReleaseSpec(IReleaseSpec spec) : IReleaseSpec
     {
         public string Name => "OverridableReleaseSpec";
-
-        public long MaximumExtraDataSize => spec.MaximumExtraDataSize;
-
-        public long MaxCodeSize => spec.MaxCodeSize;
-
-        public long MaxInitCodeSize => spec.MaxInitCodeSize;
-
-        public long MinGasLimit => spec.MinGasLimit;
-
-        public long MinHistoryRetentionEpochs => spec.MinHistoryRetentionEpochs;
-
-        public long GasLimitBoundDivisor => spec.GasLimitBoundDivisor;
-
-        private UInt256? _blockReward = spec.BlockReward;
-        public UInt256 BlockReward
-        {
-            get => _blockReward ?? spec.BlockReward;
-            set => _blockReward = value;
-        }
-
-        public long DifficultyBombDelay => spec.DifficultyBombDelay;
-
-        public long DifficultyBoundDivisor => spec.DifficultyBoundDivisor;
-
-        public long? FixedDifficulty => spec.FixedDifficulty;
-
-        public int MaximumUncleCount => spec.MaximumUncleCount;
-
-        public bool IsTimeAdjustmentPostOlympic => spec.IsTimeAdjustmentPostOlympic;
-
-        public bool IsEip2Enabled => spec.IsEip2Enabled;
-
-        public bool IsEip7Enabled => spec.IsEip7Enabled;
-
-        public bool IsEip100Enabled => spec.IsEip100Enabled;
-
-        public bool IsEip140Enabled => spec.IsEip140Enabled;
-
-        public bool IsEip150Enabled => spec.IsEip150Enabled;
-
-        public bool IsEip155Enabled => spec.IsEip155Enabled;
-
-        public bool IsEip158Enabled => spec.IsEip158Enabled;
-
-        public bool IsEip160Enabled => spec.IsEip160Enabled;
-
-        public bool IsEip170Enabled => spec.IsEip170Enabled;
-
-        public bool IsEip196Enabled => spec.IsEip196Enabled;
-
-        public bool IsEip197Enabled => spec.IsEip197Enabled;
-
-        public bool IsEip198Enabled => spec.IsEip198Enabled;
-
-        public bool IsEip211Enabled => spec.IsEip211Enabled;
-
-        public bool IsEip214Enabled => spec.IsEip214Enabled;
-
-        public bool IsEip649Enabled => spec.IsEip649Enabled;
-
-        public bool IsEip658Enabled => spec.IsEip658Enabled;
-
-        public bool IsEip145Enabled => spec.IsEip145Enabled;
-
-        public bool IsEip1014Enabled => spec.IsEip1014Enabled;
-
-        public bool IsEip1052Enabled => spec.IsEip1052Enabled;
-
-        public bool IsEip1283Enabled => spec.IsEip1283Enabled;
-
-        public bool IsEip1234Enabled => spec.IsEip1234Enabled;
-
-        public bool IsEip1344Enabled => spec.IsEip1344Enabled;
-
-        public bool IsEip2028Enabled => spec.IsEip2028Enabled;
-
-        public bool IsEip152Enabled => spec.IsEip152Enabled;
-
-        public bool IsEip1108Enabled => spec.IsEip1108Enabled;
-
-        public bool IsEip1884Enabled => spec.IsEip1884Enabled;
-
-        public bool IsEip2200Enabled => spec.IsEip2200Enabled;
-
-        public bool IsEip2537Enabled => spec.IsEip2537Enabled;
-
-        public bool IsEip2565Enabled => spec.IsEip2565Enabled;
-
-        public bool IsEip2929Enabled => spec.IsEip2929Enabled;
-
-        public bool IsEip2930Enabled => spec.IsEip2930Enabled;
-
-        public bool IsEip1559Enabled => spec.IsEip1559Enabled;
-        public bool IsEip3198Enabled => spec.IsEip3198Enabled;
-        public bool IsEip3529Enabled => spec.IsEip3529Enabled;
-
-        public bool IsEip3541Enabled => spec.IsEip3541Enabled;
-        public bool IsEip4844Enabled => spec.IsEip4844Enabled;
-        public bool IsEip7951Enabled => spec.IsEip7951Enabled;
-        public bool IsRip7212Enabled => spec.IsRip7212Enabled;
-        public bool IsOpGraniteEnabled => spec.IsOpGraniteEnabled;
-        public bool IsOpHoloceneEnabled => spec.IsOpHoloceneEnabled;
-        public bool IsOpIsthmusEnabled => spec.IsOpIsthmusEnabled;
-        public bool IsOpJovianEnabled => spec.IsOpJovianEnabled;
-
-        public bool IsEip7623Enabled => spec.IsEip7623Enabled;
-        public bool IsEip7918Enabled => spec.IsEip7918Enabled;
-
-        public bool IsEip7883Enabled => spec.IsEip7883Enabled;
-
-        public bool IsEip7934Enabled => spec.IsEip7934Enabled;
-        public int Eip7934MaxRlpBlockSize => spec.Eip7934MaxRlpBlockSize;
-        public bool ValidateChainId => spec.ValidateChainId;
+        public long MaximumExtraDataSize { get; set; } = spec.MaximumExtraDataSize;
+        public long MaxCodeSize { get; set; } = spec.MaxCodeSize;
+        public long MinGasLimit { get; set; } = spec.MinGasLimit;
+        public long MinHistoryRetentionEpochs { get; set; } = spec.MinHistoryRetentionEpochs;
+        public long GasLimitBoundDivisor { get; set; } = spec.GasLimitBoundDivisor;
+        public UInt256 BlockReward { get; set; } = spec.BlockReward;
+        public long DifficultyBombDelay { get; set; } = spec.DifficultyBombDelay;
+        public long DifficultyBoundDivisor { get; set; } = spec.DifficultyBoundDivisor;
+        public long? FixedDifficulty { get; set; } = spec.FixedDifficulty;
+        public int MaximumUncleCount { get; set; } = spec.MaximumUncleCount;
+        public bool IsTimeAdjustmentPostOlympic { get; set; } = spec.IsTimeAdjustmentPostOlympic;
+        public bool IsEip2Enabled { get; set; } = spec.IsEip2Enabled;
+        public bool IsEip7Enabled { get; set; } = spec.IsEip7Enabled;
+        public bool IsEip100Enabled { get; set; } = spec.IsEip100Enabled;
+        public bool IsEip140Enabled { get; set; } = spec.IsEip140Enabled;
+        public bool IsEip150Enabled { get; set; } = spec.IsEip150Enabled;
+        public bool IsEip155Enabled { get; set; } = spec.IsEip155Enabled;
+        public bool IsEip158Enabled { get; set; } = spec.IsEip158Enabled;
+        public bool IsEip160Enabled { get; set; } = spec.IsEip160Enabled;
+        public bool IsEip170Enabled { get; set; } = spec.IsEip170Enabled;
+        public bool IsEip196Enabled { get; set; } = spec.IsEip196Enabled;
+        public bool IsEip197Enabled { get; set; } = spec.IsEip197Enabled;
+        public bool IsEip198Enabled { get; set; } = spec.IsEip198Enabled;
+        public bool IsEip211Enabled { get; set; } = spec.IsEip211Enabled;
+        public bool IsEip214Enabled { get; set; } = spec.IsEip214Enabled;
+        public bool IsEip649Enabled { get; set; } = spec.IsEip649Enabled;
+        public bool IsEip658Enabled { get; set; } = spec.IsEip658Enabled;
+        public bool IsEip145Enabled { get; set; } = spec.IsEip145Enabled;
+        public bool IsEip1014Enabled { get; set; } = spec.IsEip1014Enabled;
+        public bool IsEip1052Enabled { get; set; } = spec.IsEip1052Enabled;
+        public bool IsEip1283Enabled { get; set; } = spec.IsEip1283Enabled;
+        public bool IsEip1234Enabled { get; set; } = spec.IsEip1234Enabled;
+        public bool IsEip1344Enabled { get; set; } = spec.IsEip1344Enabled;
+        public bool IsEip2028Enabled { get; set; } = spec.IsEip2028Enabled;
+        public bool IsEip152Enabled { get; set; } = spec.IsEip152Enabled;
+        public bool IsEip1108Enabled { get; set; } = spec.IsEip1108Enabled;
+        public bool IsEip1884Enabled { get; set; } = spec.IsEip1884Enabled;
+        public bool IsEip2200Enabled { get; set; } = spec.IsEip2200Enabled;
+        public bool IsEip2537Enabled { get; set; } = spec.IsEip2537Enabled;
+        public bool IsEip2565Enabled { get; set; } = spec.IsEip2565Enabled;
+        public bool IsEip2929Enabled { get; set; } = spec.IsEip2929Enabled;
+        public bool IsEip2930Enabled { get; set; } = spec.IsEip2930Enabled;
+        public bool IsEip1559Enabled { get; set; } = spec.IsEip1559Enabled;
+        public bool IsEip3198Enabled { get; set; } = spec.IsEip3198Enabled;
+        public bool IsEip3529Enabled { get; set; } = spec.IsEip3529Enabled;
+        public bool IsEip3541Enabled { get; set; } = spec.IsEip3541Enabled;
+        public bool IsEip4844Enabled { get; set; } = spec.IsEip4844Enabled;
+        public bool IsEip7951Enabled { get; set; } = spec.IsEip7951Enabled;
+        public bool IsRip7212Enabled { get; set; } = spec.IsRip7212Enabled;
+        public bool IsOpGraniteEnabled { get; set; } = spec.IsOpGraniteEnabled;
+        public bool IsOpHoloceneEnabled { get; set; } = spec.IsOpHoloceneEnabled;
+        public bool IsOpIsthmusEnabled { get; set; } = spec.IsOpIsthmusEnabled;
+        public bool IsOpJovianEnabled { get; set; } = spec.IsOpJovianEnabled;
+        public bool IsEip7623Enabled { get; set; } = spec.IsEip7623Enabled;
+        public bool IsEip7918Enabled { get; set; } = spec.IsEip7918Enabled;
+        public bool IsEip7883Enabled { get; set; } = spec.IsEip7883Enabled;
+        public bool IsEip7934Enabled { get; set; } = spec.IsEip7934Enabled;
+        public int Eip7934MaxRlpBlockSize { get; set; } = spec.Eip7934MaxRlpBlockSize;
+        public bool ValidateChainId { get; set; } = spec.ValidateChainId;
         public bool IsEip3607Enabled { get; set; } = spec.IsEip3607Enabled;
-
         public bool IsEip158IgnoredAccount(Address address) => spec.IsEip158IgnoredAccount(address);
-
-        private long? _overriddenEip1559TransitionBlock;
-        public long Eip1559TransitionBlock
-        {
-            get => _overriddenEip1559TransitionBlock ?? spec.Eip1559TransitionBlock;
-            set => _overriddenEip1559TransitionBlock = value;
-        }
-
-        private Address? _overriddenFeeCollector;
-        public Address? FeeCollector
-        {
-            get => _overriddenFeeCollector ?? spec.FeeCollector;
-            set => _overriddenFeeCollector = value;
-        }
-
-        private ulong? _overriddenEip4844TransitionTimestamp;
-
-        public ulong Eip4844TransitionTimestamp
-        {
-            get
-            {
-                return _overriddenEip4844TransitionTimestamp ?? spec.Eip4844TransitionTimestamp;
-            }
-            set
-            {
-                _overriddenEip4844TransitionTimestamp = value;
-            }
-        }
-
-        public ulong TargetBlobCount => spec.TargetBlobCount;
-        public ulong MaxBlobCount => spec.MaxBlobCount;
-        public ulong MaxBlobsPerTx => spec.MaxBlobsPerTx;
-        public UInt256 BlobBaseFeeUpdateFraction => spec.BlobBaseFeeUpdateFraction;
-        public bool IsEip1153Enabled => spec.IsEip1153Enabled;
-        public bool IsEip3651Enabled => spec.IsEip3651Enabled;
-        public bool IsEip3855Enabled => spec.IsEip3855Enabled;
-        public bool IsEip3860Enabled => spec.IsEip3860Enabled;
-        public bool IsEip4895Enabled => spec.IsEip4895Enabled;
-        public ulong WithdrawalTimestamp => spec.WithdrawalTimestamp;
-        public bool IsEip5656Enabled => spec.IsEip5656Enabled;
-        public long Eip2935RingBufferSize => spec.Eip2935RingBufferSize;
-        public bool IsEip6780Enabled => spec.IsEip6780Enabled;
-        public bool IsEip4788Enabled => spec.IsEip4788Enabled;
-        public bool IsEip4844FeeCollectorEnabled => spec.IsEip4844FeeCollectorEnabled;
-        public Address? Eip4788ContractAddress => spec.Eip4788ContractAddress;
-        public bool IsEip7002Enabled => spec.IsEip7002Enabled;
-        public Address Eip7002ContractAddress => spec.Eip7002ContractAddress;
-        public bool IsEip7251Enabled => spec.IsEip7251Enabled;
-        public Address Eip7251ContractAddress => spec.Eip7251ContractAddress;
-        public bool IsEip2935Enabled => spec.IsEip2935Enabled;
-        public bool IsEip7709Enabled => spec.IsEip7709Enabled;
-        public Address Eip2935ContractAddress => spec.Eip2935ContractAddress;
-        public bool IsEip7702Enabled => spec.IsEip7702Enabled;
-        public bool IsEip7823Enabled => spec.IsEip7823Enabled;
+        public long Eip1559TransitionBlock { get; set; } = spec.Eip1559TransitionBlock;
+        public Address? FeeCollector { get; set; } = spec.FeeCollector;
+        public ulong Eip4844TransitionTimestamp { get; set; } = spec.Eip4844TransitionTimestamp;
+        public ulong TargetBlobCount { get; set; } = spec.TargetBlobCount;
+        public ulong MaxBlobCount { get; set; } = spec.MaxBlobCount;
+        public ulong MaxBlobsPerTx { get; set; } = spec.MaxBlobsPerTx;
+        public UInt256 BlobBaseFeeUpdateFraction { get; set; } = spec.BlobBaseFeeUpdateFraction;
+        public bool IsEip1153Enabled { get; set; } = spec.IsEip1153Enabled;
+        public bool IsEip3651Enabled { get; set; } = spec.IsEip3651Enabled;
+        public bool IsEip3855Enabled { get; set; } = spec.IsEip3855Enabled;
+        public bool IsEip3860Enabled { get; set; } = spec.IsEip3860Enabled;
+        public bool IsEip4895Enabled { get; set; } = spec.IsEip4895Enabled;
+        public ulong WithdrawalTimestamp { get; set; } = spec.WithdrawalTimestamp;
+        public bool IsEip5656Enabled { get; set; } = spec.IsEip5656Enabled;
+        public long Eip2935RingBufferSize { get; set; } = spec.Eip2935RingBufferSize;
+        public bool IsEip6780Enabled { get; set; } = spec.IsEip6780Enabled;
+        public bool IsEip4788Enabled { get; set; } = spec.IsEip4788Enabled;
+        public bool IsEip4844FeeCollectorEnabled { get; set; } = spec.IsEip4844FeeCollectorEnabled;
+        public Address? Eip4788ContractAddress { get; set; } = spec.Eip4788ContractAddress;
+        public bool IsEip7002Enabled { get; set; } = spec.IsEip7002Enabled;
+        public Address? Eip7002ContractAddress { get; set; } = spec.Eip7002ContractAddress;
+        public bool IsEip7251Enabled { get; set; } = spec.IsEip7251Enabled;
+        public Address? Eip7251ContractAddress { get; set; } = spec.Eip7251ContractAddress;
+        public bool IsEip2935Enabled { get; set; } = spec.IsEip2935Enabled;
+        public bool IsEip7709Enabled { get; set; } = spec.IsEip7709Enabled;
+        public Address? Eip2935ContractAddress { get; set; } = spec.Eip2935ContractAddress;
+        public bool IsEip7702Enabled { get; set; } = spec.IsEip7702Enabled;
+        public bool IsEip7823Enabled { get; set; } = spec.IsEip7823Enabled;
         public bool IsEip7825Enabled { get; set; } = spec.IsEip7825Enabled;
-        public UInt256 ForkBaseFee => spec.ForkBaseFee;
-        public UInt256 BaseFeeMaxChangeDenominator => spec.BaseFeeMaxChangeDenominator;
-        public long ElasticityMultiplier => spec.ElasticityMultiplier;
-        public IBaseFeeCalculator BaseFeeCalculator => spec.BaseFeeCalculator;
-        public bool IsEofEnabled => spec.IsEofEnabled;
-        public bool IsEip6110Enabled => spec.IsEip6110Enabled;
-        public Address DepositContractAddress => spec.DepositContractAddress;
-        public bool IsEip7594Enabled => spec.IsEip7594Enabled;
-
+        public UInt256 ForkBaseFee { get; set; } = spec.ForkBaseFee;
+        public UInt256 BaseFeeMaxChangeDenominator { get; set; } = spec.BaseFeeMaxChangeDenominator;
+        public long ElasticityMultiplier { get; set; } = spec.ElasticityMultiplier;
+        public IBaseFeeCalculator BaseFeeCalculator { get; set; } = spec.BaseFeeCalculator;
+        public bool IsEofEnabled { get; set; } = spec.IsEofEnabled;
+        public bool IsEip6110Enabled { get; set; } = spec.IsEip6110Enabled;
+        public Address? DepositContractAddress { get; set; } = spec.DepositContractAddress;
+        public bool IsEip7594Enabled { get; set; } = spec.IsEip7594Enabled;
         Array? IReleaseSpec.EvmInstructionsNoTrace { get => spec.EvmInstructionsNoTrace; set => spec.EvmInstructionsNoTrace = value; }
         Array? IReleaseSpec.EvmInstructionsTraced { get => spec.EvmInstructionsTraced; set => spec.EvmInstructionsTraced = value; }
-        public bool IsEip7939Enabled => spec.IsEip7939Enabled;
-        public bool IsEip7907Enabled => spec.IsEip7907Enabled;
-        public bool IsRip7728Enabled => spec.IsRip7728Enabled;
+        public bool IsEip7939Enabled { get; set; } = spec.IsEip7939Enabled;
+        public bool IsEip7907Enabled { get; set; } = spec.IsEip7907Enabled;
+        public bool IsRip7728Enabled { get; set; } = spec.IsRip7728Enabled;
         FrozenSet<AddressAsKey> IReleaseSpec.Precompiles => spec.Precompiles;
     }
 }
