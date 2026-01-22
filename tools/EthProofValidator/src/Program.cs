f942a2a3b900e6be47c076488ae5344df0759331
ManuelArto <manuelartociao@gmail.com>
2026-01-22 11:44:09 +0100

    feat(tools/EthProofValidator): implement `System.CommandLine` for arguments in block validation

--- tools/EthProofValidator/src/Program.cs
+++ tools/EthProofValidator/src/Program.cs
@@ -1,28 +1,44 @@
 ﻿// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-namespace Nethermind.EthProofValidator;
+using System.CommandLine;
+using Nethermind.EthProofValidator;
 
-class Program
+Argument<int> latestBlockIdArg = new("latestBlockId")
 {
-    static async Task Main(string[] args)
+    Description = "The ID of the latest block to validate",
+    HelpName = "id"
+};
+
+Argument<int> blockCountArg = new("blockCount")
+{
+    Description = "The number of blocks to validate",
+    HelpName = "count"
+};
+
+RootCommand rootCommand =
+[
+    latestBlockIdArg,
+    blockCountArg
+];
+
+rootCommand.Description = "Tool to validate Ethereum block via zk proofs";
+
+rootCommand.SetAction(async (parseResult, cancellationToken) =>
+{
+    int latestBlockId = parseResult.GetValue(latestBlockIdArg);
+    int blockCount = parseResult.GetValue(blockCountArg);
+
+    BlockValidator validator = new();
+
+    while (blockCount-- > 0)
     {
-        if (args.Length < 2 || !int.TryParse(args[0], out int latestBlockId) || !int.TryParse(args[1], out int blockCount))
-        {
-            Console.WriteLine("Usage: dotnet run <LatestBlockId> <BlockCount>");
-            Console.WriteLine("Example: dotnet run 24182425 25");
-            return;
-        }
-
-        BlockValidator validator = new BlockValidator();
-
-        while (blockCount-- > 0)
-        {
-            int blockId = latestBlockId - blockCount;
-            var sw = System.Diagnostics.Stopwatch.StartNew();
-            await validator.ValidateBlockAsync(blockId);
-            sw.Stop();
-            Console.WriteLine($"⏱️  Total Time: {sw.ElapsedMilliseconds} ms");
-        }
+        int blockId = latestBlockId - blockCount;
+        var sw = System.Diagnostics.Stopwatch.StartNew();
+        await validator.ValidateBlockAsync(blockId);
+        sw.Stop();
+        Console.WriteLine($"⏱️  Total Time: {sw.ElapsedMilliseconds} ms");
     }
-}
+});
+
+return await rootCommand.Parse(args).InvokeAsync();
