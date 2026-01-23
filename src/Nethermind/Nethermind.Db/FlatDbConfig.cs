cc490268213edf668ff51d93ed950b69a30153ee
Amirul Ashraf <asdacap@gmail.com>
2026-01-23 19:34:37 +0800

    Clean config

--- src/Nethermind/Nethermind.Db/FlatDbConfig.cs
+++ src/Nethermind/Nethermind.Db/FlatDbConfig.cs
@@ -8,20 +8,18 @@ namespace Nethermind.Db;
 public class FlatDbConfig : IFlatDbConfig
 {
     public bool Enabled { get; set; } = false;
+    public bool EnablePreimageRecording { get; set; } = false;
     public bool ImportFromPruningTrieState { get; set; } = false;
-    public int MinReorgDepth { get; set; } = 128;
-    public int CompactSize { get; set; } = 32;
-    public int MidCompactSize { get; set; } = 4;
-    public int MaxInFlightCompactJob { get; set; } = 32;
+    public bool InlineCompaction { get; set; } = false;
     public bool ReadWithTrie { get; set; } = false;
     public bool VerifyWithTrie { get; set; } = false;
-    public bool InlineCompaction { get; set; } = false;
-
-    // 1 GB is enough for 10% dirty load. 512 MB is pretty good at around 20%. Without it, then the diff layers on its own have around 35% dirty load.
-    public long TrieCacheMemoryTarget { get; set; } = 512.MiB();
     public FlatLayout Layout { get; set; } = FlatLayout.Flat;
-    public long BlockCacheSizeBudget { get; set; } = 1.GiB();
+    public int CompactSize { get; set; } = 32;
+    public int MaxInFlightCompactJob { get; set; } = 32;
     public int MaxReorgDepth { get; set; } = 1024;
+    public int MidCompactSize { get; set; } = 4;
+    public int MinReorgDepth { get; set; } = 128;
     public int TrieWarmerWorkerCount { get; set; } = -1;
-    public bool EnablePreimageRecording { get; set; } = false;
+    public long BlockCacheSizeBudget { get; set; } = 1.GiB();
+    public long TrieCacheMemoryTarget { get; set; } = 512.MiB();
 }
