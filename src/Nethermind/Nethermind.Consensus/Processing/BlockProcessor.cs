c97aceeb9e26b08ef79035ea1f3ba489447187af
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2026-01-27 16:33:54 +0000

    Revert "try always setting bal hash genesis"
    This reverts commit 52563c0c8a0809c9c56190e505c7f1a418d6c33c.

--- src/Nethermind/Nethermind.Consensus/Processing/BlockProcessor.cs
+++ src/Nethermind/Nethermind.Consensus/Processing/BlockProcessor.cs
@@ -179,15 +179,18 @@ public partial class BlockProcessor
             header.StateRoot = _stateProvider.StateRoot;
         }
 
-        if (block.IsGenesis)
+        if (_parallelWorldState is not null && spec.BlockLevelAccessListsEnabled)
         {
-            header.BlockAccessListHash = Keccak.OfAnEmptySequenceRlp;
-        }
-        else if (_parallelWorldState is not null && spec.BlockLevelAccessListsEnabled)
-        {
-            block.GeneratedBlockAccessList = _parallelWorldState.BlockAccessList;
-            block.EncodedBlockAccessList = Rlp.Encode(_parallelWorldState.BlockAccessList).Bytes;
-            header.BlockAccessListHash = new(ValueKeccak.Compute(block.EncodedBlockAccessList).Bytes);
+            if (block.IsGenesis)
+            {
+                header.BlockAccessListHash = Keccak.OfAnEmptySequenceRlp;
+            }
+            else
+            {
+                block.GeneratedBlockAccessList = _parallelWorldState.BlockAccessList;
+                block.EncodedBlockAccessList = Rlp.Encode(_parallelWorldState.BlockAccessList).Bytes;
+                header.BlockAccessListHash = new(ValueKeccak.Compute(block.EncodedBlockAccessList).Bytes);
+            }
         }
 
         header.Hash = header.CalculateHash();
