1a0d584c7529a0009275c199437db327fc256047
Amirul Ashraf <asdacap@gmail.com>
2026-01-28 20:37:34 +0800

    dispose within snap provider helper

--- src/Nethermind/Nethermind.Synchronization.Test/SnapSync/RecreateStateFromAccountRangesTests.cs
+++ src/Nethermind/Nethermind.Synchronization.Test/SnapSync/RecreateStateFromAccountRangesTests.cs
@@ -318,7 +318,7 @@ public class RecreateStateFromAccountRangesTests
 
         bool HasMoreChildren(ValueHash256 limitHash)
         {
-            (AddRangeResult _, bool moreChildrenToRight, IList<PathWithAccount> _, IList<ValueHash256> _) =
+            (AddRangeResult _, bool moreChildrenToRight, IList<PathWithAccount> _, IList<ValueHash256> _, Hash256 _) =
                 SnapProviderHelper.AddAccountRange(new PatriciaSnapStateTree(newTree), 0, rootHash, Keccak.Zero, limitHash.ToCommitment(), receiptAccounts, proofs);
             return moreChildrenToRight;
         }
@@ -366,7 +366,7 @@ public class RecreateStateFromAccountRangesTests
 
         bool HasMoreChildren(ValueHash256 limitHash)
         {
-            (AddRangeResult _, bool moreChildrenToRight, IList<PathWithAccount> _, IList<ValueHash256> _) =
+            (AddRangeResult _, bool moreChildrenToRight, IList<PathWithAccount> _, IList<ValueHash256> _, Hash256 _) =
                 SnapProviderHelper.AddAccountRange(new PatriciaSnapStateTree(newTree), 0, rootHash, Keccak.Zero, limitHash.ToCommitment(), receiptAccounts, proofs);
             return moreChildrenToRight;
         }
