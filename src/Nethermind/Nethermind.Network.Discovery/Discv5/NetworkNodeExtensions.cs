28c63e5b67c348b019cb3399ebfaba98179a4475
Alexey Osipov <me@flcl.me>
2026-01-28 19:12:00 +0300

    Fix nothing

--- src/Nethermind/Nethermind.Network.Discovery/Discv5/NetworkNodeExtensions.cs
+++ src/Nethermind/Nethermind.Network.Discovery/Discv5/NetworkNodeExtensions.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
+// SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using Lantern.Discv5.Enr;
