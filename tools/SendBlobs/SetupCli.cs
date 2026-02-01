b1663f939b72edbc0e0feafde6c0471f236dce07
marukai67 <miserly.pendant0p@icloud.com>
2026-02-01 13:49:21 +0100

    Update SetupCli.cs

--- tools/SendBlobs/SetupCli.cs
+++ tools/SendBlobs/SetupCli.cs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using Nethermind.Consensus;
 using Nethermind.Crypto;
 using Nethermind.Logging;
 using System.CommandLine;
