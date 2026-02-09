e0c214a06f1061ed1a48f4c2edebfe684c370373
Alex Bespalov <alexb5dh@gmail.com>
2026-02-09 18:54:27 +0200

    Snapshot debugger display

--- src/Nethermind/Nethermind.Xdc/Types/Snapshot.cs
+++ src/Nethermind/Nethermind.Xdc/Types/Snapshot.cs
@@ -1,11 +1,13 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System.Diagnostics;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
 
 namespace Nethermind.Xdc.Types;
 
+[DebuggerDisplay("{BlockNumber} ({HeaderHash})")]
 public class Snapshot(long number, Hash256 hash, Address[] masterNodes)
 {
     public long BlockNumber { get; set; } = number;
