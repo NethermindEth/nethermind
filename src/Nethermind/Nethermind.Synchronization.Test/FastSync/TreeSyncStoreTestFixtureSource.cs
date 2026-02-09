528b5954a135566e7e31c9fdd4e4b09e54c3f384
Amirul Ashraf <asdacap@gmail.com>
2026-02-09 09:37:31 +0800

    Some rework

--- src/Nethermind/Nethermind.Synchronization.Test/FastSync/TreeSyncStoreTestFixtureSource.cs
+++ src/Nethermind/Nethermind.Synchronization.Test/FastSync/TreeSyncStoreTestFixtureSource.cs
@@ -4,8 +4,16 @@
 using System;
 using System.Collections;
 using Autofac;
+using Autofac.Features.AttributeFilters;
+using FluentAssertions;
 using Nethermind.Core;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Test;
+using Nethermind.Db;
+using Nethermind.Logging;
+using Nethermind.State;
 using Nethermind.Synchronization.FastSync;
+using Nethermind.Trie;
 using NUnit.Framework;
 
 namespace Nethermind.Synchronization.Test.FastSync;
@@ -14,7 +22,7 @@ public class TreeSyncStoreTestFixtureSource : IEnumerable
 {
     public static void RegisterPatriciaStore(ContainerBuilder builder) => builder
         .AddSingleton<ITreeSyncStore, PatriciaTreeSyncStore>()
-        .AddSingleton<ITestOperation, PatriciaTreeTestOperation>()
+        .AddSingleton<ITestOperation, LocalDbContext>()
         ;
 
     // Future:
@@ -28,14 +36,78 @@ public class TreeSyncStoreTestFixtureSource : IEnumerable
         // Future: yield return for Flat
     }
 
-    private interface ITestOperation
+    public interface ITestOperation
     {
         // Add here
     }
+}
 
-    private class PatriciaTreeTestOperation : ITestOperation
+public class LocalDbContext: TreeSyncStoreTestFixtureSource.ITestOperation
+{
+    public LocalDbContext(
+        [KeyFilter(DbNames.Code)] IDb codeDb,
+        [KeyFilter(DbNames.State)] IDb stateDb,
+        INodeStorage nodeStorage,
+        ILogManager logManager)
     {
+        NodeStorage = nodeStorage;
+        CodeDb = (TestMemDb)codeDb;
+        Db = (TestMemDb)stateDb;
+        StateTree = new StateTree(TestTrieStoreFactory.Build(nodeStorage, logManager), logManager);
+    }
 
+    private TestMemDb CodeDb { get; }
+    private TestMemDb Db { get; }
+    private INodeStorage NodeStorage { get; }
+    private StateTree StateTree { get; }
+
+    public Hash256 RootHash
+    {
+        get => StateTree.RootHash;
+        set => StateTree.RootHash = value;
+    }
+
+    public void UpdateRootHash() => StateTree.UpdateRootHash();
+
+    public void Set(Hash256 address, Account? account) => StateTree.Set(address, account);
+
+    public void Commit() => StateTree.Commit();
+
+    public void AssertFlushed()
+    {
+        Db.WasFlushed.Should().BeTrue();
+        CodeDb.WasFlushed.Should().BeTrue();
+    }
+
+    public void CompareTrees(RemoteDbContext remote, ILogger logger, string stage, bool skipLogs = false)
+    {
+        if (!skipLogs) logger.Info($"==================== {stage} ====================");
+        StateTree.RootHash = remote.StateTree.RootHash;
+
+        if (!skipLogs) logger.Info("-------------------- REMOTE --------------------");
+        TreeDumper dumper = new TreeDumper();
+        remote.StateTree.Accept(dumper, remote.StateTree.RootHash);
+        string remoteStr = dumper.ToString();
+        if (!skipLogs) logger.Info(remoteStr);
+        if (!skipLogs) logger.Info("-------------------- LOCAL --------------------");
+        dumper.Reset();
+        StateTree.Accept(dumper, StateTree.RootHash);
+        string localStr = dumper.ToString();
+        if (!skipLogs) logger.Info(localStr);
+
+        if (stage == "END")
+        {
+            Assert.That(localStr, Is.EqualTo(remoteStr), $"{stage}{Environment.NewLine}{remoteStr}{Environment.NewLine}{localStr}");
+            TrieStatsCollector collector = new(CodeDb, LimboLogs.Instance);
+            StateTree.Accept(collector, StateTree.RootHash);
+            Assert.That(collector.Stats.MissingNodes, Is.EqualTo(0));
+            Assert.That(collector.Stats.MissingCode, Is.EqualTo(0));
+        }
+    }
+
+    public void DeleteStateRoot()
+    {
+        NodeStorage.Set(null, TreePath.Empty, RootHash, null);
     }
 }
 
