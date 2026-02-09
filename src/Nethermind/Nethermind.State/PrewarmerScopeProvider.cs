afce1ad6fb68667b75e963513e9ac1d455c3e532
Ben Adams <thundercat@illyriad.co.uk>
2026-02-09 03:54:46 +0000

    Feedback

--- src/Nethermind/Nethermind.State/PrewarmerScopeProvider.cs
+++ src/Nethermind/Nethermind.State/PrewarmerScopeProvider.cs
@@ -46,7 +46,7 @@ public class PrewarmerScopeProvider(
         bool populatePreBlockCache)
         : IWorldStateScopeProvider.IScope
     {
-        SeqlockCache<AddressAsKey, Account> preBlockCache = preBlockCaches.StateCache;
+        private readonly SeqlockCache<AddressAsKey, Account> preBlockCache = preBlockCaches.StateCache;
         private readonly IMetricObserver _metricObserver = Metrics.PrewarmerGetTime;
         private readonly bool _measureMetric = Metrics.DetailedMetricsEnabled;
         private readonly PrewarmerGetTimeLabels _labels = populatePreBlockCache ? PrewarmerGetTimeLabels.Prewarmer : PrewarmerGetTimeLabels.NonPrewarmer;
