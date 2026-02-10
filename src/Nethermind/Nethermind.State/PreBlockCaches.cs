3fb6a49e1911024b6553d93e685cdd317b7a07c7
Ben Adams <thundercat@illyriad.co.uk>
2026-02-10 00:00:22 +0000

    Convert NodeStorageCache and PreBlockCaches.RlpCache to SeqlockCache

--- src/Nethermind/Nethermind.State/PreBlockCaches.cs
+++ src/Nethermind/Nethermind.State/PreBlockCaches.cs
@@ -21,7 +21,7 @@ public class PreBlockCaches
 
     private readonly SeqlockCache<StorageCell, byte[]> _storageCache = new();
     private readonly SeqlockCache<AddressAsKey, Account> _stateCache = new();
-    private readonly ConcurrentDictionary<NodeKey, byte[]?> _rlpCache = new(LockPartitions, InitialCapacity);
+    private readonly SeqlockCache<NodeKey, byte[]?> _rlpCache = new();
     private readonly ConcurrentDictionary<PrecompileCacheKey, Result<byte[]>> _precompileCache = new(LockPartitions, InitialCapacity);
 
     public PreBlockCaches()
@@ -36,7 +36,7 @@ public class PreBlockCaches
 
     public SeqlockCache<StorageCell, byte[]> StorageCache => _storageCache;
     public SeqlockCache<AddressAsKey, Account> StateCache => _stateCache;
-    public ConcurrentDictionary<NodeKey, byte[]?> RlpCache => _rlpCache;
+    public SeqlockCache<NodeKey, byte[]?> RlpCache => _rlpCache;
     public ConcurrentDictionary<PrecompileCacheKey, Result<byte[]>> PrecompileCache => _precompileCache;
 
     public CacheType ClearCaches()
