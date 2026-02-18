9976b5e8992563f5cb3fc4845556c8652e91668e
Demuirgos <Ayman.Bouchareb@outlook.com>
2026-02-18 10:19:07 +0000

    fix build issues

--- src/Nethermind/Nethermind.Xdc/SyncInfoManager.cs
+++ src/Nethermind/Nethermind.Xdc/SyncInfoManager.cs
@@ -1,7 +1,14 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using Nethermind.Core.Collections;
+using Nethermind.Core.Crypto;
+
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
 using Nethermind.Xdc.Types;
+using System.Collections.Generic;
 
 namespace Nethermind.Xdc;
 /// <summary>
@@ -9,6 +16,11 @@ namespace Nethermind.Xdc;
 /// </summary>
 internal class SyncInfoManager(IXdcConsensusContext xdcConsensusContext) : ISyncInfoManager
 {
+    public IDictionary<(ulong Round, Hash256 Hash), ArrayPoolList<SyncInfo>> GetReceivedSyncInfos()
+    {
+        throw new System.NotImplementedException();
+    }
+
     public SyncInfo GetSyncInfo() => new SyncInfo(xdcConsensusContext.HighestQC, xdcConsensusContext.HighestTC);
 
     public void ProcessSyncInfo(SyncInfo syncInfo)
