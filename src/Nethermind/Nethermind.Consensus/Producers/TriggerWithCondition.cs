a6dffe20478d04f2e7f7cabf6953259b14d320fb
sashass1315 <sashass1315@gmail.com>
2026-02-18 20:15:09 +0200

    Fix memory leak in `TriggerWithCondition` by implementing IDisposable

--- src/Nethermind/Nethermind.Consensus/Producers/TriggerWithCondition.cs
+++ src/Nethermind/Nethermind.Consensus/Producers/TriggerWithCondition.cs
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
+using Nethermind.Core.Extensions;
 
 namespace Nethermind.Consensus.Producers
 {
-    public class TriggerWithCondition : IBlockProductionTrigger
+    public class TriggerWithCondition : IBlockProductionTrigger, IDisposable
     {
+        private readonly IBlockProductionTrigger _trigger;
         private readonly Func<BlockProductionEventArgs, bool> _checkCondition;
 
         public TriggerWithCondition(IBlockProductionTrigger trigger, Func<bool> checkCondition) : this(trigger, _ => checkCondition())
@@ -15,6 +17,7 @@ namespace Nethermind.Consensus.Producers
 
         public TriggerWithCondition(IBlockProductionTrigger trigger, Func<BlockProductionEventArgs, bool> checkCondition)
         {
+            _trigger = trigger;
             _checkCondition = checkCondition;
             trigger.TriggerBlockProduction += TriggerOnTriggerBlockProduction;
         }
@@ -22,12 +25,18 @@ namespace Nethermind.Consensus.Producers
 
         private void TriggerOnTriggerBlockProduction(object? sender, BlockProductionEventArgs e)
         {
-            if (_checkCondition.Invoke(e))
+            if (_checkCondition(e))
             {
                 TriggerBlockProduction?.Invoke(this, e);
             }
         }
 
         public event EventHandler<BlockProductionEventArgs>? TriggerBlockProduction;
+
+        public void Dispose()
+        {
+            _trigger.TriggerBlockProduction -= TriggerOnTriggerBlockProduction;
+            _trigger.TryDispose();
+        }
     }
 }
