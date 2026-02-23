6425270a77b015b01819be4997874f2da934d08f
ak88 <anders.holmbjerg@hotmail.com>
2026-02-23 12:45:23 +0100

    removed not supported check

--- src/Nethermind/Nethermind.Network/Rlpx/ZeroFrameMerger.cs
+++ src/Nethermind/Nethermind.Network/Rlpx/ZeroFrameMerger.cs
@@ -75,8 +75,6 @@ namespace Nethermind.Network.Rlpx
         {
             Rlp.ValueDecoderContext rlpContext = new(input.AsSpan());
             ulong rlpPacketType = rlpContext.DecodeULong();
-            if (rlpPacketType > byte.MaxValue)
-                throw new NotSupportedException($"Received unsupported packet code={rlpPacketType}. We currently only support values 0-255.");
             int read = rlpContext.Position;
             input.SkipBytes(read);
             IByteBuffer content;
