df2e1dda1cdb34cee198f4e3e07aebae098ba04b
Alexey Osipov <me@flcl.me>
2026-02-24 14:08:47 +0300

    Fix tests - remove console output and move converters back

--- src/Nethermind/Nethermind.Core/BlockAccessLists/StorageChange.cs
+++ src/Nethermind/Nethermind.Core/BlockAccessLists/StorageChange.cs
@@ -3,17 +3,14 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
-using System.Linq;
-using System.Text.Json.Serialization;
 using Nethermind.Int256;
-using Nethermind.Serialization.Json;
 
 namespace Nethermind.Core.BlockAccessLists;
 
 public readonly struct StorageChange(ushort blockAccessIndex, UInt256 newValue) : IEquatable<StorageChange>, IIndexedChange
 {
     public ushort BlockAccessIndex { get; init; } = blockAccessIndex;
-    [JsonConverter(typeof(UInt256Converter))]
+
     public UInt256 NewValue { get; init; } = newValue;
 
     public readonly bool Equals(StorageChange other) =>
