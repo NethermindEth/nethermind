d23921593abd928d67f908a69c9dfc35e31dbe06
Alexey Osipov <me@flcl.me>
2026-02-25 20:26:06 +0300

    Account for Skipped txs

--- src/Nethermind/Nethermind.Core/BlockAccessLists/BlockAccessList.cs
+++ src/Nethermind/Nethermind.Core/BlockAccessLists/BlockAccessList.cs
@@ -3,12 +3,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
 using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Nethermind.Core.Collections;
 using Nethermind.Int256;
 
 namespace Nethermind.Core.BlockAccessLists;
@@ -55,6 +52,13 @@ public class BlockAccessList : IEquatable<BlockAccessList>, IJournal<int>
     {
         _changes.Clear();
         Index++;
+    }
+
+    public void RollbackCurrentIndex()
+    {
+        Restore(0);
+        _changes.Clear();
+        Index--;
     }
 
     public void Clear()
