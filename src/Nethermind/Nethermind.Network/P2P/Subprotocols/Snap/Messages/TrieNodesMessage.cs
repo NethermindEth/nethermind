993ab06e730bfe81ff9022494644f81343a1b886
Amirul Ashraf <asdacap@gmail.com>
2026-02-26 14:31:14 +0800

    Revert "refactor: Type TrieNodesMessage.Nodes as RlpByteArrayList to eliminate runtime type check"
    This reverts commit e384fc2a413c1e0838227828d5381e6bfad5d17d.

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Snap/Messages/TrieNodesMessage.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Snap/Messages/TrieNodesMessage.cs
@@ -1,20 +1,20 @@
 // SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using Nethermind.Serialization.Rlp;
+using Nethermind.Core.Collections;
 
 namespace Nethermind.Network.P2P.Subprotocols.Snap.Messages
 {
-    public class TrieNodesMessage(RlpByteArrayList? data) : SnapMessageBase
+    public class TrieNodesMessage(IByteArrayList? data) : SnapMessageBase
     {
         public override int PacketType => SnapMessageCode.TrieNodes;
 
-        public RlpByteArrayList? Nodes { get; } = data;
+        public IByteArrayList Nodes { get; } = data ?? EmptyByteArrayList.Instance;
 
         public override void Dispose()
         {
             base.Dispose();
-            Nodes?.Dispose();
+            Nodes.Dispose();
         }
     }
 }
