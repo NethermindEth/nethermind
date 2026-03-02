23d3e57a03c05f6fc2eb7985978bafa85b93fe7c
bigbear <155267841+aso20455@users.noreply.github.com>
2026-03-02 10:52:36 +0100

    fix Test

--- src/Nethermind/Nethermind.Config.Test/ConfigProviderTests.cs
+++ src/Nethermind/Nethermind.Config.Test/ConfigProviderTests.cs
@@ -19,11 +19,16 @@ namespace Nethermind.Config.Test
         [TestCase(typeof(int), 0)]
         [TestCase(typeof(bool), false)]
         [TestCase(typeof(long), 0L)]
-        [TestCase(typeof(string), null)]
-        public void GetDefault_returns_correct_default_value(Type type, object expected)
+        public void GetDefault_returns_correct_default_value_for_value_types(Type type, object expected)
         {
             object result = ConfigSourceHelper.GetDefault(type);
             Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetDefault_returns_null_for_reference_types()
+        {
+            Assert.That(ConfigSourceHelper.GetDefault(typeof(string)), Is.Null);
         }
     }
 
