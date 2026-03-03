aa3740dad710f0756ffe6e819569e0ecfb953432
lukasz.rozmej <lukasz.rozmej@gmail.com>
2026-03-03 13:54:41 +0100

    simplify

--- src/Nethermind/Nethermind.Evm.Test/Eip7954Tests.cs
+++ src/Nethermind/Nethermind.Evm.Test/Eip7954Tests.cs
@@ -5,6 +5,7 @@ using Nethermind.Core;
 using Nethermind.Core.Extensions;
 using Nethermind.Core.Specs;
 using Nethermind.Core.Test.Builders;
+using Nethermind.Evm.Tracing;
 using Nethermind.Evm.TransactionProcessing;
 using Nethermind.Specs;
 using NUnit.Framework;
@@ -26,22 +27,14 @@ public class Eip7954Tests : VirtualMachineTestsBase
     [TestCase(true, 55000, ExpectedResult = false, TestName = "InitCode_between_old_and_new_limit_accepted")]
     [TestCase(false, 55000, ExpectedResult = true, TestName = "InitCode_above_old_limit_rejected_before_eip7954")]
     [TestCase(true, 2 * CodeSizeConstants.MaxCodeSizeEip7954 + 1, ExpectedResult = true, TestName = "InitCode_exceeding_new_limit_rejected")]
-    public bool InitCode_size_validation(bool eip7954Enabled, long initCodeSize)
-    {
-        ulong timestamp = eip7954Enabled ? Timestamp : MainnetSpecProvider.ShanghaiBlockTimestamp;
-        (TransactionResult result, _) = ExecuteRawCreateTransaction(timestamp, initCodeSize);
-        return result == TransactionResult.TransactionSizeOverMaxInitCodeSize;
-    }
+    public bool InitCode_size_validation(bool eip7954Enabled, long initCodeSize) =>
+        ExecuteRawCreateTransaction(eip7954Enabled ? Timestamp : MainnetSpecProvider.ShanghaiBlockTimestamp, initCodeSize) == TransactionResult.TransactionSizeOverMaxInitCodeSize;
 
     [TestCase(30000, ExpectedResult = StatusCode.Success, TestName = "Code_deposit_between_old_and_new_limit_succeeds")]
     [TestCase(CodeSizeConstants.MaxCodeSizeEip7954 + 1, ExpectedResult = StatusCode.Failure, TestName = "Code_deposit_above_new_limit_fails")]
-    public byte Code_deposit_size_validation(int deployedCodeSize)
-    {
-        (_, TestAllTracerWithOutput tracer) = ExecuteDeployTransaction(Timestamp, deployedCodeSize);
-        return tracer.StatusCode;
-    }
+    public byte Code_deposit_size_validation(int deployedCodeSize) => ExecuteDeployTransaction(Timestamp, deployedCodeSize).StatusCode;
 
-    private (TransactionResult, TestAllTracerWithOutput) ExecuteRawCreateTransaction(ulong timestamp, long initCodeSize)
+    private TransactionResult ExecuteRawCreateTransaction(ulong timestamp, long initCodeSize)
     {
         byte[] initCode = new byte[initCodeSize];
 
@@ -52,12 +45,10 @@ public class Eip7954Tests : VirtualMachineTestsBase
         transaction.GasPrice = 2.GWei();
         transaction.To = null;
         transaction.Data = initCode;
-        TestAllTracerWithOutput tracer = CreateTracer();
-        TransactionResult result = _processor.Execute(transaction, new BlockExecutionContext(block.Header, SpecProvider.GetSpec(block.Header)), tracer);
-        return (result, tracer);
+        return _processor.Execute(transaction, new BlockExecutionContext(block.Header, SpecProvider.GetSpec(block.Header)), NullTxTracer.Instance);
     }
 
-    private (TransactionResult, TestAllTracerWithOutput) ExecuteDeployTransaction(ulong timestamp, int deployedCodeSize)
+    private TestAllTracerWithOutput ExecuteDeployTransaction(ulong timestamp, int deployedCodeSize)
     {
         // Build minimal init code: PUSH size, PUSH 0, RETURN
         // Returns deployedCodeSize zero bytes from memory as deployed contract code
@@ -75,7 +66,7 @@ public class Eip7954Tests : VirtualMachineTestsBase
         transaction.To = null;
         transaction.Data = initCode;
         TestAllTracerWithOutput tracer = CreateTracer();
-        TransactionResult result = _processor.Execute(transaction, new BlockExecutionContext(block.Header, SpecProvider.GetSpec(block.Header)), tracer);
-        return (result, tracer);
+        _processor.Execute(transaction, new BlockExecutionContext(block.Header, SpecProvider.GetSpec(block.Header)), tracer);
+        return tracer;
     }
 }
