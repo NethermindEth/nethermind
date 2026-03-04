8b0c0d279d8789d68d497f0767ccb32954b1e172
Alex Bespalov <alexb5dh@gmail.com>
2026-03-04 03:47:08 +0200

    Log reason for skipping vote

--- src/Nethermind/Nethermind.Xdc/IVotesManager.cs
+++ src/Nethermind/Nethermind.Xdc/IVotesManager.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System.Diagnostics.CodeAnalysis;
 using Nethermind.Core.Crypto;
 using Nethermind.Xdc.Types;
 using System.Threading.Tasks;
@@ -13,6 +14,6 @@ public interface IVotesManager
     Task HandleVote(Vote vote);
     Task OnReceiveVote(Vote vote);
     bool VerifyVotingRules(BlockRoundInfo roundInfo, QuorumCertificate certificate);
-    bool VerifyVotingRules(XdcBlockHeader header);
-    bool VerifyVotingRules(Hash256 blockHash, long blockNumber, ulong roundNumber, QuorumCertificate qc);
+    bool VerifyVotingRules(XdcBlockHeader header, [NotNullWhen(false)] out string? error);
+    bool VerifyVotingRules(Hash256 blockHash, long blockNumber, ulong roundNumber, QuorumCertificate qc, [NotNullWhen(false)] out string? error);
 }
