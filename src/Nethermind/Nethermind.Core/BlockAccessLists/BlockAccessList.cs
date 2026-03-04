8cb29d3327b4c9ee106d2f49ef95c90feb6518ba
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2026-03-04 12:30:36 +0000

    formatting

--- src/Nethermind/Nethermind.Core/BlockAccessLists/BlockAccessList.cs
+++ src/Nethermind/Nethermind.Core/BlockAccessLists/BlockAccessList.cs
@@ -55,7 +55,7 @@ public class BlockAccessList : IEquatable<BlockAccessList>, IJournal<int>
         {
             if (_accountChanges.TryGetValue(otherAccountChange.Address, out AccountChanges? accountChange))
             {
-               accountChange.Merge(otherAccountChange);
+                accountChange.Merge(otherAccountChange);
             }
             else
             {
