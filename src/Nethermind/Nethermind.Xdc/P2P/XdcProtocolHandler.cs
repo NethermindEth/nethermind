ce72f289c62d084f5e8f3a1d12d7fb51125d5108
Alex Bespalov <alexb5dh@gmail.com>
2026-03-05 11:11:06 +0200

    Get rid of cspell comment

--- src/Nethermind/Nethermind.Xdc/P2P/XdcProtocolHandler.cs
+++ src/Nethermind/Nethermind.Xdc/P2P/XdcProtocolHandler.cs
@@ -39,7 +39,6 @@ internal class XdcProtocolHandler(
     private ClockKeyCache<ValueHash256> _notifiedVotes = new(MemoryAllowance.MemPoolSize / 2);
     private ClockKeyCache<ValueHash256> _notifiedTimeouts = new(MemoryAllowance.MemPoolSize / 2);
 
-    // cspell:disable-next-line
     public override string Name => "xdpos2";
 
     public override byte ProtocolVersion => 100;
