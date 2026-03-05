a4680f0787f0be6497b2dcc903f92d720f9af515
Alex Bespalov <alexb5dh@gmail.com>
2026-03-05 14:12:35 +0200

    More `HobbitTests`

--- src/Nethermind/Nethermind.Xdc.Test/Network/HobbitTests.cs
+++ src/Nethermind/Nethermind.Xdc.Test/Network/HobbitTests.cs
@@ -1,12 +1,14 @@
 // SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using Nethermind.Core.Crypto;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Network;
 using Nethermind.Network.Rlpx;
 using Nethermind.Network.Test.Rlpx;
 using Nethermind.Serialization.Rlp;
 using Nethermind.Xdc.P2P;
+using Nethermind.Xdc.Types;
 using NUnit.Framework;
 
 namespace Nethermind.Xdc.Test.Network
@@ -14,15 +16,43 @@ namespace Nethermind.Xdc.Test.Network
     [TestFixture]
     public class HobbitTests : HobbitTestsBase
     {
-        [TestCase(StackType.Zero, StackType.Zero, true)]
-        [TestCase(StackType.Zero, StackType.Zero, false)]
-        public void Timeout_there_and_back(StackType inbound, StackType outbound, bool framingEnabled)
+        [Test]
+        public void Timeout_there_and_back([Values] StackType inbound, [Values] StackType outbound, [Values] bool framingEnabled)
         {
             using TimeoutMsg msg = new();
             msg.AdaptivePacketType = 242;
             msg.Timeout = XdcTestHelper.BuildSignedTimeout(Build.A.PrivateKey.TestObject, 123, 400);
 
             MessageSerializationService service = new(SerializerInfo.Create(new TimeoutMsgSerializer()));
+            byte[] data = service.ZeroSerialize(msg).AsSpan().ToArray();
+            Packet packet = new("eth", msg.AdaptivePacketType, data);
+
+            Run(packet, inbound, outbound, framingEnabled);
+        }
+
+        [Test]
+        public void Vote_there_and_back([Values] StackType inbound, [Values] StackType outbound, [Values] bool framingEnabled)
+        {
+            using VoteMsg msg = new();
+            msg.AdaptivePacketType = XdcMessageCode.VoteMsg;
+            msg.Vote = XdcTestHelper.BuildSignedVote(
+                new BlockRoundInfo(Hash256.Zero, 123, 100), 400, Build.A.PrivateKey.TestObject);
+
+            MessageSerializationService service = new(SerializerInfo.Create(new VoteMsgSerializer()));
+            byte[] data = service.ZeroSerialize(msg).AsSpan().ToArray();
+            Packet packet = new("eth", msg.AdaptivePacketType, data);
+
+            Run(packet, inbound, outbound, framingEnabled);
+        }
+
+        [Test]
+        public void SyncInfo_there_and_back([Values] StackType inbound, [Values] StackType outbound, [Values] bool framingEnabled)
+        {
+            using SyncInfoMsg msg = new();
+            msg.AdaptivePacketType = XdcMessageCode.SyncInfoMsg;
+            msg.SyncInfo = XdcTestHelper.BuildSyncInfo(Build.A.PrivateKey.TestObject, 123, 400);
+
+            MessageSerializationService service = new(SerializerInfo.Create(new SyncInfoMsgSerializer()));
             byte[] data = service.ZeroSerialize(msg).AsSpan().ToArray();
             Packet packet = new("eth", msg.AdaptivePacketType, data);
 
