d61913440a4c3900ec668cf43d3916f6eb8f97a6
Gaurav Dhiman <newmanifold000@gmail.com>
2026-03-11 08:37:40 +0530

    Move Core.Collections using inside #if ZK_EVM guard in MemDb.cs
    The TryRemove extension method is only needed for Dictionary's
    AlternateLookup under ZK_EVM. Prevents IDE0005 on standard builds
    while keeping the Zisk build working.

--- src/Nethermind/Nethermind.Db/MemDb.cs
+++ src/Nethermind/Nethermind.Db/MemDb.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using Nethermind.Core;
+#if ZK_EVM
+using Nethermind.Core.Collections;
+#endif
 using Nethermind.Core.Extensions;
 
 namespace Nethermind.Db
