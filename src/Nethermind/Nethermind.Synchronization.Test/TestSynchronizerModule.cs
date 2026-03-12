1410bdf2432c83120ff5f11862a34333bedc5d54
Gaurav Dhiman <newmanifold000@gmail.com>
2026-03-13 03:44:11 +0530

    fix: restore usings needed after master rebase
    Add back ProtocolHandlers, System, and SnapSync usings that were
    correctly removed by IDE0005 at the time but are now required by
    code added to master since.

--- src/Nethermind/Nethermind.Synchronization.Test/TestSynchronizerModule.cs
+++ src/Nethermind/Nethermind.Synchronization.Test/TestSynchronizerModule.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using Autofac;
 using Nethermind.Api;
 using Nethermind.Blockchain;
@@ -12,6 +13,7 @@ using Nethermind.Db;
 using Nethermind.Init.Modules;
 using Nethermind.Logging;
 using Nethermind.Stats;
+using Nethermind.Synchronization.SnapSync;
 using Nethermind.Trie;
 using NSubstitute;
 
