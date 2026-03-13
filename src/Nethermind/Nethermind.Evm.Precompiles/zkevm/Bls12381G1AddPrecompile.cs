ad98c1075f9fb59c2132bdb4b670044eb48b1649
Ruben Buniatyan <rubo@users.noreply.github.com>
2026-03-13 17:24:44 +0100

    Implement `BLS12_G1ADD` precompile

---
+++ src/Nethermind/Nethermind.Evm.Precompiles/zkevm/Bls12381G1AddPrecompile.cs
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: 2022 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using Nethermind.Core;
+using Nethermind.Core.Specs;
+
+namespace Nethermind.Evm.Precompiles;
+
+public partial class Bls12381G1AddPrecompile
+{
+    public Result<byte[]> Run(ReadOnlyMemory<byte> inputData, IReleaseSpec releaseSpec)
+    {
+        if (!ValidateInputLength(inputData))
+            return Errors.InvalidInputLength;
+
+        Span<byte> input = stackalloc byte[Eip2537.LenG1Trimmed * 2];
+        Span<byte> output = stackalloc byte[Eip2537.LenG1Trimmed];
+
+        if (!Eip2537.TryDecodeG1(inputData.Span[..Eip2537.LenG1], input[..Eip2537.LenG1Trimmed]))
+            return Errors.InvalidFieldElementTopBytes;
+
+        if (!Eip2537.TryDecodeG1(inputData.Span[Eip2537.LenG1..], input[Eip2537.LenG1Trimmed..]))
+            return Errors.InvalidFieldElementTopBytes;
+
+        // TODO: consider matching error codes with std implementation
+
+        byte result = ZiskBindings.Crypto.bls12_381_g1_add_c(
+            output, input[..Eip2537.LenG1Trimmed], input[Eip2537.LenG1Trimmed..]);
+
+        if (result <= 1)
+        {
+            byte[] outputData = new byte[Eip2537.LenG1];
+
+            if (result == 0)
+                Eip2537.EncodeG1(output, outputData);
+
+            return outputData;
+        }
+
+        return Errors.Failed;
+    }
+}
