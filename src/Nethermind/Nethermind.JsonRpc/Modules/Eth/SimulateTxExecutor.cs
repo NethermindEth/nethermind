9a98f3c121a184a4866a01991fd0082fd32503ce
Nikita Meshcheriakov <root@nikitam.io>
2026-03-16 18:49:51 -0300

    Remove console logging of exceptions in SimulateTxExecutor

--- src/Nethermind/Nethermind.JsonRpc/Modules/Eth/SimulateTxExecutor.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Eth/SimulateTxExecutor.cs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -199,7 +198,6 @@ public class SimulateTxExecutor<TTrace>(
                     {
                         EvmExceptionType exception = call.Error.EvmException;
                         call.Error.Code = MapEvmExceptionType(exception);
-                        Console.WriteLine($"EXCEPTION: {call.Error.EvmException}, {call.Error.Code}, {call.Error.Message}");
                         if (exception != EvmExceptionType.Revert)
                         {
                             call.Error.Message = call.Error.EvmException.GetEvmExceptionDescription();
