006cb3aeb08c927dbd8ac8ba1376fd0c7e362807
Ruben Buniatyan <rubo@users.noreply.github.com>
2026-03-20 18:47:39 +0100

    Implement `KZG_POINT_EVALUATION` precompile

---
+++ src/Nethermind/Nethermind.Evm.Precompiles/std/KzgPointEvaluationPrecompile.cs
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using System;
+using Nethermind.Core;
+using Nethermind.Core.Specs;
+using Nethermind.Crypto;
+
+namespace Nethermind.Evm.Precompiles;
+
+public partial class KzgPointEvaluationPrecompile
+{
+    public partial Result<byte[]> Run(ReadOnlyMemory<byte> inputData, IReleaseSpec _)
+    {
+        Metrics.KzgPointEvaluationPrecompile++;
+
+        return RunInternal(inputData);
+    }
+}
