78ff54ab763c0e0855a435109931d7ccb664e09d
Ruben Buniatyan <rubo@users.noreply.github.com>
2026-03-21 01:47:36 +0100

    Fix memory alignment issues

--- src/Nethermind/Nethermind.Evm.Precompiles/zkevm/Blake2FPrecompile.cs
+++ src/Nethermind/Nethermind.Evm.Precompiles/zkevm/Blake2FPrecompile.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Buffers.Binary;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Nethermind.Core;
 using Nethermind.Core.Specs;
@@ -18,11 +19,18 @@ public partial class Blake2FPrecompile
 
         byte[] result = GC.AllocateUninitializedArray<byte>(64);
 
-        inputSpan.Slice(4, 64).CopyTo(result);
+        Span<ulong> state = stackalloc ulong[8];
+        Span<ulong> message = stackalloc ulong[16];
+        Span<ulong> offset = stackalloc ulong[2];
 
-        Span<ulong> state = MemoryMarshal.Cast<byte, ulong>(result.AsSpan());
-        ReadOnlySpan<ulong> message = MemoryMarshal.Cast<byte, ulong>(inputSpan.Slice(68, 128));
-        ReadOnlySpan<ulong> offset = MemoryMarshal.Cast<byte, ulong>(inputSpan.Slice(196, 16));
+        ref byte inputSrc = ref MemoryMarshal.GetReference(inputSpan);
+        ref byte stateDst = ref Unsafe.As<ulong, byte>(ref MemoryMarshal.GetReference(state));
+        ref byte messageDst = ref Unsafe.As<ulong, byte>(ref MemoryMarshal.GetReference(message));
+        ref byte offsetDst = ref Unsafe.As<ulong, byte>(ref MemoryMarshal.GetReference(offset));
+
+        Unsafe.CopyBlockUnaligned(ref stateDst, ref Unsafe.Add(ref inputSrc, 4), 64);
+        Unsafe.CopyBlockUnaligned(ref messageDst, ref Unsafe.Add(ref inputSrc, 68), 128);
+        Unsafe.CopyBlockUnaligned(ref offsetDst, ref Unsafe.Add(ref inputSrc, 196), 16);
 
         ZiskBindings.Crypto.blake2b_compress_c(
             BinaryPrimitives.ReadUInt32BigEndian(inputSpan),
@@ -31,6 +39,8 @@ public partial class Blake2FPrecompile
             offset,
             inputSpan[212]
         );
+
+        Unsafe.CopyBlockUnaligned(ref MemoryMarshal.GetReference(result), ref stateDst, (uint)result.Length);
 
         return result;
     }
