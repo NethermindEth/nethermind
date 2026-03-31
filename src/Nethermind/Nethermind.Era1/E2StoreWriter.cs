ffc8742092593369e30d51be1b1932d84507a164
stavrosvl7 <stavrosvl7@gmail.com>
2026-03-31 23:38:25 +0300

    build fix

--- src/Nethermind/Nethermind.Era1/E2StoreWriter.cs
+++ src/Nethermind/Nethermind.Era1/E2StoreWriter.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System.Buffers;
 using System.Diagnostics;
 using System.IO.Compression;
 using System.Runtime.InteropServices;
