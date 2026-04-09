e9401a5d3b2761d384d2fd83f41508008b644985
Marc Harvey-Hill <10379486+Marchhill@users.noreply.github.com>
2026-04-09 01:58:20 +0100

    make singleton

--- src/Nethermind/Nethermind.Init/Modules/BlockProcessingModule.cs
+++ src/Nethermind/Nethermind.Init/Modules/BlockProcessingModule.cs
@@ -63,7 +63,7 @@ public class BlockProcessingModule(IInitConfig initConfig, IBlocksConfig blocksC
             .AddScoped<IBlockProcessor, BlockProcessor>()
             .AddScoped<IWithdrawalProcessor, WithdrawalProcessor>()
             .AddScoped<IExecutionRequestsProcessor, ExecutionRequestsProcessor>()
-            .Add<BlockAccessListManager>()
+            .AddSingleton<BlockAccessListManager>()
             .AddScoped<IProcessingStats, ProcessingStats>()
             .AddScoped<IBlockchainProcessor, BlockchainProcessor>()
             .AddScoped<IRewardCalculator, IRewardCalculatorSource, ITransactionProcessor>((rewardSource, txP) => rewardSource.Get(txP))
