c74d08ea33656535823728a130ed6d0505120d38
stavrosvl7 <stavrosvl7@gmail.com>
2026-04-10 18:31:00 +0300

    remove total difficulty

--- src/Nethermind/Nethermind.Facade/Eth/BlockForRpc.cs
+++ src/Nethermind/Nethermind.Facade/Eth/BlockForRpc.cs
@@ -69,12 +69,6 @@ public class BlockForRpc
             {
                 SlotNumber = block.SlotNumber;
             }
-
-            // Set TD only if network is not merged
-            if (specProvider.MergeBlockNumber is null)
-            {
-                TotalDifficulty = block.Difficulty.IsZero ? null : block.TotalDifficulty ?? UInt256.Zero;
-            }
         }
 
         Number = block.Number;
@@ -128,8 +122,6 @@ public class BlockForRpc
     public Hash256 StateRoot { get; set; }
     [JsonConverter(typeof(NullableRawLongConverter))]
     public long? Step { get; set; }
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public UInt256? TotalDifficulty { get; set; }
     public UInt256 Timestamp { get; set; }
 
     public UInt256? BaseFeePerGas { get; set; }
