b0cf1c1618d0e9477a1b097701c53045d84759ec
Alex Bespalov <alexb5dh@gmail.com>
2026-04-10 23:50:37 +0300

    [C-07] `FrameMacProcessor.Dispose` comment

--- src/Nethermind/Nethermind.Network/Rlpx/FrameMacProcessor.cs
+++ src/Nethermind/Nethermind.Network/Rlpx/FrameMacProcessor.cs
@@ -210,6 +210,7 @@ public sealed class FrameMacProcessor : IFrameMacProcessor
         macCopy.UpdateFinalTo(output);
     }
 
+    // not a real Dispose - copies are returned to the pool, but lazily restored if needed
     public void Dispose()
     {
         _egressMacCopy.Reset();
