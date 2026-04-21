db194546b77b8ddea3c9ef069777bcea9c1a5860
stavrosvl7 <stavrosvl7@gmail.com>
2026-04-21 23:45:29 +0300

    format

--- src/Nethermind/Nethermind.Evm/EvmStack.cs
+++ src/Nethermind/Nethermind.Evm/EvmStack.cs
@@ -539,7 +539,7 @@ public ref struct EvmStack
         Head = head;
 
         return EvmExceptionType.None;
-    // Jump forward to be unpredicted by the branch predictor.
+        // Jump forward to be unpredicted by the branch predictor.
     StackUnderflow:
         return EvmExceptionType.StackUnderflow;
     StackOverflow:
@@ -568,7 +568,7 @@ public ref struct EvmStack
         if (TTracingInst.IsActive) Trace(depth);
 
         return EvmExceptionType.None;
-    // Jump forward to be unpredicted by the branch predictor.
+        // Jump forward to be unpredicted by the branch predictor.
     StackUnderflow:
         return EvmExceptionType.StackUnderflow;
     }
