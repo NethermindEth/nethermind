aaac87bac9bc176135facede637663c0b801420b
Lukasz Rozmej <lukasz.rozmej@gmail.com>
2026-04-22 11:45:49 +0200

    Update src/Nethermind/Nethermind.JsonRpc.Test/Modules/Eth/EthRpcModuleTests.EstimateGasMissingBlock.cs

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/Eth/EthRpcModuleTests.EstimateGasMissingBlock.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/Eth/EthRpcModuleTests.EstimateGasMissingBlock.cs
@@ -12,37 +12,23 @@ namespace Nethermind.JsonRpc.Test.Modules.Eth;
 
 public partial class EthRpcModuleTests
 {
-    [TestCase("0xFFFFFFFF")]
-    [TestCase("0x123456")]
-    public async Task Eth_estimateGas_returns_geth_compatible_error_for_missing_block(string blockNumber)
+    [TestCase("0xFFFFFFFF", "block not found: 0xffffffff")]
+    [TestCase("0x123456", "block not found: 0x123456")]
+    [TestCase(
+        "0xf0b3f69cbd4e1e8d9b0ef02ff5d1384d18e19d251a4052f5f90bab190c5e8937",
+        "block not found: 0xf0b3f69cbd4e1e8d9b0ef02ff5d1384d18e19d251a4052f5f90bab190c5e8937")]
+    public async Task Eth_estimateGas_returns_geth_compatible_error_for_missing_block(string blockId, string expectedMessage)
     {
         using Context ctx = await Context.Create();
 
         TransactionForRpc transaction = ctx.Test.JsonSerializer.Deserialize<TransactionForRpc>(
             """{"from":"0xa9Ac1233699BDae25abeBae4f9Fb54DbB1b44700","to":"0x252568abdeb9de59fd8963dfcd87be2db65f1ce1","gasPrice":"0xBA43B7400"}""");
 
-        string serialized = await ctx.Test.TestEthRpc("eth_estimateGas", transaction, blockNumber);
+        string serialized = await ctx.Test.TestEthRpc("eth_estimateGas", transaction, blockId);
 
         JObject response = JObject.Parse(serialized);
         response.Should().ContainKey("error");
         response["error"]!["code"]!.Value<int>().Should().Be(-32000);
-        response["error"]!["message"]!.Value<string>().Should().Be($"block not found: 0x{Convert.ToInt64(blockNumber, 16):x}");
-    }
-
-    [Test]
-    public async Task Eth_estimateGas_returns_geth_compatible_error_for_missing_block_hash()
-    {
-        using Context ctx = await Context.Create();
-        string blockHash = "0xf0b3f69cbd4e1e8d9b0ef02ff5d1384d18e19d251a4052f5f90bab190c5e8937";
-
-        TransactionForRpc transaction = ctx.Test.JsonSerializer.Deserialize<TransactionForRpc>(
-            """{"from":"0xa9Ac1233699BDae25abeBae4f9Fb54DbB1b44700","to":"0x252568abdeb9de59fd8963dfcd87be2db65f1ce1","gasPrice":"0xBA43B7400"}""");
-
-        string serialized = await ctx.Test.TestEthRpc("eth_estimateGas", transaction, blockHash);
-
-        JObject response = JObject.Parse(serialized);
-        response.Should().ContainKey("error");
-        response["error"]!["code"]!.Value<int>().Should().Be(-32000);
-        response["error"]!["message"]!.Value<string>().Should().Be($"block not found: {blockHash}");
+        response["error"]!["message"]!.Value<string>().Should().Be(expectedMessage);
     }
 }
