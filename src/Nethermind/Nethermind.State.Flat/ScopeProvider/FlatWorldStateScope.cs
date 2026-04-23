ee14e2952e8c94db2c56856bcab3cd9f5e456af3
Kamil Chodoła <kamil.chodola@gmail.com>
2026-04-23 07:23:54 +0200

    Revert "Avoid snapshot writes for flat account reads"
    This reverts commit 8eb2e83175195bc7d48345d5b09adce66787bf5c.

--- src/Nethermind/Nethermind.State.Flat/ScopeProvider/FlatWorldStateScope.cs
+++ src/Nethermind/Nethermind.State.Flat/ScopeProvider/FlatWorldStateScope.cs
@@ -90,6 +90,8 @@ public sealed class FlatWorldStateScope : IWorldStateScopeProvider.IScope, ITrie
     {
         Account? account = _snapshotBundle.GetAccount(address);
 
+        _snapshotBundle.SetAccount(address, account);
+
         if (_configuration.VerifyWithTrie)
         {
             Account? accTrie = _stateTree.Get(address);
