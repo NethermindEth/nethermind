f6494cb96ffa3db38511fbd38d2beefb25f6b03d
ManuelArto <manuelartociao@gmail.com>
2026-04-24 13:18:41 +0200

    fix(GethLikeCallTracerTests): deduplicate to test-cases

--- src/Nethermind/Nethermind.Evm.Test/Tracing/GethLikeCallTracerTests.cs
+++ src/Nethermind/Nethermind.Evm.Test/Tracing/GethLikeCallTracerTests.cs
@@ -542,42 +542,49 @@ public class GethLikeCallTracerTests : VirtualMachineTestsBase
         Assert.That(callTrace, Is.EqualTo(expectedCallTrace));
     }
 
-    [Test]
-    public void Test_CallTrace_TopLevelCreate_Success()
+    [TestCase(false, false, TestName = "TopLevelCreate_Success")]
+    [TestCase(true,  false, TestName = "TopLevelCreate_Revert")]
+    [TestCase(false, true,  TestName = "TopLevelCreate_AddressCollision")]
+    public void Test_CallTrace_TopLevelCreate(bool revert, bool addressCollision)
     {
-        byte[] deployedCode = new byte[3];
-        byte[] initCode = Prepare.EvmCode.ForInitOf(deployedCode).Done;
+        byte[] initCode;
+        if (addressCollision)
+        {
+            initCode = Prepare.EvmCode.PushData(0).PushData(0).Op(Instruction.RETURN).Done;
+            Address deploymentAddress = ContractAddress.From(Sender, TestState.GetNonce(Sender));
+            TestState.CreateAccount(deploymentAddress, 0);
+            TestState.InsertCode(deploymentAddress, new byte[] { 0xEF }, SpecProvider.GenesisSpec);
+            TestState.Commit(SpecProvider.GenesisSpec);
+        }
+        else
+        {
+            initCode = revert
+                ? Prepare.EvmCode.PushData(0).PushData(0).Op(Instruction.REVERT).Done
+                : Prepare.EvmCode.ForInitOf(new byte[3]).Done;
+        }
 
         (Block block, Transaction tx) = PrepareInitTx(MainnetSpecProvider.CancunActivation, 100000, initCode);
         using NativeCallTracer tracer = new(tx, GetGethTraceOptions(null));
         _processor.Execute(tx, new BlockExecutionContext(block.Header, SpecProvider.GetSpec((block.Header.Number, block.Header.Timestamp))), tracer);
         using GethLikeTxTrace trace = tracer.BuildResult();
 
-        NativeCallTracerCallFrame? frame = trace.CustomTracerResult?.Value as NativeCallTracerCallFrame;
-        Assert.That(frame, Is.Not.Null, "expected a top-level call frame");
-        Assert.That(frame!.Type, Is.EqualTo(Instruction.CREATE));
-        Assert.That(frame.Error, Is.Null, "expected no error on successful CREATE");
-        Assert.That(frame.To, Is.Not.Null, "expected deployed contract address");
-    }
-
-    [Test]
-    public void Test_CallTrace_TopLevelCreate_Revert()
-    {
-        byte[] initCode = Prepare.EvmCode
-            .PushData(0)
-            .PushData(0)
-            .Op(Instruction.REVERT)
-            .Done;
-
-        (Block block, Transaction tx) = PrepareInitTx(MainnetSpecProvider.CancunActivation, 100000, initCode);
-        using NativeCallTracer tracer = new(tx, GetGethTraceOptions(null));
-        _processor.Execute(tx, new BlockExecutionContext(block.Header, SpecProvider.GetSpec((block.Header.Number, block.Header.Timestamp))), tracer);
-        using GethLikeTxTrace trace = tracer.BuildResult();
+        if (addressCollision)
+        {
+            Assert.That(trace.CustomTracerResult, Is.Null,
+                "address-collision path never enters the EVM; no call frame should be produced");
+            return;
+        }
 
         NativeCallTracerCallFrame? frame = trace.CustomTracerResult?.Value as NativeCallTracerCallFrame;
-        Assert.That(frame, Is.Not.Null, "expected a top-level call frame even on revert");
+        Assert.That(frame, Is.Not.Null, "expected a top-level CREATE call frame");
         Assert.That(frame!.Type, Is.EqualTo(Instruction.CREATE));
-        Assert.That(frame.Error, Is.Not.Null, "expected error description on reverted CREATE");
+        if (revert)
+            Assert.That(frame.Error, Is.Not.Null, "expected error description on reverted CREATE");
+        else
+        {
+            Assert.That(frame.Error, Is.Null, "expected no error on successful CREATE");
+            Assert.That(frame.To, Is.Not.Null, "expected deployed contract address");
+        }
     }
 
     [Test]
@@ -598,30 +605,6 @@ public class GethLikeCallTracerTests : VirtualMachineTestsBase
         Assert.That(frame, Is.Not.Null, "expected a top-level call frame (ReportAction populated _callStack)");
         Assert.That(frame!.Type, Is.EqualTo(Instruction.CREATE));
         Assert.That(frame.Error, Is.Null, "no EVM exception fired; Error must stay null when _error is null");
-    }
-
-    [Test]
-    public void Test_CallTrace_TopLevelCreate_AddressCollision_NoCrash()
-    {
-        byte[] initCode = Prepare.EvmCode
-            .PushData(0)
-            .PushData(0)
-            .Op(Instruction.RETURN)
-            .Done;
-
-        Address deploymentAddress = ContractAddress.From(Sender, TestState.GetNonce(Sender));
-        TestState.CreateAccount(deploymentAddress, 0);
-        TestState.InsertCode(deploymentAddress, new byte[] { 0xEF }, SpecProvider.GenesisSpec);
-        TestState.Commit(SpecProvider.GenesisSpec);
-
-        (Block block, Transaction tx) = PrepareInitTx(MainnetSpecProvider.CancunActivation, 100000, initCode);
-        using NativeCallTracer tracer = new(tx, GetGethTraceOptions(null));
-
-        _processor.Execute(tx, new BlockExecutionContext(block.Header, SpecProvider.GetSpec((block.Header.Number, block.Header.Timestamp))), tracer);
-        using GethLikeTxTrace trace = tracer.BuildResult();
-
-        Assert.That(trace.CustomTracerResult, Is.Null,
-            "address-collision path never enters the EVM; no call frame should be produced");
     }
 
     [Test]
