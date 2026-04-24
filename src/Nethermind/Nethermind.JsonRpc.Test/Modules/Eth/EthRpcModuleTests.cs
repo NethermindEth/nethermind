d9bcdb34341ab11e9dd1d89ed649325417de979c
stavrosvl7 <stavrosvl7@gmail.com>
2026-04-24 03:44:59 +0300

    eth_getBalance test

--- src/Nethermind/Nethermind.JsonRpc.Test/Modules/Eth/EthRpcModuleTests.cs
+++ src/Nethermind/Nethermind.JsonRpc.Test/Modules/Eth/EthRpcModuleTests.cs
@@ -554,6 +554,15 @@ public partial class EthRpcModuleTests
         Assert.That(serialized, Is.EqualTo("{\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32602,\"message\":\"Invalid params\"},\"id\":67}"));
     }
 
+    [TestCase("0xFFFFFFFFF")]
+    [TestCase("0x99999999999999")]
+    public async Task Eth_get_balance_future_block_returns_header_not_found(string blockParameter)
+    {
+        using Context ctx = await Context.Create();
+        string serialized = await ctx.Test.TestEthRpc("eth_getBalance", TestItem.AddressA.Bytes.ToHexString(true), blockParameter);
+        Assert.That(serialized, Is.EqualTo("{\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32000,\"message\":\"header not found\"},\"id\":67}"));
+    }
+
     [Test]
     public async Task Eth_syncing_true()
     {
