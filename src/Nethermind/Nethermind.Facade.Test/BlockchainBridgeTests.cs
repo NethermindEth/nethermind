737b67a9882a98f5df34dd65aaf467dab3a1d89e
stavrosvl7 <stavrosvl7@gmail.com>
2026-04-27 15:16:03 +0300

    fix tests

--- src/Nethermind/Nethermind.Facade.Test/BlockchainBridgeTests.cs
+++ src/Nethermind/Nethermind.Facade.Test/BlockchainBridgeTests.cs
@@ -322,7 +322,7 @@ public class BlockchainBridgeTests
 
         CallOutput callOutput = _blockchainBridge.Call(header, tx);
 
-        Assert.That(callOutput.Error, Is.EqualTo("insufficient sender balance"));
+        Assert.That(callOutput.Error, Is.EqualTo("insufficient funds for transfer"));
     }
 
     [Test]
@@ -336,7 +336,7 @@ public class BlockchainBridgeTests
 
         CallOutput callOutput = _blockchainBridge.EstimateGas(header, tx, 1);
 
-        Assert.That(callOutput.Error, Is.EqualTo("insufficient sender balance"));
+        Assert.That(callOutput.Error, Is.EqualTo("insufficient funds for transfer"));
     }
 
     [Test]
