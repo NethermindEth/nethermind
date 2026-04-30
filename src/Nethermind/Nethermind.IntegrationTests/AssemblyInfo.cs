2bcc78e1577847e654a54afaddfaef6a222c837f
Dmytro Biloshytskyi <dmitriy.biloshytskiy@gmail.com>
2026-04-30 15:52:08 +0300

    Added CI workflow draft

--- src/Nethermind/Nethermind.IntegrationTests/AssemblyInfo.cs
+++ src/Nethermind/Nethermind.IntegrationTests/AssemblyInfo.cs
@@ -1,3 +1,10 @@
 using NUnit.Framework;
 
+// Default worker count for parallel test execution. Override at runtime by passing
+// a .runsettings file:
+//
+//   <RunSettings><NUnit><NumberOfTestWorkers>N</NumberOfTestWorkers></NUnit></RunSettings>
+//
+// then `dotnet test ... --settings path/to.runsettings`. The runsettings value
+// takes precedence over this assembly attribute.
 [assembly: LevelOfParallelism(4)]
