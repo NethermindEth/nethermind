5c11699c541607e190ba69ffea74c35610997c5a
Ben Adams <thundercat@illyriad.co.uk>
2026-05-01 13:46:30 +0200

    Drop unused System.Collections.Generic / System.Linq usings (IDE0005)

--- src/Nethermind/Nethermind.Consensus/Processing/BlockAccessListManager.cs
+++ src/Nethermind/Nethermind.Consensus/Processing/BlockAccessListManager.cs
@@ -2,9 +2,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Linq;
 using Nethermind.Blockchain;
 using System.Collections.Concurrent;
 using Nethermind.Core.Caching;
