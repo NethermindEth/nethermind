fc7a9338ad7eab8462707df8516d9f5719bd3b1b
Alex Bespalov <alexb5dh@gmail.com>
2026-05-05 02:27:46 +0300

    Fix for tx with trailing bytes in execution payload

--- src/Nethermind/Nethermind.JsonRpc/Modules/Trace/TraceRpcModule.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Trace/TraceRpcModule.cs
@@ -120,7 +120,7 @@ namespace Nethermind.JsonRpc.Modules.Trace
         public ResultWrapper<ParityTxTraceFromReplay> trace_rawTransaction(byte[] data, string[] traceTypes)
         {
             Rlp.ValueDecoderContext ctx = data.AsRlpValueContext();
-            Transaction tx = _txDecoder.Decode(ref ctx, RlpBehaviors.SkipTypedWrapping);
+            Transaction tx = _txDecoder.DecodeCompleteNotNull(ref ctx, RlpBehaviors.SkipTypedWrapping);
             tx.CapGasLimit(jsonRpcConfig.GasCap);
             return TraceTx(tx, traceTypes, BlockParameter.Latest);
         }
