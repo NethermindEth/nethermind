0a9d75460e312cc4e6781173fb824c72546508bc
lukasz.rozmej <lukasz.rozmej@gmail.com>
2026-05-08 09:51:00 +0200

    simplify ForkchoiceUpdatedSszHandler

--- src/Nethermind/Nethermind.Merge.Plugin/SszRest/Handlers/ForkchoiceUpdatedSszHandler.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/SszRest/Handlers/ForkchoiceUpdatedSszHandler.cs
@@ -20,35 +20,37 @@ namespace Nethermind.Merge.Plugin.SszRest.Handlers;
 /// </summary>
 public sealed class ForkchoiceUpdatedSszHandler(IEngineRpcModule engineModule) : SszEndpointHandlerBase
 {
-    private readonly IEngineRpcModule _engineModule = engineModule;
-
     public override string HttpMethod => "POST";
     public override string Resource => "forkchoice";
 
     public override async Task HandleAsync(HttpContext ctx, int version, string extra, ReadOnlyMemory<byte> body)
     {
-        if (version > EngineApiVersions.Fcu.V4)
-        {
-            await WriteSszResultAsync(ctx,
-                ResultWrapper<ForkchoiceUpdatedV1Result>.Fail(
-                    $"Unsupported engine_forkchoiceUpdated version: {version}", ErrorCodes.MethodNotFound),
-                SszCodec.EncodeForkchoiceUpdatedResponse);
-            return;
-        }
+        Task<ResultWrapper<ForkchoiceUpdatedV1Result>> call =
+            version is > EngineApiVersions.Fcu.V4 or < EngineApiVersions.Fcu.V1
+                ? FailToResolveVersion(version)
+                : Call(version, body);
 
+        await WriteSszResultAsync(ctx, await call, SszCodec.EncodeForkchoiceUpdatedResponse);
+    }
+
+    private Task<ResultWrapper<ForkchoiceUpdatedV1Result>> Call(int version, ReadOnlyMemory<byte> body)
+    {
         (ForkchoiceStateV1 state, PayloadAttributes? attrs) =
             SszCodec.DecodeForkchoiceUpdatedRequest(body.Span, version);
 
         Task<ResultWrapper<ForkchoiceUpdatedV1Result>> call = version switch
         {
-            EngineApiVersions.Fcu.V1 => _engineModule.engine_forkchoiceUpdatedV1(state, attrs),
-            EngineApiVersions.Fcu.V2 => _engineModule.engine_forkchoiceUpdatedV2(state, attrs),
-            EngineApiVersions.Fcu.V3 => _engineModule.engine_forkchoiceUpdatedV3(state, attrs),
-            EngineApiVersions.Fcu.V4 => _engineModule.engine_forkchoiceUpdatedV4(state, attrs),
-            _ => Task.FromResult(ResultWrapper<ForkchoiceUpdatedV1Result>.Fail(
-                $"Unsupported engine_forkchoiceUpdated version: {version}", ErrorCodes.MethodNotFound))
+            EngineApiVersions.Fcu.V1 => engineModule.engine_forkchoiceUpdatedV1(state, attrs),
+            EngineApiVersions.Fcu.V2 => engineModule.engine_forkchoiceUpdatedV2(state, attrs),
+            EngineApiVersions.Fcu.V3 => engineModule.engine_forkchoiceUpdatedV3(state, attrs),
+            EngineApiVersions.Fcu.V4 => engineModule.engine_forkchoiceUpdatedV4(state, attrs),
+            _ => FailToResolveVersion(version)
         };
 
-        await WriteSszResultAsync(ctx, await call, SszCodec.EncodeForkchoiceUpdatedResponse);
+        return call;
     }
+
+    private static Task<ResultWrapper<ForkchoiceUpdatedV1Result>> FailToResolveVersion(int version) =>
+        Task.FromResult(ResultWrapper<ForkchoiceUpdatedV1Result>.Fail(
+            $"Unsupported engine_forkchoiceUpdated version: {version}", ErrorCodes.MethodNotFound));
 }
