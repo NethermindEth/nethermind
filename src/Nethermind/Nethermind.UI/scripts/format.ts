5b8e49ac984abb3c14b145f11f04ee1b03ed3483
Lautaro Emanuel <emlautarom1@gmail.com>
2026-05-18 12:27:47 -0300

    Remove Base

--- src/Nethermind/Nethermind.UI/scripts/format.ts
+++ src/Nethermind/Nethermind.UI/scripts/format.ts
@@ -180,7 +180,6 @@ const networks = {
   "1": "Ethereum mainnet",
   "100": "Gnosis Mainnet",
   "480": "World Mainnet",
-  "8453": "Base mainnet",
   "10200": "Gnosis Chiado Testnet",
   "7032118028": "Ethereum Perfnet"
 }
