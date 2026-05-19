36bd52c5cdfd15bcd7d2b06cee6957366333c0b7
Alexey Osipov <me@flcl.me>
2026-05-19 11:52:39 +0300

    Remove remaining discovery test lint import

--- src/Nethermind/Nethermind.Network.Discovery.Test/Kademlia/Hash256XorUtilsTests.cs
+++ src/Nethermind/Nethermind.Network.Discovery.Test/Kademlia/Hash256XorUtilsTests.cs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Linq;
 using System.Numerics;
 using System.Runtime.Intrinsics;
 using Nethermind.Core.Crypto;
