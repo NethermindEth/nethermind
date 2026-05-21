6ed98106484bf691fb979acc17147217f724c19b
BZO95 <deffie.jnkiee@gmail.com>
2026-05-21 20:59:06 +0800

    Test code-deposit halt gas value

--- src/Nethermind/Nethermind.Evm.Test/Eip8037Tests.cs
+++ src/Nethermind/Nethermind.Evm.Test/Eip8037Tests.cs
@@ -474,12 +474,14 @@ public class Eip8037Tests : VirtualMachineTestsBase
     [Test]
     public void Code_deposit_halt_removes_merged_child_state_usage_without_refunding_reservoir_twice()
     {
+        long parentRegularGas = 1_000;
+        long childRegularGas = 500;
         long parentStateGasUsed = GasCostOf.CreateState;
         long childStateGasUsed = GasCostOf.NewAccountState + GasCostOf.SSetState;
         long childRemainingStateReservoir = 123;
         EthereumGasPolicy parent = new()
         {
-            Value = 1_000,
+            Value = parentRegularGas,
             StateReservoir = parentStateGasUsed + childStateGasUsed + childRemainingStateReservoir,
             StateGasSpill = 77,
             StateGasSpillRefunded = 33,
@@ -488,7 +490,7 @@ public class Eip8037Tests : VirtualMachineTestsBase
         };
 
         Assert.That(EthereumGasPolicy.ConsumeStateGas(ref parent, parentStateGasUsed), Is.True);
-        EthereumGasPolicy child = EthereumGasPolicy.CreateChildFrameGas(ref parent, 500);
+        EthereumGasPolicy child = EthereumGasPolicy.CreateChildFrameGas(ref parent, childRegularGas);
         Assert.That(EthereumGasPolicy.ConsumeStateGas(ref child, childStateGasUsed), Is.True);
         EthereumGasPolicy.Refund(ref parent, in child);
 
@@ -496,6 +498,7 @@ public class Eip8037Tests : VirtualMachineTestsBase
 
         Assert.That(
             (
+                parent.Value,
                 parent.StateReservoir,
                 parent.StateGasUsed,
                 parent.StateGasSpill,
@@ -505,6 +508,7 @@ public class Eip8037Tests : VirtualMachineTestsBase
             ),
             Is.EqualTo(
                 (
+                    parentRegularGas + childRegularGas,
                     childRemainingStateReservoir + childStateGasUsed,
                     parentStateGasUsed,
                     77L,
