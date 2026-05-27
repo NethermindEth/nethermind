8d3d0e350505ea8c969af23dffdb958c82ee4512
Ben Adams <thundercat@illyriad.co.uk>
2026-05-27 09:24:15 +0100

    Cover empty-code call balance semantics

--- src/Nethermind/Nethermind.Evm.Test/CallTests.cs
+++ src/Nethermind/Nethermind.Evm.Test/CallTests.cs
@@ -1,6 +1,16 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
+using FluentAssertions;
+using Nethermind.Blockchain;
+using Nethermind.Core;
+using Nethermind.Core.Extensions;
+using Nethermind.Core.Specs;
+using Nethermind.Core.Test.Builders;
+using Nethermind.Evm.TransactionProcessing;
+using Nethermind.Evm.Tracing;
+using Nethermind.Int256;
 using Nethermind.Specs;
 using NUnit.Framework;
 
@@ -51,5 +61,35 @@ namespace Nethermind.Evm.Test
             TestAllTracerWithOutput result = Execute(Activation, 21020, code);
             Assert.That(result.Error, Is.EqualTo("OutOfGas"));
         }
+
+        [TestCase(Instruction.CALL, 99, 1)]
+        [TestCase(Instruction.CALLCODE, 100, 0)]
+        [TestCase(Instruction.DELEGATECALL, 100, 0)]
+        [TestCase(Instruction.STATICCALL, 100, 0)]
+        public void Empty_code_call_preserves_balance_semantics(Instruction instruction, int expectedRecipientBalance, int expectedTargetBalance)
+        {
+            Address target = TestItem.AddressC;
+            byte[] code = BuildEmptyCodeCall(instruction, target);
+            (Block block, Transaction transaction) = PrepareTx(Activation, 100_000, code, value: 0);
+
+            TransactionResult result = _processor.Execute(
+                transaction,
+                new BlockExecutionContext(block.Header, SpecProvider.GetSpec(block.Header)),
+                NullTxTracer.Instance);
+
+            result.TransactionExecuted.Should().BeTrue();
+            TestState.GetBalance(Recipient).Should().Be((UInt256)expectedRecipientBalance * 1.Ether);
+            TestState.GetBalance(target).Should().Be((UInt256)expectedTargetBalance * 1.Ether);
+        }
+
+        private static byte[] BuildEmptyCodeCall(Instruction instruction, Address target) =>
+            instruction switch
+            {
+                Instruction.CALL => Prepare.EvmCode.CallWithValue(target, 50_000, 1.Ether).Done,
+                Instruction.CALLCODE => Prepare.EvmCode.CallCode(target, 50_000, 1.Ether).Done,
+                Instruction.DELEGATECALL => Prepare.EvmCode.DelegateCall(target, 50_000).Done,
+                Instruction.STATICCALL => Prepare.EvmCode.StaticCall(target, 50_000).Done,
+                _ => throw new ArgumentOutOfRangeException(nameof(instruction), instruction, null)
+            };
     }
 }
