31c7d04aa40b1dd5c216740695f756e268e6a666
Dyslex7c <polynsominiac@gmail.com>
2026-05-27 14:55:31 +0530

    update known failing tests

--- src/Nethermind/Ethereum.Blockchain.Pyspec.Zkevm.Test/Tests.cs
+++ src/Nethermind/Ethereum.Blockchain.Pyspec.Zkevm.Test/Tests.cs
@@ -1,23 +1,31 @@
 // SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Ethereum.Test.Base;
 using NUnit.Framework;
 
 namespace Ethereum.Blockchain.Pyspec.Zkevm.Test;
 
-file static class SkippedTests
+file static class KnownFailingTests
 {
-    public static readonly HashSet<string> Names =
-    [
-        "test_bal_7002_partial_sweep[fork_Amsterdam-blockchain_test_engine]",
-        "test_bal_7702_delegated_storage_access[fork_Amsterdam-blockchain_test_engine]",
-        "test_bal_7702_delegation_clear[fork_Amsterdam-blockchain_test_engine-self_funded]",
-        "test_bal_7702_delegation_update[fork_Amsterdam-blockchain_test_engine-self_funded]",
-        "test_bal_create2_collision[fork_Amsterdam-blockchain_test_engine]",
-    ];
+    public static readonly HashSet<string> Names = Load();
+
+    private static HashSet<string> Load()
+    {
+        string path = Path.Combine(AppContext.BaseDirectory, "known-failing-zkevm-tests.txt");
+        if (!File.Exists(path))
+            return [];
+
+        return File.ReadLines(path)
+            .Select(l => l.Trim())
+            .Where(l => l.Length > 0 && !l.StartsWith('#'))
+            .ToHashSet();
+    }
 }
 
 [TestFixture(false)]
@@ -29,7 +37,7 @@ public class Eip7928BlockChainTests(bool parallel) : ZkEvmBlockChainTestFixture
     [TestCaseSource(nameof(LoadTests))]
     public async Task Test(BlockchainTest test)
     {
-        if (SkippedTests.Names.Contains(test.Name))
+        if (KnownFailingTests.Names.Contains(test.Name))
             Assert.Ignore($"Test '{test.Name}' is temporarily skipped pending investigation.");
         Assert.That((await RunTest(test)).Pass, Is.True);
     }
@@ -47,7 +55,7 @@ public class Eip7928EngineBlockChainTests(bool parallel) : ZkEvmBlockChainTestFi
     [TestCaseSource(nameof(LoadTests))]
     public async Task Test(BlockchainTest test)
     {
-        if (SkippedTests.Names.Contains(test.Name))
+        if (KnownFailingTests.Names.Contains(test.Name))
             Assert.Ignore($"Test '{test.Name}' is temporarily skipped pending investigation.");
         Assert.That((await RunTest(test)).Pass, Is.True);
     }
@@ -65,7 +73,7 @@ public class Eip7928WitnessEngineBlockChainTests(bool parallel) : ZkEvmWitnessEn
     [TestCaseSource(nameof(LoadTests))]
     public async Task Test(BlockchainTest test)
     {
-        if (SkippedTests.Names.Contains(test.Name))
+        if (KnownFailingTests.Names.Contains(test.Name))
             Assert.Ignore($"Test '{test.Name}' is temporarily skipped pending investigation.");
         Assert.That((await RunTest(test)).Pass, Is.True);
     }
