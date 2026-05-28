eb0e4daed43c12451e0a8be400eea282b1e5e4ab
peter941221 <peter941221@gmail.com>
2026-05-28 16:23:06 +0800

    style: drop unused jsonrpc import

--- src/Nethermind/Nethermind.JsonRpc/JsonRpcService.cs
+++ src/Nethermind/Nethermind.JsonRpc/JsonRpcService.cs
@@ -14,7 +14,6 @@ using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
 using System.Threading.Tasks;
 using Nethermind.Blockchain;
-using Nethermind.Blockchain.Find;
 using Nethermind.Core.Exceptions;
 using Nethermind.JsonRpc.Exceptions;
 using Nethermind.JsonRpc.Modules;
