f5cf03399a571bded4ddedf8868947ad29c37d85
Alex Bespalov <alexb5dh@gmail.com>
2026-05-30 05:53:56 +0300

    Add known addresses and topics

--- tools/RpcTests/RpcTestsMon/RpcTests.Monitor/TestContext.cs
+++ tools/RpcTests/RpcTestsMon/RpcTests.Monitor/TestContext.cs
@@ -14,8 +14,13 @@ internal readonly record struct TestContext(TestDefinition Definition, BlockInfo
 
     #region Helper Methods
 
-    public static string Hex(long n) => $"0x{n:x}";
     public long RecentBlock => Math.Max(1, Head.Number - 5); // recent, but old enough to assume available in all nodes
+
+    public static string Hex(long n) => $"0x{n:x}";
+    public static string AsTopic(string address) => address.StartsWith("0x") ? address[2..].PadLeft(64, '0') : address.PadLeft(64, '0');
+
+    public static KnownAddresses Address { get; } = new();
+    public static KnownTopics Topic { get; } = new();
 
     #endregion
 
@@ -30,4 +35,16 @@ internal readonly record struct TestContext(TestDefinition Definition, BlockInfo
 internal record TestFailure(TestContext Test, JsonNode Request, JsonNode ActualResponse, JsonNode ExpectedResponse)
 {
     public BlockInfo Head => Test.Head;
+}
+
+internal class KnownAddresses
+{
+    public string WETH => "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    public string USDT => "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+}
+
+internal class KnownTopics
+{
+    public string Transfer => "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+    public string Approval => "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925";
 }
