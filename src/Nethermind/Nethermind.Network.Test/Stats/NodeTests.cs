15cc266780939e01373cb998be138fdd1b24082a
Alexey Osipov <me@flcl.me>
2026-06-04 20:49:27 +0300

    Clean NodeTests imports

--- src/Nethermind/Nethermind.Network.Test/Stats/NodeTests.cs
+++ src/Nethermind/Nethermind.Network.Test/Stats/NodeTests.cs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using System.Net;
-using Nethermind.Core.Crypto;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Crypto;
 using Nethermind.Network.Enr;
