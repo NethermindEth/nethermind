7e377a5ff3371dd8a90f24eb9259fef79b3a8785
Ruben Buniatyan <rubo@users.noreply.github.com>
2026-06-05 19:00:23 +0200

    Fix fork index mapping

--- src/Nethermind/Nethermind.Stateless.Executor/StatelessSpecProvider.cs
+++ src/Nethermind/Nethermind.Stateless.Executor/StatelessSpecProvider.cs
@@ -3,7 +3,6 @@
 
 using Nethermind.Core.Specs;
 using Nethermind.Int256;
-using Nethermind.Specs.Forks;
 using Nethermind.Stateless.Execution.IO;
 
 namespace Nethermind.Stateless.Execution;
@@ -40,7 +39,7 @@ internal sealed class StatelessSpecProvider(
 
     public static StatelessSpecProvider Create(IForkAwareSpecProvider baseProvider, ForkConfig forkConfig)
     {
-        string? forkName = GetForkNameByIndex(forkConfig.Fork);
+        string? forkName = ForkIndexHelper.GetForkNameByIndex(forkConfig.Fork);
 
         if (forkName is null || !baseProvider.TryGetForkSpec(forkName, out IReleaseSpec? spec))
             throw new ArgumentException($"Unknown fork: {forkConfig.Fork}", nameof(forkConfig));
@@ -51,32 +50,6 @@ internal sealed class StatelessSpecProvider(
 
         return new(baseProvider, forkConfig.Activation.ToForkActivation(), spec!);
     }
-
-    private static string? GetForkNameByIndex(ulong index) => index switch
-    {
-        0 => Frontier.Instance.Name,
-        1 => Homestead.Instance.Name,
-        2 => Dao.Instance.Name,
-        3 => TangerineWhistle.Instance.Name,
-        4 => SpuriousDragon.Instance.Name,
-        5 => Byzantium.Instance.Name,
-        7 => ConstantinopleFix.Instance.Name, // TODO: -1 for removed Constantinople
-        8 => Istanbul.Instance.Name,
-        9 => MuirGlacier.Instance.Name,
-        10 => Berlin.Instance.Name,
-        11 => London.Instance.Name,
-        12 => ArrowGlacier.Instance.Name,
-        13 => GrayGlacier.Instance.Name,
-        14 => Paris.Instance.Name,
-        15 => Shanghai.Instance.Name,
-        16 => Cancun.Instance.Name,
-        17 => Prague.Instance.Name,
-        18 => Osaka.Instance.Name,
-        19 => BPO1.Instance.Name,
-        20 => BPO2.Instance.Name,
-        24 => Amsterdam.Instance.Name,  // TODO: -3 for removed BPOs
-        _ => null
-    };
 
     private sealed class StatelessReleaseSpec(IReleaseSpec spec, BlobSchedule blobSchedule) : ReleaseSpecDecorator(spec)
     {
