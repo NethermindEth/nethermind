c3363180e81717c19ecfb3b2f43427e55337e60a
Kamil Chodoła <kamil.chodola@gmail.com>
2026-06-07 00:52:58 +0200

    perf(flatdb): batch storage trie warmups

--- src/Nethermind/Nethermind.State.Flat/ScopeProvider/FlatStorageTree.cs
+++ src/Nethermind/Nethermind.State.Flat/ScopeProvider/FlatStorageTree.cs
@@ -103,6 +103,13 @@ public sealed class FlatStorageTree : IWorldStateScopeProvider.IStorageTree, ITr
     // Called by trie warmer.
     public bool WarmUpStorageTrie(UInt256 index, int sequenceId)
     {
+        ValueHash256 key = ValueKeccak.Zero;
+        StorageTree.ComputeKeyWithLookup(index, ref key);
+        return WarmUpStorageTrie(in key, sequenceId);
+    }
+
+    internal bool WarmUpStorageTrie(in ValueHash256 key, int sequenceId)
+    {
         try
         {
             if (_scope.HintSequenceId != sequenceId || _scope._pausePrewarmer)
@@ -112,9 +119,6 @@ public sealed class FlatStorageTree : IWorldStateScopeProvider.IStorageTree, ITr
 
             // Note: storage tree root not changed after write batch. Also not cleared. So the result is not correct.
             // this is just to warm up the nodes.
-            ValueHash256 key = ValueKeccak.Zero;
-            StorageTree.ComputeKeyWithLookup(index, ref key);
-
             _warmupStorageTree.WarmUpPath(key.BytesAsSpan);
             return true;
         }
