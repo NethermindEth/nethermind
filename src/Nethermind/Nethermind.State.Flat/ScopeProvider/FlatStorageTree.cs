57b758546f8b783bca95e009092a4c7670b9f3df
Kamil Chodoła <kamil.chodola@gmail.com>
2026-06-08 00:52:14 +0200

    Revert "perf(flatdb): defer storage trie commits"
    This reverts commit d6ec9bc983539a7e7a503fa6e6d19184f69118cb.

--- src/Nethermind/Nethermind.State.Flat/ScopeProvider/FlatStorageTree.cs
+++ src/Nethermind/Nethermind.State.Flat/ScopeProvider/FlatStorageTree.cs
@@ -159,7 +159,7 @@ public sealed class FlatStorageTree : IWorldStateScopeProvider.IStorageTree, ITr
                 _tree,
                 onRootUpdated,
                 _address,
-                commit: false);
+                commit: true);
 
         return new StorageTreeBulkWriteBatch(
             storageTreeBulkWriteBatch,
