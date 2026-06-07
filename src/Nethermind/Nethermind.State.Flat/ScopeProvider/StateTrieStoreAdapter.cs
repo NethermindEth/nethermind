7e832d30e248d70d3d52206372318037de901ba6
Kamil Chodoła <kamil.chodola@gmail.com>
2026-06-07 09:39:13 +0200

    perf(flatdb): avoid block cache fills in trie warmer

--- src/Nethermind/Nethermind.State.Flat/ScopeProvider/StateTrieStoreAdapter.cs
+++ src/Nethermind/Nethermind.State.Flat/ScopeProvider/StateTrieStoreAdapter.cs
@@ -62,7 +62,7 @@ internal sealed class StateTrieStoreWarmerAdapter(
     }
 
     public override byte[]? TryLoadRlp(in TreePath path, Hash256 hash, ReadFlags flags = ReadFlags.None) =>
-        bundle.TryLoadStateRlp(path, hash, flags);
+        bundle.TryLoadStateRlp(path, hash, flags | ReadFlags.HintCacheMiss);
 
     public override ITrieNodeResolver GetStorageTrieNodeResolver(Hash256? address)
     {
@@ -111,5 +111,5 @@ internal sealed class StorageTrieStoreWarmerAdapter(
     }
 
     public override byte[]? TryLoadRlp(in TreePath path, Hash256 hash, ReadFlags flags = ReadFlags.None) =>
-        bundle.TryLoadStorageRlp(addressHash, in path, hash, flags);
+        bundle.TryLoadStorageRlp(addressHash, in path, hash, flags | ReadFlags.HintCacheMiss);
 }
