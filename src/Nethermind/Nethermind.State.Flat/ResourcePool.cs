d2c4bd7fcb1e8669c91e761df34fb5531812fa06
Kamil Chodoła <kamil.chodola@gmail.com>
2026-06-07 17:06:23 +0200

    perf(flatdb): update cached resource sizing hint

--- src/Nethermind/Nethermind.State.Flat/ResourcePool.cs
+++ src/Nethermind/Nethermind.State.Flat/ResourcePool.cs
@@ -166,10 +166,8 @@ public class ResourcePool(IFlatDbConfig flatConfig) : IResourcePool
         public void ReturnCachedResource(TransientResource transientResource)
         {
             Metrics.ActivePooledResource.AddBy(_cachedResourceLabel, -1);
-            if (!_cachedResourcePool.Return(transientResource))
-            {
-                _lastCachedResourceSize = transientResource.GetSize();
-            }
+            _lastCachedResourceSize = transientResource.GetSize();
+            _cachedResourcePool.Return(transientResource);
 
             Metrics.CachedPooledResource[_cachedResourceLabel] = (long)_cachedResourcePool.PooledItemCount;
         }
