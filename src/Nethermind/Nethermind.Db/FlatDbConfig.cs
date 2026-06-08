1530362118c6ecf9fbe44a30d7d7a1ee725d3c65
Kamil Chodoła <kamil.chodola@gmail.com>
2026-06-08 13:00:50 +0200

    bench: ablate trie warmer workers

--- src/Nethermind/Nethermind.Db/FlatDbConfig.cs
+++ src/Nethermind/Nethermind.Db/FlatDbConfig.cs
@@ -19,7 +19,7 @@ public class FlatDbConfig : IFlatDbConfig
     public int MaxInFlightCompactJob { get; set; } = 32;
     public int MaxReorgDepth { get; set; } = 256;
     public int MinReorgDepth { get; set; } = 128;
-    public int TrieWarmerWorkerCount { get; set; } = 4;
+    public int TrieWarmerWorkerCount { get; set; } = -1;
     public long BlockCacheSizeBudget { get; set; } = 2.GiB;
     public long TrieCacheMemoryBudget { get; set; } = 512.MiB;
 }
