4f0eb073d809ecada438eaa7f72beff2e10e64d7
lukasz.rozmej <lukasz.rozmej@gmail.com>
2026-06-10 16:59:44 +0200

    fixup! refactor(flat): persist the BAL warm-read pool across blocks

--- src/Nethermind/Nethermind.State.Flat/ScopeProvider/WarmReadPool.cs
+++ src/Nethermind/Nethermind.State.Flat/ScopeProvider/WarmReadPool.cs
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Threading;
-
 namespace Nethermind.State.Flat.ScopeProvider;
 
 /// <summary>
