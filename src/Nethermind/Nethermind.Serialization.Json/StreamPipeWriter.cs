ed565091b58ba91e76d7858cb615976b49eab924
Alex Bespalov <alexb5dh@gmail.com>
2026-06-16 00:35:31 +0300

    Code cleanup

--- src/Nethermind/Nethermind.Serialization.Json/StreamPipeWriter.cs
+++ src/Nethermind/Nethermind.Serialization.Json/StreamPipeWriter.cs
@@ -6,7 +6,6 @@ using System.Buffers;
 using System.Diagnostics;
 using System.IO.Pipelines;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
