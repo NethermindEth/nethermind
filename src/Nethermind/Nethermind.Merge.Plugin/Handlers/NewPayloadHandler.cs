6a68e3b8a4b482abf69c14f299564b963df5aa7b
DarkLord017 <sambhavjain170944@gmail.com>
2026-06-21 13:07:26 +0530

    fix linting

--- src/Nethermind/Nethermind.Merge.Plugin/Handlers/NewPayloadHandler.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Handlers/NewPayloadHandler.cs
@@ -131,8 +131,6 @@ public sealed class NewPayloadHandler : IAsyncHandler<ExecutionPayload, PayloadS
         {
             if (_logger.IsWarn) _logger.Warn(InvalidBlockHelper.GetMessage(block, "invalid block hash"));
             Nethermind.Blockchain.Metrics.BadBlocks++;
-            if (block.IsByNethermindNode()) Nethermind.Blockchain.Metrics.BadBlocksByNethermindNodes++;
-            _blockTree.ReportBadBlock(block);
             return NewPayloadV1Result.Invalid(null, $"Invalid block hash {request.BlockHash} does not match calculated hash {actualHash}.");
         }
 
