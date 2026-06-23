14ec87f25212489ef7b3aff8e43c53678b56e0ae
Hugo Demeyere <demeyere.hugo@gmail.com>
2026-06-23 18:48:17 +0900

    chore: Restore single line BALManager DI registration

--- src/Nethermind/Nethermind.Init/Modules/BlockProcessingModule.cs
+++ src/Nethermind/Nethermind.Init/Modules/BlockProcessingModule.cs
@@ -62,16 +62,7 @@ public class BlockProcessingModule(IInitConfig initConfig, IBlocksConfig blocksC
             .AddScoped<IWithdrawalProcessor, WithdrawalProcessor>()
             .AddSingleton<IWithdrawalProcessorFactory, WithdrawalProcessorFactory>()
             .AddScoped<IExecutionRequestsProcessor, ExecutionRequestsProcessor>()
-            .AddScoped<IBlockAccessListManager>(ctx => new BlockAccessListManager(
-                ctx.Resolve<IWorldState>(),
-                ctx.Resolve<ISpecProvider>(),
-                ctx.Resolve<IBlockhashProvider>(),
-                ctx.Resolve<ILogManager>(),
-                ctx.Resolve<IBlocksConfig>(),
-                ctx.Resolve<IWithdrawalProcessorFactory>(),
-                ctx.ResolveOptional<PrewarmerEnvFactory>(),
-                ctx.ResolveOptional<PreBlockCaches>(),
-                ctx.ResolveOptional<IReadOnlyTxProcessingEnvFactory>()))
+            .AddScoped<IBlockAccessListManager, BlockAccessListManager>()
             .AddScoped<IProcessingStats, ProcessingStats>()
             .AddScoped<IBlockchainProcessor, BlockchainProcessor>()
             .AddScoped<IRewardCalculator, IRewardCalculatorSource, ITransactionProcessor>((rewardSource, txP) => rewardSource.Get(txP))
