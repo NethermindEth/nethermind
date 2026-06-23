f23467795a2c0ea3038e05e649f8376142cceb0d
Alexey Osipov <me@flcl.me>
2026-06-23 17:39:54 +0300

    Fix ENR lint warning

--- src/Nethermind/Nethermind.Network.Enr/UnknownEntry.cs
+++ src/Nethermind/Nethermind.Network.Enr/UnknownEntry.cs
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using Nethermind.Serialization.Rlp;
-
 namespace Nethermind.Network.Enr;
 
 internal sealed class UnknownEntry(string key, byte[] rlpValue) : EnrContentEntry<byte[]>(rlpValue)
