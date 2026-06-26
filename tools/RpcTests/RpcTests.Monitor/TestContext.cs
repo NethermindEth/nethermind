61267a9d1e4ef00b4c0b3bdfa4aa2f19ec7d1eae
Alex Bespalov <alexb5dh@gmail.com>
2026-06-26 03:54:30 +0300

    mainnet/eth_getLogs test involving log-index

--- tools/RpcTests/RpcTests.Monitor/TestContext.cs
+++ tools/RpcTests/RpcTests.Monitor/TestContext.cs
@@ -95,4 +95,5 @@ internal class KnownTopics
 {
     public string Transfer => "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
     public string Approval => "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925";
+    public string Withdrawal => "0x7fcf532c15f0a6db0bd6d0e038bea71d30d808c7d98cb3bf7268a95bf5081b65";
 }
