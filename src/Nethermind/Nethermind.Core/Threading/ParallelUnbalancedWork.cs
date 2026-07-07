aa146fa12f95b647253d8cd3de3f47207eaed131
Kamil Chodoła <kamil.chodola@gmail.com>
2026-07-07 23:39:48 +0200

    Revert "perf(core): cap unbalanced workers to available work"
    This reverts commit b1e07217fe1d9e6f877b043a8481f64c3e23ca8d.

--- src/Nethermind/Nethermind.Core/Threading/ParallelUnbalancedWork.cs
+++ src/Nethermind/Nethermind.Core/Threading/ParallelUnbalancedWork.cs
@@ -34,16 +34,7 @@ public class ParallelUnbalancedWork : IThreadPoolWorkItem
     /// <param name="action">The delegate that is invoked once per iteration.</param>
     public static void For(int fromInclusive, int toExclusive, ParallelOptions parallelOptions, Action<int> action)
     {
-        int iterations = toExclusive - fromInclusive;
-        if (iterations <= 0)
-        {
-            parallelOptions.CancellationToken.ThrowIfCancellationRequested();
-            return;
-        }
-
-        int threads = Math.Min(
-            iterations,
-            parallelOptions.MaxDegreeOfParallelism > 0 ? parallelOptions.MaxDegreeOfParallelism : Environment.ProcessorCount);
+        int threads = parallelOptions.MaxDegreeOfParallelism > 0 ? parallelOptions.MaxDegreeOfParallelism : Environment.ProcessorCount;
 
         Data data = new(threads, fromInclusive, toExclusive, action, parallelOptions.CancellationToken);
 
@@ -289,19 +280,10 @@ public class ParallelUnbalancedWork : IThreadPoolWorkItem
             Func<int, TLocal, TLocal> action,
             Action<TLocal>? @finally = null)
         {
-            int iterations = toExclusive - fromInclusive;
-            if (iterations <= 0)
-            {
-                parallelOptions.CancellationToken.ThrowIfCancellationRequested();
-                return;
-            }
-
             // Determine the number of threads to use
-            int threads = Math.Min(
-                iterations,
-                parallelOptions.MaxDegreeOfParallelism > 0
-                    ? parallelOptions.MaxDegreeOfParallelism
-                    : Environment.ProcessorCount);
+            int threads = parallelOptions.MaxDegreeOfParallelism > 0
+                ? parallelOptions.MaxDegreeOfParallelism
+                : Environment.ProcessorCount;
 
             // Create shared data with thread-local initializers and finalizers
             Data<TLocal> data = new(threads, fromInclusive, toExclusive, action, init, initValue, @finally, parallelOptions.CancellationToken);
