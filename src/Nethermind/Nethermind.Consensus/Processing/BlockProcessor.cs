7a2ca38a14e7008fe0cfad9f11f3568e24345264
Kamil Chodoła <kamil.chodola@gmail.com>
2026-07-10 16:01:43 +0200

    perf(state): reuse blooms calculated during early roots

--- src/Nethermind/Nethermind.Consensus/Processing/BlockProcessor.cs
+++ src/Nethermind/Nethermind.Consensus/Processing/BlockProcessor.cs
@@ -158,6 +158,8 @@ public partial class BlockProcessor(
         if (spec.Eip7251ContractAddress is not null) lateStorageWriters.Add(spec.Eip7251ContractAddress);
         _stateProvider.BeginEarlyStorageRoots(lateStorageWriters);
 
+        CalculateBlooms(receipts);
+
         if (spec.IsEip4844Enabled)
         {
             header.BlobGasUsed = BlobGasCalculator.CalculateBlobGas(block.Transactions);
@@ -168,13 +170,11 @@ public partial class BlockProcessor(
         {
             bloomsAndReceiptsRootTask = Task.Run(() =>
             {
-                CalculateBlooms(receipts);
                 return (AccumulateBlockBloom(receipts), CalculateReceiptsRoot(receipts, spec, block));
             });
         }
         else
         {
-            CalculateBlooms(receipts);
             header.ReceiptsRoot = CalculateReceiptsRoot(receipts, spec, block);
         }
 
