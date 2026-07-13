bbaa16b18ea7887eaa20c1a39d9a350de9495ce7
Ruben Buniatyan <rubo@users.noreply.github.com>
2026-07-13 23:42:09 +0200

    Align stateless inputs with tests-zkevm v0.6.2

--- src/Nethermind/Ethereum.Blockchain.Pyspec.Test/ZkEvmFixtures/ZkEvmBlockchainTests.cs
+++ src/Nethermind/Ethereum.Blockchain.Pyspec.Test/ZkEvmFixtures/ZkEvmBlockchainTests.cs
@@ -1,7 +1,10 @@
 // SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
+extern alias stateless;
+
 using System;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,12 +15,16 @@ using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
 using Nethermind.Core.Specs;
 using Nethermind.Int256;
+using Nethermind.Serialization.Ssz;
 using Nethermind.Specs;
 using Nethermind.Specs.Forks;
 using Nethermind.Specs.GnosisForks;
 using Nethermind.Stateless.Execution;
 using Nethermind.Stateless.Execution.IO;
 using NUnit.Framework;
+using StatelessExecutionPayloadV1 = stateless::Nethermind.Merge.Plugin.SszRest.SszExecutionPayloadV1;
+using StatelessExecutionPayloadV3 = stateless::Nethermind.Merge.Plugin.SszRest.SszExecutionPayloadV3;
+using StatelessExecutionPayloadV4 = stateless::Nethermind.Merge.Plugin.SszRest.SszExecutionPayloadV4;
 
 namespace Ethereum.Blockchain.Pyspec.Test.ZkEvmFixtures;
 
@@ -81,6 +88,52 @@ public abstract class ZkEvmBlockchainTestFixture : PyspecLinuxX64BlockchainFixtu
 [TestFixture]
 public class StatelessSchemaTests
 {
+    [TestCase(ProtocolFork.Cancun)]
+    [TestCase(ProtocolFork.Prague)]
+    [TestCase(ProtocolFork.Osaka)]
+    [TestCase(ProtocolFork.BPO1)]
+    [TestCase(ProtocolFork.BPO2)]
+    [TestCase(ProtocolFork.Amsterdam)]
+    public void Revision_1_schema_roundtrips(ProtocolFork fork)
+    {
+        byte[] encoded = fork == ProtocolFork.Amsterdam
+            ? EncodeInput<StatelessExecutionPayloadV4>(fork)
+            : EncodeInput<StatelessExecutionPayloadV3>(fork);
+
+        StatelessPayload payload = InputDecoder.Decode(encoded);
+        bool foundByName = ProtocolForkExtensions.TryGetByName(fork.GetName(), out ProtocolFork forkByName);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(payload.ProtocolFork, Is.EqualTo(fork));
+            Assert.That(payload.ChainConfig.ChainId, Is.EqualTo(1));
+            Assert.That(foundByName, Is.True);
+            Assert.That(forkByName, Is.EqualTo(fork));
+        }
+    }
+
+    [TestCase(0)]
+    [TestCase(1)]
+    public void Schema_prefix_must_be_two_bytes(int length)
+    {
+        byte[] encoded = new byte[length];
+
+        Assert.That(() => InputDecoder.Decode(encoded), Throws.TypeOf<ArgumentOutOfRangeException>());
+    }
+
+    [TestCase(0x0f01)]
+    [TestCase(0x1002)]
+    [TestCase(0x1601)]
+    public void Unsupported_schema_id_is_rejected(int schemaId)
+    {
+        byte[] encoded = new byte[sizeof(ushort)];
+        BinaryPrimitives.WriteUInt16BigEndian(encoded, (ushort)schemaId);
+
+        Assert.That(
+            () => InputDecoder.Decode(encoded),
+            Throws.TypeOf<ArgumentException>().With.Message.Contains($"0x{schemaId:x4}"));
+    }
+
     [TestCase(10UL, 20UL, true)]
     [TestCase(9UL, 20UL, false)]
     [TestCase(10UL, 19UL, false)]
@@ -125,6 +178,56 @@ public class StatelessSchemaTests
             Assert.That(spec.Name, Is.EqualTo(Amsterdam.Instance.Name));
             Assert.That(spec, usesGnosisRules ? Is.SameAs(AmsterdamGnosis.Instance) : Is.SameAs(Amsterdam.Instance));
         }
+    }
+
+    private static byte[] EncodeInput<TExecutionPayload>(ProtocolFork fork)
+        where TExecutionPayload : StatelessExecutionPayloadV1,
+        stateless::Nethermind.Merge.Plugin.SszRest.ISszExecutionPayloadFactory<TExecutionPayload>,
+        ISszCodec<TExecutionPayload>, new()
+    {
+        StatelessInput<TExecutionPayload> input = new()
+        {
+            NewPayloadRequest = new()
+            {
+                ExecutionPayload = new TExecutionPayload(),
+                VersionedHashes = [],
+                ParentBeaconBlockRoot = Hash256.Zero,
+                ExecutionRequests = new()
+                {
+                    Deposits = [],
+                    Withdrawals = [],
+                    Consolidations = [],
+                    BuilderDeposits = [],
+                    BuilderExits = []
+                }
+            },
+            Witness = new()
+            {
+                State = [],
+                Codes = [],
+                Headers = []
+            },
+            ChainConfig = new()
+            {
+                ChainId = 1,
+                ActiveFork = new()
+                {
+                    Activation = new()
+                    {
+                        BlockNumber = [0],
+                        Timestamp = []
+                    }
+                }
+            },
+            PublicKeys = []
+        };
+        byte[] payload = StatelessInput<TExecutionPayload>.Encode(input);
+        byte[] encoded = new byte[sizeof(ushort) + payload.Length];
+
+        BinaryPrimitives.WriteUInt16BigEndian(encoded, fork.ToRevision1SchemaId());
+        payload.AsSpan().CopyTo(encoded.AsSpan(sizeof(ushort)));
+
+        return encoded;
     }
 
     private static BlockHeader CreateHeader(ulong blockNumber, ulong timestamp) => new(
