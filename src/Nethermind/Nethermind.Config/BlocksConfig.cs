20442e23673ff5613431bc4b549a41d17499ad32
Ben Adams <thundercat@illyriad.co.uk>
2026-07-14 03:10:27 +0100

    Revert "feat(prewarm): experimental mid-EVM cancellation of overtaken warm executions"
    This reverts commit a78eb0a6cbd51637e59a7873a2f7d12f849e2cbb.

--- src/Nethermind/Nethermind.Config/BlocksConfig.cs
+++ src/Nethermind/Nethermind.Config/BlocksConfig.cs
@@ -65,8 +65,6 @@ namespace Nethermind.Config
 
         public int MempoolPreWarmConcurrency { get; set; } = 0;
 
-        public PreWarmCancellationMode PreWarmCancellation { get; set; } = PreWarmCancellationMode.None;
-
         public int BlockProductionTimeoutMs { get; set; } = 4_000;
         public double SingleBlockImprovementOfSlot { get; set; } = 0.25;
 
