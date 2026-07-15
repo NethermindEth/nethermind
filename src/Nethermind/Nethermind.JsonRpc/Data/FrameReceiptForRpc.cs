81e629459ff670601ae03352acba526436da29d1
stavrosvl7 <stavrosvl7@gmail.com>
2026-07-15 17:58:49 +0300

    lint

--- src/Nethermind/Nethermind.JsonRpc/Data/FrameReceiptForRpc.cs
+++ src/Nethermind/Nethermind.JsonRpc/Data/FrameReceiptForRpc.cs
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
 // SPDX-License-Identifier: LGPL-3.0-only
 
-using System.Text.Json.Serialization;
 using Nethermind.Core;
 
 namespace Nethermind.JsonRpc.Data;
