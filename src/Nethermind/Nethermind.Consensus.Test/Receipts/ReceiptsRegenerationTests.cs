306068f0efdaff3cca98b6e57d52dee39291e7d7
Stavros Vlachakis <89769224+svlachakis@users.noreply.github.com>
2026-07-27 16:33:21 +0300

    refactor(Receipts): remove unused Evm import

--- src/Nethermind/Nethermind.Consensus.Test/Receipts/ReceiptsRegenerationTests.cs
+++ src/Nethermind/Nethermind.Consensus.Test/Receipts/ReceiptsRegenerationTests.cs
@@ -17,7 +17,6 @@ using Nethermind.Core.Specs;
 using Nethermind.Core.Test.Blockchain;
 using Nethermind.Core.Test.Builders;
 using Nethermind.Crypto;
-using Nethermind.Evm;
 using Nethermind.Logging;
 using Nethermind.Specs;
 using Nethermind.Specs.Forks;
