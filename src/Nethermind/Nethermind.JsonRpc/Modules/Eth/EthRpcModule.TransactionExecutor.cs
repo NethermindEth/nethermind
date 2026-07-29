65919dbb8b6f0aacd2f46318d18b460a053fd924
Kamil Chodoła <kamil.chodola@gmail.com>
2026-07-29 09:14:35 +0200

    Queue EVM RPC calls before execution

--- src/Nethermind/Nethermind.JsonRpc/Modules/Eth/EthRpcModule.TransactionExecutor.cs
+++ src/Nethermind/Nethermind.JsonRpc/Modules/Eth/EthRpcModule.TransactionExecutor.cs
@@ -42,8 +42,6 @@ namespace Nethermind.JsonRpc.Modules.Eth
 
             protected override ResultWrapper<TResult> Execute(BlockHeader header, Transaction tx, Dictionary<Address, AccountOverride>? stateOverride, CancellationToken token)
             {
-                using RpcLimits.EvmExecutionSlot executionSlot = RpcLimits.AcquireEvmExecutionSlot();
-
                 if (stateOverride is not null)
                 {
                     IReleaseSpec spec = specProvider.GetSpec(header);
