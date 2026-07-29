b2a0cd955e1dc2f75cc782952b44b55e6f0c6f1d
Kamil Chodoła <kamil.chodola@gmail.com>
2026-07-29 22:18:01 +0200

    perf(flat): avoid state root task fan-out

--- src/Nethermind/Nethermind.State.Flat/ScopeProvider/FlatWorldStateScope.cs
+++ src/Nethermind/Nethermind.State.Flat/ScopeProvider/FlatWorldStateScope.cs
@@ -137,7 +137,7 @@ public sealed class FlatWorldStateScope : IWorldStateScopeProvider.IScope, ITrie
     }
 
     public Hash256 RootHash => _stateTree.RootHash;
-    public void UpdateRootHash() => _stateTree.UpdateRootHash();
+    public void UpdateRootHash() => _stateTree.UpdateRootHash(canBeParallel: false);
 
     public Account? Get(Address address)
     {
