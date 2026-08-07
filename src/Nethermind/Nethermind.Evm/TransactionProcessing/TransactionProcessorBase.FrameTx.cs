245619f4d8a4a691b7e6ea9edd72ac33cc014a48
AnkushinDaniil <ankushin.daniil42@gmail.com>
2026-08-07 21:46:06 +0400

    style(evm): drop the now unused Core.Extensions using

--- src/Nethermind/Nethermind.Evm/TransactionProcessing/TransactionProcessorBase.FrameTx.cs
+++ src/Nethermind/Nethermind.Evm/TransactionProcessing/TransactionProcessorBase.FrameTx.cs
@@ -4,7 +4,6 @@
 using System;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
-using Nethermind.Core.Extensions;
 using Nethermind.Core.Specs;
 using Nethermind.Evm.CodeAnalysis;
 using Nethermind.Evm.Precompiles;
