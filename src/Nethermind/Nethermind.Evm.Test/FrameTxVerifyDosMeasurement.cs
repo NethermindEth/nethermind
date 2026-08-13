dfdfe46b04508660108ce2c23b7cb52480b97a9b
AnkushinDaniil <ankushin.daniil42@gmail.com>
2026-08-13 16:56:59 +0400

    chore: pin SSH.NET past the advisory and drop unused usings
    NuGetAudit fails restore repo-wide on the transitive SSH.NET 2024.1.0
    (GHSA-q939-rpr3-3284); pin it to the patched 2026.0.0 as master already
    does. Also removes three unused usings that trip IDE0005 in the lint gate.

--- src/Nethermind/Nethermind.Evm.Test/FrameTxVerifyDosMeasurement.cs
+++ src/Nethermind/Nethermind.Evm.Test/FrameTxVerifyDosMeasurement.cs
@@ -17,7 +17,6 @@ using Nethermind.Int256;
 using Nethermind.Logging;
 using Nethermind.Specs;
 using Nethermind.Specs.Forks;
-using Nethermind.State;
 using NUnit.Framework;
 
 namespace Nethermind.Evm.Test;
