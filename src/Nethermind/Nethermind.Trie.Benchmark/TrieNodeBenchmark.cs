8158c986503e8b0cfb62d96adf89403677eca9d8
Alexey Osipov <me@flcl.me>
2026-09-02 15:52:20 +0300

    Add trie benchmarks to benchmark solution

--- src/Nethermind/Nethermind.Trie.Benchmark/TrieNodeBenchmark.cs
+++ src/Nethermind/Nethermind.Trie.Benchmark/TrieNodeBenchmark.cs
@@ -3,18 +3,13 @@
 
 using System.Buffers.Binary;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Jobs;
 using Nethermind.Core.Crypto;
-using Nethermind.Core.Extensions;
-using Nethermind.Db;
-using Nethermind.Logging;
 using Nethermind.Serialization.Rlp;
-using Nethermind.Trie.Pruning;
 
 namespace Nethermind.Trie.Benchmark
 {
     [MemoryDiagnoser]
-    [DryJob(RuntimeMoniker.NetCoreApp31)]
+    [DryJob]
     public class TrieNodeBenchmark
     {
         // public readonly struct Param
@@ -50,17 +45,17 @@ namespace Nethermind.Trie.Benchmark
         // public Param Input { get; set; }
 
         [Benchmark]
-        public TrieNode Just_trie_node_56B() => new(NodeType.Unknown);
+        public TrieNode Just_trie_node() => new(NodeType.Unknown);
 
         [Benchmark]
-        public Keccak Just_keccak_80B() => Keccak.Compute(_bytes);
+        public Hash256 Just_keccak() => Keccak.Compute(_bytes);
 
         private byte[] _bytes = new byte[32];
 
         private long _i = 0;
 
         [Benchmark]
-        public TrieNode Just_trie_node_with_hash_136B()
+        public TrieNode Just_trie_node_with_hash()
         {
             BinaryPrimitives.WriteInt64BigEndian(_bytes, _i);
             TrieNode trieNode = new(NodeType.Unknown, Keccak.Compute(_bytes));
@@ -68,10 +63,10 @@ namespace Nethermind.Trie.Benchmark
         }
 
         [Benchmark]
-        public TrieNode Just_trie_node_with_rlp_120B() => new(NodeType.Unknown, new byte[7]);
+        public TrieNode Just_trie_node_with_rlp() => new(NodeType.Unknown, new byte[7]);
 
         [Benchmark]
-        public TrieNode Just_extension_with_child_96B()
+        public TrieNode Just_extension_with_child()
         {
             TrieNode trieNode = new(NodeType.Extension);
             trieNode.SetChild(0, null);
@@ -79,7 +74,7 @@ namespace Nethermind.Trie.Benchmark
         }
 
         [Benchmark]
-        public TrieNode Just_branch_with_child_208B()
+        public TrieNode Just_branch_with_child()
         {
             TrieNode trieNode = new(NodeType.Branch);
             trieNode.SetChild(0, null);
@@ -87,7 +82,7 @@ namespace Nethermind.Trie.Benchmark
         }
 
         [Benchmark]
-        public TrieNode Just_leaf_with_value_128B()
+        public TrieNode Just_leaf_with_value()
         {
             TrieNode trieNode = new(NodeType.Leaf);
             trieNode.Value = new byte[7];
@@ -95,13 +90,13 @@ namespace Nethermind.Trie.Benchmark
         }
 
         [Benchmark]
-        public HexPrefix Just_hex_prefix_64B() => new(true, new byte[5]);
+        public byte[] Just_hex_prefix() => HexPrefix.ToBytes(new byte[5], true);
 
         [Benchmark]
-        public Rlp Just_rlp_56B() => new(new byte[8]);
+        public Rlp Just_rlp() => new(new byte[8]);
 
         [Benchmark]
-        public Rlp Just_rlp_aligned_56B() => new(new byte[1]);
+        public Rlp Just_rlp_aligned() => new(new byte[1]);
 
     }
 }
