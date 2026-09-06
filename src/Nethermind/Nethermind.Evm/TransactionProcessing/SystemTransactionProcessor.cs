6f265671b76eed4fa58220d66e1c9b8287832a03
Ben Adams <thundercat@illyriad.co.uk>
2026-09-06 23:13:16 +0100

    Assert zero intrinsic state reservoir for system transactions

--- src/Nethermind/Nethermind.Evm/TransactionProcessing/SystemTransactionProcessor.cs
+++ src/Nethermind/Nethermind.Evm/TransactionProcessing/SystemTransactionProcessor.cs
@@ -10,6 +10,7 @@ using Nethermind.Logging;
 using Nethermind.Specs;
 using Nethermind.Evm.State;
 using System;
+using System.Diagnostics;
 
 namespace Nethermind.Evm.TransactionProcessing;
 
@@ -112,6 +113,8 @@ public class SystemTransactionProcessor<TGasPolicy>(
                 : TransactionResult.GasLimitBelowIntrinsicGas;
         }
 
+        Debug.Assert(TGasPolicy.GetStateReservoir(intrinsicGas.Standard) == 0,
+            "System transactions other than SystemCall bypass minimum-gas validation and must have no intrinsic state reservoir.");
         return base.CalculateAvailableGas(tx, spec, in intrinsicGas, out gasAvailable);
     }
 
