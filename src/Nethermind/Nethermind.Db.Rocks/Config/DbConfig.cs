a446d56674a0eb3e7b4e225541bcaa259f09ac98
Kamil Chodola <kamil@nethermind.io>
2026-09-07 17:45:23 +0200

    perf: reuse RocksDB manifest on open

--- src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
+++ src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
@@ -44,6 +44,7 @@ public class DbConfig : IDbConfig
         "compression=kSnappyCompression;" +
         "optimize_filters_for_hits=true;" +
         "advise_random_on_open=true;" +
+        "reuse_manifest_on_open=true;" +
 
         // Target size of each SST file. Increase to reduce number of file. Default is 64MB.
         "target_file_size_base=64000000;" +
