f3e659b957b6d48aebbe1c61cede6a60ccc383b6
Kamil Chodola <kamil@nethermind.io>
2026-09-07 17:47:16 +0200

    perf(rocksdb): test data block hash index

--- src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
+++ src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
@@ -74,6 +74,7 @@ public class DbConfig : IDbConfig
         "block_based_table_factory.index_type=kTwoLevelIndexSearch;" +
         "block_based_table_factory.partition_filters=true;" +
         "block_based_table_factory.metadata_block_size=4096;" +
+        "block_based_table_factory.data_block_index_type=kDataBlockBinaryAndHash;" +
 
         "block_based_table_factory.filter_policy=bloomfilter:10;" +
         "";
