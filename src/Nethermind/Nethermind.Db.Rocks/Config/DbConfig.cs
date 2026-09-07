fd90a4b524b0ed6b5e44ca845c8b4569a0d869dd
Kamil Chodola <kamil@nethermind.io>
2026-09-07 17:46:08 +0200

    perf: precreate RocksDB WAL files asynchronously

--- src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
+++ src/Nethermind/Nethermind.Db.Rocks/Config/DbConfig.cs
@@ -44,6 +44,7 @@ public class DbConfig : IDbConfig
         "compression=kSnappyCompression;" +
         "optimize_filters_for_hits=true;" +
         "advise_random_on_open=true;" +
+        "async_wal_precreate=true;" +
 
         // Target size of each SST file. Increase to reduce number of file. Default is 64MB.
         "target_file_size_base=64000000;" +
