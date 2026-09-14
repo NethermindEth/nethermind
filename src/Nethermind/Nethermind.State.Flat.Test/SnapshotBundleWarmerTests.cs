b1bfe440c2e846fba23db01d8b66f0ec547dd1fb
Amirul Ashraf <asdacap@gmail.com>
2026-09-14 15:36:11 +0800

    chore(test): remove unused using in snapshot warmer tests

--- src/Nethermind/Nethermind.State.Flat.Test/SnapshotBundleWarmerTests.cs
+++ src/Nethermind/Nethermind.State.Flat.Test/SnapshotBundleWarmerTests.cs
@@ -8,7 +8,6 @@ using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
 using Nethermind.Core.Test.Builders;
-using Nethermind.Core.Threading;
 using Nethermind.Db;
 using Nethermind.Evm.State;
 using Nethermind.Int256;
