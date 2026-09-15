84b36f40d98eced17137366c31bfe9919e060a82
lukasz.rozmej <lukasz.rozmej@gmail.com>
2026-09-15 19:09:05 +0200

    test: deflake nonce manager account isolation

--- src/Nethermind/Nethermind.TxPool.Test/NonceManagerTests.cs
+++ src/Nethermind/Nethermind.TxPool.Test/NonceManagerTests.cs
@@ -210,11 +210,14 @@ public class NonceManagerTests
     {
         using NonceLocker locker = _nonceManager.ReserveNonce(TestItem.AddressA, out ulong nonce);
         Assert.That(nonce, Is.EqualTo(0UL));
+        TaskCompletionSource started = new(TaskCreationOptions.RunContinuationsAsynchronously);
         Task task = Task.Run(() =>
         {
+            started.SetResult();
             using NonceLocker locker2 = _nonceManager.ReserveNonce(TestItem.AddressB, out ulong nonce2);
             Assert.That(nonce2, Is.EqualTo(0UL));
         });
+        Assert.That(started.Task.Wait(TimeSpan.FromSeconds(1)), Is.True);
         task.Wait(TimeSpan.FromMilliseconds(10_000));
         Assert.That(task.IsCompleted, Is.EqualTo(true));
     }
