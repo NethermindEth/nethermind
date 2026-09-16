3a7b4e6b92f01c69de418dcbe521ec61d004f1bd
lukasz.rozmej <lukasz.rozmej@gmail.com>
2026-09-16 12:55:15 +0200

    Return stateless validation failures without fatal debug assertions

--- src/Nethermind/Nethermind.Consensus.Test/Stateless/StatelessInputGeneratorTests.cs
+++ src/Nethermind/Nethermind.Consensus.Test/Stateless/StatelessInputGeneratorTests.cs
@@ -34,6 +34,7 @@ using Nethermind.Serialization.Rlp;
 using Nethermind.Specs;
 using Nethermind.Specs.Forks;
 using Nethermind.State;
+using Nethermind.Stateless.Execution;
 using Nethermind.Stateless.Execution.IO;
 using Nethermind.StatelessInputGen;
 using Nethermind.Trie.Pruning;
@@ -43,6 +44,24 @@ namespace Nethermind.Consensus.Test.Stateless;
 
 public class StatelessInputGeneratorTests
 {
+    [Test]
+    public void Direct_execution_rejects_missing_or_unrelated_parent([Values] bool unrelatedParent)
+    {
+        Block block = Build.A.Block.WithParentBeaconBlockRoot(TestItem.KeccakA).TestObject;
+        using Witness witness = EmptyWitness(unrelatedParent ? [Rlp.Encode(Build.A.BlockHeader.TestObject).Bytes] : []);
+
+        Assert.That(StatelessExecutor.Execute(block, witness, new TestSpecProvider(Osaka.Instance)), Is.False);
+    }
+
+    [Test]
+    public void Malformed_input_returns_failure([Values(0, 1)] int length)
+    {
+        byte[] output = StatelessExecutor.Execute(new byte[length]);
+        StatelessValidationResult.Decode(output, out StatelessValidationResult result);
+
+        Assert.That(result.IsSuccess, Is.False);
+    }
+
     [Test]
     public async Task Raw_block_input_recovers_requests_and_public_keys([Values] bool amsterdam)
     {
