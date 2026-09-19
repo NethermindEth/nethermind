6774cacc81e6cc5f35f067d501d2c35e0a2f33bb
lukasz.rozmej <lukasz.rozmej@gmail.com>
2026-09-19 20:28:20 +0200

    probe: classify flat reads per block (measurement only)

--- src/Nethermind/Nethermind.Init/Modules/FlatWorldStateModule.cs
+++ src/Nethermind/Nethermind.Init/Modules/FlatWorldStateModule.cs
@@ -125,7 +125,7 @@ public class FlatWorldStateModule(IFlatDbConfig flatDbConfig) : Module
                 }
 
                 IPersistence cachedReader = new CachedReaderPersistence(persistence, exitSource, logManager);
-                return new CarryForwardCachingPersistence(cachedReader);
+                return new CarryForwardCachingPersistence(cachedReader, logManager: logManager);
             })
             ;
 
