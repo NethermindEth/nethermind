2ccf9d6be4c335ae764f9a6d9084c755474c3f57
lukasz.rozmej <lukasz.rozmej@gmail.com>
2026-09-20 13:17:32 +0200

    experiment: enqueue without waiting for the leading senders

--- src/Nethermind/Nethermind.Merge.Plugin/Handlers/NewPayloadHandler.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Handlers/NewPayloadHandler.cs
@@ -550,7 +550,6 @@ public sealed class NewPayloadHandler : IAsyncHandler<ExecutionPayload, PayloadS
                 // probably the block is already in the processing queue as a result
                 // of a previous newPayload or the block being discovered during syncing
                 // but add it to the processing queue just in case.
-                _senderRecovery.WaitForLeadingSenders(block.Transactions);
                 await _processingQueue.Enqueue(block, processingOptions);
                 (result, validationMessage) = await blockProcessed.Task.TimeoutOn(timeoutTask, cts);
             }
