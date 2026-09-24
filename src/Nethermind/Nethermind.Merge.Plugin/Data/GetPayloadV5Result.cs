c3a50a23aab4caaf3f572392ccb6d612f3bb33e4
Arunim Shukla <54760103+arunimshukla@users.noreply.github.com>
2026-09-24 07:34:11 +0530

    Fix getPayloadV5 fork validation at Amsterdam

--- src/Nethermind/Nethermind.Merge.Plugin/Data/GetPayloadV5Result.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/Data/GetPayloadV5Result.cs
@@ -16,7 +16,11 @@ public class GetPayloadV5Result<TVersionedExecutionPayload>(Block block, UInt256
     public override string ToString() =>
         $"{{ExecutionPayload: {ExecutionPayload}, Fees: {BlockValue}, BlobsBundle blobs count: {BlobsBundle.Blobs.Length}, ShouldOverrideBuilder {ShouldOverrideBuilder}, ExecutionRequests count : {ExecutionRequests?.Length}}}";
 
-    public override bool ValidateFork(ISpecProvider specProvider) => specProvider.GetSpec(Block.Number, Block.Timestamp).IsEip7594Enabled;
+    public override bool ValidateFork(ISpecProvider specProvider)
+    {
+        IReleaseSpec spec = specProvider.GetSpec(Block.Number, Block.Timestamp);
+        return spec.IsEip7594Enabled && !spec.BlockLevelAccessListsEnabled;
+    }
 }
 
 public class GetPayloadV5Result(Block block, UInt256 blockFees, BlobsBundleV2 blobsBundle, byte[][] executionRequests, bool shouldOverrideBuilder) : GetPayloadV5Result<ExecutionPayloadV3>(block, blockFees, blobsBundle, executionRequests, shouldOverrideBuilder);
