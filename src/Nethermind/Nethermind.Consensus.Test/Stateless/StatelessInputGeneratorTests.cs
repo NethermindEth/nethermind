fcfa44b94873e822bfe728b4f8a4872d1d68396d
Ruben Buniatyan <337518+rubo@users.noreply.github.com>
2026-09-24 20:25:36 +0200

    test(stateless): fold public key cases into header hash test

--- src/Nethermind/Nethermind.Consensus.Test/Stateless/StatelessInputGeneratorTests.cs
+++ src/Nethermind/Nethermind.Consensus.Test/Stateless/StatelessInputGeneratorTests.cs
@@ -48,7 +48,7 @@ public class StatelessInputGeneratorTests
     [Test]
     [NonParallelizable]
     public async Task Encoded_execution_checks_reconstructed_header_hash(
-        [Values] bool amsterdam, [Values("valid", "hash", "transactions", "withdrawals")] string mutation)
+        [Values] bool amsterdam, [Values("valid", "hash", "transactions", "withdrawals", "otherKey", "flippedByte", "prefix")] string mutation)
     {
         (Block block, Witness witness, ISpecProvider specProvider) = CreateBlock(amsterdam, currentChainActivation: true);
         using (witness)
@@ -64,6 +64,7 @@ public class StatelessInputGeneratorTests
             {
                 StatelessInput<TPayload>.Decode(encoded.AsSpan(sizeof(ushort)), out StatelessInput<TPayload> input);
                 TPayload payload = input.NewPayloadRequest.ExecutionPayload;
+                byte[] key = [.. input.PublicKeys[0].AsSpan()];
                 switch (mutation)
                 {
                     case "hash": payload.BlockHash = TestItem.KeccakA; break;
@@ -71,45 +72,20 @@ public class StatelessInputGeneratorTests
                     case "withdrawals":
                         payload.Withdrawals = [new SszWithdrawal { Address = TestItem.PrivateKeyA.Address, Amount = 0 }];
                         break;
+                    case "otherKey": key = TestItem.PrivateKeyB.PublicKey.PrefixedBytes; break;
+                    case "flippedByte": key[^1] ^= 1; break;
+                    case "prefix": key[0] = 0x02; break;
                 }
+                input.PublicKeys[0] = SszPublicKey.FromSpan(key);
                 Block reconstructed = input.NewPayloadRequest.ToBlock(requestsEnabled: true)!;
-                Assert.That(HeaderValidator.ValidateHash(reconstructed.Header), Is.EqualTo(mutation == "valid"));
+                Assert.That(HeaderValidator.ValidateHash(reconstructed.Header),
+                    Is.EqualTo(mutation is not ("hash" or "transactions" or "withdrawals")));
                 byte[] body = StatelessInput<TPayload>.Encode(input);
                 byte[] modified = new byte[body.Length + sizeof(ushort)];
                 encoded.AsSpan(0, sizeof(ushort)).CopyTo(modified);
                 body.CopyTo(modified, sizeof(ushort));
                 encoded = modified;
             }
-        }
-    }
-
-    [Test]
-    [NonParallelizable]
-    public async Task Encoded_execution_checks_transaction_public_keys(
-        [Values("valid", "otherKey", "flippedByte", "prefix")] string mutation)
-    {
-        (Block block, Witness witness, ISpecProvider specProvider) = CreateBlock(amsterdam: false, currentChainActivation: true);
-        using (witness)
-        {
-            byte[] encoded = (await InputGenerator.EncodeInput(block, witness, specProvider))!;
-            StatelessInput<SszExecutionPayload>.Decode(encoded.AsSpan(sizeof(ushort)), out StatelessInput<SszExecutionPayload> input);
-
-            byte[] key = [.. input.PublicKeys[0].AsSpan()];
-            switch (mutation)
-            {
-                case "otherKey": key = TestItem.PrivateKeyB.PublicKey.PrefixedBytes; break;
-                case "flippedByte": key[^1] ^= 1; break;
-                case "prefix": key[0] = 0x02; break;
-            }
-            input.PublicKeys[0] = SszPublicKey.FromSpan(key);
-
-            byte[] body = StatelessInput<SszExecutionPayload>.Encode(input);
-            byte[] modified = new byte[body.Length + sizeof(ushort)];
-            encoded.AsSpan(0, sizeof(ushort)).CopyTo(modified);
-            body.CopyTo(modified, sizeof(ushort));
-
-            StatelessValidationResult.Decode(StatelessExecutor.Execute(modified), out StatelessValidationResult result);
-            Assert.That(result.IsSuccess, Is.EqualTo(mutation == "valid"));
         }
     }
 
